337b218016767431c9f6cacc5a9ecbefdba99fb3
Nathan E. Egge <negge@mozilla.com>
2016-03-06 13:41:53 -0500

    Use Daala entropy coder to code trees.
    When building with --enable-daala_ec, calls to aom_write_tree() and
     aom_read_tree() will convert a aom_tree_index structure with associated
     aom_prob probabilities into a CDF on the fly for use with the
     od_ec_encode_cdf_q15().
    The number of symbols in the CDF is capped at 16, and trees that contain
     more than 16 leaf nodes are handled by splitting the most likely, e.g.,
     highest probability symbols, first and coding multiple symbols if
     necessary.
    
    Change-Id: Icb1a8cb854fd81fdd88fbe4bc6761c7eb4757dfe

--- aom_dsp/daalaboolwriter.h
+++ aom_dsp/daalaboolwriter.h
@@ -13,6 +13,7 @@
 #define AOM_DSP_DAALABOOLWRITER_H_
 
 #include "aom_dsp/entenc.h"
+#include "aom_dsp/prob.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,6 +38,47 @@ static INLINE void aom_daala_write(daala_writer *br, int bit, int probability) {
     int p = (32768*probability + (256 - probability)) >> 8;
     od_ec_encode_bool_q15(&br->ec, bit, p);
   }
+}
+
+static INLINE void daala_write_tree_bits(daala_writer *w,
+                                         const aom_tree_index *tree,
+                                         const aom_prob *probs, int bits,
+                                         int len, aom_tree_index i) {
+  aom_tree_index root;
+  root = i;
+  do {
+    uint16_t cdf[16];
+    aom_tree_index index[16];
+    int path[16];
+    int dist[16];
+    int nsymbs;
+    int symb;
+    int j;
+    /* Compute the CDF of the binary tree using the given probabilities. */
+    nsymbs = tree_to_cdf(tree, probs, root, cdf, index, path, dist);
+    /* Find the symbol to code. */
+    symb = -1;
+    for (j = 0; j < nsymbs; j++) {
+      /* If this symbol codes a leaf node,  */
+      if (index[j] <= 0) {
+        if (len == dist[j] && path[j] == bits) {
+          symb = j;
+          break;
+        }
+      }
+      else {
+        if (len > dist[j] && path[j] == bits >> (len - dist[j])) {
+          symb = j;
+          break;
+        }
+      }
+    }
+    OD_ASSERT(symb != -1);
+    od_ec_encode_cdf_q15(&w->ec, symb, cdf, nsymbs);
+    bits &= (1 << (len - dist[symb])) - 1;
+    len -= dist[symb];
+  }
+  while (len);
 }
 
 #ifdef __cplusplus
