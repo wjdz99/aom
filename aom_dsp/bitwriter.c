0017e88dd48008eca48ae0a8e6189f97ce05e816
Nathan E. Egge <negge@mozilla.com>
2016-03-06 12:42:47 -0500

    Use Daala entropy coder to code bits.
    When building with --enable-daala_ec, calls to aom_write() and aom_read()
     use the daala entropy coder to write and read bits.
    When the probability is exactly 0.5 (128), then raw bits are used.
    
    ntt-short-1:
    
              MEDIUM (%) HIGH (%)
        PSNR -0.025337  -0.019822
     PSNRHVS -0.025215  -0.019844
        SSIM -0.025357  -0.019844
    FASTSSIM -0.025357  -0.019803
    
    subset1:
    
             RATE (%)  DSNR (dB)
        PSNR 0.03236  -0.00206
     PSNRHVS 0.03478  -0.00276
        SSIM 0.03239  -0.00158
    FASTSSIM 0.03407  -0.00109
    
    Change-Id: I48ad8eb40fc895d62d6e241ea8abc02820d573f7

--- aom_dsp/bitwriter.c
+++ aom_dsp/bitwriter.c
@@ -13,6 +13,10 @@
 #include <stdio.h>
 
 #include "./bitwriter.h"
+#if CONFIG_DAALA_EC
+#include "entenc.h"
+#include <string.h>
+#endif
 
 void aom_start_encode(aom_writer *br, uint8_t *source) {
   br->lowvalue = 0;
@@ -20,7 +24,11 @@ void aom_start_encode(aom_writer *br, uint8_t *source) {
   br->count = -24;
   br->buffer = source;
   br->pos = 0;
+#if CONFIG_DAALA_EC
+  od_ec_enc_init(&br->ec, 62025);
+#else
   aom_write_bit(br, 0);
+#endif
 #if AOM_MEASURE_EC_OVERHEAD
   br->entropy = 0;
   br->nb_symbols = 0;
@@ -28,12 +36,24 @@ void aom_start_encode(aom_writer *br, uint8_t *source) {
 }
 
 void aom_stop_encode(aom_writer *br) {
+#if CONFIG_DAALA_EC
+  uint32_t daala_bytes;
+  unsigned char *daala_data;
+  daala_data = od_ec_enc_done(&br->ec, &daala_bytes);
+  memcpy(br->buffer, daala_data, daala_bytes);
+  br->pos = daala_bytes;
+  od_ec_enc_clear(&br->ec);
+  // Ensure there's no ambigous collision with any index marker bytes
+  // this always wastes a byte which is not great
+  br->buffer[br->pos++] = 0;
+#else
   int i;
 
   for (i = 0; i < 32; i++) aom_write_bit(br, 0);
 
   // Ensure there's no ambigous collision with any index marker bytes
   if ((br->buffer[br->pos - 1] & 0xe0) == 0xc0) br->buffer[br->pos++] = 0;
+#endif
 
 #if AOM_MEASURE_EC_OVERHEAD
   {
