fb39451be454d68492fad64ca2b3c72ccb0f0fcd
Nathan E. Egge <negge@mozilla.com>
2016-03-09 14:22:09 -0500

    Add entropy coder overhead computation.
    Port the Daala entropy coder overhead computation to aom_write() behind
     the AOM_MEASURE_EC_OVERHEAD define.
    The overhead and efficiency (bits / symbol) are printed to stderr when
     aom_stop_encode() is called.
    
    Change-Id: I9eae15225f830c9fa13784f298d7d02c0925d7c6

--- aom_dsp/bitwriter.c
+++ aom_dsp/bitwriter.c
@@ -10,6 +10,7 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
 
 #include "./bitwriter.h"
 
@@ -20,6 +21,10 @@ void aom_start_encode(aom_writer *br, uint8_t *source) {
   br->buffer = source;
   br->pos = 0;
   aom_write_bit(br, 0);
+#if AOM_MEASURE_EC_OVERHEAD
+  br->entropy = 0;
+  br->nb_symbols = 0;
+#endif
 }
 
 void aom_stop_encode(aom_writer *br) {
@@ -29,4 +34,14 @@ void aom_stop_encode(aom_writer *br) {
 
   // Ensure there's no ambigous collision with any index marker bytes
   if ((br->buffer[br->pos - 1] & 0xe0) == 0xc0) br->buffer[br->pos++] = 0;
+
+#if AOM_MEASURE_EC_OVERHEAD
+  {
+    uint32_t tell;
+    tell = br->pos << 3;
+    fprintf(stderr, "overhead: %f%%\n", 100*(tell - br->entropy)/br->entropy);
+    fprintf(stderr, "efficiency: %f bits/symbol\n",
+     (double)tell/br->nb_symbols);
+  }
+#endif
 }
