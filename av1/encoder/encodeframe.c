df6ed691dd50f2d6afae1a5c76908f85a6d71a8f
Jingning Han <jingning@google.com>
2016-04-12 09:38:17 -0700

    Store predicted mv in the collocated reference block
    This commit makes the codec to store the predicted motion vector
    at 8x8 block level and assign them when the reference motion vector
    is obtained from collocated blocks in the previous frame.
    
    Change-Id: I7e0403dad1df012f3994846939bb799e2445736c

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1038,6 +1038,10 @@ static void update_state(AV1_COMP *cpi, ThreadData *td, PICK_MODE_CONTEXT *ctx,
       mv->ref_frame[1] = mi->mbmi.ref_frame[1];
       mv->mv[0].as_int = mi->mbmi.mv[0].as_int;
       mv->mv[1].as_int = mi->mbmi.mv[1].as_int;
+#if CONFIG_REF_MV
+      mv->pred_mv[0].as_int = mi->mbmi.pred_mv[0].as_int;
+      mv->pred_mv[1].as_int = mi->mbmi.pred_mv[1].as_int;
+#endif
     }
   }
 }
