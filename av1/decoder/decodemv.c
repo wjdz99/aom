b02fb4bf2bfee2fbc97f7d269eae6160c18a7769
Nathan E. Egge <negge@mozilla.com>
2016-04-26 11:37:06 -0400

    Update switchable_interp_cdf once per frame.
    Move from computing the switchable_interp_cdf per symbol to
     computing once per frame when the probabilities are adapted.
    
    Change-Id: I6571126239f0327e22bb09ee8bad94114291683e

--- av1/decoder/decodemv.c
+++ av1/decoder/decodemv.c
@@ -536,8 +536,13 @@ static INLINE InterpFilter read_switchable_interp_filter(AV1_COMMON *const cm,
                                                          MACROBLOCKD *const xd,
                                                          aom_reader *r) {
   const int ctx = av1_get_pred_context_switchable_interp(xd);
+#if CONFIG_DAALA_EC
+  const InterpFilter type = (InterpFilter)aom_read_tree_cdf(r,
+      cm->fc->switchable_interp_cdf[ctx], SWITCHABLE_FILTERS);
+#else
   const InterpFilter type = (InterpFilter)aom_read_tree(
       r, av1_switchable_interp_tree, cm->fc->switchable_interp_prob[ctx]);
+#endif
   FRAME_COUNTS *counts = xd->counts;
   if (counts) ++counts->switchable_interp[ctx][type];
   return type;
