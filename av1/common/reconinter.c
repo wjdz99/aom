dbb6e3a44f9e485c665c8004dc692ca9ea678fc0
Angie Chiang <angiebird@google.com>
2016-06-08 12:18:48 -0700

    Add sharp2 and smooth2 interpolation filter
    PSNR gain
    hbd lowres dataset: 0.811%
    lbd lowres dataset: 0.895%
    
    Change-Id: Id6cc426883de725adf1a630f4151d5018c4b819b

--- av1/common/reconinter.c
+++ av1/common/reconinter.c
@@ -59,9 +59,9 @@ void av1_build_inter_predictor(const uint8_t *src, int src_stride, uint8_t *dst,
                   h, ref, interp_filter, sf->x_step_q4, sf->y_step_q4);
 }
 
-void build_inter_predictors(MACROBLOCKD *xd, int plane, int block, int bw,
-                            int bh, int x, int y, int w, int h, int mi_x,
-                            int mi_y) {
+static void build_inter_predictors(MACROBLOCKD *xd, int plane, int block,
+                                   int bw, int bh, int x, int y, int w, int h,
+                                   int mi_x, int mi_y) {
   struct macroblockd_plane *const pd = &xd->plane[plane];
   const MODE_INFO *mi = xd->mi[0];
   const int is_compound = has_second_ref(&mi->mbmi);
