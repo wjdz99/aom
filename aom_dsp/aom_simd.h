e06408838dca97566ad34c382c1dffb6829821ee
Steinar Midtskogen <stemidts@cisco.com>
2016-08-03 13:17:33 +0200

    Added generic SIMD library supporting x86 SSE2+ and ARM NEON.
    Change-Id: I037f4c44f621a7e909b82ccb6a299d41bcbf8607

---
+++ aom_dsp/aom_simd.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#ifndef _AOM_SIMD_H
+#define _AOM_SIMD_H
+
+#ifndef SIMD_INLINE
+#ifdef __GNUC__
+#define SIMD_INLINE static inline __attribute__((always_inline))
+#elif __STDC_VERSION__ >= 199901L
+#define SIMD_INLINE static inline
+#elif defined(_MSC_VER)
+#define SIMD_INLINE static __inline
+#else
+#define SIMD_INLINE static
+#endif
+#endif
+
+#include <stdint.h>
+
+#if defined(_WIN32)
+#include <intrin.h>
+#endif
+
+#include "./aom_config.h"
+
+static const int simd_check = 1;
+
+#if HAVE_NEON
+static const int simd_available = 1;
+#include "simd/v128_intrinsics_arm.h"
+#elif HAVE_SSE2
+static const int simd_available = 1;
+#include "simd/v128_intrinsics_x86.h"
+#else
+static const int simd_available = 0;
+#include "simd/v128_intrinsics.h"
+#endif
+
+#endif /* _AOM_SIMD_H */
