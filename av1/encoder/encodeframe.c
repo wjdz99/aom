a53256b53413f10e22688bcb90cde0512c27baec
Urvang Joshi <urvang@google.com>
2016-08-15 16:52:18 -0700

    Move palette code from libvpx/nextgenv2 to aomedia.
    Palette code originally by huisu@.
    Last 7 small speedup patches from me, giving 10% speedup overall for
    screen content set.
    
    From libvpx/nextgenv2:
    c93e5cc : Bring palette back to nextgenv2
    827e1b3 : Add test for screen content coding tools in end to end test
    78b0bd0 : Complete (mostly) migration of palette mode
    8a128c2 : Fixes for Palette mode
    4ab0091 : Palette mode: record selected transform type
    cbb8be7 : Set block size upper bound for Palette mode
    bb0e692 : Convert palette from double to float.
    a0a23b7 : faster code: replace nested for loops by memcpy().
    016a5da : Palette code: simpler and faster duplicate removal
    d000020 : Palette code cleanup
    6dde801 : Use built-in qsort(); create remove_dup() method.
    f746c10 : Handle centroid rounding inside palette.c itself.
    d68c7b6 : Palette: count Y colors only for screen content.
    f1906e9 : Palette code: remove the use of same if condition twice.
    
    Locally made sure all combinations of "--enable-ext-intra" and/or
    "--enable-palette" and/or "--enable-aom-highbitdepth" build OK.
    
    P.S. Note for future reference:
    EXT_INTRA experiment has only been moved *partially* to aomedia at this
    point. If and when the rest is moved to aomedia, EXT_INTRA + PALETTE
    combo needs to be moved to aomedia as well.
    
    Change-Id: I8826780014ec8a88281e56c8258b7c966121b46f

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1017,6 +1017,19 @@ static void rd_pick_sb_modes(const AV1_COMP *const cpi, TileDataEnc *tile_data,
     p[i].eobs = ctx->eobs[i];
   }
 
+#if CONFIG_PALETTE
+  if (cm->current_video_frame == 0 && cm->allow_screen_content_tools) {
+    for (i = 0; i < 2; ++i) {
+      if (ctx->color_index_map[i] == NULL) {
+        AV1_COMMON *const mutable_cm = (AV1_COMMON *)cm;
+        CHECK_MEM_ERROR(mutable_cm, ctx->color_index_map[i],
+                        aom_memalign(16, (ctx->num_4x4_blk << 4) *
+                                             sizeof(*ctx->color_index_map[i])));
+      }
+    }
+  }
+#endif  // CONFIG_PALETTE
+
   ctx->skippable = 0;
   ctx->pred_pixel_ready = 0;
 
@@ -2997,7 +3010,19 @@ static void encode_superblock(const AV1_COMP *const cpi, ThreadData *td,
     mbmi->skip = 1;
     for (plane = 0; plane < MAX_MB_PLANE; ++plane)
       av1_encode_intra_block_plane(x, AOMMAX(bsize, BLOCK_8X8), plane);
-
+#if CONFIG_PALETTE
+    if (bsize >= BLOCK_8X8 && output_enabled) {
+      for (plane = 0; plane <= 1; ++plane) {
+        if (mbmi->palette_mode_info.palette_size[plane] > 0) {
+          mbmi->palette_mode_info.palette_first_color_idx[plane] =
+              xd->plane[plane].color_index_map[0];
+          // TODO(huisu): this increases the use of token buffer. Needs stretch
+          // test to verify.
+          av1_tokenize_palette_sb(td, bsize, plane, t);
+        }
+      }
+    }
+#endif  // CONFIG_PALETTE
     av1_tokenize_sb(cpi, td, t, !output_enabled, AOMMAX(bsize, BLOCK_8X8));
   } else {
     int ref;
