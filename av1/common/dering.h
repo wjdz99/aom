02e9216e53b65744d8a0a2fb22019c4f9fb3512d
Jean-Marc Valin <jmvalin@mozilla.com>
2016-10-09 13:36:39 -0400

    compute bskip as we go
    Change-Id: I5681e3329ad3677296161de59f5ff1236a14f086

--- av1/common/dering.h
+++ av1/common/dering.h
@@ -29,6 +29,8 @@ extern "C" {
 
 int compute_level_from_index(int global_level, int gi);
 int sb_all_skip(const AV1_COMMON *const cm, int mi_row, int mi_col);
+int sb_all_skip_out(const AV1_COMMON *const cm, int mi_row, int mi_col,
+    unsigned char *bskip);
 void av1_dering_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
                       MACROBLOCKD *xd, int global_level);
 
