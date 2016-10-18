9b180a0bee5eb4cd661d591214d903012b3e457d
Jean-Marc Valin <jmvalin@mozilla.com>
2016-10-18 17:39:03 -0400

    Deringing support for 4:2:2 by not deringing chroma
    No change in output for 4:2:0 and 4:4:4
    
    Change-Id: Ic46753d23a5b5f90b611a3da1a4574870519957c

--- av1/common/dering.c
+++ av1/common/dering.c
@@ -111,11 +111,12 @@ void copy_blocks_16_8bit(uint8_t *dst, int dstride, int16_t *src,
 }
 
 /* TODO: Optimize this function for SSE. */
-static void copy_sb8_16(AV1_COMMON *cm, int16_t * restrict dst, int dstride,
-    const uint8_t * restrict src, int src_voffset, int src_hoffset, int sstride,
+static void copy_sb8_16(AV1_COMMON *cm, int16_t *dst, int dstride,
+    const uint8_t *src, int src_voffset, int src_hoffset, int sstride,
     int vsize, int hsize)
 {
   int r, c;
+  (void)cm;
 #if CONFIG_AOM_HIGHBITDEPTH
   if (cm->use_highbitdepth) {
     const uint16_t *base = &CONVERT_TO_SHORTPTR(src)[src_voffset * sstride
@@ -157,6 +158,12 @@ void av1_dering_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
   int pli;
   int last_sbc;
   int coeff_shift = AOMMAX(cm->bit_depth - 8, 0);
+  int nplanes;
+  if (xd->plane[1].subsampling_x == xd->plane[1].subsampling_y &&
+      xd->plane[2].subsampling_x == xd->plane[2].subsampling_y)
+    nplanes = 3;
+  else
+    nplanes = 1;
   nvsb = (cm->mi_rows + MAX_MIB_SIZE - 1) / MAX_MIB_SIZE;
   nhsb = (cm->mi_cols + MAX_MIB_SIZE - 1) / MAX_MIB_SIZE;
   av1_setup_dst_planes(xd->plane, frame, 0, 0);
@@ -164,17 +171,17 @@ void av1_dering_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
   memset(row_dering, 1, sizeof(*row_dering) * (nhsb + 2) * 2);
   prev_row_dering = row_dering + 1;
   curr_row_dering = prev_row_dering + nhsb + 2;
-  for (pli = 0; pli < 3; pli++) {
+  for (pli = 0; pli < nplanes; pli++) {
     dec[pli] = xd->plane[pli].subsampling_x;
     bsize[pli] = 3 - dec[pli];
   }
   stride = (cm->mi_cols << bsize[0]) + 2*OD_FILT_HBORDER;
-  for (pli = 0; pli < 3; pli++) {
+  for (pli = 0; pli < nplanes; pli++) {
     linebuf[pli] = aom_malloc(sizeof(*linebuf) * OD_FILT_VBORDER * stride);
   }
   for (sbr = 0; sbr < nvsb; sbr++) {
     last_sbc = -1;
-    for (pli = 0; pli < 3; pli++) {
+    for (pli = 0; pli < nplanes; pli++) {
       for (r = 0; r < (MAX_MIB_SIZE << bsize[pli]) + 2*OD_FILT_VBORDER; r++) {
         for (c = 0; c < OD_FILT_HBORDER; c++) {
           colbuf[pli][r][c] = OD_DERING_VERY_LARGE;
@@ -199,7 +206,7 @@ void av1_dering_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
               sbc * MAX_MIB_SIZE, dlist)) == 0)
         continue;
       curr_row_dering[sbc] = 1;
-      for (pli = 0; pli < 3; pli++) {
+      for (pli = 0; pli < nplanes; pli++) {
         int16_t dst[MAX_MIB_SIZE * MAX_MIB_SIZE * 8 * 8];
         int threshold;
         int coffset;
@@ -367,7 +374,7 @@ void av1_dering_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
     }
   }
   aom_free(row_dering);
-  for (pli = 0; pli < 3; pli++) {
+  for (pli = 0; pli < nplanes; pli++) {
     aom_free(linebuf[pli]);
   }
 }
