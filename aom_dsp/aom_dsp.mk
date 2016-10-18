df471a37739ccc62bbdec4709779a0a34ac79f12
Steinar Midtskogen <stemidts@cisco.com>
2016-10-18 08:04:28 +0200

    Define SIMD_INLINE using AOM_FORCE_INLINE
    Change-Id: I7d6394e48e9b6093e5b523387ed250f371ee7fb9

--- aom_dsp/aom_dsp.mk
+++ aom_dsp/aom_dsp.mk
@@ -367,5 +367,7 @@ DSP_SRCS-yes += aom_dsp_rtcd.c
 DSP_SRCS-yes += aom_dsp_rtcd_defs.pl
 
 DSP_SRCS-yes += aom_simd.c
+DSP_SRCS-yes += aom_simd.h
+DSP_SRCS-yes += aom_simd_inline.h
 
 $(eval $(call rtcd_h_template,aom_dsp_rtcd,aom_dsp/aom_dsp_rtcd_defs.pl))
