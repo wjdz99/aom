8a3be698524ea5aff04f9537eb14bb45b92b3fca
Debargha Mukherjee <debargha@google.com>
2016-11-13 11:35:44 -0800

    Some refactoring and improvements to gobal motion
    To facilitate further experiments
    
    Change-Id: I38af6cb83723f92baed23e28d7fc9a0a9544757e

--- av1/common/warped_motion.c
+++ av1/common/warped_motion.c
@@ -451,8 +451,8 @@ static double highbd_warp_erroradv(WarpedMotionParams *wm, uint8_t *ref8,
       gm_err = dst[(j - p_col) + (i - p_row) * p_stride] -
                highbd_warp_interpolate(ref, out[0], out[1], width, height,
                                        stride, bd);
-      no_gm_err = dst[(j - p_col) + (i - p_row) * p_stride] -
-                  ref[(j - p_col) + (i - p_row) * stride];
+      no_gm_err =
+          dst[(j - p_col) + (i - p_row) * p_stride] - ref[j + i * stride];
       gm_err = abs(gm_err);
       no_gm_err = abs(no_gm_err);
       gm_sumerr += (int64_t)AOMMIN(gm_err, ERRORADV_CLAMP << (bd - 8));
@@ -514,8 +514,8 @@ static double warp_erroradv(WarpedMotionParams *wm, uint8_t *ref, int width,
       out[1] = ROUND_POWER_OF_TWO_SIGNED(out[1] * y_scale, 4);
       gm_err = dst[(j - p_col) + (i - p_row) * p_stride] -
                warp_interpolate(ref, out[0], out[1], width, height, stride);
-      no_gm_err = dst[(j - p_col) + (i - p_row) * p_stride] -
-                  ref[(j - p_col) + (i - p_row) * stride];
+      no_gm_err =
+          dst[(j - p_col) + (i - p_row) * p_stride] - ref[j + i * stride];
       gm_err = abs(gm_err);
       no_gm_err = abs(no_gm_err);
       gm_sumerr += AOMMIN(gm_err, ERRORADV_CLAMP);
