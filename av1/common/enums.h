2debc9f98e097df6daa0dd544d2f094a858bc1fc
Debargha Mukherjee <debargha@google.com>
2016-11-17 09:59:14 -0800

    Add default 64x64 scan orders and entropy models
    Adds 64x64 default scan orders
    Adds 64x64 coefficient entropy models (just copy the 32x32 ones)
    Entropy context updates for 64x64 transform
    Various misc. changes to support 64x64 transforms
    
    Change-Id: I2c0bc4ba540886dd196e87a78b205407ad3e866b

--- av1/common/enums.h
+++ av1/common/enums.h
@@ -157,7 +157,7 @@ typedef enum ATTRIBUTE_PACKED {
   TX_INVALID = 255    // Invalid transform size
 } TX_SIZE;
 
-#define MAX_TX_DEPTH (TX_32X32 - TX_4X4)
+#define MAX_TX_DEPTH (TX_SIZES - 1 - TX_4X4)
 
 #define MAX_TX_SIZE_LOG2 (5 + CONFIG_TX64X64)
 #define MAX_TX_SIZE (1 << MAX_TX_SIZE_LOG2)
