4ab58b55780c5a2a3173d51d117b7626bef730af
Sarah Parker <sarahparker@google.com>
2016-11-16 17:47:13 -0800

    Refactor ext-inter to loop through all masked modes in rdopt
    No change in performance
    
    Change-Id: Ie105a7baf6a2c2258d3ef117e727957e4393f51b

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1975,8 +1975,9 @@ static void update_stats(const AV1_COMMON *const cm, ThreadData *td, int mi_row,
             !(is_motion_variation_allowed(mbmi) &&
               mbmi->motion_mode != SIMPLE_TRANSLATION) &&
 #endif  // CONFIG_MOTION_VAR || CONFIG_WARPED_MOTION
-            is_interinter_wedge_used(bsize)) {
-          counts->compound_interinter[bsize][mbmi->interinter_compound]++;
+            ) {
+          counts->compound_interinter[bsize]
+                                     [mbmi->interinter_compound_data.type]++;
         }
 #endif  // CONFIG_EXT_INTER
       }
