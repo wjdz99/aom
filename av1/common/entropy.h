f278e1e4ccd7556a7822fd5373f6ff8c71d1653d
Thomas Davies <thdavies@cisco.com>
2016-11-21 10:54:29 +0000

    EC_MULTISYMBOL: merge ZERO_TOKEN into coding scheme.
    Zero, one, and two or more coded as one symbol (head).
    Remaining tokens coded as a tail symbol.
    
    The pareto CDF distribution is adjusted to cover tokens from
    two onwards.
    
    Change-Id: I98b33fab6b9f52690f6ad618ac55e725a97be056

--- av1/common/entropy.h
+++ av1/common/entropy.h
@@ -200,6 +200,8 @@ typedef aom_cdf_prob coeff_cdf_model[REF_TYPES][COEF_BANDS][COEFF_CONTEXTS]
                                     [ENTROPY_TOKENS];
 extern const aom_cdf_prob av1_pareto8_token_probs[COEFF_PROB_MODELS]
                                                  [ENTROPY_TOKENS - 2];
+extern const aom_cdf_prob av1_pareto8_tail_cdfs[COEFF_PROB_MODELS]
+                                               [ENTROPY_TOKENS - 3];
 struct frame_contexts;
 void av1_coef_pareto_cdfs(struct frame_contexts *fc);
 #endif  // CONFIG_EC_MULTISYMBOL
