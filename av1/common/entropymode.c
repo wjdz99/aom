1830434b42dc3995814664ba874e2ba1ddcc6b3c
Jingning Han <jingning@google.com>
2016-12-21 16:02:07 -0800

    Make cb4x4 mode support supertx
    This commit makes the cb4x4 mode support supertx operation.
    
    Change-Id: I1a713b2268c1029aebeb43aa6aeb0fa37b16810f

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -1674,8 +1674,18 @@ static const aom_prob default_supertx_prob[PARTITION_SUPERTX_CONTEXTS]
                                             { 1, 160, 160, 170, 180 },
                                             { 1, 200, 200, 210, 220 },
 #else
+#if CONFIG_CB4X4
+#if CONFIG_TX64X64
+                                            { 1, 1, 160, 160, 170, 180 },
+                                            { 1, 1, 200, 200, 210, 220 },
+#else
+                                            { 1, 1, 160, 160, 170 },
+                                            { 1, 1, 200, 200, 210 },
+#endif  // CONFIG_TX64X64
+#else
                                             { 1, 160, 160, 170 },
                                             { 1, 200, 200, 210 },
+#endif  // CONFIG_CB4X4
 #endif  // CONFIG_TX64X64
                                           };
 #endif  // CONFIG_SUPERTX
