ab49eae42e60197ec8016c8a09fbf9f8a5a7930c
Nathan E. Egge <negge@mozilla.com>
2017-01-04 15:15:06 -0500

    Use aom_reader with od_decode_cdf_adapt().
    Change the od_decode_cdf_adapt() function to take an aom_reader
     struct instead of an od_ec_dec struct.
    Rename od_decode_cdf_adapt() to aom_decode_cdf_adapt().
    
    Change-Id: I0713d2f56acfea3f67f1b4087c0feee77c2e25cb

--- av1/common/generic_code.h
+++ av1/common/generic_code.h
@@ -25,12 +25,14 @@
 # define generic_decode(dec, model, max, ex_q16, integration, str) generic_decode_(dec, model, max, ex_q16, integration, str)
 # define aom_decode_cdf_adapt_q15(r, cdf, n, count, rate, str) \
   aom_decode_cdf_adapt_q15_(r, cdf, n, count, rate, str)
-# define od_decode_cdf_adapt(ec, cdf, n, increment, str) od_decode_cdf_adapt_(ec, cdf, n, increment, str)
+# define od_decode_cdf_adapt(r, cdf, n, increment, str) \
+  od_decode_cdf_adapt_(r, cdf, n, increment, str)
 #else
 # define generic_decode(dec, model, max, ex_q16, integration, str) generic_decode_(dec, model, max, ex_q16, integration)
 # define aom_decode_cdf_adapt_q15(r, cdf, n, count, rate, str) \
   aom_decode_cdf_adapt_q15_(r, cdf, n, count, rate)
-# define od_decode_cdf_adapt(ec, cdf, n, increment, str) od_decode_cdf_adapt_(ec, cdf, n, increment)
+# define od_decode_cdf_adapt(r, cdf, n, increment, str) \
+  od_decode_cdf_adapt_(r, cdf, n, increment)
 #endif
 
 typedef struct {
@@ -66,7 +68,7 @@ void aom_encode_cdf_adapt_q15(aom_writer *w, int val, uint16_t *cdf, int n,
 void aom_encode_cdf_adapt(aom_writer *w, int val, uint16_t *cdf, int n,
  int increment);
 
-int od_decode_cdf_adapt_(od_ec_dec *ec, uint16_t *cdf, int n,
+int aom_decode_cdf_adapt_(aom_reader *r, uint16_t *cdf, int n,
  int increment OD_ACC_STR);
 
 void generic_encode(aom_writer *w, generic_encoder *model, int x, int max,
