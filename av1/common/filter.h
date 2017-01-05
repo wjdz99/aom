958447e1ca780c26a743e65e622e1ab2d058a6f6
Angie Chiang <angiebird@google.com>
2017-01-05 09:54:51 -0800

    Remove unused SUPPORT_NONINTERPOLATING_FILTERS
    Change-Id: I5ef2d3fe49553710692071963a3884f8b79ecb7e

--- av1/common/filter.h
+++ av1/common/filter.h
@@ -30,8 +30,7 @@ extern "C" {
 
 #define MAX_SUBPEL_TAPS 12
 
-#define SUPPORT_NONINTERPOLATING_FILTERS 0 /* turn on for experimentation */
-#define SWITCHABLE_FILTERS 4               /* Number of switchable filters */
+#define SWITCHABLE_FILTERS 4 /* Number of switchable filters */
 #define LOG_SWITCHABLE_FILTERS \
   3 /* (1 << LOG_SWITCHABLE_FILTERS) > SWITCHABLE_FILTERS */
 #else
