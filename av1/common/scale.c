16d15a2441a942c0620e7a1a5e4574f638f1507e
Angie Chiang <angiebird@google.com>
2017-01-05 09:54:51 -0800

    Remove unused SUPPORT_NONINTERPOLATING_FILTERS
    Change-Id: I5ef2d3fe49553710692071963a3884f8b79ecb7e

--- av1/common/scale.c
+++ av1/common/scale.c
@@ -76,16 +76,6 @@ void av1_setup_scale_factors_for_frame(struct scale_factors *sf, int other_w,
 // applied in one direction only, and not at all for 0,0, seems to give the
 // best quality, but it may be worth trying an additional mode that does
 // do the filtering on full-pel.
-#if CONFIG_EXT_INTERP && SUPPORT_NONINTERPOLATING_FILTERS
-  sf->predict_ni[0][0][0] = aom_convolve8_c;
-  sf->predict_ni[0][0][1] = aom_convolve8_avg_c;
-  sf->predict_ni[0][1][0] = aom_convolve8_c;
-  sf->predict_ni[0][1][1] = aom_convolve8_avg_c;
-  sf->predict_ni[1][0][0] = aom_convolve8_c;
-  sf->predict_ni[1][0][1] = aom_convolve8_avg_c;
-  sf->predict_ni[1][1][0] = aom_convolve8;
-  sf->predict_ni[1][1][1] = aom_convolve8_avg;
-#endif  // CONFIG_EXT_INTERP && SUPPORT_NONINTERPOLATING_FILTERS
   if (sf->x_step_q4 == 16) {
     if (sf->y_step_q4 == 16) {
       // No scaling in either direction.
@@ -129,16 +119,6 @@ void av1_setup_scale_factors_for_frame(struct scale_factors *sf, int other_w,
 
 #if CONFIG_AOM_HIGHBITDEPTH
   if (use_highbd) {
-#if CONFIG_EXT_INTERP && SUPPORT_NONINTERPOLATING_FILTERS
-    sf->highbd_predict_ni[0][0][0] = aom_highbd_convolve8_c;
-    sf->highbd_predict_ni[0][0][1] = aom_highbd_convolve8_avg_c;
-    sf->highbd_predict_ni[0][1][0] = aom_highbd_convolve8_c;
-    sf->highbd_predict_ni[0][1][1] = aom_highbd_convolve8_avg_c;
-    sf->highbd_predict_ni[1][0][0] = aom_highbd_convolve8_c;
-    sf->highbd_predict_ni[1][0][1] = aom_highbd_convolve8_avg_c;
-    sf->highbd_predict_ni[1][1][0] = aom_highbd_convolve8;
-    sf->highbd_predict_ni[1][1][1] = aom_highbd_convolve8_avg;
-#endif  // CONFIG_EXT_INTERP && SUPPORT_NONINTERPOLATING_FILTERS
     if (sf->x_step_q4 == 16) {
       if (sf->y_step_q4 == 16) {
         // No scaling in either direction.
