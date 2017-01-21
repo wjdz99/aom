affec68e129b1e8e271d86dd274afc25913c10c3
Zoe Liu <zoeliu@google.com>
2017-01-20 19:41:23 -0800

    Add a new experiment COMP_TRIPRED
    This experiment is targeted to implement the idea of uniform
    tri-prediction, which adds a 3rd inter prediction on top of the
    baseline compound inter prediction. The final predictor for a inter-
    coded block is the average of the compound bi-predictor and the third
    predictor, which is signaled by a third reference frame and a third
    motion vector.
    
    Change-Id: I9ebcf0589e3605494a328c9bdf4aef6627378d00

--- av1/common/reconinter.h
+++ av1/common/reconinter.h
@@ -573,6 +573,15 @@ void av1_build_wedge_inter_predictor_from_buf(MACROBLOCKD *xd, BLOCK_SIZE bsize,
                                               int ext_dst_stride1[3]);
 #endif  // CONFIG_EXT_INTER
 
+#if CONFIG_COMP_TRIPRED
+void av1_build_single_inter_predictor_for_planes_from_any_ref(
+    const AV1_COMMON *cm, const MACROBLOCKD *xd, BLOCK_SIZE bsize,
+    int plane_from, int plane_to, int mi_row, int mi_col,
+    MV_REFERENCE_FRAME ref_frame, int_mv mv,
+    const struct buf_2d yv12_mb_one_ref[MAX_MB_PLANE], uint8_t *ext_dst[3],
+    int ext_dst_stride[3]);
+#endif  // CONFIG_COMP_TRIPRED
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
