d86b70447077be19cb8b1eca547ecaeef51e3c17
zhipin deng <zhipin.deng@intel.com>
2017-01-23 22:04:13 +0800

    add --enable-mv-compress to reduce the storage of temporal mv candidate
    Change-Id: Ic2b31f98533a5d11d3291c742071455713a08357

--- av1/common/mvref_common.c
+++ av1/common/mvref_common.c
@@ -345,8 +345,13 @@ static int add_col_ref_mv(const AV1_COMMON *cm,
   int coll_blk_count = 0;
   const int weight_unit = mi_size_wide[BLOCK_8X8];
 
+#if CONFIG_MV_COMPRESS
+  mi_pos.row = (mi_row & 0x01) ? blk_row : blk_row + 1;
+  mi_pos.col = (mi_col & 0x01) ? blk_col : blk_col + 1;
+#else
   mi_pos.row = blk_row;
   mi_pos.col = blk_col;
+#endif
 
   if (!is_inside(&xd->tile, mi_col, mi_row, &mi_pos)) return coll_blk_count;
 
@@ -388,10 +393,18 @@ static void setup_ref_mv_list(const AV1_COMMON *cm, const MACROBLOCKD *xd,
   CANDIDATE_MV tmp_mv;
   int len, nr_len;
 
+#if CONFIG_MV_COMPRESS
+  const MV_REF *const prev_frame_mvs_base =
+      cm->use_prev_frame_mvs
+          ? cm->prev_frame->mvs + (((mi_row >> 1) << 1) + 1) * cm->mi_cols +
+                ((mi_col >> 1) << 1) + 1
+          : NULL;
+#else
   const MV_REF *const prev_frame_mvs_base =
       cm->use_prev_frame_mvs
           ? cm->prev_frame->mvs + mi_row * cm->mi_cols + mi_col
           : NULL;
+#endif
 
   const int bs = AOMMAX(xd->n8_w, xd->n8_h);
   const int has_tr = has_top_right(xd, mi_row, mi_col, bs);
@@ -546,11 +559,27 @@ static void find_mv_refs_idx(const AV1_COMMON *cm, const MACROBLOCKD *xd,
 #endif
   int different_ref_found = 0;
   int context_counter = 0;
+#if CONFIG_SIMP_REF_MV
+  const TileInfo *const tile_ = &xd->tile;
+  int mi_row_end = tile_->mi_row_end;
+  int mi_col_end = tile_->mi_col_end;
+  const MV_REF *const prev_frame_mvs =
+      cm->use_prev_frame_mvs
+          ? cm->prev_frame->mvs +
+                AOMMIN(((mi_row >> 1) << 1) + 1 + (((xd->n8_h - 1) >> 1) << 1),
+                       mi_row_end - 1) *
+                    cm->mi_cols +
+                AOMMIN(((mi_col >> 1) << 1) + 1 + (((xd->n8_w - 1) >> 1) << 1),
+                       mi_col_end - 1)
+          : NULL;
+#else
   const MV_REF *const prev_frame_mvs =
       cm->use_prev_frame_mvs
           ? cm->prev_frame->mvs + mi_row * cm->mi_cols + mi_col
           : NULL;
   const TileInfo *const tile = &xd->tile;
+#endif
+
   const BLOCK_SIZE bsize = mi->mbmi.sb_type;
   const int bw = block_size_wide[AOMMAX(bsize, BLOCK_8X8)];
   const int bh = block_size_high[AOMMAX(bsize, BLOCK_8X8)];
