ac0afdda2d4103ca59559d51855d1428433144d4
Steinar Midtskogen <stemidts@cisco.com>
2017-02-03 15:27:09 +0100

    New clipping function for CLPF (experimental)
    Change-Id: Iaa01fa8d5c2390057f37e58a616db15a6c862636

--- av1/common/clpf.c
+++ av1/common/clpf.c
@@ -14,16 +14,28 @@
 #include "aom/aom_image.h"
 #include "aom_dsp/aom_dsp_common.h"
 
-int av1_clpf_sample(int X, int A, int B, int C, int D, int E, int F, int b) {
-  int delta = 4 * clamp(A - X, -b, b) + clamp(B - X, -b, b) +
-              3 * clamp(C - X, -b, b) + 3 * clamp(D - X, -b, b) +
-              clamp(E - X, -b, b) + 4 * clamp(F - X, -b, b);
+int sign(int i) { return i < 0 ? -1 : 1; }
+
+int constrain(int x, int s, unsigned int bitdepth) {
+  return sign(x) *
+    AOMMAX(0, abs(x) - AOMMAX(0, abs(x) - s + (abs(x) >> (bitdepth-3-get_msb(s)))));
+  
+}
+
+int av1_clpf_sample(int X, int A, int B, int C, int D, int E, int F, int G, int H, int I, int J, int K, int L, int b, unsigned int bd) {
+  int delta =
+    3 * constrain(A - X, b, bd) + 1 * constrain(B - X, b, bd) +
+    3 * constrain(C - X, b, bd) + 3 * constrain(D - X, b, bd) +
+    1 * constrain(E - X, b, bd) + 3 * constrain(F - X, b, bd) +
+    1 * constrain(G - X, b, bd) + 1 * constrain(H - X, b, bd) +
+    0 * constrain(I - X, b, bd) + 0 * constrain(J - X, b, bd) +
+    0 * constrain(K - X, b, bd) + 0 * constrain(L - X, b, bd);
   return (8 + delta - (delta < 0)) >> 4;
 }
 
 void aom_clpf_block_c(const uint8_t *src, uint8_t *dst, int sstride,
                       int dstride, int x0, int y0, int sizex, int sizey,
-                      int width, int height, unsigned int strength) {
+                      int width, int height, unsigned int strength, unsigned int bitdepth) {
   int x, y;
   for (y = y0; y < y0 + sizey; y++) {
     for (x = x0; x < x0 + sizex; x++) {
@@ -34,8 +46,16 @@ void aom_clpf_block_c(const uint8_t *src, uint8_t *dst, int sstride,
       int D = src[y * sstride + AOMMIN(width - 1, x + 1)];
       int E = src[y * sstride + AOMMIN(width - 1, x + 2)];
       int F = src[AOMMIN(height - 1, y + 1) * sstride + x];
+      int G = src[AOMMAX(0, y - 2) * sstride + x];
+      int H = src[AOMMIN(height - 1, y + 2) * sstride + x];
+
+      int I = src[AOMMAX(0, y - 1) * sstride + AOMMAX(0, x - 1)];
+      int J = src[AOMMAX(0, y - 1) * sstride + AOMMIN(width - 1, x + 1)];
+      int K = src[AOMMIN(height - 1, y + 1) * sstride + AOMMAX(0, x - 1)];
+      int L = src[AOMMIN(height - 1, y + 1) * sstride + AOMMIN(width - 1, x + 1)];
+      
       int delta;
-      delta = av1_clpf_sample(X, A, B, C, D, E, F, strength);
+      delta = av1_clpf_sample(X, A, B, C, D, E, F, G, H, I, J, K, L, strength, bitdepth);
       dst[y * dstride + x] = X + delta;
     }
   }
@@ -231,12 +251,12 @@ void av1_clpf_frame(const YV12_BUFFER_CONFIG *frame,
                                    dstride, xpos, ypos, sizex, sizey, width,
                                    height, strength);
               } else {
-                aom_clpf_block(src_buffer, dst_buffer, sstride, dstride, xpos,
+                aom_clpf_block_c(src_buffer, dst_buffer, sstride, dstride, xpos,
                                ypos, sizex, sizey, width, height, strength);
               }
 #else
-              aom_clpf_block(src_buffer, dst_buffer, sstride, dstride, xpos,
-                             ypos, sizex, sizey, width, height, strength);
+              aom_clpf_block_c(src_buffer, dst_buffer, sstride, dstride, xpos,
+                               ypos, sizex, sizey, width, height, strength, cm->bit_depth);
 #endif
             }
           }
