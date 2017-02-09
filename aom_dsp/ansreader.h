0ce6ece2a3be70304f5197f4f611d2824b0bdfe6
Alex Converse <aconverse@google.com>
2017-02-09 12:00:36 -0800

    Optimized rabs bit
    Change-Id: Icb4ab1de851db9f4016af8757e17fcb935a2db5a

--- aom_dsp/ansreader.h
+++ aom_dsp/ansreader.h
@@ -84,9 +84,17 @@ static INLINE int rabs_read(struct AnsDecoder *ans, AnsP8 p0) {
 // Decode one rABS encoded boolean where the probability of the value being zero
 // is one half.
 static INLINE int rabs_read_bit(struct AnsDecoder *ans) {
-  // TODO(aconverse@google.com): Provide an optimized implementation of this
-  // routine.
-  return rabs_read(ans, 128);
+#if ANS_MAX_SYMBOLS
+  if (ans->symbols_left-- == 0) {
+    ans_read_reinit(ans);
+    ans->symbols_left--;
+  }
+#endif
+  unsigned state = ans->state;
+  const int value = !!(state & 0x80);
+  state = ((state >> 1) & ~0x7F) | (state & 0x7F);
+  ans->state = refill_state(ans, state);
+  return value;
 }
 
 struct rans_dec_sym {
