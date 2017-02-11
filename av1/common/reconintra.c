dfab6aba9f846693a81ca8c62995a7fb400fb109
Debargha Mukherjee <debargha@google.com>
2017-02-10 21:44:13 -0800

    Add config flags and macros to control ext-inter
    Adds a dependent config flag 'interintra' to turn on/off interintra
    modes altogether.
    Adds a dependent config flag 'wedge' to turn on/off wedge compound
    for both interinter and interintra.
    
    Adds another macro to change wedge predictors to use
    only 0, 1/2, or 1 weights.
    
    From now, use
    --enable-ext-inter --enable-wedge --enable-interintra to get the
    same behavior as the old --enable-ext-inter.
    
    Change-Id: I2e787e6994163b6b859a9d6431b87c4217834ddc

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -2263,7 +2263,7 @@ void av1_predict_intra_block(const MACROBLOCKD *xd, int wpx, int hpx,
                                dst, dst_stride, col_off, row_off, plane);
   } else {
 #if (CONFIG_RECT_TX && (CONFIG_VAR_TX || CONFIG_EXT_TX)) || \
-    (CONFIG_EXT_INTER && USE_RECT_INTERINTRA)
+    (CONFIG_EXT_INTER)
 #if CONFIG_AOM_HIGHBITDEPTH
     uint16_t tmp16[MAX_SB_SIZE];
 #endif
@@ -2385,7 +2385,7 @@ void av1_predict_intra_block(const MACROBLOCKD *xd, int wpx, int hpx,
 #else
     assert(0);
 #endif  // (CONFIG_RECT_TX && (CONFIG_VAR_TX || CONFIG_EXT_TX)) ||
-        // (CONFIG_EXT_INTER && USE_RECT_INTERINTRA)
+        // (CONFIG_EXT_INTER)
   }
 }
 
