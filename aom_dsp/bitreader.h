396804869c9d55cf0ff56df2654b23c769751290
Nathan E. Egge <negge@mozilla.com>
2017-02-13 20:11:06 -0500

    Disable converting trees to CDFs.
    Change-Id: Ie30a1d5fd6537887947b223f2be7f7b941efdb8e

--- aom_dsp/bitreader.h
+++ aom_dsp/bitreader.h
@@ -271,7 +271,7 @@ OD_DEPRECATED
 int aom_read_tree_(aom_reader *r, const aom_tree_index *tree,
                                  const aom_prob *probs ACCT_STR_PARAM) {
   int ret;
-#if CONFIG_EC_MULTISYMBOL
+#if 0 && CONFIG_EC_MULTISYMBOL
   ret = aom_read_tree_as_cdf(r, tree, probs);
 #else
   ret = aom_read_tree_as_bits(r, tree, probs);
