f0d70ddf9cbfa50791632035f1b01c7ae137fd3f
Jingning Han <jingning@google.com>
2017-02-17 14:49:57 -0800

    Re-schedule sub8x8 chroma component encoding process
    Use the top-left 4x4 luma block's coding information for chroma
    component encoding at size 4x4.
    
    Change-Id: I4bcfbc2bf8b71f7fc30094553503468460a56f9b

--- av1/common/blockd.c
+++ av1/common/blockd.c
@@ -130,7 +130,8 @@ void av1_foreach_transformed_block_in_plane(
   // 4x4=0, 8x8=2, 16x16=4, 32x32=6, 64x64=8
   // transform size varies per plane, look it up in a common way.
   const TX_SIZE tx_size = plane ? get_uv_tx_size(mbmi, pd) : mbmi->tx_size;
-  const BLOCK_SIZE plane_bsize = get_plane_block_size(bsize, pd);
+  const BLOCK_SIZE plane_bsize =
+      AOMMAX(BLOCK_4X4, get_plane_block_size(bsize, pd));
   const uint8_t txw_unit = tx_size_wide_unit[tx_size];
   const uint8_t txh_unit = tx_size_high_unit[tx_size];
   const int step = txw_unit * txh_unit;
