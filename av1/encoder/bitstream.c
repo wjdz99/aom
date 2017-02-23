8e377b9ea5f0e4e4c95082a7f39cf0579c08624d
Fergus Simpson <afergs@google.com>
2017-02-22 16:45:52 -0800

    bitstream: void instruction for mi_row and mi_col
    In the bitstream.c functions pack_inter_mode_mvs and
    write_mb_modes_kf the arguments mi_row and mi_col are only used if
    CONFIG_DELTA_Q set, or CONFIG_EC_MULTISYMBOL is not and CONFIG_CB4X4
    is. This yields an unused parmeter warning if the required flags are
    not set.
    
    This patch adds a void instruction for each parameter in the case
    that they aren't used.
    
    Change-Id: I29bf1e33692b7d4c636d65fca9d130be9313fc9d

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1402,6 +1402,9 @@ static void pack_inter_mode_mvs(AV1_COMP *cpi, const MODE_INFO *mi,
   const int unify_bsize = 0;
 #endif
 
+  (void)mi_row;
+  (void)mi_col;
+
   if (seg->update_map) {
     if (seg->temporal_update) {
       const int pred_flag = mbmi->seg_id_predicted;
@@ -1792,6 +1795,9 @@ static void write_mb_modes_kf(AV1_COMMON *cm, const MACROBLOCKD *xd,
 #else
   const int unify_bsize = 0;
 #endif
+
+  (void)mi_row;
+  (void)mi_col;
 
 #if CONFIG_EC_ADAPT
   FRAME_CONTEXT *ec_ctx = xd->tile_ctx;
