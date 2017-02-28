af3b0629dad301652a88472cfe3dc7eee5da8df8
Luc Trudeau <ltrudeau@mozilla.com>
2017-02-28 10:53:50 -0500

    Fix warning ISO C does not allow extra ‘;'
    This fixes the warnings: ISO C does not allow extra ‘;’ outside of a
    function [-Wpedantic]. You get when compiling with pedantic
    
    Change-Id: I3e349de9ce30c1a9fe6573fe594d7b0645cde0e0

--- aom_dsp/x86/aom_asm_stubs.c
+++ aom_dsp/x86/aom_asm_stubs.c
@@ -60,10 +60,10 @@ filter8_1dfunction aom_filter_block1d4_h2_avg_sse2;
 //                                  const int16_t *filter_x, int x_step_q4,
 //                                  const int16_t *filter_y, int y_step_q4,
 //                                  int w, int h);
-FUN_CONV_1D(horiz, x_step_q4, filter_x, h, src, , sse2);
-FUN_CONV_1D(vert, y_step_q4, filter_y, v, src - src_stride * 3, , sse2);
-FUN_CONV_1D(avg_horiz, x_step_q4, filter_x, h, src, avg_, sse2);
-FUN_CONV_1D(avg_vert, y_step_q4, filter_y, v, src - src_stride * 3, avg_, sse2);
+FUN_CONV_1D(horiz, x_step_q4, filter_x, h, src, , sse2)
+FUN_CONV_1D(vert, y_step_q4, filter_y, v, src - src_stride * 3, , sse2)
+FUN_CONV_1D(avg_horiz, x_step_q4, filter_x, h, src, avg_, sse2)
+FUN_CONV_1D(avg_vert, y_step_q4, filter_y, v, src - src_stride * 3, avg_, sse2)
 
 // void aom_convolve8_sse2(const uint8_t *src, ptrdiff_t src_stride,
 //                         uint8_t *dst, ptrdiff_t dst_stride,
@@ -75,8 +75,8 @@ FUN_CONV_1D(avg_vert, y_step_q4, filter_y, v, src - src_stride * 3, avg_, sse2);
 //                             const int16_t *filter_x, int x_step_q4,
 //                             const int16_t *filter_y, int y_step_q4,
 //                             int w, int h);
-FUN_CONV_2D(, sse2);
-FUN_CONV_2D(avg_, sse2);
+FUN_CONV_2D(, sse2)
+FUN_CONV_2D(avg_, sse2)
 
 #if CONFIG_AOM_HIGHBITDEPTH && ARCH_X86_64
 highbd_filter8_1dfunction aom_highbd_filter_block1d16_v8_sse2;
