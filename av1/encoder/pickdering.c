69b4e1215706788b1f172708cbf7f751bb31df36
Jean-Marc Valin <jmvalin@mozilla.com>
2017-03-01 16:17:22 -0500

    Adds a greedy search for the best dering strengths
    Change-Id: I3cc24e657d6626a15a7a7700d0ea9ad695d2f50e

--- av1/encoder/pickdering.c
+++ av1/encoder/pickdering.c
@@ -34,6 +34,63 @@ static double compute_dist(int16_t *x, int xstride, int16_t *y, int ystride,
   return sum / (double)(1 << 2 * coeff_shift);
 }
 
+/* Search for the best strength to add as an option, knowing we
+   already selected nb_strengths options. */
+static double search_one(int *lev, int nb_strengths,
+    double mse[][DERING_STRENGTHS], int sb_count) {
+  double tot_mse[DERING_STRENGTHS];
+  int i, j;
+  double best_tot_mse = 1e100;
+  int best_id = 0;
+  for (i=0;i<DERING_STRENGTHS;i++)
+    tot_mse[i] = 0;
+  for (i=0;i<sb_count;i++) {
+    int gi;
+    double best_mse = INT32_MAX;
+    /* Find best mse among already selected options. */
+    for (gi = 0; gi < nb_strengths; gi++) {
+      if (mse[i][lev[gi]] < best_mse) {
+        best_mse = mse[i][lev[gi]];
+      }
+    }
+    /* Find best mse when adding each possible new option. */
+    for (j=0;j<DERING_STRENGTHS;j++) {
+      double best = best_mse;
+      if (mse[i][j] < best) best = mse[i][j];
+      tot_mse[j] += best;
+    }
+  }
+  for (j=0;j<DERING_STRENGTHS;j++) {
+    if (tot_mse[j] < best_tot_mse) {
+      best_tot_mse = tot_mse[j];
+      best_id = j;
+    }
+  }
+  lev[nb_strengths] = best_id;
+  return best_tot_mse;
+}
+
+static double joint_strength_search(int *best_lev, int nb_strengths,
+    double mse[][DERING_STRENGTHS], int sb_count) {
+  double best_tot_mse;
+  int i;
+  best_tot_mse = 1e100;
+  /* Greedy search: add one strength options at a time. */
+  for (i=0;i<nb_strengths;i++) {
+    best_tot_mse = search_one(best_lev, i, mse, sb_count);
+  }
+#if 1
+  /* Trying to refine the greedy search by reconsidering each
+     already-selected option. */
+  for (i=0;i<nb_strengths;i++) {
+    int j;
+    for (j=0;j<nb_strengths-1;j++) best_lev[j] = best_lev[j+1];
+    best_tot_mse = search_one(best_lev, 3, mse, sb_count);
+  }
+#endif
+  return best_tot_mse;
+}
+
 int av1_dering_search(YV12_BUFFER_CONFIG *frame, const YV12_BUFFER_CONFIG *ref,
                       AV1_COMMON *cm, MACROBLOCKD *xd) {
   int r, c;
@@ -48,9 +105,14 @@ int av1_dering_search(YV12_BUFFER_CONFIG *frame, const YV12_BUFFER_CONFIG *ref,
   int dec[3];
   int pli;
   int level;
-  int best_level;
   int dering_count;
   int coeff_shift = AOMMAX(cm->bit_depth - 8, 0);
+  double mse[10000][DERING_STRENGTHS];
+  int sb_count;
+  int sb_index[10000];
+  int best_lev[4];
+  int i;
+  int lev[4];
   src = aom_malloc(sizeof(*src) * cm->mi_rows * cm->mi_cols * 64);
   ref_coeff = aom_malloc(sizeof(*ref_coeff) * cm->mi_rows * cm->mi_cols * 64);
   av1_setup_dst_planes(xd->plane, frame, 0, 0);
@@ -79,22 +141,11 @@ int av1_dering_search(YV12_BUFFER_CONFIG *frame, const YV12_BUFFER_CONFIG *ref,
   }
   nvsb = (cm->mi_rows + MAX_MIB_SIZE - 1) / MAX_MIB_SIZE;
   nhsb = (cm->mi_cols + MAX_MIB_SIZE - 1) / MAX_MIB_SIZE;
-  /* Pick a base threshold based on the quantizer. The threshold will then be
-     adjusted on a 64x64 basis. We use a threshold of the form T = a*Q^b,
-     where a and b are derived empirically trying to optimize rate-distortion
-     at different quantizer settings. */
-  best_level = AOMMIN(
-      MAX_DERING_LEVEL - 1,
-      (int)floor(.5 +
-                 .45 * pow(av1_ac_quant(cm->base_qindex, 0, cm->bit_depth) >>
-                               (cm->bit_depth - 8),
-                           0.6)));
+  sb_count = 0;
   for (sbr = 0; sbr < nvsb; sbr++) {
     for (sbc = 0; sbc < nhsb; sbc++) {
       int nvb, nhb;
       int gi;
-      int best_gi;
-      int32_t best_mse = INT32_MAX;
       int16_t dst[MAX_MIB_SIZE * MAX_MIB_SIZE * 8 * 8];
       int16_t tmp_dst[MAX_MIB_SIZE * MAX_MIB_SIZE * 8 * 8];
       nhb = AOMMIN(MAX_MIB_SIZE, cm->mi_cols - MAX_MIB_SIZE * sbc);
@@ -102,14 +153,12 @@ int av1_dering_search(YV12_BUFFER_CONFIG *frame, const YV12_BUFFER_CONFIG *ref,
       dering_count = sb_compute_dering_list(cm, sbr * MAX_MIB_SIZE,
                                             sbc * MAX_MIB_SIZE, dlist);
       if (dering_count == 0) continue;
-      best_gi = 0;
-      for (gi = 0; gi < DERING_REFINEMENT_LEVELS; gi++) {
-        int cur_mse;
+      for (gi = 0; gi < DERING_STRENGTHS; gi++) {
         int threshold;
         int16_t inbuf[OD_DERING_INBUF_SIZE];
         int16_t *in;
-        int i, j;
-        level = compute_level_from_index(best_level, gi);
+        int j;
+        level = dering_level_table[gi];
         threshold = level << coeff_shift;
         for (r = 0; r < nvb << bsize[0]; r++) {
           for (c = 0; c < nhb << bsize[0]; c++) {
@@ -140,22 +189,38 @@ int av1_dering_search(YV12_BUFFER_CONFIG *frame, const YV12_BUFFER_CONFIG *ref,
                   coeff_shift);
         copy_dering_16bit_to_16bit(dst, MAX_MIB_SIZE << bsize[0], tmp_dst,
                                    dlist, dering_count, bsize[0]);
-        cur_mse = (int)compute_dist(
+        mse[sb_count][gi] = (int)compute_dist(
             dst, MAX_MIB_SIZE << bsize[0],
             &ref_coeff[(sbr * stride * MAX_MIB_SIZE << bsize[0]) +
                        (sbc * MAX_MIB_SIZE << bsize[0])],
             stride, nhb, nvb, coeff_shift);
-        if (cur_mse < best_mse) {
-          best_gi = gi;
-          best_mse = cur_mse;
-        }
+        sb_index[sb_count] = MAX_MIB_SIZE * sbr * cm->mi_stride +
+                                  MAX_MIB_SIZE * sbc;
       }
-      cm->mi_grid_visible[MAX_MIB_SIZE * sbr * cm->mi_stride +
-                          MAX_MIB_SIZE * sbc]
-          ->mbmi.dering_gain = best_gi;
+      sb_count++;
     }
+  }
+  joint_strength_search(best_lev, DERING_REFINEMENT_LEVELS, mse, sb_count);
+  /*best_lev[0] = 0;
+  best_lev[1] = 0;
+  best_lev[2] = 0;
+  best_lev[3] = 0;*/
+  for (i=0;i<4;i++) lev[i] = best_lev[i];
+  for (i=0;i<sb_count;i++) {
+    int gi;
+    int best_gi;
+    double best_mse = INT32_MAX;
+    best_gi = 0;
+    for (gi = 0; gi < DERING_REFINEMENT_LEVELS; gi++) {
+      if (mse[i][lev[gi]] < best_mse) {
+        best_gi = gi;
+        best_mse = mse[i][lev[gi]];
+      }
+    }
+    cm->mi_grid_visible[sb_index[i]]
+        ->mbmi.dering_gain = best_gi;
   }
   aom_free(src);
   aom_free(ref_coeff);
-  return best_level;
+  return levels_to_id(best_lev);
 }
