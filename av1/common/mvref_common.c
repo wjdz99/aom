ef3b5a9fa5bf3d21f35a86d9fa481ad028fddb3a
Debargha Mukherjee <debargha@google.com>
2017-03-01 10:44:46 -0800

    Some optimizations on integer affine estimation
    1. Adds a limit on number of candidate samples used for the
    estimation.
    2. Adds a limit on max mv magnitude for use in the least-squares
    3. Makes some of the internal variables 32-bit.
    
    Impact on coding efficiency in the noise range.
    
    Change-Id: I8c1c3216368ceb2e3548660a3b8c159df54a8312

--- av1/common/mvref_common.c
+++ av1/common/mvref_common.c
@@ -1154,7 +1154,6 @@ int findSamples(const AV1_COMMON *cm, MACROBLOCKD *xd, int mi_row, int mi_col,
   int mvnumber = 0;
   int global_offset_c = mi_col * MI_SIZE;
   int global_offset_r = mi_row * MI_SIZE;
-  int samples_per_neighbor = 4;
 
   // scan the above row
   if (up_available) {
@@ -1173,7 +1172,7 @@ int findSamples(const AV1_COMMON *cm, MACROBLOCKD *xd, int mi_row, int mi_col,
         int cr_offset = -AOMMAX(bh, MI_SIZE) / 2 - 1;
         int cc_offset = i * MI_SIZE + AOMMAX(bw, MI_SIZE) / 2 - 1;
         int j;
-        int pixelperblock = samples_per_neighbor;
+        int pixelperblock = SAMPLES_PER_NEIGHBOR;
 
         mvasint[mvnumber] = mbmi->mv[0].as_int;
         mvnumber++;
@@ -1216,7 +1215,7 @@ int findSamples(const AV1_COMMON *cm, MACROBLOCKD *xd, int mi_row, int mi_col,
         int cr_offset = i * MI_SIZE + AOMMAX(bh, MI_SIZE) / 2 - 1;
         int cc_offset = -AOMMAX(bw, MI_SIZE) / 2 - 1;
         int j;
-        int pixelperblock = samples_per_neighbor;
+        int pixelperblock = SAMPLES_PER_NEIGHBOR;
 
         mvasint[mvnumber] = mbmi->mv[0].as_int;
         mvnumber++;
@@ -1255,7 +1254,7 @@ int findSamples(const AV1_COMMON *cm, MACROBLOCKD *xd, int mi_row, int mi_col,
       int cr_offset = -AOMMAX(bh, MI_SIZE) / 2 - 1;
       int cc_offset = -AOMMAX(bw, MI_SIZE) / 2 - 1;
       int j;
-      int pixelperblock = samples_per_neighbor;
+      int pixelperblock = SAMPLES_PER_NEIGHBOR;
 
       mvasint[mvnumber] = mbmi->mv[0].as_int;
       mvnumber++;
@@ -1296,7 +1295,7 @@ int findSamples(const AV1_COMMON *cm, MACROBLOCKD *xd, int mi_row, int mi_col,
     int cr_offset = AOMMAX(bh, MI_SIZE) / 2 - 1;
     int cc_offset = AOMMAX(bw, MI_SIZE) / 2 - 1;
     int j;
-    int pixelperblock = samples_per_neighbor;
+    int pixelperblock = SAMPLES_PER_NEIGHBOR;
 
     for (j = 0; j < pixelperblock; j++) {
       int r_offset = j / 2;
