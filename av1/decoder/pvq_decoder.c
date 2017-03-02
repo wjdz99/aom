0710bb30e64399c4585683160c6bf20e87f74950
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-02 14:04:55 -0500

    Replace is_keyframe checks with is_skip_copy check in PVQ
    Because of H\V pred and CfL, block level skip on keyframes in Daala
    implied zeroing out the coeff instead of copying the prediction. In AV1,
    block level skips in keyframes impli copying the prediction.
    
    To better reflect the nature of AV1, the checks are done on
    is_skip_copy instead of is_keyframe. This will allow a skip zero
    behavior if needed in the future.
    
    is_keyframe is now enabled on keyframes for PVQ the changes are 0.0000
    on AWCY. Enabling keyframe mostly changes the probabilities of keyframes
    for EC. I'm unable to test with objective-fast at the moment because we
    run out of RAM on AWCY.
    
    Change-Id: I422f5a766ef4eb366e96df7a9068c8bd2f87666a

--- av1/decoder/pvq_decoder.c
+++ av1/decoder/pvq_decoder.c
@@ -124,8 +124,7 @@ static void pvq_decode_partition(aom_reader *r,
                                  int *noref,
                                  od_val16 beta,
                                  int nodesync,
-                                 int is_keyframe,
-                                 int pli,
+                                 int cfl_enabled,
                                  int cdf_ctx,
                                  cfl_ctx *cfl,
                                  int has_skip,
@@ -147,19 +146,20 @@ static void pvq_decode_partition(aom_reader *r,
   int i;
   od_val16 ref16[MAXN];
   int rshift;
+  int is_skip_copy;
   theta = 0;
   gr = 0;
   gain_offset = 0;
+  is_skip_copy = !cfl_enabled;
   /* Skip is per-direction. For band=0, we can use any of the flags. */
   if (skip_rest[(band + 2) % 3]) {
     qg = 0;
-    if (is_keyframe) {
-      itheta = -1;
-      *noref = 1;
-    }
-    else {
+    if (is_skip_copy) {
       itheta = 0;
       *noref = 0;
+    } else {
+      itheta = -1;
+      *noref = 1;
     }
   }
   else {
@@ -169,8 +169,12 @@ static void pvq_decode_partition(aom_reader *r,
     id = aom_decode_cdf_adapt(r, &adapt->pvq.pvq_gaintheta_cdf[cdf_ctx][0],
      8 + 7*has_skip, adapt->pvq.pvq_gaintheta_increment,
      "pvq:gaintheta");
-    if (!is_keyframe && id >= 10) id++;
-    if (is_keyframe && id >= 8) id++;
+    /* id=10 implies skip copy, when skip can copy from the reference, it isn't
+     * allowed, as skip can be signaled at the block level */
+    if (is_skip_copy && id >= 10) id++;
+    /* id=8 implies skip zero, when skip zeroes out the coeffs, it isn't
+     * allowed, as skip can be signaled at the block level. */
+    if (!is_skip_copy && id >= 8) id++;
     if (id >= 8) {
       id -= 8;
       skip_rest[0] = skip_rest[1] = skip_rest[2] = 1;
@@ -213,8 +217,6 @@ static void pvq_decode_partition(aom_reader *r,
     /* we have a reference; compute its gain */
     od_val32 cgr;
     int icgr;
-    int cfl_enabled;
-    cfl_enabled = pli != 0 && is_keyframe && !OD_DISABLE_CFL;
     cgr = od_pvq_compute_gain(ref16, n, q0, &gr, beta, rshift);
     if (cfl_enabled) cgr = OD_CGAIN_SCALE;
 #if defined(OD_FLOAT_PVQ)
@@ -224,10 +226,11 @@ static void pvq_decode_partition(aom_reader *r,
 #endif
     /* quantized gain is interleave encoded when there's a reference;
        deinterleave it now */
-    if (is_keyframe) qg = neg_deinterleave(qg, icgr);
-    else {
+    if (is_skip_copy) {
       qg = neg_deinterleave(qg, icgr + 1) - 1;
       if (qg == 0) *skip = (icgr ? OD_PVQ_SKIP_ZERO : OD_PVQ_SKIP_COPY);
+    } else {
+      qg = neg_deinterleave(qg, icgr);
     }
     if (qg == icgr && itheta == 0 && !cfl_enabled) *skip = OD_PVQ_SKIP_COPY;
     gain_offset = cgr - OD_SHL(icgr, OD_CGAIN_SHIFT);
@@ -245,7 +248,7 @@ static void pvq_decode_partition(aom_reader *r,
   }
   else{
     itheta = 0;
-    if (!is_keyframe) qg++;
+    if (is_skip_copy) qg++;
     qcg = OD_SHL(qg, OD_CGAIN_SHIFT);
     if (qg == 0) *skip = OD_PVQ_SKIP_ZERO;
   }
@@ -312,11 +315,14 @@ void od_pvq_decode(daala_dec_ctx *dec,
   int size[PVQ_MAX_PARTITIONS];
   generic_encoder *model;
   int skip_rest[3] = {0};
+  int is_skip_copy;
+  int cfl_enabled;
   cfl_ctx cfl;
   const unsigned char *pvq_qm;
   int use_masking;
 
   aom_clear_system_state();
+
 
   /*Default to skip=1 and noref=0 for all bands.*/
   for (i = 0; i < PVQ_MAX_PARTITIONS; i++) {
@@ -337,15 +343,17 @@ void od_pvq_decode(daala_dec_ctx *dec,
   nb_bands = OD_BAND_OFFSETS[bs][0];
   off = &OD_BAND_OFFSETS[bs][1];
   out[0] = ac_dc_coded & DC_CODED;
+  cfl_enabled = pli != 0 && is_keyframe && !OD_DISABLE_CFL;
+  is_skip_copy = !cfl_enabled;
   if (ac_dc_coded < AC_CODED) {
-    if (is_keyframe) for (i = 1; i < 1 << (2*bs + 4); i++) out[i] = 0;
-    else for (i = 1; i < 1 << (2*bs + 4); i++) out[i] = ref[i];
-  }
-  else {
+    if (is_skip_copy) for (i = 1; i < 1 << (2*bs + 4); i++) out[i] = ref[i];
+    else
+      for (i = 1; i < 1 << (2*bs + 4); i++) out[i] = 0;
+  } else {
     for (i = 0; i < nb_bands; i++) size[i] = off[i+1] - off[i];
     cfl.ref = ref;
     cfl.nb_coeffs = off[nb_bands];
-    cfl.allow_flip = pli != 0 && is_keyframe;
+    cfl.allow_flip = cfl_enabled;
     for (i = 0; i < nb_bands; i++) {
       int q;
 
@@ -356,7 +364,7 @@ void od_pvq_decode(daala_dec_ctx *dec,
 
       pvq_decode_partition(dec->r, q, size[i],
        model, &dec->state.adapt, exg + i, ext + i, ref + off[i], out + off[i],
-       &noref[i], beta[i], nodesync, is_keyframe, pli,
+       &noref[i], beta[i], nodesync, cfl_enabled,
        (pli != 0)*OD_TXSIZES*PVQ_MAX_PARTITIONS + bs*PVQ_MAX_PARTITIONS + i,
        &cfl, i == 0 && (i < nb_bands - 1), skip_rest, i, &skip[i],
        qm + off[i], qm_inv + off[i]);
