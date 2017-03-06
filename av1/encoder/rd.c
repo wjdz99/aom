316b1b9de9d38a5ad0502287f7b27291badd7cbd
Thomas Davies <thdavies@cisco.com>
2017-03-06 12:10:46 +0000

    NEW_TOKENSET: improve RDO and remove 8-bit prob dependence.
    Use the head and tail CDFs to initialise the token costs,
    and more closely match coeff cost estimates to the
    new tokenset coding. Coefficient encoding now does not
    depend on the 8 bit probabilities.
    
    Change-Id: I7f2dd9f3cd24bb64eb4131cba94dda466b9691b0

--- av1/encoder/rd.c
+++ av1/encoder/rd.c
@@ -164,6 +164,90 @@ static void fill_mode_costs(AV1_COMP *cpi) {
 #endif  // CONFIG_GLOBAL_MOTION
 }
 
+#if CONFIG_NEW_TOKENSET
+static void set_prob_tokens(int prob_head[ENTROPY_TOKENS],
+                            int prob_tail[ENTROPY_TOKENS],
+                            const aom_cdf_prob *cdf_head,
+                            const aom_cdf_prob *cdf_tail) {
+  int prob_tmp[5];
+  int prob_block_nz = CDF_PROB_TOP - cdf_head[0];
+  int scale;
+  int r;
+
+  prob_tmp[ZERO_TOKEN] =
+      cdf_head[1 + ZERO_TOKEN] - cdf_head[1 + ZERO_TOKEN - 1];
+  prob_tmp[ONE_TOKEN_EOB] =
+      cdf_head[1 + ONE_TOKEN_EOB] - cdf_head[1 + ONE_TOKEN_EOB - 1];
+  prob_tmp[ONE_TOKEN_NEOB] =
+      cdf_head[1 + ONE_TOKEN_NEOB] - cdf_head[1 + ONE_TOKEN_NEOB - 1];
+  prob_tmp[TWO_TOKEN_PLUS_EOB] =
+      cdf_head[1 + TWO_TOKEN_PLUS_EOB] - cdf_head[1 + TWO_TOKEN_PLUS_EOB - 1];
+  prob_tmp[TWO_TOKEN_PLUS_NEOB] =
+      cdf_head[1 + TWO_TOKEN_PLUS_NEOB] - cdf_head[1 + TWO_TOKEN_PLUS_NEOB - 1];
+
+  prob_head[ZERO_TOKEN] =
+      AOMMIN(MAX_PROB, (prob_tmp[ZERO_TOKEN] << 8) / prob_block_nz);
+  prob_head[ONE_TOKEN] =
+      AOMMIN(MAX_PROB,
+             ((/*prob_tmp[ONE_TOKEN_EOB] +*/ prob_tmp[ONE_TOKEN_NEOB]) << 8) /
+                 prob_block_nz);
+  prob_head[TWO_TOKEN] = AOMMIN(
+      MAX_PROB,
+      ((/*prob_tmp[TWO_TOKEN_PLUS_EOB] +*/ prob_tmp[TWO_TOKEN_PLUS_NEOB]) *
+       (1 << 8)) /
+          prob_block_nz);
+
+  scale = (prob_tmp[ONE_TOKEN_EOB] + prob_tmp[TWO_TOKEN_PLUS_EOB] +
+           prob_tmp[ONE_TOKEN_NEOB] + prob_tmp[TWO_TOKEN_PLUS_NEOB]);
+  prob_head[EOB_TOKEN] = AOMMIN(
+      MAX_PROB,
+      ((prob_tmp[ONE_TOKEN_EOB] + prob_tmp[TWO_TOKEN_PLUS_EOB]) << 8) / scale);
+
+  prob_tail[TWO_TOKEN] = AOMMIN(MAX_PROB, cdf_tail[0] >> (CDF_PROB_BITS - 8));
+  for (r = TWO_TOKEN + 1; r < EOB_TOKEN; ++r) {
+    prob_tail[r] = cdf_tail[r - TWO_TOKEN] - cdf_tail[r - TWO_TOKEN - 1];
+    prob_tail[r] = AOMMIN(MAX_PROB, prob_tail[r] >> (CDF_PROB_BITS - 8));
+  }
+}
+
+void av1_fill_token_costs(av1_coeff_cost *c,
+                          coeff_cdf_model (*cdf_head)[PLANE_TYPES],
+                          coeff_cdf_model (*cdf_tail)[PLANE_TYPES]) {
+  int i, j, k, l;
+  TX_SIZE t;
+  for (t = 0; t < TX_SIZES; ++t)
+    for (i = 0; i < PLANE_TYPES; ++i)
+      for (j = 0; j < REF_TYPES; ++j)
+        for (k = 0; k < COEF_BANDS; ++k)
+          for (l = 0; l < BAND_COEFF_CONTEXTS(k); ++l) {
+            int prob_head[ENTROPY_TOKENS];
+            int prob_tail[ENTROPY_TOKENS];
+            int r;
+
+            // Head costs
+            set_prob_tokens(prob_head, prob_tail, cdf_head[t][i][j][k][l],
+                            cdf_tail[t][i][j][k][l]);
+
+            c[t][i][j][k][1][l][ZERO_TOKEN] =
+                av1_cost_bit(prob_head[ZERO_TOKEN], 0);
+            c[t][i][j][k][0][l][ZERO_TOKEN] = c[t][i][j][k][1][l][ZERO_TOKEN];
+            c[t][i][j][k][0][l][ONE_TOKEN] =
+                av1_cost_bit(prob_head[ONE_TOKEN], 0);
+            c[t][i][j][k][1][l][ONE_TOKEN] = c[t][i][j][k][0][l][ONE_TOKEN];
+            c[t][i][j][k][0][l][EOB_TOKEN] =
+                av1_cost_bit(prob_head[EOB_TOKEN], 0);
+            c[t][i][j][k][1][l][EOB_TOKEN] = c[t][i][j][k][0][l][EOB_TOKEN];
+
+            // Now look at the tail
+            int two_plus_cost = av1_cost_bit(prob_head[TWO_TOKEN], 0);
+            for (r = TWO_TOKEN; r < EOB_TOKEN; ++r) {
+              c[t][i][j][k][0][l][r] =
+                  two_plus_cost + av1_cost_bit(prob_tail[r], 0);
+              c[t][i][j][k][1][l][r] = c[t][i][j][k][0][j][r];
+            }
+          }
+}
+#else
 void av1_fill_token_costs(av1_coeff_cost *c,
                           av1_coeff_probs_model (*p)[PLANE_TYPES]) {
   int i, j, k, l;
@@ -182,6 +266,7 @@ void av1_fill_token_costs(av1_coeff_cost *c,
                    c[t][i][j][k][1][l][EOB_TOKEN]);
           }
 }
+#endif
 
 // Values are now correlated to quantizer.
 static int sad_per_bit16lut_8[QINDEX_RANGE];
@@ -394,7 +479,12 @@ void av1_initialize_rd_consts(AV1_COMP *cpi) {
 #endif
 
   if (cpi->oxcf.pass != 1) {
+#if CONFIG_NEW_TOKENSET
+    av1_fill_token_costs(x->token_costs, cm->fc->coef_head_cdfs,
+                         cm->fc->coef_tail_cdfs);
+#else
     av1_fill_token_costs(x->token_costs, cm->fc->coef_probs);
+#endif
 
     if (cpi->sf.partition_search_type != VAR_BASED_PARTITION ||
         cm->frame_type == KEY_FRAME) {
