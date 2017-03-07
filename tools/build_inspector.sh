d081dc070abce05d247125c19372ae5e2d9f2b6b
Michael Bebenita <mbebenita@mozilla.com>
2017-03-07 11:00:02 -0800

    Fix Emscripten build script.
    Change-Id: I6e5a287cbb65137f6c6d8b9f65a8d3f5bfed7c9a

--- tools/build_inspector.sh
+++ tools/build_inspector.sh
@@ -6,7 +6,9 @@ fi
 echo 'Building JS Inspector'
 if [ ! -d ".inspect" ]; then
   mkdir .inspect
-  cd .inspect && emconfigure ../../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-inspection --enable-aom_highbitdepth --extra-cflags="-D_POSIX_SOURCE"
+  cd .inspect
+  emconfigure ../../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-inspection --enable-aom_highbitdepth --extra-cflags="-D_POSIX_SOURCE"
+  cd ..
 fi
 
 cd .inspect
@@ -14,3 +16,4 @@ emmake make -j 8
 cp examples/inspect inspect.bc
 emcc -O3 inspect.bc -o inspect.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../inspect-post.js" --memory-init-file 0
 cp inspect.js ../inspect.js
+cd ..
