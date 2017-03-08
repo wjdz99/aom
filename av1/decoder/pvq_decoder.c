dd200281264d3aa7dfdba9f0121c4a097950118a
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-08 16:38:04 -0500

    PVQ_CFL Experiment flag
    This experiment does NOT replace the Chroma intra prediction
    with the Luma reconstructed values (i.e. it does not perform CfL). It
    only enables the PVQ mecanisms for it. PVQ is mislead to believe the
    content is CfL in order to validate that it behaves properly.
    
    OD_DISABLE_CFL is removed as CFL can be disabled by disabling the
    PVQ_CFL flag. Enabling PVQ_CFL flag enables quantization on keyframe
    DCs for PVQ. When a Chroma plane is skipped, the reference is not
    copied, but the coefficients are zeroed.
    
    CfL flipping is disabled as this feature can flip a value multiple
    time during RDO.
    
    Change-Id: I19621978bedb7ab4df6eb8050ad7781d35b3bce3

--- av1/decoder/pvq_decoder.c
+++ av1/decoder/pvq_decoder.c
@@ -345,7 +345,8 @@ void od_pvq_decode(daala_dec_ctx *dec,
     for (i = 0; i < nb_bands; i++) size[i] = off[i+1] - off[i];
     cfl.ref = ref;
     cfl.nb_coeffs = off[nb_bands];
-    cfl.allow_flip = pli != 0 && is_keyframe;
+    // TODO(ltrudeau) enable flip after CfL is added to RDO.
+    cfl.allow_flip = 0/*cfl_enabled*/;
     for (i = 0; i < nb_bands; i++) {
       int q;
 
