f137ba91d2a4d5895121e7dd5df11b607d5dd4c2
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-09 13:57:57 -0500

    Chroma from Luma (CfL) for Intra in PVQ
    Uses the reconstructed Luma pixel as an intra prediction for the
    Chroma pixels. In this first version, CfL is always on.
    
    This preliminary version of CfL only supports 4:2:0 and does not
    perform CfL during RDO. PVQ's gain prediction is used and CfLà
    flipping is disable to avoid RDO issues.
    
    The CFL_CTX struct is introduced, it stores a pixel buffer per thread
    group. CFL_CTX will replace PVQ's cfl_ctx struct in a subsequent
    version.
    
    When 4:2:0 is used, it is possible to have Chromatic overrun (i.e.
    when the surface covered by Chroma is bigger than the surface covered
    by luma). When this happens we pad by copying the last available luma
    pixel value.
    
    Subset 1:
    pvq@2017-03-13T15:15:32.984Z -> CfL-8510@2017-03-13T15:14:05.645Z
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    12.2008 |  5.1913 |  4.8758 |      N/A | 12.3095 | 12.2991 |    10.2220
    Change-Id: I20e27508aab493dacc6ebc0aa8082d6d53de9580

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1022,7 +1022,6 @@ static void pack_pvq_tokens(aom_writer *w, MACROBLOCK *const x,
       generic_encoder *model = adapt->pvq.pvq_param_model;
 
       pvq = get_pvq_block(x->pvq_q);
-
       // encode block skip info
       aom_encode_cdf_adapt(w, pvq->ac_dc_coded,
                            adapt->skip_cdf[2 * tx_size + (plane != 0)], 4,
