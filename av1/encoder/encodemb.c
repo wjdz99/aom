db7808db606961fe040da06af985b72585629a16
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-09 13:57:57 -0500

    Chroma from Luma (CfL) for Intra in PVQ
    Uses the reconstructed Luma pixel as an intra prediction for the
    Chroma pixels. In this first version, CfL is always on.
    
    This preliminary version of CfL only supports 4:2:0 and does not
    perform CfL during RDO. PVQ's gain prediction is used and CfLà
    flipping is disable to avoid RDO issues.
    
    The CFL_CTX struct is introduced, it stores a pixel buffer per thread
    group. CFL_CTX will replace PVQ's cfl_ctx struct in a subsequent
    version.
    
    When 4:2:0 is used, it is possible to have Chromatic overrun (i.e.
    when the surface covered by Chroma is bigger than the surface covered
    by luma). When this happens we pad by copying the last available luma
    pixel value.
    
    Subset 1:
    pvq@2017-03-13T15:15:32.984Z -> CfL-8510@2017-03-13T15:14:05.645Z
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    12.2008 |  5.1913 |  4.8758 |      N/A | 12.3095 | 12.2991 |    10.2220
    Change-Id: I20e27508aab493dacc6ebc0aa8082d6d53de9580

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -575,6 +575,25 @@ void av1_xform_quant(const AV1_COMMON *cm, MACROBLOCK *x, int plane, int block,
   // transform block size in pixels
   tx_blk_size = tx_size_wide[tx_size];
 
+  PVQ_INFO _pvq_info;
+  PVQ_INFO *pvq_info = &_pvq_info;
+  if (x->pvq_coded) {
+    assert(block < MAX_PVQ_BLOCKS_IN_SB);
+    pvq_info = &x->pvq[block][plane];
+    pvq_info->is_coded = 1;
+#if CONFIG_PVQ_CFL
+    pvq_info->cfl_enabled = plane != 0 && cm->frame_type == KEY_FRAME;
+    if (pvq_info->cfl_enabled) {
+      xd->cfl->flat_val = dst[0];
+      cfl_load(xd->cfl, dst, dst_stride, blk_row, blk_col, tx_blk_size);
+    }
+#else
+    pvq_info->cfl_enabled = 0;
+#endif
+  } else {
+    pvq_info->is_coded = 0;
+  }
+
   // copy uint8 orig and predicted block to int16 buffer
   // in order to use existing VP10 transform functions
   for (j = 0; j < tx_blk_size; j++)
@@ -626,13 +645,6 @@ void av1_xform_quant(const AV1_COMMON *cm, MACROBLOCK *x, int plane, int block,
     fwd_txfm(src_int16, coeff, diff_stride, &fwd_txfm_param);
     fwd_txfm(pred, ref_coeff, diff_stride, &fwd_txfm_param);
 
-    PVQ_INFO _pvq_info;
-    PVQ_INFO *pvq_info = &_pvq_info;
-    if (x->pvq_coded) {
-      assert(block < MAX_PVQ_BLOCKS_IN_SB);
-      pvq_info = &x->pvq[block][plane];
-      pvq_info->is_coded = 1;
-    }
 #if CONFIG_PVQ_CFL
     /*If we are coding a chroma block of a keyframe, we are doing CfL.*/
     pvq_info->cfl_enabled = cm->frame_type == KEY_FRAME && plane != 0;
@@ -652,10 +664,26 @@ void av1_xform_quant(const AV1_COMMON *cm, MACROBLOCK *x, int plane, int block,
                           x->pvq_speed,
                           pvq_info);  // PVQ info for a block
     skip = pvq_info->ac_dc_coded == PVQ_SKIP;
+#if CONFIG_PVQ_CFL
+    if (skip && pvq_info->is_coded && pvq_info->cfl_enabled) {
+      // For CfL skip implies zeroing out the coefficients.
+      // In order to get away with not doing the inverse transform,
+      // we copy the flat value resulting from DC_PRED.
+      for (j = 0; j < tx_blk_size; j++) {
+        for (i = 0; i < tx_blk_size; i++) {
+          dst[j * dst_stride + i] = xd->cfl->flat_val;
+        }
+      }
+    }
+#endif
   }
   x->pvq_skip[plane] = skip;
 
   if (!skip) mbmi->skip = 0;
+#if CONFIG_PVQ_CFL
+  if (skip && x->pvq_coded && plane == 0 && cm->frame_type == KEY_FRAME)
+    cfl_store(xd->cfl, dst, dst_stride, blk_row, blk_col, tx_blk_size);
+#endif
 #endif  // #if !CONFIG_PVQ
 }
 
@@ -851,10 +879,12 @@ static void encode_block_pass1(int plane, int block, int blk_row, int blk_col,
       // transform block size in pixels
       tx_blk_size = tx_size_wide[tx_size];
 
-      // Since av1 does not have separate function which does inverse transform
+      // Since av1 does not have separate function which does inverse
+      // transform
       // but av1_inv_txfm_add_*x*() also does addition of predicted image to
       // inverse transformed image,
-      // pass blank dummy image to av1_inv_txfm_add_*x*(), i.e. set dst as zeros
+      // pass blank dummy image to av1_inv_txfm_add_*x*(), i.e. set dst as
+      // zeros
       for (j = 0; j < tx_blk_size; j++)
         for (i = 0; i < tx_blk_size; i++) dst[j * pd->dst.stride + i] = 0;
     }
@@ -1005,7 +1035,7 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
   int ctx = 0;
   INV_TXFM_PARAM inv_txfm_param;
 #if CONFIG_PVQ
-  int tx_blk_size;
+  const int tx_blk_size = tx_size_wide[tx_size];
   int i, j;
 #endif
 
@@ -1096,7 +1126,6 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
     *(args->skip) = 0;
   }
 #else  // #if !CONFIG_PVQ
-
 #if CONFIG_NEW_QUANT
   av1_xform_quant(cm, x, plane, block, blk_row, blk_col, plane_bsize, tx_size,
                   ctx, AV1_XFORM_QUANT_FP_NUQ);
@@ -1111,9 +1140,6 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
   if (!x->pvq_skip[plane]) *(args->skip) = 0;
 
   if (x->pvq_skip[plane]) return;
-
-  // transform block size in pixels
-  tx_blk_size = tx_size_wide[tx_size];
 
   // Since av1 does not have separate function which does inverse transform
   // but av1_inv_txfm_add_*x*() also does addition of predicted image to
@@ -1139,6 +1165,11 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
   if (*eob) *(args->skip) = 0;
 #else
 // Note : *(args->skip) == mbmi->skip
+#endif
+#if CONFIG_PVQ_CFL
+  if (x->pvq_coded && plane == 0) {
+    cfl_store(xd->cfl, dst, dst_stride, blk_row, blk_col, tx_blk_size);
+  }
 #endif
 }
 
