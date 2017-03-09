83303cd2a5275484ee3414f7d3479c72f08659fa
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-09 11:02:42 -0500

    [PVQ_CFL] Disable uv_mode signaling
    We disable uv_mode signaling in the bitstream and set
    uv_mode = DC_PRED.
    
    In other words, spatial Intra prediction is used to predict the
    Chroma DCs and the ACs are all zeros. In a subsequent patch, the ACs
    will be predicted using the reconstructed Luma pixels (CfL).
    
    Note that the computation of probabilities for EC is not modified.
    
    Subset1:
    pvq@2017-03-09T19:17:07.051Z
      -> DisableUVMode-7943@2017-03-09T19:18:16.429Z
      PSNR | PSNR Cb | PSNR Cr | PSNR HVS |   SSIM | MS SSIM | CIEDE 2000
    3.1683 |  1.6431 |  2.7514 |   3.0238 | 3.1924 |  3.0864 |     2.8269
    
    Change-Id: I9ea301512bb4966f2ad886183e9c2515189e5274

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -615,12 +615,15 @@ void av1_xform_quant(const AV1_COMMON *cm, MACROBLOCK *x, int plane, int block,
       av1_quantize_skip(tx2d_size, qcoeff, dqcoeff, eob);
     }
   }
-#else   // #if !CONFIG_PVQ
+#else  // #if !CONFIG_PVQ
 
   (void)xform_quant_idx;
   fwd_txfm(src_int16, coeff, diff_stride, &fwd_txfm_param);
   fwd_txfm(pred, ref_coeff, diff_stride, &fwd_txfm_param);
 
+#if CONFIG_PVQ_CFL
+  if (plane != 0) assert(mbmi->uv_mode == DC_PRED);
+#endif
   // PVQ for inter mode block
   if (!x->skip_block) {
     PVQ_INFO _pvq_info;
