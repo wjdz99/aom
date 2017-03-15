792b523e55a6c671db259494c5d3d4969eaed0c4
Ryan Lei <ryan.z.lei@intel.com>
2017-03-15 13:36:34 -0700

    add a new experiment to unify intra prediction angle for 8x8 block
    this change makes the angle step to be 3 for 8x8 block, so that all
    possible angles for intra prediciton are consistent for all block
    sizes
    
    Change-Id: Ibd5476584986ead35877fce71dd2cae92f77a52f

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -807,7 +807,7 @@ static INLINE TX_SIZE tx_size_from_tx_mode(BLOCK_SIZE bsize, TX_MODE tx_mode,
 #if CONFIG_EXTRA_INTRA_UNIFIED_ANGLE
 #define ANGLE_STEP_UV 3
 static const uint8_t av1_angle_step_y[TX_SIZES] = {
-    0, 3, 3, 3,
+  0, 3, 3, 3,
 };
 #else
 #define ANGLE_STEP_UV 4
