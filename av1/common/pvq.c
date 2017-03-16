7c0f0a4c191901fe0f44d1202b05e59146dc0220
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-16 00:29:04 -0400

    [PVQ_CFL] Chroma prediction is average of Luma values in 4:2:0
    Change-Id: I1dd8514d2516b950bc9cb3de431a5b1e950a4908

--- av1/common/pvq.c
+++ av1/common/pvq.c
@@ -1042,7 +1042,12 @@ void cfl_load(const CFL_CTX *const cfl, uint8_t *const output,
 
   for (j = 0; j < tx_blk_size; j++) {
     for (i = 0; i < tx_blk_size;  i++) {
-      output[output_row_offset + i] = pred[step * (pred_row_offset + i)];
+      output[output_row_offset + i] =
+        (pred[step * (pred_row_offset + i)]
+        + pred[step * (pred_row_offset + i) + 1]
+        + pred[step * (pred_row_offset + i) + MAX_SB_SIZE]
+        + pred[step * (pred_row_offset + i) + MAX_SB_SIZE + 1]
+        )/4;
     }
     pred_row_offset += MAX_SB_SIZE;
     output_row_offset += output_stride;
