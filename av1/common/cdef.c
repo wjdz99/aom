68a82a1f756e50141fc7ca016fa330ac96585111
Michael Bebenita <mbebenita@mozilla.com>
2017-03-31 18:37:00 -0700

    Remove most loops in av1_cdef_frame.
    Change-Id: I4f3efd713ab4789ca6437c8c39777d2f96d96ea3

--- av1/common/cdef.c
+++ av1/common/cdef.c
@@ -115,6 +115,16 @@ void copy_sb8_16(UNUSED AV1_COMMON *cm, uint16_t *dst, int dstride,
 #endif
 }
 
+static void fill_nxm_16bit(uint16_t *dst, int dstride, int n, int m,
+                           uint16_t x) {
+  int i, j;
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      dst[i * dstride + j] = x;
+    }
+  }
+}
+
 void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
                     MACROBLOCKD *xd) {
   int r, c;
@@ -228,22 +238,16 @@ void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
         if (sbc == nhsb - 1) {
           /* On the last superblock column, fill in the right border with
              OD_DERING_VERY_LARGE to avoid filtering with the outside. */
-          for (r = 0; r < rend + OD_FILT_VBORDER; r++) {
-            for (c = cend; c < nhbs + OD_FILT_HBORDER; ++c) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  OD_DERING_VERY_LARGE;
-            }
-          }
+          fill_nxm_16bit(&src[cend + OD_FILT_HBORDER], OD_FILT_BSTRIDE,
+                         nhbs + OD_FILT_HBORDER - cend, rend + OD_FILT_VBORDER,
+                         OD_DERING_VERY_LARGE);
         }
         if (sbr == nvsb - 1) {
           /* On the last superblock row, fill in the bottom border with
              OD_DERING_VERY_LARGE to avoid filtering with the outside. */
-          for (r = rend; r < rend + OD_FILT_VBORDER; r++) {
-            for (c = 0; c < nhbs + 2 * OD_FILT_HBORDER; c++) {
-              src[(r + OD_FILT_VBORDER) * OD_FILT_BSTRIDE + c] =
-                  OD_DERING_VERY_LARGE;
-            }
-          }
+          fill_nxm_16bit(&src[(rend + OD_FILT_VBORDER) * OD_FILT_BSTRIDE],
+                         OD_FILT_BSTRIDE, nhbs + 2 * OD_FILT_HBORDER,
+                         OD_FILT_VBORDER, OD_DERING_VERY_LARGE);
         }
         /* Copy in the pixels we need from the current superblock for
            deringing.*/
@@ -260,19 +264,12 @@ void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
                       coffset, xd->plane[pli].dst.stride, OD_FILT_VBORDER,
                       nhbs);
         } else if (sbr > 0) {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = 0; c < nhbs; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  linebuf[pli][r * stride + coffset + c];
-            }
-          }
+          copy_nxm_16bit_to_16bit(&src[OD_FILT_HBORDER], OD_FILT_BSTRIDE,
+                                  linebuf[pli] + coffset, stride, nhbs,
+                                  OD_FILT_VBORDER);
         } else {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = 0; c < nhbs; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  OD_DERING_VERY_LARGE;
-            }
-          }
+          fill_nxm_16bit(&src[OD_FILT_HBORDER], OD_FILT_BSTRIDE, nhbs,
+                         OD_FILT_VBORDER, OD_DERING_VERY_LARGE);
         }
         if (!prev_row_dering[sbc - 1]) {
           copy_sb8_16(cm, src, OD_FILT_BSTRIDE, xd->plane[pli].dst.buf,
@@ -280,19 +277,12 @@ void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
                       coffset - OD_FILT_HBORDER, xd->plane[pli].dst.stride,
                       OD_FILT_VBORDER, OD_FILT_HBORDER);
         } else if (sbr > 0 && sbc > 0) {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = -OD_FILT_HBORDER; c < 0; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  linebuf[pli][r * stride + coffset + c];
-            }
-          }
+          copy_nxm_16bit_to_16bit(src, OD_FILT_BSTRIDE, linebuf[pli] + coffset,
+                                  stride, OD_FILT_HBORDER, OD_FILT_VBORDER);
         } else {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = -OD_FILT_HBORDER; c < 0; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  OD_DERING_VERY_LARGE;
-            }
-          }
+          fill_nxm_16bit(&src[c - OD_FILT_HBORDER], OD_FILT_BSTRIDE,
+                         OD_FILT_HBORDER, OD_FILT_VBORDER,
+                         OD_DERING_VERY_LARGE);
         }
         if (!prev_row_dering[sbc + 1]) {
           copy_sb8_16(cm, &src[OD_FILT_HBORDER + nhbs], OD_FILT_BSTRIDE,
@@ -301,19 +291,13 @@ void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
                       coffset + nhbs, xd->plane[pli].dst.stride,
                       OD_FILT_VBORDER, OD_FILT_HBORDER);
         } else if (sbr > 0 && sbc < nhsb - 1) {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = nhbs; c < nhbs + OD_FILT_HBORDER; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  linebuf[pli][r * stride + coffset + c];
-            }
-          }
+          copy_nxm_16bit_to_16bit(&src[nhbs + OD_FILT_HBORDER], OD_FILT_BSTRIDE,
+                                  linebuf[pli] + coffset + nhbs, stride,
+                                  OD_FILT_HBORDER, OD_FILT_VBORDER);
         } else {
-          for (r = 0; r < OD_FILT_VBORDER; r++) {
-            for (c = nhbs; c < nhbs + OD_FILT_HBORDER; c++) {
-              src[r * OD_FILT_BSTRIDE + c + OD_FILT_HBORDER] =
-                  OD_DERING_VERY_LARGE;
-            }
-          }
+          fill_nxm_16bit(&src[nhbs + OD_FILT_HBORDER], OD_FILT_BSTRIDE,
+                         OD_FILT_HBORDER, OD_FILT_VBORDER,
+                         OD_DERING_VERY_LARGE);
         }
         if (dering_left) {
           /* If we deringed the superblock on the left then we need to copy in
