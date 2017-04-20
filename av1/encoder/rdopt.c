776cb4507710cc66aee135597c24cfa34b805a30
Yue Chen <yuec@google.com>
2017-04-20 16:19:39 -0700

    Make motion_var on by default
    The condition to adopt it has been satisfied after this patch
    https://aomedia-review.googlesource.com/c/10220/
    
    Also properly set params to default values to clear a unit test
    failure when seg feature and motion_var are both on.
    
    Change-Id: Iaf4ee47c9941a228638d6046bcd1206f4a4f566a

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -11474,6 +11474,11 @@ void av1_rd_pick_inter_mode_sb_seg_skip(const AV1_COMP *cpi,
   mbmi->pred_mv[0].as_int = 0;
 #endif  // CONFIG_REF_MV
 
+#if CONFIG_MOTION_VAR
+  mbmi->motion_mode = SIMPLE_TRANSLATION;
+  av1_count_overlappable_neighbors(cm, xd, mi_row, mi_col);
+#endif
+
   if (cm->interp_filter != BILINEAR) {
     best_filter = EIGHTTAP_REGULAR;
     if (cm->interp_filter == SWITCHABLE &&
