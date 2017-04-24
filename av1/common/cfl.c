40381575704d247180ee538377cccb998cac9204
Luc Trudeau <ltrudeau@mozilla.com>
2017-04-24 11:19:25 -0400

    [CFL] Load luma as prediction for chroma
    Loads the stored reconstructed luma pixels for each trasnform block
    inside a prediction block. Supports 4:4:4 and 4:2:0 chroma subsampling
    modes.
    
    The CFL_CTX struct is now in cfl.h with appropriate forward declarations
    
    Change-Id: I44c117899414a10a8318d14ecaed402f803de97d

--- av1/common/cfl.c
+++ av1/common/cfl.c
@@ -11,12 +11,26 @@
 
 #include "av1/common/cfl.h"
 #include "av1/common/common_data.h"
+#include "av1/common/onyxc_int.h"
+
+#include "aom/internal/aom_codec_internal.h"
+
+void cfl_init(CFL_CTX *cfl, AV1_COMMON *cm, int subsampling_x,
+              int subsampling_y) {
+  if (subsampling_x == 0 && subsampling_y == 1) {
+    aom_internal_error(&cm->error, AOM_CODEC_UNSUP_BITSTREAM,
+                       "4:2:2 color not supported by CfL");
+  }
+  memset(&cfl->y_pix, 0, sizeof(uint8_t) * MAX_SB_SQUARE);
+  cfl->subsampling_x = 1 << subsampling_x;
+  cfl->subsampling_y = 1 << subsampling_y;
+}
 
 // CfL computes its own block-level DC_PRED. This is required to compute both
 // alpha_cb and alpha_cr before the prediction are computed.
 void cfl_dc_pred(MACROBLOCKD *xd, BLOCK_SIZE plane_bsize, TX_SIZE tx_size) {
-  const struct macroblockd_plane *const pd_cb = &xd->plane[1];
-  const struct macroblockd_plane *const pd_cr = &xd->plane[2];
+  const struct macroblockd_plane *const pd_cb = &xd->plane[AOM_PLANE_U];
+  const struct macroblockd_plane *const pd_cr = &xd->plane[AOM_PLANE_V];
 
   const uint8_t *const dst_cb = pd_cb->dst.buf;
   const uint8_t *const dst_cr = pd_cr->dst.buf;
@@ -66,20 +80,25 @@ void cfl_dc_pred(MACROBLOCKD *xd, BLOCK_SIZE plane_bsize, TX_SIZE tx_size) {
     sum_cr += block_height * 129;
   }
 
-  xd->cfl->dc_pred[0] = (sum_cb + (num_pel >> 1)) / num_pel;
-  xd->cfl->dc_pred[1] = (sum_cr + (num_pel >> 1)) / num_pel;
+  xd->cfl->dc_pred[CFL_PRED_U] = (sum_cb + (num_pel >> 1)) / num_pel;
+  xd->cfl->dc_pred[CFL_PRED_V] = (sum_cr + (num_pel >> 1)) / num_pel;
 }
 
 // Predict the current transform block using CfL.
 // it is assumed that dst points at the start of the transform block
-void cfl_predict_block(uint8_t *dst, int dst_stride, TX_SIZE tx_size,
-                       int dc_pred) {
+void cfl_predict_block(const CFL_CTX *cfl, uint8_t *dst, int dst_stride,
+                       int row, int col, TX_SIZE tx_size, int dc_pred) {
   const int tx_block_width = tx_size_wide[tx_size];
   const int tx_block_height = tx_size_high[tx_size];
 
+  // TODO(ltrudeau) implement alpha
+  // Place holder for alpha
+  const double alpha = 0;
+  const double y_avg = cfl_load(cfl, dst, dst_stride, row, col, tx_size);
+
   for (int j = 0; j < tx_block_height; j++) {
     for (int i = 0; i < tx_block_width; i++) {
-      dst[i] = dc_pred;
+      dst[i] = (uint8_t)(alpha * y_avg + dc_pred + 0.5);
     }
     dst += dst_stride;
   }
@@ -87,6 +106,7 @@ void cfl_predict_block(uint8_t *dst, int dst_stride, TX_SIZE tx_size,
 
 void cfl_store(CFL_CTX *cfl, const uint8_t *input, int input_stride, int row,
                int col, TX_SIZE tx_size) {
+  // Store the input into the CfL pixel buffer
   const int tx_width = tx_size_wide[tx_size];
   const int tx_height = tx_size_high[tx_size];
   const int tx_off_log2 = tx_size_wide_log2[0];
@@ -116,4 +136,102 @@ void cfl_store(CFL_CTX *cfl, const uint8_t *input, int input_stride, int row,
     cfl->y_width = OD_MAXI((col << tx_off_log2) + tx_width, cfl->y_width);
     cfl->y_height = OD_MAXI((row << tx_off_log2) + tx_height, cfl->y_height);
   }
+}
+
+// Load from the CfL pixel buffer into output
+double cfl_load(const CFL_CTX *cfl, uint8_t *output, int output_stride, int row,
+                int col, TX_SIZE tx_size) {
+  const int tx_width = tx_size_wide[tx_size];
+  const int tx_height = tx_size_high[tx_size];
+  const int sub_x = cfl->subsampling_x;
+  const int sub_y = cfl->subsampling_y;
+  const int tx_off_log2 = tx_size_wide_log2[0];
+
+  const uint8_t *y_pix;
+
+  int uv_width, uv_height;
+  int diff_width = 0;
+  int diff_height = 0;
+
+  int pred_row_offset = 0;
+  int output_row_offset = 0;
+  int top_left, bot_left;
+
+  // TODO(ltrudeau) add support for 4:2:2
+  if (sub_y == 1 && sub_x == 1) {
+    y_pix = &cfl->y_pix[(row * MAX_SB_SIZE + col) << tx_off_log2];
+    uv_width = (col << tx_off_log2) + tx_width;
+    diff_width = uv_width - cfl->y_width;
+    uv_height = (row << tx_off_log2) + tx_width;
+    diff_height = uv_height - cfl->y_height;
+    for (int j = 0; j < tx_height; j++) {
+      for (int i = 0; i < tx_width; i++) {
+        // In 4:4:4, pixels match 1 to 1
+        output[output_row_offset + i] = y_pix[pred_row_offset + i];
+      }
+      pred_row_offset += MAX_SB_SIZE;
+      output_row_offset += output_stride;
+    }
+  } else if (sub_y == 2 && sub_x == 2) {
+    y_pix = &cfl->y_pix[(sub_y * (row * MAX_SB_SIZE + col)) << tx_off_log2];
+    uv_width = sub_x * ((col << tx_off_log2) + tx_width);
+    diff_width = (uv_width - cfl->y_width) / sub_x;
+    uv_height = sub_y * ((row << tx_off_log2) + tx_width);
+    diff_height = (uv_height - cfl->y_height) / sub_y;
+    for (int j = 0; j < tx_height; j++) {
+      for (int i = 0; i < tx_width; i++) {
+        top_left = sub_y * (pred_row_offset + i);
+        bot_left = top_left + MAX_SB_SIZE;
+        // In 4:2:0, average pixels in 2x2 grid
+        output[output_row_offset + i] = OD_SHR_ROUND(
+            y_pix[top_left] + y_pix[top_left + 1]        // Top row
+                + y_pix[bot_left] + y_pix[bot_left + 1]  // Bottom row
+            ,
+            2);
+      }
+      pred_row_offset += MAX_SB_SIZE;
+      output_row_offset += output_stride;
+    }
+  }
+  // Due to frame boundary issues, it is possible that the total area of
+  // covered by Chroma exceeds that of Luma. When this happens, we write over
+  // the broken data by repeating the last columns and/or rows.
+  //
+  // Note that in order to manage the case where both rows and columns
+  // overrun,
+  // we apply rows first. This way, when the rows overrun the bottom of the
+  // frame, the columns will be copied over them.
+  if (diff_width > 0) {
+    int last_pixel;
+    output_row_offset = tx_width - diff_width;
+
+    for (int j = 0; j < tx_height; j++) {
+      last_pixel = output_row_offset - 1;
+      for (int i = 0; i < diff_width; i++) {
+        output[output_row_offset + i] = output[last_pixel];
+      }
+      output_row_offset += output_stride;
+    }
+  }
+
+  if (diff_height > 0) {
+    output_row_offset = diff_height * output_stride;
+    const int last_row_offset = output_row_offset - output_stride;
+    for (int j = 0; j < diff_height; j++) {
+      for (int i = 0; i < tx_width; i++) {
+        output[output_row_offset + i] = output[last_row_offset + i];
+      }
+      output_row_offset += output_stride;
+    }
+  }
+
+  int avg = 0;
+  output_row_offset = 0;
+  for (int j = 0; j < tx_height; j++) {
+    for (int i = 0; i < tx_width; i++) {
+      avg += output[output_row_offset + i];
+    }
+    output_row_offset += output_stride;
+  }
+  return avg / (double)(tx_width * tx_height);
 }
