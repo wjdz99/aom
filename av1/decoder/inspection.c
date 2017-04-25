b29669f2ee26c05e6c342efd220829e3b734c393
Jingning Han <jingning@google.com>
2017-04-24 21:42:42 -0700

    Fix inspection in dual filter mode
    Change-Id: Ie6c8f6247436a5dde3183566eebf46b06e4cb169

--- av1/decoder/inspection.c
+++ av1/decoder/inspection.c
@@ -78,7 +78,12 @@ int ifd_inspect(insp_frame_data *fd, void *decoder) {
       // Skip Flag
       mi->skip = mbmi->skip;
       // Filters
+#if CONFIG_DUAL_FILTER
+      mi->filter[0] = mbmi->interp_filter[0];
+      mi->filter[1] = mbmi->interp_filter[1];
+#else
       mi->filter = mbmi->interp_filter;
+#endif
       // Transform
       mi->tx_type = mbmi->tx_type;
       mi->tx_size = mbmi->tx_size;
