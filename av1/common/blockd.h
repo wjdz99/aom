eed83028c38e228c44da09f734b18950be24c381
Angie Chiang <angiebird@google.com>
2017-04-25 16:08:47 -0700

    Merge RD_COST and RD_STATS
    Change-Id: I9b304230acd2f64d6eab59418c9535f8c302765b

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -281,21 +281,6 @@ typedef struct {
 #endif
 #endif
 
-// TODO(angiebird): Merge RD_COST and RD_STATS
-typedef struct RD_STATS {
-  int rate;
-  int64_t dist;
-  int64_t sse;
-  int skip;
-#if CONFIG_RD_DEBUG
-  int txb_coeff_cost[MAX_MB_PLANE];
-#if CONFIG_VAR_TX
-  int txb_coeff_cost_map[MAX_MB_PLANE][TXB_COEFF_COST_MAP_SIZE]
-                        [TXB_COEFF_COST_MAP_SIZE];
-#endif  // CONFIG_VAR_TX
-#endif  // CONFIG_RD_DEBUG
-} RD_STATS;
-
 #if CONFIG_EXT_INTER
 typedef struct {
   COMPOUND_TYPE type;
