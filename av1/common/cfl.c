f3d488018f473b09043865972538aef785cd2f73
Luc Trudeau <ltrudeau@mozilla.com>
2017-04-25 12:21:26 -0400

    [CFL] Alpha signaling
    Writes and reads alpha to and from the bitstream.
    
    A special case is needed on the encoder side to handle prediction block
    skips. Since whether or not a prediction block is skipped during CfL, a
    rollback is required if the block was skipped and the alpha index was
    not zero. The advantage of this is that no signaling is required when
    the prediction block is skipped as it is assumed tha the alpha index is
    zero.
    
    A encode facade is added to the intra prediction facade as CfL requires
    special encoder side operations.
    
    Change-Id: Ic3b11d0fdbd51389d862112eb09d8785127a6b06

--- av1/common/cfl.c
+++ av1/common/cfl.c
@@ -85,16 +85,20 @@ void cfl_dc_pred(MACROBLOCKD *xd, BLOCK_SIZE plane_bsize, TX_SIZE tx_size) {
   xd->cfl->dc_pred[CFL_PRED_V] = (sum_cr + (num_pel >> 1)) / num_pel;
 }
 
+double cfl_ind_to_alpha(const MB_MODE_INFO *const mbmi,
+                        CFL_PRED_TYPE pred_type) {
+  return (mbmi->cfl_alpha_signs[pred_type] == CFL_SIGN_POS)
+             ? cfl_alpha_codes[mbmi->cfl_alpha_ind][pred_type]
+             : -cfl_alpha_codes[mbmi->cfl_alpha_ind][pred_type];
+}
+
 // Predict the current transform block using CfL.
-// it is assumed that dst points at the start of the transform block
 void cfl_predict_block(const CFL_CTX *cfl, uint8_t *dst, int dst_stride,
-                       int row, int col, TX_SIZE tx_size, int dc_pred) {
+                       int row, int col, TX_SIZE tx_size, int dc_pred,
+                       double alpha) {
   const int tx_block_width = tx_size_wide[tx_size];
   const int tx_block_height = tx_size_high[tx_size];
 
-  // TODO(ltrudeau) implement alpha
-  // Place holder for alpha
-  const double alpha = 0;
   const double y_avg = cfl_load(cfl, dst, dst_stride, row, col, tx_size);
 
   for (int j = 0; j < tx_block_height; j++) {
