63a7b22dafb8b18036f08571bf84b9e0ba79cd78
Luc Trudeau <ltrudeau@mozilla.com>
2017-04-25 22:54:28 -0400

    [CfL] Store luma as prediction for chroma
    Stores the reconstructed luma pixels for each transform block inside a
    prediction block. Rectangular transform blocks are supported.
    
    As for RDO, after all the modes have been tested for luma, an extra
    encoding is perform in order to store the reconstructed pixel values of
    the best mode. These values are then used for RDO on the chromatic
    planes.
    
    Change-Id: I354d9827e32fd41065f1b2ce02832d943a6fa156

--- av1/common/cfl.h
+++ av1/common/cfl.h
@@ -15,10 +15,12 @@
 #include "av1/common/blockd.h"
 #include "av1/common/enums.h"
 
-void cfl_dc_pred(MACROBLOCKD *const xd, BLOCK_SIZE plane_bsize,
-                 TX_SIZE tx_size);
+void cfl_dc_pred(MACROBLOCKD *xd, BLOCK_SIZE plane_bsize, TX_SIZE tx_size);
 
-void cfl_predict_block(uint8_t *const dst, int dst_stride, TX_SIZE tx_size,
+void cfl_predict_block(uint8_t *dst, int dst_stride, TX_SIZE tx_size,
                        int dc_pred);
+
+void cfl_store(CFL_CTX *cfl, const uint8_t *input, int input_stride, int row,
+               int col, TX_SIZE tx_size);
 
 #endif  // AV1_COMMON_CFL_H_
