8cc58c412828826367a3e873f6174b93836199a8
hui su <huisu@google.com>
2017-05-03 17:29:36 -0700

    Plaette: automatically turn on/off
    Change-Id: I7f7a8b1c8d361e84bc79fb0ed73047d16b0e577a

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -486,8 +486,7 @@ static void dealloc_compressor_data(AV1_COMP *cpi) {
   av1_free_var_tree(&cpi->td);
 
 #if CONFIG_PALETTE
-  if (cpi->common.allow_screen_content_tools)
-    aom_free(cpi->td.mb.palette_buffer);
+  aom_free(cpi->td.mb.palette_buffer);
 #endif  // CONFIG_PALETTE
 
   if (cpi->source_diff_var != NULL) {
@@ -1996,7 +1995,8 @@ void av1_change_config(struct AV1_COMP *cpi, const AV1EncoderConfig *oxcf) {
 
 #if CONFIG_PALETTE
   cm->allow_screen_content_tools = (cpi->oxcf.content == AOM_CONTENT_SCREEN);
-  if (cm->allow_screen_content_tools) {
+  cm->auto_tune_content = (cpi->oxcf.content == AOM_CONTENT_AUTO);
+  if (cm->allow_screen_content_tools || cm->auto_tune_content) {
     MACROBLOCK *x = &cpi->td.mb;
     if (x->palette_buffer == 0) {
       CHECK_MEM_ERROR(cm, x->palette_buffer,
