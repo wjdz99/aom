5acc045744857d3468f1477114e2632403961583
David Barker <david.barker@argondesign.com>
2017-05-09 15:07:32 +0100

    Mismatch fix for resizing with ext-refs enabled
    There are various preconditions which need to be true for it to
    be valid to set cm->use_prev_frame_mvs = 1, including that the
    sizes of this frame and cm->prev_frame must be equal.
    
    With ext-refs enabled, we would sometimes decide to change
    cm->prev_frame to point to the LAST_FRAME reference, without
    re-checking the preconditions. If the LAST_FRAME was smaller
    than the current frame, this could lead to reading garbage off the
    end of its mv array, and eventually to an encode/decode mismatch.
    
    We fix this by rewriting the preconditions as checks on
    cm->prev_frame directly (rather than using cm->last_width and
    cm->last_height), and by testing the preconditions after any
    possible adjustment.
    
    This should not affect the bitstream unless ext-refs is enabled,
    but may affect the bitstream with ext-refs even if resizing is
    not used.
    
    BUG=aomedia:521
    
    Change-Id: I7dfd9ba82cdf77acc2e27e0c9f3aee21d6afeb54

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -4945,25 +4945,6 @@ void av1_decode_frame(AV1Decoder *pbi, const uint8_t *data,
 
   cm->setup_mi(cm);
 
-#if CONFIG_TEMPMV_SIGNALING
-  RefBuffer *last_ref_buf = &cm->frame_refs[LAST_FRAME - LAST_FRAME];
-  if (last_ref_buf->idx != INVALID_IDX) {
-    cm->prev_frame = &cm->buffer_pool->frame_bufs[last_ref_buf->idx];
-    if (cm->use_prev_frame_mvs) {
-      assert(!cm->error_resilient_mode &&
-             cm->width == last_ref_buf->buf->y_width &&
-             cm->height == last_ref_buf->buf->y_height &&
-             !cm->prev_frame->intra_only);
-    }
-  } else {
-    assert(cm->use_prev_frame_mvs == 0);
-  }
-#else
-  cm->use_prev_frame_mvs =
-      !cm->error_resilient_mode && cm->width == cm->last_width &&
-      cm->height == cm->last_height && !cm->last_intra_only &&
-      cm->last_show_frame && (cm->last_frame_type != KEY_FRAME);
-#endif
 #if CONFIG_EXT_REFS
   // NOTE(zoeliu): As cm->prev_frame can take neither a frame of
   //               show_exisiting_frame=1, nor can it take a frame not used as
@@ -4975,12 +4956,33 @@ void av1_decode_frame(AV1Decoder *pbi, const uint8_t *data,
   //               (1) Simply disable the use of previous frame mvs; or
   //               (2) Have cm->prev_frame point to one reference frame buffer,
   //                   e.g. LAST_FRAME.
-  if (cm->use_prev_frame_mvs && !dec_is_ref_frame_buf(pbi, cm->prev_frame)) {
+  if (!dec_is_ref_frame_buf(pbi, cm->prev_frame)) {
     // Reassign the LAST_FRAME buffer to cm->prev_frame.
     RefBuffer *last_fb_ref_buf = &cm->frame_refs[LAST_FRAME - LAST_FRAME];
     cm->prev_frame = &cm->buffer_pool->frame_bufs[last_fb_ref_buf->idx];
   }
 #endif  // CONFIG_EXT_REFS
+
+#if CONFIG_TEMPMV_SIGNALING
+  if (cm->use_prev_frame_mvs) {
+    RefBuffer *last_fb_ref_buf = &cm->frame_refs[LAST_FRAME - LAST_FRAME];
+    cm->prev_frame = &cm->buffer_pool->frame_bufs[last_fb_ref_buf->idx];
+    assert(!cm->error_resilient_mode &&
+           cm->width == last_fb_ref_buf->buf->y_width &&
+           cm->height == last_fb_ref_buf->buf->y_height &&
+           !cm->prev_frame->intra_only);
+  }
+#else
+  RefBuffer *last_fb_ref_buf = &cm->frame_refs[LAST_FRAME - LAST_FRAME];
+  if (last_fb_ref_buf->idx != INVALID_IDX)
+    cm->use_prev_frame_mvs = !cm->error_resilient_mode && cm->prev_frame &&
+        cm->width == cm->prev_frame->buf.y_crop_width &&
+        cm->height == cm->prev_frame->buf.y_crop_height &&
+        !cm->last_intra_only && cm->last_show_frame &&
+                           (cm->last_frame_type != KEY_FRAME);
+  else
+    cm->use_prev_frame_mvs = 0;
+#endif
 
   av1_setup_block_planes(xd, cm->subsampling_x, cm->subsampling_y);
 
