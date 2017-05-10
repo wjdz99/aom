a180f2a42bc622f15e23936dbba94c7764e2ae09
Thomas Daede <tdaede@mozilla.com>
2017-05-10 15:40:06 -0700

    Remove VAR_BASED_PARTITION.
    BUG=aomedia:526
    
    Change-Id: I5d9b86a36f412ded2d6f20e198d2f4de4f97aaeb

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -483,7 +483,6 @@ static void dealloc_compressor_data(AV1_COMP *cpi) {
   cpi->tile_tok[0][0] = 0;
 
   av1_free_pc_tree(&cpi->td);
-  av1_free_var_tree(&cpi->td);
 
 #if CONFIG_PALETTE
   if (cpi->common.allow_screen_content_tools)
@@ -2630,7 +2629,6 @@ void av1_remove_compressor(AV1_COMP *cpi) {
 #endif  // CONFIG_PALETTE
       aom_free(thread_data->td->counts);
       av1_free_pc_tree(thread_data->td);
-      av1_free_var_tree(thread_data->td);
       aom_free(thread_data->td);
     }
   }
@@ -4009,7 +4007,6 @@ static void encode_without_recode_loop(AV1_COMP *cpi) {
     reset_use_upsampled_references(cpi);
 
   av1_set_quantizer(cm, q);
-  av1_set_variance_partition_thresholds(cpi, q);
   setup_frame(cpi);
   suppress_active_map(cpi);
 
