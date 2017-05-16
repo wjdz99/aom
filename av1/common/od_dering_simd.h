975d46ffe126438822706f18d65d0f0c2261b2a8
Steinar Midtskogen <stemidts@cisco.com>
2017-05-16 14:31:53 +0200

    Fix a bug in the CDEF strength calculation.
    This will change the bitstream to agree with the spec.
    
    Low delay, cpu-used=4
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |   SSIM | MS SSIM | CIEDE 2000
    -0.0935 | -0.1839 | -0.2358 |  -0.1349 | 0.0493 | -0.0730 |    -0.1193
    
    High delay, cpu-used=4
      PSNR | PSNR Cb | PSNR Cr | PSNR HVS |   SSIM | MS SSIM | CIEDE 2000
    0.0000 | -0.0148 |  0.0362 |  -0.0184 | 0.1736 |  0.0826 |     0.0251
    
    Change-Id: I88256854cd5c6e983c144343440d9557319eb2d5

--- av1/common/od_dering_simd.h
+++ av1/common/od_dering_simd.h
@@ -204,10 +204,7 @@ int SIMD_FUNC(od_dir_find8)(const od_dering_in *img, int stride, int32_t *var,
 
   /* Difference between the optimal variance and the variance along the
      orthogonal direction. Again, the sum(x^2) terms cancel out. */
-  *var = best_cost - cost[(best_dir + 4) & 7];
-  /* We'd normally divide by 840, but dividing by 1024 is close enough
-     for what we're going to do with this. */
-  *var >>= 10;
+  *var = (best_cost - cost[(best_dir + 4) & 7]) >> 16;
   return best_dir;
 }
 
