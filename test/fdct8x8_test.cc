7d3852b3ecadca4a6ab5ad687c9deab8baeec70b
Sarah Parker <sarahparker@google.com>
2017-05-19 16:51:07 -0700

    Remove deprecated high-bitdepth functions
    This unifies the codepath for high-bitdepth transforms and deletes
    all calls to the old deprecated versions. This required reworking
    the way 1d configurations are combined in order to support rectangular
    transforms.
    
    There is one remaining codepath that calls the deprecated 4x4 hbd
    transform from encoder/encodemb.c. I need to take a closer look
    at what is happening there and will leave that for a followup
    since this change has already gotten so large.
    
    I tested results on the lowres set before a rebase, but want to redo
    them once more before submitting.
    lowres 10 bit: -0.035%
    lowres 12 bit: 0.021%
    
    Change-Id: I34cdeaed2461ed7942364147cef10d7d21e3779c

--- test/fdct8x8_test.cc
+++ test/fdct8x8_test.cc
@@ -87,12 +87,20 @@ void fht8x8_ref(const int16_t *in, tran_low_t *out, int stride, int tx_type) {
 }
 
 #if CONFIG_HIGHBITDEPTH
+void fht8x8_10(const int16_t *in, tran_low_t *out, int stride, int tx_type) {
+  av1_fwd_txfm2d_8x8_c(in, out, stride, tx_type, 10);
+}
+
+void fht8x8_12(const int16_t *in, tran_low_t *out, int stride, int tx_type) {
+  av1_fwd_txfm2d_8x8_c(in, out, stride, tx_type, 12);
+}
+
 void iht8x8_10(const tran_low_t *in, uint8_t *out, int stride, int tx_type) {
-  av1_highbd_iht8x8_64_add_c(in, out, stride, tx_type, 10);
+  av1_inv_txfm2d_add_8x8(in, CONVERT_TO_SHORTPTR(out), stride, tx_type, 10);
 }
 
 void iht8x8_12(const tran_low_t *in, uint8_t *out, int stride, int tx_type) {
-  av1_highbd_iht8x8_64_add_c(in, out, stride, tx_type, 12);
+  av1_inv_txfm2d_add_8x8(in, CONVERT_TO_SHORTPTR(out), stride, tx_type, 12);
 }
 
 #endif  // CONFIG_HIGHBITDEPTH
@@ -606,14 +614,14 @@ INSTANTIATE_TEST_CASE_P(
     C, FwdTrans8x8HT,
     ::testing::Values(
         make_tuple(&av1_fht8x8_c, &av1_iht8x8_64_add_c, 0, AOM_BITS_8),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_10, 0, AOM_BITS_10),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_10, 1, AOM_BITS_10),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_10, 2, AOM_BITS_10),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_10, 3, AOM_BITS_10),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_12, 0, AOM_BITS_12),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_12, 1, AOM_BITS_12),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_12, 2, AOM_BITS_12),
-        make_tuple(&av1_highbd_fht8x8_c, &iht8x8_12, 3, AOM_BITS_12),
+        make_tuple(&fht8x8_10, &iht8x8_10, 0, AOM_BITS_10),
+        make_tuple(&fht8x8_10, &iht8x8_10, 1, AOM_BITS_10),
+        make_tuple(&fht8x8_10, &iht8x8_10, 2, AOM_BITS_10),
+        make_tuple(&fht8x8_10, &iht8x8_10, 3, AOM_BITS_10),
+        make_tuple(&fht8x8_12, &iht8x8_12, 0, AOM_BITS_12),
+        make_tuple(&fht8x8_12, &iht8x8_12, 1, AOM_BITS_12),
+        make_tuple(&fht8x8_12, &iht8x8_12, 2, AOM_BITS_12),
+        make_tuple(&fht8x8_12, &iht8x8_12, 3, AOM_BITS_12),
         make_tuple(&av1_fht8x8_c, &av1_iht8x8_64_add_c, 1, AOM_BITS_8),
         make_tuple(&av1_fht8x8_c, &av1_iht8x8_64_add_c, 2, AOM_BITS_8),
         make_tuple(&av1_fht8x8_c, &av1_iht8x8_64_add_c, 3, AOM_BITS_8)));
