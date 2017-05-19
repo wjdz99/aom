21d2dfb8c5e79628363287fb7916f5272edc70d1
Sarah Parker <sarahparker@google.com>
2017-05-19 16:51:07 -0700

    Remove deprecated high-bitdepth functions
    This unifies the codepath for high-bitdepth transforms and deletes
    all calls to the old deprecated versions. This required reworking
    the way 1d configurations are combined in order to support rectangular
    transforms.
    
    There is one remaining codepath that calls the deprecated 4x4 hbd
    transform from encoder/encodemb.c. I need to take a closer look
    at what is happening there and will leave that for a followup
    since this change has already gotten so large.
    
    I tested results on the lowres set before a rebase, but want to redo
    them once more before submitting.
    lowres 10 bit: -0.035%
    lowres 12 bit: 0.021%
    
    BUG=aomedia:524
    
    Change-Id: I34cdeaed2461ed7942364147cef10d7d21e3779c

--- test/fdct4x4_test.cc
+++ test/fdct4x4_test.cc
@@ -55,6 +55,14 @@ void fwht4x4_ref(const int16_t *in, tran_low_t *out, int stride,
 }
 
 #if CONFIG_HIGHBITDEPTH
+void fht4x4_10(const int16_t *in, tran_low_t *out, int stride, int tx_type) {
+  av1_fwd_txfm2d_4x4_c(in, out, stride, tx_type, 10);
+}
+
+void fht4x4_12(const int16_t *in, tran_low_t *out, int stride, int tx_type) {
+  av1_fwd_txfm2d_4x4_c(in, out, stride, tx_type, 12);
+}
+
 void idct4x4_10(const tran_low_t *in, uint8_t *out, int stride) {
   aom_highbd_idct4x4_16_add_c(in, out, stride, 10);
 }
@@ -64,11 +72,11 @@ void idct4x4_12(const tran_low_t *in, uint8_t *out, int stride) {
 }
 
 void iht4x4_10(const tran_low_t *in, uint8_t *out, int stride, int tx_type) {
-  av1_highbd_iht4x4_16_add_c(in, out, stride, tx_type, 10);
+  av1_inv_txfm2d_add_4x4_c(in, CONVERT_TO_SHORTPTR(out), stride, tx_type, 10);
 }
 
 void iht4x4_12(const tran_low_t *in, uint8_t *out, int stride, int tx_type) {
-  av1_highbd_iht4x4_16_add_c(in, out, stride, tx_type, 12);
+  av1_inv_txfm2d_add_4x4_c(in, CONVERT_TO_SHORTPTR(out), stride, tx_type, 12);
 }
 
 void iwht4x4_10(const tran_low_t *in, uint8_t *out, int stride) {
@@ -224,14 +232,14 @@ INSTANTIATE_TEST_CASE_P(C, Trans4x4DCT,
 INSTANTIATE_TEST_CASE_P(
     C, Trans4x4HT,
     ::testing::Values(
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_10, 0, AOM_BITS_10, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_10, 1, AOM_BITS_10, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_10, 2, AOM_BITS_10, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_10, 3, AOM_BITS_10, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_12, 0, AOM_BITS_12, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_12, 1, AOM_BITS_12, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_12, 2, AOM_BITS_12, 16),
-        make_tuple(&av1_highbd_fht4x4_c, &iht4x4_12, 3, AOM_BITS_12, 16),
+        make_tuple(&fht4x4_10, &iht4x4_10, 0, AOM_BITS_10, 16),
+        make_tuple(&fht4x4_10, &iht4x4_10, 1, AOM_BITS_10, 16),
+        make_tuple(&fht4x4_10, &iht4x4_10, 2, AOM_BITS_10, 16),
+        make_tuple(&fht4x4_10, &iht4x4_10, 3, AOM_BITS_10, 16),
+        make_tuple(&fht4x4_12, &iht4x4_12, 0, AOM_BITS_12, 16),
+        make_tuple(&fht4x4_12, &iht4x4_12, 1, AOM_BITS_12, 16),
+        make_tuple(&fht4x4_12, &iht4x4_12, 2, AOM_BITS_12, 16),
+        make_tuple(&fht4x4_12, &iht4x4_12, 3, AOM_BITS_12, 16),
         make_tuple(&av1_fht4x4_c, &av1_iht4x4_16_add_c, 0, AOM_BITS_8, 16),
         make_tuple(&av1_fht4x4_c, &av1_iht4x4_16_add_c, 1, AOM_BITS_8, 16),
         make_tuple(&av1_fht4x4_c, &av1_iht4x4_16_add_c, 2, AOM_BITS_8, 16),
