95653dcec04b4ff6269b8be122b3a8e784538c87
Tom Finegan <tomfinegan@google.com>
2017-05-25 17:01:28 -0700

    Change some CMake build behaviors wrt example targets.
    - When using make for the build, put examples in the examples
      sub dir of the configuration dir to mimic the behavior of
      the configure build.
    
    !This part is WIP: DO NOT REVIEW THIS PATCH!
    
    - Include the examples in the install rule, but put them in
      bin/aom/examples, and then,
    - Split install into dist and install.
    - Docs and examples are dist _only_
    
    BUG=aomedia:76,aomedia:375
    
    Change-Id: If42832ebd21184e6f9bf95c3b43c6d4e05663bf2

---
+++ build/cmake/dist.cmake
@@ -0,0 +1,19 @@
+##
+## Copyright (c) 2017, Alliance for Open Media. All rights reserved
+##
+## This source code is subject to the terms of the BSD 2 Clause License and
+## the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+## was not distributed with this source code in the LICENSE file, you can
+## obtain it at www.aomedia.org/license/software. If the Alliance for Open
+## Media Patent License 1.0 was not distributed with this source code in the
+## PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+##
+
+message("======== AOM_ROOT=${AOM_ROOT}")
+message("======== AOM_CONFIG_DIR=${AOM_CONFIG_DIR}")
+message("======== AOM_DIST_DIR=${AOM_DIST_DIR}")
+message("======== AOM_DIST_EXAMPLES=${AOM_DIST_EXAMPLES}")
+message("======== AOM_DIST_APPS=${AOM_DIST_APPS}")
+message("======== AOM_DIST_INCLUDES=${AOM_DIST_INCLUDES}")
+message("======== AOM_DIST_LIBS=${AOM_DIST_LIBS}")
+message("======== ENABLE_DOCS=${ENABLE_DOCS}")
