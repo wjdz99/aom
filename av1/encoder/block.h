655bf3ef4d8283004ddc56078335cfad292a8a3a
Urvang Joshi <urvang@google.com>
2017-06-01 14:46:23 -0700

    Remove PALETTE flag
    This experiment is now adopted as it was cleared by Tapas.
    
    Note: Palette use can still be controlled by command-line option
    "--tune-content=..." in 'aomenc'.
    
    Change-Id: I832f49f20f60c34bdef5b424755849c496687e87

--- av1/encoder/block.h
+++ av1/encoder/block.h
@@ -91,12 +91,10 @@ typedef struct {
   int row_max;
 } MvLimits;
 
-#if CONFIG_PALETTE
 typedef struct {
   uint8_t best_palette_color_map[MAX_SB_SQUARE];
   float kmeans_data_buf[2 * MAX_SB_SQUARE];
 } PALETTE_BUFFER;
-#endif  // CONFIG_PALETTE
 
 typedef struct macroblock MACROBLOCK;
 struct macroblock {
@@ -149,9 +147,7 @@ struct macroblock {
   int32_t *mask_buf;
 #endif  // CONFIG_MOTION_VAR
 
-#if CONFIG_PALETTE
   PALETTE_BUFFER *palette_buffer;
-#endif  // CONFIG_PALETTE
 
   // These define limits to motion vector components to prevent them
   // from extending outside the UMV borders
