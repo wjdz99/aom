b4b7a9a36e87b934b38c6bb3457f8f3e1aa48981
Thomas Davies <thdavies@cisco.com>
2017-06-12 18:11:55 +0100

    NEW_MULTISYMBOL: adapt inter mode flags.
    Old inter_mode coding was obsoleted by REF_MV.
    
    Change-Id: Ib812f626fe9f8e3342eee5d7b0214ed55701bec3

--- av1/common/entropy.c
+++ av1/common/entropy.c
@@ -5692,7 +5692,12 @@ void av1_average_tile_inter_cdfs(AV1_COMMON *cm, FRAME_CONTEXT *fc,
 
   // FIXME: single_ref_cdf not defined
 
-  // FIXME: cdfs not defined for newmv_mode, zeromv_mode, drl_mode, new2mv_mode
+#if CONFIG_NEW_MULTISYMBOL
+  AVERAGE_TILE_CDFS(newmv_cdf)
+  AVERAGE_TILE_CDFS(zeromv_cdf)
+  AVERAGE_TILE_CDFS(refmv_cdf)
+  AVERAGE_TILE_CDFS(drl_cdf)
+#endif
 
   // FIXME: cdfs not defined for motion_mode_prob, obmc_prob
 
