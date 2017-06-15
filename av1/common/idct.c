c875d1d79c3f0e03be2625e3e141cef5f9f75390
Monty Montgomery <cmontgomery@mozilla.com>
2017-06-15 17:53:31 -0400

    Add CONFIG_DAALA_DCT4 experiment.
    This experiment replaces the 4-point Type-II scaled-output vp9 DCT
     transform with the 4-point Type-II orthonormal Daala DCT transform.
    Right now the CONFIG_DAALA_DCT4 experiment depends on CONFIG_DCT_ONLY
     as it does not add an orthonormal 4-point DST.
    
    subset-1:
    
    master-dct_only@2017-02-21T02:57:28.585Z ->
      daala_dct4-dct_only@2017-02-21T14:40:29.830Z
    
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |   SSIM | MS SSIM | CIEDE 2000
    -0.0014 | -0.0036 |  0.1561 |   0.0134 | 0.0135 |  0.0109 |     0.0506
    
    objective-1-fast:
    
    master-dct_only@2017-02-20T16:54:03.578Z ->
     daala_dct4-dct_only@2017-02-21T14:58:30.647Z
    
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    -0.0323 | -0.0161 | -0.2828 |  -0.0265 | -0.0516 | -0.0573 |    -0.0411
    
    Change-Id: Idef8f6e5525037d5bbb2d0927675c21d1922d69a

--- av1/common/idct.c
+++ av1/common/idct.c
@@ -198,10 +198,12 @@ static void highbd_inv_idtx_add_c(const tran_low_t *input, uint8_t *dest8,
 
 void av1_iht4x4_16_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                          int tx_type) {
+#if !CONFIG_DAALA_DCT4
   if (tx_type == DCT_DCT) {
     aom_idct4x4_16_add(input, dest, stride);
     return;
   }
+#endif
   static const transform_2d IHT_4[] = {
     { aom_idct4_c, aom_idct4_c },    // DCT_DCT  = 0
     { aom_iadst4_c, aom_idct4_c },   // ADST_DCT = 1
@@ -235,7 +237,13 @@ void av1_iht4x4_16_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 
   // inverse transform row vectors
   for (i = 0; i < 4; ++i) {
+#if CONFIG_DAALA_DCT4 && CONFIG_DCT_ONLY
+    tran_low_t temp_in[4];
+    for (j = 0; j < 4; j++) temp_in[j] = input[j] << 1;
+    IHT_4[tx_type].rows(temp_in, out[i]);
+#else
     IHT_4[tx_type].rows(input, out[i]);
+#endif
     input += 4;
   }
 
@@ -260,7 +268,11 @@ void av1_iht4x4_16_add_c(const tran_low_t *input, uint8_t *dest, int stride,
     for (j = 0; j < 4; ++j) {
       int d = i * stride + j;
       int s = j * outstride + i;
+#if CONFIG_DAALA_DCT4 && CONFIG_DCT_ONLY
       dest[d] = clip_pixel_add(dest[d], ROUND_POWER_OF_TWO(outp[s], 4));
+#else
+      dest[d] = clip_pixel_add(dest[d], ROUND_POWER_OF_TWO(outp[s], 4));
+#endif
     }
   }
 }
@@ -388,7 +400,7 @@ void av1_iht8x4_32_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht4x16_64_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                           int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+    assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_4x16[] = {
     { aom_idct16_c, aom_idct4_c },    // DCT_DCT
@@ -562,7 +574,7 @@ void av1_iht8x16_128_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht16x8_128_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                            int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+    assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_16x8[] = {
     { aom_idct8_c, aom_idct16_c },    // DCT_DCT
@@ -622,7 +634,7 @@ void av1_iht16x8_128_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht8x32_256_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                            int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+    assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_8x32[] = {
     { aom_idct32_c, aom_idct8_c },     // DCT_DCT
@@ -796,7 +808,7 @@ void av1_iht16x32_512_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht32x16_512_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                             int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+    assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_32x16[] = {
     { aom_idct16_c, aom_idct32_c },     // DCT_DCT
@@ -856,7 +868,7 @@ void av1_iht32x16_512_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht8x8_64_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                          int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+    assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_8[] = {
     { aom_idct8_c, aom_idct8_c },    // DCT_DCT  = 0
@@ -1047,7 +1059,7 @@ void av1_iht32x32_1024_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 void av1_iht64x64_4096_add_c(const tran_low_t *input, uint8_t *dest, int stride,
                              int tx_type) {
 #if CONFIG_DCT_ONLY
-  assert(tx_type == DCT_DCT);
+                          assert(tx_type == DCT_DCT);
 #endif
   static const transform_2d IHT_64[] = {
     { idct64_col_c, idct64_row_c },      // DCT_DCT
@@ -1244,7 +1256,11 @@ static void inv_txfm_add_4x4(const tran_low_t *input, uint8_t *dest, int stride,
   }
 
   switch (tx_type) {
+#if !CONFIG_DAALA_DCT4
     case DCT_DCT: av1_idct4x4_add(input, dest, stride, eob); break;
+#else
+    case DCT_DCT:
+#endif
     case ADST_DCT:
     case DCT_ADST:
     case ADST_ADST: av1_iht4x4_16_add(input, dest, stride, tx_type); break;
