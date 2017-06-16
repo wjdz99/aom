916bf773ec5f4e6addfd56599dba91e33ee83508
Sebastien Alaiwan <sebastien.alaiwan@allegrodvt.com>
2017-06-16 10:11:58 +0200

    Clamp inverse transform coeffs after 1st/2nd pass
    Add clamping to 16/18/20 bits after first pass of the inverse
    transform for 8/10/12 bit source espectively, and clampaing to 16
    bits after second stage of the inverse transform.
    
    BUG=aomedia:30
    
    Change-Id: I7a68334c77baecb359a4fb3b801fdfd9d5a62f29

--- av1/common/av1_inv_txfm1d.c
+++ av1/common/av1_inv_txfm1d.c
@@ -67,7 +67,15 @@ void range_check_func(int32_t stage, const int32_t *input, const int32_t *buf,
     (void)size;                                   \
     (void)bit;                                    \
   }
-#endif
+#endif  // CONFIG_COEFFICIENT_RANGE_CHECKING
+
+void clamp_buf(int32_t *buf, int32_t size, int8_t bit) {
+  const int64_t maxValue = (1LL << (bit - 1)) - 1;
+  const int64_t minValue = -(1LL << (bit - 1));
+
+  for (int i = 0; i < size; ++i)
+    buf[i] = (int32_t)clamp64(buf[i], minValue, maxValue);
+}
 
 // TODO(angiebird): Make 1-d txfm functions static
 void av1_idct4_new(const int32_t *input, int32_t *output, const int8_t *cos_bit,
