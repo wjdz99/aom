9dc00510a9a13a2ded6f8b4ed9d8c82392abffca
Luc Trudeau <luc@trud.ca>
2017-06-21 13:13:08 -0400

    [CFL] UV_PREDICTION_MODE
    A separate prediction mode struct is added to allow
    for uv-only modes (like CfL). Note: CfL will be
    added as a separate mode in an upcoming commit.
    
    Change-Id: Ie80711c641c97f745daac899eadce6201ed97fcc

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -5377,7 +5377,11 @@ static void sum_intra_stats(FRAME_COUNTS *counts, MACROBLOCKD *xd,
                             const int mi_row, const int mi_col) {
   const MB_MODE_INFO *const mbmi = &mi->mbmi;
   const PREDICTION_MODE y_mode = mbmi->mode;
+#if CONFIG_CFL
+  const UV_PREDICTION_MODE uv_mode = mbmi->uv_mode;
+#else
   const PREDICTION_MODE uv_mode = mbmi->uv_mode;
+#endif  // CONFIG_CFL
   const BLOCK_SIZE bsize = mbmi->sb_type;
   const int unify_bsize = CONFIG_CB4X4;
 
@@ -5415,7 +5419,11 @@ static void sum_intra_stats(FRAME_COUNTS *counts, MACROBLOCKD *xd,
           mbmi->filter_intra_mode_info.use_filter_intra_mode[0];
       ++counts->filter_intra[0][use_filter_intra_mode];
     }
+#if CONFIG_CFL
+    if (mbmi->uv_mode == UV_DC_PRED
+#else
     if (mbmi->uv_mode == DC_PRED
+#endif  // CONFIG_CFL
 #if CONFIG_CB4X4
         &&
         is_chroma_reference(mi_row, mi_col, bsize, xd->plane[1].subsampling_x,
