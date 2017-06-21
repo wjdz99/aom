dd96f26a549063a3a9c0661c1cd31ad6fc2afe80
Luc Trudeau <luc@trud.ca>
2017-06-21 13:52:36 -0400

    [CFL] New UV_PREDICTION_MODE for CFL
    CfL is now an independent mode.
    
    Results on Subset1 (Compared to 8657ad with CFL enabled)
    
      PSNR | PSNR Cb | PSNR Cr | PSNR HVS |   SSIM | MS SSIM | CIEDE 2000
    
    Change-Id: I2e86e7ea7bfc12bb1d763e70a136ca992d57a3c5

--- av1/decoder/decodemv.c
+++ av1/decoder/decodemv.c
@@ -163,7 +163,12 @@ static PREDICTION_MODE read_intra_mode_uv(
     FRAME_CONTEXT *ec_ctx, MACROBLOCKD *xd, aom_reader *r,
     PREDICTION_MODE y_mode) {
   const PREDICTION_MODE uv_mode =
+#if CONFIG_CFL
+      aom_read_symbol(r, ec_ctx->uv_mode_cdf[y_mode], UV_INTRA_MODES, ACCT_STR);
+#else
       read_intra_mode(r, ec_ctx->uv_mode_cdf[y_mode]);
+#endif  // CONFIG_CFL
+
 #if !CONFIG_EC_ADAPT || CONFIG_ENTROPY_STATS
   FRAME_COUNTS *counts = xd->counts;
   if (counts) ++counts->uv_mode[y_mode][uv_mode];
@@ -1224,7 +1229,7 @@ static void read_intra_frame_mode_info(AV1_COMMON *const cm,
 
 #if CONFIG_CFL
     // TODO(ltrudeau) support PALETTE
-    if (mbmi->uv_mode == UV_DC_PRED) {
+    if (mbmi->uv_mode == UV_CFL_PRED) {
       mbmi->cfl_alpha_idx = read_cfl_alphas(ec_ctx, r, mbmi->cfl_alpha_signs);
     }
 #endif  // CONFIG_CFL
@@ -1719,7 +1724,7 @@ static void read_intra_block_mode_info(AV1_COMMON *const cm, const int mi_row,
 
 #if CONFIG_CFL
     // TODO(ltrudeau) support PALETTE
-    if (mbmi->uv_mode == UV_DC_PRED) {
+    if (mbmi->uv_mode == UV_CFL_PRED) {
       mbmi->cfl_alpha_idx = read_cfl_alphas(
 #if CONFIG_EC_ADAPT
           xd->tile_ctx,
