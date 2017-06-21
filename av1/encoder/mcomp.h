b21f8bd9d8a5788b846f5ef7f4778cf56adda026
RogerZhou <youzhou@microsoft.com>
2017-06-21 16:55:59 -0700

    hash based motion estimation for screen data
    Change-Id: I617a4f26e779e9cc03e3d8cbe9e6186e4e96cf87

--- av1/encoder/mcomp.h
+++ av1/encoder/mcomp.h
@@ -134,7 +134,12 @@ struct AV1_COMP;
 int av1_full_pixel_search(const struct AV1_COMP *cpi, MACROBLOCK *x,
                           BLOCK_SIZE bsize, MV *mvp_full, int step_param,
                           int error_per_bit, int *cost_list, const MV *ref_mv,
-                          int var_max, int rd);
+                          int var_max, int rd
+#if CONFIG_HASH_ME
+                          ,
+                          int xPos, int yPos
+#endif
+                          );
 
 #if CONFIG_MOTION_VAR
 int av1_obmc_full_pixel_diamond(const struct AV1_COMP *cpi, MACROBLOCK *x,
