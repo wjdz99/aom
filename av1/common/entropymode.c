38646e43ba8f9fcabfc68f3b4e28056a39f5ee4c
Thomas Daede <tdaede@mozilla.com>
2017-06-22 17:49:11 -0700

    Frame context signaling: Remove reset symbols from the bitstream.
    Because frame contexts now follow reference frames, explicit resets
    are no longer necessary, but can simply happen at the same time
    as reference frame resets.
    
    Change-Id: Idbed3794e3ed52fa298346943a3014fa1ca23897

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -5125,7 +5125,6 @@ void av1_setup_past_independence(AV1_COMMON *cm) {
 
   // To force update of the sharpness
   lf->last_sharpness_level = -1;
-
   av1_default_coef_probs(cm);
   init_mode_probs(cm->fc);
   av1_init_mv_probs(cm);
@@ -5138,19 +5137,21 @@ void av1_setup_past_independence(AV1_COMMON *cm) {
   av1_convolve_init(cm);
   cm->fc->initialized = 1;
 
+#if CONFIG_FRAME_CONTEXT_SIGNALING
+  if (cm->frame_type == KEY_FRAME) {
+    // Reset all frame contexts, as all reference frames will be lost.
+    for (i = 0; i < FRAME_CONTEXTS; ++i) cm->frame_contexts[i] = *cm->fc;
+  }
+#else
   if (cm->frame_type == KEY_FRAME || cm->error_resilient_mode ||
       cm->reset_frame_context == RESET_FRAME_CONTEXT_ALL) {
     // Reset all frame contexts.
     for (i = 0; i < FRAME_CONTEXTS; ++i) cm->frame_contexts[i] = *cm->fc;
   } else if (cm->reset_frame_context == RESET_FRAME_CONTEXT_CURRENT) {
-#if CONFIG_FRAME_CONTEXT_SIGNALING
-    // Reset the frame context of the first specified ref frame.
-    cm->frame_contexts[cm->frame_refs[0].idx] = *cm->fc;
-#else
     // Reset only the frame context specified in the frame header.
     cm->frame_contexts[cm->frame_context_idx] = *cm->fc;
-#endif  // CONFIG_FRAME_CONTEXT_SIGNALING
   }
+#endif  // CONFIG_FRAME_CONTEXT_SIGNALING
 
   // prev_mip will only be allocated in encoder.
   if (frame_is_intra_only(cm) && cm->prev_mip && !cm->frame_parallel_decode)
