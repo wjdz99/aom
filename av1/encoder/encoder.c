795feec3d40d545a1fae8fd3c02902ad350fe371
Thomas Daede <tdaede@mozilla.com>
2017-06-22 17:49:11 -0700

    Frame context signaling: Remove reset symbols from the bitstream.
    Because frame contexts now follow reference frames, explicit resets
    are no longer necessary, but can simply happen at the same time
    as reference frame resets.
    
    Change-Id: Idbed3794e3ed52fa298346943a3014fa1ca23897

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -351,6 +351,7 @@ static void setup_frame(AV1_COMP *cpi) {
     // use default frame context values
     cm->pre_fc = &cm->frame_contexts[FRAME_CONTEXT_DEFAULTS];
   } else {
+    *cm->fc = cm->frame_contexts[cm->frame_refs[0].idx];
     cm->pre_fc = &cm->frame_contexts[cm->frame_refs[0].idx];
   }
 #else
@@ -2213,7 +2214,9 @@ void av1_change_config(struct AV1_COMP *cpi, const AV1EncoderConfig *oxcf) {
       (oxcf->error_resilient_mode || oxcf->frame_parallel_decoding_mode)
           ? REFRESH_FRAME_CONTEXT_FORWARD
           : REFRESH_FRAME_CONTEXT_BACKWARD;
+#if !CONFIG_NO_FRAME_CONTEXT_SIGNALING
   cm->reset_frame_context = RESET_FRAME_CONTEXT_NONE;
+#endif
 
 #if CONFIG_PALETTE
   if (x->palette_buffer == NULL) {
@@ -4749,6 +4752,7 @@ static void encode_frame_to_data_rate(AV1_COMP *cpi, size_t *size,
 
     cm->error_resilient_mode = oxcf->error_resilient_mode;
 
+#if !CONFIG_NO_FRAME_CONTEXT_SIGNALING
     // By default, encoder assumes decoder can use prev_mi.
     if (cm->error_resilient_mode) {
       cm->reset_frame_context = RESET_FRAME_CONTEXT_NONE;
@@ -4757,6 +4761,7 @@ static void encode_frame_to_data_rate(AV1_COMP *cpi, size_t *size,
       // Only reset the current context.
       cm->reset_frame_context = RESET_FRAME_CONTEXT_CURRENT;
     }
+#endif
   }
   if (cpi->oxcf.mtu == 0) {
     cm->num_tg = cpi->oxcf.num_tile_groups;
@@ -5462,8 +5467,10 @@ int av1_get_compressed_data(AV1_COMP *cpi, unsigned int *frame_flags,
   else
     cpi->multi_arf_allowed = 0;
 
-  // Normal defaults
+// Normal defaults
+#if !CONFIG_NO_FRAME_CONTEXT_SIGNALING
   cm->reset_frame_context = RESET_FRAME_CONTEXT_NONE;
+#endif
   cm->refresh_frame_context =
       (oxcf->error_resilient_mode || oxcf->frame_parallel_decoding_mode)
           ? REFRESH_FRAME_CONTEXT_FORWARD
@@ -5733,8 +5740,7 @@ int av1_get_compressed_data(AV1_COMP *cpi, unsigned int *frame_flags,
   }
 #endif
 #if CONFIG_NO_FRAME_CONTEXT_SIGNALING
-  // TODO(tdaede): Check if this condition is really needed.
-  if (!cm->error_resilient_mode) cm->frame_contexts[cm->new_fb_idx] = *cm->fc;
+  cm->frame_contexts[cm->new_fb_idx] = *cm->fc;
 #else
   if (!cm->error_resilient_mode)
     cm->frame_contexts[cm->frame_context_idx] = *cm->fc;
