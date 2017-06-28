d3b25f9eae9218f4c21ef81d0c07bfdf14b21cfb
Yunqing Wang <yunqingwang@google.com>
2017-06-28 15:49:48 -0700

    Sample selection in warped motion
    Added a sample selection process in warped motion.
    1. Gather more samples including multiple rows on the top, multiple
    columns on the left, and the upper-right block.
    2. Sort samples by the MV difference between the neighbour's MV and
    the current block's MV. Trim the samples with considerably large MV
    difference.
    
    Borg test result:
                 avg_psnr ovr_psnr ssim
    cam_lowres:  -0.169   -0.170  -0.286
    lowres:      -0.085   -0.084  -0.148
    
    Change-Id: I2730bb31a0a3ad28215ccd16fd6da0ea8b2ed404

--- av1/encoder/mcomp.h
+++ av1/encoder/mcomp.h
@@ -158,8 +158,9 @@ unsigned int av1_compute_motion_cost(const struct AV1_COMP *cpi,
                                      int mi_row, int mi_col, const MV *this_mv);
 unsigned int av1_refine_warped_mv(const struct AV1_COMP *cpi,
                                   MACROBLOCK *const x, BLOCK_SIZE bsize,
-                                  int mi_row, int mi_col, int *pts,
-                                  int *pts_inref);
+                                  int mi_row, int mi_col, int *pts0,
+                                  int *pts_inref0, int *pts_mv0,
+                                  int total_samples);
 #endif  // CONFIG_WARPED_MOTION
 
 #endif  // AV1_ENCODER_MCOMP_H_
