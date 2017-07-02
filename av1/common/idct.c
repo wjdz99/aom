bae5fca6e2fb3b38e3e4b298b619d81a3d8e2180
Monty Montgomery <cmontgomery@mozilla.com>
2017-07-02 04:08:01 -0400

    Add CONFIG_DAALA_DCT16 experiment.
        This experiment replaces the 16-point Type-II DCT and 16-point Type-IV DST
         scaling vp9 transforms with the 16-point orthonormal Daala transforms.
        These have reduced complexity and are perfect reconstruction at the cost
         of a very slightly worse coding performance.
        This is because the Daala transforms expect the input to be shifted by 4
         bits but the output scale of the vp9 transforms is only 3 bits.
    
        subset-1:
    
        monty-baseline-dctonly-squaretx-subset1 → monty-dct16-dctonly-squaretx-subset1
    
         PSNR | PSNR HVS |  SSIM | CIEDE 2000 | PSNR Cb | PSNR Cr | MS SSIM
         0.01       0.06    0.03         0.03     -0.03     -0.12      0.05
    
        monty-baseline-squaretx-subset1 → monty-dct16-squaretx-subset1
    
         PSNR | PSNR HVS |  SSIM | CIEDE 2000 | PSNR Cb | PSNR Cr | MS SSIM
        -0.14 |     0.19 | -0.14 |       0.14 |    0.45 |    0.55 |    0.01
    
        objective-1-fast:
    
    Change-Id: I54fed26d65fd8450693334bb400b1fafd7e0dacb

--- av1/common/idct.c
+++ av1/common/idct.c
@@ -58,8 +58,13 @@ static void iidtx8_c(const tran_low_t *input, tran_low_t *output) {
 
 static void iidtx16_c(const tran_low_t *input, tran_low_t *output) {
   int i;
-  for (i = 0; i < 16; ++i)
+  for (i = 0; i < 16; ++i) {
+#if CONFIG_DAALA_DCT16
+    output[i] = input[i];
+#else
     output[i] = (tran_low_t)dct_const_round_shift(input[i] * 2 * Sqrt2);
+#endif
+  }
 }
 
 static void iidtx32_c(const tran_low_t *input, tran_low_t *output) {
@@ -984,7 +989,13 @@ void av1_iht16x16_256_add_c(const tran_low_t *input, uint8_t *dest, int stride,
 
   // inverse transform row vectors
   for (i = 0; i < 16; ++i) {
+#if CONFIG_DAALA_DCT16
+    tran_low_t temp_in[16];
+    for (j = 0; j < 16; j++) temp_in[j] = input[j] << 1;
+    IHT_16[tx_type].rows(temp_in, out[i]);
+#else
     IHT_16[tx_type].rows(input, out[i]);
+#endif
     input += 16;
   }
 
@@ -1009,7 +1020,11 @@ void av1_iht16x16_256_add_c(const tran_low_t *input, uint8_t *dest, int stride,
     for (j = 0; j < 16; ++j) {
       int d = i * stride + j;
       int s = j * outstride + i;
+#if CONFIG_DAALA_DCT16
+      dest[d] = clip_pixel_add(dest[d], ROUND_POWER_OF_TWO(outp[s], 4));
+#else
       dest[d] = clip_pixel_add(dest[d], ROUND_POWER_OF_TWO(outp[s], 6));
+#endif
     }
   }
 }
@@ -1187,6 +1202,7 @@ static void idct8x8_add(const tran_low_t *input, uint8_t *dest, int stride,
 }
 #endif
 
+#if !CONFIG_DAALA_DCT16
 static void idct16x16_add(const tran_low_t *input, uint8_t *dest, int stride,
                           const INV_TXFM_PARAM *param) {
 // The calculation can be simplified if there are not many non-zero dct
@@ -1209,6 +1225,7 @@ static void idct16x16_add(const tran_low_t *input, uint8_t *dest, int stride,
   else
     aom_idct16x16_256_add(input, dest, stride);
 }
+#endif
 
 static void idct32x32_add(const tran_low_t *input, uint8_t *dest, int stride,
                           const INV_TXFM_PARAM *param) {
@@ -1469,7 +1486,11 @@ static void inv_txfm_add_16x16(const tran_low_t *input, uint8_t *dest,
                                int stride, const INV_TXFM_PARAM *param) {
   const TX_TYPE tx_type = param->tx_type;
   switch (tx_type) {
+#if !CONFIG_DAALA_DCT16
     case DCT_DCT: idct16x16_add(input, dest, stride, param); break;
+#else
+    case DCT_DCT:
+#endif
     case ADST_DCT:
     case DCT_ADST:
     case ADST_ADST: av1_iht16x16_256_add(input, dest, stride, tx_type); break;
