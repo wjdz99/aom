fff630762dfb5d07076d3c03a844da4925944e64
Luc Trudeau <luc@trud.ca>
2017-07-04 12:41:53 -0400

    [CFL] Convert dc_pred to fixed point
    The dc_pred values stored in the CfL context are in Q8.7 (Worst case
    division will be of 1/128).
    
    Change-Id: I0701e04fb76f03eff12ed01fd5fda675fbb15e32

--- av1/common/cfl.h
+++ av1/common/cfl.h
@@ -53,8 +53,10 @@ typedef struct {
   // Chroma subsampling
   int subsampling_x, subsampling_y;
 
-  // CfL Performs its own block level DC_PRED for each chromatic plane
-  double dc_pred[CFL_PRED_PLANES];
+  // Block level DC_PRED for each chromatic plane
+  // Fixed point dc_pred is Q12.7:
+  //   * Worst case division is 1/128
+  int dc_pred_q7[CFL_PRED_PLANES];
 
   // The rate associated with each alpha codeword
   int costs[CFL_ALPHABET_SIZE];
