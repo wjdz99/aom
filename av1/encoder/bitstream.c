6930102633b52c2bef1581dd17b599b1fa5346a8
Jingning Han <jingning@google.com>
2017-07-06 15:10:12 -0700

    Refactor txk_type indexing system
    Explicitly use the row and column indexes to fetch txk_type.
    This allows the chroma components to fetch the tx type from the
    corresponding luma components. This improves the coding performance
    of txk-sel by 0.2%.
    
    Change-Id: I3f4bca5839e13ae95e51053e76cd86fe58202ac9

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1552,7 +1552,8 @@ void av1_write_tx_type(const AV1_COMMON *const cm, const MACROBLOCKD *xd,
                        const int supertx_enabled,
 #endif
 #if CONFIG_TXK_SEL
-                       int block, int plane, TX_SIZE tx_size,
+                       int blk_row, int blk_col, int block, int plane,
+                       TX_SIZE tx_size,
 #endif
                        aom_writer *w) {
   MB_MODE_INFO *mbmi = &xd->mi[0]->mbmi;
@@ -1572,7 +1573,8 @@ void av1_write_tx_type(const AV1_COMMON *const cm, const MACROBLOCKD *xd,
   // Only y plane's tx_type is transmitted
   if (plane > 0) return;
   PLANE_TYPE plane_type = get_plane_type(plane);
-  TX_TYPE tx_type = get_tx_type(plane_type, xd, block, tx_size);
+  TX_TYPE tx_type =
+      get_tx_type(plane_type, xd, blk_row, blk_col, block, tx_size);
 #endif
 
   if (!FIXED_TX_TYPE) {
