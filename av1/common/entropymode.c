46d18993de6f720a93e2735bf09971d64bca52d7
Tristan Matthews <tmatth@videolan.org>
2017-07-11 23:48:00 -0400

    entropymode: fix sizeof mismatch
    Fixes "aomenc: init_mode_probs: Assertion `sizeof(fc->motion_mode_prob)
    == sizeof(default_motion_mode_prob)' failed"
    
    Change-Id: I7b71304f50756ffd6e14b03fe338c0d624ce35c5

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -714,7 +714,7 @@ static const aom_prob
 const aom_tree_index av1_motion_mode_tree[TREE_SIZE(MOTION_MODES)] = {
   -SIMPLE_TRANSLATION, -OBMC_CAUSAL
 };
-static const aom_cdf_prob
+static const aom_prob
     default_motion_mode_prob[BLOCK_SIZES][MOTION_MODES - 1] = {
 #if CONFIG_CHROMA_2X2 || CONFIG_CHROMA_SUB8X8
       { 255 }, { 255 }, { 255 },
