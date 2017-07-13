d2a9d16b1cf5a7daa9b9ee463dd2134519fc21bb
Wei-Ting Lin <weitinglin@google.com>
2017-07-13 11:59:34 -0700

    ncobmc_adapt_weight: add rd functions to select ncobmc modes
    These functions will be called at the final round of
    encoding each superblock, wherein a ncobmc_adapr_weight
    prediction will be built to check if it can provide
    better results.
    
    This commit contains two approaches to build the prediction.
    One of them divides a squared block into four quadrants and
    select an ncobmc mode for each quadrant. This method increases
    the number of interpolation kernels we can used, and is used for
    training purpose.
    
    Change-Id: I117773a8133485362eebad5f3f55443cb5dc5136

--- av1/encoder/rdopt.h
+++ av1/encoder/rdopt.h
@@ -146,4 +146,22 @@ int av1_tx_type_cost(const AV1_COMP *cpi, const MACROBLOCKD *xd,
                      BLOCK_SIZE bsize, int plane, TX_SIZE tx_size,
                      TX_TYPE tx_type);
 
+#if CONFIG_NCOBMC_ADAPT_WEIGHT
+void av1_check_ncobmc_adapt_weight_rd(const struct AV1_COMP *cpi,
+                                      struct macroblock *x, int mi_row,
+                                      int mi_col);
+
+// used in training
+int get_ncobmc_mode_qd(const AV1_COMP *const cpi, MACROBLOCK *const x,
+                       MACROBLOCKD *xd, int mi_row, int mi_col, int bsize,
+                       int xd_mi_offset, int block);
+
+int get_ncobmc_mode(const AV1_COMP *const cpi, MACROBLOCK *const x,
+                    MACROBLOCKD *xd, int mi_row, int mi_col, int bsize,
+                    int xd_mi_offset);
+
+void av1_setup_src_planes_pxl(MACROBLOCK *x, const YV12_BUFFER_CONFIG *src,
+                              int pxl_row, int pxl_col);
+#endif
+
 #endif  // AV1_ENCODER_RDOPT_H_
