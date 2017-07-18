5e5d79fd4e1597783072f3f263a28d02da8128b2
Peter de Rivaz <peter.derivaz@argondesign.com>
2017-07-18 15:34:16 +0100

    ext-partition-types: Add 64x16 and 16x64 bsizes
    Change-Id: I0c3772110e9fa62ac687bd99e290b5006bf3bd6c

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -1046,6 +1046,8 @@ static INLINE int is_rect_tx_allowed_bsize(BLOCK_SIZE bsize) {
     0,  // BLOCK_16X4
     0,  // BLOCK_8X32
     0,  // BLOCK_32X8
+    0,  // BLOCK_16X64
+    0,  // BLOCK_64X16
   };
 
   return LUT[bsize];
@@ -1087,6 +1089,8 @@ static INLINE int is_quarter_tx_allowed_bsize(BLOCK_SIZE bsize) {
     0,  // BLOCK_16X4
     0,  // BLOCK_8X32
     0,  // BLOCK_32X8
+    0,  // BLOCK_16X64
+    0,  // BLOCK_64X16
   };
 
   return LUT_QTTX[bsize];
