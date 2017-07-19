0b93251c78ecddb4e9cae8740fcdd76957a48c55
David Michael Barr <b@rr-dav.id.au>
2017-07-19 19:42:28 +0900

    [CFL] Uniform grid in Q4 with unit range for alpha
    Expand the range of alpha to [-1, 1] in Q4.
    Jointly signal the signs, including zeros.
    Use the signs to give context for each quadrant
    and half-axis. The (0, 0) point is excluded.
    
    Results on Subset1 (Compared to 8493f916 with CFL enabled)
    
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    -0.1269 | -0.6841 | -0.5104 |  -0.1513 | -0.1804 | -0.1870 |    -0.5040
    
    Change-Id: I250369692e92a91d9c8d174a203d441217d15063
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- av1/common/entropymode.h
+++ av1/common/entropymode.h
@@ -366,7 +366,9 @@ typedef struct frame_contexts {
   od_adapt_ctx pvq_context;
 #endif  // CONFIG_PVQ
 #if CONFIG_CFL
-  aom_cdf_prob cfl_alpha_cdf[CDF_SIZE(CFL_ALPHABET_SIZE)];
+  aom_cdf_prob cfl_sign_cdf[CDF_SIZE(CFL_JOINT_SIGNS)];
+  aom_cdf_prob cfl_alpha_cdf[CFL_JOINT_SIGNS][CFL_PRED_PLANES]
+                            [CDF_SIZE(UV_ALPHABET_SIZE)];
 #endif
 } FRAME_CONTEXT;
 
