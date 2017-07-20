41ed2aeef9f5dec2d3e01a3b687ea72764532bd4
Steinar Midtskogen <stemidts@cisco.com>
2017-07-20 08:49:43 +0200

    Add experiment CONFIG_CDEF_SINGLEPASS: Make CDEF single pass
    Low latency, cpu-used=4:
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    -0.2248 | -0.7764 | -0.6630 |  -0.2109 | -0.3240 | -0.2532 |    -0.6980
    
    High latency, cpu-used=4:
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    -0.1118 | -0.5841 | -0.7406 |  -0.0463 | -0.2442 | -0.1064 |    -0.4187
    
    Change-Id: I9ca8399c8f45489541a66f535fb3d771eb1d59ab

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -3469,7 +3469,11 @@ static void encode_loopfilter(AV1_COMMON *cm, struct aom_write_bit_buffer *wb) {
 #if CONFIG_CDEF
 static void encode_cdef(const AV1_COMMON *cm, struct aom_write_bit_buffer *wb) {
   int i;
+#if CONFIG_CDEF_SINGLEPASS
+  aom_wb_write_literal(wb, cm->cdef_pri_damping - 3, 2);
+#else
   aom_wb_write_literal(wb, cm->cdef_pri_damping - 5, 1);
+#endif
   aom_wb_write_literal(wb, cm->cdef_sec_damping - 3, 2);
   aom_wb_write_literal(wb, cm->cdef_bits, 2);
   for (i = 0; i < cm->nb_cdef_strengths; i++) {
