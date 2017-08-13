5a2810559d82a5b81ee7af3bc1022a2c2d6b942b
Debargha Mukherjee <debargha@google.com>
2017-08-13 09:33:03 -0700

    Miscellaneous fixes for resize/frame-superres
    1. Fixes logic for disabling global motion w/ resize. Turns off
    conditional write/read of global parameters since it does not
    work currenlty. To be fixed later.
    2. Fixes call for av1_scale_if_required in encoder_frame_with_resize
    function
    3. Remove asserts for the scaled convolve functions
    
    Change-Id: I6a0f18b5ab5cc819bc27ef9fc2f28b0e391643a0

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -4594,9 +4594,12 @@ static void write_global_motion_params(WarpedMotionParams *params,
 static void write_global_motion(AV1_COMP *cpi, aom_writer *w) {
   AV1_COMMON *const cm = &cpi->common;
   int frame;
-  YV12_BUFFER_CONFIG *ref_buf;
   for (frame = LAST_FRAME; frame <= ALTREF_FRAME; ++frame) {
-    ref_buf = get_ref_frame_buffer(cpi, frame);
+    write_global_motion_params(&cm->global_motion[frame],
+                               &cm->prev_frame->global_motion[frame], w,
+                               cm->allow_high_precision_mv);
+    /*
+    YV12_BUFFER_CONFIG *ref_buf = get_ref_frame_buffer(cpi, frame);
     if (cpi->source->y_crop_width == ref_buf->y_crop_width &&
         cpi->source->y_crop_height == ref_buf->y_crop_height) {
       write_global_motion_params(&cm->global_motion[frame],
@@ -4606,6 +4609,7 @@ static void write_global_motion(AV1_COMP *cpi, aom_writer *w) {
       assert(cm->global_motion[frame].wmtype == IDENTITY &&
              "Invalid warp type for frames of different resolutions");
     }
+    */
     /*
     printf("Frame %d/%d: Enc Ref %d (used %d): %d %d %d %d\n",
            cm->current_video_frame, cm->show_frame, frame,
