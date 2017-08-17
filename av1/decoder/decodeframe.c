dc4e1721af805111a00ba9d673c869c16eee9d1a
Sarah Parker <sarahparker@google.com>
2017-08-17 14:58:03 -0700

    fix gm bitstream bug
    Change-Id: I8a419b167fe608d8e7075b8d348c4a1ccc89b14c

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -4873,7 +4873,11 @@ static void read_global_motion_params(WarpedMotionParams *params,
                                       WarpedMotionParams *ref_params,
                                       aom_reader *r, int allow_hp) {
   TransformationType type = aom_read_bit(r, ACCT_STR);
-  if (type != IDENTITY) type += aom_read_literal(r, GLOBAL_TYPE_BITS, ACCT_STR);
+  if (type != IDENTITY) {
+    if (aom_read_bit(r, ACCT_STR)) type = ROTZOOM;
+    else aom_read_bit(r, ACCT_STR) ? TRANSLATION : AFFINE;
+  }
+
   int trans_bits;
   int trans_dec_factor;
   int trans_prec_diff;
