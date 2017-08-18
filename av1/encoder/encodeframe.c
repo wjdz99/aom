f10b2698b174bbf92c4e7d19598514d942419724
Luc Trudeau <luc@trud.ca>
2017-08-18 15:14:30 -0400

    [CFL] Store Reconstructed Luma for Intra In Inter
    Like for intra block in intra frames, an extra call to
    txfm_rd_in_plane is added to the RDO of intra blocks in inter frames.
    This extra call is performed using the best parameters found during
    RDO and the reconstructed luma pixel are stored.
    
    Change-Id: I70ea2c01859b6c55d7c3eb9680d492c0bfc2aad4

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -5976,7 +5976,7 @@ static void encode_superblock(const AV1_COMP *const cpi, ThreadData *td,
     }
 #if CONFIG_CFL
     xd->cfl->store_y = 0;
-#if CONFIG_CB4X4 && CONFIG_DEBUG
+#if CONFIG_CHROMA_SUB8X8 && CONFIG_DEBUG
     if (is_chroma_reference(mi_row, mi_col, bsize, xd->cfl->subsampling_x,
                             xd->cfl->subsampling_y) &&
         !xd->cfl->are_parameters_computed) {
@@ -6178,6 +6178,15 @@ static void encode_superblock(const AV1_COMP *const cpi, ThreadData *td,
     set_txfm_ctxs(tx_size, xd->n8_w, xd->n8_h, (mbmi->skip || seg_skip), xd);
   }
 #endif  // CONFIG_VAR_TX
+#if CONFIG_CFL && CONFIG_CHROMA_SUB8X8
+  CFL_CTX *const cfl = xd->cfl;
+  if (is_inter_block(mbmi) &&
+      !is_chroma_reference(mi_row, mi_col, bsize, cfl->subsampling_x,
+                           cfl->subsampling_y)) {
+    // Store the reconstructed pixels of sub8x8 luma-only inter blocks
+    cfl_store_block(xd, mbmi->sb_type, mbmi->tx_size);
+  }
+#endif  // CONFIG_CFL && CONFIG_CHROMA_SUB8X8
 }
 
 #if CONFIG_SUPERTX
