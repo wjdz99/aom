9bde7f955b7ae93dd9257548ff4fd759e41396f8
Yi Luo <luoyi@google.com>
2017-08-29 09:40:01 -0700

    Add an assert to protect MODE_INFO array access
    This assert is to protect the last MODE_INFO array access in
    current tile boundary assignment from outside the allocated
    MODE_INFO memory space.
    
    Change-Id: I51d0a095c08c64a93b883d16805aea820a5761fa

--- av1/common/tile_common.c
+++ av1/common/tile_common.c
@@ -134,6 +134,11 @@ void av1_setup_across_tile_boundary_info(const AV1_COMMON *const cm,
     }
 
     mi = mi_start + (row_diff - 1) * cm->mi_stride;
+    // Note:
+    // This assert and above are used to guarantee MODE_INFO array accessing
+    // is strictly inside allocated space.
+    assert((intptr_t)&(mi + col_diff - 1)->mbmi.boundary_info <
+           (intptr_t)cm->mip + cm->mi_alloc_size);
     for (col = 0; col < col_diff; ++col) {
       mi->mbmi.boundary_info |= TILE_BOTTOM_BOUNDARY;
       mi += 1;
