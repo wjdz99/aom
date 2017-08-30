af585a280f9d8d4352c4019bba0d5281996b5a56
Zoe Liu <zoeliu@google.com>
2017-08-30 16:35:55 -0700

    Enable altref2 by default and merge it into ext-refs
    Change-Id: Ie67266538db41c6aa7d79a5c270c26c021583c48

--- av1/common/pred_common.h
+++ av1/common/pred_common.h
@@ -243,7 +243,6 @@ static INLINE aom_prob av1_get_pred_prob_comp_bwdref_p(const AV1_COMMON *cm,
   return cm->fc->comp_bwdref_prob[pred_context][0];
 }
 
-#if CONFIG_ALTREF2
 // TODO(zoeliu): ALTREF2 to work with NEW_MULTISYMBOL
 int av1_get_pred_context_comp_bwdref_p1(const AV1_COMMON *cm,
                                         const MACROBLOCKD *xd);
@@ -253,7 +252,6 @@ static INLINE aom_prob av1_get_pred_prob_comp_bwdref_p1(const AV1_COMMON *cm,
   const int pred_context = av1_get_pred_context_comp_bwdref_p1(cm, xd);
   return cm->fc->comp_bwdref_prob[pred_context][1];
 }
-#endif  // CONFIG_ALTREF2
 #endif  // CONFIG_EXT_REFS
 
 int av1_get_pred_context_single_ref_p1(const MACROBLOCKD *xd);
@@ -292,14 +290,12 @@ static INLINE aom_prob av1_get_pred_prob_single_ref_p5(const AV1_COMMON *cm,
   return cm->fc->single_ref_prob[av1_get_pred_context_single_ref_p5(xd)][4];
 }
 
-#if CONFIG_ALTREF2
 int av1_get_pred_context_single_ref_p6(const MACROBLOCKD *xd);
 
 static INLINE aom_prob av1_get_pred_prob_single_ref_p6(const AV1_COMMON *cm,
                                                        const MACROBLOCKD *xd) {
   return cm->fc->single_ref_prob[av1_get_pred_context_single_ref_p6(xd)][5];
 }
-#endif  // CONFIG_ALTREF2
 #endif  // CONFIG_EXT_REFS
 
 #if CONFIG_NEW_MULTISYMBOL
