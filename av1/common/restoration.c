8b6fd417f591cb9c015fc19df58fabe7401bcbda
Debargha Mukherjee <debargha@google.com>
2017-08-31 12:30:10 -0700

    Eliminate line buffer size for Wiener filter.
    This patch forces the vertical filtering for the top and bottom
    rows of a processing unit for the Wiener filter to not cross
    boundaries of processing units.
    This will effectively eliminate the line buffer requirement for
    the Wiener filter.
    
    Change-Id: Ie58a98c784a0db547627b9cfcf55f018c30e8e79

--- av1/common/restoration.c
+++ av1/common/restoration.c
@@ -146,6 +146,43 @@ static void loop_copy_tile(uint8_t *data, int tile_idx, int subtile_idx,
            h_end - h_start);
 }
 
+static void stepdown_wiener_kernel(InterpKernel orig, InterpKernel vert,
+                                   int boundary_dist, int istop) {
+  memcpy(vert, orig, sizeof(InterpKernel));
+  int delta;
+  switch (boundary_dist) {
+    case 0:
+      delta = 0;
+      vert[WIENER_HALFWIN] += vert[2] - delta;
+      vert[WIENER_WIN - 3] += delta;
+      vert[2] = 0;
+    case 1:
+      delta = 0;
+      vert[WIENER_HALFWIN] += vert[1] - delta;
+      vert[WIENER_WIN - 2] += delta;
+      vert[1] = 0;
+    case 2:
+      delta = 0;
+      vert[WIENER_HALFWIN] += vert[0] - delta;
+      vert[WIENER_WIN - 1] += delta;
+      vert[0] = 0;
+    default: break;
+  }
+  if (!istop) {
+    int tmp;
+    tmp = vert[0];
+    vert[0] = vert[WIENER_WIN - 1];
+    vert[WIENER_WIN - 1] = tmp;
+    tmp = vert[1];
+    vert[1] = vert[WIENER_WIN - 2];
+    vert[WIENER_WIN - 2] = tmp;
+    tmp = vert[2];
+    vert[2] = vert[WIENER_WIN - 3];
+    vert[WIENER_WIN - 3] = tmp;
+  }
+}
+
+#define WIENER_BORDER 0
 static void loop_wiener_filter_tile(uint8_t *data, int tile_idx, int width,
                                     int height, int stride,
                                     RestorationInternal *rst, uint8_t *dst,
@@ -161,6 +198,7 @@ static void loop_wiener_filter_tile(uint8_t *data, int tile_idx, int width,
                    dst_stride);
     return;
   }
+  InterpKernel vertical_topbot;
   av1_get_rest_tile_limits(tile_idx, 0, 0, rst->nhtiles, rst->nvtiles,
                            tile_width, tile_height, width, height, 0, 0,
                            &h_start, &h_end, &v_start, &v_end);
@@ -170,18 +208,53 @@ static void loop_wiener_filter_tile(uint8_t *data, int tile_idx, int width,
     for (j = h_start; j < h_end; j += procunit_width) {
       int w = AOMMIN(procunit_width, (h_end - j + 15) & ~15);
       int h = AOMMIN(procunit_height, (v_end - i + 15) & ~15);
-      const uint8_t *data_p = data + i * stride + j;
+      uint8_t *data_p = data + i * stride + j;
       uint8_t *dst_p = dst + i * dst_stride + j;
+      // Use 5-tap vertical filtering for top and bottom rows in
+      // processing unit
+      for (int b = 0; b < WIENER_HALFWIN - WIENER_BORDER; ++b) {
+        stepdown_wiener_kernel(rst->rsi->wiener_info[tile_idx].vfilter,
+                               vertical_topbot, WIENER_BORDER + b, 1);
+#if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        aom_convolve8_add_src_hip(data_p, stride, dst_p, dst_stride,
+                                  rst->rsi->wiener_info[tile_idx].hfilter, 16,
+                                  vertical_topbot, 16, w, 1);
+#else
+        aom_convolve8_add_src(data_p, stride, dst_p, dst_stride,
+                              rst->rsi->wiener_info[tile_idx].hfilter, 16,
+                              vertical_topbot, 16, w, 1);
+#endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        data_p += stride;
+        dst_p += dst_stride;
+      }
 #if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
       aom_convolve8_add_src_hip(data_p, stride, dst_p, dst_stride,
                                 rst->rsi->wiener_info[tile_idx].hfilter, 16,
                                 rst->rsi->wiener_info[tile_idx].vfilter, 16, w,
-                                h);
+                                h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
 #else
       aom_convolve8_add_src(data_p, stride, dst_p, dst_stride,
                             rst->rsi->wiener_info[tile_idx].hfilter, 16,
-                            rst->rsi->wiener_info[tile_idx].vfilter, 16, w, h);
+                            rst->rsi->wiener_info[tile_idx].vfilter, 16, w,
+                            h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
 #endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+      data_p += stride * (h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
+      dst_p += dst_stride * (h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
+      for (int b = 0; b < WIENER_HALFWIN - WIENER_BORDER; ++b) {
+        stepdown_wiener_kernel(rst->rsi->wiener_info[tile_idx].vfilter,
+                               vertical_topbot, WIENER_BORDER + b, 0);
+#if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        aom_convolve8_add_src_hip(data_p, stride, dst_p, dst_stride,
+                                  rst->rsi->wiener_info[tile_idx].hfilter, 16,
+                                  vertical_topbot, 16, w, 1);
+#else
+        aom_convolve8_add_src(data_p, stride, dst_p, dst_stride,
+                              rst->rsi->wiener_info[tile_idx].hfilter, 16,
+                              vertical_topbot, 16, w, 1);
+#endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        data_p += stride;
+        dst_p += dst_stride;
+      }
     }
 }
 
@@ -1011,25 +1084,64 @@ static void loop_wiener_filter_tile_highbd(uint16_t *data, int tile_idx,
   av1_get_rest_tile_limits(tile_idx, 0, 0, rst->nhtiles, rst->nvtiles,
                            tile_width, tile_height, width, height, 0, 0,
                            &h_start, &h_end, &v_start, &v_end);
+  InterpKernel vertical_topbot;
   // Convolve the whole tile (done in blocks here to match the requirements
   // of the vectorized convolve functions, but the result is equivalent)
   for (i = v_start; i < v_end; i += procunit_height)
     for (j = h_start; j < h_end; j += procunit_width) {
       int w = AOMMIN(procunit_width, (h_end - j + 15) & ~15);
       int h = AOMMIN(procunit_height, (v_end - i + 15) & ~15);
-      const uint16_t *data_p = data + i * stride + j;
+      uint16_t *data_p = data + i * stride + j;
       uint16_t *dst_p = dst + i * dst_stride + j;
+      for (int b = 0; b < WIENER_HALFWIN - WIENER_BORDER; ++b) {
+        stepdown_wiener_kernel(rst->rsi->wiener_info[tile_idx].vfilter,
+                               vertical_topbot, WIENER_BORDER + b, 1);
+#if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        aom_highbd_convolve8_add_src_hip(
+            CONVERT_TO_BYTEPTR(data_p), stride, CONVERT_TO_BYTEPTR(dst_p),
+            dst_stride, rst->rsi->wiener_info[tile_idx].hfilter, 16,
+            vertical_topbot, 16, w, 1, bit_depth);
+#else
+        aom_highbd_convolve8_add_src(CONVERT_TO_BYTEPTR(data_p), stride,
+                                     CONVERT_TO_BYTEPTR(dst_p), dst_stride,
+                                     rst->rsi->wiener_info[tile_idx].hfilter,
+                                     16, vertical_topbot, 16, w, 1, bit_depth);
+#endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        data_p += stride;
+        dst_p += dst_stride;
+      }
 #if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
       aom_highbd_convolve8_add_src_hip(
           CONVERT_TO_BYTEPTR(data_p), stride, CONVERT_TO_BYTEPTR(dst_p),
           dst_stride, rst->rsi->wiener_info[tile_idx].hfilter, 16,
-          rst->rsi->wiener_info[tile_idx].vfilter, 16, w, h, bit_depth);
+          rst->rsi->wiener_info[tile_idx].vfilter, 16, w,
+          h - (WIENER_HALFWIN - WIENER_BORDER) * 2, bit_depth);
 #else
       aom_highbd_convolve8_add_src(
           CONVERT_TO_BYTEPTR(data_p), stride, CONVERT_TO_BYTEPTR(dst_p),
           dst_stride, rst->rsi->wiener_info[tile_idx].hfilter, 16,
-          rst->rsi->wiener_info[tile_idx].vfilter, 16, w, h, bit_depth);
+          rst->rsi->wiener_info[tile_idx].vfilter, 16, w,
+          h - (WIENER_HALFWIN - WIENER_BORDER) * 2, bit_depth);
 #endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+      data_p += stride * (h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
+      dst_p += dst_stride * (h - (WIENER_HALFWIN - WIENER_BORDER) * 2);
+      for (int b = 0; b < WIENER_HALFWIN - WIENER_BORDER; ++b) {
+        stepdown_wiener_kernel(rst->rsi->wiener_info[tile_idx].vfilter,
+                               vertical_topbot, WIENER_BORDER + b, 0);
+#if USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        aom_highbd_convolve8_add_src_hip(
+            CONVERT_TO_BYTEPTR(data_p), stride, CONVERT_TO_BYTEPTR(dst_p),
+            dst_stride, rst->rsi->wiener_info[tile_idx].hfilter, 16,
+            vertical_topbot, 16, w, 1, bit_depth);
+#else
+        aom_highbd_convolve8_add_src(CONVERT_TO_BYTEPTR(data_p), stride,
+                                     CONVERT_TO_BYTEPTR(dst_p), dst_stride,
+                                     rst->rsi->wiener_info[tile_idx].hfilter,
+                                     16, vertical_topbot, 16, w, 1, bit_depth);
+#endif  // USE_WIENER_HIGH_INTERMEDIATE_PRECISION
+        data_p += stride;
+        dst_p += dst_stride;
+      }
     }
 }
 
