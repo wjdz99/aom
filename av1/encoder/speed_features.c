086fbe61c30ad337656b74ed434f7e0d3ea25892
Alexander Bokov <alexanderbokov@alexanderbokov.mtv.corp.google.com>
2017-08-31 18:07:05 -0700

    Predict skip flag to speed up the TX type search
    Speed-up depends on the particular clip and bitrate, on average
    it's around 3-5% with the default configuration.
    
    Average PSNR loss (default configuration):
    lowres: 0.010%
    midres: 0.005%
    
    Change-Id: Id34fb247e5e31f04ca324c58142e4b5ac4edacda

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -193,6 +193,7 @@ static void set_good_speed_features_framesize_independent(AV1_COMP *cpi,
     sf->tx_size_search_breakout = 1;
     sf->partition_search_breakout_rate_thr = 80;
     sf->tx_type_search.prune_mode = PRUNE_ONE;
+    sf->tx_type_search.use_skip_flag_prediction = 1;
     // Use transform domain distortion.
     // Note var-tx expt always uses pixel domain distortion.
     sf->use_transform_domain_distortion = 1;
@@ -404,6 +405,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi) {
   sf->alt_ref_search_fp = 0;
   sf->partition_search_type = SEARCH_PARTITION;
   sf->tx_type_search.prune_mode = NO_PRUNE;
+  sf->tx_type_search.use_skip_flag_prediction = 0;
   sf->tx_type_search.fast_intra_tx_type_search = 0;
   sf->tx_type_search.fast_inter_tx_type_search = 0;
   sf->less_rectangular_check = 0;
