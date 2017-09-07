9c49b1cb602e03a3f8c475bab9dbcfb84c1abc74
Soo-Chul Han <shan@vidyo.com>
2017-09-07 13:12:35 -0400

    initial implementation of HLS based on R18
    Change-Id: I78bb38e3d61d74442278bd5ed1c4f695b7c8e24a

--- av1/decoder/decoder.c
+++ av1/decoder/decoder.c
@@ -379,7 +379,16 @@ int av1_receive_compressed_data(AV1Decoder *pbi, size_t size,
   }
 
   cm->error.setjmp = 1;
-  av1_decode_frame(pbi, source, source + size, psource);
+
+#if !CONFIG_OBU
+  //  av1_decode_frame(pbi, source, source + size, psource);
+  if (av1_decode_frame_headers_and_setup(pbi, source, source + size, psource)) {
+    av1_decode_tg_tiles_and_wrapup(pbi, source, source + size, psource, 0,
+                                   cm->tile_rows * cm->tile_cols - 1, 1);
+  }
+#else
+  av1_decode_frame_from_obus(pbi, source, source + size, psource);
+#endif
 
   swap_frame_buffers(pbi);
 
