cbed38766c8a608c7cc1a9164d03f4dcf77c2873
Alexander Bokov <alexanderbokov@google.com>
2017-09-07 18:49:00 -0700

    [WIP] Improving the model for pruning the TX type search
    Introduces two new TX type pruning modes that provide better
    speed-quality trade-off compared to the existing ones.
    
    Evaluation results (preliminary, still running):
    ----------------------------------------------------------
    Prune mode | Avg. speed-up | Quality loss | Quality loss
               |(high bitrates)|   (lowres)   |   (midres)
    ----------------------------------------------------------
    PRUNE_ONE  |     18.7%     |    0.396%    |    0.308%
    ----------------------------------------------------------
    PRUNE_TWO  |     27.2%     |    0.439%    |
    ----------------------------------------------------------
    PRUNE_2D_  |     18.8%     |    0.032%    |
    ACCURATE   |               |              |
    ----------------------------------------------------------
    PRUNE_2D_  |     33.3%     |    0.504%    |
    FAST       |               |              |
    
    Change-Id: Ibd59f52eef493a499e529d824edad267daa65f9d

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -192,7 +192,13 @@ static void set_good_speed_features_framesize_independent(AV1_COMP *cpi,
 
     sf->tx_size_search_breakout = 1;
     sf->partition_search_breakout_rate_thr = 80;
+
+#if CONFIG_EXT_TX
+    sf->tx_type_search.prune_mode = PRUNE_2D_ACCURATE;
+#else
     sf->tx_type_search.prune_mode = PRUNE_ONE;
+#endif
+
     // Use transform domain distortion.
     // Note var-tx expt always uses pixel domain distortion.
     sf->use_transform_domain_distortion = 1;
@@ -215,7 +221,7 @@ static void set_good_speed_features_framesize_independent(AV1_COMP *cpi,
     sf->use_upsampled_references = 0;
     sf->adaptive_rd_thresh = 2;
 #if CONFIG_EXT_TX
-    sf->tx_type_search.prune_mode = PRUNE_TWO;
+    sf->tx_type_search.prune_mode = PRUNE_2D_FAST;
 #endif
 #if CONFIG_GLOBAL_MOTION
     sf->gm_search_type = GM_DISABLE_SEARCH;
