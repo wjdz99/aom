b69028d44d2ad9192119598933216fca83c96f74
Cheng Chen <chengchen@google.com>
2017-09-08 16:56:07 -0700

    Fix select filter level
    Only use cm->mi, instead of cm->mi_grid_visible to get filter level.
    The latter may not work for shifted grid loop filtering.
    
    Change-Id: I55ed6a42f0abb19d6814e8c0cf8d64ce79bf6abc

--- av1/common/av1_loopfilter.c
+++ av1/common/av1_loopfilter.c
@@ -598,9 +598,12 @@ static uint8_t get_filter_level(const AV1_COMMON *cm,
 #if CONFIG_LOOPFILTER_LEVEL
                                 const int dir_idx,
 #endif
+#if CONFIG_LPF_SB
+                                int mi_row, int mi_col,
+#endif
                                 const MB_MODE_INFO *mbmi) {
 #if CONFIG_LPF_SB
-  return mbmi->filt_lvl;
+  return cm->mi[mi_row * cm->mi_stride + mi_col].mbmi.filt_lvl;
 #endif
 
 #if CONFIG_SUPERTX
@@ -648,10 +651,6 @@ static uint8_t get_filter_level(const AV1_COMMON *cm,
 #else
 static uint8_t get_filter_level(const loop_filter_info_n *lfi_n,
                                 const MB_MODE_INFO *mbmi) {
-#if CONFIG_LPF_SB
-  return mbmi->filt_lvl;
-#endif
-
 #if CONFIG_SUPERTX
   const int segment_id = AOMMIN(mbmi->segment_id, mbmi->segment_id_supertx);
   assert(
@@ -1462,7 +1461,11 @@ static void build_masks(AV1_COMMON *const cm,
 #if CONFIG_LOOPFILTER_LEVEL
   const int filter_level = get_filter_level(cm, lfi_n, 0, mbmi);
 #else
+#if CONFIG_LPF_SB
+  const int filter_level = get_filter_level(cm, lfi_n, 0, 0, mbmi);
+#else
   const int filter_level = get_filter_level(cm, lfi_n, mbmi);
+#endif  // CONFIG_LPF_SB
 #endif
 #else
   const int filter_level = get_filter_level(lfi_n, mbmi);
@@ -1560,7 +1563,11 @@ static void build_y_mask(AV1_COMMON *const cm,
 #if CONFIG_LOOPFILTER_LEVEL
   const int filter_level = get_filter_level(cm, lfi_n, 0, mbmi);
 #else
+#if CONFIG_LPF_SB
+  const int filter_level = get_filter_level(cm, lfi_n, 0, 0, mbmi);
+#else
   const int filter_level = get_filter_level(cm, lfi_n, mbmi);
+#endif  // CONFIG_LPF_SB
 #endif
 #else
   const int filter_level = get_filter_level(lfi_n, mbmi);
@@ -2170,7 +2177,13 @@ static void get_filter_level_and_masks_non420(
     if (!(lfl_r[c_step] = get_filter_level(cm, &cm->lf_info, 0, mbmi)))
       continue;
 #else
+#if CONFIG_LPF_SB
+    if (!(lfl_r[c_step] =
+              get_filter_level(cm, &cm->lf_info, mi_row, mi_col, mbmi)))
+      continue;
+#else
     if (!(lfl_r[c_step] = get_filter_level(cm, &cm->lf_info, mbmi))) continue;
+#endif  // CONFIG_LPF_SB
 #endif
 #else
     if (!(lfl_r[c_step] = get_filter_level(&cm->lf_info, mbmi))) continue;
@@ -2868,7 +2881,12 @@ static void set_lpf_parameters(
     const uint32_t curr_level =
         get_filter_level(cm, &cm->lf_info, edge_dir, mbmi);
 #else
+#if CONFIG_LPF_SB
+    const uint32_t curr_level =
+        get_filter_level(cm, &cm->lf_info, mi_row, mi_col, mbmi);
+#else
     const uint32_t curr_level = get_filter_level(cm, &cm->lf_info, mbmi);
+#endif  // CONFIG_LPF_SB
 #endif
 #else
     const uint32_t curr_level = get_filter_level(&cm->lf_info, mbmi);
@@ -2905,8 +2923,13 @@ static void set_lpf_parameters(
           const uint32_t pv_lvl =
               get_filter_level(cm, &cm->lf_info, edge_dir, &mi_prev->mbmi);
 #else
+#if CONFIG_LPF_SB
+          const uint32_t pv_lvl = get_filter_level(cm, &cm->lf_info, pv_row,
+                                                   pv_col, &mi_prev->mbmi);
+#else
           const uint32_t pv_lvl =
               get_filter_level(cm, &cm->lf_info, &mi_prev->mbmi);
+#endif  // CONFIG_LPF_SB
 #endif
 #else
           const uint32_t pv_lvl =
