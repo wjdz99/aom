bloomberg/chromium.bb

commit a752d1d5051142ffaa51204a4b2e4a4a7540c187

add SEG_LVL_ZEROMV
Change-Id: Icd04302886a4d12890d04f9f15563169a91e3a0d

av1/common/seg_common.h
@@ -32,7 +32,12 @@ typedef enum {
   SEG_LVL_ALT_LF = 1,     // Use alternate loop filter value...
   SEG_LVL_REF_FRAME = 2,  // Optional Segment reference frame
   SEG_LVL_SKIP = 3,       // Optional Segment (0,0) + skip mode
-  SEG_LVL_MAX = 4         // Number of features supported
+#ifdef CONFIG_SEGMENT_ZEROMV
+  SEG_LVL_ZEROMV = 4,
+  SEG_LVL_MAX = 5
+#else
+  SEG_LVL_MAX = 4
+#endif
 } SEG_LVL_FEATURES;
 
 struct segmentation {

av1/decoder/decodemv.c
@@ -2312,7 +2312,12 @@ static void read_inter_block_mode_info(AV1Decoder *const pbi,
         av1_mode_context_analyzer(inter_mode_ctx, mbmi->ref_frame, bsize, -1);
   mbmi->ref_mv_idx = 0;
 
+#if CONFIG_SEGMENT_ZEROMV
+  if (segfeature_active(&cm->seg, mbmi->segment_id, SEG_LVL_SKIP) ||
+      segfeature_active(&cm->seg, mbmi->segment_id, SEG_LVL_ZEROMV)) {
+#else
   if (segfeature_active(&cm->seg, mbmi->segment_id, SEG_LVL_SKIP)) {
+#endif
     mbmi->mode = ZEROMV;
     if (bsize < BLOCK_8X8 && !unify_bsize) {
       aom_internal_error(xd->error_info, AOM_CODEC_UNSUP_BITSTREAM,

build/cmake/aom_config_defaults.cmake
@@ -190,6 +190,7 @@ set(CONFIG_SMOOTH_HV 1 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_SPEED_REFS 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_SUPERTX 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_TEMPMV_SIGNALING 1 CACHE NUMBER "AV1 experiment flag.")
+set(CONFIG_SEGMENT_ZEROMV 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_TPL_MV 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_TRIPRED 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_TX64X64 0 CACHE NUMBER "AV1 experiment flag.")

configure
@@ -321,6 +321,7 @@ EXPERIMENT_LIST="
     lv_map
     txk_sel
     mv_compress
+    segment_zeromv
     frame_superres
     new_multisymbol
     compound_singleref

wjdz99/aom

commit 4c9d1462a9af596944242bdf54abb45f30b36ae0

Fix typo introduced in a752d1d5051142ff
When CONFIG_SEGMENT_ZEROMV is set to 0, SEG_LVL_MAX shall not change.

Change-Id: Idf547b3e9e1dffb4054b6a7c4251fc0290f95ad0

av1/common/seg_common.h
@@ -32,7 +32,7 @@ typedef enum {
   SEG_LVL_ALT_LF = 1,     // Use alternate loop filter value...
   SEG_LVL_REF_FRAME = 2,  // Optional Segment reference frame
   SEG_LVL_SKIP = 3,       // Optional Segment (0,0) + skip mode
-#ifdef CONFIG_SEGMENT_ZEROMV
+#if CONFIG_SEGMENT_ZEROMV
   SEG_LVL_ZEROMV = 4,
   SEG_LVL_MAX = 5
 #else