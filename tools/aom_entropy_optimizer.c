e2c39c3f31c570d06e27c6913a5394707def66e1
Angie Chiang <angiebird@google.com>
2017-09-19 16:02:51 -0700

    Add coeff_lps prob training code
    Change-Id: Id88b4d9b97fec59ad8ce3f08fc554335cc1b2fe2

--- tools/aom_entropy_optimizer.c
+++ tools/aom_entropy_optimizer.c
@@ -852,16 +852,19 @@ int main(int argc, const char **argv) {
 
   cts_each_dim[0] = TX_SIZES;
   cts_each_dim[1] = PLANE_TYPES;
-  cts_each_dim[2] = LEVEL_CONTEXTS;
-  cts_each_dim[3] = 2;
-  optimize_entropy_table(
-      &fc.coeff_lps[0][0][0][0], probsfile, 4, cts_each_dim, NULL, 1,
-      "static const aom_prob "
-      "default_coeff_lps[TX_SIZES][PLANE_TYPES][LEVEL_CONTEXTS]");
-  optimize_cdf_table(&fc.coeff_lps[0][0][0][0], probsfile, 4, cts_each_dim,
-                     "static const aom_cdf_prob "
-                     "default_coeff_lps_cdf[TX_SIZES][PLANE_TYPES][LEVEL_"
-                     "CONTEXTS][CDF_SIZE(2)]");
+  cts_each_dim[2] = BASE_RANGE_SETS;
+  cts_each_dim[3] = LEVEL_CONTEXTS;
+  cts_each_dim[4] = 2;
+  optimize_entropy_table(&fc.coeff_lps[0][0][0][0][0], probsfile, 5,
+                         cts_each_dim, NULL, 1,
+                         "static const aom_prob "
+                         "default_coeff_lps[TX_SIZES][PLANE_TYPES][BASE_RANGE_"
+                         "SETS][LEVEL_CONTEXTS]");
+  optimize_cdf_table(
+      &fc.coeff_lps[0][0][0][0][0], probsfile, 5, cts_each_dim,
+      "static const aom_cdf_prob "
+      "default_coeff_lps_cdf[TX_SIZES][PLANE_TYPES][BASE_RANGE_SETS][LEVEL_"
+      "CONTEXTS][CDF_SIZE(2)]");
 
 #if BR_NODE
   cts_each_dim[0] = TX_SIZES;
