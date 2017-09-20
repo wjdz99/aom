23f52e1db6ef5151e48872fc8ac1bb1e62dec0bc
Monty Montgomery <cmontgomery@mozilla.com>
2017-09-20 09:06:56 -0400

    Add Daala TX to 4x16 and 16x4 transforms
    Rectangular 4x16 and 16x4 will now use Daala TX when CONFIG_DAALA_DCT4 and
    CONFIG_DAALA_DCT16 are both enabled.
    
    Change-Id: Ice4e416d7e414195184240cf8d54a2492f9bfdcb

--- test/av1_dct_test.cc
+++ test/av1_dct_test.cc
@@ -105,7 +105,10 @@ TEST_P(AV1FwdTxfm, RunFwdAccuracyCheck) { RunFwdAccuracyCheck(); }
 
 INSTANTIATE_TEST_CASE_P(
     C, AV1FwdTxfm,
-    ::testing::Values(FdctParam(&fdct4, &reference_dct_1d, 4, 1),
+    ::testing::Values(
+#if !CONFIG_DAALA_DCT4 || !CONFIG_DAALA_DCT8 || !CONFIG_DAALA_DCT16
+                      FdctParam(&fdct4, &reference_dct_1d, 4, 1),
+#endif
                       FdctParam(&fdct8, &reference_dct_1d, 8, 1),
                       FdctParam(&fdct16, &reference_dct_1d, 16, 2),
                       FdctParam(&fdct32, &reference_dct_1d, 32, 3)));
