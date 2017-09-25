143858122f5e9fc7dc20a55f0b564a8052798f7e
David Barker <david.barker@argondesign.com>
2017-09-25 11:23:03 +0100

    ext-partition: Don't read not-yet-decoded values
    When deciding whether the top-right or bottom-left blocks are
    available, we currently always act as if we're using 128x128
    superblocks. This means that, when using 64x64 superblocks,
    we sometimes conclude that blocks are available when they haven't
    been decoded yet!
    
    This typically happens at, for example, mi_row=15, mi_col=16
    (for bottom left), where we're at a 64x64 boundary but not
    a 128x128 boundary.
    
    This patch fixes the issue by checking based on the signalled
    superblock size.
    
    Note: Most of this patch is just threading 'cm' through the
    intra prediction process, so that we have access to cm->sb_size
    in has_top_right() and has_bottom_left()
    
    Change-Id: I126964c510aafffc870e7cd8b3e64a46abb14b3a

--- av1/common/reconintra.h
+++ av1/common/reconintra.h
@@ -20,12 +20,14 @@ extern "C" {
 #endif
 
 void av1_init_intra_predictors(void);
-void av1_predict_intra_block_facade(MACROBLOCKD *xd, int plane, int block_idx,
-                                    int blk_col, int blk_row, TX_SIZE tx_size);
-void av1_predict_intra_block(const MACROBLOCKD *xd, int bw, int bh,
-                             BLOCK_SIZE bsize, PREDICTION_MODE mode,
-                             const uint8_t *ref, int ref_stride, uint8_t *dst,
-                             int dst_stride, int aoff, int loff, int plane);
+void av1_predict_intra_block_facade(const AV1_COMMON *cm, MACROBLOCKD *xd,
+                                    int plane, int block_idx, int blk_col,
+                                    int blk_row, TX_SIZE tx_size);
+void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
+                             int bw, int bh, BLOCK_SIZE bsize,
+                             PREDICTION_MODE mode, const uint8_t *ref,
+                             int ref_stride, uint8_t *dst, int dst_stride,
+                             int aoff, int loff, int plane);
 
 #if CONFIG_EXT_INTER && CONFIG_INTERINTRA
 // Mapping of interintra to intra mode for use in the intra component
