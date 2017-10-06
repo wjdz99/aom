778ed87f38205a5138842453003532422d8db3b5
Sarah Parker <sarahparker@google.com>
2017-10-06 16:50:47 -0700

    dct default vartx
    Change-Id: Icded0742d70c3766efc8cb05eedba5ce5c5d2854

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -5309,10 +5309,16 @@ static void select_tx_type_yrd(const AV1_COMP *cpi, MACROBLOCK *x,
   RD_STATS *rd_stats_stack = aom_malloc(4 * sizeof(*rd_stats_stack));
 #endif  // CONFIG_EXT_PARTITION
 #if CONFIG_EXT_TX
+    const struct macroblockd_plane *const pd = &xd->plane[0];
+    const BLOCK_SIZE plane_bsize = get_plane_block_size(bsize, pd);
+    const int mi_width = block_size_wide[plane_bsize] >> tx_size_wide_log2[0];
+    const int mi_height = block_size_high[plane_bsize] >> tx_size_high_log2[0];
+  const TX_SIZE min_tx_size = (mi_width != mi_height) ? max_tx_size : // is this correct for rect?
+                              sub_tx_size_map[sub_tx_size_map[max_tx_size]];
   const TxSetType tx_set_type = get_ext_tx_set_type(
-      max_tx_size, bsize, is_inter, cm->reduced_tx_set_used);
+      min_tx_size, bsize, is_inter, cm->reduced_tx_set_used);
   const int ext_tx_set =
-      get_ext_tx_set(max_tx_size, bsize, is_inter, cm->reduced_tx_set_used);
+      get_ext_tx_set(min_tx_size, bsize, is_inter, cm->reduced_tx_set_used);
 #endif  // CONFIG_EXT_TX
 
   av1_invalid_rd_stats(rd_stats);
