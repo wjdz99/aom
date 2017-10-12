523254aaebeb2e577f234d1ed2493bcd2fc38ff6
Debargha Mukherjee <debargha@google.com>
2017-10-12 12:40:25 -0700

    Use cdfs for loop-restoration types
    BUG=aomedia:907
    
    Change-Id: I30a0290e499d338250343b5f9092bdcabe7d867a

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -2501,6 +2501,21 @@ static const aom_prob
     default_switchable_restore_prob[RESTORE_SWITCHABLE_TYPES - 1] = {
       32, 128,
     };
+
+#if CONFIG_NEW_MULTISYMBOL
+static const aom_cdf_prob
+    default_switchable_restore_cdf[CDF_SIZE(RESTORE_SWITCHABLE_TYPES)] = {
+      AOM_ICDF(32 * 128), AOM_ICDF(144 * 128), AOM_ICDF(32768), 0,
+    };
+
+static const aom_cdf_prob default_wiener_restore_cdf[CDF_SIZE(2)] = {
+  AOM_ICDF(64 * 128), AOM_ICDF(32768), 0,
+};
+
+static const aom_cdf_prob default_sgrproj_restore_cdf[CDF_SIZE(2)] = {
+  AOM_ICDF(64 * 128), AOM_ICDF(32768), 0,
+};
+#endif  // CONFIG_NEW_MULTISYMBOL
 #endif  // CONFIG_LOOP_RESTORATION
 
 #define NUM_PALETTE_NEIGHBORS 3  // left, top-left and top.
@@ -6645,6 +6660,11 @@ static void init_mode_probs(FRAME_CONTEXT *fc) {
 #endif  // CONFIG_LGT_FROM_PRED
 #if CONFIG_LOOP_RESTORATION
   av1_copy(fc->switchable_restore_prob, default_switchable_restore_prob);
+#if CONFIG_NEW_MULTISYMBOL
+  av1_copy(fc->switchable_restore_cdf, default_switchable_restore_cdf);
+  av1_copy(fc->wiener_restore_cdf, default_wiener_restore_cdf);
+  av1_copy(fc->sgrproj_restore_cdf, default_sgrproj_restore_cdf);
+#endif  // CONFIG_NEW_MULTISYMBOL
 #endif  // CONFIG_LOOP_RESTORATION
   av1_copy(fc->y_mode_cdf, default_if_y_mode_cdf);
   av1_copy(fc->uv_mode_cdf, default_uv_mode_cdf);
