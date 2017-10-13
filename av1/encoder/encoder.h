57e0f954a664015712efd7aba6bc38f6758646b3
Sebastien Alaiwan <sebastien.alaiwan@allegrodvt.com>
2017-10-13 15:02:57 +0200

    Fix mem corruption due to undersized token buffer
    Double the size to take a safe margin.
    
    BUG=aomedia:647
    
    Change-Id: I04638a73deee334aa1f083f67c602c8a18cb951c

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -709,10 +709,12 @@ static INLINE int enc_is_ref_frame_buf(AV1_COMP *cpi, RefCntBuffer *frame_buf) {
 }
 
 static INLINE unsigned int get_token_alloc(int mb_rows, int mb_cols) {
-  // We assume 3 planes all at full resolution. We assume up to 1 token per
+  // We assume 3 planes all at full resolution. We assume up to 2 tokens per
   // pixel, and then allow a head room of 1 EOSB token per 4x4 block per plane,
   // plus EOSB_TOKEN per plane.
-  return mb_rows * mb_cols * (16 * 16 + 17) * 3;
+  // TODO(sebastien): Figure out the exact max size needed.
+  // See: https://crbug.com/aomedia/647"
+  return mb_rows * mb_cols * (16 * 16 + 17) * 3 * 2;
 }
 
 // Get the allocated token size for a tile. It does the same calculation as in
