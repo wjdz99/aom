13ddff4678f4c7e0de471ce7dc8b8f5aec112c5e
Debargha Mukherjee <debargha@google.com>
2017-10-26 09:43:40 -0700

    Remove unused homography projection functions
    Change-Id: Iad8f4934155d4f1fcc99849387668e0d72f849bf

--- av1/common/warped_motion.h
+++ av1/common/warped_motion.h
@@ -67,19 +67,6 @@ void project_points_affine(const int32_t *mat, int *points, int *proj,
                            const int stride_proj, const int subsampling_x,
                            const int subsampling_y);
 
-void project_points_hortrapezoid(const int32_t *mat, int *points, int *proj,
-                                 const int n, const int stride_points,
-                                 const int stride_proj, const int subsampling_x,
-                                 const int subsampling_y);
-void project_points_vertrapezoid(const int32_t *mat, int *points, int *proj,
-                                 const int n, const int stride_points,
-                                 const int stride_proj, const int subsampling_x,
-                                 const int subsampling_y);
-void project_points_homography(const int32_t *mat, int *points, int *proj,
-                               const int n, const int stride_points,
-                               const int stride_proj, const int subsampling_x,
-                               const int subsampling_y);
-
 // Returns the error between the result of applying motion 'wm' to the frame
 // described by 'ref' and the frame described by 'dst'.
 int64_t av1_warp_error(WarpedMotionParams *wm,
