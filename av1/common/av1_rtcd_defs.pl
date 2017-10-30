3ace69b66c88b1ec225577982047a76ac51aa0fc
Cheng Chen <chengchen@google.com>
2017-10-30 15:59:26 -0700

    JNT_COMP: add SIMD implementations for c functions
    Both low and high bit-depth has SIMD implementations
    
    Change-Id: Icca0b07b25489759be9504aaec09d1239076fc52

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -577,6 +577,11 @@ if (aom_config("CONFIG_CONVOLVE_ROUND") eq "yes") {
     specialize qw/av1_convolve_2d_scale sse4_1/;
   }
 
+  if (aom_config("CONFIG_JNT_COMP") eq "yes") {
+    add_proto qw/void av1_jnt_convolve_2d/, "const uint8_t *src, int src_stride, CONV_BUF_TYPE *dst, int dst_stride, int w, int h, InterpFilterParams *filter_params_x, InterpFilterParams *filter_params_y, const int subpel_x_q4, const int subpel_y_q4, ConvolveParams *conv_params";
+    specialize qw/av1_jnt_convolve_2d sse4_1/;
+  }
+
   if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
     add_proto qw/void av1_highbd_convolve_2d/, "const uint16_t *src, int src_stride, CONV_BUF_TYPE *dst, int dst_stride, int w, int h, InterpFilterParams *filter_params_x, InterpFilterParams *filter_params_y, const int subpel_x_q4, const int subpel_y_q4, ConvolveParams *conv_params, int bd";
     specialize qw/av1_highbd_convolve_2d ssse3/;
@@ -586,6 +591,11 @@ if (aom_config("CONFIG_CONVOLVE_ROUND") eq "yes") {
     add_proto qw/void av1_highbd_convolve_2d_scale/, "const uint16_t *src, int src_stride, CONV_BUF_TYPE *dst, int dst_stride, int w, int h, InterpFilterParams *filter_params_x, InterpFilterParams *filter_params_y, const int subpel_x_q4, const int x_step_qn, const int subpel_y_q4, const int y_step_qn, ConvolveParams *conv_params, int bd";
     if (aom_config("CONFIG_COMPOUND_ROUND") ne "yes") {
         specialize qw/av1_highbd_convolve_2d_scale sse4_1/;
+    }
+
+    if (aom_config("CONFIG_JNT_COMP") eq "yes") {
+      add_proto qw/void av1_highbd_jnt_convolve_2d/, "const uint16_t *src, int src_stride, CONV_BUF_TYPE *dst, int dst_stride, int w, int h, InterpFilterParams *filter_params_x, InterpFilterParams *filter_params_y, const int subpel_x_q4, const int subpel_y_q4, ConvolveParams *conv_params, int bd";
+      specialize qw/av1_highbd_jnt_convolve_2d sse4_1/;
     }
   }
 }
