09223f0ec89f52d359d8fb330bf0bf62105133c3
Monty Montgomery <cmontgomery@mozilla.com>
2017-11-01 19:48:38 -0400

    Add Daala TX fixed-coeff-depth capability to quantization
    This patch completes the work to add fixed-depth TX domain support to
    the quantization and dequantization code.  At present, it is active but
    configured to behave identically to current AV1 master as RDO and TX
    have not yet been updated to also support this functionality.
    
    Change-Id: I3773a1fc128136c9fea227f4b547576a8aa6efa3

--- av1/encoder/ratectrl_xiph.c
+++ av1/encoder/ratectrl_xiph.c
@@ -369,23 +369,23 @@ static void od_enc_rc_reset(od_rc_state *rc) {
     The B-frame values especially are simply copies of the P-frame values.*/
   if (ibpp < 1) {
     rc->exp[OD_I_FRAME] = 59;
-    rc->log_scale[OD_I_FRAME] = od_blog64(1997) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_I_FRAME] = od_blog64(1997) - OD_Q57(TX_COEFF_DEPTH - 8);
   } else if (ibpp < 2) {
     rc->exp[OD_I_FRAME] = 55;
-    rc->log_scale[OD_I_FRAME] = od_blog64(1604) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_I_FRAME] = od_blog64(1604) - OD_Q57(TX_COEFF_DEPTH - 8);
   } else {
     rc->exp[OD_I_FRAME] = 48;
-    rc->log_scale[OD_I_FRAME] = od_blog64(834) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_I_FRAME] = od_blog64(834) - OD_Q57(TX_COEFF_DEPTH - 8);
   }
   if (ibpp < 4) {
     rc->exp[OD_P_FRAME] = 100;
-    rc->log_scale[OD_P_FRAME] = od_blog64(2249) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_P_FRAME] = od_blog64(2249) - OD_Q57(TX_COEFF_DEPTH - 8);
   } else if (ibpp < 8) {
     rc->exp[OD_P_FRAME] = 95;
-    rc->log_scale[OD_P_FRAME] = od_blog64(1751) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_P_FRAME] = od_blog64(1751) - OD_Q57(TX_COEFF_DEPTH - 8);
   } else {
     rc->exp[OD_P_FRAME] = 73;
-    rc->log_scale[OD_P_FRAME] = od_blog64(1260) - OD_Q57(OD_COEFF_SHIFT);
+    rc->log_scale[OD_P_FRAME] = od_blog64(1260) - OD_Q57(TX_COEFF_DEPTH - 8);
   }
   /*Golden P-frames both use the same log_scale and exp modeling
      values as regular P-frames and the same scale follower.
@@ -810,7 +810,8 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
         calculation, that needs to be modulated as well.
         Calculate what is, effectively, a fractional coded quantizer. */
       /*Get the log2 quantizer in Q57 (normalized for coefficient shift).*/
-      log_quantizer = od_blog64(rc->base_quantizer) - OD_Q57(OD_COEFF_SHIFT);
+      log_quantizer =
+          od_blog64(rc->base_quantizer) - OD_Q57(TX_COEFF_DEPTH - 8);
       /*log_quantizer to Q21.*/
       log_quantizer >>= 36;
       /*scale log quantizer, result is Q33.*/
@@ -824,7 +825,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
       /*Back to log2 quantizer in Q57.*/
       log_quantizer = (log_quantizer - OD_LOG_QUANTIZER_OFFSET_Q45) *
                           OD_LOG_QUANTIZER_EXP_Q12 +
-                      OD_Q57(OD_COEFF_SHIFT);
+                      OD_Q57(TX_COEFF_DEPTH - 8);
       /*Convert Q57 log2 quantizer to unclamped linear target quantizer value.*/
       rc->target_quantizer = od_bexp64(log_quantizer);
     }
@@ -881,7 +882,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
       for (i = 0; i < OD_FRAME_NSUBTYPES; i++) {
         /*Modulate base quantizer by frame type.*/
         /*Get the log2 quantizer in Q57 (normalized for coefficient shift).*/
-        log_quantizer = log_base_quantizer - OD_Q57(OD_COEFF_SHIFT);
+        log_quantizer = log_base_quantizer - OD_Q57(TX_COEFF_DEPTH - 8);
         /*log_quantizer to Q21.*/
         log_quantizer >>= 36;
         /*scale log quantizer, result is Q33.*/
@@ -895,7 +896,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
         /*Back to log2 quantizer in Q57.*/
         log_quantizer = (log_quantizer - OD_LOG_QUANTIZER_OFFSET_Q45) *
                             OD_LOG_QUANTIZER_EXP_Q12 +
-                        OD_Q57(OD_COEFF_SHIFT);
+                        OD_Q57(TX_COEFF_DEPTH - 8);
         /*Clamp modulated quantizer values.*/
         log_quantizer = OD_CLAMPI(od_blog64(lossy_quantizer_min), log_quantizer,
                                   od_blog64(lossy_quantizer_max));
@@ -924,7 +925,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
     /*Modulate chosen base quantizer to produce target quantizer.*/
     log_quantizer = od_blog64(base_quantizer);
     /*Get the log2 quantizer in Q57 (normalized for coefficient shift).*/
-    log_quantizer -= OD_Q57(OD_COEFF_SHIFT);
+    log_quantizer -= OD_Q57(TX_COEFF_DEPTH - 8);
     /*log_quantizer to Q21.*/
     log_quantizer >>= 36;
     /*scale log quantizer, result is Q33.*/
@@ -938,7 +939,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
     /*Back to log2 quantizer in Q57.*/
     log_quantizer = (log_quantizer - OD_LOG_QUANTIZER_OFFSET_Q45) *
                         OD_LOG_QUANTIZER_EXP_Q12 +
-                    OD_Q57(OD_COEFF_SHIFT);
+                    OD_Q57(TX_COEFF_DEPTH - 8);
     /*Clamp modulated quantizer values.*/
     log_quantizer = OD_CLAMPI(od_blog64(lossy_quantizer_min), log_quantizer,
                               od_blog64(lossy_quantizer_max));
@@ -1023,7 +1024,7 @@ int od_enc_rc_select_quantizers_and_lambdas(od_rc_state *rc,
   }
   *bottom_idx = lossy_quantizer_min;
   *top_idx = lossy_quantizer_max;
-  rc->target_quantizer = av1_qindex_from_ac(
+  rc->target_quantizer = av1_qindex_from_ac_Q3(
       OD_CLAMPI(lossy_quantizer_min, rc->target_quantizer, lossy_quantizer_max),
       rc->bit_depth);
   return rc->target_quantizer;
