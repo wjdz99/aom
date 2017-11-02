d84cbc463fbb7fc354b2d929f003a481a67adad8
Debargha Mukherjee <debargha@google.com>
2017-11-01 22:57:28 -0700

    Speed up of ext-partition types
    Search the new horz/vert a/b/4 partitions only if the best so far
    is either oriented along the same direction or split/none, or if
    the rd costs obtained from the previous partition searches indicate
    there is potential in searching these partitions.
    
    The pruned search is implemented in two levels of a speed feature.
    
    This brings about 25-30% speedup.
    
    Change-Id: I6c6c347e06c34ee0ca17479aeeb4075a66dc7e2c

--- av1/encoder/speed_features.h
+++ av1/encoder/speed_features.h
@@ -333,6 +333,12 @@ typedef struct SPEED_FEATURES {
   // Drop less likely picked reference frames in the RD search
   int selective_ref_frame;
 
+#if CONFIG_EXT_PARTITION_TYPES
+  // Conditionally prune extended partition types search in two levels:
+  // 1 and 2, where 2 is faster than 1.
+  int prune_ext_partition_types_search;
+#endif  // CONFIG_EXT_PARTITION_TYPES
+
   // Skip rectangular partition test when partition type none gives better
   // rd than partition type split.
   int less_rectangular_check;
