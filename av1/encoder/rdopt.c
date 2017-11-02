e833dfcc62a1d6e583f42613eaf5f5691b0f22c0
Cheng Chen <chengchen@google.com>
2017-11-02 15:00:20 -0700

    JNT_COMP: turn off for one_sided_compound
    One_sided_compound is less likely to provide gains for jnt_comp.
    Stop jnt_comp search when one_sided_compound is true. It save coding
    time as well as bit overhead.
    
    Change-Id: I6714e5d211eba14a5793bdae7215ccfcc1d8f67a

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -6039,7 +6039,7 @@ static void joint_motion_search(const AV1_COMP *cpi, MACROBLOCK *x,
 #if CONFIG_JNT_COMP
     const int order_idx = id != 0;
     av1_jnt_comp_weight_assign(cm, mbmi, order_idx, &xd->jcp_param.fwd_offset,
-                               &xd->jcp_param.bck_offset, 1);
+                               &xd->jcp_param.bck_offset);
 #endif  // CONFIG_JNT_COMP
 
     // Do compound motion search on the current reference frame.
@@ -6791,7 +6791,7 @@ static void build_second_inter_pred(const AV1_COMP *cpi, MACROBLOCK *x,
 
 #if CONFIG_JNT_COMP
   av1_jnt_comp_weight_assign(cm, mbmi, 0, &xd->jcp_param.fwd_offset,
-                             &xd->jcp_param.bck_offset, 1);
+                             &xd->jcp_param.bck_offset);
 #endif  // CONFIG_JNT_COMP
 
   if (scaled_ref_frame) {
@@ -8435,7 +8435,7 @@ static int64_t handle_inter_mode(const AV1_COMP *const cpi, MACROBLOCK *x,
 #endif  // CONFIG_COMPOUND_SINGLEREF
 
 #if CONFIG_JNT_COMP
-  if (is_comp_pred) {
+  if (has_two_sided_comp_refs(mbmi)) {
     const int comp_index_ctx = get_comp_index_context(cm, xd);
     rd_stats->rate += av1_cost_bit(cm->fc->compound_index_probs[comp_index_ctx],
                                    mbmi->compound_idx);
@@ -10219,6 +10219,7 @@ void av1_rd_pick_inter_mode_sb(const AV1_COMP *cpi, TileDataEnc *tile_data,
 #if CONFIG_JNT_COMP
       {
         int cum_rate = rate2;
+        mbmi->compound_idx = 1;
         MB_MODE_INFO backup_mbmi = *mbmi;
 
         int_mv backup_frame_mv[MB_MODE_COUNT][TOTAL_REFS_PER_FRAME];
@@ -10236,6 +10237,7 @@ void av1_rd_pick_inter_mode_sb(const AV1_COMP *cpi, TileDataEnc *tile_data,
 
         for (int comp_idx = 0; comp_idx < 1 + has_second_ref(mbmi);
              ++comp_idx) {
+          if (comp_idx == 0 && has_uni_comp_refs(mbmi)) continue;
           RD_STATS rd_stats, rd_stats_y, rd_stats_uv;
           av1_init_rd_stats(&rd_stats);
           av1_init_rd_stats(&rd_stats_y);
@@ -10432,6 +10434,7 @@ void av1_rd_pick_inter_mode_sb(const AV1_COMP *cpi, TileDataEnc *tile_data,
           ref_idx = sidx;
           if (has_second_ref(mbmi)) ref_idx /= 2;
           mbmi->compound_idx = sidx % 2;
+          if (mbmi->compound_idx == 0 && has_uni_comp_refs(mbmi)) continue;
 #endif  // CONFIG_JNT_COMP
 
           av1_invalid_rd_stats(&tmp_rd_stats);
