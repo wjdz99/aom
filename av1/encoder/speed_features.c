7a46c0dd6c6a254a60d1bab87c9d087494826dc7
Jingning Han <jingning@google.com>
2017-11-03 11:54:36 -0700

    Enc speed
    Change-Id: I2d2f176db0804a79ed166131d7dbc72f664cd61a

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -148,6 +148,11 @@ static void set_good_speed_features_framesize_independent(AV1_COMP *cpi,
   if (speed >= 1) {
     sf->tx_size_search_init_depth_rect = 1;
     sf->tx_size_search_init_depth_sqr = 1;
+
+    sf->tx_type_search.fast_intra_tx_type_search = 1;
+    sf->tx_type_search.fast_inter_tx_type_search = 1;
+
+    sf->selective_ref_frame = 1;
   }
 
   if (speed >= 2) {
