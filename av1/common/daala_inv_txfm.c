69b12966919ea30d26249c9af56412c85a733602
Monty Montgomery <cmontgomery@mozilla.com>
2017-11-03 10:32:37 -0400

    Enable configurable fixed-depth coefficients in Daala TX
    This patch turns on the fixed-depth TX code in the Daala toplevel TX
    
    Change-Id: Ibbe17226dd47980da632814422d6201c9fc6fa36

--- av1/common/daala_inv_txfm.c
+++ av1/common/daala_inv_txfm.c
@@ -66,14 +66,8 @@ void daala_inv_txfm_add(const tran_low_t *input_coeffs, void *output_pixels,
       av1_highbd_iwht4x4_add(input_coeffs, output_pixels, output_stride,
                              txfm_param->eob, px_depth);
   } else {
-// General TX case
-#if 1
-    // Q3 coeff Q4 TX compatability mode
-    const int downshift = 4;
-#else
-    // How we intend to run these transforms in the near future
+    // General TX case
     const int downshift = TX_COEFF_DEPTH - px_depth;
-#endif
 
     assert(downshift >= 0);
     assert(sizeof(tran_low_t) == sizeof(od_coeff));
@@ -99,18 +93,6 @@ void daala_inv_txfm_add(const tran_low_t *input_coeffs, void *output_pixels,
 
     assert(col_tx);
     assert(row_tx);
-
-#if 1
-    // This is temporary while we're testing against existing
-    // behavior (preshift up one plus av1_get_tx_scale).
-    // Remove before flight
-    od_coeff tmp[MAX_TX_SQUARE];
-    int upshift = 1;
-    for (r = 0; r < rows; ++r)
-      for (c = 0; c < cols; ++c)
-        tmp[r * cols + c] = input_coeffs[r * cols + c] << upshift;
-    input_coeffs = tmp;
-#endif
 
     // Inverse-transform rows
     for (r = 0; r < rows; ++r) {
