7d22e36ad615718c7d68fad94a90e016ef1b8c43
Thomas Daede <tdaede@mozilla.com>
2017-11-06 20:07:01 -0800

    Allow CDF precision to be configured with CDF_PROB_BITS.
    Change-Id: I4cd40a60d472eb58f37ac4fda1056b5dfe8b39a5

--- av1/encoder/cost.c
+++ av1/encoder/cost.c
@@ -71,7 +71,8 @@ void av1_cost_tokens_from_cdf(int *costs, const aom_cdf_prob *cdf,
   int i;
   aom_cdf_prob prev_cdf = 0;
   for (i = 0;; ++i) {
-    aom_cdf_prob p15 = AOM_ICDF(cdf[i]) - prev_cdf;
+    printf("%d,", cdf[i]);
+    const aom_cdf_prob p15 = AOM_ICDF(cdf[i]) - prev_cdf;
     prev_cdf = AOM_ICDF(cdf[i]);
 
     if (inv_map)
@@ -82,4 +83,5 @@ void av1_cost_tokens_from_cdf(int *costs, const aom_cdf_prob *cdf,
     // Stop once we reach the end of the CDF
     if (cdf[i] == AOM_ICDF(CDF_PROB_TOP)) break;
   }
+  printf("\n");
 }
