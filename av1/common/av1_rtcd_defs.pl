398fa7c11bc8feeabbfa6d593994e23a2b3fef75
Linfeng Zhang <linfengz@google.com>
2017-11-08 15:46:57 -0800

    Add av1_get_br_level_counts_sse2()
    Change-Id: I6ce7aea19e3bdeef24d3fe66ac6eba7b8d585f9a

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -67,6 +67,12 @@ if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
 }
 
 #
+# txb
+#
+add_proto qw/void av1_get_br_level_counts/, "const uint8_t *const levels, const int width, const int height, uint8_t *const level_counts";
+specialize qw/av1_get_br_level_counts sse2/;
+
+#
 # Inverse dct
 #
 add_proto qw/void av1_iht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
