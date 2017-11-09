a93ccb1910aea1bcbd10ded07f6fc53c07942326
Yaowu Xu <yaowu@google.com>
2017-11-09 13:39:11 -0800

    Support Separate qmatrix for U and V planes
    This commit adds support of a separate qmatrix and iqmatrix for each
    of the U and V planes. Currently the separate matrices are intialized
    to the same ones, then the commit does not have any coding impact
    yet.
    
    Change-Id: I5c4045fe1879db262c6ec1567d8d7dfd9a98fbed

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -1423,7 +1423,8 @@ static void setup_segmentation_dequant(AV1_COMMON *const cm) {
                             : aom_get_qmlevel(cm->base_qindex, minqm, maxqm);
     for (int j = 0; j < TX_SIZES_ALL; ++j) {
       cm->y_iqmatrix[i][j] = aom_iqmatrix(cm, qmlevel, 0, j);
-      cm->uv_iqmatrix[i][j] = aom_iqmatrix(cm, qmlevel, 1, j);
+      cm->u_iqmatrix[i][j] = aom_iqmatrix(cm, qmlevel, 1, j);
+      cm->v_iqmatrix[i][j] = aom_iqmatrix(cm, qmlevel, 2, j);
     }
 #endif  // CONFIG_AOM_QM
 #if CONFIG_NEW_QUANT
