5538e341dcd2f7edfc3ad4e042c7021e85341d92
Yaowu Xu <yaowu@google.com>
2017-11-09 13:39:11 -0800

    Support Separate qmatrix for U and V planes
    Change-Id: I5c4045fe1879db262c6ec1567d8d7dfd9a98fbed

--- av1/common/quant_common.c
+++ av1/common/quant_common.c
@@ -341,24 +341,24 @@ int av1_get_qindex(const struct segmentation *seg, int segment_id,
 }
 
 #if CONFIG_AOM_QM
-qm_val_t *aom_iqmatrix(AV1_COMMON *cm, int qmlevel, int is_chroma,
+qm_val_t *aom_iqmatrix(AV1_COMMON *cm, int qmlevel, int plane,
                        TX_SIZE tx_size) {
-  return &cm->giqmatrix[qmlevel][!!is_chroma][tx_size][0];
+  return &cm->giqmatrix[qmlevel][plane][tx_size][0];
 }
-qm_val_t *aom_qmatrix(AV1_COMMON *cm, int qmlevel, int is_chroma,
+qm_val_t *aom_qmatrix(AV1_COMMON *cm, int qmlevel, int plane,
                       TX_SIZE tx_size) {
-  return &cm->gqmatrix[qmlevel][!!is_chroma][tx_size][0];
+  return &cm->gqmatrix[qmlevel][plane][tx_size][0];
 }
 
 #define QM_TOTAL_SIZE 3344
-static uint16_t wt_matrix_ref[NUM_QM_LEVELS][2][QM_TOTAL_SIZE];
-static uint16_t iwt_matrix_ref[NUM_QM_LEVELS][2][QM_TOTAL_SIZE];
+static uint16_t wt_matrix_ref[NUM_QM_LEVELS][3][QM_TOTAL_SIZE];
+static uint16_t iwt_matrix_ref[NUM_QM_LEVELS][3][QM_TOTAL_SIZE];
 
 void aom_qm_init(AV1_COMMON *cm) {
   int q, c, t;
   int current;
   for (q = 0; q < NUM_QM_LEVELS; ++q) {
-    for (c = 0; c < 2; ++c) {
+    for (c = 0; c < 3; ++c) {
       current = 0;
       for (t = 0; t < TX_SIZES_ALL; ++t) {
         const int size = tx_size_2d[t];
@@ -388,7 +388,7 @@ void aom_qm_init(AV1_COMMON *cm) {
    frequency domain according to different nominal viewing
    distances.
  */
-static uint16_t iwt_matrix_ref[NUM_QM_LEVELS][2][QM_TOTAL_SIZE] = {
+static uint16_t iwt_matrix_ref[NUM_QM_LEVELS][3][QM_TOTAL_SIZE] = {
   {
       { /* Luma */
         /* Size 4x4 */
@@ -7156,7 +7156,7 @@ static uint16_t iwt_matrix_ref[NUM_QM_LEVELS][2][QM_TOTAL_SIZE] = {
   },
 };
 
-static uint16_t wt_matrix_ref[NUM_QM_LEVELS][2][QM_TOTAL_SIZE] = {
+static uint16_t wt_matrix_ref[NUM_QM_LEVELS][3][QM_TOTAL_SIZE] = {
   {
       { /* Luma */
         /* Size 4x4 */
