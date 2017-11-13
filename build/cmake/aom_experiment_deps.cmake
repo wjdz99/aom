f715c3b099bc173e25bce5f0773b717d8c3a1b2e
Zoe Liu <zoeliu@google.com>
2017-11-12 16:51:31 -0800

    Add experimental flag of SCALE_REFMV
    When getting the motion vector reference candidates from the spatial
    neighboring blocks, if the reference frame of the neighboring block
    is different from that of the current frame, this patch will add a
    scaled version of the mv(s) of the spatial neighbors, according to
    the temporal distance between frames, instead of adding the mv as is
    excepting flipping the signs if needed.
    
    Without the use of mfmv, this patch obtains a coding gain at level of
    ~-0.25% in BDRate saving; With the use of mfmv, it obtains ~0.1% in
    bitrate saving.
    
    For instance, the biggest gain of this patch has been obtained in the
    clip of Keiba:
    Keiba_240p:    (vs w/o mfmv) -1.906% (vs with mfmv) -0.899%
    Keiba_832x480: (vs w/o mfmv) -2.556% (vs with mfmv) -1.042%
    
    Change-Id: I40255355978825dc0c056cb184082d3196326501

--- build/cmake/aom_experiment_deps.cmake
+++ build/cmake/aom_experiment_deps.cmake
@@ -116,6 +116,12 @@ macro (fix_experiment_configs)
     endif ()
   endif ()
 
+  if (CONFIG_SCALE_REFMV)
+    if (NOT CONFIG_FRAME_MARKER)
+      change_config_and_warn(CONFIG_FRAME_MARKER 1 CONFIG_SCALE_REFMV)
+    endif ()
+  endif ()
+
   if (CONFIG_LPF_SB)
     if (CONFIG_LOOPFILTER_LEVEL)
       change_config_and_warn(CONFIG_LOOPFILTER_LEVEL 0 CONFIG_LPF_SB)
