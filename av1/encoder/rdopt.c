eb2ad1774447b85d60ca06ffaae94470ba2be882
Luc Trudeau <luc@trud.ca>
2017-11-22 14:17:21 -0500

    [CFL] DC_PRED on Partition Unit Neighboring Pixels
    Change-Id: I2a4adde79c10089130775b8e0df5f9c198855cad

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -5338,12 +5338,18 @@ static void txfm_rd_in_plane_once(MACROBLOCK *const x,
 }
 
 static int cfl_rd_pick_alpha(MACROBLOCK *const x, const AV1_COMP *const cpi,
-                             BLOCK_SIZE bsize, TX_SIZE tx_size,
-                             int64_t best_rd) {
+                             TX_SIZE tx_size, int64_t best_rd) {
   MACROBLOCKD *const xd = &x->e_mbd;
   MB_MODE_INFO *const mbmi = &xd->mi[0]->mbmi;
-  bsize = scale_chroma_bsize(bsize, xd->plane[AOM_PLANE_U].subsampling_x,
-                             xd->plane[AOM_PLANE_U].subsampling_y);
+
+  const struct macroblockd_plane *const pd = &xd->plane[AOM_PLANE_U];
+  const BLOCK_SIZE bsize = mbmi->sb_type;
+#if CONFIG_DEBUG
+  const BLOCK_SIZE plane_bsize =
+      AOMMAX(BLOCK_4X4, get_plane_block_size(mbmi->sb_type, pd));
+  assert(block_size_wide[plane_bsize] == tx_size_wide[tx_size]);
+  assert(block_size_high[plane_bsize] == tx_size_high[tx_size]);
+#endif
 
   int rates[CFL_PRED_PLANES][CFL_MAGS_SIZE];
   int64_t dists[CFL_PRED_PLANES][CFL_MAGS_SIZE];
@@ -5397,6 +5403,7 @@ static int cfl_rd_pick_alpha(MACROBLOCK *const x, const AV1_COMP *const cpi,
         int rate = x->intra_uv_mode_cost[mbmi->mode][UV_CFL_PRED] +
                    rate_overhead + rates[CFL_PRED_U][idx_u] +
                    rates[CFL_PRED_V][idx_v];
+
         cost = RDCOST(x->rdmult, rate, dist);
         if (cost < best_cost) {
           best_cost = cost;
@@ -5459,8 +5466,9 @@ static int64_t rd_pick_intra_sbuv_mode(const AV1_COMP *const cpi, MACROBLOCK *x,
     if (mode == UV_CFL_PRED) {
       if (!is_cfl_allowed(mbmi)) continue;
       assert(!is_directional_mode);
-      const TX_SIZE uv_tx_size = av1_get_uv_tx_size(mbmi, &xd->plane[1]);
-      cfl_alpha_rate = cfl_rd_pick_alpha(x, cpi, bsize, uv_tx_size, best_rd);
+      const struct macroblockd_plane *const pd = &xd->plane[AOM_PLANE_U];
+      const TX_SIZE uv_tx_size = av1_get_uv_tx_size(mbmi, pd);
+      cfl_alpha_rate = cfl_rd_pick_alpha(x, cpi, uv_tx_size, best_rd);
       if (cfl_alpha_rate == INT_MAX) continue;
     }
 #endif
