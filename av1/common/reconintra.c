a6951889da04298a6d93f466fbcef3ac0fa8a4d8
Luc Trudeau <luc@trud.ca>
2017-11-22 14:17:21 -0500

    [CFL] DC_PRED on Partition Unit Neighboring Pixels
    In order to avoid a cascade of encodes when performing
    CfL RDO, we use DC_PRED on the partition unit. This
    requires disabling CfL when a chroma
    partition-unit-sized DC_PRED is unavailable (i.e. 4:1,
    1:4 partitions and chroma partitions > 32X32).
    
    This change is acheived by changing the tx_size of CfL
    to match the size of the partition unit. This probably
    explains the observed gains:
    
    Results on Subset1 (compared to disabling 4:1 and 1:4 partU):
       PSNR | PSNR Cb | PSNR Cr | PSNR HVS |    SSIM | MS SSIM | CIEDE 2000
    
    https://two.arewecompressedyet.com/?job=CfL-Disable-4to1%402017-11-30T19%3A04%3A00.761Z&job=CfL-DCPRED_PartU%402017-11-30T19%3A02%3A58.246Z
    
    Change-Id: I2a4adde79c10089130775b8e0df5f9c198855cad

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -2666,6 +2666,14 @@ void av1_predict_intra_block_facade(const AV1_COMMON *cm, MACROBLOCKD *xd,
 
 #if CONFIG_CFL
   if (plane != AOM_PLANE_Y && mbmi->uv_mode == UV_CFL_PRED) {
+#if CONFIG_DEBUG
+    assert(blk_col == 0);
+    assert(blk_row == 0);
+    const BLOCK_SIZE plane_bsize =
+        AOMMAX(BLOCK_4X4, get_plane_block_size(mbmi->sb_type, pd));
+    assert(block_size_wide[plane_bsize] == tx_size_wide[tx_size]);
+    assert(block_size_high[plane_bsize] == tx_size_high[tx_size]);
+#endif
     cfl_predict_block(xd, dst, dst_stride, blk_row, blk_col, tx_size, plane);
   }
 #endif
