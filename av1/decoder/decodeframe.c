5ba3565caf19bfb24b4cd9edac97ba0235d14624
Jonathan Matthews <jonathan.matthews@argondesign.com>
2017-11-29 12:28:48 +0000

    q_segmentation: Only encode q_lvls==0 in one way
    BUG=aomedia:1077
    
    Change-Id: I4bb4c78ed2d66ecbb5c886b48a6b9e1d2df1d603

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -1029,7 +1029,7 @@ static void setup_q_segmentation(AV1_COMMON *const cm,
     return;
   }
 
-  seg->q_lvls = decode_unsigned_max(rb, MAX_SEGMENTS);
+  seg->q_lvls = decode_unsigned_max(rb, MAX_SEGMENTS - 1) + 1;
 
   for (i = 0; i < seg->q_lvls; i++) {
     int val = decode_unsigned_max(rb, MAXQ);
