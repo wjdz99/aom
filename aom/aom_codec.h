efbc22ad91a0b405453b2112b3467e08064da577
Maxym Dmytrychenko <maxim.d33@gmail.com>
2017-11-30 12:05:27 +0100

    Adding config file parsing implementation
    Parameters from config file will be added at --cfg options location to
    be processed
    
    Config file example:
     #ignore comment
     ext-partition   : 1 #ignore as well
     codec           : av1
     psnr            : ON
    
    Note(s):
        - Config file is a simple text file
        - Comment starts with hash(#)
          Can be full line or part of the line, after hash(#) details are
    ignored
        - Format: field : value
          colon(:) as delimeter, otherwise full line will be ignored
          Space(s) and tab(s) can be used, not inside field
        - long names for field are prefered
           existing --long_name option format
        - "no value" fields should contain ON as value
    
    Example of usage:
        aomenc --cfg=some.cfg src_filename
    
    Configurations support matrix:
    enable-ext-partition         : done
    enable-loop-restoration      : wip
    enable-deblocking            : wip
    ...
    
    Change-Id: Iad867c5d2da64271cdafa825c89f7d6444582f61

--- aom/aom_codec.h
+++ aom/aom_codec.h
@@ -498,6 +498,19 @@ typedef enum {
   OBU_METADATA_TYPE_HDR_MDCV = 2,
 } OBU_METADATA_TYPE;
 
+/*!\brief Config Options
+ *
+ * This type allows to enumerate and control options defined for control
+ * via config file at runtime.
+ */
+typedef struct cfg_options {
+  /*!\brief Reflects if ext_partition should be enabled
+   *
+   * If this value is non-zero it enabled the feature
+   */
+  unsigned int ext_partition;
+} cfg_options_t;
+
 /*!@} - end defgroup codec*/
 #ifdef __cplusplus
 }
