f5d1d24cc3189e4f6e04e009c7753f3147b643f2
Angie Chiang <angiebird@google.com>
2017-12-04 18:00:40 -0800

    fh < fl --> fh <= fl in od_ec_encode_q15
    od_ec_encode_q15 is able to handle the situation of fh == fl
    
    Change-Id: I7c837dda561f1d25b0203c018763dadd0cbbc75a

--- aom_dsp/entenc.c
+++ aom_dsp/entenc.c
@@ -156,7 +156,7 @@ static void od_ec_encode_q15(od_ec_enc *enc, unsigned fl, unsigned fh, int s,
   l = enc->low;
   r = enc->rng;
   OD_ASSERT(32768U <= r);
-  OD_ASSERT(fh < fl);
+  OD_ASSERT(fh <= fl);
   OD_ASSERT(fl <= 32768U);
   const int N = nsyms - 1;
   if (fl < 32768U) {
