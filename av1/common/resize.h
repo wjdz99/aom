5cca8265aea842c54ae755d2c01d238567008671
David Barker <david.barker@argondesign.com>
2017-12-12 13:13:12 +0000

    Harmonize superres + loop-restoration + lf-across-tiles
    Properly support loopfiltering-across-tiles in combination with
    superres and/or loop-restoration:
    
    Upscale one tile column at a time, rather than doing the whole
    frame at once. This allows us to correctly support the loop filter
    across tiles flag, by temporarily extending the left/right boundaries
    of each tile column to avoid sampling from adjacent tiles.
    
    This code is also reused by striped-loop-restoration, when upscaling the
    deblocked context above/below each stripe. That way, we i) ensure
    that the upscaling is done consistently, and ii) fix the last remaining
    case where loop-restoration didn't respect the loop filter across tiles
    flag.
    
    This also makes it easy to perform extension of the left/right edges
    of the frame "as needed", so we don't need to extend the frame borders
    immediately after deblocking. This should give marginally better CDEF
    filtering for frames using superres.
    
    Change-Id: I28712a177853a20c9eb2993e740da8ba7c95a8cc

--- av1/common/resize.h
+++ av1/common/resize.h
@@ -72,15 +72,12 @@ void av1_resize_and_extend_frame(const YV12_BUFFER_CONFIG *src,
 #endif  // CONFIG_HIGHBITDEPTH
 
 #if CONFIG_HORZONLY_FRAME_SUPERRES
-#if CONFIG_HIGHBITDEPTH
-void av1_upscale_normative_and_extend_frame(const YV12_BUFFER_CONFIG *src,
-                                            YV12_BUFFER_CONFIG *dst,
-                                            int superres_denom, int bd);
-#else
-void av1_upscale_normative_and_extend_frame(const YV12_BUFFER_CONFIG *src,
-                                            YV12_BUFFER_CONFIG *dst,
-                                            int superres_denom);
-#endif  // CONFIG_HIGHBITDEPTH
+void av1_upscale_normative_rows(const AV1_COMMON *cm, const uint8_t *src,
+                                int src_stride, uint8_t *dst, int dst_stride,
+                                int plane, int rows);
+void av1_upscale_normative_and_extend_frame(const AV1_COMMON *cm,
+                                            const YV12_BUFFER_CONFIG *src,
+                                            YV12_BUFFER_CONFIG *dst);
 #endif  // CONFIG_HORZONLY_FRAME_SUPERRES
 
 YV12_BUFFER_CONFIG *av1_scale_if_required(AV1_COMMON *cm,
@@ -110,14 +107,14 @@ static INLINE int av1_superres_unscaled(const AV1_COMMON *cm) {
 }
 #endif  // CONFIG_HORZONLY_FRAME_SUPERRES
 
-#if CONFIG_HORZONLY_FRAME_SUPERRES && CONFIG_LOOP_RESTORATION
+#if CONFIG_HORZONLY_FRAME_SUPERRES
 #define UPSCALE_NORMATIVE_TAPS 8
 
 extern const int16_t av1_resize_filter_normative[1 << RS_SUBPEL_BITS]
                                                 [UPSCALE_NORMATIVE_TAPS];
 
 int32_t av1_get_upscale_convolve_step(int in_length, int out_length);
-#endif  // CONFIG_HORZONLY_FRAME_SUPERRES && CONFIG_LOOP_RESTORATION
+#endif  // CONFIG_HORZONLY_FRAME_SUPERRES
 
 #ifdef __cplusplus
 }  // extern "C"
