6560fdf4c80f6da32d6af514bcbb2ada1d82b0b0
Yue Chen <yuec@google.com>
2017-12-11 16:31:21 -0800

    Remove dead code for old rect_tx_ext
    Change-Id: Ifa3b55d9c8022a66b6c0b1a15d3e8943cd7c69e6

--- tools/aom_entropy_optimizer.c
+++ tools/aom_entropy_optimizer.c
@@ -566,15 +566,6 @@ int main(int argc, const char **argv) {
 /* Compound single ref inter mode */
 
 /* Transform size */
-#if CONFIG_RECT_TX_EXT
-  cts_each_dim[0] = 2;
-  optimize_entropy_table(&fc.quarter_tx_size[0], probsfile, 1, cts_each_dim,
-                         NULL, 1,
-                         "static const aom_prob default_quarter_tx_size_prob");
-  optimize_cdf_table(
-      &fc.quarter_tx_size[0], probsfile, 1, cts_each_dim,
-      "static const aom_cdf_prob default_quarter_tx_size_cdf[CDF_SIZE(2)]");
-#endif
   cts_each_dim[0] = TXFM_PARTITION_CONTEXTS;
   cts_each_dim[1] = 2;
   optimize_cdf_table(
