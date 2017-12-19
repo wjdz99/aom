bloomberg/chromium.bb

commit 8089315a6c2b180de2c5db75e89b3075c8047927

TX64X64: Reuse scan, quant etc from 32x32.
- Reuse scan order
- Truncate to max eob of 32*32
- Quantization and entropy coding same as done for TX_32X32
- Reuse quantization matrices of TX_32X32

Compression performance is roughly neutral:
https://arewecompressedyet.com/?job=tx64x64_oldscans%402017-11-06T03%3A11%3A53.868Z&job=tx64x64_reusescans%402017-11-06T03%3A12%3A55.738Z

Change-Id: Ie9182c1c69a42a3c1ab4fc980abbd6000c64f179

av1/common/blockd.h
@@ -1409,6 +1409,16 @@ static INLINE void transpose_int32(int32_t *dst, int dst_stride,
     for (c = 0; c < w; ++c) dst[c * dst_stride + r] = src[r * src_stride + c];
 }
 
+static INLINE int av1_get_max_eob(TX_SIZE tx_size) {
+  return
+#if CONFIG_TX64X64 && !CONFIG_DAALA_TX
+      tx_size == TX_64X64 || tx_size == TX_64X32 || tx_size == TX_32X64
+          ? 1024
+          :
+#endif  // CONFIG_TX64X64 && !CONFIG_DAALA_TX
+          tx_size_2d[tx_size];
+}
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif

av1/common/av1_fwd_txfm2d.c
@@ -351,6 +351,10 @@ void av1_fwd_txfm2d_64x64_c(const int16_t *input, int32_t *output, int stride,
   }
   // Zero out the bottom 64x32 area.
   memset(output + 32 * 64, 0, 32 * 64 * sizeof(*output));
+  // Re-pack non-zero coeffs in the first 32x32 indices.
+  for (int row = 1; row < 32; ++row) {
+    memcpy(output + row * 32, output + row * 64, 32 * sizeof(*output));
+  }
 }
 
 void av1_fwd_txfm2d_32x64_c(const int16_t *input, int32_t *output, int stride,
@@ -361,6 +365,7 @@ void av1_fwd_txfm2d_32x64_c(const int16_t *input, int32_t *output, int stride,
 
   // Zero out the bottom 32x32 area.
   memset(output + 32 * 32, 0, 32 * 32 * sizeof(*output));
+  // Note: no repacking needed here.
 }
 
 void av1_fwd_txfm2d_64x32_c(const int16_t *input, int32_t *output, int stride,
@@ -372,6 +377,10 @@ void av1_fwd_txfm2d_64x32_c(const int16_t *input, int32_t *output, int stride,
   // Zero out right 32x32 area.
   for (int row = 0; row < 32; ++row) {
     memset(output + row * 64 + 32, 0, 32 * sizeof(*output));
+  }
+  // Re-pack non-zero coeffs in the first 32x32 indices.
+  for (int row = 1; row < 32; ++row) {
+    memcpy(output + row * 32, output + row * 64, 32 * sizeof(*output));
   }
 }
 #endif  // CONFIG_TX64X64

av1/common/av1_inv_txfm2d.c
@@ -391,12 +391,31 @@ void av1_inv_txfm2d_add_32x32_c(const int32_t *input, uint16_t *output,
 #if CONFIG_TX64X64
 void av1_inv_txfm2d_add_64x64_c(const int32_t *input, uint16_t *output,
                                 int stride, TX_TYPE tx_type, int bd) {
+  // TODO(urvang): Can the same array be reused, instead of using a new array?
+  // Remap 32x32 input into a modified 64x64 by:
+  // - Copying over these values in top-left 32x32 locations.
+  // - Setting the rest of the locations to 0.
+  int32_t mod_input[64 * 64];
+  for (int row = 0; row < 32; ++row) {
+    memcpy(mod_input + row * 64, input + row * 32, 32 * sizeof(*mod_input));
+    memset(mod_input + row * 64 + 32, 0, 32 * sizeof(*mod_input));
+  }
+  memset(mod_input + 32 * 64, 0, 32 * 64 * sizeof(*mod_input));
   int txfm_buf[64 * 64 + 64 + 64];
-  inv_txfm2d_add_facade(input, output, stride, txfm_buf, tx_type, TX_64X64, bd);
+  inv_txfm2d_add_facade(mod_input, output, stride, txfm_buf, tx_type, TX_64X64,
+                        bd);
 }
 
 void av1_inv_txfm2d_add_64x32_c(const int32_t *input, uint16_t *output,
                                 int stride, TX_TYPE tx_type, int bd) {
+  // Remap 32x32 input into a modified 64x32 by:
+  // - Copying over these values in top-left 32x32 locations.
+  // - Setting the rest of the locations to 0.
+  int32_t mod_input[64 * 32];
+  for (int row = 0; row < 32; ++row) {
+    memcpy(mod_input + row * 64, input + row * 32, 32 * sizeof(*mod_input));
+    memset(mod_input + row * 64 + 32, 0, 32 * sizeof(*mod_input));
+  }
 #if CONFIG_TXMG
   int txfm_buf[64 * 32 + 64 + 64];
   int32_t rinput[64 * 32];
@@ -408,20 +427,28 @@ void av1_inv_txfm2d_add_64x32_c(const int32_t *input, uint16_t *output,
   int h = tx_size_high[tx_size];
   int rw = h;
   int rh = w;
-  transpose_int32(rinput, rw, input, w, w, h);
+  transpose_int32(rinput, rw, mod_input, w, w, h);
   transpose_uint16(routput, rw, output, stride, w, h);
   inv_txfm2d_add_facade(rinput, routput, rw, txfm_buf, rtx_type, rtx_size, bd);
   transpose_uint16(output, stride, routput, rw, rw, rh);
 #else
   int txfm_buf[64 * 32 + 64 + 64];
-  inv_txfm2d_add_facade(input, output, stride, txfm_buf, tx_type, TX_64X32, bd);
-#endif
+  inv_txfm2d_add_facade(mod_input, output, stride, txfm_buf, tx_type, TX_64X32,
+                        bd);
+#endif  // CONFIG_TXMG
 }
 
 void av1_inv_txfm2d_add_32x64_c(const int32_t *input, uint16_t *output,
                                 int stride, TX_TYPE tx_type, int bd) {
+  // Remap 32x32 input into a modified 32x64 input by:
+  // - Copying over these values in top-left 32x32 locations.
+  // - Setting the rest of the locations to 0.
+  int32_t mod_input[32 * 64];
+  memcpy(mod_input, input, 32 * 32 * sizeof(*mod_input));
+  memset(mod_input + 32 * 32, 0, 32 * 32 * sizeof(*mod_input));
   int txfm_buf[64 * 32 + 64 + 64];
-  inv_txfm2d_add_facade(input, output, stride, txfm_buf, tx_type, TX_32X64, bd);
+  inv_txfm2d_add_facade(mod_input, output, stride, txfm_buf, tx_type, TX_32X64,
+                        bd);
 }
 #endif  // CONFIG_TX64X64
 

av1/common/idct.c
@@ -2502,6 +2502,8 @@ void av1_inverse_transform_block(const MACROBLOCKD *xd,
                                  uint8_t *dst, int stride, int eob) {
   if (!eob) return;
 
+  assert(eob <= av1_get_max_eob(tx_size));
+
   TxfmParam txfm_param;
   init_txfm_param(xd, plane, tx_size, tx_type, eob, &txfm_param);
 #if CONFIG_MRC_TX

av1/common/quant_common.c
@@ -487,10 +487,12 @@ void aom_qm_init(AV1_COMMON *cm) {
       current = 0;
       for (t = 0; t < TX_SIZES_ALL; ++t) {
         const int size = tx_size_2d[t];
-        // Don't use QM for sizes > 32x32
-        if (q == NUM_QM_LEVELS - 1 || size > 1024) {
+        if (q == NUM_QM_LEVELS - 1) {
           cm->gqmatrix[q][c][t] = NULL;
           cm->giqmatrix[q][c][t] = NULL;
+        } else if (size > 1024) {  // Reuse matrices for TX_32X32
+          cm->gqmatrix[q][c][t] = cm->gqmatrix[q][c][TX_32X32];
+          cm->giqmatrix[q][c][t] = cm->giqmatrix[q][c][TX_32X32];
         } else {
           assert(current + size <= QM_TOTAL_SIZE);
           cm->gqmatrix[q][c][t] = &wt_matrix_ref[q][c >= 1][current];

av1/decoder/decodetxb.c
@@ -69,7 +69,7 @@ uint8_t av1_read_coeffs_txb(const AV1_COMMON *const cm, MACROBLOCKD *const xd,
   const TX_SIZE txs_ctx = get_txsize_context(tx_size);
   const PLANE_TYPE plane_type = get_plane_type(plane);
   MB_MODE_INFO *const mbmi = &xd->mi[0]->mbmi;
-  const int seg_eob = tx_size_2d[tx_size];
+  const int seg_eob = av1_get_max_eob(tx_size);
   int c = 0;
   int update_eob = -1;
   const int16_t *const dequant =

av1/decoder/detokenize.c
@@ -110,7 +110,7 @@ static int decode_coefs(MACROBLOCKD *xd, PLANE_TYPE type, tran_low_t *dqcoeff,
                         int ctx, const int16_t *scan, const int16_t *nb,
                         int16_t *max_scan_line, aom_reader *r) {
   FRAME_CONTEXT *ec_ctx = xd->tile_ctx;
-  const int max_eob = tx_size_2d[tx_size];
+  const int max_eob = av1_get_max_eob(tx_size);
   const int ref = is_inter_block(&xd->mi[0]->mbmi);
 #if CONFIG_AOM_QM && !CONFIG_NEW_QUANT
   const qm_val_t *iqmatrix = iqm[tx_size];

wjdz99/aom

commit d75b8d571a7e6d2c2eb849dfd4dd775f9d9705aa

Fix bustage caused by 8089315a6c2b with daala_tx.
The inverse transform API was changed to pass in an unpadded 32x32
block of coefficients for transforms larger than 32x32, but the
code path actually used for daala_tx was not modified to pad it out
to the full size like the others were.

Change-Id: Ibda5d20a9d839ba41f8a1a0308c414111219da92

av1/common/blockd.h
@@ -1440,7 +1440,7 @@ static INLINE void transpose_int32(int32_t *dst, int dst_stride,
 }
 
 static INLINE int av1_get_max_eob(TX_SIZE tx_size) {
-#if CONFIG_TX64X64 && !CONFIG_DAALA_TX
+#if CONFIG_TX64X64
   if (tx_size == TX_64X64 || tx_size == TX_64X32 || tx_size == TX_32X64) {
     return 1024;
   }

av1/common/daala_inv_txfm.c
@@ -87,11 +87,33 @@ void daala_inv_txfm_add_c(const tran_low_t *input_coeffs, void *output_pixels,
     int col_flip = tx_flip(vtx_tab[tx_type]);
     int row_flip = tx_flip(htx_tab[tx_type]);
     od_coeff tmpsq[MAX_TX_SQUARE];
+    tran_low_t pad_input[MAX_TX_SQUARE];
     int r;
     int c;
 
     assert(col_tx);
     assert(row_tx);
+
+    if (rows > 32 || cols > 32) {
+      // TODO(urvang): Can the same array be reused, instead of using a new
+      // array?
+      // Remap 32x32 input into a modified input by:
+      // - Copying over these values in top-left 32x32 locations.
+      // - Setting the rest of the locations to 0.
+      for (r = 0; r < 32; r++) {
+        memcpy(pad_input + r * cols, input_coeffs + r * 32,
+               32 * sizeof(*pad_input));
+        if (cols > 32) {
+          memset(pad_input + r * cols + 32, 0,
+                 (cols - 32) * sizeof(*pad_input));
+        }
+      }
+      if (rows > 32) {
+        memset(pad_input + 32 * cols, 0,
+               (rows - 32) * cols * sizeof(*pad_input));
+      }
+      input_coeffs = pad_input;
+   }
 
     // Inverse-transform rows
     for (r = 0; r < rows; ++r) {