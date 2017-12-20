5ead789406501b1a71dd57ac3720ca6d8cc935cf
Luc Trudeau <luc@trud.ca>
2017-12-19 23:15:28 -0500

    [CFL] SSE2/AVX2 versions of luma_subsampling_420_lbd
    Includes unit tests for conformance and speed.
    
    SSE2/SubsampleSpeedTest:
    4x4: C time = 804 us, SIMD time = 261 us (~3.1x)
    8x8: C time = 2915 us, SIMD time = 453 us (~6.4x)
    16x16: C time = 11418 us, SIMD time = 1799 us (~6.3x)
    
    AVX2/SubsampleSpeedTest:
    4x4: C time = 893 us, SIMD time = 282 us (~3.2x)
    8x8: C time = 3065 us, SIMD time = 496 us (~6.2x)
    16x16: C time = 11656 us, SIMD time = 1086 us (~11x)
    
    Change-Id: I7d5958097542599d57d1a9f9a0a1b809c6a345b0

--- av1/common/cfl_avx2.c
+++ av1/common/cfl_avx2.c
@@ -37,3 +37,44 @@ void av1_cfl_subtract_avx2(int16_t *pred_buf_q3, int width, int height,
     _mm256_storeu_si256((__m256i *)pred_buf_q3, _mm256_sub_epi16(vals0, avg));
   } while ((pred_buf_q3 += stride) < end);
 }
+
+static void cfl_luma_subsampling_420_lbd_avx2(const uint8_t *input,
+                                              int input_stride,
+                                              int16_t *output_q3, int width,
+                                              int height) {
+  const __m256i zeros = _mm256_setzero_si256();
+  const __m256i twos = _mm256_set1_epi16(2);
+  const int16_t *row_end = output_q3 + height * CFL_BUF_LINE;
+  const int luma_stride = input_stride << 1;
+  (void)width;
+  do {
+    __m256i top = _mm256_loadu_si256((__m256i *)(input));
+    __m256i bot = _mm256_loadu_si256((__m256i *)(input + input_stride));
+
+    __m256i top_lo = _mm256_unpacklo_epi8(top, zeros);
+    __m256i bot_lo = _mm256_unpacklo_epi8(bot, zeros);
+    __m256i top_hi = _mm256_unpackhi_epi8(top, zeros);
+    __m256i bot_hi = _mm256_unpackhi_epi8(bot, zeros);
+
+    top_lo = _mm256_add_epi16(top_lo, bot_lo);
+    top_hi = _mm256_add_epi16(top_hi, bot_hi);
+
+    top_lo = _mm256_madd_epi16(top_lo, twos);
+    top_hi = _mm256_madd_epi16(top_hi, twos);
+
+    _mm256_storeu_si256((__m256i *)output_q3,
+                        _mm256_packs_epi32(top_lo, top_hi));
+    input += luma_stride;
+    output_q3 += CFL_BUF_LINE;
+  } while (output_q3 < row_end);
+}
+
+cfl_subsample_lbd_fn get_subsample_lbd_fn_avx2(int sub_x, int sub_y) {
+  static const cfl_subsample_lbd_fn subsample_lbd[2][2] = {
+    //  (sub_y == 0, sub_x == 0)       (sub_y == 0, sub_x == 1)
+    //  (sub_y == 1, sub_x == 0)       (sub_y == 1, sub_x == 1)
+    { cfl_luma_subsampling_444_lbd, cfl_luma_subsampling_422_lbd },
+    { cfl_luma_subsampling_440_lbd, cfl_luma_subsampling_420_lbd_avx2 },
+  };
+  return subsample_lbd[sub_y & 1][sub_x & 1];
+}
