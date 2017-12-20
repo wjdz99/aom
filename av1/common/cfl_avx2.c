92ce4d206940ceeb8df4f0e25442d35cc5a0e5ea
David Michael Barr <b@rr-dav.id.au>
2017-12-21 01:56:03 +0900

    [CFL] SSSE3/AVX2 versions of cfl_build_prediction_lbd
    Includes unit tests for conformance and speed.
    
    SSSE3/CFLPredictTest:
    4x4: C time = 2964 us, SIMD time = 478 us (~6.2x)
    8x8: C time = 3135 us, SIMD time = 516 us (~6.1x)
    16x16: C time = 14417 us, SIMD time = 1719 us (~8.4x)
    32x32: C time = 51280 us, SIMD time = 5318 us (~9.6x)
    
    AVX2/CFLPredictTest:
    4x4: C time = 2062 us, SIMD time = 393 us (~5.2x)
    8x8: C time = 6627 us, SIMD time = 681 us (~9.7x)
    16x16: C time = 25064 us, SIMD time = 2087 us (~12x)
    32x32: C time = 59581 us, SIMD time = 7864 us (~7.6x)
    
    Change-Id: I08690a548be981ff10e184de468b9e0e691ee812

--- av1/common/cfl_avx2.c
+++ av1/common/cfl_avx2.c
@@ -99,3 +99,60 @@ cfl_subsample_lbd_fn get_subsample_lbd_fn_avx2(int sub_x, int sub_y) {
   // index the function pointer array out of bounds.
   return subsample_lbd[sub_y & 1][sub_x & 1];
 }
+
+static INLINE __m256i get_scaled_luma_q0_avx2(const __m256i *input,
+                                              __m256i alpha_q12,
+                                              __m256i alpha_sign) {
+  __m256i ac_q3 = _mm256_loadu_si256(input);
+  __m256i ac_sign = _mm256_srai_epi16(ac_q3, 15);
+  ac_q3 = _mm256_xor_si256(ac_q3, ac_sign);
+  ac_q3 = _mm256_sub_epi16(ac_q3, ac_sign);
+  ac_sign = _mm256_xor_si256(ac_sign, alpha_sign);
+  __m256i scaled_luma_q0 = _mm256_mulhrs_epi16(ac_q3, alpha_q12);
+  scaled_luma_q0 = _mm256_xor_si256(scaled_luma_q0, ac_sign);
+  scaled_luma_q0 = _mm256_sub_epi16(scaled_luma_q0, ac_sign);
+  return scaled_luma_q0;
+}
+
+static void av1_cfl_build_prediction_lbd_avx2(const int16_t *pred_buf_q3,
+                                              uint8_t *dst, int dst_stride,
+                                              TX_SIZE tx_size, int alpha_q3) {
+  const int height = tx_size_high[tx_size];
+  const int width = tx_size_wide[tx_size];
+  const __m256i zeros = _mm256_setzero_si256();
+  const __m256i alpha_q12 = _mm256_set1_epi16(abs(alpha_q3) * (1 << 9));
+  const __m256i alpha_sign = alpha_q3 < 0 ? _mm256_set1_epi16(-1) : zeros;
+  const __m256i dc_packed = _mm256_loadu_si256((__m256i *)dst);
+  const __m256i dc_q0 = _mm256_unpacklo_epi8(dc_packed, zeros);
+
+  const int16_t *row_end = pred_buf_q3 + height * CFL_BUF_LINE;
+  (void)width;
+  do {
+    __m256i scaled_luma_q0 =
+        get_scaled_luma_q0_avx2((__m256i *)pred_buf_q3, alpha_q12, alpha_sign);
+    __m256i tmp0 = _mm256_add_epi16(scaled_luma_q0, dc_q0);
+    if (width >= 16)
+      scaled_luma_q0 = get_scaled_luma_q0_avx2((__m256i *)(pred_buf_q3 + 8),
+                                               alpha_q12, alpha_sign);
+    __m256i tmp1 = _mm256_add_epi16(scaled_luma_q0, dc_q0);
+    __m256i res = _mm256_packus_epi16(tmp0, tmp1);
+    _mm256_storeu_si256((__m256i *)dst, res);
+    if (width == 32) {
+      scaled_luma_q0 = get_scaled_luma_q0_avx2((__m256i *)(pred_buf_q3 + 16),
+                                               alpha_q12, alpha_sign);
+      tmp0 = _mm256_add_epi16(scaled_luma_q0, dc_q0);
+      scaled_luma_q0 = get_scaled_luma_q0_avx2((__m256i *)(pred_buf_q3 + 24),
+                                               alpha_q12, alpha_sign);
+      tmp1 = _mm256_add_epi16(scaled_luma_q0, dc_q0);
+      res = _mm256_packus_epi16(tmp0, tmp1);
+      _mm256_storeu_si256((__m256i *)(dst + 16), res);
+    }
+    dst += dst_stride;
+    pred_buf_q3 += CFL_BUF_LINE;
+  } while (pred_buf_q3 < row_end);
+}
+
+cfl_predict_lbd_fn get_predict_lbd_fn_avx2(TX_SIZE tx_size) {
+  (void)tx_size;
+  return av1_cfl_build_prediction_lbd_avx2;
+}
