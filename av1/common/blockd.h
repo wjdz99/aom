251b1b00e3296e1a8527a7c99a19f68c30a6c4cc
Yue Chen <yuec@google.com>
2017-12-20 23:33:41 -0800

    Make wedge_index entropy coded
    Make wedge_index of interinter and interintra wedge-based compound
    mode entropy coded. An entropy table has been trained offline.
    This feature is controlled by macro WEDGE_IDX_ENTROPY_CODING and
    II_WEDGE_IDX_ENTROPY_CODING.
    
    Change-Id: Id9420d2d47cfaa7b441f965e92d4141cf14c3c9a

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -1176,6 +1176,11 @@ void av1_set_contexts(const MACROBLOCKD *xd, struct macroblockd_plane *pd,
                       int plane, TX_SIZE tx_size, int has_eob, int aoff,
                       int loff);
 
+static INLINE int is_interintra_mode(const MB_MODE_INFO *mbmi) {
+  return (mbmi->ref_frame[0] > INTRA_FRAME &&
+          mbmi->ref_frame[1] == INTRA_FRAME);
+}
+
 static INLINE int is_interintra_allowed_bsize(const BLOCK_SIZE bsize) {
   return (bsize >= BLOCK_8X8) && (bsize <= BLOCK_32X32);
 }
