f699f3313f5a3fd60ae98151525e143438213b7a
Debargha Mukherjee <debargha@google.com>
2017-12-20 22:23:35 -0800

    Disable txfm domain dist for 64-pt transforms
    Any 64-pt transforms only preserves half the coefficients.
    Therefore transform domain distortion is no longer valid
    for these transform sizes.
    
    BUG=aomedia:1170
    
    Change-Id: Ib99fad5497c766e2b9d2b432640c03cd49c99f50

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -1908,6 +1908,9 @@ void av1_dist_block(const AV1_COMP *cpi, MACROBLOCK *x, int plane,
 #endif  // CONFIG_DIST_8X8
 
   if (cpi->sf.use_transform_domain_distortion
+#if CONFIG_TX64X64
+      && txsize_sqr_up_map[tx_size] != TX_64X64
+#endif  // CONFIG_TX64X64
 #if CONFIG_DIST_8X8
       && !x->using_dist_8x8
 #endif
