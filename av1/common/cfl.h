31091554854cf02410ad2d7eab78dff5ba10f0a5
Luc Trudeau <luc@trud.ca>
2017-12-23 00:07:29 -0500

    [CFL] SSSE3/AVX2 Versions of Sum and Subtract Average
    Includes unit tests for conformance and speed.
    
    SSSE3/CFLSubAvgTest
    4x4: C time = 1848 us, SIMD time = 1419 us (~1.3x)
    8x8: C time = 6615 us, SIMD time = 2509 us (~2.6x)
    16x16: C time = 26552 us, SIMD time = 7434 us (~3.6x)
    32x32: C time = 112318 us, SIMD time = 25562 us (~4.4x)
    
    AVX2/CFLSubAvgTest
    4x4: C time = 1861 us, SIMD time = 1515 us (~1.2x)
    8x8: C time = 6705 us, SIMD time = 2470 us (~2.7x)
    16x16: C time = 27250 us, SIMD time = 6633 us (~4.1x)
    32x32: C time = 111368 us, SIMD time = 22032 us (~5.1x)
    
    Change-Id: I44ffedc683829d2c16089854ac43d4ddb4415bcd

--- av1/common/cfl.h
+++ av1/common/cfl.h
@@ -17,6 +17,8 @@
 typedef void (*cfl_subsample_lbd_fn)(const uint8_t *input, int input_stride,
                                      int16_t *output_q3, int width, int height);
 
+typedef void (*cfl_subtract_average_fn)(int16_t *pred_buf_q3);
+
 typedef void (*cfl_predict_lbd_fn)(const int16_t *pred_buf_q3, uint8_t *dst,
                                    int dst_stride, TX_SIZE tx_size,
                                    int alpha_q3);
@@ -64,4 +66,69 @@ void cfl_luma_subsampling_440_lbd(const uint8_t *input, int input_stride,
 // TODO(ltrudeau) Remove this when 444 SIMD is added
 void cfl_luma_subsampling_444_lbd(const uint8_t *input, int input_stride,
                                   int16_t *output_q3, int width, int height);
+
+static INLINE void cfl_subtract_average_null(int16_t *pred_buf_q3) {
+  (void)pred_buf_q3;
+  assert(0);
+}
+
+#define CFL_SUB_AVG_X(width, height, round_offset, num_pel_log2)              \
+  static void subtract_average_##width##x##height##_x(int16_t *pred_buf_q3) { \
+    subtract_average_x(pred_buf_q3, width, height, round_offset,              \
+                       num_pel_log2);                                         \
+  }
+
+#if CONFIG_TX64X64
+#define CFL_SUB_AVG_FN(arch)                                                \
+  cfl_subtract_average_fn get_subtract_average_fn_##arch(TX_SIZE tx_size) { \
+    static const cfl_subtract_average_fn sub_avg[TX_SIZES_ALL] = {          \
+      subtract_average_4x4_x,    /* 4x4 */                                  \
+      subtract_average_8x8_x,    /* 8x8 */                                  \
+      subtract_average_16x16_x,  /* 16x16 */                                \
+      subtract_average_32x32_x,  /* 32x32 */                                \
+      cfl_subtract_average_null, /* 64x64 (invalid CFL size) */             \
+      subtract_average_4x8_x,    /* 4x8 */                                  \
+      subtract_average_8x4_x,    /* 8x4 */                                  \
+      subtract_average_8x16_x,   /* 8x16 */                                 \
+      subtract_average_16x8_x,   /* 16x8 */                                 \
+      subtract_average_16x32_x,  /* 16x32 */                                \
+      subtract_average_32x16_x,  /* 32x16 */                                \
+      cfl_subtract_average_null, /* 32x64 (invalid CFL size) */             \
+      cfl_subtract_average_null, /* 64x32 (invalid CFL size) */             \
+      cfl_subtract_average_null, /* 4x16 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 16x4 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 8x32 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 32x8 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 16x64 (invalid CFL size) */             \
+      cfl_subtract_average_null, /* 64x16 (invalid CFL size) */             \
+    };                                                                      \
+    /* Modulo TX_SIZES_ALL to ensure that an attacker won't be able to */   \
+    /* index the function pointer array out of bounds. */                   \
+    return sub_avg[tx_size % TX_SIZES_ALL];                                 \
+  }
+#else
+#define CFL_SUB_AVG_FN(arch)                                                \
+  cfl_subtract_average_fn get_subtract_average_fn_##arch(TX_SIZE tx_size) { \
+    static const cfl_subtract_average_fn sub_avg[TX_SIZES_ALL] = {          \
+      subtract_average_4x4_x,    /* 4x4 */                                  \
+      subtract_average_8x8_x,    /* 8x8 */                                  \
+      subtract_average_16x16_x,  /* 16x16 */                                \
+      subtract_average_32x32_x,  /* 32x32 */                                \
+      subtract_average_4x8_x,    /* 4x8 */                                  \
+      subtract_average_8x4_x,    /* 8x4 */                                  \
+      subtract_average_8x16_x,   /* 8x16 */                                 \
+      subtract_average_16x8_x,   /* 16x8 */                                 \
+      subtract_average_16x32_x,  /* 16x32 */                                \
+      subtract_average_32x16_x,  /* 32x16 */                                \
+      cfl_subtract_average_null, /* 4x16 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 16x4 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 8x32 (invalid CFL size) */              \
+      cfl_subtract_average_null, /* 32x8 (invalid CFL size) */              \
+    };                                                                      \
+    /* Modulo TX_SIZES_ALL to ensure that an attacker won't be able to */   \
+    /* index the function pointer array out of bounds. */                   \
+    return sub_avg[tx_size % TX_SIZES_ALL];                                 \
+  }
+#endif
+
 #endif  // AV1_COMMON_CFL_H_
--- av1/common/cfl_ssse3.c
+++ av1/common/cfl_ssse3.c
@@ -15,6 +15,8 @@
 
 #include "av1/common/cfl.h"
 
+#define CFL_BUF_LINE_I128 (CFL_BUF_LINE >> 3)
+
 /**
  * Adds 4 pixels (in a 2x2 grid) and multiplies them by 2. Resulting in a more
  * precise version of a box filter 4:2:0 pixel subsampling in Q3.
@@ -222,3 +224,84 @@ cfl_predict_hbd_fn get_predict_hbd_fn_ssse3(TX_SIZE tx_size) {
   };
   return predict_hbd[(tx_size_wide_log2[tx_size] - tx_size_wide_log2[0]) & 3];
 }
+
+static INLINE __m128i fill_sum_epi32(__m128i l0) {
+  l0 = _mm_add_epi32(l0, _mm_shuffle_epi32(l0, _MM_SHUFFLE(1, 0, 3, 2)));
+  return _mm_add_epi32(l0, _mm_shuffle_epi32(l0, _MM_SHUFFLE(2, 3, 0, 1)));
+}
+
+static INLINE void subtract_average_x(int16_t *pred_buf, int width, int height,
+                                      int round_offset, int num_pel_log2) {
+  const __m128i zeros = _mm_setzero_si128();
+  const __m128i round_offset_epi32 = _mm_set1_epi32(round_offset);
+  __m128i *pred_buf_i128 = (__m128i *)pred_buf;
+
+  __m128i sum = zeros;
+  int i = 0;
+  while (i < height) {
+    __m128i *const row = pred_buf_i128 + i * CFL_BUF_LINE_I128;
+    if (width == 4) {
+      __m128i l0 = _mm_add_epi16(_mm_loadl_epi64(row),
+                                 _mm_loadl_epi64(row + CFL_BUF_LINE_I128));
+      __m128i l1 = _mm_add_epi16(_mm_loadl_epi64(row + 2 * CFL_BUF_LINE_I128),
+                                 _mm_loadl_epi64(row + 3 * CFL_BUF_LINE_I128));
+      sum = _mm_add_epi32(sum, _mm_add_epi32(_mm_unpacklo_epi16(l0, zeros),
+                                             _mm_unpacklo_epi16(l1, zeros)));
+      i += 4;
+    } else {
+      __m128i l0;
+      if (width == 8) {
+        l0 = _mm_add_epi16(_mm_loadu_si128(row),
+                           _mm_loadu_si128(row + CFL_BUF_LINE_I128));
+        i += 2;
+      } else {
+        l0 = _mm_add_epi16(_mm_loadu_si128(row), _mm_loadu_si128(row + 1));
+        i++;
+      }
+      sum = _mm_add_epi32(sum, _mm_add_epi32(_mm_unpacklo_epi16(l0, zeros),
+                                             _mm_unpackhi_epi16(l0, zeros)));
+      if (width == 32) {
+        l0 = _mm_add_epi16(_mm_loadu_si128(row + 2), _mm_loadu_si128(row + 3));
+        sum = _mm_add_epi32(sum, _mm_add_epi32(_mm_unpacklo_epi16(l0, zeros),
+                                               _mm_unpackhi_epi16(l0, zeros)));
+      }
+    }
+  }
+  sum = fill_sum_epi32(sum);
+
+  __m128i avg_x16 =
+      _mm_srli_epi32(_mm_add_epi32(sum, round_offset_epi32), num_pel_log2);
+  avg_x16 = _mm_packs_epi32(avg_x16, avg_x16);
+
+  for (i = 0; i < height; i++) {
+    __m128i *const row = pred_buf_i128 + i * CFL_BUF_LINE_I128;
+    if (width == 4) {
+      _mm_storel_epi64(row, _mm_sub_epi16(_mm_loadl_epi64(row), avg_x16));
+    } else {
+      _mm_storeu_si128(row, _mm_sub_epi16(_mm_loadu_si128(row), avg_x16));
+      if (width > 8) {
+        _mm_storeu_si128(row + 1,
+                         _mm_sub_epi16(_mm_loadu_si128(row + 1), avg_x16));
+        if (width == 32) {
+          _mm_storeu_si128(row + 2,
+                           _mm_sub_epi16(_mm_loadu_si128(row + 2), avg_x16));
+          _mm_storeu_si128(row + 3,
+                           _mm_sub_epi16(_mm_loadu_si128(row + 3), avg_x16));
+        }
+      }
+    }
+  }
+}
+
+CFL_SUB_AVG_X(4, 4, 8, 4)
+CFL_SUB_AVG_X(4, 8, 16, 5)
+CFL_SUB_AVG_X(8, 4, 16, 5)
+CFL_SUB_AVG_X(8, 8, 32, 6)
+CFL_SUB_AVG_X(8, 16, 64, 7)
+CFL_SUB_AVG_X(16, 8, 64, 7)
+CFL_SUB_AVG_X(16, 16, 128, 8)
+CFL_SUB_AVG_X(16, 32, 256, 9)
+CFL_SUB_AVG_X(32, 16, 256, 9)
+CFL_SUB_AVG_X(32, 32, 512, 10)
+
+CFL_SUB_AVG_FN(ssse3)
