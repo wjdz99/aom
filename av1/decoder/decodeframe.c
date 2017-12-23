13e1aa5e83223d6056f74d73e99489e3ef9bc78d
Zoe Liu <zoeliu@google.com>
2017-12-22 17:14:48 -0800

    Not signal reference_mode if one ref avaialble
    Use the frame ID that indicates the frame display order to identify
    whether two different reference frames exist for inter-coded frames.
    If there is only one unique reference valid in the reference buffer,
    there is no need to signal reference_mode. Instead, the decoder may
    identify such scenario and set reference_mode to SINGLE_REFERENCE.
    
    Change-Id: If7d374f5355f153c50b408be5a9956a833c976c3

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -89,10 +89,6 @@ static int read_compressed_header(AV1Decoder *pbi, const uint8_t *data,
 static size_t read_uncompressed_header(AV1Decoder *pbi,
                                        struct aom_read_bit_buffer *rb);
 
-static int is_compound_reference_allowed(const AV1_COMMON *cm) {
-  return !frame_is_intra_only(cm);
-}
-
 static void setup_compound_reference_mode(AV1_COMMON *cm) {
   cm->comp_fwd_ref[0] = LAST_FRAME;
   cm->comp_fwd_ref[1] = LAST2_FRAME;
