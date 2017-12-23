d48a034d2db3f0e5a1dd6a0bf20311d6bd812f8e
Luc Trudeau <luc@trud.ca>
2017-12-23 00:07:29 -0500

    [CFL] SSSE3/AVX2 Versions of Sum and Subtract Average
    Includes unit tests for conformance and speed.
    
    SSSE3/CFLSubAvgTest
    4x4: C time = 1848 us, SIMD time = 1419 us (~1.3x)
    8x8: C time = 6615 us, SIMD time = 2509 us (~2.6x)
    16x16: C time = 26552 us, SIMD time = 7434 us (~3.6x)
    32x32: C time = 112318 us, SIMD time = 25562 us (~4.4x)
    
    AVX2/CFLSubAvgTest
    4x4: C time = 1861 us, SIMD time = 1515 us (~1.2x)
    8x8: C time = 6705 us, SIMD time = 2470 us (~2.7x)
    16x16: C time = 27250 us, SIMD time = 6633 us (~4.1x)
    32x32: C time = 111368 us, SIMD time = 22032 us (~5.1x)
    
    Change-Id: I44ffedc683829d2c16089854ac43d4ddb4415bcd

--- av1/common/cfl.h
+++ av1/common/cfl.h
@@ -17,6 +17,8 @@
 typedef void (*cfl_subsample_lbd_fn)(const uint8_t *input, int input_stride,
                                      int16_t *output_q3, int width, int height);
 
+typedef void (*cfl_subtract_average_fn)(int16_t *pred_buf_q3);
+
 typedef void (*cfl_predict_lbd_fn)(const int16_t *pred_buf_q3, uint8_t *dst,
                                    int dst_stride, TX_SIZE tx_size,
                                    int alpha_q3);
@@ -64,4 +66,10 @@ void cfl_luma_subsampling_440_lbd(const uint8_t *input, int input_stride,
 // TODO(ltrudeau) Remove this when 444 SIMD is added
 void cfl_luma_subsampling_444_lbd(const uint8_t *input, int input_stride,
                                   int16_t *output_q3, int width, int height);
+
+static INLINE void cfl_subtract_average_null(int16_t *pred_buf_q3) {
+  (void)pred_buf_q3;
+  assert(0);
+}
+
 #endif  // AV1_COMMON_CFL_H_
