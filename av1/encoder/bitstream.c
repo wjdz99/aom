bb491af433ab028eb59fc0bd682bece42029afa7
Debargha Mukherjee <debargha@google.com>
2017-12-26 07:10:40 -0800

    Change dq_type in frame header in new_quant expt
    dq_type now takes 4 possible values.
    
    Change-Id: I2180f632f0d7f91dc391a6e802a93a77de789228

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -3781,7 +3781,7 @@ static void write_uncompressed_header_frame(AV1_COMP *cpi,
   }
 #if CONFIG_NEW_QUANT
   if (!cm->all_lossless) {
-    aom_wb_write_bit(wb, cm->dq_type);
+    aom_wb_write_literal(wb, cm->dq_type, DQ_TYPE_BITS);
   }
 #endif  // CONFIG_NEW_QUANT
   if (!cm->all_lossless) {
@@ -4163,7 +4163,7 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
   }
 #if CONFIG_NEW_QUANT
   if (!cm->all_lossless) {
-    aom_wb_write_bit(wb, cm->dq_type);
+    aom_wb_write_literal(wb, cm->dq_type, DQ_TYPE_BITS);
   }
 #endif  // CONFIG_NEW_QUANT
   if (!cm->all_lossless) {
