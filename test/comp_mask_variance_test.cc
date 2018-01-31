a07b413a9e46aa16bddc54efb744387ce08f3216
Peng Bin <binpengsmail@gmail.com>
2018-01-31 16:22:27 +0800

    remove aom_comp_mask_upsampled_pred from rtcd
    Since aom_comp_mask_upsampled_pred just call aom_upsampled_pred and
    aom_comp_mask_pred, don't need separate c version and simd version any more.
    
    Change-Id: I1ff8bcae87d501c68a80708fd2dc6b74c6952f88

--- test/comp_mask_variance_test.cc
+++ test/comp_mask_variance_test.cc
@@ -11,6 +11,7 @@
 
 #include <cstdlib>
 #include <new>
+#include <vector>
 
 #include "./aom_config.h"
 #include "./aom_dsp_rtcd.h"
@@ -18,6 +19,7 @@
 #include "aom/aom_integer.h"
 #include "aom_mem/aom_mem.h"
 #include "aom_ports/aom_timer.h"
+#include "aom_dsp/variance.h"
 #include "aom_ports/mem.h"
 #include "av1/common/reconinter.h"
 #include "test/acm_random.h"
@@ -26,18 +28,28 @@
 #include "test/util.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 
+using std::vector;
+
 namespace AV1CompMaskVariance {
 typedef void (*comp_mask_pred_func)(uint8_t *comp_pred, const uint8_t *pred,
                                     int width, int height, const uint8_t *ref,
                                     int ref_stride, const uint8_t *mask,
                                     int mask_stride, int invert_mask);
-typedef void (*comp_mask_up_pred_func)(uint8_t *comp_pred, const uint8_t *pred,
-                                       int width, int height, int subpel_x_q3,
-                                       int subpel_y_q3, const uint8_t *ref,
-                                       int ref_stride, const uint8_t *mask,
-                                       int mask_stride, int invert_mask);
 
-typedef std::tr1::tuple<intptr_t, BLOCK_SIZE> CompMaskPredParam;
+const BLOCK_SIZE valid_bsize[] = {
+  BLOCK_8X8,   BLOCK_8X16, BLOCK_8X32,  BLOCK_16X8,  BLOCK_16X16,
+  BLOCK_16X32, BLOCK_32X8, BLOCK_32X16, BLOCK_32X32,
+};
+typedef std::tr1::tuple<comp_mask_pred_func, BLOCK_SIZE> CompMaskPredParam;
+
+vector<CompMaskPredParam> GetCompMaskPredParams(comp_mask_pred_func testImpl) {
+  vector<CompMaskPredParam> params;
+  int cnt = sizeof(valid_bsize) / sizeof(valid_bsize[0]);
+  for (int i = 0; i < cnt; ++i) {
+    params.push_back(CompMaskPredParam(testImpl, valid_bsize[i]));
+  }
+  return params;
+}
 
 class AV1CompMaskVarianceTest
     : public ::testing::TestWithParam<CompMaskPredParam> {
@@ -48,8 +60,20 @@ class AV1CompMaskVarianceTest
   void TearDown();
 
  protected:
-  void RunCheckOutput(comp_mask_pred_func test_impl, BLOCK_SIZE bsize);
+  void RunCheckOutput(comp_mask_pred_func test_impl, BLOCK_SIZE bsize, int inv);
   void RunSpeedTest(comp_mask_pred_func test_impl, BLOCK_SIZE bsize);
+  bool checkResult(int w, int h) {
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        int idx = i * w + j;
+        if (comp_pred1_[idx] != comp_pred2_[idx]) {
+          printf("%dx%d mismatch @%d(%d,%d) ", w, h, idx, i, j);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
   libaom_test::ACMRandom rnd_;
   uint8_t *comp_pred1_;
@@ -86,27 +110,20 @@ void AV1CompMaskVarianceTest::TearDown() {
 }
 
 void AV1CompMaskVarianceTest::RunCheckOutput(comp_mask_pred_func test_impl,
-                                             BLOCK_SIZE bsize) {
+                                             BLOCK_SIZE bsize, int inv) {
   const int w = block_size_wide[bsize];
   const int h = block_size_high[bsize];
 
   int wedge_types = (1 << get_wedge_bits_lookup(bsize));
   for (int wedge_index = 0; wedge_index < wedge_types; ++wedge_index) {
     const uint8_t *mask = av1_get_contiguous_soft_mask(wedge_index, 1, bsize);
-    for (int inv = 0; inv < 2; ++inv) {
-      aom_comp_mask_pred_c(comp_pred1_, pred_, w, h, ref_, MAX_SB_SIZE, mask, w,
-                           inv);
-      test_impl(comp_pred2_, pred_, w, h, ref_, MAX_SB_SIZE, mask, w, inv);
-      // check result
-      for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-          int idx = i * w + j;
-          ASSERT_EQ(comp_pred1_[idx], comp_pred2_[idx])
-              << w << "x" << h << " Pixel mismatch at index " << idx << " = ("
-              << i << ", " << j << "), wedge " << wedge_index << " inv " << inv;
-        }
-      }
-    }
+
+    aom_comp_mask_pred_c(comp_pred1_, pred_, w, h, ref_, MAX_SB_SIZE, mask, w,
+                         inv);
+    test_impl(comp_pred2_, pred_, w, h, ref_, MAX_SB_SIZE, mask, w, inv);
+
+    ASSERT_EQ(checkResult(w, h), true)
+        << " wedge " << wedge_index << " inv " << inv;
   }
 }
 
@@ -133,13 +150,15 @@ void AV1CompMaskVarianceTest::RunSpeedTest(comp_mask_pred_func test_impl,
     double time = static_cast<double>(aom_usec_timer_elapsed(&timer));
     elapsed_time[i] = 1000.0 * time / num_loops;
   }
-  printf("comp_mask_pred %3dx%-3d: %7.2f/%7.2f ns", w, h, elapsed_time[0],
+  printf("compMask %3dx%-3d: %7.2f/%7.2fns", w, h, elapsed_time[0],
          elapsed_time[1]);
-  printf(" (%3.2f)\n", elapsed_time[0] / elapsed_time[1]);
+  printf("(%3.2f)\n", elapsed_time[0] / elapsed_time[1]);
 }
 
 TEST_P(AV1CompMaskVarianceTest, CheckOutput) {
-  RunCheckOutput((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1));
+  // inv = 0, 1
+  RunCheckOutput((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1), 0);
+  RunCheckOutput((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1), 1);
 }
 
 TEST_P(AV1CompMaskVarianceTest, DISABLED_Speed) {
@@ -147,21 +166,9 @@ TEST_P(AV1CompMaskVarianceTest, DISABLED_Speed) {
 }
 
 #if HAVE_SSSE3
-const intptr_t comp_mask_pred_ssse3_f = (intptr_t)(&aom_comp_mask_pred_ssse3);
-const CompMaskPredParam kArrayCompMaskPred_ssse3[] = {
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_8X8),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_8X16),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_16X8),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_16X16),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_16X32),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_32X16),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_32X32),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_8X32),
-  testing::make_tuple(comp_mask_pred_ssse3_f, BLOCK_32X8),
-};
-
-INSTANTIATE_TEST_CASE_P(SSSE3, AV1CompMaskVarianceTest,
-                        ::testing::ValuesIn(kArrayCompMaskPred_ssse3));
+INSTANTIATE_TEST_CASE_P(
+    SSSE3, AV1CompMaskVarianceTest,
+    ::testing::ValuesIn(GetCompMaskPredParams(&aom_comp_mask_pred_ssse3)));
 #endif
 
 class AV1CompMaskUpVarianceTest : public AV1CompMaskVarianceTest {
@@ -169,16 +176,15 @@ class AV1CompMaskUpVarianceTest : public AV1CompMaskVarianceTest {
   ~AV1CompMaskUpVarianceTest();
 
  protected:
-  void RunCheckOutput(comp_mask_up_pred_func test_impl, BLOCK_SIZE bsize);
-  void RunSpeedTest(comp_mask_up_pred_func test_impl, BLOCK_SIZE bsize);
-  void RunSpeedTestSub(comp_mask_up_pred_func test_impl, BLOCK_SIZE bsize,
-                       int havSub);
+  void RunCheckOutput(comp_mask_pred_func test_impl, BLOCK_SIZE bsize, int inv);
+  void RunSpeedTest(comp_mask_pred_func test_impl, BLOCK_SIZE bsize,
+                    int havSub);
 };
 
 AV1CompMaskUpVarianceTest::~AV1CompMaskUpVarianceTest() { ; }
 
-void AV1CompMaskUpVarianceTest::RunCheckOutput(comp_mask_up_pred_func test_impl,
-                                               BLOCK_SIZE bsize) {
+void AV1CompMaskUpVarianceTest::RunCheckOutput(comp_mask_pred_func test_impl,
+                                               BLOCK_SIZE bsize, int inv) {
   const int w = block_size_wide[bsize];
   const int h = block_size_high[bsize];
   int wedge_types = (1 << get_wedge_bits_lookup(bsize));
@@ -190,28 +196,22 @@ void AV1CompMaskUpVarianceTest::RunCheckOutput(comp_mask_up_pred_func test_impl,
     for (int wedge_index = 0; wedge_index < wedge_types; ++wedge_index) {
       const uint8_t *mask = av1_get_contiguous_soft_mask(wedge_index, 1, bsize);
 
-      for (int inv = 0; inv < 2; ++inv) {
-        aom_comp_mask_upsampled_pred_c(comp_pred1_, pred_, w, h, subx, suby,
-                                       ref_, MAX_SB_SIZE, mask, w, inv);
-        test_impl(comp_pred2_, pred_, w, h, subx, suby, ref_, MAX_SB_SIZE, mask,
-                  w, inv);
-        // check result
-        for (int i = 0; i < h; ++i) {
-          for (int j = 0; j < w; ++j) {
-            int idx = i * w + j;
-            ASSERT_EQ(comp_pred1_[idx], comp_pred2_[idx])
-                << w << "x" << h << " Pixel mismatch at index " << idx << " = ("
-                << i << ", " << j << "), wedge " << wedge_index << " inv "
-                << inv << "sub (" << subx << "," << suby << ")";
-          }
-        }
-      }
+      aom_comp_mask_pred = aom_comp_mask_pred_c;  // ref
+      aom_comp_mask_upsampled_pred(comp_pred1_, pred_, w, h, subx, suby, ref_,
+                                   MAX_SB_SIZE, mask, w, inv);
+
+      aom_comp_mask_pred = test_impl;  // test
+      aom_comp_mask_upsampled_pred(comp_pred2_, pred_, w, h, subx, suby, ref_,
+                                   MAX_SB_SIZE, mask, w, inv);
+      ASSERT_EQ(checkResult(w, h), true)
+          << " wedge " << wedge_index << " inv " << inv << "sub (" << subx
+          << "," << suby << ")";
     }
   }
 }
 
-void AV1CompMaskUpVarianceTest::RunSpeedTestSub(
-    comp_mask_up_pred_func test_impl, BLOCK_SIZE bsize, int havSub) {
+void AV1CompMaskUpVarianceTest::RunSpeedTest(comp_mask_pred_func test_impl,
+                                             BLOCK_SIZE bsize, int havSub) {
   const int w = block_size_wide[bsize];
   const int h = block_size_high[bsize];
   const int subx = havSub ? 3 : 0;
@@ -222,57 +222,38 @@ void AV1CompMaskUpVarianceTest::RunSpeedTestSub(
   const uint8_t *mask = av1_get_contiguous_soft_mask(wedge_index, 1, bsize);
 
   const int num_loops = 1000000000 / (w + h);
-  comp_mask_up_pred_func funcs[2] = { &aom_comp_mask_upsampled_pred_c,
-                                      test_impl };
+  comp_mask_pred_func funcs[2] = { &aom_comp_mask_pred_c, test_impl };
   double elapsed_time[2] = { 0 };
   for (int i = 0; i < 2; ++i) {
     aom_usec_timer timer;
     aom_usec_timer_start(&timer);
-
-    comp_mask_up_pred_func func = funcs[i];
+    aom_comp_mask_pred = funcs[i];
     for (int j = 0; j < num_loops; ++j) {
-      func(comp_pred1_, pred_, w, h, subx, suby, ref_, MAX_SB_SIZE, mask, w, 0);
+      aom_comp_mask_upsampled_pred(comp_pred1_, pred_, w, h, subx, suby, ref_,
+                                   MAX_SB_SIZE, mask, w, 0);
     }
-
     aom_usec_timer_mark(&timer);
     double time = static_cast<double>(aom_usec_timer_elapsed(&timer));
     elapsed_time[i] = 1000.0 * time / num_loops;
   }
-  printf("CompMask[%d] %3dx%-3d:%7.2f/%7.2fns", havSub, w, h, elapsed_time[0],
+  printf("CompMaskUp[%d] %3dx%-3d:%7.2f/%7.2fns", havSub, w, h, elapsed_time[0],
          elapsed_time[1]);
   printf("(%3.2f)\n", elapsed_time[0] / elapsed_time[1]);
 }
 
-void AV1CompMaskUpVarianceTest::RunSpeedTest(comp_mask_up_pred_func test_impl,
-                                             BLOCK_SIZE bsize) {
-  RunSpeedTestSub(test_impl, bsize, 0);  // could skip upsample
-  RunSpeedTestSub(test_impl, bsize, 1);
-}
-
 TEST_P(AV1CompMaskUpVarianceTest, CheckOutput) {
-  RunCheckOutput((comp_mask_up_pred_func)GET_PARAM(0), GET_PARAM(1));
+  // inv mask = 0, 1
+  RunCheckOutput((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1), 0);
+  RunCheckOutput((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1), 1);
 }
 
 TEST_P(AV1CompMaskUpVarianceTest, DISABLED_Speed) {
-  RunSpeedTest((comp_mask_up_pred_func)GET_PARAM(0), GET_PARAM(1));
+  RunSpeedTest((comp_mask_pred_func)GET_PARAM(0), GET_PARAM(1), 1);
 }
 
 #if HAVE_SSSE3
-const intptr_t comp_mask_up_pred_ssse3_f =
-    (intptr_t)(&aom_comp_mask_upsampled_pred_ssse3);
-const CompMaskPredParam kArrayCompMaskUpPred_ssse3[] = {
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_8X8),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_8X16),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_16X8),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_16X16),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_16X32),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_32X16),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_32X32),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_8X32),
-  testing::make_tuple(comp_mask_up_pred_ssse3_f, BLOCK_32X8),
-};
-
-INSTANTIATE_TEST_CASE_P(SSSE3, AV1CompMaskUpVarianceTest,
-                        ::testing::ValuesIn(kArrayCompMaskUpPred_ssse3));
+INSTANTIATE_TEST_CASE_P(
+    SSSE3, AV1CompMaskUpVarianceTest,
+    ::testing::ValuesIn(GetCompMaskPredParams(&aom_comp_mask_pred_ssse3)));
 #endif
 }  // namespace AV1CompMaskVariance
