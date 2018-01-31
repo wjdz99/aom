7468fdbd7dd8d9c9e719ba8235f67ea67967a9b6
Yushin Cho <ycho@mozilla.com>
2018-01-31 15:46:19 -0800

    Remove Cyclic Refresh AQ mode
    Remove aq-mode=3, i.e. Cyclic Refresh mode of Adaptive Quantization.
    
    Change-Id: I9b7353c68856d2926375c2cc47a382324b4a2bde

--- aomenc.c
+++ aomenc.c
@@ -498,12 +498,12 @@ static const arg_def_t frame_parallel_decoding =
 static const arg_def_t aq_mode = ARG_DEF(
     NULL, "aq-mode", 1,
     "Adaptive quantization mode (0: off (default), 1: variance 2: complexity, "
-    "3: cyclic refresh, 4: delta quant)");
+    "3: delta quant)");
 #else
 static const arg_def_t aq_mode = ARG_DEF(
     NULL, "aq-mode", 1,
-    "Adaptive quantization mode (0: off (default), 1: variance 2: complexity, "
-    "3: cyclic refresh)");
+    "Adaptive quantization mode (0: off (default), 1: variance 2: complexity"
+    ")");
 #endif
 #if CONFIG_EXT_DELTA_Q
 static const arg_def_t deltaq_mode = ARG_DEF(
