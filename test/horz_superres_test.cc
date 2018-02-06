e2ae897246442c15a1f51c5c2ee9659679ccdae4
Debargha Mukherjee <debargha@google.com>
2018-02-06 07:40:39 +0000

    Modify first pass stats handling
    Makes the first pass stats handling similar to VP9 tip of tree,
    and tunes parameters.
    
    STATS_CHANGED expected.
    
    Change-Id: Ifdc7324b634e6fde6f82740fbb1d088ac9d9d1f3

--- test/horz_superres_test.cc
+++ test/horz_superres_test.cc
@@ -31,7 +31,7 @@ using ::testing::tuple;
 const int kBitrate = 40;
 
 // PSNR thresholds found by experiment
-const double kPSNRThresholds[] = { 27.45, 29, 21.7 };
+const double kPSNRThresholds[] = { 26.5, 28.5, 21.0 };
 
 typedef struct {
   const char *filename;
