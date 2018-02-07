605b425d79cf8b3b90d284725c332ef3036dde5f
Yunqing Wang <yunqingwang@google.com>
2018-02-07 15:12:38 -0800

    Modify reference MV function prototypes
    Change-Id: I0d645983233753861d940b603d13957576ab51fb

--- av1/decoder/decodemv.c
+++ av1/decoder/decodemv.c
@@ -944,18 +944,18 @@ static void read_intrabc_info(AV1_COMMON *const cm, MACROBLOCKD *const xd,
     mbmi->interp_filters = av1_broadcast_interp_filter(BILINEAR);
 
     int16_t inter_mode_ctx[MODE_CTX_REF_FRAMES];
-    int_mv ref_mvs[MAX_MV_REF_CANDIDATES];
+    int_mv ref_mvs[1][MAX_MV_REF_CANDIDATES];
 
-    av1_find_mv_refs(cm, xd, mi, INTRA_FRAME, &xd->ref_mv_count[INTRA_FRAME],
-                     xd->ref_mv_stack[INTRA_FRAME], NULL, ref_mvs, mi_row,
-                     mi_col, NULL, NULL, inter_mode_ctx);
+    av1_find_mv_refs(cm, xd, mi, INTRA_FRAME, xd->ref_mv_count,
+                     xd->ref_mv_stack, NULL, ref_mvs, mi_row, mi_col, NULL,
+                     NULL, inter_mode_ctx, 0);
 
     int_mv nearestmv, nearmv;
 
 #if CONFIG_AMVR
-    av1_find_best_ref_mvs(0, ref_mvs, &nearestmv, &nearmv, 0);
+    av1_find_best_ref_mvs(0, ref_mvs[0], &nearestmv, &nearmv, 0);
 #else
-    av1_find_best_ref_mvs(0, ref_mvs, &nearestmv, &nearmv);
+    av1_find_best_ref_mvs(0, ref_mvs[0], &nearestmv, &nearmv);
 #endif
     int_mv dv_ref = nearestmv.as_int == 0 ? nearmv : nearestmv;
     if (dv_ref.as_int == 0)
@@ -1708,21 +1708,28 @@ static void read_inter_block_mode_info(AV1Decoder *const pbi,
   read_ref_frames(cm, xd, r, mbmi->segment_id, mbmi->ref_frame);
   const int is_compound = has_second_ref(mbmi);
 
-  for (int ref = 0; ref < 1 + is_compound; ++ref) {
-    MV_REFERENCE_FRAME frame = mbmi->ref_frame[ref];
+  if (!is_compound)
+    for (int ref = 0; ref < 1 + is_compound; ++ref) {
+      MV_REFERENCE_FRAME frame = mbmi->ref_frame[ref];
 
-    av1_find_mv_refs(cm, xd, mi, frame, &xd->ref_mv_count[frame],
-                     xd->ref_mv_stack[frame], compound_inter_mode_ctx,
-                     ref_mvs[frame], mi_row, mi_col, fpm_sync, (void *)pbi,
-                     inter_mode_ctx);
-  }
+      av1_find_mv_refs(cm, xd, mi, frame, xd->ref_mv_count, xd->ref_mv_stack,
+                       compound_inter_mode_ctx, ref_mvs, mi_row, mi_col,
+                       fpm_sync, (void *)pbi, inter_mode_ctx, 0);
+    }
 
   if (is_compound) {
+    for (int ref = 0; ref < 1 + is_compound; ++ref) {
+      MV_REFERENCE_FRAME frame = mbmi->ref_frame[ref];
+
+      av1_find_mv_refs(cm, xd, mi, frame, xd->ref_mv_count, xd->ref_mv_stack,
+                       compound_inter_mode_ctx, ref_mvs, mi_row, mi_col,
+                       fpm_sync, (void *)pbi, inter_mode_ctx, 0);
+    }
+
     MV_REFERENCE_FRAME ref_frame = av1_ref_frame_type(mbmi->ref_frame);
-    av1_find_mv_refs(cm, xd, mi, ref_frame, &xd->ref_mv_count[ref_frame],
-                     xd->ref_mv_stack[ref_frame], compound_inter_mode_ctx,
-                     ref_mvs[ref_frame], mi_row, mi_col, fpm_sync, (void *)pbi,
-                     inter_mode_ctx);
+    av1_find_mv_refs(cm, xd, mi, ref_frame, xd->ref_mv_count, xd->ref_mv_stack,
+                     compound_inter_mode_ctx, ref_mvs, mi_row, mi_col, fpm_sync,
+                     (void *)pbi, inter_mode_ctx, 0);
 
     if (xd->ref_mv_count[ref_frame] < 2) {
       MV_REFERENCE_FRAME rf[2];
