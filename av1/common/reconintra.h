a70afb0ee7cb790406949cc66c6fb45914564016
Yue Chen <yuec@google.com>
2018-02-07 18:20:55 -0800

    Add sse4 implementation of lbd filter_intra modes
    Speed up filter_intra_predictor() by 2.4x
    Unit tests are also added.
    
    Change-Id: I675fc07b0ec369aa3884bd8ebcd83d5c0f00e9cd

--- av1/common/reconintra.h
+++ av1/common/reconintra.h
@@ -72,6 +72,10 @@ static INLINE int av1_allow_intrabc(const AV1_COMMON *const cm) {
 }
 #endif  // CONFIG_INTRABC
 
+#if CONFIG_FILTER_INTRA
+extern const int16_t filter_intra_taps[FILTER_INTRA_MODES][8][8];
+#endif
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
