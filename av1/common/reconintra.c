75d07bd03b504631ea1fbdc9269316e3b911bccb
Hui Su <huisu@google.com>
2018-02-10 22:59:15 -0800

    [NORMATIVE]Fix has_bottom_left() for 128*x blocks
    BUG=aomedia:1309
    
    Change-Id: Icc0e607e2d16c9041a9b8bab1b267da21ebbddd0

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -512,8 +512,23 @@ static const uint8_t *get_has_bl_table(PARTITION_TYPE partition,
 static int has_bottom_left(const AV1_COMMON *cm, BLOCK_SIZE bsize, int mi_row,
                            int mi_col, int bottom_available, int left_available,
                            PARTITION_TYPE partition, TX_SIZE txsz, int row_off,
-                           int col_off, int ss_y) {
+                           int col_off, int ss_x, int ss_y) {
   if (!bottom_available || !left_available) return 0;
+
+#if CONFIG_EXT_PARTITION
+  // Special case for 128X* blocks, when col_off is half the block width.
+  if (block_size_wide[bsize] > block_size_wide[BLOCK_64X64] && col_off > 0) {
+    const int plane_bw_unit_64 = mi_size_wide[BLOCK_64X64] >> ss_x;
+    const int col_off_64 = col_off % plane_bw_unit_64;
+    if (col_off_64 == 0) {
+      const int plane_bh_unit =
+          AOMMIN(mi_size_high[bsize], mi_size_high[BLOCK_64X64]) >> ss_y;
+      return row_off + tx_size_high_unit[txsz] < plane_bh_unit;
+    }
+  }
+#else
+  (void)ss_x;
+#endif  // CONFIG_EXT_PARTITION
 
   if (col_off > 0) {
     // Bottom-left pixels are in the bottom-left block, which is not available.
@@ -2121,7 +2136,8 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
                     partition, tx_size, row_off, col_off, pd->subsampling_x);
   const int have_bottom_left =
       has_bottom_left(cm, bsize, mi_row, mi_col, bottom_available, have_left,
-                      partition, tx_size, row_off, col_off, pd->subsampling_y);
+                      partition, tx_size, row_off, col_off,
+                      pd->subsampling_x, pd->subsampling_y);
 
   if (mbmi->palette_mode_info.palette_size[plane != 0] > 0) {
     const int stride = wpx;
