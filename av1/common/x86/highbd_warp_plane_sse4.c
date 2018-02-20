ae6d85b68b3b053aebb14db56e1bf6205ede7aef
Imdad Sardharwalla <imdad.sardharwalla@argondesign.com>
2018-02-20 14:25:18 +0000

    Fix and clean up warp filter SIMD code
    - Fixed SSE4.1 implementation of the highbd warp filter.
    - Removed the SSE2 and SSSE3 implementations of both the lowbd and highbd
      versions of the warp filter, as these will not work with CONFIG_JNT_COMP=1.
      (There are now only SSE4.1 implementations)
    - The warp filter tests have been correspondingly updated.
    
    Change-Id: I2bc1ebb69f2ed876803902908586c3848c03c11e

--- av1/common/x86/highbd_warp_plane_sse4.c
+++ av1/common/x86/highbd_warp_plane_sse4.c
@@ -39,6 +39,8 @@ void av1_highbd_warp_affine_sse4_1(const int32_t *mat, const uint16_t *ref,
     conv_params->do_post_rounding = 1;
   }
   assert(FILTER_BITS == FILTER_BITS);
+  assert(!(bd == 12 && reduce_bits_horiz < 5));
+
 #if CONFIG_JNT_COMP
   const int w0 = conv_params->fwd_offset;
   const int w1 = conv_params->bck_offset;
@@ -311,7 +313,7 @@ void av1_highbd_warp_affine_sse4_1(const int32_t *mat, const uint16_t *ref,
               ((1 << (conv_params->round_1)) >> 1));
           res_lo = _mm_add_epi32(res_lo, round_const);
           res_lo =
-              _mm_srl_epi32(res_lo, _mm_cvtsi32_si128(conv_params->round_1));
+              _mm_sra_epi32(res_lo, _mm_cvtsi32_si128(conv_params->round_1));
 #if CONFIG_JNT_COMP
           if (conv_params->use_jnt_comp_avg) {
             if (comp_avg) {
@@ -334,7 +336,7 @@ void av1_highbd_warp_affine_sse4_1(const int32_t *mat, const uint16_t *ref,
           if (p_width > 4) {
             res_hi = _mm_add_epi32(res_hi, round_const);
             res_hi =
-                _mm_srl_epi32(res_hi, _mm_cvtsi32_si128(conv_params->round_1));
+                _mm_sra_epi32(res_hi, _mm_cvtsi32_si128(conv_params->round_1));
 
 #if CONFIG_JNT_COMP
             if (conv_params->use_jnt_comp_avg) {
