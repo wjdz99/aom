96961bd3e67c907aed0fc01861d9cd485fec9b42
Hui Su <huisu@google.com>
2018-02-20 22:08:20 -0800

    Turn on q-adapt-probs by default
    Change-Id: I6cbf928fe2bfc634cecbfce8f0942e02d5a17db1

--- test/decode_to_md5.sh
+++ test/decode_to_md5.sh
@@ -57,7 +57,7 @@ decode_to_md5() {
 
 decode_to_md5_av1() {
   # expected MD5 sum for the last frame.
-  local expected_md5="155facd9ade570f14502bbfcaaeeca06"
+  local expected_md5="0f0acafc3fad18535985621b586c061e"
   local file="${AV1_IVF_FILE}"
 
   # TODO(urvang): Check in the encoded file (like libvpx does) to avoid
