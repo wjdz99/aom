06b1904916dec23c1729f38d73bde685b201057c
Sebastien Alaiwan <sebastien.alaiwan@allegrodvt.com>
2018-02-21 16:30:28 +0100

    Remove experimental flag of OBU
    This experiment has been adopted, we can simplify the code
    by dropping the associated preprocessor conditionals.
    
    Change-Id: I520dbba434745b0655942b25c49e16ff076ab741

--- av1/av1.cmake
+++ av1/av1.cmake
@@ -81,13 +81,9 @@ set(AOM_AV1_DECODER_SOURCES
     "${AOM_ROOT}/av1/decoder/detokenize.c"
     "${AOM_ROOT}/av1/decoder/detokenize.h"
     "${AOM_ROOT}/av1/decoder/dthread.c"
-    "${AOM_ROOT}/av1/decoder/dthread.h")
-
-if (CONFIG_OBU)
-  list(APPEND AOM_AV1_DECODER_SOURCES
-       "${AOM_ROOT}/av1/decoder/obu.c"
-       "${AOM_ROOT}/av1/decoder/obu.h")
-endif()
+    "${AOM_ROOT}/av1/decoder/dthread.h"
+    "${AOM_ROOT}/av1/decoder/obu.c"
+    "${AOM_ROOT}/av1/decoder/obu.h")
 
 set(AOM_AV1_ENCODER_SOURCES
     "${AOM_ROOT}/av1/av1_cx_iface.c"
