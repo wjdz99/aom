f000e562bed118a677435ead5519fd2c841922a4
Jingning Han <jingning@google.com>
2018-02-21 11:28:59 -0800

    [NORMATIVE] Unify tx size context model for inter / intra blocks
    Unify the transform block size coding context model for intra
    block with that for inter block.
    
    Tested on 30 frames in speed 1. The coding performance changes are
    
    lowres: PSNR 0.019%, SSIM -0.125%
    lowres_cam: PSNR -0.045%, SSIM -0.25%
    midres: PSNR -0.042%, SSIM -0.089%
    
    BUG=aomedia:1057
    
    Change-Id: If52825a7c8355f2c4baf1854cecb29ee61579200

--- test/decode_to_md5.sh
+++ test/decode_to_md5.sh
@@ -57,7 +57,7 @@ decode_to_md5() {
 
 decode_to_md5_av1() {
   # expected MD5 sum for the last frame.
-  local expected_md5="6ebec9f224ad2ad53910c05261b04ee3"
+  local expected_md5="da271a5bca88563f6c856757a56e56ff"
   local file="${AV1_IVF_FILE}"
 
   # TODO(urvang): Check in the encoded file (like libvpx does) to avoid
