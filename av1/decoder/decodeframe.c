8c7ee46149a2e444b3052bc2d94bf2b2e86a845e
Joe Young <joeyoung@google.com>
2018-02-23 09:14:09 -0800

    [intra-edge2] send header flag every frame
    BUG=aomedia:1424
    
    Change-Id: Ia669ac6f17d337abe87934dadf1fd83b8afc38a9

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -2733,9 +2733,7 @@ static int read_uncompressed_header(AV1Decoder *pbi,
 #if CONFIG_REFERENCE_BUFFER
 
 #if CONFIG_INTRA_EDGE2
-  if (frame_is_intra_only(cm)) {
-    cm->disable_intra_edge_filter = aom_rb_read_bit(rb);
-  }
+  cm->disable_intra_edge_filter = aom_rb_read_bit(rb);
 #endif  // CONFIG_INTRA_EDGE2
 
   if (cm->seq_params.force_screen_content_tools == 2) {
