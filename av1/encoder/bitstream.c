20093561e59881b3a630ff840818e73ebb31a90e
Jingning Han <jingning@google.com>
2018-02-27 10:46:42 -0800

    [NORMATIVE] Update txfm context using tx_size in intra mode
    When a coding block is coded in the intra mode, always update its
    tx size regardless if it is coded as skip or not.
    
    BUG=aomedia:1066
    
    Change-Id: I807bb5a5c44609adeaf54fe4e1bc805477751332

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1771,10 +1771,11 @@ static void write_modes_b(AV1_COMP *cpi, const TileInfo *const tile,
           write_tx_size_vartx(cm, xd, mbmi, max_tx_size, 0, idy, idx, w);
     } else {
       write_selected_tx_size(cm, xd, w);
-      set_txfm_ctxs(mbmi->tx_size, xd->n8_w, xd->n8_h, skip, xd);
+      set_txfm_ctxs(mbmi->tx_size, xd->n8_w, xd->n8_h, 0, xd);
     }
   } else {
-    set_txfm_ctxs(mbmi->tx_size, xd->n8_w, xd->n8_h, skip, xd);
+    set_txfm_ctxs(mbmi->tx_size, xd->n8_w, xd->n8_h,
+                  skip && is_inter_block(mbmi), xd);
   }
 
 #if CONFIG_FILTER_INTRA
