77f5d0335d3d46268f635f4f1c73b46295c38751
Jingning Han <jingning@google.com>
2018-02-27 11:36:25 -0800

    Fix overriding skip flag for intra blocks
    When a transform block selects the skip mode, avoid overriding the
    coding block skip flag for intra blocks.
    
    Change-Id: Ib28b319ae7aa7b37dfba7971c81b9f96b81a6777

--- test/decode_to_md5.sh
+++ test/decode_to_md5.sh
@@ -57,7 +57,7 @@ decode_to_md5() {
 
 decode_to_md5_av1() {
   # expected MD5 sum for the last frame.
-  local expected_md5="08d6acb77b20a408237fd42c7575e67d"
+  local expected_md5="45caca9b40d5d76b411179bbe0aaa427"
   local file="${AV1_IVF_FILE}"
 
   # TODO(urvang): Check in the encoded file (like libvpx does) to avoid
