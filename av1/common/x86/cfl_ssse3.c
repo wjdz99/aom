49e20eb46a67d9d3ba7f3d13efb48345ddcf5e50
Luc Trudeau <luc@trud.ca>
2018-02-28 11:57:14 -0500

    [CFL] SSSE3 Versions of luma_subsampling_420_hbd
    We include unit tests for conformance and speed.
    
    SSSE3/CFLSubsampleHBDSpeedTest (i7-7820X)
    4x4: C time = 96 us, SIMD time = 56 us (~1.7x)
    8x8: C time = 319 us, SIMD time = 96 us (~3.3x)
    16x16: C time = 1637 us, SIMD time = 243 us (~6.7x)
    32x32: C time = 8057 us, SIMD time = 890 us (~9.1x)
    
    Change-Id: Ie49c133e45c3a5d216519d46e27eb7ce4553faa7

--- av1/common/x86/cfl_ssse3.c
+++ av1/common/x86/cfl_ssse3.c
@@ -171,6 +171,75 @@ static INLINE void cfl_luma_subsampling_444_lbd_ssse3(const uint8_t *input,
   } while (pred_buf_m128i < end);
 }
 
+/**
+ * Adds 4 pixels (in a 2x2 grid) and multiplies them by 2. Resulting in a more
+ * precise version of a box filter 4:2:0 pixel subsampling in Q3.
+ *
+ * The CfL prediction buffer is always of size CFL_BUF_SQUARE. However, the
+ * active area is specified using width and height.
+ *
+ * Note: We don't need to worry about going over the active area, as long as we
+ * stay inside the CfL prediction buffer.
+ */
+static INLINE void cfl_luma_subsampling_420_hbd_ssse3(const uint16_t *input,
+                                                      int input_stride,
+                                                      int16_t *pred_buf_q3,
+                                                      int width, int height) {
+  const int16_t *end = pred_buf_q3 + (height >> 1) * CFL_BUF_LINE;
+  const int luma_stride = input_stride << 1;
+
+  __m128i top, bot, top_1, bot_1, top_2, bot_2, top_3, bot_3, next_sum;
+  do {
+    if (width == 4) {
+      top = _mm_loadl_epi64((__m128i *)input);
+      bot = _mm_loadl_epi64((__m128i *)(input + input_stride));
+    } else {
+      top = _mm_loadu_si128((__m128i *)input);
+      bot = _mm_loadu_si128((__m128i *)(input + input_stride));
+      if (width >= 16) {
+        top_1 = _mm_loadu_si128((__m128i *)(input + 8));
+        bot_1 = _mm_loadu_si128((__m128i *)(input + 8 + input_stride));
+      }
+      if (width == 32) {
+        top_2 = _mm_loadu_si128((__m128i *)(input + 16));
+        bot_2 = _mm_loadu_si128((__m128i *)(input + 16 + input_stride));
+        top_3 = _mm_loadu_si128((__m128i *)(input + 24));
+        bot_3 = _mm_loadu_si128((__m128i *)(input + 24 + input_stride));
+      }
+    }
+    __m128i sum = _mm_add_epi16(top, bot);
+    if (width >= 16) {
+      sum = _mm_hadd_epi16(sum, _mm_add_epi16(top_1, bot_1));
+      if (width == 32) {
+        const __m128i sum_2 = _mm_add_epi16(top_2, bot_2);
+        const __m128i sum_3 = _mm_add_epi16(top_3, bot_3);
+        next_sum = _mm_hadd_epi16(sum_2, sum_3);
+        next_sum = _mm_add_epi16(next_sum, next_sum);
+      }
+    } else {
+      sum = _mm_hadd_epi16(sum, sum);
+    }
+    sum = _mm_add_epi16(sum, sum);
+
+    if (width == 4) {
+      *((int *)pred_buf_q3) = _mm_cvtsi128_si32(sum);
+    } else if (width == 8) {
+      _mm_storel_epi64((__m128i *)pred_buf_q3, sum);
+    } else {
+      _mm_storeu_si128((__m128i *)pred_buf_q3, sum);
+      if (width == 32) {
+        _mm_storeu_si128((__m128i *)(pred_buf_q3 + 8), next_sum);
+      }
+    }
+
+    input += luma_stride;
+  } while ((pred_buf_q3 += CFL_BUF_LINE) < end);
+}
+
+// TODO(ltrudeau) Move this function into CFL_GET_SUBSAMPLE_FUNCTION when AVX2
+// and NEON versions are ready.
+CFL_SUBSAMPLE_FUNCTIONS(ssse3, 420, hbd)
+
 CFL_GET_SUBSAMPLE_FUNCTION(ssse3)
 
 static INLINE __m128i predict_unclipped(const __m128i *input, __m128i alpha_q12,
