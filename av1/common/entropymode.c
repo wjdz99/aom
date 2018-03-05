3f05e172245a0aa814fdf71e4741a94cc17eb6f3
Tarek AMARA <amatarek@justin.tv>
2018-03-05 18:40:18 -0500

    [NORMATIVE] S_FRAME handling update
    Add sframe implementation in the encoder and fix sframe in the decoder
    
    BUG=aomedia:1459
    
    Change-Id: I53b6acde8f8ea24fee9bfc3a33dc53255ae83a57

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -1174,7 +1174,8 @@ void av1_setup_frame_contexts(AV1_COMMON *cm) {
   cm->frame_contexts[FRAME_CONTEXT_DEFAULTS] = *cm->fc;
 #else
   if (cm->frame_type == KEY_FRAME || cm->error_resilient_mode ||
-      cm->reset_frame_context == RESET_FRAME_CONTEXT_ALL) {
+      cm->reset_frame_context == RESET_FRAME_CONTEXT_ALL ||
+      cm->frame_type == S_FRAME) {
     // Reset all frame contexts.
     for (int i = 0; i < FRAME_CONTEXTS; ++i) cm->frame_contexts[i] = *cm->fc;
   } else if (cm->reset_frame_context == RESET_FRAME_CONTEXT_CURRENT) {
