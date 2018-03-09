b60351fb560158158bc86d0a23e5a6086e711945
Yue Chen <yuec@google.com>
2018-03-09 12:46:58 -0800

    Silence alignment warnings in filter_intra sse4 implementation
    memcpy() will handle all cases by making necessary optimizations.
    
    Change-Id: If2a8ffb48f7412ac68f224dfdd0121500fa386a0

--- av1/common/x86/filterintra_sse4.c
+++ av1/common/x86/filterintra_sse4.c
@@ -10,6 +10,7 @@
  */
 
 #include <smmintrin.h>
+#include <assert.h>
 #include "./av1_rtcd.h"
 #include "aom_dsp/x86/synonyms.h"
 #include "av1/common/enums.h"
@@ -18,7 +19,6 @@
 void av1_filter_intra_predictor_sse4_1(uint8_t *dst, ptrdiff_t stride,
                                        TX_SIZE tx_size, const uint8_t *above,
                                        const uint8_t *left, int mode) {
-  int r, c;
   uint8_t buffer[33][33];
   const int bw = tx_size_wide[tx_size];
   const int bh = tx_size_high[tx_size];
@@ -26,10 +26,9 @@ void av1_filter_intra_predictor_sse4_1(uint8_t *dst, ptrdiff_t stride,
   assert(bw <= 32 && bh <= 32);
 
   // The initialization is just for silencing Jenkins static analysis warnings
-  for (r = 0; r < bh + 1; ++r)
-    memset(buffer[r], 0, (bw + 1) * sizeof(buffer[0][0]));
+  for (int r = 0; r < 33; ++r) memset(buffer[r], 0, 33 * sizeof(buffer[0][0]));
 
-  for (r = 0; r < bh; ++r) buffer[r + 1][0] = left[r];
+  for (int r = 0; r < bh; ++r) buffer[r + 1][0] = left[r];
   memcpy(buffer[0], &above[-1], (bw + 1) * sizeof(uint8_t));
 
   const __m128i f1f0 = xx_load_128(av1_filter_intra_taps[mode][0]);
@@ -39,8 +38,8 @@ void av1_filter_intra_predictor_sse4_1(uint8_t *dst, ptrdiff_t stride,
   const __m128i filter_intra_scale_bits =
       _mm_set1_epi16(1 << (15 - FILTER_INTRA_SCALE_BITS));
 
-  for (r = 1; r < bh + 1; r += 2) {
-    for (c = 1; c < bw + 1; c += 4) {
+  for (int r = 1; r < bh + 1; r += 2) {
+    for (int c = 1; c < bw + 1; c += 4) {
       DECLARE_ALIGNED(16, uint8_t, p[8]);
       memcpy(p, &buffer[r - 1][c - 1], 5 * sizeof(uint8_t));
       p[5] = buffer[r][c - 1];
@@ -61,12 +60,12 @@ void av1_filter_intra_predictor_sse4_1(uint8_t *dst, ptrdiff_t stride,
       const __m128i out_r = _mm_packus_epi16(round_w, round_w);
       const __m128i out_r1 = _mm_srli_si128(out_r, 4);
       // Storing
-      xx_storel_32(&buffer[r][c], out_r);
-      xx_storel_32(&buffer[r + 1][c], out_r1);
+      memcpy(&buffer[r][c], out_r, 4 * sizeof(buffer[0][0]));
+      memcpy(&buffer[r + 1][c], out_r1, 4 * sizeof(buffer[0][0]));
     }
   }
 
-  for (r = 0; r < bh; ++r) {
+  for (int r = 0; r < bh; ++r) {
     memcpy(dst, &buffer[r + 1][1], bw * sizeof(uint8_t));
     dst += stride;
   }
