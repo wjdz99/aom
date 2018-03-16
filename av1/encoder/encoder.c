d7abb6049c6b2fe27249b0387a09e802683f98dc
Cheng Chen <chengchen@google.com>
2018-03-15 19:11:03 -0700

    [Normative] Move global loopfilter info to ref buf
    Let loop filer ref_delta, mode_deltas, sharpness_level
    associated with corresponding reference buffer, such
    that for error resilient frame or droppable frame, the
    decoder could correctly decode contexts.
    
    BUG=aomedia:1581
    
    Change-Id: I3c297ec2b36b32c89b8ac783348c23d57d27d4d0

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -369,6 +369,16 @@ static void setup_frame(AV1_COMP *cpi) {
   else
     cm->last_frame_seg_map = NULL;
   cm->current_frame_seg_map = cm->cur_frame->seg_map;
+
+  if (cm->allow_intrabc && NO_FILTER_FOR_IBC) {
+    av1_set_default_ref_deltas(cm->lf.ref_deltas);
+    av1_set_default_mode_deltas(cm->lf.mode_deltas);
+  } else if (cm->prev_frame) {
+    memcpy(cm->lf.ref_deltas, cm->prev_frame->ref_deltas, TOTAL_REFS_PER_FRAME);
+    memcpy(cm->lf.mode_deltas, cm->prev_frame->mode_deltas, MAX_MODE_LF_DELTAS);
+  }
+  memcpy(cm->cur_frame->ref_deltas, cm->lf.ref_deltas, TOTAL_REFS_PER_FRAME);
+  memcpy(cm->cur_frame->mode_deltas, cm->lf.mode_deltas, MAX_MODE_LF_DELTAS);
 }
 
 static void enc_setup_mi(AV1_COMMON *cm) {
@@ -558,8 +568,22 @@ static void save_coding_context(AV1_COMP *cpi) {
   av1_copy(cc->nmv_costs, cpi->nmv_costs);
   av1_copy(cc->nmv_costs_hp, cpi->nmv_costs_hp);
 
-  av1_copy(cc->last_ref_lf_deltas, cm->lf.last_ref_deltas);
-  av1_copy(cc->last_mode_lf_deltas, cm->lf.last_mode_deltas);
+  const int prime_idx = cm->primary_ref_frame;
+  const int buf_idx =
+      prime_idx == PRIMARY_REF_NONE ? -1 : cm->frame_refs[prime_idx].idx;
+  int8_t last_ref_deltas[TOTAL_REFS_PER_FRAME];
+  int8_t last_mode_deltas[MAX_MODE_LF_DELTAS];
+  if (prime_idx == PRIMARY_REF_NONE || buf_idx < 0) {
+    av1_set_default_ref_deltas(last_ref_deltas);
+    av1_set_default_mode_deltas(last_mode_deltas);
+  } else {
+    memcpy(last_ref_deltas, cm->buffer_pool->frame_bufs[buf_idx].ref_deltas,
+           TOTAL_REFS_PER_FRAME);
+    memcpy(last_mode_deltas, cm->buffer_pool->frame_bufs[buf_idx].mode_deltas,
+           MAX_MODE_LF_DELTAS);
+  }
+  av1_copy(cc->last_ref_lf_deltas, last_ref_deltas);
+  av1_copy(cc->last_mode_lf_deltas, last_mode_deltas);
 
   cc->fc = *cm->fc;
 }
@@ -574,8 +598,15 @@ static void restore_coding_context(AV1_COMP *cpi) {
   av1_copy(cpi->nmv_costs, cc->nmv_costs);
   av1_copy(cpi->nmv_costs_hp, cc->nmv_costs_hp);
 
-  av1_copy(cm->lf.last_ref_deltas, cc->last_ref_lf_deltas);
-  av1_copy(cm->lf.last_mode_deltas, cc->last_mode_lf_deltas);
+  const int prime_idx = cm->primary_ref_frame;
+  const int buf_idx =
+      prime_idx == PRIMARY_REF_NONE ? -1 : cm->frame_refs[prime_idx].idx;
+  if (prime_idx != PRIMARY_REF_NONE && buf_idx >= 0) {
+    memcpy(cm->buffer_pool->frame_bufs[buf_idx].ref_deltas,
+           cc->last_ref_lf_deltas, TOTAL_REFS_PER_FRAME);
+    memcpy(cm->buffer_pool->frame_bufs[buf_idx].mode_deltas,
+           cc->last_mode_lf_deltas, MAX_MODE_LF_DELTAS);
+  }
 
   *cm->fc = cc->fc;
 }
