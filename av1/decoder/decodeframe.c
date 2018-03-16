3a129b855743ce0696c3772eaa6d05ed59abad26
Cheng Chen <chengchen@google.com>
2018-03-15 19:11:03 -0700

    [Normative] Move global loopfilter info to ref buf
    Let loop filer ref_delta, mode_deltas, sharpness_level
    associated with corresponding reference buffer, such
    that for error resilient frame or droppable frame, the
    decoder could correctly decode contexts.
    
    BUG=aomedia:1581
    
    Change-Id: I3c297ec2b36b32c89b8ac783348c23d57d27d4d0

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -1101,7 +1101,12 @@ static void loop_restoration_read_sb_coeffs(const AV1_COMMON *const cm,
 static void setup_loopfilter(AV1_COMMON *cm, struct aom_read_bit_buffer *rb) {
   assert(!cm->all_lossless);
   const int num_planes = av1_num_planes(cm);
-  if (cm->allow_intrabc && NO_FILTER_FOR_IBC) return;
+  if (cm->allow_intrabc && NO_FILTER_FOR_IBC) {
+    // write deltas to frame buffer
+    memcpy(cm->cur_frame->ref_deltas, lf->ref_deltas, TOTAL_REFS_PER_FRAME);
+    memcpy(cm->cur_frame->mode_deltas, lf->mode_deltas, MAX_MODE_LF_DELTAS);
+    return;
+  }
   struct loopfilter *lf = &cm->lf;
   lf->filter_level[0] = aom_rb_read_literal(rb, 6);
   lf->filter_level[1] = aom_rb_read_literal(rb, 6);
@@ -1130,6 +1135,10 @@ static void setup_loopfilter(AV1_COMMON *cm, struct aom_read_bit_buffer *rb) {
           lf->mode_deltas[i] = aom_rb_read_inv_signed_literal(rb, 6);
     }
   }
+
+  // write deltas to frame buffer
+  memcpy(cm->cur_frame->ref_deltas, lf->ref_deltas, TOTAL_REFS_PER_FRAME);
+  memcpy(cm->cur_frame->mode_deltas, lf->mode_deltas, MAX_MODE_LF_DELTAS);
 }
 
 static void setup_cdef(AV1_COMMON *cm, struct aom_read_bit_buffer *rb) {
