50cd223d650195ceb0a59d1f2d1fceb9ba616b01
Urvang Joshi <urvang@google.com>
2018-03-26 11:46:34 -0700

    Fix possible infinite loop in get_down2_steps().
    BUG=aomedia:1659
    
    Change-Id: Ie2ac0df4f6bdeda0eef8345d8ab678d2c774a58b

--- av1/common/resize.c
+++ av1/common/resize.c
@@ -490,7 +490,7 @@ static int get_down2_length(int length, int steps) {
 static int get_down2_steps(int in_length, int out_length) {
   int steps = 0;
   int proj_in_length;
-  while ((proj_in_length = get_down2_length(in_length, 1)) >= out_length) {
+  while ((proj_in_length = get_down2_length(in_length, 1)) > out_length) {
     ++steps;
     in_length = proj_in_length;
   }
