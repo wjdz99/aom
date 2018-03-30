6fa2031a70e0a746175ebdebeb31e313583cdad9
Luc Trudeau <luc@trud.ca>
2018-03-30 18:38:02 -0400

    [CFL] NEON Versions of luma_subsampling_420_hbd
    We include unit tests for conformance and speed.
    
    NEON/CFLSubsampleHBDSpeedTest (Cortex-A53)
    
    
    Change-Id: Icb3d3a197d40d925ef2cb41602288e9654cb3c05

--- test/cfl_test.cc
+++ test/cfl_test.cc
@@ -520,6 +520,15 @@ const subsample_lbd_param subsample_lbd_sizes_neon[] = {
                              cfl_get_luma_subsampling_444_lbd_neon)
 };
 
+const subsample_hbd_param subsample_hbd_sizes_neon[] = {
+  ALL_CFL_TX_SIZES_SUBSAMPLE(
+      cfl_get_luma_subsampling_420_hbd_neon,
+      cfl_get_luma_subsampling_420_hbd_neon,  // TODO(ltrudeau) replace with
+      // 422 when SIMD is available
+      cfl_get_luma_subsampling_420_hbd_neon)  // TODO(ltrudeau) replace with
+                                              // 444 when SIMD is available
+};
+
 const predict_param predict_sizes_neon[] = { ALL_CFL_TX_SIZES(
     get_predict_lbd_fn_neon) };
 
