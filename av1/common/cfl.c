e7dee63a7eea29f461670442f08907b828441c04
Luc Trudeau <luc@trud.ca>
2018-04-05 14:50:35 -0400

    [CFL] Neon Version of 4:4:4 HBD Subsampling
    Includes unit tests for conformance and speed.
    
    NEON/CFLSubsampleHBD444SpeedTest
    
    Change-Id: I708147da051ebcd28c51cceaba1017a658911c88

--- av1/common/cfl.c
+++ av1/common/cfl.c
@@ -335,10 +335,6 @@ static void cfl_luma_subsampling_444_hbd_c(const uint16_t *input,
   }
 }
 
-// TODO(ltrudeau) Move into the CFL_GET_SUBSAMPLE_FUNCTION when LBD 444 SIMD
-// will be implemented
-CFL_SUBSAMPLE_FUNCTIONS(c, 444, hbd)
-
 CFL_GET_SUBSAMPLE_FUNCTION(c)
 
 static INLINE cfl_subsample_hbd_fn cfl_subsampling_hbd(TX_SIZE tx_size,
@@ -347,11 +343,9 @@ static INLINE cfl_subsample_hbd_fn cfl_subsampling_hbd(TX_SIZE tx_size,
     if (sub_y == 1) {
       return cfl_get_luma_subsampling_420_hbd(tx_size);
     }
-    // TODO(ltrudeau) Remove _c when HBD 422 SIMD is added
-    return cfl_get_luma_subsampling_422_hbd_c(tx_size);
+    return cfl_get_luma_subsampling_422_hbd(tx_size);
   }
-  // TODO(ltrudeau) Remove _c when HBD 444 SIMD is added
-  return cfl_get_luma_subsampling_444_hbd_c(tx_size);
+  return cfl_get_luma_subsampling_444_hbd(tx_size);
 }
 
 static INLINE cfl_subsample_lbd_fn cfl_subsampling_lbd(TX_SIZE tx_size,
