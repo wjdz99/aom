4286902160caf5320b512407a268d7fa77951f18
Luc Trudeau <luc@trud.ca>
2018-04-05 12:34:43 -0400

    [CFL] Neon Version of 4:2:2 HBD Subsampling
    Includes unit tests for conformance and speed.
    
    NEON/CFLSubsampleHBD422SpeedTest
    4x4: C time = 1131 us, SIMD time = 768 us (~1.5x)
    8x8: C time = 4216 us, SIMD time = 1622 us (~2.6x)
    16x16: C time = 16909 us, SIMD time = 4203 us (~4x)
    32x32: C time = 66084 us, SIMD time = 16874 us (~3.9x)
    
    Change-Id: Id5553bd0184d3b95fd64364607c0cf548bb04487

--- test/cfl_test.cc
+++ test/cfl_test.cc
@@ -528,8 +528,7 @@ const subsample_lbd_param subsample_lbd_sizes_neon[] = {
 const subsample_hbd_param subsample_hbd_sizes_neon[] = {
   ALL_CFL_TX_SIZES_SUBSAMPLE(
       cfl_get_luma_subsampling_420_hbd_neon,
-      cfl_get_luma_subsampling_422_hbd_c,  // TODO(ltrudeau) replace with
-      // 422 when SIMD is available
+      cfl_get_luma_subsampling_422_hbd_neon,
       cfl_get_luma_subsampling_420_hbd_neon)  // TODO(ltrudeau) replace with
                                               // 444 when SIMD is available
 };
@@ -545,6 +544,9 @@ INSTANTIATE_TEST_CASE_P(NEON, CFLSubAvgTest,
 
 INSTANTIATE_TEST_CASE_P(NEON, CFLSubsampleLBDTest,
                         ::testing::ValuesIn(subsample_lbd_sizes_neon));
+
+INSTANTIATE_TEST_CASE_P(NEON, CFLSubsampleHBDTest,
+                        ::testing::ValuesIn(subsample_hbd_sizes_neon));
 
 INSTANTIATE_TEST_CASE_P(NEON, CFLPredictTest,
                         ::testing::ValuesIn(predict_sizes_neon));
