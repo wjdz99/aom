f210e45680abaacabe8443ee371808a7f2908529
Urvang Joshi <urvang@google.com>
2018-04-20 17:41:27 -0400

    Remove the av1_get_interp_filter_params() function
    We use the av1_get_interp_filter_params_with_block_size() function
    introduced by short filter experiment instead.
    
    Note: For motion search, we use 8-tap filter even for small blocks
    as before, because that seems to yield slightly better compression than
    using a short (4-tap) filter.
    
    This is a cleanup only: no change in output.
    
    Change-Id: Ie92a07b16f01ed3e946e972d0925568a01a1fc63

--- aom_dsp/x86/variance_sse2.c
+++ aom_dsp/x86/variance_sse2.c
@@ -565,33 +565,34 @@ void aom_upsampled_pred_sse2(uint8_t *comp_pred, int width, int height,
       }
     }
   } else {
-    InterpFilterParams filter;
-    filter = av1_get_interp_filter_params(EIGHTTAP_REGULAR);
+    const InterpFilterParams filter_x =
+        av1_get_interp_filter_params_with_block_size(EIGHTTAP_REGULAR, 8);
+    const InterpFilterParams filter_y =
+        av1_get_interp_filter_params_with_block_size(EIGHTTAP_REGULAR, 8);
     if (!subpel_y_q3) {
-      const int16_t *kernel;
-      kernel = av1_get_interp_filter_subpel_kernel(filter, subpel_x_q3 << 1);
+      const int16_t *kernel =
+          av1_get_interp_filter_subpel_kernel(filter_x, subpel_x_q3 << 1);
       aom_convolve8_horiz(ref, ref_stride, comp_pred, width, kernel, 16, NULL,
                           -1, width, height);
     } else if (!subpel_x_q3) {
-      const int16_t *kernel;
-      kernel = av1_get_interp_filter_subpel_kernel(filter, subpel_y_q3 << 1);
+      const int16_t *kernel =
+          av1_get_interp_filter_subpel_kernel(filter_y, subpel_y_q3 << 1);
       aom_convolve8_vert(ref, ref_stride, comp_pred, width, NULL, -1, kernel,
                          16, width, height);
     } else {
       DECLARE_ALIGNED(16, uint8_t,
                       temp[((MAX_SB_SIZE * 2 + 16) + 16) * MAX_SB_SIZE]);
-      const int16_t *kernel_x;
-      const int16_t *kernel_y;
-      int intermediate_height;
-      kernel_x = av1_get_interp_filter_subpel_kernel(filter, subpel_x_q3 << 1);
-      kernel_y = av1_get_interp_filter_subpel_kernel(filter, subpel_y_q3 << 1);
-      intermediate_height =
-          (((height - 1) * 8 + subpel_y_q3) >> 3) + filter.taps;
+      const int16_t *kernel_x =
+          av1_get_interp_filter_subpel_kernel(filter_x, subpel_x_q3 << 1);
+      const int16_t *kernel_y =
+          av1_get_interp_filter_subpel_kernel(filter_y, subpel_y_q3 << 1);
+      const int intermediate_height =
+          (((height - 1) * 8 + subpel_y_q3) >> 3) + filter_y.taps;
       assert(intermediate_height <= (MAX_SB_SIZE * 2 + 16) + 16);
-      aom_convolve8_horiz(ref - ref_stride * ((filter.taps >> 1) - 1),
+      aom_convolve8_horiz(ref - ref_stride * ((filter_x.taps >> 1) - 1),
                           ref_stride, temp, MAX_SB_SIZE, kernel_x, 16, NULL, -1,
                           width, intermediate_height);
-      aom_convolve8_vert(temp + MAX_SB_SIZE * ((filter.taps >> 1) - 1),
+      aom_convolve8_vert(temp + MAX_SB_SIZE * ((filter_x.taps >> 1) - 1),
                          MAX_SB_SIZE, comp_pred, width, NULL, -1, kernel_y, 16,
                          width, height);
     }
