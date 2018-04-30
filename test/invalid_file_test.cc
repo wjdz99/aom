ff2b8a2ca1d12b02c5711274d56b5ee5e6e6e1d0
Wan-Teh Chang <wtc@google.com>
2018-04-30 16:50:29 -0700

    Add test/invalid_file_test.cc.
    Also add AddressSanitizer manual poisoning to test/ivf_video_source.h
    so that memory access to the bytes beyond frame_sz_ in the buffer
    is reported as an error.
    
    Change-Id: I4d7208ca38ecec0bc33dbdf82ba4ec32e6f83884

---
+++ test/invalid_file_test.cc
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+#include "test/codec_factory.h"
+#include "test/ivf_video_source.h"
+#include "test/util.h"
+#include "test/video_source.h"
+
+namespace {
+
+struct DecodeParam {
+  int threads;
+  const char *filename;
+};
+
+class InvalidFileTest : public ::libaom_test::DecoderTest,
+                        public ::libaom_test::CodecTestWithParam<DecodeParam> {
+ protected:
+  InvalidFileTest() : DecoderTest(GET_PARAM(0)) {}
+
+  virtual ~InvalidFileTest() {}
+
+  virtual bool HandleDecodeResult(const aom_codec_err_t res_dec,
+                                  libaom_test::Decoder *decoder) {
+    EXPECT_EQ(expected_res_, res_dec) << decoder->DecodeError();
+    return !::testing::Test::HasFailure();
+  }
+
+  void RunTest() {
+    const DecodeParam input = GET_PARAM(1);
+
+    aom_codec_dec_cfg_t cfg = { 0, 0, 0, CONFIG_LOWBITDEPTH, { 1 } };
+    cfg.threads = input.threads;
+    const std::string filename = input.filename;
+    libaom_test::IVFVideoSource decode_video(filename);
+    decode_video.Init();
+
+    libaom_test::AV1Decoder decoder(cfg, 0);
+
+    for (decode_video.Begin(); decode_video.cxdata() != NULL;
+         decode_video.Next()) {
+      const aom_codec_err_t res_dec =
+          decoder.DecodeFrame(decode_video.cxdata(), decode_video.frame_size());
+      EXPECT_EQ(AOM_CODEC_CORRUPT_FRAME, res_dec) << decoder.DecodeError();
+    }
+  }
+
+  int expected_res_;
+};
+
+TEST_P(InvalidFileTest, ReturnCode) { RunTest(); }
+
+const DecodeParam kAV1InvalidFileTests[] = {
+  { 1, "1814.ivf" },
+};
+
+AV1_INSTANTIATE_TEST_CASE(InvalidFileTest,
+                          ::testing::ValuesIn(kAV1InvalidFileTests));
+
+}  // namespace
