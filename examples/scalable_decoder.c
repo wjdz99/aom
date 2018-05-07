7acac6fcdebf2c1b7a62a2355e2a9330094c3c57
Tom Finegan <tomfinegan@google.com>
2018-05-07 14:26:15 -0700

    aom_codec_stream_info_t: naming fix
    Rename the enhancement_layers_cnt to
    operating_points_minus1_cnt; the value of
    operating_points_minus1_cnt is what's actually
    reported.
    
    Change-Id: Ibcb4ae2af11383ad20d2968c6c5a700e8efd2701

--- examples/scalable_decoder.c
+++ examples/scalable_decoder.c
@@ -134,7 +134,7 @@ int main(int argc, char **argv) {
     die_codec(&codec, "Input is not a valid obu file");
 
   // open output yuv files
-  for (i = 0; i <= si.enhancement_layers_cnt; i++) {
+  for (i = 0; i <= si.operating_points_minus1_cnt; i++) {
     snprintf(filename, sizeof(filename), "out_lyr%d.yuv", i);
     if (!(outfile[i] = fopen(filename, "wb")))
       die("Failed to open output for writing.");
@@ -157,25 +157,26 @@ int main(int argc, char **argv) {
         printf("Writing       base layer 0 %d\n", frame_cnt);
         aom_img_write(img_shifted, outfile[0]);
         obu_ctx.last_layer_id++;
-      } else if (img->enhancement_id <= (int)si.enhancement_layers_cnt) {
+      } else if (img->enhancement_id <= (int)si.operating_points_minus1_cnt) {
         printf("Writing enhancemnt layer %d %d\n", img->enhancement_id,
                frame_cnt);
         aom_img_write(img_shifted, outfile[img->enhancement_id]);
-        if (img->enhancement_id == (int)si.enhancement_layers_cnt)
+        if (img->enhancement_id == (int)si.operating_points_minus1_cnt)
           obu_ctx.last_layer_id = 0;
         else
           obu_ctx.last_layer_id++;
       } else {
         die_codec(&codec, "Invalid bitstream.  Layer id exceeds layer count");
       }
-      if (img->enhancement_id == (int)si.enhancement_layers_cnt) ++frame_cnt;
+      if (img->enhancement_id == (int)si.operating_points_minus1_cnt)
+        ++frame_cnt;
     }
   }
 
   printf("Processed %d frames.\n", frame_cnt);
   if (aom_codec_destroy(&codec)) die_codec(&codec, "Failed to destroy codec");
 
-  for (i = 0; i <= si.enhancement_layers_cnt; i++) fclose(outfile[i]);
+  for (i = 0; i <= si.operating_points_minus1_cnt; i++) fclose(outfile[i]);
 
   fclose(inputfile);
 
