0edb0fef446467c3185fe2dffa33f12b93baab8e
Yunqing Wang <yunqingwang@google.com>
2018-05-08 14:59:24 -0700

    Fix a segmentation fault in multi-thread code
    The segmentation fault happened while using arch=x86-linux-gcc, and
    -DSANITIZE=integer. It crashed at
      memcpy(&lf_data->planes[i].dst, &pd[i].dst,
             sizeof(lf_data->planes[i].dst));
    That is in loop_filter_data_reset() function.
    It seemed that MOVAPS was used in memcpy(), which required that the
    memory operand must be aligned on a 16-byte boundary.
    
    First fix(patch set 1): Aligned lf_sync->lfdata to 16 bytes during
    memory allocation.
    
    Second fix(patch set 2): The above fix was not quite robust. Changed
    to copy each element instead of calling memcpy.
    
    BUG=aomedia:1856
    BUG=aomedia:1859
    
    Change-Id: I2d2d4bae1c1546ed7b7e61bafea2334124079719

--- av1/common/thread_common.c
+++ av1/common/thread_common.c
@@ -124,7 +124,11 @@ static void loop_filter_data_reset(LFWorkerData *lf_data,
   lf_data->cm = cm;
   lf_data->xd = xd;
   for (int i = 0; i < MAX_MB_PLANE; i++) {
-    memcpy(&lf_data->planes[i].dst, &pd[i].dst, sizeof(lf_data->planes[i].dst));
+    lf_data->planes[i].dst.buf = pd[i].dst.buf;
+    lf_data->planes[i].dst.buf0 = pd[i].dst.buf0;
+    lf_data->planes[i].dst.width = pd[i].dst.width;
+    lf_data->planes[i].dst.height = pd[i].dst.height;
+    lf_data->planes[i].dst.stride = pd[i].dst.stride;
     lf_data->planes[i].subsampling_x = pd[i].subsampling_x;
     lf_data->planes[i].subsampling_y = pd[i].subsampling_y;
   }
