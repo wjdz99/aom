785ec520464bbe0eeeb3b9c5cb103c8426ea4d1c
Urvang Joshi <urvang@google.com>
2018-05-10 15:27:14 -0700

    Make some functions static to reduce lib size.
    libaom.so size was reduced:
    Before: 7117424 bytes
    After:  7109136 bytes
    
    Change-Id: Ia797f75544f8f021cafd7159bd12127d73325c38

--- av1/encoder/encodetxb.c
+++ av1/encoder/encodetxb.c
@@ -650,6 +650,39 @@ void av1_write_coeffs_mb(const AV1_COMMON *const cm, MACROBLOCK *x, int mi_row,
   }
 }
 
+// TODO(angiebird): use this function whenever it's possible
+static int get_tx_type_cost(const AV1_COMMON *cm, const MACROBLOCK *x,
+                            const MACROBLOCKD *xd, int plane, TX_SIZE tx_size,
+                            TX_TYPE tx_type) {
+  if (plane > 0) return 0;
+
+  const TX_SIZE square_tx_size = txsize_sqr_map[tx_size];
+
+  const MB_MODE_INFO *mbmi = xd->mi[0];
+  const int is_inter = is_inter_block(mbmi);
+  if (get_ext_tx_types(tx_size, is_inter, cm->reduced_tx_set_used) > 1 &&
+      !xd->lossless[xd->mi[0]->segment_id]) {
+    const int ext_tx_set =
+        get_ext_tx_set(tx_size, is_inter, cm->reduced_tx_set_used);
+    if (is_inter) {
+      if (ext_tx_set > 0)
+        return x->inter_tx_type_costs[ext_tx_set][square_tx_size][tx_type];
+    } else {
+      if (ext_tx_set > 0) {
+        PREDICTION_MODE intra_dir;
+        if (mbmi->filter_intra_mode_info.use_filter_intra)
+          intra_dir = fimode_to_intradir[mbmi->filter_intra_mode_info
+                                             .filter_intra_mode];
+        else
+          intra_dir = mbmi->mode;
+        return x->intra_tx_type_costs[ext_tx_set][square_tx_size][intra_dir]
+                                     [tx_type];
+      }
+    }
+  }
+  return 0;
+}
+
 static AOM_FORCE_INLINE int warehouse_efficients_txb(
     const AV1_COMMON *const cm, const MACROBLOCK *x, const int plane,
     const int block, const TX_SIZE tx_size, const TXB_CTX *const txb_ctx,
@@ -674,7 +707,7 @@ static AOM_FORCE_INLINE int warehouse_efficients_txb(
 
   av1_txb_init_levels(qcoeff, width, height, levels);
 
-  cost += av1_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
+  cost += get_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
 
   cost += get_eob_cost(eob, eob_costs, coeff_costs, tx_class);
 
@@ -1638,7 +1671,7 @@ int av1_optimize_txb_new(const struct AV1_COMP *cpi, MACROBLOCK *x, int plane,
                          levels);
   }
 
-  const int tx_type_cost = av1_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
+  const int tx_type_cost = get_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
   if (eob == 0)
     accu_rate += skip_cost;
   else
@@ -1695,7 +1728,7 @@ int av1_optimize_txb(const struct AV1_COMP *cpi, MACROBLOCK *x, int plane,
           ? pd->seg_iqmatrix[mbmi->segment_id][qm_tx_size]
           : cm->giqmatrix[NUM_QM_LEVELS - 1][0][qm_tx_size];
   assert(width == (1 << bwl));
-  const int tx_type_cost = av1_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
+  const int tx_type_cost = get_tx_type_cost(cm, x, xd, plane, tx_size, tx_type);
   TxbInfo txb_info = {
     qcoeff,   levels,       dqcoeff,    tcoeff,  dequant, shift,
     tx_size,  txs_ctx,      tx_type,    bwl,     width,   height,
