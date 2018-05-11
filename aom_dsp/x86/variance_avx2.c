aa99773790e6cf5af7973e6180b0865b29315ac8
Peng Bin <binpengsmail@gmail.com>
2018-05-11 15:51:33 +0800

    Refactor: Remove variance_avx2
    1. Same idea as CL58321, just apply to avx2.
    2. Add more avx2 functions(32x{8,64},16x{4,8,32,64}).
    
    3. For encoder, about 1.3% faster shows by encoding
    15 frame of city_cif, with CL58321 and this CL.
    333356 ms --> 328924 ms
    
    a) gcc (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609
    b) CPU: Intel(R) Core(TM) i5-4590 CPU @ 3.30GHz
    c) Config cmd
    cmake ../ -DENABLE_CCACHE=1 -DCONFIG_LOWBITDEPTH=1
    d) Test cmd:
    ./aomenc --cpu-used=1 --end-usage=vbr \
    --target-bitrate=600 --limit=15
    
    Change-Id: I8b058944ad23353b77c0cdd5b4714b4413e31d73

--- aom_dsp/x86/variance_avx2.c
+++ aom_dsp/x86/variance_avx2.c
@@ -13,75 +13,256 @@
 #include "./aom_dsp_rtcd.h"
 #include "aom_dsp/x86/masked_variance_intrin_ssse3.h"
 
-typedef void (*get_var_avx2)(const uint8_t *src, int src_stride,
-                             const uint8_t *ref, int ref_stride,
-                             unsigned int *sse, int *sum);
+static INLINE void get16xn_var_avx2(const unsigned char *src_ptr,
+                                    int source_stride,
+                                    const unsigned char *ref_ptr,
+                                    int recon_stride, int height,
+                                    unsigned int *SSE, int *Sum) {
+  __m256i src, src_expand_low, src_expand_high, ref, ref_expand_low;
+  __m256i ref_expand_high, madd_low, madd_high;
+  unsigned int src_2strides, ref_2strides;
+  __m256i zero_reg = _mm256_set1_epi16(0);
+  __m256i sum_ref_src = _mm256_set1_epi16(0);
+  __m256i madd_ref_src = _mm256_set1_epi16(0);
 
-void aom_get32x32var_avx2(const uint8_t *src, int src_stride,
-                          const uint8_t *ref, int ref_stride, unsigned int *sse,
-                          int *sum);
+  // processing two strides in a 256 bit register reducing the number
+  // of loop stride by half (comparing to the sse2 code)
+  src_2strides = source_stride << 1;
+  ref_2strides = recon_stride << 1;
+  height = (height >> 1);
+  for (int i = 0; i < height; i++) {
+    src = _mm256_castsi128_si256(_mm_loadu_si128((__m128i const *)(src_ptr)));
+    src = _mm256_inserti128_si256(
+        src, _mm_loadu_si128((__m128i const *)(src_ptr + source_stride)), 1);
 
-static void variance_avx2(const uint8_t *src, int src_stride,
-                          const uint8_t *ref, int ref_stride, int w, int h,
-                          unsigned int *sse, int *sum, get_var_avx2 var_fn,
-                          int block_size) {
-  int i, j;
+    ref = _mm256_castsi128_si256(_mm_loadu_si128((__m128i const *)(ref_ptr)));
+    ref = _mm256_inserti128_si256(
+        ref, _mm_loadu_si128((__m128i const *)(ref_ptr + recon_stride)), 1);
 
-  *sse = 0;
-  *sum = 0;
+    // expanding to 16 bit each lane
+    src_expand_low = _mm256_unpacklo_epi8(src, zero_reg);
+    src_expand_high = _mm256_unpackhi_epi8(src, zero_reg);
 
-  for (i = 0; i < h; i += 16) {
-    for (j = 0; j < w; j += block_size) {
-      unsigned int sse0;
-      int sum0;
-      var_fn(&src[src_stride * i + j], src_stride, &ref[ref_stride * i + j],
-             ref_stride, &sse0, &sum0);
-      *sse += sse0;
-      *sum += sum0;
-    }
+    ref_expand_low = _mm256_unpacklo_epi8(ref, zero_reg);
+    ref_expand_high = _mm256_unpackhi_epi8(ref, zero_reg);
+
+    // src-ref
+    src_expand_low = _mm256_sub_epi16(src_expand_low, ref_expand_low);
+    src_expand_high = _mm256_sub_epi16(src_expand_high, ref_expand_high);
+
+    // madd low (src - ref)
+    madd_low = _mm256_madd_epi16(src_expand_low, src_expand_low);
+
+    // add high to low
+    src_expand_low = _mm256_add_epi16(src_expand_low, src_expand_high);
+
+    // madd high (src - ref)
+    madd_high = _mm256_madd_epi16(src_expand_high, src_expand_high);
+
+    sum_ref_src = _mm256_add_epi16(sum_ref_src, src_expand_low);
+
+    // add high to low
+    madd_ref_src =
+        _mm256_add_epi32(madd_ref_src, _mm256_add_epi32(madd_low, madd_high));
+
+    src_ptr += src_2strides;
+    ref_ptr += ref_2strides;
   }
+
+  {
+    __m128i sum_res, madd_res;
+    __m128i expand_sum_low, expand_sum_high, expand_sum;
+    __m128i expand_madd_low, expand_madd_high, expand_madd;
+    __m128i ex_expand_sum_low, ex_expand_sum_high, ex_expand_sum;
+
+    // extract the low lane and add it to the high lane
+    sum_res = _mm_add_epi16(_mm256_castsi256_si128(sum_ref_src),
+                            _mm256_extractf128_si256(sum_ref_src, 1));
+
+    madd_res = _mm_add_epi32(_mm256_castsi256_si128(madd_ref_src),
+                             _mm256_extractf128_si256(madd_ref_src, 1));
+
+    // padding each 2 bytes with another 2 zeroed bytes
+    expand_sum_low =
+        _mm_unpacklo_epi16(_mm256_castsi256_si128(zero_reg), sum_res);
+    expand_sum_high =
+        _mm_unpackhi_epi16(_mm256_castsi256_si128(zero_reg), sum_res);
+
+    // shifting the sign 16 bits right
+    expand_sum_low = _mm_srai_epi32(expand_sum_low, 16);
+    expand_sum_high = _mm_srai_epi32(expand_sum_high, 16);
+
+    expand_sum = _mm_add_epi32(expand_sum_low, expand_sum_high);
+
+    // expand each 32 bits of the madd result to 64 bits
+    expand_madd_low =
+        _mm_unpacklo_epi32(madd_res, _mm256_castsi256_si128(zero_reg));
+    expand_madd_high =
+        _mm_unpackhi_epi32(madd_res, _mm256_castsi256_si128(zero_reg));
+
+    expand_madd = _mm_add_epi32(expand_madd_low, expand_madd_high);
+
+    ex_expand_sum_low =
+        _mm_unpacklo_epi32(expand_sum, _mm256_castsi256_si128(zero_reg));
+    ex_expand_sum_high =
+        _mm_unpackhi_epi32(expand_sum, _mm256_castsi256_si128(zero_reg));
+
+    ex_expand_sum = _mm_add_epi32(ex_expand_sum_low, ex_expand_sum_high);
+
+    // shift 8 bytes eight
+    madd_res = _mm_srli_si128(expand_madd, 8);
+    sum_res = _mm_srli_si128(ex_expand_sum, 8);
+
+    madd_res = _mm_add_epi32(madd_res, expand_madd);
+    sum_res = _mm_add_epi32(sum_res, ex_expand_sum);
+
+    *((int *)SSE) = _mm_cvtsi128_si32(madd_res);
+
+    *((int *)Sum) = _mm_cvtsi128_si32(sum_res);
+  }
+  _mm256_zeroupper();
 }
 
-unsigned int aom_variance16x16_avx2(const uint8_t *src, int src_stride,
-                                    const uint8_t *ref, int ref_stride,
-                                    unsigned int *sse) {
-  int sum;
-  unsigned int variance;
-  aom_get16x16var_avx2(src, src_stride, ref, ref_stride, sse, &sum);
-  variance = *sse - (((uint32_t)((int64_t)sum * sum)) >> 8);
+static INLINE void get32xn_var_avx2(const unsigned char *src_ptr,
+                                    int source_stride,
+                                    const unsigned char *ref_ptr,
+                                    int recon_stride, int height,
+                                    unsigned int *SSE, int *Sum) {
+  __m256i src, src_expand_low, src_expand_high, ref, ref_expand_low;
+  __m256i ref_expand_high, madd_low, madd_high;
+  __m256i zero_reg = _mm256_set1_epi16(0);
+  __m256i sum_ref_src = _mm256_set1_epi16(0);
+  __m256i madd_ref_src = _mm256_set1_epi16(0);
+
+  // processing 32 elements in parallel
+  for (int i = 0; i < height; i++) {
+    src = _mm256_loadu_si256((__m256i const *)(src_ptr));
+
+    ref = _mm256_loadu_si256((__m256i const *)(ref_ptr));
+
+    // expanding to 16 bit each lane
+    src_expand_low = _mm256_unpacklo_epi8(src, zero_reg);
+    src_expand_high = _mm256_unpackhi_epi8(src, zero_reg);
+
+    ref_expand_low = _mm256_unpacklo_epi8(ref, zero_reg);
+    ref_expand_high = _mm256_unpackhi_epi8(ref, zero_reg);
+
+    // src-ref
+    src_expand_low = _mm256_sub_epi16(src_expand_low, ref_expand_low);
+    src_expand_high = _mm256_sub_epi16(src_expand_high, ref_expand_high);
+
+    // madd low (src - ref)
+    madd_low = _mm256_madd_epi16(src_expand_low, src_expand_low);
+
+    // add high to low
+    src_expand_low = _mm256_add_epi16(src_expand_low, src_expand_high);
+
+    // madd high (src - ref)
+    madd_high = _mm256_madd_epi16(src_expand_high, src_expand_high);
+
+    sum_ref_src = _mm256_add_epi16(sum_ref_src, src_expand_low);
+
+    // add high to low
+    madd_ref_src =
+        _mm256_add_epi32(madd_ref_src, _mm256_add_epi32(madd_low, madd_high));
+
+    src_ptr += source_stride;
+    ref_ptr += recon_stride;
+  }
+
+  {
+    __m256i expand_sum_low, expand_sum_high, expand_sum;
+    __m256i expand_madd_low, expand_madd_high, expand_madd;
+    __m256i ex_expand_sum_low, ex_expand_sum_high, ex_expand_sum;
+
+    // padding each 2 bytes with another 2 zeroed bytes
+    expand_sum_low = _mm256_unpacklo_epi16(zero_reg, sum_ref_src);
+    expand_sum_high = _mm256_unpackhi_epi16(zero_reg, sum_ref_src);
+
+    // shifting the sign 16 bits right
+    expand_sum_low = _mm256_srai_epi32(expand_sum_low, 16);
+    expand_sum_high = _mm256_srai_epi32(expand_sum_high, 16);
+
+    expand_sum = _mm256_add_epi32(expand_sum_low, expand_sum_high);
+
+    // expand each 32 bits of the madd result to 64 bits
+    expand_madd_low = _mm256_unpacklo_epi32(madd_ref_src, zero_reg);
+    expand_madd_high = _mm256_unpackhi_epi32(madd_ref_src, zero_reg);
+
+    expand_madd = _mm256_add_epi32(expand_madd_low, expand_madd_high);
+
+    ex_expand_sum_low = _mm256_unpacklo_epi32(expand_sum, zero_reg);
+    ex_expand_sum_high = _mm256_unpackhi_epi32(expand_sum, zero_reg);
+
+    ex_expand_sum = _mm256_add_epi32(ex_expand_sum_low, ex_expand_sum_high);
+
+    // shift 8 bytes eight
+    madd_ref_src = _mm256_srli_si256(expand_madd, 8);
+    sum_ref_src = _mm256_srli_si256(ex_expand_sum, 8);
+
+    madd_ref_src = _mm256_add_epi32(madd_ref_src, expand_madd);
+    sum_ref_src = _mm256_add_epi32(sum_ref_src, ex_expand_sum);
+
+    // extract the low lane and the high lane and add the results
+    *((int *)SSE) =
+        _mm_cvtsi128_si32(_mm256_castsi256_si128(madd_ref_src)) +
+        _mm_cvtsi128_si32(_mm256_extractf128_si256(madd_ref_src, 1));
+
+    *((int *)Sum) = _mm_cvtsi128_si32(_mm256_castsi256_si128(sum_ref_src)) +
+                    _mm_cvtsi128_si32(_mm256_extractf128_si256(sum_ref_src, 1));
+  }
   _mm256_zeroupper();
-  return variance;
 }
+
+#define AOM_VAR_AVX2(bw, bh, uw, uh, bits)                                     \
+  unsigned int aom_variance##bw##x##bh##_avx2(                                 \
+      const uint8_t *src, int src_stride, const uint8_t *ref, int ref_stride,  \
+      unsigned int *sse) {                                                     \
+    int sum = 0;                                                               \
+    unsigned int variance;                                                     \
+    *sse = 0;                                                                  \
+    for (int i = 0; i < bh; i += uh) {                                         \
+      const uint8_t *src_i = src + i * src_stride;                             \
+      const uint8_t *ref_i = ref + i * ref_stride;                             \
+      for (int j = 0; j < bw; j += uw) {                                       \
+        unsigned int sse0 = 0;                                                 \
+        int sum0 = 0;                                                          \
+        get##uw##xn_var_avx2(src_i + j, src_stride, ref_i + j, ref_stride, uh, \
+                             &sse0, &sum0);                                    \
+        *sse += sse0;                                                          \
+        sum += sum0;                                                           \
+      }                                                                        \
+    }                                                                          \
+    variance = *sse - (uint32_t)(((int64_t)sum * sum) >> bits);                \
+    _mm256_zeroupper();                                                        \
+    return variance;                                                           \
+  }
+
+AOM_VAR_AVX2(16, 4, 16, 4, 6);
+AOM_VAR_AVX2(16, 8, 16, 8, 7);
+AOM_VAR_AVX2(16, 16, 16, 16, 8);
+AOM_VAR_AVX2(16, 32, 16, 16, 9);
+AOM_VAR_AVX2(16, 64, 16, 16, 10);
+
+AOM_VAR_AVX2(32, 8, 32, 8, 8);
+AOM_VAR_AVX2(32, 16, 32, 16, 9);
+AOM_VAR_AVX2(32, 32, 32, 16, 10);
+AOM_VAR_AVX2(32, 64, 32, 16, 11);
+
+AOM_VAR_AVX2(64, 16, 32, 16, 10);
+AOM_VAR_AVX2(64, 32, 32, 16, 11);
+AOM_VAR_AVX2(64, 64, 32, 16, 12);
+AOM_VAR_AVX2(64, 128, 32, 16, 13);
+
+AOM_VAR_AVX2(128, 128, 32, 16, 14);
+AOM_VAR_AVX2(128, 64, 32, 16, 13);
 
 unsigned int aom_mse16x16_avx2(const uint8_t *src, int src_stride,
                                const uint8_t *ref, int ref_stride,
                                unsigned int *sse) {
-  int sum;
-  aom_get16x16var_avx2(src, src_stride, ref, ref_stride, sse, &sum);
-  _mm256_zeroupper();
+  aom_variance16x16_avx2(src, src_stride, ref, ref_stride, sse);
   return *sse;
 }
-
-#define AOM_VAR_AVX2(bw, bh, w, bits)                                         \
-  unsigned int aom_variance##bw##x##bh##_avx2(                                \
-      const uint8_t *src, int src_stride, const uint8_t *ref, int ref_stride, \
-      unsigned int *sse) {                                                    \
-    int sum;                                                                  \
-    unsigned int variance;                                                    \
-    variance_avx2(src, src_stride, ref, ref_stride, bw, bh, sse, &sum,        \
-                  aom_get##w##x##w##var_avx2, w);                             \
-    variance = *sse - (uint32_t)(((int64_t)sum * sum) >> bits);               \
-    _mm256_zeroupper();                                                       \
-    return variance;                                                          \
-  }
-
-AOM_VAR_AVX2(32, 16, 16, 9);
-AOM_VAR_AVX2(32, 32, 32, 10);
-AOM_VAR_AVX2(64, 64, 32, 12);
-AOM_VAR_AVX2(64, 32, 32, 11);
-AOM_VAR_AVX2(128, 128, 32, 14);
-AOM_VAR_AVX2(128, 64, 32, 13);
-AOM_VAR_AVX2(64, 128, 32, 13);
 
 unsigned int aom_sub_pixel_variance32xh_avx2(const uint8_t *src, int src_stride,
                                              int x_offset, int y_offset,
