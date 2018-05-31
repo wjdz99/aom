ff42147decd0befa2b63a964d7b45c3899b16a00
Adrian Grange <agrange@google.com>
2018-05-31 14:19:32 -0700

    Decoder model changes.
    Update the decoder model to match the latest design.
    
    Change-Id: Id698227a83daf7573fec2131b74eb8054d5bc2b0

--- av1/common/timing.h
+++ av1/common/timing.h
@@ -33,7 +33,6 @@ typedef struct aom_dec_model_info {
 } aom_dec_model_info_t;
 
 typedef struct aom_dec_model_op_parameters {
-  int decoder_model_operating_point_idc;
   int decoder_model_param_present_flag;
   uint32_t bitrate;
   uint32_t buffer_size;
@@ -46,7 +45,6 @@ typedef struct aom_dec_model_op_parameters {
 } aom_dec_model_op_parameters_t;
 
 typedef struct aom_op_timing_info_t {
-  int buffer_removal_delay_flag;
   int64_t buffer_removal_delay;
 } aom_op_timing_info_t;
 
