91e3ed46c3b88ec7acbd9fa115c2f7f2e6ca0b60
Scott LaVarnway <slavarnway@google.com>
2018-06-04 14:58:48 -0700

    Add dr prediction test
    Change-Id: I76da1e630017b3b11103a2ab79d2f56e6a72ecf4

--- test/test.cmake
+++ test/test.cmake
@@ -126,6 +126,7 @@ if(NOT BUILD_SHARED_LIBS)
 
     if(HAVE_SSE4_1)
       set(AOM_UNIT_TEST_COMMON_SOURCES ${AOM_UNIT_TEST_COMMON_SOURCES}
+          "${AOM_ROOT}/test/dr_prediction_test.cc"
           "${AOM_ROOT}/test/filterintra_test.cc")
     endif()
 
