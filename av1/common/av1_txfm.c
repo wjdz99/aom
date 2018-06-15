191aff40b18a80a255da8b4803de2d50ab89d896
Zoe Liu <zoeliu@google.com>
2018-06-15 11:40:02 -0700

    Clamp int64_t first before its int32_t truncation
    BUG=aomedia:1960
    
    Change-Id: I66a34953a0c966fcee8a0bf20e47b6b6a879e551

--- av1/common/av1_txfm.c
+++ av1/common/av1_txfm.c
@@ -78,7 +78,8 @@ void av1_round_shift_array_c(int32_t *arr, int size, int bit) {
       }
     } else {
       for (i = 0; i < size; i++) {
-        arr[i] = (int32_t)((int64_t)arr[i] * (1ULL << (-bit)));
+        arr[i] = (int32_t)clamp64((int64_t)arr[i] * (1 << (-bit)),
+                                  (int64_t)INT32_MIN, (int64_t)INT32_MAX);
       }
     }
   }
