8d1bead5bf78641cc04e9e6e178a851f8a3e5ace
Wan-Teh Chang <wtc@google.com>
2018-06-29 10:21:39 -0700

    Remove the init_done local variable.
    We only call initialize_dec() and av1_initialize_enc() via once(), so
    the init_done local variable in initialize_dec() and
    av1_initialize_enc() is redundant.
    
    Change-Id: I35e9e27291fa01aff1358536861c432b0717e856

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -690,6 +690,7 @@ typedef struct AV1_COMP {
   AV1LrStruct lr_ctxt;
 } AV1_COMP;
 
+// Must not be called more than once.
 void av1_initialize_enc(void);
 
 struct AV1_COMP *av1_create_compressor(AV1EncoderConfig *oxcf,
