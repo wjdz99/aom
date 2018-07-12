5e961b709dc3071536ceec41dcd5c0b95d8d3fe2
Wei-Ting Lin <weitinglin@google.com>
2018-07-12 11:48:02 -0700

    Enable quality boost for internal ARFs
    Also remove some unused MACRO constants
    
    Change-Id: Id2d296922a2b9bb53437f644ee329b43837497fa

--- av1/encoder/firstpass.c
+++ av1/encoder/firstpass.c
@@ -2333,15 +2333,11 @@ static const unsigned char gf4_multi_layer_params[][GF_FRAME_PARAMS] = {
       0                          // arf_update_idx
   },
   {
-// gf_group->index == 3 (Frame 1)
-#if MATCH_OLD_BITRATE
+      // gf_group->index == 3 (Frame 1)
       LAST_BIPRED_UPDATE,  // update_type
-#else
-      LF_UPDATE,  // update_type
-#endif
-      0,  // arf_src_offset
-      0,  // arf_pos_in_gf
-      0   // arf_update_idx
+      0,                   // arf_src_offset
+      0,                   // arf_pos_in_gf
+      0                    // arf_update_idx
   },
 
   {
@@ -2768,7 +2764,6 @@ static void allocate_gf_group_bits(AV1_COMP *cpi, int64_t gf_group_bits,
       // TODO(zoeliu): To investigate whether the allocated bits on
       // BIPRED_UPDATE frames need to be further adjusted.
       gf_group->bit_allocation[frame_index] = target_frame_size;
-#if MATCH_OLD_BITRATE
     } else if (cpi->new_bwdref_update_rule == 1 &&
                gf_group->update_type[frame_index] == INTNL_OVERLAY_UPDATE) {
       int arf_pos = gf_group->arf_pos_in_gf[frame_index];
@@ -2776,7 +2771,6 @@ static void allocate_gf_group_bits(AV1_COMP *cpi, int64_t gf_group_bits,
       // Boost up the allocated bits on backward reference frame
       gf_group->bit_allocation[arf_pos] = target_frame_size;
       // target_frame_size + (target_frame_size >> 2);
-#endif
     } else {
       assert(gf_group->update_type[frame_index] == LF_UPDATE ||
              gf_group->update_type[frame_index] == INTNL_OVERLAY_UPDATE);
