92202a65e166a7558233ead91e84f3a1c6d9102c
Wan-Teh Chang <wtc@google.com>
2018-07-20 14:53:32 -0700

    Implement the frame_header_copy() function in spec.
    BUG=aomedia:2030
    
    Change-Id: I3765d4e7c5ae1b3f679b6cefec898e8b9b73bfbf

--- av1/decoder/decodeframe.h
+++ av1/decoder/decodeframe.h
@@ -35,14 +35,10 @@ BITSTREAM_PROFILE av1_read_profile(struct aom_read_bit_buffer *rb);
 int av1_check_trailing_bits(struct AV1Decoder *pbi,
                             struct aom_read_bit_buffer *rb);
 
-// On success, returns the frame header size. On failure, calls
+// On success, returns the uncompressed_header size. On failure, calls
 // aom_internal_error and does not return.
-// TODO(wtc): Figure out and document the p_data_end parameter.
-uint32_t av1_decode_frame_headers_and_setup(struct AV1Decoder *pbi,
-                                            struct aom_read_bit_buffer *rb,
-                                            const uint8_t *data,
-                                            const uint8_t **p_data_end,
-                                            int trailing_bits_present);
+uint32_t av1_decode_frame_header_and_setup(struct AV1Decoder *pbi,
+                                           struct aom_read_bit_buffer *rb);
 
 void av1_decode_tg_tiles_and_wrapup(struct AV1Decoder *pbi, const uint8_t *data,
                                     const uint8_t *data_end,
