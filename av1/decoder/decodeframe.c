58276d1530251f5ab2061916b2bf218a7f7fba3d
Cheng Chen <chengchen@google.com>
2018-07-26 16:10:43 -0700

    Bitmask patch 3: Add more lookup table entries for bitmask.
    For a given partition block, when transform sizes inside it are the
    same, we can directly find an entry from the lookup table and build
    the bitmask. This saves bitmask operations.
    
    Change-Id: I1394f704cccf3b4570aa77c1047dea19e23a5b41

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -1417,7 +1417,6 @@ static void store_bitmask_info(AV1_COMMON *cm, int mi_row, int mi_col,
   } else {
     // TODO(chengchen): optimize step
     LoopFilterMask *lfm = get_loop_filter_mask(cm, mi_row, mi_col);
-    // vertical direction
     const TX_SIZE tx_size_y_vert = txsize_vert_map[mbmi->tx_size];
     const TX_SIZE tx_size_y_horz = txsize_horz_map[mbmi->tx_size];
     const TX_SIZE tx_size_uv_vert = txsize_vert_map[av1_get_max_uv_txsize(
@@ -1450,6 +1449,7 @@ static void store_bitmask_info(AV1_COMMON *cm, int mi_row, int mi_col,
         }
       }
     }
+    // u/v vertical.
     for (int r = mi_row; r < mi_row + mi_size_high[bsize];
          r += tx_size_high_unit[tx_size_uv_vert]) {
       for (int c = mi_col; c < mi_col + mi_size_wide[bsize];
@@ -1477,6 +1477,7 @@ static void store_bitmask_info(AV1_COMMON *cm, int mi_row, int mi_col,
       }
     }
     // horizontal direction
+    // y horizontal.
     for (int c = mi_col; c < mi_col + mi_size_wide[bsize];
          c += tx_size_wide_unit[mbmi->tx_size]) {
       for (int r = mi_row; r < mi_row + mi_size_high[bsize];
@@ -1488,6 +1489,7 @@ static void store_bitmask_info(AV1_COMMON *cm, int mi_row, int mi_col,
             (above_txform_mask[0][tx_size_y_horz] << shift);
       }
     }
+    // u/v horizontal.
     for (int c = mi_col; c < mi_col + mi_size_wide[bsize];
          c += tx_size_wide_unit[tx_size_uv_horz]) {
       for (int r = mi_row; r < mi_row + mi_size_high[bsize];
