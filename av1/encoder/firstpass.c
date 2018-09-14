be11af6c1f506750d6b7cbe21da555f8a57f8ef5
Wei-Ting Lin <weitinglin@google.com>
2018-09-14 12:07:06 -0700

    Define the maximum GOP size for the 4-layer structure
    Allow a GOP of size <= 23 to use pyramid struture, but currently this
    change has no effective since we use fix GF size = 16.
    
    We might want to group some few stray frames into current GOP for
    forward keyframe setting or using ALTREF to match the quality of the
    next keyframe.
    
    Change-Id: I228d4515a6775d14f0ea22ad2e88b418964bcdc2

--- av1/encoder/firstpass.c
+++ av1/encoder/firstpass.c
@@ -1695,7 +1695,8 @@ void define_customized_gf_group_structure(AV1_COMP *cpi) {
   GF_GROUP *const gf_group = &twopass->gf_group;
   const int key_frame = cpi->common.frame_type == KEY_FRAME;
 
-  assert(rc->baseline_gf_interval >= 4 && rc->baseline_gf_interval <= 16);
+  assert(rc->baseline_gf_interval >= 4 &&
+         rc->baseline_gf_interval <= MAX_PYRAMID_SIZE);
 
   const int gf_update_frames =
       construct_multi_layer_gf_structure(gf_group, rc->baseline_gf_interval);
@@ -1896,7 +1897,8 @@ static void define_gf_group_structure(AV1_COMP *cpi) {
 
 #if USE_SYMM_MULTI_LAYER
   const int valid_customized_gf_length =
-      rc->baseline_gf_interval >= 4 && rc->baseline_gf_interval <= 16;
+      rc->baseline_gf_interval >= 4 &&
+      rc->baseline_gf_interval <= MAX_PYRAMID_SIZE;
   // used the new structure only if extra_arf is allowed
   if (valid_customized_gf_length && rc->source_alt_ref_pending &&
       cpi->extra_arf_allowed > 0) {
