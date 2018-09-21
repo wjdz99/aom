810b2e54baf6366328b14b0063b89ed7a210cf27
Yunqing Wang <yunqingwang@google.com>
2018-09-21 16:17:48 -0700

    Pass in use_accurate_subpel_search to sub-pel search
    The speed feature cpi->sf.use_accurate_subpel_search was modified.
    Passed in cpi->sf.use_accurate_subpel_search explicitly to conduct
    desired sub-pel search.
    
    STATS_CHANGED
    
    Change-Id: I356bdbee87e48412dfd02ae18e114ce69bf043db

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -6964,7 +6964,7 @@ static void single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
               x->errorperbit, &cpi->fn_ptr[bsize], cpi->sf.mv.subpel_force_stop,
               cpi->sf.mv.subpel_iters_per_step, cond_cost_list(cpi, cost_list),
               x->nmvjointcost, x->mvcost, &dis, &x->pred_sse[ref], NULL, NULL,
-              0, 0, pw, ph, 1);
+              0, 0, pw, ph, cpi->sf.use_accurate_subpel_search);
 
           if (try_second) {
             const int minc =
@@ -6989,7 +6989,8 @@ static void single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
                   cpi->sf.mv.subpel_force_stop,
                   cpi->sf.mv.subpel_iters_per_step,
                   cond_cost_list(cpi, cost_list), x->nmvjointcost, x->mvcost,
-                  &dis, &x->pred_sse[ref], NULL, NULL, 0, 0, pw, ph, 1);
+                  &dis, &x->pred_sse[ref], NULL, NULL, 0, 0, pw, ph,
+                  cpi->sf.use_accurate_subpel_search);
               if (this_var < best_mv_var) best_mv = x->best_mv.as_mv;
               x->best_mv.as_mv = best_mv;
             }
