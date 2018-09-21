73c8882bdf4f4e8f9e34c1862cc8b3593b5c9cba
chiyotsai <chiyotsai@google.com>
2018-09-21 13:30:39 -0700

    Add code that extract data for one pass svm.
    This code fixes the eob calculation and estimates the eob and rdcost of
    the four sub-blocks. Currently the estimations only works correctly when
    skip flag is off.
    
    Here, we save the EOB and rdcost in the struct RD_STATS when we do
    transform. Then we pass the info in rdcost upstream until we reach
    rd_pick_sb_modes, where to store the information in none_context. Due to
    the complexity of the code, there might be some updates missing.
    
    To turn on data generation, enable the CONFIG_ONE_PASS_SVM and
    CONFIG_TRAIN_SVM flags in build/cmake/aom_config_defaults.cmake.
    
    Change-Id: I626870fde623561f1943eea76a8b5cdc0e7a6714

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -137,6 +137,8 @@ set_aom_config_var(CONFIG_2PASS_PARTITION_SEARCH_LVL 1 NUMBER
                    "AV1 experiment flag.")
 set_aom_config_var(CONFIG_SHARP_SETTINGS 0 NUMBER
                    "Use sharper encoding settings")
+set_aom_config_var(CONFIG_ONE_PASS_SVM 0 NUMBER "AV1 experiment flag.")
+set_aom_config_var(CONFIG_TRAIN_SVM 0 NUMBER "AV1 experiment flag.")
 
 #
 # Variables in this section control optional features of the build system.
