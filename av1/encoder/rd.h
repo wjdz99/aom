8b1302225c4f16a1e91b73fcba3b4488f24129eb
chiyotsai <chiyotsai@google.com>
2018-09-21 13:30:39 -0700

    Introduce one pass svm early termination speed feature.
    This speed feature extracts regional data after rd_pick_partition
    tries none partition, and then uses a rd_weighted svm to decide whether
    to early terminate partition decisions.
    
    This speed feature currently out-performs two_pass_partition_search in
    both BDBR and time reduction. However, most of the performance gain
    comes from low bitrate regime, so two_pass_partititon_search still
    outperforms at high bit-rate regime. Thus this speed feature is not on
    by default.
    
                     | ONE PASS SVM | TWO PASS |
    BDBR             |    0.5310    |  0.6930  |
    Time Reduction 1 |    0.1080    |  0.0790  |
    Time Reduction 2 |    0.1268    |  0.1224  |
    
    All metrics are calculated relative to one pass baseline. BDBR is
    calculated on midres. Time reduction 1 is calculated over 15 midres clips
    at qp 35, 45, 55. Time reduction 2 is calculated over 5 midres clips at 6
    differnt bitrates from 75 to 8000.
    
    Change-Id: I640f5546b1eb3b212fcc6821e3311c4b13798ccf

--- av1/encoder/rd.h
+++ av1/encoder/rd.h
@@ -319,6 +319,25 @@ static INLINE void av1_init_rd_stats(RD_STATS *rd_stats) {
     }
   }
 #endif
+#if CONFIG_ONE_PASS_SVM
+  rd_stats->eob = 0;
+  rd_stats->eob_0 = 0;
+  rd_stats->eob_1 = 0;
+  rd_stats->eob_2 = 0;
+  rd_stats->eob_3 = 0;
+
+  rd_stats->rd = 0;
+  rd_stats->rd_0 = 0;
+  rd_stats->rd_1 = 0;
+  rd_stats->rd_2 = 0;
+  rd_stats->rd_3 = 0;
+
+  rd_stats->y_sse = 0;
+  rd_stats->sse_0 = 0;
+  rd_stats->sse_1 = 0;
+  rd_stats->sse_2 = 0;
+  rd_stats->sse_3 = 0;
+#endif
 }
 
 static INLINE void av1_invalid_rd_stats(RD_STATS *rd_stats) {
@@ -345,6 +364,25 @@ static INLINE void av1_invalid_rd_stats(RD_STATS *rd_stats) {
           rd_stats->txb_coeff_cost_map[plane][r][c] = INT_MAX;
     }
   }
+#endif
+#if CONFIG_ONE_PASS_SVM
+  rd_stats->eob = 0;
+  rd_stats->eob_0 = 0;
+  rd_stats->eob_1 = 0;
+  rd_stats->eob_2 = 0;
+  rd_stats->eob_3 = 0;
+
+  rd_stats->rd = 0;
+  rd_stats->rd_0 = 0;
+  rd_stats->rd_1 = 0;
+  rd_stats->rd_2 = 0;
+  rd_stats->rd_3 = 0;
+
+  rd_stats->y_sse = 0;
+  rd_stats->sse_0 = 0;
+  rd_stats->sse_1 = 0;
+  rd_stats->sse_2 = 0;
+  rd_stats->sse_3 = 0;
 #endif
 }
 
@@ -379,7 +417,141 @@ static INLINE void av1_merge_rd_stats(RD_STATS *rd_stats_dst,
     }
   }
 #endif
+#if CONFIG_ONE_PASS_SVM
+  rd_stats_dst->eob += rd_stats_src->eob;
+  rd_stats_dst->eob_0 += rd_stats_src->eob_0;
+  rd_stats_dst->eob_1 += rd_stats_src->eob_1;
+  rd_stats_dst->eob_2 += rd_stats_src->eob_2;
+  rd_stats_dst->eob_3 += rd_stats_src->eob_3;
+
+  rd_stats_dst->rd += rd_stats_src->rd;
+  rd_stats_dst->rd_0 += rd_stats_src->rd_0;
+  rd_stats_dst->rd_1 += rd_stats_src->rd_1;
+  rd_stats_dst->rd_2 += rd_stats_src->rd_2;
+  rd_stats_dst->rd_3 += rd_stats_src->rd_3;
+
+  rd_stats_dst->y_sse += rd_stats_src->y_sse;
+  rd_stats_dst->sse_0 += rd_stats_src->sse_0;
+  rd_stats_dst->sse_1 += rd_stats_src->sse_1;
+  rd_stats_dst->sse_2 += rd_stats_src->sse_2;
+  rd_stats_dst->sse_3 += rd_stats_src->sse_3;
+#endif
 }
+
+#if CONFIG_ONE_PASS_SVM
+static INLINE void av1_add_reg_stat(RD_STATS *rd_stats, int eob, int64_t rd,
+                                    int64_t sse, int blk_row, int blk_col,
+                                    BLOCK_SIZE bsize, BLOCK_SIZE crop_bsize) {
+  // Size of TX block and SB
+  int block_width_mi = mi_size_wide[bsize];
+  int block_height_mi = mi_size_high[bsize];
+  int crop_width_mi = mi_size_wide[crop_bsize];
+  int crop_height_mi = mi_size_high[crop_bsize];
+
+  // Get how much the tx_block in in each quadrant of the macroblock
+  int height_in_0 = AOMMIN(block_height_mi / 2 - blk_row, crop_height_mi);
+  int height_not_in_0 = crop_height_mi - height_in_0;
+  int width_in_0 = AOMMIN(block_width_mi / 2 - blk_col, crop_width_mi);
+  int width_not_in_0 = crop_width_mi - width_in_0;
+
+  // Increment the eob proportionally to how much the tx_block overlaps with
+  // each quadrant. We will scale it by MAX_MIB_SIZE * MAX_MIB_SIZE to avoid
+  // being truncated.
+  int scaling_factor =
+      MAX_MIB_SIZE * MAX_MIB_SIZE / (crop_width_mi * crop_height_mi);
+
+  // Update the stats
+  rd_stats->eob = eob;
+  rd_stats->eob_0 = eob * (height_in_0 * width_in_0) * scaling_factor;
+  rd_stats->eob_1 = eob * (height_in_0 * width_not_in_0) * scaling_factor;
+  rd_stats->eob_2 = eob * (height_not_in_0 * width_in_0) * scaling_factor;
+  rd_stats->eob_3 = eob * (height_not_in_0 * width_not_in_0) * scaling_factor;
+
+  rd_stats->rd = rd;
+  rd_stats->rd_0 = rd * (height_in_0 * width_in_0) * scaling_factor;
+  rd_stats->rd_1 = rd * (height_in_0 * width_not_in_0) * scaling_factor;
+  rd_stats->rd_2 = rd * (height_not_in_0 * width_in_0) * scaling_factor;
+  rd_stats->rd_3 = rd * (height_not_in_0 * width_not_in_0) * scaling_factor;
+
+  rd_stats->y_sse = sse;
+  rd_stats->sse_0 = sse * (height_in_0 * width_in_0) * scaling_factor;
+  rd_stats->sse_1 = sse * (height_in_0 * width_not_in_0) * scaling_factor;
+  rd_stats->sse_2 = sse * (height_not_in_0 * width_in_0) * scaling_factor;
+  rd_stats->sse_3 = sse * (height_not_in_0 * width_not_in_0) * scaling_factor;
+}
+
+static INLINE void av1_reg_stat_skipmode_update(RD_STATS *rd_stats,
+                                                int rdmult) {
+  // Update the stats
+  rd_stats->eob = 0;
+  rd_stats->eob_0 = 0;
+  rd_stats->eob_1 = 0;
+  rd_stats->eob_2 = 0;
+  rd_stats->eob_3 = 0;
+
+  rd_stats->rd = RDCOST(rdmult, 0, rd_stats->sse);
+  rd_stats->rd_0 = RDCOST(rdmult, 0, rd_stats->sse_0);
+  rd_stats->rd_1 = RDCOST(rdmult, 0, rd_stats->sse_1);
+  rd_stats->rd_2 = RDCOST(rdmult, 0, rd_stats->sse_2);
+  rd_stats->rd_3 = RDCOST(rdmult, 0, rd_stats->sse_3);
+}
+
+static INLINE void av1_copy_reg_stat(RD_STATS *rd_stats_dst,
+                                     RD_STATS *rd_stats_src) {
+  rd_stats_dst->eob = rd_stats_src->eob;
+  rd_stats_dst->eob_0 = rd_stats_src->eob_0;
+  rd_stats_dst->eob_1 = rd_stats_src->eob_1;
+  rd_stats_dst->eob_2 = rd_stats_src->eob_2;
+  rd_stats_dst->eob_3 = rd_stats_src->eob_3;
+
+  rd_stats_dst->rd = rd_stats_src->rd;
+  rd_stats_dst->rd_0 = rd_stats_src->rd_0;
+  rd_stats_dst->rd_1 = rd_stats_src->rd_1;
+  rd_stats_dst->rd_2 = rd_stats_src->rd_2;
+  rd_stats_dst->rd_3 = rd_stats_src->rd_3;
+
+  rd_stats_dst->y_sse = rd_stats_src->y_sse;
+  rd_stats_dst->sse_0 = rd_stats_src->sse_0;
+  rd_stats_dst->sse_1 = rd_stats_src->sse_1;
+  rd_stats_dst->sse_2 = rd_stats_src->sse_2;
+  rd_stats_dst->sse_3 = rd_stats_src->sse_3;
+}
+
+static INLINE void av1_unpack_reg_stat(RD_STATS *rd_stats, int *eob, int *eob_0,
+                                       int *eob_1, int *eob_2, int *eob_3,
+                                       int64_t *rd, int64_t *rd_0,
+                                       int64_t *rd_1, int64_t *rd_2,
+                                       int64_t *rd_3) {
+  *rd = rd_stats->rd;
+  *rd_0 = rd_stats->rd_0;
+  *rd_1 = rd_stats->rd_1;
+  *rd_2 = rd_stats->rd_2;
+  *rd_3 = rd_stats->rd_3;
+
+  *eob = rd_stats->eob;
+  *eob_0 = rd_stats->eob_0;
+  *eob_1 = rd_stats->eob_1;
+  *eob_2 = rd_stats->eob_2;
+  *eob_3 = rd_stats->eob_3;
+}
+
+static INLINE void av1_set_reg_stat(RD_STATS *rd_stats, int eob, int eob_0,
+                                    int eob_1, int eob_2, int eob_3, int64_t rd,
+                                    int64_t rd_0, int64_t rd_1, int64_t rd_2,
+                                    int64_t rd_3) {
+  rd_stats->rd = rd;
+  rd_stats->rd_0 = rd_0;
+  rd_stats->rd_1 = rd_1;
+  rd_stats->rd_2 = rd_2;
+  rd_stats->rd_3 = rd_3;
+
+  rd_stats->eob = eob;
+  rd_stats->eob_0 = eob_0;
+  rd_stats->eob_1 = eob_1;
+  rd_stats->eob_2 = eob_2;
+  rd_stats->eob_3 = eob_3;
+}
+#endif
 
 struct TileInfo;
 struct TileDataEnc;
