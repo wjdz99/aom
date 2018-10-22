d02e91fbe547644afc90aa795f87bf19ece7d107
Yue Chen <yuec@google.com>
2018-10-22 14:49:15 -0700

    Skip obmc and wm if mv field is identical
    Check if the neighbors' motions used by obmc have same parameters
    as for the current block. If all the parameters are identical, as
    a speed feature, we skip trying obmc and warped motion modes.
    
    Test run at speed 0
    Compression performance loss: 0.59% midres, 0.21% lowres
    Speed up: ~1%
    
    Change-Id: Ie4abfe50c0eb5b785f75b27101dcbfc3ddbb94a5

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -8922,6 +8922,8 @@ static int64_t motion_mode_rd(const AV1_COMP *const cpi, MACROBLOCK *const x,
       av1_is_interp_needed(xd) ? av1_get_switchable_rate(cm, x, xd) : 0;
   int64_t best_rd = INT64_MAX;
   int best_rate_mv = rate_mv0;
+  int identical_obmc_mv_field =
+      av1_check_identical_obmc_mv_field(cm, xd, mi_row, mi_col);
   for (int mode_index = (int)SIMPLE_TRANSLATION;
        mode_index <= (int)last_motion_mode_allowed + interintra_allowed;
        mode_index++) {
@@ -8941,6 +8943,12 @@ static int64_t motion_mode_rd(const AV1_COMP *const cpi, MACROBLOCK *const x,
     } else {
       mbmi->motion_mode = (MOTION_MODE)mode_index;
       assert(mbmi->ref_frame[1] != INTRA_FRAME);
+    }
+
+    if (identical_obmc_mv_field) {
+      if (mbmi->motion_mode == OBMC_CAUSAL ||
+          mbmi->motion_mode == WARPED_CAUSAL)
+        continue;
     }
 
     if (mbmi->motion_mode == SIMPLE_TRANSLATION && !is_interintra_mode) {
