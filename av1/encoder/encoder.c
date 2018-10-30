863d58cf965732e225e2ff6c15cd7232e44d15ca
Yunqing Wang <yunqingwang@google.com>
2018-10-30 15:54:47 -0700

    wip: lr test
    Change-Id: I347f309d8e915ac38fbd6016311d0ee6135a5faf

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -4307,7 +4307,8 @@ static void loopfilter_frame(AV1_COMP *cpi, AV1_COMMON *cm) {
   const int no_cdef =
       !cm->seq_params.enable_cdef || cm->coded_lossless || cm->large_scale_tile;
   const int no_restoration = !cm->seq_params.enable_restoration ||
-                             cm->all_lossless || cm->large_scale_tile;
+                             cm->all_lossless || cm->large_scale_tile ||
+                             !cpi->sf.use_restoration;
 
   struct loopfilter *lf = &cm->lf;
 
