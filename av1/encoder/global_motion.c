0df40cb42d5f66a3dc57a4eac1d50e376a2d3c42
Sarah Parker <sarahparker@google.com>
2018-11-13 16:15:15 -0800

    WIP Compute flow field at each pyramid level
    Change-Id: I132eecb64d1606a541f27cefe3e8fad4ffe9a305

--- av1/encoder/global_motion.c
+++ av1/encoder/global_motion.c
@@ -16,6 +16,7 @@
 #include <assert.h>
 
 #include "config/aom_dsp_rtcd.h"
+#include "aom_ports/system_state.h"
 
 #include "av1/encoder/global_motion.h"
 
@@ -32,17 +33,25 @@
 #define MIN_INLIER_PROB 0.1
 
 #define MIN_TRANS_THRESH (1 * GM_TRANS_DECODE_FACTOR)
-#define USE_GM_FEATURE_BASED 1
+#define USE_GM_FEATURE_BASED 0
 
 // Border over which to compute the global motion
 #define ERRORADV_BORDER 0
 
 // Number of pyramid levels in disflow computation
-#define N_LEVELS 5
+#define N_LEVELS 4
 // Size of square patches in the disflow dense grid
-#define PATCH_SIZE 5
+#define PATCH_SIZE 8
+// Center point of square patch
+#define PATCH_CENTER ((PATCH_SIZE + 1) >> 1)
+// Step size between patches, lower value means greater patch overlap
+#define PATCH_STEP 1
 // Minimum size of border padding for disflow
 #define MIN_PAD 7
+// Warp error convergence threshold for disflow
+#define DISFLOW_ERROR_TR 0.01
+// Max number of iterations if warp convergence is not found
+#define DISFLOW_MAX_ITR 10
 
 // Struct for an image pyramid
 typedef struct {
@@ -319,6 +328,12 @@ static int compute_global_motion_feature_based(
 
   // Return true if any one of the motions has inliers.
   for (i = 0; i < num_motions; ++i) {
+    printf("motion %f %f %f %f %f %f\n", params_by_motion[0],
+                                params_by_motion[1],
+                                params_by_motion[2],
+                                params_by_motion[3],
+                                params_by_motion[4],
+                                params_by_motion[5]);
     if (num_inliers_by_motion[i] > 0) return 1;
   }
   return 0;
@@ -332,6 +347,35 @@ get_ransac_double_prec_type(TransformationType type) {
     case TRANSLATION: return ransac_translation_double_prec;
     default: assert(0); return NULL;
   }
+}
+
+// Don't use points around the frame border since they are less reliable
+static INLINE int valid_point(int x, int y, int width, int height) {
+  return (x > (PATCH_SIZE + PATCH_CENTER)) &&
+         (x < (width - PATCH_SIZE - PATCH_CENTER)) &&
+         (y > (PATCH_SIZE + PATCH_CENTER)) &&
+         (y < (height - PATCH_SIZE - PATCH_CENTER));
+}
+
+static int determine_disflow_correspondence(int *frm_corners,
+                                            int num_frm_corners, double *flow_u,
+                                            double *flow_v, int width,
+                                            int height, int stride,
+                                            double *correspondences) {
+  int num_correspondences = 0;
+  int x, y;
+  for (int i = 0; i < num_frm_corners; ++i) {
+    x = frm_corners[2 * i];
+    y = frm_corners[2 * i + 1];
+    if (valid_point(x, y, width, height)) {
+      correspondences[4 * num_correspondences] = x;
+      correspondences[4 * num_correspondences + 1] = y;
+      correspondences[4 * num_correspondences + 2] = x + flow_u[y * stride + x];
+      correspondences[4 * num_correspondences + 3] = y + flow_v[y * stride + x];
+      num_correspondences++;
+    }
+  }
+  return num_correspondences;
 }
 
 double getCubicValue(double p[4], double x) {
@@ -436,22 +480,39 @@ unsigned char interpolate(unsigned char *ref, double x, double y, int width,
 
 // Warps a block using flow vector [u, v] and computes the mse
 double compute_warp_and_error(unsigned char *ref, unsigned char *frm, int width,
-                              int height, int stride, double u, double v) {
+                              int height, int stride, int x, int y, double u, double v,
+                              int16_t *dt) {
   int i, j;
-  double warped, x, y;
+  unsigned char warped;
+  double x_w, y_w;
   double mse = 0;
-  double err = 0;
-  for (i = 0; i < height; ++i)
-    for (j = 0; j < width; ++j) {
-      x = (double)j - u;
-      y = (double)i - v;
-      warped = interpolate(ref, x, y, width, height, stride);
+  int16_t err = 0;
+  for (i = y; i < y + PATCH_SIZE; ++i)
+    for (j = x; j < x + PATCH_SIZE; ++j) {
+      x_w = (double)j + u;
+      y_w = (double)i + v;
+      warped = interpolate(ref, x_w, y_w, width, height, stride);
       err = warped - frm[j + i * stride];
       mse += err * err;
+      dt[(i - y) * PATCH_SIZE + (j - x)] = err;
     }
 
-  mse /= (width * height);
+  mse /= (PATCH_SIZE * PATCH_SIZE);
   return mse;
+}
+
+void compute_warp(unsigned char *ref, unsigned char *frm, int width,
+                    int height, int stride, double u, double v) {
+  int i, j;
+  unsigned char warped;
+  double x, y;
+  for (i = 0; i < height; ++i)
+    for (j = 0; j < width; ++j) {
+      x = (double)j + u;
+      y = (double)i + v;
+      warped = interpolate(ref, x, y, width, height, stride);
+      frm[i * stride + j] = warped;
+    }
 }
 
 // Computes the components of the system of equations used to solve for
@@ -465,19 +526,21 @@ double compute_warp_and_error(unsigned char *ref, unsigned char *frm, int width,
 // 2.)   b = |sum(dx * dt)|
 //           |sum(dy * dt)|
 // Where the sums are computed over a square window of PATCH_SIZE.
-static INLINE void compute_flow_system(const double *dx, const double *dy,
-                                       const double *dt, int stride, double *M,
-                                       double *b) {
+static INLINE void compute_flow_system(const double *dx, int dx_stride,
+                                       const double *dy, int dy_stride,
+                                       const int16_t *dt, int dt_stride,
+                                       double *M, double *b) {
   for (int i = 0; i < PATCH_SIZE; i++) {
     for (int j = 0; j < PATCH_SIZE; j++) {
-      M[0] += dx[i * stride + j] * dx[i * stride + j];
-      M[1] += dx[i * stride + j] * dy[i * stride + j];
-      M[3] += dy[i * stride + j] * dy[i * stride + j];
+      M[0] += dx[i * dx_stride + j] * dx[i * dx_stride + j];
+      M[1] += dx[i * dx_stride + j] * dy[i * dy_stride + j];
+      M[3] += dy[i * dy_stride + j] * dy[i * dy_stride + j];
 
-      b[0] += dx[i * stride + j] * dt[i * stride + j];
-      b[1] += dy[i * stride + j] * dt[i * stride + j];
+      b[0] += dx[i * dx_stride + j] * dt[i * dt_stride + j];
+      b[1] += dy[i * dy_stride + j] * dt[i * dt_stride + j];
     }
   }
+
   M[2] = M[1];
 }
 
@@ -490,8 +553,9 @@ static INLINE void solve_2x2_system(const double *M, const double *b,
   if (det < 1e-5) {
     // Handle singular matrix
     // TODO(sarahparker) compare results using pseudo inverse instead
-    M_0 += 1e-10;
-    M_3 += 1e-10;
+    printf("SINGLULAR ");
+    M_0 += 0.1;
+    M_3 += 0.1;
     det = (M_0 * M_3) - (M[1] * M[2]);
   }
   const double det_inv = 1 / det;
@@ -507,11 +571,16 @@ static INLINE void image_difference(const uint8_t *src, int src_stride,
                                     int width) {
   const int block_unit = 8;
   // Take difference in 8x8 blocks to make use of optimized diff function
-  for (int i = 0; i < height; i += block_unit) {
-    for (int j = 0; j < width; j += block_unit) {
+  for (int i = 0; i < height; i += 1) {
+    for (int j = 0; j < width; j += 1) {
+//for (int i = 0; i < height; i += block_unit) {
+//  for (int j = 0; j < width; j += block_unit) {
+        dst[i * dst_stride + j] = 1 * (src[i * src_stride + j] - ref[i * ref_stride + j]);
+/*
       aom_subtract_block(block_unit, block_unit, dst + i * dst_stride + j,
                          dst_stride, src + i * src_stride + j, src_stride,
                          ref + i * ref_stride + j, ref_stride);
+*/
     }
   }
 }
@@ -523,7 +592,7 @@ static INLINE void image_difference(const uint8_t *src, int src_stride,
 static INLINE void sobel_xy_image_gradient(const uint8_t *src, int src_stride,
                                            double *dst, int dst_stride,
                                            int height, int width, int dir) {
-  double norm = 1.0 / 8;
+  double norm = 1.0;// / 8;
   // TODO(sarahparker) experiment with doing this over larger block sizes
   const int block_unit = 8;
   // Filter in 8x8 blocks to eventually make use of optimized convolve function
@@ -605,6 +674,27 @@ static void compute_flow_pyramids(unsigned char *frm, const int frm_width,
                    frm_pyr->level_buffer + frm_pyr->level_loc[0],
                    frm_pyr->heights[0], frm_pyr->widths[0],
                    frm_pyr->strides[0]);
+        aom_clear_system_state();
+
+    if (compute_grad) {
+    cur_width = frm_pyr->widths[0];
+    cur_height = frm_pyr->heights[0];
+    cur_stride = frm_pyr->strides[0];
+    cur_loc = frm_pyr->level_loc[0];
+      assert(frm_pyr->has_gradient && frm_pyr->level_dx_buffer != NULL &&
+             frm_pyr->level_dy_buffer != NULL);
+      // Computation x gradient
+      sobel_xy_image_gradient(frm_pyr->level_buffer + cur_loc, cur_stride,
+                              frm_pyr->level_dx_buffer + cur_loc, cur_stride,
+                              cur_height, cur_width, 1);
+        aom_clear_system_state();
+
+      // Computation y gradient
+      sobel_xy_image_gradient(frm_pyr->level_buffer + cur_loc, cur_stride,
+                              frm_pyr->level_dy_buffer + cur_loc, cur_stride,
+                              cur_height, cur_width, 0);
+        aom_clear_system_state();
+    }
 
   // Start at the finest level and resize down to the coarsest level
   for (int level = 1; level < n_levels; ++level) {
@@ -619,6 +709,7 @@ static void compute_flow_pyramids(unsigned char *frm, const int frm_width,
                      frm_pyr->strides[level - 1],
                      frm_pyr->level_buffer + cur_loc, cur_height, cur_width,
                      cur_stride);
+        aom_clear_system_state();
 
     if (compute_grad) {
       assert(frm_pyr->has_gradient && frm_pyr->level_dx_buffer != NULL &&
@@ -627,13 +718,119 @@ static void compute_flow_pyramids(unsigned char *frm, const int frm_width,
       sobel_xy_image_gradient(frm_pyr->level_buffer + cur_loc, cur_stride,
                               frm_pyr->level_dx_buffer + cur_loc, cur_stride,
                               cur_height, cur_width, 1);
+        aom_clear_system_state();
 
       // Computation y gradient
       sobel_xy_image_gradient(frm_pyr->level_buffer + cur_loc, cur_stride,
                               frm_pyr->level_dy_buffer + cur_loc, cur_stride,
                               cur_height, cur_width, 0);
+        aom_clear_system_state();
     }
   }
+}
+
+// TODO(sarahparker) make sure all these buffers can use the same stride
+static INLINE void compute_flow_at_point(unsigned char *frm, unsigned char *ref,
+                                         double *dx, double *dy,
+                                         int x, int y,
+                                         int width, int height, int stride,
+                                         double *u, double *v) {
+  double M[4] = { 0 };
+  double b[2] = { 0 };
+  double tmp_output_vec[2] = { 0 };
+  double error = 0;
+  int16_t dt[PATCH_SIZE * PATCH_SIZE];
+  double o_u = *u;
+  double o_v = *v;
+
+  for (int itr = 0; itr < DISFLOW_MAX_ITR; itr++) {
+    error = compute_warp_and_error(ref, frm,
+                                   width, height, stride, x, y, *u, *v, dt);
+        aom_clear_system_state();
+    if (error <= DISFLOW_ERROR_TR) break;
+    compute_flow_system(dx, stride, dy, stride, dt, PATCH_SIZE, M, b);
+    solve_2x2_system(M, b, tmp_output_vec);
+    *u += tmp_output_vec[0];
+    *v += tmp_output_vec[1];
+  }
+    if (fabs(*u - o_u) > PATCH_SIZE || fabs(*v - o_u) > PATCH_SIZE) {
+      *u = o_u;
+      *v = o_v;
+    }
+}
+
+// make sure flow_u and flow_v start at 0
+static void compute_flow_field(ImagePyramid *frm_pyr, ImagePyramid *ref_pyr,
+                               double *flow_u, double *flow_v) {
+  int cur_width, cur_height, cur_stride, cur_loc, patch_loc, patch_center;
+  double *u_upscale =
+      aom_malloc(frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_u));
+  double *v_upscale =
+      aom_malloc(frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_v));
+
+  assert(frm_pyr->n_levels == ref_pyr->n_levels);
+
+  // Compute flow field from coarsest to finest level of the pyramid
+  for (int level = frm_pyr->n_levels - 1; level >= 0; --level) {
+    cur_width = frm_pyr->widths[level];
+    cur_height = frm_pyr->heights[level];
+    cur_stride = frm_pyr->strides[level];
+    cur_loc = frm_pyr->level_loc[level];
+        aom_clear_system_state();
+
+    for (int i = PATCH_SIZE; i < cur_height - PATCH_SIZE; i += PATCH_STEP) {
+      for (int j = PATCH_SIZE; j < cur_width - PATCH_SIZE; j += PATCH_STEP) {
+        patch_loc = i * cur_stride + j;
+        patch_center = patch_loc + PATCH_CENTER * cur_stride + PATCH_CENTER;
+        compute_flow_at_point(frm_pyr->level_buffer + cur_loc,
+                              ref_pyr->level_buffer + cur_loc,
+                              frm_pyr->level_dx_buffer + cur_loc + patch_loc,
+                              frm_pyr->level_dy_buffer + cur_loc + patch_loc,
+                              j, i,
+                              cur_width, cur_height, cur_stride,
+                              flow_u + patch_center, flow_v + patch_center);
+      }
+    }
+    /*
+     *print out mse image for debugging
+    printf("\n\n\nmse2%d = [", level);
+    for (int i = 0; i < cur_height; i++) {
+      for (int j = 0; j < cur_width; j++) {
+        //int error = compute_warp_and_error(ref_pyr->level_buffer + cur_loc, frm_pyr->level_buffer + cur_loc,
+         //                          cur_width, cur_height, cur_stride, j, i, flow_u[i * cur_stride + j], flow_v[i * cur_stride + j], dt);
+
+      double x_w = (double)j + flow_u[i * cur_stride + j];
+      double y_w = (double)i + flow_v[i * cur_stride + j];;
+      int warped = interpolate(ref_pyr->level_buffer + cur_loc, x_w, y_w, cur_width, cur_height, cur_stride);
+      int err = warped - (frm_pyr->level_buffer + cur_loc)[j + i * cur_stride];
+        printf("%d ", err * err);
+      }
+      printf(";\n");
+    }
+    printf("]\n\n\n");
+    */
+        aom_clear_system_state();
+    // TODO(sarahparker) Upscale the flow field if we are not at the top level
+    if (level > 0) {
+      int h_upscale = frm_pyr->heights[level - 1];
+      int w_upscale = frm_pyr->widths[level - 1];
+      int s_upscale = frm_pyr->strides[level - 1];
+      for (int i = 0; i < h_upscale; ++i) {
+        for (int j = 0; j < w_upscale; ++j) {
+          u_upscale[j + i * s_upscale] = flow_u[(int)(j >> 1) +
+                                                 (int)(i >> 1) * cur_stride];
+          v_upscale[j + i * s_upscale] = flow_v[(int)(j >> 1) +
+                                                 (int)(i >> 1) * cur_stride];
+        }
+      }
+        aom_clear_system_state();
+   // memcpy(flow_u, u_upscale, frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_u));
+   // memcpy(flow_v, v_upscale, frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_v));
+    }
+  }
+        aom_clear_system_state();
+  aom_free(u_upscale);
+  aom_free(v_upscale);
 }
 
 static int compute_global_motion_disflow_based(
@@ -647,6 +844,11 @@ static int compute_global_motion_disflow_based(
   const int ref_width = ref->y_width;
   const int ref_height = ref->y_height;
   const int pad_size = AOMMAX(PATCH_SIZE, MIN_PAD);
+  int num_frm_corners;
+  int num_correspondences;
+  double *correspondences;
+  int frm_corners[2 * MAX_CORNERS];
+  RansacFuncDouble ransac = get_ransac_double_prec_type(type);
   assert(frm_width == ref_width);
   assert(frm_height == ref_height);
 
@@ -663,6 +865,12 @@ static int compute_global_motion_disflow_based(
   if (ref->flags & YV12_FLAG_HIGHBITDEPTH) {
     ref_buffer = downconvert_frame(ref, bit_depth);
   }
+        aom_clear_system_state();
+
+  /*
+  compute_warp(ref_buffer, frm_buffer, frm_width,
+                    frm_height, frm->y_stride, 2.0, 2.0);
+*/
 
   // TODO(sarahparker) We will want to do the source pyramid computation
   // outside of this function so it doesn't get recomputed for every
@@ -683,14 +891,56 @@ static int compute_global_motion_disflow_based(
   compute_flow_pyramids(ref_buffer, ref_width, ref_height, ref->y_stride,
                         n_levels, pad_size, compute_gradient, ref_pyr);
 
-  // TODO(sarahparker) Implement the rest of DISFlow, currently only the image
-  // pyramid is implemented.
-  (void)num_inliers_by_motion;
-  (void)params_by_motion;
-  (void)num_motions;
-  (void)type;
+  double *flow_u =
+      aom_malloc(frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_u));
+  double *flow_v =
+      aom_malloc(frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_v));
+
+  memset(flow_u, 0,
+         frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_u));
+  memset(flow_v, 0,
+         frm_pyr->strides[0] * frm_pyr->heights[0] * sizeof(*flow_v));
+
+  compute_flow_field(frm_pyr, ref_pyr, flow_u, flow_v);
+        aom_clear_system_state();
+
+  // compute interest points in images using FAST features
+  num_frm_corners = fast_corner_detect(frm_buffer, frm_width, frm_height,
+                                       frm->y_stride, frm_corners, MAX_CORNERS);
+  // find correspondences between the two images using the flow field
+  //correspondences = aom_malloc(num_frm_corners * 4 * sizeof(*correspondences));
+  correspondences = aom_malloc(frm_width * frm_height * 4 * sizeof(*correspondences));
+  num_correspondences = determine_disflow_correspondence(
+      frm_corners, num_frm_corners, flow_u, flow_v, frm_width, frm_height,
+      frm_pyr->strides[0], correspondences);
+        aom_clear_system_state();
+  ransac(correspondences, num_correspondences, num_inliers_by_motion,
+         params_by_motion, num_motions);
+        aom_clear_system_state();
+
   free_pyramid(frm_pyr);
   free_pyramid(ref_pyr);
+  aom_free(correspondences);
+  aom_free(flow_u);
+  aom_free(flow_v);
+  // Set num_inliers = 0 for motions with too few inliers so they are ignored.
+  for (int i = 0; i < num_motions; ++i) {
+    if (num_inliers_by_motion[i] < MIN_INLIER_PROB * num_correspondences) {
+      num_inliers_by_motion[i] = 0;
+    }
+  }
+
+  // Return true if any one of the motions has inliers.
+  for (int i = 0; i < num_motions; ++i) {
+    printf("motion %f %f %f %f %f %f\n", params_by_motion[0],
+                                params_by_motion[1],
+                                params_by_motion[2],
+                                params_by_motion[3],
+                                params_by_motion[4],
+                                params_by_motion[5]);
+    if (num_inliers_by_motion[i] > 0)
+      return 1;
+  }
   return 0;
 }
 #endif
