1478529d8585ad0918ca84a58fc0daa73badf8bc
Ranjit Kumar Tulabandu <ranjit.tulabandu@ittiam.com>
2018-12-29 13:23:33 +0530

    Skip tx_type search in first partition search pass
    In first pass of 2 pass partition search, final tx type search is
    skipped in sf_refine_fast_tx_type_search(). This change is applicable
    for speed >= 4.
    
    For speed = 4 preset, BD-rate impact is seen as 0.00% (as per AWCY
    runs) with encode time reduction of 1.1% (averaged across multiple
    test-cases)
    
    STATS_CHANGED
    
    Change-Id: I55d690d5ae36afcd3d5d453bd77a3e849fedce5d

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -3082,7 +3082,8 @@ static int64_t search_txk_type(const AV1_COMP *cpi, MACROBLOCK *x, int plane,
   TX_TYPE txk_end = TX_TYPES - 1;
   if ((!is_inter && x->use_default_intra_tx_type) ||
       (is_inter && x->use_default_inter_tx_type)) {
-    txk_start = txk_end = get_default_tx_type(0, xd, tx_size);
+    txk_start = txk_end =
+        get_default_tx_type(0, xd, tx_size, cpi->is_screen_content_type);
   } else if (x->rd_model == LOW_TXFM_RD || x->cb_partition_scan) {
     if (plane == 0) txk_end = DCT_DCT;
   }
@@ -10372,7 +10373,8 @@ static void sf_refine_fast_tx_type_search(
       ((sf->tx_type_search.fast_inter_tx_type_search == 1 &&
         is_inter_mode(best_mbmode->mode)) ||
        (sf->tx_type_search.fast_intra_tx_type_search == 1 &&
-        !is_inter_mode(best_mbmode->mode)))) {
+        !is_inter_mode(best_mbmode->mode))) &&
+      (!x->cb_partition_scan)) {
     int skip_blk = 0;
     RD_STATS rd_stats_y, rd_stats_uv;
     const int skip_ctx = av1_get_skip_context(xd);
