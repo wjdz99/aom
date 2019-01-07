3fba8d5b82297691e7663ba15e355f77c4d7ddf6
Ranjit Kumar Tulabandu <ranjit.tulabandu@ittiam.com>
2019-01-07 13:57:06 +0530

    Disable coeff R-D optimization during eval stage
    During default transform type decision, R-D optimization of coeffs is
    disabled. However coeff optimization is kept enabled during transform
    type evaluation. This change is applicable for speed >=4
    
    STATS_CHANGED
    
    Change-Id: Iba934b6cfb4e1e45da2a2aec91f8d4d41af356c0

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -3033,6 +3033,7 @@ static int64_t search_txk_type(const AV1_COMP *cpi, MACROBLOCK *x, int plane,
   tran_low_t *best_dqcoeff = this_dqcoeff;
   const int txk_type_idx =
       av1_get_txk_type_index(plane_bsize, blk_row, blk_col);
+  int optimize_b = 1;
   av1_invalid_rd_stats(best_rd_stats);
 
   TXB_RD_INFO *intra_txb_rd_info = NULL;
@@ -3085,6 +3086,7 @@ static int64_t search_txk_type(const AV1_COMP *cpi, MACROBLOCK *x, int plane,
       (is_inter && x->use_default_inter_tx_type)) {
     txk_start = txk_end =
         get_default_tx_type(0, xd, tx_size, cpi->is_screen_content_type);
+    optimize_b = 0;
   } else if (x->rd_model == LOW_TXFM_RD || x->cb_partition_scan) {
     if (plane == 0) txk_end = DCT_DCT;
   }
@@ -3174,19 +3176,25 @@ static int64_t search_txk_type(const AV1_COMP *cpi, MACROBLOCK *x, int plane,
     } else {
       av1_xform_quant(cm, x, plane, block, blk_row, blk_col, plane_bsize,
                       tx_size, tx_type, AV1_XFORM_QUANT_FP);
-      if (cpi->sf.optimize_b_precheck && best_rd < INT64_MAX &&
-          eobs_ptr[block] >= 4) {
-        // Calculate distortion quickly in transform domain.
-        dist_block_tx_domain(x, plane, block, tx_size, &this_rd_stats.dist,
-                             &this_rd_stats.sse);
+      if (optimize_b) {
+        if (cpi->sf.optimize_b_precheck && best_rd < INT64_MAX &&
+            eobs_ptr[block] >= 4) {
+          // Calculate distortion quickly in transform domain.
+          dist_block_tx_domain(x, plane, block, tx_size, &this_rd_stats.dist,
+                               &this_rd_stats.sse);
 
-        const int64_t best_rd_ = AOMMIN(best_rd, ref_best_rd);
-        const int64_t dist_cost_estimate =
-            RDCOST(x->rdmult, 0, AOMMIN(this_rd_stats.dist, this_rd_stats.sse));
-        if (dist_cost_estimate - (dist_cost_estimate >> 3) > best_rd_) continue;
+          const int64_t best_rd_ = AOMMIN(best_rd, ref_best_rd);
+          const int64_t dist_cost_estimate = RDCOST(
+              x->rdmult, 0, AOMMIN(this_rd_stats.dist, this_rd_stats.sse));
+          if (dist_cost_estimate - (dist_cost_estimate >> 3) > best_rd_)
+            continue;
+        }
+        av1_optimize_b(cpi, x, plane, block, tx_size, tx_type, txb_ctx, 1,
+                       &rate_cost);
+      } else {
+        rate_cost = av1_cost_coeffs(cm, x, plane, block, tx_size, tx_type,
+                                    txb_ctx, use_fast_coef_costing);
       }
-      av1_optimize_b(cpi, x, plane, block, tx_size, tx_type, txb_ctx, 1,
-                     &rate_cost);
     }
     if (eobs_ptr[block] == 0) {
       // When eob is 0, pixel domain distortion is more efficient and accurate.
