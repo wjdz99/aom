440a404efceaf9d1fb68055a9dcc64098fded6df
Venkat <sanampudi.venkatarao@ittiam.com>
2019-01-23 07:14:26 +0530

    Refine R-D optimization of coeffs for boosted frames
    For speed = 3 and 4 presets, BD-rate impact is seen as 0.23% and 0.13% (as per AWCY runs),
    with encode time reduction of 8.89% and 4.21% (averaged across multiple test cases) respectively.
    
    STATS_CHANGED
    
    Change-Id: I86ca2f54e7f5f086df4d2fd91aa0ae3044acf0c2

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -331,7 +331,7 @@ static void set_good_speed_features_framesize_independent(
     // TODO(yunqing): evaluate this speed feature for speed 1 & 2, and combine
     // it with cpi->sf.disable_wedge_search_var_thresh.
     sf->disable_wedge_interintra_search = 1;
-    sf->perform_coeff_opt = boosted ? 0 : 3;
+    sf->perform_coeff_opt = frame_is_intra_only(cm) ? 2 : 3;
   }
 
   if (speed >= 4) {
@@ -346,7 +346,7 @@ static void set_good_speed_features_framesize_independent(
     sf->cb_partition_search = !boosted;
     sf->alt_ref_search_fp = 1;
     sf->skip_sharp_interp_filter_search = 1;
-    sf->perform_coeff_opt = boosted ? 0 : 4;
+    sf->perform_coeff_opt = frame_is_intra_only(cm) ? 2 : 4;
     sf->adaptive_txb_search_level = boosted ? 2 : 3;
   }
 
