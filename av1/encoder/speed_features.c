20b9e09f8578074d5a31e97626fbf2cbb079e709
Venkat <sanampudi.venkatarao@ittiam.com>
2019-01-24 18:08:06 +0530

    Refine compound_type_rd using model_rd
    STATS_CHANGED
    Change-Id: I6877f6d07707e4209ab882bbc2dd4390d7862fd3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -335,6 +335,7 @@ static void set_good_speed_features_framesize_independent(
     // it with cpi->sf.disable_wedge_search_var_thresh.
     sf->disable_wedge_interintra_search = 1;
     sf->perform_coeff_opt = is_boosted_arf2_bwd_type ? 2 : 3;
+    sf->prune_comp_type_by_model_rd = 1;
   }
 
   if (speed >= 4) {
@@ -783,6 +784,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
   sf->prune_warp_using_wmtype = 0;
   sf->disable_wedge_interintra_search = 0;
   sf->perform_coeff_opt = 0;
+  sf->prune_comp_type_by_model_rd = 0;
 
   if (oxcf->mode == GOOD)
     set_good_speed_features_framesize_independent(cpi, sf, speed);
