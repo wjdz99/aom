92c56ee35b1bbdc3024f0485147f7a31ac4cd650
Venkat <sanampudi.venkatarao@ittiam.com>
2019-01-24 18:08:06 +0530

    Refine compound_type_rd based on model_rd
    Gate compound_type wedge and segment estimate_yrd_for_sb evaluation using model_rd value.
    
    For speed = 3 and 4 presets, BD-rate impact is seen as 0.02% and 0.01% (as per AWCY runs),
    with encode time reduction of 1.00% and 2.80% (averaged across multiple test cases) respectively.
    
    STATS_CHANGED
    Change-Id: I6877f6d07707e4209ab882bbc2dd4390d7862fd3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -335,6 +335,7 @@ static void set_good_speed_features_framesize_independent(
     // it with cpi->sf.disable_wedge_search_var_thresh.
     sf->disable_wedge_interintra_search = 1;
     sf->perform_coeff_opt = is_boosted_arf2_bwd_type ? 2 : 3;
+    sf->prune_comp_type_by_model_rd = is_boosted_arf2_bwd_type ? 0 : 1;
     sf->disable_smooth_intra =
         !frame_is_intra_only(&cpi->common) || (cpi->rc.frames_to_key != 1);
   }
@@ -352,6 +353,7 @@ static void set_good_speed_features_framesize_independent(
     sf->skip_sharp_interp_filter_search = 1;
     sf->perform_coeff_opt = is_boosted_arf2_bwd_type ? 2 : 4;
     sf->adaptive_txb_search_level = boosted ? 2 : 3;
+    sf->prune_comp_type_by_model_rd = boosted ? 0 : 1;
   }
 
   if (speed >= 5) {
@@ -785,6 +787,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
   sf->prune_warp_using_wmtype = 0;
   sf->disable_wedge_interintra_search = 0;
   sf->perform_coeff_opt = 0;
+  sf->prune_comp_type_by_model_rd = 0;
   sf->disable_smooth_intra = 0;
 
   if (oxcf->mode == GOOD)
