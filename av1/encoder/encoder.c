06bc361f3145613c25cd3afb13e57886b20bb8cd
Satish Kumar Suman <satish.suman@ittiam.com>
2019-02-11 11:27:25 +0530

    change offset use in diamond search
    Diamond search in temporal filter uses source buffer,
    but offset used in search is based on scaled source
    buffer. So maintaining offset array seperately.
    
    Change-Id: Ib6c2577b114c1cea96868e014bda761d78e97e9a

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -3884,9 +3884,17 @@ static void init_motion_estimation(AV1_COMP *cpi) {
   int y_stride = cpi->scaled_source.y_stride;
 
   if (cpi->sf.mv.search_method == NSTEP) {
-    av1_init3smotion_compensation(&cpi->ss_cfg, y_stride);
+    av1_init3smotion_compensation(&cpi->ss_cfg[0], y_stride);
   } else if (cpi->sf.mv.search_method == DIAMOND) {
-    av1_init_dsmotion_compensation(&cpi->ss_cfg, y_stride);
+    av1_init_dsmotion_compensation(&cpi->ss_cfg[0], y_stride);
+  }
+
+  int y_stride_src = cpi->lookahead->buf->img.y_stride;
+
+  if (cpi->sf.mv.search_method == NSTEP) {
+    av1_init3smotion_compensation(&cpi->ss_cfg[1], y_stride_src);
+  } else if (cpi->sf.mv.search_method == DIAMOND) {
+    av1_init_dsmotion_compensation(&cpi->ss_cfg[1], y_stride_src);
   }
 }
 
