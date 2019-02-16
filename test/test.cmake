86cca4626726398e31292cf8c88f41df0d23a3e4
chiyotsai <chiyotsai@google.com>
2019-02-15 16:35:44 -0800

    Add test cases for av1_apply_temporal_filter
    Change-Id: I748bdbb4ee8c126bebfc16540afc230af73008f0

--- test/test.cmake
+++ test/test.cmake
@@ -76,6 +76,7 @@ list(APPEND AOM_UNIT_TEST_ENCODER_SOURCES
             "${AOM_ROOT}/test/scalability_test.cc"
             "${AOM_ROOT}/test/y4m_test.cc"
             "${AOM_ROOT}/test/y4m_video_source.h"
+            "${AOM_ROOT}/test/yuv_temporal_filter_test.cc"
             "${AOM_ROOT}/test/yuv_video_source.h")
 
 list(APPEND AOM_DECODE_PERF_TEST_SOURCES "${AOM_ROOT}/test/decode_perf_test.cc")
