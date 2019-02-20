1df558bd9f4536c467fe1c8ce64bc892df6eec7d
logangw <logangw@google.com>
2019-02-20 09:55:56 -0800

    Incorporate CNN restoration experiment.
    Change-Id: Ia6875f7a0782dc4ddb5291d7d040316f327855c8

--- av1/encoder/call_tensorflow.h
+++ av1/encoder/call_tensorflow.h
@@ -9,35 +9,6 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
-#pragma once
-#include <limits.h>
-#include <math.h>
-#include <stdio.h>
-
-#include "config/aom_config.h"
-#include "config/aom_dsp_rtcd.h"
-#include "config/aom_scale_rtcd.h"
-#include "config/av1_rtcd.h"
-
-#include "aom_dsp/aom_dsp_common.h"
-#include "aom_dsp/aom_filter.h"
-#if CONFIG_DENOISE
-#include "aom_dsp/grain_table.h"
-#include "aom_dsp/noise_util.h"
-#include "aom_dsp/noise_model.h"
-#endif
-#include "aom_dsp/psnr.h"
-#if CONFIG_INTERNAL_STATS
-#include "aom_dsp/ssim.h"
-#endif
-#include "aom_ports/aom_timer.h"
-#include "aom_ports/mem.h"
-#include "aom_ports/system_state.h"
-#include "aom_scale/aom_scale.h"
-#if CONFIG_BITSTREAM_DEBUG || CONFIG_MISMATCH_DEBUG
-#include "aom_util/debug_util.h"
-#endif  // CONFIG_BITSTREAM_DEBUG || CONFIG_MISMATCH_DEBUG
-
 #include "av1/common/alloccommon.h"
 #include "av1/common/cdef.h"
 #include "av1/common/filter.h"
@@ -45,8 +16,6 @@
 #include "av1/common/reconinter.h"
 #include "av1/common/reconintra.h"
 #include "av1/common/resize.h"
-#include "av1/common/tile_common.h"
-#include "av1/common/onyxc_int.h"
 
 #include "av1/encoder/aq_complexity.h"
 #include "av1/encoder/aq_cyclicrefresh.h"
@@ -71,22 +40,20 @@
 #include "av1/encoder/speed_features.h"
 #include "av1/encoder/temporal_filter.h"
 
-#ifndef ADDITION_HANDLE_FRAME
-#define ADDITION_HANDLE_FRAME
+#include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifndef CALL_TENSORFLOW
+#define CALL_TENSORFLOW
 
-void addition_handle_frame(AV1_COMMON *cm, FRAME_TYPE frame_type);
-void addition_handle_blocks(AV1_COMMON *cm, FRAME_TYPE frame_type);
+uint8_t **call_tensorflow(uint8_t *ppp, int height, int width, int stride,
+                          FRAME_TYPE frame_type);
+uint8_t **block_call_tensorflow(uint8_t *ppp, int cur_buf_height,
+                                int cur_buf_width, int stride,
+                                FRAME_TYPE frame_type);
+uint16_t **call_tensorflow_hbd(uint16_t *ppp, int height, int width, int stride,
+                               FRAME_TYPE frame_type);
+uint16_t **block_call_tensorflow_hbd(uint16_t *ppp, int cur_buf_height,
+                                     int cur_buf_width, int stride,
+                                     FRAME_TYPE frame_type);
 
-uint8_t **blocks_to_cnn_secondly(uint8_t *pBuffer_y, int height, int width,
-                                 int stride, FRAME_TYPE frame_type);
-#ifdef __cplusplus
-}  // extern "C"
-#endif
-
-#endif  // ADDITION_HANDLE_FRAME
-
-#pragma once
+#endif  // CALL_TENSORFLOW
