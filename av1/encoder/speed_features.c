f113d417e54dd6989a75a912b3074ec80fd667d7
Venkat <sanampudi.venkatarao@ittiam.com>
2019-02-22 11:33:42 +0530

    Correct use_square_partition_only_threshold speed feature
    For speeds >= 3, change use_square_partition_only_threshold for
    non-boosted frames.
    
    STATS_CHANGED
    
    Change-Id: I10c890db609a44ca58c08b31bdca11c510280f17

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -102,6 +102,9 @@ static void set_good_speed_feature_framesize_dependent(
   const AV1_COMMON *const cm = &cpi->common;
   const int is_720p_or_larger = AOMMIN(cm->width, cm->height) >= 720;
   const int is_480p_or_larger = AOMMIN(cm->width, cm->height) >= 480;
+  const int boosted = frame_is_boosted(cpi);
+  const int is_boosted_arf2_bwd_type =
+      boosted || cpi->refresh_bwd_ref_frame || cpi->refresh_alt2_ref_frame;
 
   if (is_480p_or_larger) {
     sf->use_square_partition_only_threshold = BLOCK_128X128;
@@ -165,10 +168,14 @@ static void set_good_speed_feature_framesize_dependent(
 
   if (speed >= 3) {
     if (is_720p_or_larger) {
+      sf->use_square_partition_only_threshold =
+          is_boosted_arf2_bwd_type ? BLOCK_64X64 : BLOCK_32X32;
       sf->disable_split_mask = DISABLE_ALL_SPLIT;
       sf->partition_search_breakout_dist_thr = (1 << 25);
       sf->partition_search_breakout_rate_thr = 200;
     } else {
+      sf->use_square_partition_only_threshold =
+          is_boosted_arf2_bwd_type ? BLOCK_32X32 : BLOCK_16X16;
       sf->max_intra_bsize = BLOCK_32X32;
       sf->disable_split_mask = DISABLE_ALL_INTER_SPLIT;
       sf->partition_search_breakout_dist_thr = (1 << 23);
@@ -340,8 +347,6 @@ static void set_good_speed_features_framesize_independent(
   if (speed >= 4) {
     sf->use_intra_txb_hash = 0;
     sf->tx_type_search.fast_intra_tx_type_search = 1;
-    sf->use_square_partition_only_threshold =
-        boosted ? BLOCK_128X128 : BLOCK_4X4;
     sf->disable_loop_restoration_chroma =
         (boosted || cm->allow_screen_content_tools) ? 0 : 1;
     sf->mv.subpel_search_method = SUBPEL_TREE_PRUNED;
