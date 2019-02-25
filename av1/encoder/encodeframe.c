06da62b4ae23343b210d3c72a0470a22d97f6a98
Remya <remya.prakasan@ittiam.com>
2019-02-25 14:44:07 +0530

    Gate interintra using first pass info
    STATS_CHANGED
    
    Change-Id: I648897e987eecb15c196ecb9833096b6009ec95c

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -5411,6 +5411,8 @@ static void init_first_partition_pass_stats_tables(
     memset(stats[i].ref0_counts, 0xff, sizeof(stats[i].ref0_counts));
     memset(stats[i].ref1_counts, 0xff, sizeof(stats[i].ref1_counts));
     stats[i].sample_counts = INT_MAX;
+    memset(stats[i].interintra_motion_mode_count, 0xff,
+           sizeof(stats[i].interintra_motion_mode_count));
   }
 }
 
@@ -5585,6 +5587,8 @@ static void first_partition_search_pass(AV1_COMP *cpi, ThreadData *td,
       // If there are not enough samples collected, make all available.
       memset(stat->ref0_counts, 0xff, sizeof(stat->ref0_counts));
       memset(stat->ref1_counts, 0xff, sizeof(stat->ref1_counts));
+      memset(stat->interintra_motion_mode_count, 0xff,
+             sizeof(stat->interintra_motion_mode_count));
     } else if (sf->selective_ref_frame < 3) {
       // ALTREF2_FRAME and BWDREF_FRAME may be skipped during the
       // initial partition scan, so we don't eliminate them.
@@ -5592,6 +5596,8 @@ static void first_partition_search_pass(AV1_COMP *cpi, ThreadData *td,
       stat->ref1_counts[ALTREF2_FRAME] = 0xff;
       stat->ref0_counts[BWDREF_FRAME] = 0xff;
       stat->ref1_counts[BWDREF_FRAME] = 0xff;
+      stat->interintra_motion_mode_count[ALTREF2_FRAME] = 0xff;
+      stat->interintra_motion_mode_count[BWDREF_FRAME] = 0xff;
     }
   }
 }
@@ -7064,6 +7070,11 @@ static void encode_superblock(const AV1_COMP *const cpi, TileDataEnc *tile_data,
         if (mbmi->ref_frame[1] >= 0 &&
             stats->ref1_counts[mbmi->ref_frame[1]] < 255)
           ++stats->ref1_counts[mbmi->ref_frame[1]];
+        // Increase the counter for interintra_motion_mode_count.
+        if (mbmi->motion_mode == 0 && mbmi->ref_frame[1] == INTRA_FRAME &&
+            stats->interintra_motion_mode_count[mbmi->ref_frame[0]] < 255) {
+          ++stats->interintra_motion_mode_count[mbmi->ref_frame[0]];
+        }
       }
     }
   }
