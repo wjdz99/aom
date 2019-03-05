4988d53d153a6da9d1597dde62c19dee81a2ed67
chiyotsai <chiyotsai@google.com>
2019-03-05 12:39:32 -0800

    Deploy DNN Model to terminate after PARTITION_NONE
    STATS_CHANGED
    
    Change-Id: Idb01ecb27ebf75eb934e48ce046166f4946458fe

---
+++ av1/encoder/ml_thresholds.h
@@ -0,0 +1,5 @@
+static const float av1_simple_motion_search_early_term_split_thresh_128 = 0.0f;
+static const float av1_simple_motion_search_early_term_split_thresh_64 = 0.0f;
+static const float av1_simple_motion_search_early_term_split_thresh_32 = 0.0f;
+static const float av1_simple_motion_search_early_term_split_thresh_16 = 0.0f;
+static const float av1_simple_motion_search_early_term_split_thresh_8 = 0.0f;
