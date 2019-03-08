992f0ce0774fed8987f1d436b8b26eb8c1b7f992
Venkat <sanampudi.venkatarao@ittiam.com>
2019-03-08 11:30:46 +0530

    Refine No.of candidates for compound search
    STATS_CHANGED
    
    Change-Id: Iee844071afab5f0abbee7b14ca73caeddb871ceb

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -335,6 +335,7 @@ static void set_good_speed_features_framesize_independent(
     sf->disable_wedge_interintra_search = 1;
     sf->perform_coeff_opt = is_boosted_arf2_bwd_type ? 2 : 3;
     sf->prune_comp_type_by_model_rd = boosted ? 0 : 1;
+    sf->mv.search_method = boosted ? NSTEP : BIGDIA;
     sf->disable_smooth_intra =
         !frame_is_intra_only(&cpi->common) || (cpi->rc.frames_to_key != 1);
   }
