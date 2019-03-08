bbaa8e3c6e0b06f8bf9af9247a8db46c3180a269
Sarah Parker <sarahparker@google.com>
2019-03-08 12:56:43 -0800

    Use quantize b for final pass in FINAL_PASS_TRELLIS_OPT
    This gives about 0.4% gain with
    disable-trellis-quant=FINAL_PASS_TRELLIS_OPT and quant-b-adapt=1
    on lowres.
    
    Change-Id: I0e5ca6c936718afc30dbc71a9cf041e171647517

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -228,7 +228,11 @@ static void encode_block(int plane, int block, int blk_row, int blk_col,
                                       tx_size, cm->reduced_tx_set_used);
     if (args->enable_optimize_b != NO_TRELLIS_OPT) {
       av1_xform_quant(cm, x, plane, block, blk_row, blk_col, plane_bsize,
-                      tx_size, tx_type, AV1_XFORM_QUANT_FP);
+                      tx_size, tx_type,
+                      USE_B_QUANT_NO_TRELLIS &&
+                              (args->enable_optimize_b == FINAL_PASS_TRELLIS_OPT)
+                          ? AV1_XFORM_QUANT_B
+                          : AV1_XFORM_QUANT_FP);
       TXB_CTX txb_ctx;
       get_txb_ctx(plane_bsize, tx_size, plane, a, l, &txb_ctx);
       av1_optimize_b(args->cpi, x, plane, block, tx_size, tx_type, &txb_ctx,
@@ -580,7 +584,11 @@ void av1_encode_block_intra(int plane, int block, int blk_row, int blk_col,
     const ENTROPY_CONTEXT *l = &args->tl[blk_row];
     if (args->enable_optimize_b != NO_TRELLIS_OPT) {
       av1_xform_quant(cm, x, plane, block, blk_row, blk_col, plane_bsize,
-                      tx_size, tx_type, AV1_XFORM_QUANT_FP);
+                      tx_size, tx_type,
+                      USE_B_QUANT_NO_TRELLIS &&
+                              (args->enable_optimize_b == FINAL_PASS_TRELLIS_OPT)
+                          ? AV1_XFORM_QUANT_B
+                          : AV1_XFORM_QUANT_FP);
       TXB_CTX txb_ctx;
       get_txb_ctx(plane_bsize, tx_size, plane, a, l, &txb_ctx);
       av1_optimize_b(args->cpi, x, plane, block, tx_size, tx_type, &txb_ctx,
