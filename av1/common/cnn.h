0bacae3d552b198f0b887ed5499e8f7b663d0cc4
Debargha Mukherjee <debargha@google.com>
2019-03-13 13:33:45 -0700

    Extend CNN library to allow extended inputs
    Implements VALID (tensorflow) mode for convolution.
    Also, adds the ability for the input to be extended outside
    the input image bounds, so that in combination with VALID
    and SAME padding over several convolutional layers, the
    output can be of the same size as the input.
    
    Change-Id: I9cc7676568809729ee5ea5a9c5d318a21ca64c5e

--- av1/common/cnn.h
+++ av1/common/cnn.h
@@ -29,6 +29,7 @@ enum {
                            // the image area assumed to be 0 (default)
   PADDING_SAME_REPLICATE,  // tensorflow's SAME padding with pixels outside
                            // the image area replicated from closest edge
+  PADDING_VALID            // tensorflow's VALID padding
 } UENUM1BYTE(PADDING_TYPE);
 
 struct CNN_LAYER_CONFIG {
@@ -48,6 +49,11 @@ struct CNN_LAYER_CONFIG {
 struct CNN_CONFIG {
   int num_layers;  // number of CNN layers ( = number of hidden layers + 1)
   int is_residue;  // whether the output activation is a residue
+  int ext_width, ext_height;  // extension horizontally and vertically
+  int strict_bounds;          // whether the input bounds are strict or not.
+                              // If strict, the extension area is filled by
+                              // replication; if not strict, image data is
+                              // assumed available beyond the boounds.
   CNN_LAYER_CONFIG layer_config[CNN_MAX_LAYERS];
 };
 
