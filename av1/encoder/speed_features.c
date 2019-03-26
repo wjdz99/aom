6a6f67341a705bdddec6178af553244e7276af35
chiyotsai <chiyotsai@google.com>
2019-03-25 17:18:51 -0700

    Disable two pass partition search on lowres and midres
    Currently two pass partition search has an average slow down of 2.3% on
    lowres and 4% on midres when the PSNR are limited to 35dB to 45B. There
    is however a 9% average speed up on hdres. This patch enables it only on
    hdres speed 1 and speed 2.
    
    Change-Id: I8055a1a108879d4be47b073578206dae5e2a486b

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -119,6 +119,11 @@ static void set_good_speed_feature_framesize_dependent(
   if (speed >= 1) {
     if (is_720p_or_larger) {
       sf->use_square_partition_only_threshold = BLOCK_128X128;
+
+      if (speed >= CONFIG_2PASS_PARTITION_SEARCH_LVL_START &&
+          speed < CONFIG_2PASS_PARTITION_SEARCH_LVL_END) {
+        sf->two_pass_partition_search = 1;
+      }
     } else if (is_480p_or_larger) {
       sf->use_square_partition_only_threshold = BLOCK_64X64;
     } else {
@@ -225,9 +230,6 @@ static void set_good_speed_features_framesize_independent(
 
     sf->intra_tx_size_search_init_depth_rect = 1;
     sf->tx_size_search_lgr_block = 1;
-    if (speed >= CONFIG_2PASS_PARTITION_SEARCH_LVL_START &&
-        speed < CONFIG_2PASS_PARTITION_SEARCH_LVL_END)
-      sf->two_pass_partition_search = 1;
 
     sf->prune_ext_partition_types_search_level = 2;
     sf->skip_repeat_interpolation_filter_search = 1;
