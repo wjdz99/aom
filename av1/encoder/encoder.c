61a1e8a85608bb9bd96231231eee0bd7aaffff4c
Debargha Mukherjee <debargha@google.com>
2019-03-26 16:58:55 -0700

    Temp fix for ctrl based resize setting
    Temporary fix for a resize test. When resize is set through
    a control, the border size is currently not upgraded to
    a thicker border. This patch is a temporary fix to always
    use the thicker border for scaled reference frames.
    However this patch does not fix the border for existing
    reference buffers if resize is set through a control
    mid-stream. That will be fixed subsequently.
    
    BUG=aomedia:2348
    
    Change-Id: I1ccb82f0444ab8bab58f864a58cf6d600c5616f0

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -3377,11 +3377,12 @@ static void scale_references(AV1_COMP *cpi) {
         }
 
         if (force_scaling || new_fb->buf.y_crop_width != cm->width ||
-            new_fb->buf.y_crop_height != cm->height) {
+            new_fb->buf.y_crop_height != cm->height ||
+            new_fb->buf.border != AOM_BORDER_IN_PIXELS) {
           if (aom_realloc_frame_buffer(
                   &new_fb->buf, cm->width, cm->height,
                   cm->seq_params.subsampling_x, cm->seq_params.subsampling_y,
-                  cm->seq_params.use_highbitdepth, cpi->oxcf.border_in_pixels,
+                  cm->seq_params.use_highbitdepth, AOM_BORDER_IN_PIXELS,
                   cm->byte_alignment, NULL, NULL, NULL)) {
             if (force_scaling) {
               // Release the reference acquired in the get_free_fb() call above.
