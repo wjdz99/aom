078c250542c53ef6c7a859b3c9ee638553fe8369
Debargha Mukherjee <debargha@google.com>
2019-03-27 23:49:14 -0700

    Update border for ref buffer to allow scaled pred
    Adds a function to realloc and copy a frame buffer
    to accommodate a different border size. The new buffer is
    also extended by replication.
    
    BUG=aomedia:2348
    
    Change-Id: I83cd559a201e810df273dcba361cc3e261d1a260

--- aom_scale/generic/yv12extend.c
+++ aom_scale/generic/yv12extend.c
@@ -434,3 +434,26 @@ void aom_yv12_partial_coloc_copy_v_c(const YV12_BUFFER_CONFIG *src_bc,
   aom_yv12_partial_copy_v_c(src_bc, hstart, hend, vstart, vend, dst_bc, hstart,
                             vstart);
 }
+
+int aom_yv12_realloc_with_new_border_c(YV12_BUFFER_CONFIG *ybf, int new_border,
+                                       int byte_alignment, int num_planes) {
+  if (ybf) {
+    if (new_border == ybf->border) return 0;
+    YV12_BUFFER_CONFIG new_buf = { 0 };
+    memset(&new_buf, 0, sizeof(new_buf));
+    int error;
+    if ((error = aom_alloc_frame_buffer(&new_buf, ybf->y_crop_width,
+                                        ybf->y_crop_height, ybf->subsampling_x,
+                                        ybf->subsampling_y,
+                                        ybf->flags & YV12_FLAG_HIGHBITDEPTH,
+                                        new_border, byte_alignment)) != 0) {
+      return error;
+    }
+    aom_yv12_copy_frame(ybf, &new_buf, num_planes);
+    aom_extend_frame_borders(&new_buf, num_planes);
+    aom_free_frame_buffer(ybf);
+    memcpy(ybf, &new_buf, sizeof(new_buf));
+    return 0;
+  }
+  return -2;
+}
