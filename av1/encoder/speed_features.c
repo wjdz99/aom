370f502b5fd083a6db61e91d8762d545c78876b8
Hui Su <huisu@google.com>
2019-04-08 10:56:20 -0700

    Speed feature: skip part search after trying split
    Add a ML based speed feature to terminate partition RD search after
    trying PARTITION_SPLIT. The features for the model are based on QP,
    previous partition RDO results and the variance from simple motion
    search.
    
    This feature is currently enabled for low and mid resolution, speed 1
    and 2.
    
    Coding loss(%):
             speed 1    speed 2
    lowres    0.05       0.05
    midres    0.04       0.02
    
    Speed gains(%):
             speed 1    speed 2
    lowres     4.7       3.7
    midres     5.4       5.3
    
    STATS_CHANGED
    
    Change-Id: I6c32707e52da5c3ff84e784a4b4fe43ebb19cb97

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -280,6 +280,7 @@ static void set_good_speed_features_framesize_independent(
     sf->use_transform_domain_distortion = boosted ? 0 : 1;
     sf->perform_coeff_opt = boosted ? 0 : 1;
     sf->use_inter_txb_hash = 0;
+    sf->ml_early_term_after_part_split = 1;
   }
 
   if (speed >= 2) {
@@ -347,6 +348,7 @@ static void set_good_speed_features_framesize_independent(
     sf->prune_comp_type_by_model_rd = boosted ? 0 : 1;
     sf->disable_smooth_intra =
         !frame_is_intra_only(&cpi->common) || (cpi->rc.frames_to_key != 1);
+    sf->ml_early_term_after_part_split = 0;
   }
 
   if (speed >= 4) {
@@ -761,6 +763,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
   sf->ml_prune_rect_partition = 0;
   sf->ml_prune_ab_partition = 0;
   sf->ml_prune_4_partition = 0;
+  sf->ml_early_term_after_part_split = 0;
   sf->fast_cdef_search = 0;
   for (i = 0; i < PARTITION_BLOCK_SIZES; ++i) {
     sf->ml_partition_search_breakout_thresh[i] = -1;  // -1 means not enabled.
