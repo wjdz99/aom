19ec31694aabb0adc5f92a0dfdc0747755929abc
Yue Chen <yuec@google.com>
2019-04-10 16:54:03 -0700

    Support parsing multi-layer gf group and 7 ref frame in tpl tool
    - Maintain an arf frame stack and a last frame queue to identify
    (ALTREF, BWDREF, ALTREF2) and (LAST, LAST2, LAST3)
    - Expand the length of tpl stats buffer from MAX_LAG_BUFFERS to
    MAX_STATIC_GF_GROUP_LENGTH + 3, to include maximum numbers of gf
    pictures + the key/golden frame + 2 extended frames in the next
    gop
    - Handle the case that the first gf picture of the group is not
    an ARF frame
    - Only enable rdmult modulation on ARF frames
    
    Change-Id: Ibfea1086445dc948fda4b9bb14024101036fb9e0

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -2713,7 +2713,7 @@ AV1_COMP *av1_create_compressor(AV1EncoderConfig *oxcf,
   av1_set_speed_features_framesize_independent(cpi, oxcf->speed);
   av1_set_speed_features_framesize_dependent(cpi, oxcf->speed);
 
-  for (int frame = 0; frame < MAX_LAG_BUFFERS; ++frame) {
+  for (int frame = 0; frame < MAX_LENGTH_TPL_FRAME_STATS; ++frame) {
     int mi_cols = ALIGN_POWER_OF_TWO(cm->mi_cols, MAX_MIB_SIZE_LOG2);
     int mi_rows = ALIGN_POWER_OF_TWO(cm->mi_rows, MAX_MIB_SIZE_LOG2);
 
