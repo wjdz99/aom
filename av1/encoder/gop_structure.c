cb9963a777a1fbfe7fae80098fc6d087a890f4d5
Yue Chen <yuec@google.com>
2019-04-11 17:07:10 -0700

    Set up explicit reference frame maps in GF_GROUP
    - Add two arrays ref_frame_disp_idx and ref_frame_gop_idx, holding
    gop indices and display order indices of reference frames of frames
    in the current gop.
    - Rename 'frame_gop_index' as 'frame_disp_idx' because the values
    are indices in display order
    - If a reference frame, other than the golden frame, is not in the
    current gop, the index is set as -1
    
    Change-Id: Ica311c800052784acc8f3aa70405ab66ee7e8ece

--- av1/encoder/gop_structure.c
+++ av1/encoder/gop_structure.c
@@ -42,7 +42,7 @@ static void set_multi_layer_params(GF_GROUP *const gf_group, int start, int end,
       gf_group->arf_src_offset[*frame_ind] = 0;
       gf_group->arf_pos_in_gf[*frame_ind] = 0;
       gf_group->arf_update_idx[*frame_ind] = arf_ind;
-      gf_group->frame_gop_index[*frame_ind] = start;
+      gf_group->frame_disp_idx[*frame_ind] = start;
       gf_group->pyramid_level[*frame_ind] = MIN_PYRAMID_LVL;
       ++gf_group->pyramid_lvl_nodes[MIN_PYRAMID_LVL];
       ++(*frame_ind);
@@ -56,7 +56,7 @@ static void set_multi_layer_params(GF_GROUP *const gf_group, int start, int end,
     gf_group->arf_src_offset[*frame_ind] = m - start - 1;
     gf_group->arf_pos_in_gf[*frame_ind] = 0;
     gf_group->arf_update_idx[*frame_ind] = 1;  // mark all internal ARF 1
-    gf_group->frame_gop_index[*frame_ind] = m;
+    gf_group->frame_disp_idx[*frame_ind] = m;
     gf_group->pyramid_level[*frame_ind] = level;
     ++gf_group->pyramid_lvl_nodes[level];
     ++(*frame_ind);
@@ -69,7 +69,7 @@ static void set_multi_layer_params(GF_GROUP *const gf_group, int start, int end,
     gf_group->arf_src_offset[*frame_ind] = 0;
     gf_group->arf_pos_in_gf[*frame_ind] = arf_pos_in_gf;  // For bit allocation.
     gf_group->arf_update_idx[*frame_ind] = 1;
-    gf_group->frame_gop_index[*frame_ind] = m;
+    gf_group->frame_disp_idx[*frame_ind] = m;
     gf_group->pyramid_level[*frame_ind] = MIN_PYRAMID_LVL;
     ++(*frame_ind);
 
@@ -104,7 +104,7 @@ static int construct_multi_layer_gf_structure(
     gf_group->arf_src_offset[frame_index] = gf_interval - 1;
     gf_group->arf_pos_in_gf[frame_index] = 0;
     gf_group->arf_update_idx[frame_index] = 0;
-    gf_group->frame_gop_index[frame_index] = gf_interval;
+    gf_group->frame_disp_idx[frame_index] = gf_interval;
     gf_group->pyramid_level[frame_index] = gf_group->pyramid_height;
     ++frame_index;
   }
@@ -169,6 +169,112 @@ static int get_pyramid_height(const AV1_COMP *const cpi) {
                 cpi->oxcf.gf_max_pyr_height);
 }
 
+// Stack data structure utilities
+static INLINE int stack_pop(int *stack, int *stack_size) {
+  const int r = stack[0];
+  for (int idx = 1; idx < *stack_size; ++idx) stack[idx - 1] = stack[idx];
+  *stack_size = *stack_size - 1;
+
+  return r;
+}
+
+static INLINE void stack_push(int *stack, int new_item, int *stack_size) {
+  for (int idx = *stack_size; idx > 0; --idx) stack[idx] = stack[idx - 1];
+  stack[0] = new_item;
+  *stack_size = *stack_size + 1;
+}
+
+// Queue data structure utility
+static INLINE void queue_enqueue(int *queue, int new_item, int *queue_size,
+                                 int max_queue_size) {
+  if (*queue_size < max_queue_size) {
+    queue[*queue_size] = new_item;
+    *queue_size = *queue_size + 1;
+  } else {
+    assert(*queue_size == max_queue_size);
+
+    for (int idx = 1; idx < max_queue_size; ++idx) queue[idx - 1] = queue[idx];
+    queue[max_queue_size - 1] = new_item;
+  }
+}
+
+#define REF_IDX(ref) ((ref)-LAST_FRAME)
+
+static INLINE void reset_ref_frame_idx(int *ref_idx, int reset_value) {
+  for (int i = 0; i < INTER_REFS_PER_FRAME; ++i) ref_idx[i] = reset_value;
+}
+
+static INLINE void set_ref_frame_disp_idx(GF_GROUP *const gf_group) {
+  for (int i = 0; i <= gf_group->size; ++i) {
+    for (int ref = 0; ref < INTER_REFS_PER_FRAME; ++ref) {
+      int ref_gop_idx = gf_group->ref_frame_gop_idx[i][ref];
+      if (ref_gop_idx == -1) {
+        gf_group->ref_frame_disp_idx[i][ref] = -1;
+      } else {
+        gf_group->ref_frame_disp_idx[i][ref] =
+            gf_group->frame_disp_idx[ref_gop_idx];
+      }
+    }
+  }
+}
+
+static void set_gop_ref_frame_map(GF_GROUP *const gf_group) {
+  reset_ref_frame_idx(gf_group->ref_frame_gop_idx[0], -1);
+  int init_ref_idx;
+  if (gf_group->update_type[0] == KF_UPDATE)
+    init_ref_idx = 0;
+  else
+    init_ref_idx = -1;
+  for (int frame_idx = 1; frame_idx <= gf_group->size; ++frame_idx)
+    reset_ref_frame_idx(gf_group->ref_frame_gop_idx[frame_idx], init_ref_idx);
+
+  // Set the map for frames in the current gop
+  int arf_stack[MAX_PYRAMID_LVL] = { -1, -1, -1, -1 };
+  int arf_stack_size = 0;
+  int lst_queue[3] = { -1, -1, -1 };
+  int lst_queue_size = 0;
+  int int_arf_idx = -1;
+
+  for (int frame_idx = 1; frame_idx <= gf_group->size; ++frame_idx) {
+    gf_group->ref_frame_gop_idx[frame_idx][REF_IDX(GOLDEN_FRAME)] = 0;
+    if (arf_stack_size > 0) {
+      gf_group->ref_frame_gop_idx[frame_idx][REF_IDX(ALTREF_FRAME)] =
+          arf_stack[arf_stack_size - 1];
+    }
+    if (arf_stack_size > 1) {
+      gf_group->ref_frame_gop_idx[frame_idx][REF_IDX(BWDREF_FRAME)] =
+          arf_stack[0];
+    }
+    if (arf_stack_size > 2) {
+      gf_group->ref_frame_gop_idx[frame_idx][REF_IDX(ALTREF2_FRAME)] =
+          arf_stack[1];
+    }
+    if (lst_queue_size > 0) {
+      for (int lst_idx = lst_queue_size - 1; lst_idx >= 0; --lst_idx) {
+        int lst_frame_type_idx = LAST_FRAME + (lst_queue_size - 1 - lst_idx);
+        gf_group->ref_frame_gop_idx[frame_idx][REF_IDX(lst_frame_type_idx)] =
+            lst_queue[lst_idx];
+      }
+    }
+
+    switch (gf_group->update_type[frame_idx]) {
+      case LF_UPDATE:
+        queue_enqueue(lst_queue, frame_idx, &lst_queue_size, 3);
+        break;
+      case INTNL_OVERLAY_UPDATE:
+        int_arf_idx = stack_pop(arf_stack, &arf_stack_size);
+        queue_enqueue(lst_queue, int_arf_idx, &lst_queue_size, 3);
+        break;
+      case OVERLAY_UPDATE: stack_pop(arf_stack, &arf_stack_size); break;
+      case INTNL_ARF_UPDATE:
+      case ARF_UPDATE: stack_push(arf_stack, frame_idx, &arf_stack_size); break;
+      default: break;
+    }
+  }
+
+  set_ref_frame_disp_idx(gf_group);
+}
+
 void av1_gop_setup_structure(AV1_COMP *cpi,
                              const EncodeFrameParams *const frame_params) {
   RATE_CONTROL *const rc = &cpi->rc;
@@ -189,6 +295,8 @@ void av1_gop_setup_structure(AV1_COMP *cpi,
       (rc->source_alt_ref_pending) ? OVERLAY_UPDATE : GF_UPDATE;
   gf_group->arf_update_idx[gf_group->size] = 0;
   gf_group->arf_pos_in_gf[gf_group->size] = 0;
+
+  set_gop_ref_frame_map(gf_group);
 
 #if CHECK_GF_PARAMETER
   check_frame_params(gf_group, rc->baseline_gf_interval);
