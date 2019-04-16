5412bd2e28ec5e09815723664c274004a25f6681
Sarah Parker <sarahparker@google.com>
2019-04-15 20:28:17 -0700

    WIP change current_frame_id with fwd keyframes
    This is the simplest fix for the issue, but will cause frame_ids to
    be reused. Alternatively, we could modify the encoder to leave a gap
    for a different ID for the keyframe when it is displayed.
    BUG=aomedia:2325
    
    Change-Id: I2b6700e81cc17965830682cae9151efca7286607

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -4811,6 +4811,10 @@ static void show_existing_frame_reset(AV1Decoder *const pbi,
   // have been selected.
   if (cm->seq_params.frame_id_numbers_present_flag) {
     update_ref_frame_id(cm, cm->ref_frame_id[existing_frame_idx]);
+    cm->current_frame_id = cm->ref_frame_id[existing_frame_idx];
+    for (int i = 0; i < REF_FRAMES; i++) {
+      cm->valid_for_referencing[i] = 0;
+    }
   }
 
   cm->refresh_frame_context = REFRESH_FRAME_CONTEXT_DISABLED;
