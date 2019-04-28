8b7a69057c142f27796c29dc038e163c3974f9f6
Venkat <sanampudi.venkatarao@ittiam.com>
2019-04-28 14:30:30 +0530

    Refine R-D optimization of coeffs
    STATS_CHANGED
    
    Change-Id: Ia142f3238c905474eed38b9b0229835ced8d711c

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -960,7 +960,7 @@ typedef struct AV1_COMP {
   int max_comp_type_rd_threshold_mul;
   int max_comp_type_rd_threshold_div;
 
-  unsigned int tx_domain_dist_threshold;
+  double tx_domain_dist_threshold;
 
   // Factor to control R-D optimization of coeffs based on block
   // mse.
