54aee59894810bf12737fe7b6f4cc843da157801
Sachin Kumar Garg <sachin.kumargarg@ittiam.com>
2019-05-07 10:22:15 +0530

    Disable idtx for tx32
    STATS_CHANGED
    
    Change-Id: I0cc3ae112734bc0870d82c3a5bc575826d111797

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -688,9 +688,7 @@ static const uint16_t av1_ext_tx_used_flag[EXT_TX_SET_TYPES] = {
 static INLINE TxSetType av1_get_ext_tx_set_type(TX_SIZE tx_size, int is_inter,
                                                 int use_reduced_set) {
   const TX_SIZE tx_size_sqr_up = txsize_sqr_up_map[tx_size];
-  if (tx_size_sqr_up > TX_32X32) return EXT_TX_SET_DCTONLY;
-  if (tx_size_sqr_up == TX_32X32)
-    return is_inter ? EXT_TX_SET_DCT_IDTX : EXT_TX_SET_DCTONLY;
+  if (tx_size_sqr_up >= TX_32X32) return EXT_TX_SET_DCTONLY;
   if (use_reduced_set)
     return is_inter ? EXT_TX_SET_DCT_IDTX : EXT_TX_SET_DTT4_IDTX;
   const TX_SIZE tx_size_sqr = txsize_sqr_map[tx_size];
