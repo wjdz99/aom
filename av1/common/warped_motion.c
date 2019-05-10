a5c5d5685fd2d71a199a8e10599e28be7f0d8490
Sachin Kumar Garg <sachin.kumargarg@ittiam.com>
2019-05-10 19:18:19 +0530

    Optimize frame error module
    Added AVX2 variant for frame error
    
    For speed = 1 and 2 presets observed encode time reduction of
    0.45% and 0.55%(averaged across multiple test cases).
    
    Module gains improved by factor of ~2.5x w.r.t C code.
    
    Change-Id: I0e356134fd3fb486e7b121f6c63e29efdc870ff9

--- av1/common/warped_motion.c
+++ av1/common/warped_motion.c
@@ -23,7 +23,7 @@
 #define WARP_ERROR_BLOCK 32
 
 /* clang-format off */
-static const int error_measure_lut[512] = {
+const int error_measure_lut[512] = {
   // pow 0.7
   16384, 16339, 16294, 16249, 16204, 16158, 16113, 16068,
   16022, 15977, 15932, 15886, 15840, 15795, 15749, 15703,
@@ -539,9 +539,11 @@ static void highbd_warp_plane(WarpedMotionParams *wm, const uint8_t *const ref8,
                          delta);
 }
 
-static int64_t highbd_frame_error(const uint16_t *const ref, int stride,
-                                  const uint16_t *const dst, int p_width,
-                                  int p_height, int p_stride, int bd) {
+static int64_t av1_calc_highbd_frame_error(const uint16_t *const ref,
+                                           int stride,
+                                           const uint16_t *const dst,
+                                           int p_width, int p_height,
+                                           int p_stride, int bd) {
   int64_t sum_error = 0;
   for (int i = 0; i < p_height; ++i) {
     for (int j = 0; j < p_width; ++j) {
@@ -574,17 +576,13 @@ static int64_t highbd_warp_error(
                         CONVERT_TO_BYTEPTR(tmp), j, i, warp_w, warp_h,
                         WARP_ERROR_BLOCK, subsampling_x, subsampling_y, bd,
                         &conv_params);
-      gm_sumerr += highbd_frame_error(
+      gm_sumerr += av1_calc_highbd_frame_error(
           tmp, WARP_ERROR_BLOCK, CONVERT_TO_SHORTPTR(dst8) + j + i * p_stride,
           warp_w, warp_h, p_stride, bd);
       if (gm_sumerr > best_error) return gm_sumerr;
     }
   }
   return gm_sumerr;
-}
-
-static INLINE int error_measure(int err) {
-  return error_measure_lut[255 + err];
 }
 
 /* The warp filter for ROTZOOM and AFFINE models works as follows:
@@ -820,9 +818,9 @@ static void warp_plane(WarpedMotionParams *wm, const uint8_t *const ref,
                   alpha, beta, gamma, delta);
 }
 
-static int64_t frame_error(const uint8_t *const ref, int stride,
-                           const uint8_t *const dst, int p_width, int p_height,
-                           int p_stride) {
+int64_t av1_calc_frame_error_c(const uint8_t *const ref, int stride,
+                               const uint8_t *const dst, int p_width,
+                               int p_height, int p_stride) {
   int64_t sum_error = 0;
   for (int i = 0; i < p_height; ++i) {
     for (int j = 0; j < p_width; ++j) {
@@ -856,7 +854,8 @@ static int64_t warp_error(WarpedMotionParams *wm, const uint8_t *const ref,
       warp_plane(wm, ref, width, height, stride, tmp, j, i, warp_w, warp_h,
                  WARP_ERROR_BLOCK, subsampling_x, subsampling_y, &conv_params);
 
-      gm_sumerr += frame_error(tmp, WARP_ERROR_BLOCK, dst + j + i * p_stride,
+      gm_sumerr +=
+          av1_calc_frame_error(tmp, WARP_ERROR_BLOCK, dst + j + i * p_stride,
                                warp_w, warp_h, p_stride);
       if (gm_sumerr > best_error) return gm_sumerr;
     }
@@ -867,11 +866,11 @@ static int64_t warp_error(WarpedMotionParams *wm, const uint8_t *const ref,
 int64_t av1_frame_error(int use_hbd, int bd, const uint8_t *ref, int stride,
                         uint8_t *dst, int p_width, int p_height, int p_stride) {
   if (use_hbd) {
-    return highbd_frame_error(CONVERT_TO_SHORTPTR(ref), stride,
-                              CONVERT_TO_SHORTPTR(dst), p_width, p_height,
-                              p_stride, bd);
+    return av1_calc_highbd_frame_error(CONVERT_TO_SHORTPTR(ref), stride,
+                                       CONVERT_TO_SHORTPTR(dst), p_width,
+                                       p_height, p_stride, bd);
   }
-  return frame_error(ref, stride, dst, p_width, p_height, p_stride);
+  return av1_calc_frame_error(ref, stride, dst, p_width, p_height, p_stride);
 }
 
 int64_t av1_warp_error(WarpedMotionParams *wm, int use_hbd, int bd,
