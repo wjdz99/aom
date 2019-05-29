9aedd8ce5257996c880c32096342fa820322ff5b
Hui Su <huisu@google.com>
2019-05-29 09:56:19 -0700

    Use the decoder model to check level conformance
    BUG=aomedia:2332
    
    Change-Id: I88f25c2047782867f36001b57e792c95e9546ead

--- av1/encoder/level.h
+++ av1/encoder/level.h
@@ -67,11 +67,6 @@ typedef struct {
   double min_cr;
 } AV1LevelStats;
 
-typedef struct {
-  AV1LevelStats level_stats;
-  AV1LevelSpec level_spec;
-} AV1LevelInfo;
-
 // The following data structures are for the decoder model.
 typedef struct {
   int decoder_ref_count;
@@ -111,7 +106,8 @@ enum {
   DECODE_EXISTING_FRAME_BUF_EMPTY,
   DISPLAY_FRAME_LATE,
   SMOOTHING_BUFFER_UNDERFLOW,
-  SMOOTHING_BUFFER_OVERFLOW
+  SMOOTHING_BUFFER_OVERFLOW,
+  DECODER_MODEL_DISABLED
 } UENUM1BYTE(DECODER_MODEL_STATUS);
 
 #define BUFFER_POOL_MAX_SIZE 10
@@ -143,7 +139,13 @@ typedef struct {
   size_t coded_bits;
 } DECODER_MODEL;
 
-void av1_init_level_info(AV1LevelInfo *level_info[]);
+typedef struct {
+  AV1LevelStats level_stats;
+  AV1LevelSpec level_spec;
+  DECODER_MODEL decoder_models[SEQ_LEVELS];
+} AV1LevelInfo;
+
+void av1_init_level_info(struct AV1_COMP *cpi);
 
 void av1_update_level_info(struct AV1_COMP *cpi, size_t size, int64_t ts_start,
                            int64_t ts_end);
