b182ad6b41398d9f7016ea10ea9d6d4a17cbe852
sdeng <sdeng@google.com>
2019-06-12 16:15:56 -0700

    Update rdcost after the change of block size
    This is a preparation for adding tune=ssim mode. In tuning for SSIM
    mode rdmult will be different in different blocks. So, we need to
    recalculate the rdcost using the accumulated rates and distortions
    whenever the block size changes in the recursive partition searching
    stage.
    
    STATS_CHANGED:
             avg_psnr  ovr_psnr  ssim
    low_res  0.005     0.001     -0.025
    mid_res  pending
    hd_res   pending
    
    Change-Id: Ica461b25ca85748bf4e9be59dc6bf5d8d310c809

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -12791,6 +12791,7 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
         rd_cost->rate = rate2;
         rd_cost->dist = distortion2;
         rd_cost->rdcost = this_rd;
+        av1_rd_cost_update(x->rdmult, rd_cost);
         search_state.best_rd = this_rd;
         search_state.best_mbmode = *mbmi;
         search_state.best_skip2 = this_skip2;
@@ -12913,6 +12914,7 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
             mbmi->mode, mbmi->ref_frame[0], mbmi->ref_frame[1]);
         search_state.best_mode_index = mode_index;
         *rd_cost = rd_stats;
+        av1_rd_cost_update(x->rdmult, rd_cost);
         search_state.best_rd = rd_stats.rdcost;
         search_state.best_mbmode = *mbmi;
         search_state.best_skip2 = mbmi->skip;
@@ -12960,6 +12962,7 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
       // Note index of best mode so far
       search_state.best_mode_index = mode_index;
       *rd_cost = intra_rd_stats;
+      av1_rd_cost_update(x->rdmult, rd_cost);
       search_state.best_rd = intra_rd_stats.rdcost;
       search_state.best_mbmode = *mbmi;
       search_state.best_skip2 = 0;
@@ -12981,11 +12984,13 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
       cpi, x, mi_row, mi_col, rd_cost, bsize, ctx, search_state.best_mode_index,
       &search_state.best_mbmode, yv12_mb, search_state.best_rate_y,
       search_state.best_rate_uv, &search_state.best_skip2);
+  av1_rd_cost_update(x->rdmult, rd_cost);
 
   // Only try palette mode when the best mode so far is an intra mode.
   if (try_palette && !is_inter_mode(search_state.best_mbmode.mode)) {
     search_palette_mode(cpi, x, mi_row, mi_col, rd_cost, ctx, bsize, mbmi, pmi,
                         ref_costs_single, &search_state);
+    av1_rd_cost_update(x->rdmult, rd_cost);
   }
   search_state.best_mbmode.skip_mode = 0;
   if (cm->current_frame.skip_mode_info.skip_mode_flag &&
@@ -12993,6 +12998,7 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
       is_comp_ref_allowed(bsize)) {
     rd_pick_skip_mode(rd_cost, &search_state, cpi, x, bsize, mi_row, mi_col,
                       yv12_mb);
+    av1_rd_cost_update(x->rdmult, rd_cost);
   }
 
   // Make sure that the ref_mv_idx is only nonzero when we're
