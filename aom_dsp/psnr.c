4ebf578d354ca18ec168fcc2c76004810062b0cd
Sarah Parker <sarahparker@google.com>
2019-06-17 17:08:55 -0700

    Delete duplicate psnr function
    Change-Id: I9f8db7a0be412033924c135c8955887a8e05deb8

--- aom_dsp/psnr.c
+++ aom_dsp/psnr.c
@@ -16,15 +16,7 @@
 
 #include "aom_dsp/psnr.h"
 #include "aom_scale/yv12config.h"
-
-double aom_sse_to_psnr(double samples, double peak, double sse) {
-  if (sse > 0.0) {
-    const double psnr = 10.0 * log10(samples * peak * peak / sse);
-    return psnr > MAX_PSNR ? MAX_PSNR : psnr;
-  } else {
-    return MAX_PSNR;
-  }
-}
+#include "common/tools_common.h"
 
 /* TODO(yaowu): The block_variance calls the unoptimized versions of variance()
  * and highbd_8_variance(). It should not.
