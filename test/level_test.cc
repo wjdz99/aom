1c9ac53338b2228352111fa7adba1bdade71f681
Deepa K G <deepa.kg@ittiam.com>
2019-07-04 15:07:26 +0530

    Fix in calculation of key frame boost
    The FIRSTPASS_STATS pointer passed to the function
    calc_frame_boost was incorrect. This CL fixes the issue.
    
    (cherry picked from e7e9e5a9f144abb66483226ec38f0aaa1e60fea4)
    
    Change-Id: Ibf95a7395857b966b37c906ef92e6fed28347160

--- test/level_test.cc
+++ test/level_test.cc
@@ -69,6 +69,7 @@ class LevelTest
     }
 
     encoder->Control(AV1E_GET_SEQ_LEVEL_IDX, level_);
+    ASSERT_EQ(level_[0], 0);
     ASSERT_LE(level_[0], kLevelMax);
     ASSERT_GE(level_[0], kLevelMin);
   }
@@ -129,7 +130,7 @@ TEST_P(LevelTest, TestLevelMonitoringHighBitrate) {
     libaom_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 40);
     target_level_ = kLevelKeepStats;
-    cfg_.rc_target_bitrate = 3000;
+    cfg_.rc_target_bitrate = 4000;
     ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
     ASSERT_EQ(level_[0], 1);
   }
