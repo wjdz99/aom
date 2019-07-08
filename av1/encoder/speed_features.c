b1a570e2c2ad7622b07eee8c6b008cbb8448c6a3
Ranjit Kumar Tulabandu <ranjit.tulabandu@ittiam.com>
2019-07-08 15:08:09 +0530

    Reset hash logic before winner mode processing
    Hash logic is reset before winner mode evaluation
    to avoid incorrect usage.
    
    STATS_CHANGED
    
    Change-Id: Icd4537eac3e100b00d8fd29bcaef992d9a6e0d52

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -369,8 +369,6 @@ static void set_good_speed_features_framesize_independent(
     sf->prune_comp_type_by_model_rd = boosted ? 0 : 1;
     sf->disable_smooth_intra =
         !frame_is_intra_only(&cpi->common) || (cpi->rc.frames_to_key != 1);
-    // TODO(any): Experiment on the dependency of this speed feature with
-    // use_intra_txb_hash, use_inter_txb_hash and use_mb_rd_hash speed features
     // TODO(any): Refactor the code related to following winner mode speed
     // features
     sf->enable_winner_mode_for_coeff_opt = 1;
@@ -383,6 +381,8 @@ static void set_good_speed_features_framesize_independent(
 
   if (speed >= 4) {
     sf->selective_ref_frame = 4;
+    // TODO(any): Experiment with enabling of this speed feature as hash state
+    // is reset during winner mode processing
     sf->use_intra_txb_hash = 0;
     sf->tx_type_search.fast_intra_tx_type_search = 1;
     sf->disable_loop_restoration_chroma =
