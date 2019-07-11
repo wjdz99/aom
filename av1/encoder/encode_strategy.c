e7e918bfc43c3aba612e4e8a2b5e5d0465a74550
Marco Paniconi <marpan@google.com>
2019-07-11 14:13:25 -0700

    rtc: Add separate branch for 1 pass CBR RC
    Change-Id: I9c0529bdfa7a0ecaf929b80f61f8d6018344e923

--- av1/encoder/encode_strategy.c
+++ av1/encoder/encode_strategy.c
@@ -1084,13 +1084,14 @@ int av1_encode_strategy(AV1_COMP *const cpi, size_t *const size,
     cpi->common.frame_presentation_time = (uint32_t)pts64;
   }
 
-#if !CONFIG_REALTIME_ONLY
-  if (oxcf->pass != 1 && (!frame_params.show_existing_frame || is_overlay)) {
+  if (oxcf->pass == 0 && oxcf->rc_mode == AOM_CBR) {
+    av1_get_one_pass_cbr_params(cpi, &frame_params, *frame_flags);
+    frame_update_type = get_frame_update_type(cpi);
+  } else if (oxcf->pass != 1 && (!frame_params.show_existing_frame || is_overlay)) {
     // GF_GROUP needs updating for arf overlays as well as non-show-existing
     av1_get_second_pass_params(cpi, &frame_params, *frame_flags);
     frame_update_type = get_frame_update_type(cpi);
   }
-#endif
 
   if (frame_params.show_existing_frame &&
       frame_params.frame_type != KEY_FRAME) {
