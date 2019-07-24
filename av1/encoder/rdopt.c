791c2c4ac6ca7e71007ce6d2d839326c41140bb0
Remya <remya.prakasan@ittiam.com>
2019-07-24 11:51:18 +0530

    Add angle estimation for chroma
    STATS_CHANGED
    
    Change-Id: I4b6c0a9852289fac08a9c8e2c48139f1fcdc44c8

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -6350,9 +6350,35 @@ static int64_t rd_pick_intra_sbuv_mode(const AV1_COMP *const cpi, MACROBLOCK *x,
                                        BLOCK_SIZE bsize, TX_SIZE max_tx_size) {
   MACROBLOCKD *xd = &x->e_mbd;
   MB_MODE_INFO *mbmi = xd->mi[0];
+  const struct macroblockd_plane *const pd = &xd->plane[AOM_PLANE_U];
   assert(!is_inter_block(mbmi));
   MB_MODE_INFO best_mbmi = *mbmi;
   int64_t best_rd = INT64_MAX, this_rd;
+  uint8_t directional_mode_skip_mask[2][UV_INTRA_MODES] = { { 0 } };
+  int is_key_frame = cpi->common.current_frame.frame_type == KEY_FRAME;
+
+  if (cpi->sf.intra_angle_estimation && !is_key_frame) {
+    // Obtain chroma specific parameters
+    BLOCK_SIZE uv_bsize =
+        scale_chroma_bsize(bsize, pd->subsampling_x, pd->subsampling_y);
+    uv_bsize =
+        get_plane_block_size(uv_bsize, pd->subsampling_x, pd->subsampling_y);
+    const int rows = block_size_high[uv_bsize];
+    const int cols = block_size_wide[uv_bsize];
+    const int src_stride = x->plane[AOM_PLANE_U].src.stride;
+
+    // angle estimation for U and V
+    for (int plane = AOM_PLANE_U; plane <= AOM_PLANE_V; plane++) {
+      const uint8_t *src = x->plane[plane].src.buf;
+      angle_estimation(src, src_stride, rows, cols, bsize, is_cur_buf_hbd(xd),
+                       directional_mode_skip_mask[plane - 1]);
+    }
+    // Set directional_mode_skip_mask as a conjuction of U and V
+    for (int mode_idx = 0; mode_idx < UV_INTRA_MODES; mode_idx++) {
+      directional_mode_skip_mask[0][mode_idx] &=
+          directional_mode_skip_mask[1][mode_idx];
+    }
+  }
 
   for (int mode_idx = 0; mode_idx < UV_INTRA_MODES; ++mode_idx) {
     int this_rate;
@@ -6367,7 +6393,7 @@ static int64_t rd_pick_intra_sbuv_mode(const AV1_COMP *const cpi, MACROBLOCK *x,
       continue;
 
     if (!cpi->oxcf.enable_paeth_intra && mode == UV_PAETH_PRED) continue;
-
+    if (is_directional_mode && directional_mode_skip_mask[0][mode]) continue;
     mbmi->uv_mode = mode;
     int cfl_alpha_rate = 0;
     if (mode == UV_CFL_PRED) {
