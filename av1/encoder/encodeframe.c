dc0035ede7d357f904c10b1c624b720b1b345178
Lokeshwar  Reddy B <lokeshwar.reddy@ittiam.com>
2019-07-26 18:13:04 +0530

    Use lower rounding offset for coding coeffs
    STATS_CHANGED
    
    Change-Id: I89d884c4b6611d5a3df90f7b7d41b6dbcf7ed1f4

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -686,8 +686,7 @@ static void pick_sb_modes(AV1_COMP *const cpi, TileDataEnc *tile_data,
 
   // Default initialization of the threshold for R-D optimization of
   // coefficients for mode decision
-  x->coeff_opt_dist_threshold =
-      get_rd_opt_coeff_thresh(cpi->coeff_opt_dist_threshold, 0, 0);
+  update_rd_opt_coeff_thresh(x, cpi->coeff_opt_dist_threshold, 0, 0);
 
   // Save rdmult before it might be changed, so it can be restored later.
   const int orig_rdmult = x->rdmult;
