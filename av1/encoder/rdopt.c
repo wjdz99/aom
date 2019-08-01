602adf5e02967b0c456c9313287f1dc563fde414
Satish Kumar Suman <satish.suman@ittiam.com>
2019-08-01 19:37:20 +0530

    Revert "Fix issues with prune_compound_using_single_ref"
    This reverts commit 29ee2ae8d78470c24e1c251d18afdc5db931ebd7
    
    Reason for revert: SEGV on unknown address, in 32-bit x86
    AddressSanitizer build
    
    BUG=aomedia:2449
    
    STATS_CHANGED
    
    Change-Id: I3e6c320b76482360b5f1404d47c2c7bc5f0a9e66

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -12790,13 +12790,6 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
                                        INT64_MAX, INT64_MAX };
   const int skip_ctx = av1_get_skip_context(xd);
   for (int midx = 0; midx < MAX_MODES; ++midx) {
-    // After we done with single reference modes, find the 2nd best RD
-    // for a reference frame. Only search compound modes that have a reference
-    // frame at least as good as the 2nd best.
-    if (sf->prune_compound_using_single_ref &&
-        midx == MAX_SINGLE_REF_MODES + 1) {
-      find_top_ref(ref_frame_rd);
-    }
 
     if (inter_mode_compatible_skip(cpi, x, bsize, midx)) continue;
 
@@ -12811,6 +12804,14 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
     const MV_REFERENCE_FRAME ref_frame = mode_order->ref_frame[0];
     const MV_REFERENCE_FRAME second_ref_frame = mode_order->ref_frame[1];
     const int comp_pred = second_ref_frame > INTRA_FRAME;
+
+    // After we done with single reference modes, find the 2nd best RD
+    // for a reference frame. Only search compound modes that have a reference
+    // frame at least as good as the 2nd best.
+    if (sf->prune_compound_using_single_ref &&
+        midx == MAX_SINGLE_REF_MODES + 1) {
+      find_top_ref(ref_frame_rd);
+    }
 
     if (sf->prune_compound_using_single_ref && midx > MAX_SINGLE_REF_MODES &&
         comp_pred &&
