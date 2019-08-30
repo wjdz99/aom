d80b4f5c7fab70c82e676976141161f4e0ae55c0
Wan-Teh Chang <wtc@google.com>
2019-08-30 12:01:26 -0700

    Remove cm->next_ref_frame_map, pbi->hold_ref_buf.
    cm->next_ref_frame_map was intended to support frame parallel decoding.
    Since frame parallel decoding was not implemented, we can remove
    cm->next_ref_frame_map and the associated pbi->hold_ref_buf boolean.
    
    swap_frame_buffers() is renamed update_frame_buffers() because the
    function no longer swaps cm->ref_frame_map and cm->next_ref_frame_map;
    now it updates cm->ref_frame_map. Also, release_frame_buffers() is
    renamed release_current_frame() because the only frame buffer it
    releases now is cm->current_frame.
    
    BUG=aomedia:2110,oss-fuzz:16437
    
    Change-Id: I32dab3d01fb4574f2563f535c24804c6775933d2

--- av1/decoder/decoder.h
+++ av1/decoder/decoder.h
@@ -198,8 +198,6 @@ typedef struct AV1Decoder {
   int max_threads;
   int inv_tile_order;
   int need_resync;   // wait for key/intra-only frame.
-  int hold_ref_buf;  // Boolean: whether we are holding reference buffers in
-                     // common.next_ref_frame_map.
   int reset_decoder_state;
 
   int tile_size_bytes;
