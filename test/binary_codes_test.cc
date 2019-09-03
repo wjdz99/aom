4c6177b6d0b3e2aeae329857909f91f9d4f4d073
Wan-Teh Chang <wtc@google.com>
2019-09-03 13:01:37 -0700

    Test aom_write_symbol for nsymbs=2,3,4,7,8.
    Change-Id: I19437958a04d47bf20a5f98a3f9172f52eb5fca9

--- test/binary_codes_test.cc
+++ test/binary_codes_test.cc
@@ -33,7 +33,7 @@ namespace {
 // Test for Finite subexponential code with reference
 TEST(AV1, TestPrimitiveRefsubexpfin) {
   ACMRandom rnd(ACMRandom::DeterministicSeed());
-  const int kBufferSize = 65536;
+  const unsigned int kBufferSize = 65536;
   aom_writer bw;
   uint8_t bw_buffer[kBufferSize];
   const uint16_t kRanges = 8;
@@ -60,6 +60,7 @@ TEST(AV1, TestPrimitiveRefsubexpfin) {
     }
   }
   aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
   aom_reader br;
   aom_reader_init(&br, bw_buffer, bw.pos);
   GTEST_ASSERT_GE(aom_reader_tell(&br), 0u);
@@ -79,5 +80,312 @@ TEST(AV1, TestPrimitiveRefsubexpfin) {
     }
   }
 }
+
+// Test for aom_write_symbol with nsymbs=2.
+TEST(AV1, TestSymbol2) {
+  aom_cdf_prob cdf[4][3] = {
+    { 16384, 0, 0 },
+    { 32768 - 8386, 0, 0 },
+    { 32768 - 24312, 0, 0 },
+    { 16384, 0, 0 },
+  };
+  constexpr int kSymbols[4][4] = { { 0, 0, 1, 1 },  //
+                                   { 0, 1, 1, 0 },  //
+                                   { 1, 0, 1, 0 },  //
+                                   { 1, 0, 0, 1 } };
+  const unsigned int kBufferSize = 65536;
+  uint8_t bw_buffer[kBufferSize];
+  aom_writer bw;
+  bw.allow_update_cdf = 1;
+  aom_start_encode(&bw, bw_buffer);
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        aom_write_symbol(&bw, kSymbols[j][k], cdf[k], 2);
+      }
+    }
+  }
+  aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
+  aom_cdf_prob cdf0[4][3] = {
+    { 16384, 0, 0 },
+    { 32768 - 8386, 0, 0 },
+    { 32768 - 24312, 0, 0 },
+    { 16384, 0, 0 },
+  };
+  aom_reader br;
+  br.allow_update_cdf = 1;
+  aom_reader_init(&br, bw_buffer, bw.pos);
+  int symbol;
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        symbol = aom_read_symbol(&br, cdf0[k], 2, ACCT_STR);
+        EXPECT_EQ(symbol, kSymbols[j][k]);
+      }
+    }
+  }
+}
+
+// Test for aom_write_symbol with nsymbs=3.
+TEST(AV1, TestSymbol3) {
+  aom_cdf_prob cdf[4][4] = {
+    // pdf: 1/3, 1/3, 1/3
+    { 32768 - 10923, 32768 - 21845, 0, 0 },
+    // pdf: 1/6, 2/6, 3/6
+    { 32768 - 5461, 32768 - 16384, 0, 0 },
+    // pdf: 2/6, 3/6, 1/6
+    { 32768 - 10923, 32768 - 27307, 0, 0 },
+    // pdf: 3/6, 1/6, 2/6
+    { 32768 - 16384, 32768 - 21845, 0, 0 },
+  };
+  constexpr int kSymbols[6][4] = { { 0, 2, 1, 2 },  //
+                                   { 1, 1, 2, 1 },  //
+                                   { 2, 0, 0, 0 },  //
+                                   { 0, 2, 0, 2 },  //
+                                   { 1, 2, 1, 0 },  //
+                                   { 2, 1, 1, 0 } };
+  const unsigned int kBufferSize = 65536;
+  uint8_t bw_buffer[kBufferSize];
+  aom_writer bw;
+  bw.allow_update_cdf = 1;
+  aom_start_encode(&bw, bw_buffer);
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 6; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        aom_write_symbol(&bw, kSymbols[j][k], cdf[k], 3);
+      }
+    }
+  }
+  aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
+  aom_cdf_prob cdf0[4][4] = {
+    // pdf: 1/3, 1/3, 1/3
+    { 32768 - 10923, 32768 - 21845, 0, 0 },
+    // pdf: 1/6, 2/6, 3/6
+    { 32768 - 5461, 32768 - 16384, 0, 0 },
+    // pdf: 2/6, 3/6, 1/6
+    { 32768 - 10923, 32768 - 27307, 0, 0 },
+    // pdf: 3/6, 1/6, 2/6
+    { 32768 - 16384, 32768 - 21845, 0, 0 },
+  };
+  aom_reader br;
+  br.allow_update_cdf = 1;
+  aom_reader_init(&br, bw_buffer, bw.pos);
+  int symbol;
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 6; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        symbol = aom_read_symbol(&br, cdf0[k], 3, ACCT_STR);
+        EXPECT_EQ(symbol, kSymbols[j][k]);
+      }
+    }
+  }
+}
+
+// Test for aom_write_symbol with nsymbs=4.
+TEST(AV1, TestSymbol4) {
+  aom_cdf_prob cdf[4][5] = {
+    // pdf: 1/4, 1/4, 1/4, 1/4
+    { 32768 - 8192, 32768 - 16384, 32768 - 24576, 0, 0 },
+    // pdf: 2/8, 1/8, 2/8, 3/8
+    { 32768 - 8192, 32768 - 12288, 32768 - 20480, 0, 0 },
+    // pdf: 1/4, 1/4, 1/4, 1/4
+    { 32768 - 8192, 32768 - 16384, 32768 - 24576, 0, 0 },
+    // pdf: 2/8, 3/8, 2/8, 1/8
+    { 32768 - 8192, 32768 - 20480, 32768 - 28672, 0, 0 },
+  };
+  constexpr int kSymbols[8][4] = { { 0, 0, 3, 3 },  //
+                                   { 0, 0, 2, 2 },  //
+                                   { 1, 1, 0, 0 },  //
+                                   { 1, 2, 1, 1 },  //
+                                   { 2, 2, 3, 2 },  //
+                                   { 2, 3, 2, 1 },  //
+                                   { 3, 3, 0, 0 },  //
+                                   { 3, 3, 1, 1 } };
+  const unsigned int kBufferSize = 65536;
+  uint8_t bw_buffer[kBufferSize];
+  aom_writer bw;
+  bw.allow_update_cdf = 1;
+  aom_start_encode(&bw, bw_buffer);
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 8; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        aom_write_symbol(&bw, kSymbols[j][k], cdf[k], 4);
+      }
+    }
+  }
+  aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
+  aom_cdf_prob cdf0[4][5] = {
+    // pdf: 1/4, 1/4, 1/4, 1/4
+    { 32768 - 8192, 32768 - 16384, 32768 - 24576, 0, 0 },
+    // pdf: 2/8, 1/8, 2/8, 3/8
+    { 32768 - 8192, 32768 - 12288, 32768 - 20480, 0, 0 },
+    // pdf: 1/4, 1/4, 1/4, 1/4
+    { 32768 - 8192, 32768 - 16384, 32768 - 24576, 0, 0 },
+    // pdf: 2/8, 3/8, 2/8, 1/8
+    { 32768 - 8192, 32768 - 20480, 32768 - 28672, 0, 0 },
+  };
+  aom_reader br;
+  br.allow_update_cdf = 1;
+  aom_reader_init(&br, bw_buffer, bw.pos);
+  int symbol;
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 8; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        symbol = aom_read_symbol(&br, cdf0[k], 4, ACCT_STR);
+        EXPECT_EQ(symbol, kSymbols[j][k]);
+      }
+    }
+  }
+}
+
+// Test for aom_write_symbol with nsymbs=7.
+TEST(AV1, TestSymbol7) {
+  aom_cdf_prob cdf[4][8] = {
+    // pdf: 1/7, 1/7, 1/7, 1/7, 1/7, 1/7, 1/7
+    { 32768 - 4681, 32768 - 9362, 32768 - 14043, 32768 - 18725, 32768 - 23406,
+      32768 - 28087, 0, 0 },
+    // pdf: 3/14, 2/14, 2/14, 2/14, 2/14, 2/14, 1/14
+    { 32768 - 7022, 32768 - 11703, 32768 - 16384, 32768 - 21065, 32768 - 25746,
+      32768 - 30427, 0, 0 },
+    // pdf: 1/14, 1/14, 2/14, 2/14, 2/14, 3/14, 3/14
+    { 32768 - 2341, 32768 - 4681, 32768 - 9362, 32768 - 14043, 32768 - 18725,
+      32768 - 25746, 0, 0 },
+    // pdf: 1/14, 2/14, 3/14, 3/14, 2/14, 2/14, 1/14
+    { 32768 - 2341, 32768 - 7022, 32768 - 14043, 32768 - 21065, 32768 - 25746,
+      32768 - 30427, 0, 0 },
+  };
+  constexpr int kSymbols[14][4] = { { 0, 4, 6, 3 },  //
+                                    { 1, 5, 5, 2 },  //
+                                    { 2, 6, 4, 1 },  //
+                                    { 3, 0, 3, 0 },  //
+                                    { 4, 1, 2, 6 },  //
+                                    { 5, 2, 1, 5 },  //
+                                    { 6, 3, 0, 4 },  //
+                                    { 0, 0, 6, 5 },  //
+                                    { 2, 1, 4, 3 },  //
+                                    { 4, 3, 6, 1 },  //
+                                    { 6, 5, 2, 4 },  //
+                                    { 1, 0, 5, 2 },  //
+                                    { 3, 2, 3, 2 },  //
+                                    { 5, 4, 5, 3 } };
+  const unsigned int kBufferSize = 65536;
+  uint8_t bw_buffer[kBufferSize];
+  aom_writer bw;
+  bw.allow_update_cdf = 1;
+  aom_start_encode(&bw, bw_buffer);
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 14; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        aom_write_symbol(&bw, kSymbols[j][k], cdf[k], 7);
+      }
+    }
+  }
+  aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
+  aom_cdf_prob cdf0[4][8] = {
+    // pdf: 1/7, 1/7, 1/7, 1/7, 1/7, 1/7, 1/7
+    { 32768 - 4681, 32768 - 9362, 32768 - 14043, 32768 - 18725, 32768 - 23406,
+      32768 - 28087, 0, 0 },
+    // pdf: 3/14, 2/14, 2/14, 2/14, 2/14, 2/14, 1/14
+    { 32768 - 7022, 32768 - 11703, 32768 - 16384, 32768 - 21065, 32768 - 25746,
+      32768 - 30427, 0, 0 },
+    // pdf: 1/14, 1/14, 2/14, 2/14, 2/14, 3/14, 3/14
+    { 32768 - 2341, 32768 - 4681, 32768 - 9362, 32768 - 14043, 32768 - 18725,
+      32768 - 25746, 0, 0 },
+    // pdf: 1/14, 2/14, 3/14, 3/14, 2/14, 2/14, 1/14
+    { 32768 - 2341, 32768 - 7022, 32768 - 14043, 32768 - 21065, 32768 - 25746,
+      32768 - 30427, 0, 0 },
+  };
+  aom_reader br;
+  br.allow_update_cdf = 1;
+  aom_reader_init(&br, bw_buffer, bw.pos);
+  int symbol;
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 14; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        symbol = aom_read_symbol(&br, cdf0[k], 7, ACCT_STR);
+        EXPECT_EQ(symbol, kSymbols[j][k]);
+      }
+    }
+  }
+}
+
+// Test for aom_write_symbol with nsymbs=8.
+TEST(AV1, TestSymbol8) {
+  aom_cdf_prob cdf[4][9] = {
+    // pdf: 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8
+    { 32768 - 4096, 32768 - 8192, 32768 - 12288, 32768 - 16384, 32768 - 20480,
+      32768 - 24576, 32768 - 28672, 0, 0 },
+    // pdf: 3/16, 2/16, 2/16, 2/16, 2/16, 2/16, 2/16, 1/16
+    { 32768 - 6144, 32768 - 10240, 32768 - 14336, 32768 - 18432, 32768 - 22528,
+      32768 - 26624, 32768 - 30720, 0, 0 },
+    // pdf: 1/16, 1/16, 2/16, 2/16, 2/16, 2/16, 3/16, 3/16
+    { 32768 - 2048, 32768 - 4096, 32768 - 8192, 32768 - 12288, 32768 - 16384,
+      32768 - 20480, 32768 - 26624, 0, 0 },
+    // pdf: 1/16, 1/16, 3/16, 3/16, 3/16, 3/16, 1/16, 1/16
+    { 32768 - 2048, 32768 - 4096, 32768 - 10240, 32768 - 16384, 32768 - 22528,
+      32768 - 28672, 32768 - 30720, 0, 0 },
+  };
+  constexpr int kSymbols[16][4] = { { 0, 4, 7, 3 },  //
+                                    { 1, 5, 6, 2 },  //
+                                    { 2, 6, 5, 1 },  //
+                                    { 3, 7, 4, 0 },  //
+                                    { 4, 0, 3, 7 },  //
+                                    { 5, 1, 2, 6 },  //
+                                    { 6, 2, 1, 5 },  //
+                                    { 7, 3, 0, 4 },  //
+                                    { 0, 0, 6, 5 },  //
+                                    { 2, 1, 4, 3 },  //
+                                    { 4, 3, 6, 4 },  //
+                                    { 6, 5, 2, 2 },  //
+                                    { 1, 0, 7, 3 },  //
+                                    { 3, 2, 5, 5 },  //
+                                    { 5, 4, 7, 2 },  //
+                                    { 7, 6, 3, 4 } };
+  const unsigned int kBufferSize = 65536;
+  uint8_t bw_buffer[kBufferSize];
+  aom_writer bw;
+  bw.allow_update_cdf = 1;
+  aom_start_encode(&bw, bw_buffer);
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 16; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        aom_write_symbol(&bw, kSymbols[j][k], cdf[k], 8);
+      }
+    }
+  }
+  aom_stop_encode(&bw);
+  GTEST_ASSERT_LE(bw.pos, kBufferSize);
+  aom_cdf_prob cdf0[4][9] = {
+    // pdf: 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8
+    { 32768 - 4096, 32768 - 8192, 32768 - 12288, 32768 - 16384, 32768 - 20480,
+      32768 - 24576, 32768 - 28672, 0, 0 },
+    // pdf: 3/16, 2/16, 2/16, 2/16, 2/16, 2/16, 2/16, 1/16
+    { 32768 - 6144, 32768 - 10240, 32768 - 14336, 32768 - 18432, 32768 - 22528,
+      32768 - 26624, 32768 - 30720, 0, 0 },
+    // pdf: 1/16, 1/16, 2/16, 2/16, 2/16, 2/16, 3/16, 3/16
+    { 32768 - 2048, 32768 - 4096, 32768 - 8192, 32768 - 12288, 32768 - 16384,
+      32768 - 20480, 32768 - 26624, 0, 0 },
+    // pdf: 1/16, 1/16, 3/16, 3/16, 3/16, 3/16, 1/16, 1/16
+    { 32768 - 2048, 32768 - 4096, 32768 - 10240, 32768 - 16384, 32768 - 22528,
+      32768 - 28672, 32768 - 30720, 0, 0 },
+  };
+  aom_reader br;
+  br.allow_update_cdf = 1;
+  aom_reader_init(&br, bw_buffer, bw.pos);
+  int symbol;
+  for (int i = 0; i < 1024; ++i) {
+    for (int j = 0; j < 16; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        symbol = aom_read_symbol(&br, cdf0[k], 8, ACCT_STR);
+        EXPECT_EQ(symbol, kSymbols[j][k]);
+      }
+    }
+  }
+}
+
 // TODO(debargha): Adds tests for other primitives
 }  // namespace
