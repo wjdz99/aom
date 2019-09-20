34d84d3adcacda48460bd2307122db3af05c104b
Jerome Jiang <jianj@google.com>
2019-09-20 15:38:27 -0700

    Remove cnn.{c,h} from CONFIG_REALTIME_ONLY
    BUG=aomedia:2397
    
    Change-Id: I96eea24b1c2df4afced322932a449695ce199f07

--- av1/av1.cmake
+++ av1/av1.cmake
@@ -401,6 +401,8 @@ endif()
 
 if(CONFIG_REALTIME_ONLY)
   list(REMOVE_ITEM AOM_AV1_ENCODER_SOURCES
+                   "${AOM_ROOT}/av1/encoder/cnn.c"
+                   "${AOM_ROOT}/av1/encoder/cnn.h"
                    "${AOM_ROOT}/av1/encoder/firstpass.c"
                    "${AOM_ROOT}/av1/encoder/firstpass.h"
                    "${AOM_ROOT}/av1/encoder/gop_structure.c"
@@ -409,6 +411,7 @@ if(CONFIG_REALTIME_ONLY)
                    "${AOM_ROOT}/av1/encoder/mbgraph.c"
                    "${AOM_ROOT}/av1/encoder/mbgraph.h"
                    "${AOM_ROOT}/av1/encoder/partition_model_weights.h"
+                   "${AOM_ROOT}/av1/encoder/partition_cnn_weights.h"
                    "${AOM_ROOT}/av1/encoder/temporal_filter.c"
                    "${AOM_ROOT}/av1/encoder/temporal_filter.h"
                    "${AOM_ROOT}/av1/encoder/temporal_filter_constants.h"
