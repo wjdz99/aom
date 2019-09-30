9212d582dd378f9dbfdb627eaf1b44787ac9fa97
Urvang Joshi <urvang@google.com>
2019-09-30 11:50:28 -0700

    Correctly restore floating point exceptions
    BUG=aomedia:2459
    
    Change-Id: I7efbca9ba2ba3802637f60c9d5f0a50a8c043d69

--- aom/src/aom_encoder.c
+++ aom/src/aom_encoder.c
@@ -192,7 +192,7 @@ aom_codec_err_t aom_codec_enc_config_default(aom_codec_iface_t *iface,
 #define FLOATING_POINT_SET_EXCEPTIONS \
   const int float_excepts =           \
       feenableexcept(FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW);
-#define FLOATING_POINT_RESTORE_EXCEPTIONS feenableexcept(float_excepts);
+#define FLOATING_POINT_RESTORE_EXCEPTIONS fdisableexcept(float_excepts);
 #else
 #define FLOATING_POINT_SET_EXCEPTIONS
 #define FLOATING_POINT_RESTORE_EXCEPTIONS
