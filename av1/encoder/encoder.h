a8747260d23e75a8b56d2e85c5b45d1cca4982ad
Jingning Han <jingning@google.com>
2019-10-02 16:05:39 -0700

    Introduce rate propagation factor
    Support a rate cost propagation pipeline to estimate the associated
    rate cost due to quantization noise. The new tpl propagation kernel
    improves the compression efficiency. In the settings of speed 1, VBR
    mode, and 150 frames, the coding gains are:
    
            avg PSNR   ovr PSNR  SSIM
    lowres   0.01%      0.04%    -0.43%
    midres  -0.29%     -0.16%    -0.98%
    ugc360  -0.15%      0.05%    -0.77%
    hdres   -0.31%     -0.27%    -1.36%
    
    Change-Id: I4c08474adfe98f2bf6dc3372ff17089a0d74c206

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -168,7 +168,11 @@ typedef struct TplDepStats {
   int64_t mc_dep_cost;
   int64_t srcrf_dist;
   int64_t recrf_dist;
+  int64_t srcrf_rate;
+  int64_t recrf_rate;
   int64_t mc_dep_delta;
+  int64_t mc_dep_rate;
+  int64_t mc_dep_dist;
   int64_t src_rdcost;
   int64_t rec_rdcost;
   int_mv mv;
@@ -192,6 +196,7 @@ typedef struct TplDepFrame {
   int height;
   int mi_rows;
   int mi_cols;
+  int base_rdmult;
 } TplDepFrame;
 
 typedef enum {
