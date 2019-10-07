f293d5f7a33ddb7db4f54a7980b54dd35b267d64
Urvang Joshi <urvang@google.com>
2019-10-07 12:32:51 -0700

    Introduce NEW_TX64X64 experiment.
    The eventual idea is:
    Encode side:
    - Downscale the 64x64 residuals to 32x32
    - Perform 32x32 2D transform
    Decode side:
    - 32x32 inverse transform
    - Upscale to 64x64
    (TODO later: refine the upscaled residuals with trasmitted filter
    coefficients).
    
    This patch only implements this for 64x64 transform, other 64 length
    transforms TODO later.
    
    STATS_CHANGED
    
    Change-Id: I2db4888ff874490f83de1df91f1c02c23189aa65

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -162,6 +162,8 @@ set_aom_config_var(CONFIG_ENTROPY_CONTEXTS 0 NUMBER
                    "AV2 entropy coding contexts experiment flag")
 set_aom_config_var(CONFIG_WIENER_NONSEP 0 NUMBER
                    "AV2 nonsep Wiener filter experiment flag")
+set_aom_config_var(CONFIG_NEW_TX64X64 1 NUMBER
+                   "AV2 miscellaneous bitstream changes experiment flag")
 
 # To include Tensorflow, make sure to build tensorflow locally using
 # tensorflow/contrib/makefile/build_all_linux.sh and then providing the correct
