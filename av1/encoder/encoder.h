b4526e1b4969bc3da4482b7a602e4b865bf6766b
Jingning Han <jingning@google.com>
2019-10-07 09:14:44 -0700

    Consolidate computations needed in the tpl synthesizer
    Factor out common computations and remove unneeded ones from the
    model synthesizer.
    
    Change-Id: Ibf4085284e13bde3b0e86a541039f1c306564556
    (cherry picked from commit 60a00de69ca79fe5f51dcbf862aaaa8eb50ec344)

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -165,8 +165,6 @@ enum {
 typedef struct TplDepStats {
   int64_t intra_cost;
   int64_t inter_cost;
-  int64_t mc_flow;
-  int64_t mc_dep_cost;
   int64_t srcrf_dist;
   int64_t recrf_dist;
   int64_t srcrf_rate;
@@ -178,7 +176,6 @@ typedef struct TplDepStats {
   int64_t rec_rdcost;
   int_mv mv;
   int ref_frame_index;
-  double quant_ratio;
   int64_t mc_count;
   int64_t mc_saved;
 } TplDepStats;
