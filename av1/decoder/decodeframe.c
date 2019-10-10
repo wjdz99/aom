71039bd574a11a5e66032606434e0674020b9f4b
Hui Su <huisu@google.com>
2019-10-10 16:27:16 -0700

    Store tx type in frame level buffer
    Change-Id: Ib306735cc320a6c6953d358f9197b4b33c68c554

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -342,6 +342,8 @@ static AOM_INLINE void set_offsets(AV1_COMMON *const cm, MACROBLOCKD *const xd,
 
   xd->mi = cm->mi_grid_base + get_mi_grid_idx(cm, mi_row, mi_col);
   xd->mi[0] = &cm->mi[get_alloc_mi_idx(cm, mi_row, mi_col)];
+  xd->tx_type_map = &cm->tx_type_map[mi_row * cm->mi_stride + mi_col];
+  xd->tx_type_map_stride = cm->mi_stride;
   // TODO(slavarnway): Generate sb_type based on bwl and bhl, instead of
   // passing bsize from decode_partition().
   xd->mi[0]->sb_type = bsize;
