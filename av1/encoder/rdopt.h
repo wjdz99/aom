3976e7438f6303ec8861e7d2fe0aea3fc1a1d0e4
Cherma Rajan A <cherma.rajan@ittiam.com>
2019-10-15 14:32:35 +0530

    Experiment on tx size search for inter frames
    STATS_CHANGED
    
    Change-Id: Idf837a07d7fa1c4e122a171033757f47ce7ec49b

--- av1/encoder/rdopt.h
+++ av1/encoder/rdopt.h
@@ -320,6 +320,9 @@ static INLINE void set_mode_eval_params(const struct AV1_COMP *cpi,
           get_rd_opt_coeff_thresh(cpi->coeff_opt_dist_threshold, 0, 0);
       // Set default transform size search method
       set_tx_size_search_method(cpi, x, 0, 0);
+      x->prune_tx_type = 0;
+      x->max_tx_count = TX_TYPES;
+      x->prune_mode = cpi->sf.tx_type_search.prune_mode;
       break;
     case MODE_EVAL:
       x->use_default_intra_tx_type =
@@ -340,6 +343,9 @@ static INLINE void set_mode_eval_params(const struct AV1_COMP *cpi,
       // Set the transform size search method for mode evaluation
       set_tx_size_search_method(cpi, x, sf->enable_winner_mode_for_tx_size_srch,
                                 0);
+      x->prune_tx_type = 1;
+      x->max_tx_count = 3;
+      x->prune_mode = cpi->sf.tx_type_search.prune_mode;
       break;
     case WINNER_MODE_EVAL:
       x->use_default_inter_tx_type = 0;
@@ -357,6 +363,9 @@ static INLINE void set_mode_eval_params(const struct AV1_COMP *cpi,
       // Set the transform size search method for winner mode evaluation
       set_tx_size_search_method(cpi, x, sf->enable_winner_mode_for_tx_size_srch,
                                 1);
+      x->prune_tx_type = 0;
+      x->max_tx_count = TX_TYPES;
+      x->prune_mode = PRUNE_2D_ACCURATE;
       break;
     default: assert(0);
   }
