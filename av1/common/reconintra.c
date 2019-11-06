971e72bb075f0622cb65a8d455dcc9597b2c7545
Hui Su <huisu@google.com>
2019-11-05 16:11:47 -0800

    Derived intra prediction mode experiment
    With this experiment, a derived intra prediction mode is added where the
    prediction angle is derived from histogram of gradents of reconstructed
    pixel values..
    
    This patch only implements the experiment for keyframe luma channel.
    Current gains for all-intra coding:
    lowres -0.07%; midres -0.24%; hdres -0.29%.
    
    Change-Id: If2ca2bbcd9320dd2f02d0437e16235ba05e477bc

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -1924,6 +1924,9 @@ static void build_intra_predictors_high(
 #if CONFIG_ADAPT_FILTER_INTRA
     ADAPT_FILTER_INTRA_MODE adapt_filter_intra_mode, int col_off, int row_off,
 #endif
+#if CONFIG_DERIVED_INTRA_MODE
+    int derived_angle,
+#endif  // CONFIG_DERIVED_INTRA_MODE
     int plane) {
   int i;
   uint16_t *dst = CONVERT_TO_SHORTPTR(dst8);
@@ -1953,6 +1956,9 @@ static void build_intra_predictors_high(
 
   if (is_dr_mode) {
     p_angle = mode_to_angle_map[mode] + angle_delta;
+#if CONFIG_DERIVED_INTRA_MODE
+    if (derived_angle > 0) p_angle = derived_angle;
+#endif  // CONFIG_DERIVED_INTRA_MODE
     if (p_angle <= 90)
       need_above = 1, need_left = 0, need_above_left = 1;
     else if (p_angle < 180)
@@ -2135,6 +2141,9 @@ static void build_intra_predictors(
 #if CONFIG_ADAPT_FILTER_INTRA
     ADAPT_FILTER_INTRA_MODE adapt_filter_intra_mode, int col_off, int row_off,
 #endif
+#if CONFIG_DERIVED_INTRA_MODE
+    int derived_angle,
+#endif  // CONFIG_DERIVED_INTRA_MODE
     int plane) {
   int i;
   const uint8_t *above_ref = ref - ref_stride;
@@ -2162,6 +2171,9 @@ static void build_intra_predictors(
 
   if (is_dr_mode) {
     p_angle = mode_to_angle_map[mode] + angle_delta;
+#if CONFIG_DERIVED_INTRA_MODE
+    if (derived_angle > 0) p_angle = derived_angle;
+#endif  // CONFIG_DERIVED_INTRA_MODE
     if (p_angle <= 90)
       need_above = 1, need_left = 0, need_above_left = 1;
     else if (p_angle < 180)
@@ -2342,6 +2354,9 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
 #if CONFIG_ADAPT_FILTER_INTRA
                              ADAPT_FILTER_INTRA_MODE adapt_filter_intra_mode,
 #endif
+#if CONFIG_DERIVED_INTRA_MODE
+                             int derived_angle,
+#endif  // CONFIG_DERIVED_INTRA_MODE
                              const uint8_t *ref, int ref_stride, uint8_t *dst,
                              int dst_stride, int col_off, int row_off,
                              int plane) {
@@ -2431,6 +2446,9 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
 #if CONFIG_ADAPT_FILTER_INTRA
                                 adapt_filter_intra_mode, col_off, row_off,
 #endif
+#if CONFIG_DERIVED_INTRA_MODE
+                                derived_angle,
+#endif  // CONFIG_DERIVED_INTRA_MODE
                                 plane);
     return;
   }
@@ -2445,6 +2463,9 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
 #if CONFIG_ADAPT_FILTER_INTRA
                          adapt_filter_intra_mode, col_off, row_off,
 #endif
+#if CONFIG_DERIVED_INTRA_MODE
+                         derived_angle,
+#endif  // CONFIG_DERIVED_INTRA_MODE
                          plane);
 }
 
@@ -2491,13 +2512,16 @@ void av1_predict_intra_block_facade(const AV1_COMMON *cm, MACROBLOCKD *xd,
     CFL_CTX *const cfl = &xd->cfl;
     CFL_PRED_TYPE pred_plane = get_cfl_pred_type(plane);
     if (cfl->dc_pred_is_cached[pred_plane] == 0) {
-      av1_predict_intra_block(cm, xd, pd->width, pd->height, tx_size, mode,
-                              angle_delta, use_palette, filter_intra_mode,
+      av1_predict_intra_block(
+          cm, xd, pd->width, pd->height, tx_size, mode, angle_delta,
+          use_palette, filter_intra_mode,
 #if CONFIG_ADAPT_FILTER_INTRA
-                              adapt_filter_intra_mode,
+          adapt_filter_intra_mode,
 #endif
-                              dst, dst_stride, dst, dst_stride, blk_col,
-                              blk_row, plane);
+#if CONFIG_DERIVED_INTRA_MODE
+          !plane && mbmi->use_derived_intra_mode ? mbmi->derived_angle : 0,
+#endif  // CONFIG_DERIVED_INTRA_MODE
+          dst, dst_stride, dst, dst_stride, blk_col, blk_row, plane);
       if (cfl->use_dc_pred_cache) {
         cfl_store_dc_pred(xd, dst, pred_plane, tx_size_wide[tx_size]);
         cfl->dc_pred_is_cached[pred_plane] = 1;
@@ -2508,13 +2532,16 @@ void av1_predict_intra_block_facade(const AV1_COMMON *cm, MACROBLOCKD *xd,
     cfl_predict_block(xd, dst, dst_stride, tx_size, plane);
     return;
   }
-  av1_predict_intra_block(cm, xd, pd->width, pd->height, tx_size, mode,
-                          angle_delta, use_palette, filter_intra_mode,
+  av1_predict_intra_block(
+      cm, xd, pd->width, pd->height, tx_size, mode, angle_delta, use_palette,
+      filter_intra_mode,
 #if CONFIG_ADAPT_FILTER_INTRA
-                          adapt_filter_intra_mode,
+      adapt_filter_intra_mode,
 #endif
-                          dst, dst_stride, dst, dst_stride, blk_col, blk_row,
-                          plane);
+#if CONFIG_DERIVED_INTRA_MODE
+      !plane && mbmi->use_derived_intra_mode ? mbmi->derived_angle : 0,
+#endif  // CONFIG_DERIVED_INTRA_MODE
+      dst, dst_stride, dst, dst_stride, blk_col, blk_row, plane);
 }
 
 void av1_init_intra_predictors(void) {
