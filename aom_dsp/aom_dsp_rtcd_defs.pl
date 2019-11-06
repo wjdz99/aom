31aa3952078e562a12bc6cbe9115348849e6c541
Sachin Kumar Garg <sachin.kumargarg@ittiam.com>
2019-11-06 14:26:20 +0530

    Enable sub_pixel_variance SSSE3 modules
    The following sub_pixel_variance ssse3 modules
    disabled for CONFIG_3WAY_PARTITIONS have been
    re-enabled:
    sub_pixel_variance128x128
    sub_pixel_variance128x64
    sub_pixel_variance64x128
    sub_pixel_variance64x64
    sub_pixel_variance64x32
    sub_pixel_variance32x64
    sub_pixel_variance32x32
    sub_pixel_variance32x16
    sub_pixel_variance32x8
    
    No alignment issues were observed with the above modules.
    
    Change-Id: I8d3065a6d53356f827d7857ee43d1596658764f3

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -1202,30 +1202,30 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   specialize qw/aom_variance4x8       sse2           msa/;
   specialize qw/aom_variance4x4       sse2           msa/;
 
-  specialize qw/aom_sub_pixel_variance128x128    avx2/;
-  specialize qw/aom_sub_pixel_variance128x64     avx2/;
-  specialize qw/aom_sub_pixel_variance64x128     avx2/;
-  specialize qw/aom_sub_pixel_variance64x64      avx2/;
-  specialize qw/aom_sub_pixel_variance64x32      avx2/;
-  specialize qw/aom_sub_pixel_variance32x64      avx2/;
-  specialize qw/aom_sub_pixel_variance32x32      avx2/;
-  specialize qw/aom_sub_pixel_variance32x16      avx2/;
+  specialize qw/aom_sub_pixel_variance128x128   ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance128x64    ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance64x128    ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance64x64     ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance64x32     ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance32x64     ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance32x32     ssse3 avx2/;
+  specialize qw/aom_sub_pixel_variance32x16     ssse3 avx2/;
   specialize qw/aom_sub_pixel_variance16x32     ssse3/;
   specialize qw/aom_sub_pixel_variance16x16     ssse3/;
   specialize qw/aom_sub_pixel_variance16x8      ssse3/;
 
 if (aom_config("CONFIG_3WAY_PARTITIONS") eq "") {
-    specialize qw/aom_sub_pixel_variance128x128                 sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance128x64                  sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance64x128                  sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance64x64          neon msa sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance64x32               msa sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance32x64               msa sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance32x32          neon msa sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance32x16               msa sse2 ssse3/;
-    specialize qw/aom_sub_pixel_variance16x32                     msa sse2/;
-    specialize qw/aom_sub_pixel_variance16x16                neon msa sse2/;
-    specialize qw/aom_sub_pixel_variance16x8                      msa sse2/;
+    specialize qw/aom_sub_pixel_variance128x128                 sse2/;
+    specialize qw/aom_sub_pixel_variance128x64                  sse2/;
+    specialize qw/aom_sub_pixel_variance64x128                  sse2/;
+    specialize qw/aom_sub_pixel_variance64x64          neon msa sse2/;
+    specialize qw/aom_sub_pixel_variance64x32               msa sse2/;
+    specialize qw/aom_sub_pixel_variance32x64               msa sse2/;
+    specialize qw/aom_sub_pixel_variance32x32          neon msa sse2/;
+    specialize qw/aom_sub_pixel_variance32x16               msa sse2/;
+    specialize qw/aom_sub_pixel_variance16x32               msa sse2/;
+    specialize qw/aom_sub_pixel_variance16x16          neon msa sse2/;
+    specialize qw/aom_sub_pixel_variance16x8                msa sse2/;
 } # CONFIG_3WAY_PARTITIONS
   specialize qw/aom_sub_pixel_variance8x16                msa sse2 ssse3/;
   specialize qw/aom_sub_pixel_variance8x8            neon msa sse2 ssse3/;
@@ -1288,7 +1288,7 @@ if (aom_config("CONFIG_3WAY_PARTITIONS") eq "") {
   specialize qw/aom_sub_pixel_avg_variance8x32 sse2 ssse3/;
   specialize qw/aom_sub_pixel_avg_variance64x16 sse2 ssse3 avx2/;
   specialize qw/aom_sub_pixel_variance16x4  ssse3/;
-  specialize qw/aom_sub_pixel_variance32x8   avx2/;
+  specialize qw/aom_sub_pixel_variance32x8  ssse3 avx2/;
   specialize qw/aom_sub_pixel_variance16x64 ssse3/;
   specialize qw/aom_sub_pixel_avg_variance16x4            ssse3/;
   specialize qw/aom_sub_pixel_avg_variance32x8             avx2/;
@@ -1296,7 +1296,7 @@ if (aom_config("CONFIG_3WAY_PARTITIONS") eq "") {
 
   if (aom_config("CONFIG_3WAY_PARTITIONS") eq "") {
     specialize qw/aom_sub_pixel_variance16x4           sse2/;
-    specialize qw/aom_sub_pixel_variance32x8     sse2 ssse3/;
+    specialize qw/aom_sub_pixel_variance32x8           sse2/;
     specialize qw/aom_sub_pixel_variance16x64          sse2/;
     specialize qw/aom_sub_pixel_avg_variance16x4       sse2/;
     specialize qw/aom_sub_pixel_avg_variance32x8 sse2 ssse3/;
