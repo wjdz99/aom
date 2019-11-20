27f5af27f806b45dfc015aec0cf190b09e11aeca
Rohit Singh <siroh@google.com>
2019-11-20 11:16:06 -0800

    Remove NEW_NEAREST and NEAR_NEWEST
    Remove NEW_NEAREST and NEAR_NEWEST.
    WIP to remove NEAREST_NEAREST, and eventually merge NEAREST with NEAR.
    WIP also to merge NEAREST with NEW.
    
    Extend DRL index range by writing/reading the full 3 bits when doing
    write_drl_idx and read_drl_idx.
    
    These changes currently drop PSNR by ~0.8% across the board, but when
    all the symbols are fully merged later things will hopefully get better.
    
    Change-Id: Icd0506e2a834a0630d21b05f16fbc618c4fc85ee

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1537,10 +1537,10 @@ static void update_stats(const AV1_COMMON *const cm, TileDataEnc *tile_data,
 #endif  // CONFIG_FLEX_MVRES
       }
 
+#if !CONFIG_EXTEND_DRL_INDEX
       if (have_nearmv_in_inter_mode(mbmi->mode)) {
         uint8_t ref_frame_type = av1_ref_frame_type(mbmi->ref_frame);
         int idx;
-
         for (idx = 1; idx < 3; ++idx) {
           if (mbmi_ext->ref_mv_count[ref_frame_type] > idx + 1) {
 #if CONFIG_ENTROPY_STATS
@@ -1553,6 +1553,17 @@ static void update_stats(const AV1_COMMON *const cm, TileDataEnc *tile_data,
           }
         }
       }
+#else
+      if (have_nearmv_in_inter_mode(mbmi->mode)) {
+        uint8_t ref_frame_type = av1_ref_frame_type(mbmi->ref_frame);
+        int idx;
+        for (idx = 0; idx < 3; ++idx) {
+          if (mbmi_ext->ref_mv_count[ref_frame_type] > idx + 1) {
+            if (mbmi->ref_mv_idx == idx) break;
+          }
+        }
+      }
+#endif
 
       if (have_newmv_in_inter_mode(mbmi->mode)) {
 #if CONFIG_FLEX_MVRES
@@ -1587,8 +1598,12 @@ static void update_stats(const AV1_COMMON *const cm, TileDataEnc *tile_data,
                                 mbmi->mv_precision);
           }
         } else {
+#if CONFIG_NEW_INTER_MODES
+          const int ref = (mbmi->mode == NEAR_NEWMV);
+#else
           const int ref =
               (mbmi->mode == NEAREST_NEWMV || mbmi->mode == NEAR_NEWMV);
+#endif
           const int_mv ref_mv = av1_get_ref_mv(x, ref);
           av1_update_mv_stats(&mbmi->mv[ref].as_mv, &ref_mv.as_mv, &fc->nmvc,
                               mbmi->mv_precision);
