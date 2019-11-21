66ed8f42ad417d80b30d36105b943577e361ee20
Jerome Jiang <jianj@google.com>
2019-11-21 14:06:38 -0800

    Better sharding for frame drop tests in datarate_test.cc
    Frame drop test doesn't run thru different target bitrates which are set
    in other tests. Instead it runs thru different frame drop thresholds.
    
    Before this, it ran same tests twice on same target bitrates.
    
    Change-Id: I6df4680e0b983c1b3f29846b1c839f5f03f2c172

--- test/datarate_test.cc
+++ test/datarate_test.cc
@@ -137,6 +137,27 @@ class DatarateTestLarge
         << " The datarate for the file missed the target!"
         << cfg_.rc_target_bitrate << " " << effective_datarate_;
   }
+};
+
+// Params: test mode, speed, aq mode, frame drop threshold.
+class DatarateTestFrameDropLarge
+    : public ::libaom_test::CodecTestWith4Params<libaom_test::TestMode, int,
+                                                 unsigned int, int, int>,
+      public DatarateTest {
+ public:
+  DatarateTestFrameDropLarge() : DatarateTest(GET_PARAM(0)) {
+    set_cpu_used_ = GET_PARAM(2);
+    aq_mode_ = GET_PARAM(3);
+  }
+
+ protected:
+  virtual ~DatarateTestFrameDropLarge() {}
+
+  virtual void SetUp() {
+    InitializeConfig();
+    SetMode(GET_PARAM(1));
+    ResetModel();
+  }
 
   virtual void ChangingDropFrameThreshTest() {
     cfg_.rc_buf_initial_sz = 500;
@@ -161,27 +182,25 @@ class DatarateTestLarge
     const int kDropFrameThreshTestStep = 30;
     aom_codec_pts_t last_drop = 140;
     int last_num_drops = 0;
-    for (int i = 40; i < 100; i += kDropFrameThreshTestStep) {
-      cfg_.rc_dropframe_thresh = i;
-      ResetModel();
-      ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
-      ASSERT_GE(effective_datarate_, cfg_.rc_target_bitrate * 0.85)
-          << " The datarate for the file is lower than target by too much!";
-      ASSERT_LE(effective_datarate_, cfg_.rc_target_bitrate * 1.17)
-          << " The datarate for the file is greater than target by too much!";
-      if (last_drop > 0) {
-        ASSERT_LE(first_drop_, last_drop)
-            << " The first dropped frame for drop_thresh " << i
-            << " > first dropped frame for drop_thresh "
-            << i - kDropFrameThreshTestStep;
-      }
-      ASSERT_GE(num_drops_, last_num_drops * 0.7)
-          << " The number of dropped frames for drop_thresh " << i
-          << " < number of dropped frames for drop_thresh "
+    cfg_.rc_dropframe_thresh = GET_PARAM(4);
+    ResetModel();
+    ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+    ASSERT_GE(effective_datarate_, cfg_.rc_target_bitrate * 0.85)
+        << " The datarate for the file is lower than target by too much!";
+    ASSERT_LE(effective_datarate_, cfg_.rc_target_bitrate * 1.17)
+        << " The datarate for the file is greater than target by too much!";
+    if (last_drop > 0) {
+      ASSERT_LE(first_drop_, last_drop)
+          << " The first dropped frame for drop_thresh " << i
+          << " > first dropped frame for drop_thresh "
           << i - kDropFrameThreshTestStep;
-      last_drop = first_drop_;
-      last_num_drops = num_drops_;
     }
+    ASSERT_GE(num_drops_, last_num_drops * 0.7)
+        << " The number of dropped frames for drop_thresh " << i
+        << " < number of dropped frames for drop_thresh "
+        << i - kDropFrameThreshTestStep;
+    last_drop = first_drop_;
+    last_num_drops = num_drops_;
   }
 };
 
@@ -204,7 +223,7 @@ TEST_P(DatarateTestLarge, BasicRateTargeting444CBR) {
 // as the drop frame threshold is increased, and (2) that the total number of
 // frame drops does not decrease as we increase frame drop threshold.
 // Use a lower qp-max to force some frame drops.
-TEST_P(DatarateTestLarge, ChangingDropFrameThresh) {
+TEST_P(DatarateTestFrameDropLarge, ChangingDropFrameThresh) {
   ChangingDropFrameThreshTest();
 }
 
@@ -213,6 +232,8 @@ TEST_P(DatarateTestLarge, BasicRateTargetingAQModeOnOffCBR) {
 }
 
 class DatarateTestRealtime : public DatarateTestLarge {};
+
+class DatarateTestFrameDropRealtime : public DatarateTestFrameDropLarge {};
 
 // Check basic rate targeting for VBR mode.
 TEST_P(DatarateTestRealtime, BasicRateTargetingVBR) {
@@ -233,7 +254,7 @@ TEST_P(DatarateTestRealtime, BasicRateTargeting444CBR) {
 // as the drop frame threshold is increased, and (2) that the total number of
 // frame drops does not decrease as we increase frame drop threshold.
 // Use a lower qp-max to force some frame drops.
-TEST_P(DatarateTestRealtime, ChangingDropFrameThresh) {
+TEST_P(DatarateTestFrameDropRealtime, ChangingDropFrameThresh) {
   ChangingDropFrameThreshTest();
 }
 
@@ -242,10 +263,20 @@ AV1_INSTANTIATE_TEST_CASE(DatarateTestLarge,
                           ::testing::Range(5, 7), ::testing::Values(0, 3),
                           ::testing::Values(0, 1));
 
+AV1_INSTANTIATE_TEST_CASE(DatarateTestFrameDropLarge,
+                          ::testing::Values(::libaom_test::kRealTime),
+                          ::testing::Range(5, 7), ::testing::Values(0, 3),
+                          ::testing::Values(40, 70));
+
 AV1_INSTANTIATE_TEST_CASE(DatarateTestRealtime,
                           ::testing::Values(::libaom_test::kRealTime),
                           ::testing::Range(7, 9), ::testing::Values(0, 3),
                           ::testing::Values(0, 1));
+
+AV1_INSTANTIATE_TEST_CASE(DatarateTestFrameDropRealtime,
+                          ::testing::Values(::libaom_test::kRealTime),
+                          ::testing::Range(7, 9), ::testing::Values(0, 3),
+                          ::testing::Values(40, 70));
 
 }  // namespace
 }  // namespace datarate_test
