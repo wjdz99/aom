c83f735ad2a03c20eb0cd56d30a20fac49fb94f3
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2019-12-04 14:49:09 +0530

    Add is_first_pass() & is_stat_generation_stage()
    is_first_pass() simply checks if pass == 1 and replaces all
    such instances of this condition in the current code base,
    except for those instances of pass == 1 checks where we
    could require other passes to enter in the future, say in
    the event of lookahead processing.
    is_stat_generation_stage() just internally calls is_fist_pass
    for now, however, it could be modified later, leaving
    is_fist_pass() untouched.
    
    Change-Id: Ic0d34c7654840ff8f5dfbaf7153fe90af9ac9dd7

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -1359,6 +1359,15 @@ static INLINE int is_altref_enabled(const AV1_COMP *const cpi) {
   return cpi->oxcf.lag_in_frames >= ALT_MIN_LAG && cpi->oxcf.enable_auto_arf;
 }
 
+// Check if first pass mode
+static INLINE int is_first_pass(const AV1_COMP *const cpi) {
+  return (cpi->oxcf.pass == 1);
+}
+// Check if this context generates statistics
+static INLINE int is_stat_generation_stage(const AV1_COMP *const cpi) {
+  return is_first_pass(cpi);
+}
+
 // TODO(zoeliu): To set up cpi->oxcf.enable_auto_brf
 
 static INLINE void set_ref_ptrs(const AV1_COMMON *cm, MACROBLOCKD *xd,
