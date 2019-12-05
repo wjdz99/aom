606b9cebe9dae19100feba29bf8771765ff563fc
Marco Paniconi <marpan@google.com>
2019-12-05 09:17:34 -0800

    rtc: Fix to golden exit in nonrd pickmode
    When frames_since_golden = 0, set
    skip_ref_find_pred[GOLDEN] to 1 and set
    usable_ref_frame to LAST only if ALT_REF is
    not used. This prevents ALT_REF reference
    from being skipped in this case.
    
    Smalll bdrate gain on low-res speed 7 of ~0.23.
    
    Change-Id: I497cee3704a2ab034edae55c749eb6b4e75d6265

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -1481,7 +1481,8 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
       cpi->sf.use_nonrd_altref_frame ? ALTREF_FRAME : GOLDEN_FRAME;
 
   if (cpi->rc.frames_since_golden == 0 && gf_temporal_ref) {
-    usable_ref_frame = LAST_FRAME;
+    skip_ref_find_pred[GOLDEN_FRAME] = 0;
+    if (!cpi->sf.use_nonrd_altref_frame) usable_ref_frame = LAST_FRAME;
   }
 
   const int mi_row = xd->mi_row;
