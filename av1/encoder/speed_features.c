cc3cb7efa3950554c99b07beb3c880c430c58f5b
Yunqing Wang <yunqingwang@google.com>
2019-12-09 13:16:24 -0800

    Prune warped motion search based on stats
    STATS_CHANGED
    
    Change-Id: Id0ee6c5122acea35417044d934965d221a6d61c9

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -230,6 +230,10 @@ static void set_good_speed_feature_framesize_dependent(
 
     sf->prune_obmc_prob_thresh = 16;
   }
+
+  if (speed >= 5) {
+    sf->prune_warped_prob_thresh = 24;
+  }
 }
 
 static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
@@ -944,6 +948,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
   sf->nonrd_merge_partition = 0;
   sf->disable_interinter_wedge = 0;
   sf->prune_ref_mv_idx_search = 0;
+  sf->prune_warped_prob_thresh = 0;
 
   if (oxcf->mode == GOOD)
     set_good_speed_features_framesize_independent(cpi, sf, speed);
