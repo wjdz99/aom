377c1499f827b60ca680600a9d91375026ad3be5
chiyotsai <chiyotsai@google.com>
2019-12-20 10:49:34 -0800

    Add smart motion vector precision
    Add the ability to adapt current frame's motion vector precision based
    on data from last encoded frame. This is turned on speed 1 and 2.
    
    Performance on speed 1:
     TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   | AVG_SPD | OVR_SPD | STD_SPD
      LOWRES |  -0.078% |  -0.074% | +0.054% | -2.177% | -2.837% |  3.458%
      MIDRES |  -0.096% |  -0.096% | -0.029% | -0.529% | +0.456% |  3.524%
      HDRES  |  -0.110% |  -0.122% | -0.108% | -0.287% | +2.855% |  4.135%
       AWCY  |  -0.110% |  -0.112% | -0.016% |   N/A   |   N/A   |   N/A
    
    Performance on speed 2:
     TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   | AVG_SPD | OVR_SPD | STD_SPD
      LOWRES |  -0.083% |  -0.076% | +0.052% | -2.325% | -1.976% |  3.271%
      MIDRES |  -0.092% |  -0.092% | -0.003% | -0.891% | -0.848% |  2.892%
      HDRES  |  -0.157% |  -0.162% | -0.046% | -0.450% | +0.532% |  2.765%
       AWCY  |  -0.124% |  -0.126% | -0.008% |   N/A   |   N/A   |   N/A
    
    STATS_CHANGED
    
    BUG=aomedia:2534
    
    Change-Id: Icae712f20ec49268b1cf7f5845ba2fc4d844a0f3

--- av1/av1.cmake
+++ av1/av1.cmake
@@ -411,6 +411,7 @@ if(CONFIG_REALTIME_ONLY)
                    "${AOM_ROOT}/av1/encoder/gop_structure.h"
                    "${AOM_ROOT}/av1/encoder/mbgraph.c"
                    "${AOM_ROOT}/av1/encoder/mbgraph.h"
+                   "${AOM_ROOT}/av1/encoder/misc_model_weights.h"
                    "${AOM_ROOT}/av1/encoder/partition_cnn_weights.h"
                    "${AOM_ROOT}/av1/encoder/partition_model_weights.h"
                    "${AOM_ROOT}/av1/encoder/pass2_strategy.c"
