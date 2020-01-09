9b60c7dca1eb513a9c130473c14bb3be14fcdbf5
Rohit Singh <siroh@google.com>
2020-01-08 16:41:27 -0800

    Disable extended NEWMV index and opts
    For two-frame tests, disable the extended NEWMV index and optimizations.
    
    Change-Id: I97d913aa7f640495e21c6a712111cf88bddbd300

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -814,6 +814,9 @@ static void update_drl_index_stats(FRAME_CONTEXT *fc, FRAME_COUNTS *counts,
 #endif  // !CONFIG_ENTROPY_STATS
   uint8_t ref_frame_type = av1_ref_frame_type(mbmi->ref_frame);
   int range = AOMMIN(mbmi_ext->ref_mv_count[ref_frame_type] - 1, 3);
+  if (have_newmv_in_inter_mode(mbmi->mode)) {
+    range = AOMMIN(2, range);
+  }
   for (int idx = 0; idx < range; ++idx) {
     uint8_t drl_ctx = av1_drl_ctx(mode_ctx, mbmi->mode,
                                   mbmi_ext->weight[ref_frame_type], idx);
