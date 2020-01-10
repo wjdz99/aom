a9ba59c162bd0291da8669c4587f8b398a199828
Jingning Han <jingning@google.com>
2020-01-10 00:03:03 +0000

    Revert "Move mv precision decision to a separate file"
    This reverts commit c666b1f3a7672b8fa3a1d01650329834399e1527.
    
    Reason for revert:
    
    There is an encoder/decoder mismatch issue, which biseced to this CL.
    Please revisit. Thanks!
    
    To reproduce:
    
    -o test.av1 UgcVert_360-2b536825739e73d8-66.42.y4m --codec=av1 --good -p 2 --target-bitrate=1800 --test-decode=warn --cpu-used=1 --threads=0 --limit=150 --profile=0 --lag-in-frames=25 --min-q=0 --max-q=63 --auto-alt-ref=1 --passes=2 --kf-max-dist=150 --kf-min-dist=0 --drop-frame=0 --static-thresh=0 --bias-pct=50 --minsection-pct=0 --maxsection-pct=200 --arnr-maxframes=7 --arnr-strength=5 --sharpness=0 --undershoot-pct=100 --overshoot-pct=100 --tile-columns=0 --frame-parallel=0 --psnr --deltaq-mode=1 --end-usage=vbr
    
    STATS_CHANGED
    
    Change-Id: Ifaef31cf855b86659066abf9992e04aa6f9f0082

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -836,11 +836,11 @@ static AOM_INLINE void init_hl_sf(HIGH_LEVEL_SPEED_FEATURES *hl_sf) {
   // best quality defaults
   hl_sf->frame_parameter_update = 1;
   hl_sf->recode_loop = ALLOW_RECODE;
+  hl_sf->reduce_high_precision_mv_usage = 0;
   hl_sf->disable_overlay_frames = 0;
   hl_sf->adaptive_overlay_encoding = 1;
   // Recode loop tolerance %.
   hl_sf->recode_tolerance = 25;
-  hl_sf->high_precision_mv_usage = CURRENT_Q;
   hl_sf->second_alt_ref_filtering = 1;
 }
 
