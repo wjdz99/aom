3423c59195622eba9c083f7c7e59207a7a3b72f9
Rohit Singh <siroh@google.com>
2020-01-13 13:25:33 -0800

    Add size-16 mode dependent transforms
    These transforms were trained on lowres + midres, all frames.
    This includes mode dependent intra transforms as well as a
    non-mode-dependent inter transform.
    
    Change-Id: Ic8a5f3f062de61d1b2e89ae3845f25214aeebf31

--- av1/common/av1_inv_txfm1d.c
+++ av1/common/av1_inv_txfm1d.c
@@ -685,10 +685,10 @@ void av1_imdt8(const int32_t *input, int32_t *output, int8_t cos_bit,
 void av1_imdt16(const int32_t *input, int32_t *output, int8_t cos_bit,
                 const int8_t *side_info) {
   (void)cos_bit;
-  (void)side_info;  // This transform is not mode-dependent
   int32_t s[16] = { 0 };
+  const int32_t *mdt = mdt16_arr(side_info[0]);
   for (int i = 0; i < 16; i++)
-    for (int j = 0; j < 16; j++) s[j] += dst7_16[i * 16 + j] * input[i];
+    for (int j = 0; j < 16; j++) s[j] += mdt[i * 16 + j] * input[i];
 
   for (int i = 0; i < 16; i++) output[i] = round_shift(s[i], 11);
 }
--- av1/encoder/av1_fwd_txfm1d.c
+++ av1/encoder/av1_fwd_txfm1d.c
@@ -702,10 +702,11 @@ void av1_fmdt8(const int32_t *input, int32_t *output, int8_t cos_bit,
 void av1_fmdt16(const int32_t *input, int32_t *output, int8_t cos_bit,
                 const int8_t *side_info) {
   (void)cos_bit;
-  (void)side_info;  // this transform is not mode-dependent
+  (void)side_info;
   int32_t s[16] = { 0 };
+  const int32_t *mdt = mdt16_arr(side_info[0]);
   for (int i = 0; i < 16; i++)
-    for (int j = 0; j < 16; j++) s[j] += dst7_16[j * 16 + i] * input[i];
+    for (int j = 0; j < 16; j++) s[j] += mdt[j * 16 + i] * input[i];
 
   for (int i = 0; i < 16; i++) output[i] = round_shift(s[i], 11);
 }
