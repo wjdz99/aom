06e3a0c1022bf5ebe502aa4a855ecd5bff672c99
Rohit Singh <siroh@google.com>
2020-01-13 18:02:36 -0800

    Better imitate old behavior of early breakout
    This change focuses on the following snippet:
    
    > if (sf->reduce_inter_modes && ref_mv_idx > 0) {
    
    Before NEW_INTER_MODES, this effectively prevented NEARMV 1 from being
    pruned in early_breakout.  For the sake of a fair comparison, this
    change prevents NEARMV 1 (but unfortunately also NEWMV 1) from being
    pruned early.
    
    Change-Id: If355ed1f3ea3586bbc1ec6d4a0a8cd17fb32f2cc

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -11352,7 +11352,7 @@ static bool ref_mv_idx_early_breakout(MACROBLOCK *x,
   const MB_MODE_INFO_EXT *const mbmi_ext = x->mbmi_ext;
   const int8_t ref_frame_type = av1_ref_frame_type(mbmi->ref_frame);
 #if CONFIG_NEW_INTER_MODES
-  if (sf->reduce_inter_modes) {
+  if (sf->reduce_inter_modes && ref_mv_idx > 1) {
     if (mbmi->ref_frame[0] == LAST2_FRAME ||
         mbmi->ref_frame[0] == LAST3_FRAME ||
         mbmi->ref_frame[1] == LAST2_FRAME ||
