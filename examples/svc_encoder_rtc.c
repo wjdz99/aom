51295d2070c1f4190917033544fd77e6805d507b
Marco Paniconi <marpan@google.com>
2020-01-22 21:46:22 -0800

    rtc: Fixes for scaling within stream
    For 1 layer rtc encoding: add scaling_mode
    control to svc_encoder_rtc to test scaling source
    frames within stream.
    
    A few fixes: force golden update on resize_pending
    frame, and modify qp when resolution increases to
    avoid possible big overshoot.
    
    Change-Id: Ic8169251df4e6d89db33f4153614851b554a99bf

--- examples/svc_encoder_rtc.c
+++ examples/svc_encoder_rtc.c
@@ -580,6 +580,9 @@ int main(int argc, char **argv) {
   memset(&input_ctx, 0, sizeof(input_ctx));
   memset(&svc_params, 0, sizeof(svc_params));
 
+  // Flag to test scaling frames within stream, using the scaling_mode control.
+  int test_dynamic_scaling_single_layer = 1;
+
   /* Setup default input stream settings */
   input_ctx.framerate.numerator = 30;
   input_ctx.framerate.denominator = 1;
@@ -793,6 +796,16 @@ int main(int argc, char **argv) {
 
       layer = slx * ts_number_layers + layer_id.temporal_layer_id;
       if (frame_avail && slx == 0) ++rc.layer_input_frames[layer];
+
+      if (test_dynamic_scaling_single_layer) {
+        if (frame_cnt >= 200 && frame_cnt <= 400) {
+         struct aom_scaling_mode mode = { AOME_ONETWO, AOME_ONETWO };
+         aom_codec_control(&codec, AOME_SET_SCALEMODE, &mode);
+        } else {
+          struct aom_scaling_mode mode = { AOME_NORMAL, AOME_NORMAL };
+           aom_codec_control(&codec, AOME_SET_SCALEMODE, &mode);
+        }
+      }
 
       // Do the layer encode.
       aom_usec_timer_start(&timer);
