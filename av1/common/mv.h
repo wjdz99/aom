be439a20872eceb206b3ceb6de419272bf7c0a9b
chiyotsai <chiyotsai@google.com>
2020-01-22 17:35:08 -0800

    Sb level mv precision
    We are seeing a slight loss due to the change in
    dcb9682d428a583f4538d3beb492448e21075ab8.
    
    Change-Id: I106e88ecf4d764c415f16c8288bec96d81f6f46a

--- av1/common/mv.h
+++ av1/common/mv.h
@@ -52,12 +52,12 @@ enum {
 // DISALLOW_ONE_DOWN_FLEX_MVRES 0 => allow all possible down precisions
 // DISALLOW_ONE_DOWN_FLEX_MVRES 1 => allow all possible down precisions except 1
 // DISALLOW_ONE_DOWN_FLEX_MVRES 2 => allow only 0 and 2 down precisions
-#if CONFIG_FLEX_MVRES
+#if CONFIG_FLEX_MVRES && !CONFIG_SB_FLEX_MVRES
 #define DISALLOW_ONE_DOWN_FLEX_MVRES 2  // Choose one of the above
 #define MV_PREC_DOWN_CONTEXTS 2
 #else
 #define DISALLOW_ONE_DOWN_FLEX_MVRES 0
-#endif  // CONFIG_FLEX_MVRES
+#endif  // CONFIG_FLEX_MVRES && !CONFIG_SB_FLEX_MVRES
 
 #if CONFIG_COMPANDED_MV
 #define COMPANDED_INTMV_THRESH_QTR 96
