2b9b8025528f92d556ff3690876a045ee0796b20
chiyotsai <chiyotsai@google.com>
2020-01-22 17:35:08 -0800

    Add the option to use flex_mvres at sb level
    This CL introduces the CONFIG_SB_FLEX_MVRES flag under
    CONFIG_FLEX_MVRES, which moves the mvres decision to the superblock
    level instead of partition block level. CONFIG_SB_FLEX_MVRES is on by
    default when CONFIG_FLEX_MVRES is on, but it can be turned off in
    av1/common/mv.h.
    
    Originally, this experiment had 0.55% BDRate gain on lowres (cf
    cbdd9bd3fb), but the gain has reduced to 0.42% after rebasing.
    
    Change-Id: I106e88ecf4d764c415f16c8288bec96d81f6f46a

--- av1/encoder/encodemv.c
+++ av1/encoder/encodemv.c
@@ -254,15 +254,20 @@ static void build_nmv_component_cost_table(int *mvcost,
 void av1_encode_mv(AV1_COMP *cpi, aom_writer *w, const MV *mv, const MV *ref,
                    nmv_context *mvctx, MvSubpelPrecision precision) {
   MV ref_ = *ref;
+
 #if CONFIG_FLEX_MVRES
   lower_mv_precision(&ref_, precision);
+#endif  // CONFIG_FLEX_MVRES
+
+  const MV diff = { mv->row - ref_.row, mv->col - ref_.col };
+  const MV_JOINT_TYPE j = av1_get_mv_joint(&diff);
+
+#if CONFIG_FLEX_MVRES
   assert((diff.row & ((1 << (MV_SUBPEL_EIGHTH_PRECISION - precision)) - 1)) ==
          0);
   assert((diff.col & ((1 << (MV_SUBPEL_EIGHTH_PRECISION - precision)) - 1)) ==
          0);
 #endif  // CONFIG_FLEX_MVRES
-  const MV diff = { mv->row - ref_.row, mv->col - ref_.col };
-  const MV_JOINT_TYPE j = av1_get_mv_joint(&diff);
 
   aom_write_symbol(w, j, mvctx->joints_cdf, MV_JOINTS);
   if (mv_joint_vertical(j))
