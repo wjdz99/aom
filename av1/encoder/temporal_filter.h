19e59bad58d69fa288315ff598cb5a421095d01a
yjshen <yjshen@google.com>
2020-01-23 13:55:46 -0800

    Refactor `temporal_filter.c` (Step-6).
    WIP
    
    Change-Id: I159ad9ccbbf205fbd15f96e373ed1c59c2060fa0

--- av1/encoder/temporal_filter.h
+++ av1/encoder/temporal_filter.h
@@ -24,31 +24,39 @@ extern "C" {
 #define BW 32
 
 #define NUM_KEY_FRAME_DENOISING 7
-#define EDGE_THRESHOLD 50
-#define SQRT_PI_BY_2 1.25331413732
 
 // Window size for temporal filtering on YUV planes.
 // This is particually used for function `av1_apply_temporal_filter_yuv()`.
-static const int YUV_FILTER_WINDOW_LENGTH = 3;
+#define YUV_FILTER_WINDOW_LENGTH 3
 
 // Window size for temporal filtering on Y planes.
 // This is particually used for function `av1_apply_temporal_filter_yonly()`.
-static const int YONLY_FILTER_WINDOW_LENGTH = 3;
+#define YONLY_FILTER_WINDOW_LENGTH 3
 
-static const int ENABLE_PLANEWISE_STRATEGY = 1;
+#define ENABLE_PLANEWISE_STRATEGY 1
 // Window size for plane-wise temporal filtering.
 // This is particually used for function `av1_apply_temporal_filter_planewise()`
-static const int PLANEWISE_FILTER_WINDOW_LENGTH = 5;
+#define PLANEWISE_FILTER_WINDOW_LENGTH 5
 // A scale factor used in plane-wise temporal filtering to raise the filter
 // weight from `double` with range [0, 1] to `int` with range [0, 1000].
-static const int PLANEWISE_FILTER_WEIGHT_SCALE = 1000;
+#define PLANEWISE_FILTER_WEIGHT_SCALE 1000
+
+#define NOISE_ESTIMATION_EDGE_THRESHOLD 50
+// Estimates noise level from a given frame (ONLY using Y-plane).
+// This is an adaptation of the mehtod in the following paper:
+// Shen-Chuan Tai, Shih-Ming Yang, "A fast method for image noise
+// estimation using Laplacian operator and adaptive edge detection",
+// Proc. 3rd International Symposium on Communications, Control and
+// Signal Processing, 2008, St Julians, Malta.
+// Inputs:
+//   frame: Pointer to the frame to estimate noise level from.
+//   bit_depth: Actual bit-depth of the frame.
+// Returns:
+//   The estimated noise, or -1.0 if there are too few smooth pixels.
+double av1_estimate_noise(const YV12_BUFFER_CONFIG *frame, const int bit_depth);
 
 int av1_temporal_filter(AV1_COMP *cpi, int distance,
                         int *show_existing_alt_ref);
-double estimate_noise(const uint8_t *src, int width, int height, int stride,
-                      int edge_thresh);
-double highbd_estimate_noise(const uint8_t *src8, int width, int height,
-                             int stride, int bd, int edge_thresh);
 
 #ifdef __cplusplus
 }  // extern "C"
