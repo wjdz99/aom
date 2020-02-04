bf14cee67b9d62678f65bdd5e93d0a986dfbdfdc
Yunqing Wang <yunqingwang@google.com>
2020-02-03 16:02:50 -0800

    Reduce mesh motion search range
    Reduced mesh motion search range, which is mostly used in temporal
    filtering.
    
    STATS_CHANGED
    
    Change-Id: I59db8d7a8ea6668431211d998bffb1d9033fb228

--- av1/encoder/speed_features.h
+++ av1/encoder/speed_features.h
@@ -485,6 +485,9 @@ typedef struct MV_SPEED_FEATURES {
 
   // Reduce single motion search range based on MV result of prior ref_mv_idx.
   int reduce_search_range;
+
+  // Limit exhaustive motion search in temporal filtering.
+  int limit_exhaustive_search;
 } MV_SPEED_FEATURES;
 
 typedef struct INTER_MODE_SPEED_FEATURES {
