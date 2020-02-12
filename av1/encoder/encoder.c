d2406e004d9b9d470237513acde9cbb785819d66
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2020-02-12 15:42:23 +0530

    Fix tpl factor for arf when lap_enabled
    Change-Id: I528a43f48b60e21b9c31ff5013f63b352f671368

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -4235,11 +4235,11 @@ static void process_tpl_stats_frame(AV1_COMP *cpi) {
       if (is_frame_arf_and_tpl_eligible(cpi)) {
         cpi->rd.arf_r0 = cpi->rd.r0;
         const int gfu_boost =
-            get_gfu_boost_from_r0(cpi->rd.arf_r0, cpi->rc.frames_to_key);
+            get_gfu_boost_from_r0(cpi->rd.arf_r0, MAX_GF_INTERVAL * 2);
         // printf("old boost %d new boost %d\n", cpi->rc.gfu_boost,
         //        gfu_boost);
         cpi->rc.gfu_boost = combine_prior_with_tpl_boost(
-            cpi->rc.gfu_boost, gfu_boost, cpi->rc.frames_to_key);
+            cpi->rc.gfu_boost, gfu_boost, MAX_GF_INTERVAL * 2);
       } else if (frame_is_intra_only(cm)) {
         // TODO(debargha): Turn off q adjustment for kf temporarily to
         // reduce impact on speed of encoding. Need to investigate how
