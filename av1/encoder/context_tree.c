0df5bb743c309086406b7691e1b2f8729aa654a0
Yue Chen <yuec@google.com>
2020-02-21 10:59:11 -0800

    Copy color index map when reusing pc mode context
    If not, it is causing a color index map holding nonsense indices
    that exceeds the palette size, therefore leading to segment faults
    in EXT_RECUR_PARTITIONS+EXT_PARTITIONS tool,
    
    Change-Id: I705d26d6a9dcbabd553d97de338d38a19ec0d57a

--- av1/encoder/context_tree.c
+++ av1/encoder/context_tree.c
@@ -36,6 +36,10 @@ void av1_copy_tree_context(PICK_MODE_CONTEXT *dst_ctx,
   dst_ctx->rd_mode_is_ready = src_ctx->rd_mode_is_ready;
 
   memcpy(dst_ctx->pred_mv, src_ctx->pred_mv, sizeof(MV) * REF_FRAMES);
+  for (int i = 0; i < 2; ++i) {
+    memcpy(dst_ctx->color_index_map[i], src_ctx->color_index_map[i],
+           sizeof(src_ctx->color_index_map[i][0]) * src_ctx->num_4x4_blk * 16);
+  }
 }
 
 void av1_setup_shared_coeff_buffer(AV1_COMMON *cm,
