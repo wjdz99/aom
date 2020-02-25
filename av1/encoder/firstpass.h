9810d8de0b0be9a460e4d0970c78c9e6545737f9
Jingning Han <jingning@google.com>
2020-02-24 22:06:52 -0800

    Add display frame index tracker in gf_group
    Control the lookahead offset according to the number of frames
    displayed. The coding performance results are mixed:
    
             avg PSNR     overall PSNR    SSIM
    lowres   0.030% 	0.092%        0.025%
    midres  -0.203%	       -0.064%	     -0.211%
    ugc360   0.148%	        0.215%	      0.153%
    hdres   -0.143%	       -0.199%	     -0.198%
    
    STATS_CHANGED
    
    Change-Id: I6b5e15b6961876e62c4e3ec504982cc6e13991c5

--- av1/encoder/firstpass.h
+++ av1/encoder/firstpass.h
@@ -112,6 +112,7 @@ typedef struct {
   unsigned char index;
   FRAME_UPDATE_TYPE update_type[MAX_STATIC_GF_GROUP_LENGTH];
   unsigned char arf_src_offset[MAX_STATIC_GF_GROUP_LENGTH];
+  unsigned char cur_frame_idx[MAX_STATIC_GF_GROUP_LENGTH];
   unsigned char frame_disp_idx[MAX_STATIC_GF_GROUP_LENGTH];
   int ref_frame_disp_idx[MAX_STATIC_GF_GROUP_LENGTH][REF_FRAMES];
   int ref_frame_gop_idx[MAX_STATIC_GF_GROUP_LENGTH][REF_FRAMES];
