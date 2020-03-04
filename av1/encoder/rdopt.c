wjdz99/aom

commit 0622d2c0c46ef9419165c6da22538ac08cdbda0f

Add variance based full pixel search
Change-Id: I91b871b5298b110a02853a3f6c210c4f7b857517

av1/encoder/rdopt.c
@@ -7853,7 +7853,8 @@ static void setup_buffer_ref_mvs_inter(
 
 static void single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
                                  BLOCK_SIZE bsize, int mi_row, int mi_col,
-                                 int ref_idx, int *rate_mv) {
+                                 int ref_idx, int *rate_mv,
+                                 bool use_full_pixel_var) {
   MACROBLOCKD *xd = &x->e_mbd;
   const AV1_COMMON *cm = &cpi->common;
   const int num_planes = av1_num_planes(cm);
@@ -7947,21 +7948,27 @@ static void single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
   mvp_full.row >>= 3;
 
   x->best_mv.as_int = x->second_best_mv.as_int = INVALID_MV;
-
-  switch (mbmi->motion_mode) {
-    case SIMPLE_TRANSLATION:
-      bestsme = av1_full_pixel_search(
-          cpi, x, bsize, &mvp_full, step_param, cpi->sf.mv.search_method, 0,
-          sadpb, cond_cost_list(cpi, cost_list), &ref_mv, INT_MAX, 1,
-          (MI_SIZE * mi_col), (MI_SIZE * mi_row), 0, &cpi->ss_cfg[SS_CFG_SRC]);
-      break;
-    case OBMC_CAUSAL:
-      bestsme = av1_obmc_full_pixel_search(
-          cpi, x, &mvp_full, step_param, sadpb,
-          MAX_MVSEARCH_STEPS - 1 - step_param, 1, &cpi->fn_ptr[bsize], &ref_mv,
-          &(x->best_mv.as_mv), 0, &cpi->ss_cfg[SS_CFG_SRC]);
-      break;
-    default: assert(0 && "Invalid motion mode!\n");
+  if (use_full_pixel_var) {
+    bestsme = av1_full_pixel_search_var(cpi, x, bsize, &mvp_full, step_param,
+                                        cond_cost_list(cpi, cost_list), &ref_mv,
+                                        &cpi->ss_cfg[SS_CFG_SRC]);
+  } else {
+    switch (mbmi->motion_mode) {
+      case SIMPLE_TRANSLATION:
+        bestsme = av1_full_pixel_search(
+            cpi, x, bsize, &mvp_full, step_param, cpi->sf.mv.search_method, 0,
+            sadpb, cond_cost_list(cpi, cost_list), &ref_mv, INT_MAX, 1,
+            (MI_SIZE * mi_col), (MI_SIZE * mi_row), 0,
+            &cpi->ss_cfg[SS_CFG_SRC]);
+        break;
+      case OBMC_CAUSAL:
+        bestsme = av1_obmc_full_pixel_search(
+            cpi, x, &mvp_full, step_param, sadpb,
+            MAX_MVSEARCH_STEPS - 1 - step_param, 1, &cpi->fn_ptr[bsize],
+            &ref_mv, &(x->best_mv.as_mv), 0, &cpi->ss_cfg[SS_CFG_SRC]);
+        break;
+      default: assert(0 && "Invalid motion mode!\n");
+    }
   }
 
   if (scaled_ref_frame) {
@@ -9161,7 +9168,7 @@ static int64_t handle_newmv(const AV1_COMP *const cpi, MACROBLOCK *const x,
       }
     }
   } else {
-    single_motion_search(cpi, x, bsize, mi_row, mi_col, 0, rate_mv);
+    single_motion_search(cpi, x, bsize, mi_row, mi_col, 0, rate_mv, false);
     if (x->best_mv.as_int == INVALID_MV) return INT64_MAX;
 
     args->single_newmv[ref_mv_idx][refs[0]] = x->best_mv;
@@ -10147,9 +10154,24 @@ static int handle_inter_intra_mode(const AV1_COMP *const cpi,
       !cpi->sf.disable_smooth_interintra) {
     mbmi->use_wedge_interintra = 0;
     int j = 0;
+#if CONFIG_ILLUM_MCOMP
+    MV old_best = x->best_mv.as_mv;
+#endif  // CONFIG_ILLUM_MCOMP
     if (cpi->sf.reuse_inter_intra_mode == 0 ||
         best_interintra_mode == INTERINTRA_MODES) {
-      for (j = 0; j < total_modes; ++j) {
+      for (j = 0; j < INTERINTRA_MODES; ++j) {
+#if CONFIG_ILLUM_MCOMP
+        if (j == II_ILLUM_MCOMP_PRED) {
+          single_motion_search(cpi, x, bsize, mi_row, mi_col, 0, &tmp_rate_mv,
+                               true);
+          av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                        AOM_PLANE_Y, AOM_PLANE_Y);
+        } else if (j == II_ILLUM_MCOMP_PRED + 1) {
+          x->best_mv.as_mv = old_best;
+          av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                        AOM_PLANE_Y, AOM_PLANE_Y);
+        }
+#endif  // CONFIG_ILLUM_MCOMP
         if ((!cpi->oxcf.enable_smooth_intra || cpi->sf.disable_smooth_intra) &&
             (INTERINTRA_MODE)j == II_SMOOTH_PRED)
           continue;
@@ -10187,6 +10209,13 @@ static int handle_inter_intra_mode(const AV1_COMP *const cpi,
       }
       args->inter_intra_mode[mbmi->ref_frame[0]] = best_interintra_mode;
     }
+#if CONFIG_ILLUM_MCOMP
+    if (best_interintra_mode != II_ILLUM_MCOMP_PRED) {
+      x->best_mv.as_mv = old_best;
+      av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                    AOM_PLANE_Y, AOM_PLANE_Y);
+    }
+#endif  // CONFIG_ILLUM_MCOMP
     assert(IMPLIES(!cpi->oxcf.enable_smooth_interintra ||
                        cpi->sf.disable_smooth_interintra,
                    best_interintra_mode != II_SMOOTH_PRED));
@@ -10301,7 +10330,22 @@ static int handle_inter_intra_mode(const AV1_COMP *const cpi,
               pick_interintra_wedge(cpi, x, bsize, intrapred_, tmp_buf_);
 
           int j = 0;
-          for (j = 0; j < total_modes; ++j) {
+#if CONFIG_ILLUM_MCOMP
+          MV old_best = x->best_mv.as_mv;
+#endif  // CONFIG_ILLUM_MCOMP
+          for (j = 0; j < INTERINTRA_MODES; ++j) {
+#if CONFIG_ILLUM_MCOMP
+            if (j == II_ILLUM_MCOMP_PRED) {
+              single_motion_search(cpi, x, bsize, mi_row, mi_col, 0,
+                                   &tmp_rate_mv, true);
+              av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                            AOM_PLANE_Y, AOM_PLANE_Y);
+            } else if (j == II_ILLUM_MCOMP_PRED + 1) {
+              x->best_mv.as_mv = old_best;
+              av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                            AOM_PLANE_Y, AOM_PLANE_Y);
+            }
+#endif  // CONFIG_ILLUM_MCOMP
             mbmi->interintra_mode = (INTERINTRA_MODE)j;
 #if CONFIG_DERIVED_INTRA_MODE
             mbmi->use_derived_intra_mode[0] = 0;
@@ -10334,6 +10378,13 @@ static int handle_inter_intra_mode(const AV1_COMP *const cpi,
 #endif  // CONFIG_DERIVED_INTRA_MODE
             }
           }
+#if CONFIG_ILLUM_MCOMP
+          if (best_interintra_mode != II_ILLUM_MCOMP_PRED) {
+            x->best_mv.as_mv = old_best;
+            av1_enc_build_inter_predictor(cm, xd, mi_row, mi_col, NULL, bsize,
+                                          AOM_PLANE_Y, AOM_PLANE_Y);
+          }
+#endif  // CONFIG_ILLUM_MCOMP
           args->inter_intra_mode[mbmi->ref_frame[0]] = best_interintra_mode;
           mbmi->interintra_mode = best_interintra_mode;
 #if CONFIG_DERIVED_INTRA_MODE
@@ -10637,7 +10688,8 @@ static int64_t motion_mode_rd(
       const uint32_t cur_mv = mbmi->mv[0].as_int;
       assert(!is_comp_pred);
       if (have_newmv_in_inter_mode(this_mode)) {
-        single_motion_search(cpi, x, bsize, mi_row, mi_col, 0, &tmp_rate_mv);
+        single_motion_search(cpi, x, bsize, mi_row, mi_col, 0, &tmp_rate_mv,
+                             false);
         mbmi->mv[0].as_int = x->best_mv.as_int;
 #if USE_DISCOUNT_NEWMV_TEST
         if (discount_newmv_test(cpi, x, this_mode, mbmi->mv[0])) {

av1/encoder/mcomp.c
@@ -3681,3 +3681,309 @@ void av1_simple_motion_sse_var(AV1_COMP *cpi, MACROBLOCK *x, int mi_row,
 
   *var = cpi->fn_ptr[bsize].vf(src, src_stride, dst, dst_stride, sse);
 }
+
+// Exhuastive motion search around a given centre position with a given
+// step size.
+static int exhaustive_mesh_search_var(const AV1_COMMON *cm, MACROBLOCK *x,
+                                      MV *ref_mv, MV *best_mv, int range,
+                                      int step,
+                                      const aom_variance_fn_ptr_t *fn_ptr,
+                                      const MV *center_mv) {
+  MV fcenter_mv = { center_mv->row, center_mv->col };
+  int start_col, end_col, start_row, end_row;
+  assert(step >= 1);
+
+  clamp_mv(&fcenter_mv, x->mv_limits.col_min, x->mv_limits.col_max,
+           x->mv_limits.row_min, x->mv_limits.row_max);
+  *best_mv = fcenter_mv;
+  unsigned int best_var =
+      av1_get_mvpred_var(cm, x, &fcenter_mv, ref_mv, fn_ptr, 1);
+  start_row = AOMMAX(-range, x->mv_limits.row_min - fcenter_mv.row);
+  start_col = AOMMAX(-range, x->mv_limits.col_min - fcenter_mv.col);
+  end_row = AOMMIN(range, x->mv_limits.row_max - fcenter_mv.row);
+  end_col = AOMMIN(range, x->mv_limits.col_max - fcenter_mv.col);
+
+  for (int r = start_row; r <= end_row; r += step) {
+    for (int c = start_col; c <= end_col; c += step) {
+      const MV mv = { fcenter_mv.row + r, fcenter_mv.col + c };
+      unsigned int var = av1_get_mvpred_var(cm, x, &mv, ref_mv, fn_ptr, 1);
+      if (var < best_var) {
+        best_var = var;
+        x->second_best_mv.as_mv = *best_mv;
+        *best_mv = mv;
+      }
+    }
+  }
+  return best_var;
+}
+
+static int diamond_search_var(const AV1_COMMON *cm, MACROBLOCK *x,
+                              const search_site_config *cfg, MV *ref_mv,
+                              MV *best_mv, int search_param, int *num00,
+                              const aom_variance_fn_ptr_t *fn_ptr,
+                              const MV *center_mv) {
+  int i, j, step;
+  const MACROBLOCKD *const xd = &x->e_mbd;
+  const uint8_t *in_what;
+  const int in_what_stride = xd->plane[0].pre[0].stride;
+  const uint8_t *best_address;
+
+  unsigned int bestvar = INT_MAX;
+  int best_site = 0;
+  int last_site = 0;
+
+  int ref_row;
+  int ref_col;
+
+  // search_param determines the length of the initial step and hence the number
+  // of iterations.
+  // 0 = initial step (MAX_FIRST_STEP) pel
+  // 1 = (MAX_FIRST_STEP/2) pel,
+  // 2 = (MAX_FIRST_STEP/4) pel...
+  const search_site *ss = &cfg->ss[search_param * cfg->searches_per_step];
+  const int tot_steps = (cfg->ss_count / cfg->searches_per_step) - search_param;
+
+  const MV fcenter_mv = { center_mv->row >> 3, center_mv->col >> 3 };
+  clamp_mv(ref_mv, x->mv_limits.col_min, x->mv_limits.col_max,
+           x->mv_limits.row_min, x->mv_limits.row_max);
+  ref_row = ref_mv->row;
+  ref_col = ref_mv->col;
+  *num00 = 0;
+  best_mv->row = ref_row;
+  best_mv->col = ref_col;
+
+  // Work out the start point for the search
+  in_what = xd->plane[0].pre[0].buf + ref_row * in_what_stride + ref_col;
+  best_address = in_what;
+
+  // Check the starting position
+  bestvar = av1_get_mvpred_var(cm, x, best_mv, &fcenter_mv, fn_ptr, 1);
+  i = 1;
+
+  for (step = 0; step < tot_steps; step++) {
+    for (j = 0; j < cfg->searches_per_step; j++) {
+      // Trap illegal vectors
+      const MV this_mv = { best_mv->row + ss[i].mv.row,
+                           best_mv->col + ss[i].mv.col };
+
+      if (is_mv_in(&x->mv_limits, &this_mv)) {
+        unsigned int thisvar =
+            av1_get_mvpred_var(cm, x, &this_mv, &fcenter_mv, fn_ptr, 1);
+        if (thisvar < bestvar) {
+          bestvar = thisvar;
+          best_site = i;
+        }
+      }
+      i++;
+    }
+
+    if (best_site != last_site) {
+      x->second_best_mv.as_mv = *best_mv;
+      best_mv->row += ss[best_site].mv.row;
+      best_mv->col += ss[best_site].mv.col;
+      best_address += ss[best_site].offset;
+      last_site = best_site;
+    } else if (best_address == in_what) {
+      (*num00)++;
+    }
+  }
+  return bestvar;
+}
+
+static int refining_search_var(const AV1_COMMON *cm, MACROBLOCK *x, MV *ref_mv,
+                               int search_range,
+                               const aom_variance_fn_ptr_t *fn_ptr,
+                               const MV *center_mv) {
+  const MV neighbors[4] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
+  const MV fcenter_mv = { center_mv->row >> 3, center_mv->col >> 3 };
+  unsigned int bestvar =
+      av1_get_mvpred_var(cm, x, center_mv, ref_mv, fn_ptr, 1);
+  int i, j;
+
+  for (i = 0; i < search_range; i++) {
+    int best_site = -1;
+    for (j = 0; j < 4; ++j) {
+      const MV mv = { ref_mv->row + neighbors[j].row,
+                      ref_mv->col + neighbors[j].col };
+
+      if (is_mv_in(&x->mv_limits, &mv)) {
+        unsigned int var =
+            av1_get_mvpred_var(cm, x, &mv, &fcenter_mv, fn_ptr, 1);
+        if (var < bestvar) {
+          bestvar = var;
+          best_site = j;
+        }
+      }
+    }
+
+    if (best_site == -1) {
+      break;
+    } else {
+      x->second_best_mv.as_mv = *ref_mv;
+      ref_mv->row += neighbors[best_site].row;
+      ref_mv->col += neighbors[best_site].col;
+    }
+  }
+
+  return bestvar;
+}
+
+static int full_pixel_diamond_var(const AV1_COMP *const cpi, MACROBLOCK *x,
+                                  MV *mvp_full, int step_param,
+                                  int further_steps, int do_refine,
+                                  int *cost_list,
+                                  const aom_variance_fn_ptr_t *fn_ptr,
+                                  const MV *ref_mv,
+                                  const search_site_config *cfg) {
+  MV temp_mv;
+  int thissme, n, num00 = 0;
+  int bestsme = diamond_search_var(&cpi->common, x, cfg, mvp_full, &temp_mv,
+                                   step_param, &n, fn_ptr, ref_mv);
+  if (bestsme < INT_MAX)
+    bestsme = av1_get_mvpred_var(&cpi->common, x, &temp_mv, ref_mv, fn_ptr, 1);
+  x->best_mv.as_mv = temp_mv;
+
+  // If there won't be more n-step search, check to see if refining search is
+  // needed.
+  if (n > further_steps) do_refine = 0;
+
+  while (n < further_steps) {
+    ++n;
+
+    if (num00) {
+      num00--;
+    } else {
+      thissme = diamond_search_var(&cpi->common, x, cfg, mvp_full, &temp_mv,
+                                   step_param + n, &num00, fn_ptr, ref_mv);
+      if (thissme < INT_MAX)
+        thissme =
+            av1_get_mvpred_var(&cpi->common, x, &temp_mv, ref_mv, fn_ptr, 1);
+
+      // check to see if refining search is needed.
+      if (num00 > further_steps - n) do_refine = 0;
+
+      if (thissme < bestsme) {
+        bestsme = thissme;
+        x->best_mv.as_mv = temp_mv;
+      }
+    }
+  }
+
+  // final 1-away diamond refining search
+  if (do_refine) {
+    const int search_range = 8;
+    MV best_mv = x->best_mv.as_mv;
+    thissme = refining_search_var(&cpi->common, x, &best_mv, search_range,
+                                  fn_ptr, ref_mv);
+    if (thissme < INT_MAX)
+      thissme =
+          av1_get_mvpred_var(&cpi->common, x, &best_mv, ref_mv, fn_ptr, 1);
+    if (thissme < bestsme) {
+      bestsme = thissme;
+      x->best_mv.as_mv = best_mv;
+    }
+  }
+
+  // Return cost list.
+  if (cost_list) {
+    calc_int_cost_list(&cpi->common, x, ref_mv, x->errorperbit, fn_ptr,
+                       &x->best_mv.as_mv, cost_list);
+  }
+  return bestsme;
+}
+
+static int full_pixel_exhaustive_var(const AV1_COMP *const cpi, MACROBLOCK *x,
+                                     const MV *centre_mv_full, int *cost_list,
+                                     const aom_variance_fn_ptr_t *fn_ptr,
+                                     const MV *ref_mv, MV *dst_mv) {
+  MV temp_mv = { centre_mv_full->row, centre_mv_full->col };
+  MV f_ref_mv = { ref_mv->row >> 3, ref_mv->col >> 3 };
+  int bestsme;
+  int i;
+  int interval = cpi->sf.mesh_patterns[0].interval;
+  int range = cpi->sf.mesh_patterns[0].range;
+  int baseline_interval_divisor;
+
+  // Trap illegal values for interval and range for this function.
+  if ((range < MIN_RANGE) || (range > MAX_RANGE) || (interval < MIN_INTERVAL) ||
+      (interval > range))
+    return INT_MAX;
+
+  baseline_interval_divisor = range / interval;
+
+  // Check size of proposed first range against magnitude of the centre
+  // value used as a starting point.
+  range = AOMMAX(range, (5 * AOMMAX(abs(temp_mv.row), abs(temp_mv.col))) / 4);
+  range = AOMMIN(range, MAX_RANGE);
+  interval = AOMMAX(interval, range / baseline_interval_divisor);
+
+  // initial search
+  bestsme = exhaustive_mesh_search_var(&cpi->common, x, &f_ref_mv, &temp_mv,
+                                       range, interval, fn_ptr, &temp_mv);
+
+  if ((interval > MIN_INTERVAL) && (range > MIN_RANGE)) {
+    // Progressive searches with range and step size decreasing each time
+    // till we reach a step size of 1. Then break out.
+    for (i = 1; i < MAX_MESH_STEP; ++i) {
+      // First pass with coarser step and longer range
+      bestsme = exhaustive_mesh_search_var(
+          &cpi->common, x, &f_ref_mv, &temp_mv, cpi->sf.mesh_patterns[i].range,
+          cpi->sf.mesh_patterns[i].interval, fn_ptr, &temp_mv);
+
+      if (cpi->sf.mesh_patterns[i].interval == 1) break;
+    }
+  }
+
+  if (bestsme < INT_MAX)
+    bestsme = av1_get_mvpred_var(&cpi->common, x, &temp_mv, ref_mv, fn_ptr, 1);
+  *dst_mv = temp_mv;
+
+  // Return cost list.
+  if (cost_list) {
+    calc_int_cost_list(
+        // TODO(elliottk): check that errrorperbit is correct here
+        &cpi->common, x, ref_mv, x->errorperbit, fn_ptr, dst_mv, cost_list);
+  }
+  return bestsme;
+}
+
+int av1_full_pixel_search_var(const AV1_COMP *cpi, MACROBLOCK *x,
+                              BLOCK_SIZE bsize, MV *mvp_full, int step_param,
+                              int *cost_list, const MV *ref_mv,
+                              const search_site_config *cfg) {
+  if (cost_list) {
+    cost_list[0] = INT_MAX;
+    cost_list[1] = INT_MAX;
+    cost_list[2] = INT_MAX;
+    cost_list[3] = INT_MAX;
+    cost_list[4] = INT_MAX;
+  }
+
+  const SPEED_FEATURES *const sf = &cpi->sf;
+  const aom_variance_fn_ptr_t *fn_ptr = &cpi->fn_ptr[bsize];
+  int var = full_pixel_diamond_var(cpi, x, mvp_full, step_param,
+                                   MAX_MVSEARCH_STEPS - 1 - step_param, 1,
+                                   cost_list, fn_ptr, ref_mv, cfg);
+
+  // Should we allow a follow on exhaustive search?
+  bool run_exhaustive_search = false;
+  if (is_exhaustive_allowed(cpi, x)) {
+    int exhuastive_thr = sf->exhaustive_searches_thresh;
+    exhuastive_thr >>=
+        10 - (mi_size_wide_log2[bsize] + mi_size_high_log2[bsize]);
+
+    // Threshold variance for an exhaustive full search.
+    run_exhaustive_search = var > exhuastive_thr;
+  }
+  if (run_exhaustive_search) {
+    int var_ex;
+    MV tmp_mv_ex;
+    var_ex = full_pixel_exhaustive_var(cpi, x, &x->best_mv.as_mv, cost_list,
+                                       fn_ptr, ref_mv, &tmp_mv_ex);
+    if (var_ex < var) {
+      var = var_ex;
+      x->best_mv.as_mv = tmp_mv_ex;
+    }
+  }
+  return var;
+}

av1/encoder/mcomp.h
@@ -175,6 +175,11 @@ int av1_full_pixel_search(const struct AV1_COMP *cpi, MACROBLOCK *x,
                           int x_pos, int y_pos, int intra,
                           const search_site_config *cfg);
 
+int av1_full_pixel_search_var(const struct AV1_COMP *cpi, MACROBLOCK *x,
+                              BLOCK_SIZE bsize, MV *mvp_full, int step_param,
+                              int *cost_list, const MV *ref_mv,
+                              const search_site_config *cfg);
+
 int av1_obmc_full_pixel_search(const struct AV1_COMP *cpi, MACROBLOCK *x,
                                MV *mvp_full, int step_param, int sadpb,
                                int further_steps, int do_refine,

commit b6e7bbefc295457c809bf6b7da152c7d4a2b5ea6

derive_intra_mode: fix interintra RDO
Before this patch, the derived intra mode was never searched during smooth
interintra RDO (broken by 0622d2c).

Change-Id: Ibee59bb84fef82b27048370b910564d6b2e81d5b

av1/encoder/rdopt.c
@@ -10404,7 +10404,7 @@ static int handle_inter_intra_mode(const AV1_COMP *const cpi,
 #endif  // CONFIG_ILLUM_MCOMP
     if (cpi->sf.reuse_inter_intra_mode == 0 ||
         best_interintra_mode == INTERINTRA_MODES) {
-      for (j = 0; j < INTERINTRA_MODES; ++j) {
+      for (j = 0; j < total_modes; ++j) {
 #if CONFIG_ILLUM_MCOMP
         if (j == II_ILLUM_MCOMP_PRED) {
           single_motion_search(cpi, x, bsize, 0, &tmp_rate_mv, true);