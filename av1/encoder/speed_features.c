0c0bb8be2adbfc80066bef700a84cd17fbcd4bb8
Marco Paniconi <marpan@google.com>
2020-03-19 11:05:51 -0700

    rtc: Enable use_modeled_rd_cost flag for speed 8
    Also enable source_metrics_sb for speed 8.
    Keep the conditions for using modeled_rd_cost at
    block level conservative for now. And only for low-resolns.
    ~2% bdrate loss on rtc_derf. ~3% speedup on some qvga clips.
    
    Change-Id: Ia673c928082c6c0a66cfb1dc120af113ce434ab3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -252,7 +252,11 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
   (void)is_720p_or_larger;  // Not used so far
 
   if (!is_360p_or_larger) {
-    if (speed >= 8) sf->rt_sf.use_nonrd_filter_search = 0;
+    if (speed >= 8) {
+      sf->rt_sf.use_nonrd_filter_search = 0;
+      sf->rt_sf.use_modeled_non_rd_cost = 1;
+      sf->rt_sf.source_metrics_sb_nonrd = 1;
+    }
   }
   if (is_360p_or_larger) {
     if (speed >= 7) {
