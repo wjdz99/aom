fac698d055b59976da6d1294fe3f6c2e02929239
Debargha Mukherjee <debargha@google.com>
2020-03-25 01:01:33 -0700

    Rwork perform_coeff_opt speed feature
    Reworks speed feature to skip coefficient
    optimization based on mse/qstep^2 rather than
    mse alone.
    
    Seems to provide 5-15% speed-up for complex videos
    at speeds 1 and 2.
    BDRATE (hdres2, 17 frames, q mode): shows a slight improvement.
    
    The thresholds need to be adjusted somewhat for optimal
    performance.
    
    STATS_CHANGED
    
    Change-Id: I67133f980b6201d42546339b45ff4910541ebab0

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -72,10 +72,10 @@ static unsigned int tx_domain_dist_types[3][MODE_EVAL_TYPES] = { { 0, 2, 0 },
 // feature is ON
 static unsigned int coeff_opt_dist_thresholds[5][MODE_EVAL_TYPES] = {
   { UINT_MAX, UINT_MAX, UINT_MAX },
-  { 442413, 36314, UINT_MAX },
-  { 162754, 36314, UINT_MAX },
-  { 22026, 22026, UINT_MAX },
-  { 22026, 22026, UINT_MAX }
+  { 1728, 142, UINT_MAX },
+  { 864, 142, UINT_MAX },
+  { 432, 86, UINT_MAX },
+  { 216, 86, UINT_MAX }
 };
 
 // Transform size to be used for default, mode and winner mode evaluation
