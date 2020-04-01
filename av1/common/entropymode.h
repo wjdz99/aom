a7f40519e76236dabed4aa59da1c1bff0cc53f36
chiyotsai <chiyotsai@google.com>
2020-04-01 14:51:10 -0700

    Consolidate FLEX_MVRES and SB_FLEX_MVRES experiments
    This CL combines both FLEX_MVRES and SB_FLEX_MVRES into a single
    experiment under FLEX_MVRES flag. By default, no mvres adaptation is
    used. To turn on sb level resolution, set CONFIG_SB_FLEX_MVRES to 1.
    To turn on pb level resolution, set CONFIG_PB_FLEX_MVRES to 1.
    
    If only pb level mvres is turned on, the new syntax has a 0.016% bitrate
    increase compared to the old scheme. No stats changed is observed for sb
    level mvres.
    
    Change-Id: Iaaacfc7bd582fcef5514d246dc356682044e09d1

--- av1/common/entropymode.h
+++ av1/common/entropymode.h
@@ -263,27 +263,21 @@ typedef struct frame_contexts {
                           [CDF_SIZE(UV_INTRA_MODES)];
 #endif  // CONFIG_INTRA_ENTROPY
 
-#if CONFIG_SB_FLEX_MVRES
+#if CONFIG_FLEX_MVRES
   aom_cdf_prob
       sb_mv_precision_cdf[MV_SUBPEL_PRECISIONS - MV_SUBPEL_HALF_PRECISION]
                          [CDF_SIZE(MV_SUBPEL_PRECISIONS)];
-#elif CONFIG_FLEX_MVRES
 #if DISALLOW_ONE_DOWN_FLEX_MVRES == 2
-  aom_cdf_prob pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS]
-                                  [MV_SUBPEL_PRECISIONS -
-                                   MV_SUBPEL_QTR_PRECISION][CDF_SIZE(2)];
+  aom_cdf_prob pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS][FLEX_MV_COSTS_SIZE]
+                                  [CDF_SIZE(2)];
 #elif DISALLOW_ONE_DOWN_FLEX_MVRES == 1
-  aom_cdf_prob
-      pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS]
-                         [MV_SUBPEL_PRECISIONS - MV_SUBPEL_QTR_PRECISION]
-                         [CDF_SIZE(MV_SUBPEL_PRECISIONS - 1)];
+  aom_cdf_prob pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS][FLEX_MV_COSTS_SIZE]
+                                  [CDF_SIZE(MV_SUBPEL_PRECISIONS - 1)];
 #else
-  aom_cdf_prob
-      pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS]
-                         [MV_SUBPEL_PRECISIONS - MV_SUBPEL_QTR_PRECISION]
-                         [CDF_SIZE(MV_SUBPEL_PRECISIONS)];
+  aom_cdf_prob pb_mv_precision_cdf[MV_PREC_DOWN_CONTEXTS][FLEX_MV_COSTS_SIZE]
+                                  [CDF_SIZE(MV_SUBPEL_PRECISIONS)];
 #endif  // DISALLOW_ONE_DOWN_FLEX_MVRES
-#endif  // CONFIG_SB_FLEX_MVRES
+#endif  // CONFIG_FLEX_MVRES
 
   aom_cdf_prob angle_delta_cdf[DIRECTIONAL_MODES]
                               [CDF_SIZE(2 * MAX_ANGLE_DELTA + 1)];
