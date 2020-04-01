9722478482e6c6e108253e344e87b17fc224bca0
venkat sanampudi <sanampudi.venkatarao@ittiam.com>
2020-04-01 12:30:49 +0530

    Extend ext_partition for all frm with 80, 100
    STATS_CHANGED:
    
    Change-Id: Ia08674126f2dd372cc6995001c5ccb39a84c5661

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -571,8 +571,6 @@ static void set_good_speed_features_framesize_independent(
 
   if (speed >= 5) {
     sf->part_sf.simple_motion_search_prune_agg = 3;
-    sf->part_sf.ext_partition_eval_thresh =
-        allow_screen_content_tools ? BLOCK_8X8 : BLOCK_16X16;
 
     sf->inter_sf.prune_inter_modes_based_on_tpl = boosted ? 0 : 3;
     sf->inter_sf.disable_interinter_wedge = 1;
@@ -1275,6 +1273,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
 void av1_set_speed_features_qindex_dependent(AV1_COMP *cpi, int speed) {
   AV1_COMMON *const cm = &cpi->common;
   SPEED_FEATURES *const sf = &cpi->sf;
+  const int boosted = frame_is_boosted(cpi);
   const int is_720p_or_larger = AOMMIN(cm->width, cm->height) >= 720;
   if (is_720p_or_larger && cpi->oxcf.mode == GOOD && speed == 0) {
     if (cm->quant_params.base_qindex <= 80) {
@@ -1288,5 +1287,42 @@ void av1_set_speed_features_qindex_dependent(AV1_COMP *cpi, int speed) {
       sf->tx_sf.inter_tx_size_search_init_depth_sqr = 1;
       sf->tx_sf.intra_tx_size_search_init_depth_rect = 1;
     }
+  }
+
+  /* disable extended partitions for lower qindex */
+  if (speed >= 3 && cm->quant_params.base_qindex <= 80) {
+    sf->part_sf.ext_partition_eval_thresh =
+        (cm->features.allow_screen_content_tools || boosted) ? BLOCK_8X8
+                                                             : BLOCK_128X128;
+  }
+
+  // Extended partitions greater than BLOCK_8X8 will be disabled as follows:
+  // boosted frames: Qindex <= 80
+  // non-boosted frames: Qindex <= 120
+  // for screen contents partitions will not be skipped.
+  if (speed >= 4) {
+    if (cm->quant_params.base_qindex <= 80) {
+      sf->part_sf.ext_partition_eval_thresh =
+          (cm->features.allow_screen_content_tools) ? BLOCK_8X8 : BLOCK_128X128;
+    } else if (cm->quant_params.base_qindex <= 120) {
+      sf->part_sf.ext_partition_eval_thresh =
+          (cm->features.allow_screen_content_tools || boosted) ? BLOCK_8X8
+                                                               : BLOCK_128X128;
+    }
+  }
+
+  // Extended partitions greater than BLOCK_16X16 will be disabled as follows:
+  // boosted frames: Qindex <= 80
+  // non-boosted frames: Qindex <= 120
+  // for screen contents partitions will not be skipped.
+  if (speed >= 5) {
+    int part_eval_thresh = BLOCK_16X16;
+    if (cm->quant_params.base_qindex <= 80) {
+      part_eval_thresh = BLOCK_128X128;
+    } else if (cm->quant_params.base_qindex <= 120) {
+      part_eval_thresh = boosted ? BLOCK_16X16 : BLOCK_128X128;
+    }
+    sf->part_sf.ext_partition_eval_thresh =
+        cm->features.allow_screen_content_tools ? BLOCK_8X8 : part_eval_thresh;
   }
 }
