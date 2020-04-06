2998ad7c28e19976bb3c007e383cd8df6df042b5
Cherma Rajan A <cherma.rajan@ittiam.com>
2020-04-06 13:25:12 +0530

    Prune comp mode using mode type and ref frame of neighbor block
    Introduced a speed feature to skip compound mode using reference
    frame of above and left neighbor blocks. Pruning is applicable
    for extended compound modes only.
    
    This speed feature is enabled for speed 5.
    
                Encode Time           BD-Rate Loss
    cpu-used     Reduction     avg.psnr    ovr.psnr    ssim
        5         3.529%       0.1271%     0.1295%    0.0799%
    
    STATS_CHANGED
    
    Change-Id: I4bdcb4f20c2e417b813a6e53b90a52709f8bf3e9

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -4025,6 +4025,41 @@ static int compound_skip_by_single_states(
   return 0;
 }
 
+// Check ref frame of current block matches with given block.
+static INLINE void match_ref_frame(const MB_MODE_INFO *mbmi,
+                                   const MV_REFERENCE_FRAME *ref_frames,
+                                   int *is_ref_match) {
+  if (is_inter_block(mbmi)) {
+    is_ref_match[0] |= ref_frames[0] == mbmi->ref_frame[0];
+    is_ref_match[0] |= ref_frames[1] == mbmi->ref_frame[0];
+    if (has_second_ref(mbmi)) {
+      is_ref_match[1] |= ref_frames[0] == mbmi->ref_frame[1];
+      is_ref_match[1] |= ref_frames[1] == mbmi->ref_frame[1];
+    }
+  }
+}
+
+// Prune compound mode using ref frame of neighbor blocks.
+static int compound_skip_using_neighbor_refs(
+    MACROBLOCKD *const xd, const PREDICTION_MODE this_mode,
+    const MV_REFERENCE_FRAME *ref_frames) {
+  // Exclude non-extended compound modes from pruning
+  if (this_mode == NEAREST_NEARESTMV || this_mode == NEAR_NEARMV ||
+      this_mode == NEW_NEWMV || this_mode == GLOBAL_GLOBALMV)
+    return 0;
+
+  int is_ref_match[2] = { 0 };
+  // Check ref frame of this block matches with left neighbor.
+  if (xd->left_available)
+    match_ref_frame(xd->left_mbmi, ref_frames, is_ref_match);
+
+  // Check ref frame of this block matches with above neighbor.
+  if (xd->up_available)
+    match_ref_frame(xd->above_mbmi, ref_frames, is_ref_match);
+
+  return !(is_ref_match[0] && is_ref_match[1]);
+}
+
 static int compare_int64(const void *a, const void *b) {
   int64_t a64 = *((int64_t *)a);
   int64_t b64 = *((int64_t *)b);
@@ -4273,6 +4308,11 @@ static int skip_inter_mode(AV1_COMP *cpi, MACROBLOCK *x, const BLOCK_SIZE bsize,
       !in_single_ref_cutoff(ref_frame_rd, ref_frame, second_ref_frame)) {
     return 1;
   }
+
+  if (sf->inter_sf.prune_compound_using_neighbors && comp_pred) {
+    if (compound_skip_using_neighbor_refs(xd, this_mode, ref_frames)) return 1;
+  }
+
   return 0;
 }
 
