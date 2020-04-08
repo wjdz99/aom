0f2b9f36649018083fb47c0dbdbd846ed49659c9
Vishesh <vishesh.garg@ittiam.com>
2020-04-08 13:49:55 +0530

    Modularize frame level probabilities in AV1_COMP
    This CL groups probability arrays for various tools
    from AV1_COMP into new struct FrameProbInfo, removes
    redundant variables and adds relevant documentation.
    
    BUG=aomedia:2610
    
    Change-Id: Ie09bcf674e78c865b0709f77844dbc7be04c7d6d

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -5370,6 +5370,7 @@ static int encode_with_recode_loop(AV1_COMP *cpi, size_t *size, uint8_t *dest) {
   AV1_COMMON *const cm = &cpi->common;
   RATE_CONTROL *const rc = &cpi->rc;
   GlobalMotionInfo *const gm_info = &cpi->gm_info;
+  FrameProbInfo *const frame_probs = &cpi->frame_probs;
   const int allow_recode = (cpi->sf.hl_sf.recode_loop != DISALLOW_RECODE);
   // Must allow recode if minimum compression ratio is set.
   assert(IMPLIES(cpi->oxcf.min_cr > 0, allow_recode));
@@ -5412,37 +5413,25 @@ static int encode_with_recode_loop(AV1_COMP *cpi, size_t *size, uint8_t *dest) {
     }
   }
 
-  if (cpi->sf.tx_sf.tx_type_search.prune_tx_type_using_stats) {
-    const int thr[2][2] = { { 15, 10 }, { 17, 10 } };
-    for (int f = 0; f < FRAME_UPDATE_TYPES; f++) {
-      int kf_arf_update = (f == KF_UPDATE || f == ARF_UPDATE);
-      cpi->tx_type_probs_thresh[f] =
-          thr[cpi->sf.tx_sf.tx_type_search.prune_tx_type_using_stats - 1]
-             [kf_arf_update];
-    }
-    if (cm->current_frame.frame_type == KEY_FRAME) {
-      av1_copy(cpi->tx_type_probs, default_tx_type_probs);
-    }
+  if (cpi->sf.tx_sf.tx_type_search.prune_tx_type_using_stats &&
+      cm->current_frame.frame_type == KEY_FRAME) {
+    av1_copy(frame_probs->tx_type_probs, default_tx_type_probs);
   }
 
   if (!cpi->sf.inter_sf.disable_obmc &&
       cpi->sf.inter_sf.prune_obmc_prob_thresh > 0 &&
       cm->current_frame.frame_type == KEY_FRAME) {
-    av1_copy(cpi->obmc_probs, default_obmc_probs);
+    av1_copy(frame_probs->obmc_probs, default_obmc_probs);
   }
   if (cpi->sf.inter_sf.prune_warped_prob_thresh > 0 &&
       cm->current_frame.frame_type == KEY_FRAME) {
-    av1_copy(cpi->warped_probs, default_warped_probs);
+    av1_copy(frame_probs->warped_probs, default_warped_probs);
   }
 
   if (cpi->sf.interp_sf.adaptive_interp_filter_search == 2 &&
       cm->current_frame.frame_type == KEY_FRAME) {
-    av1_copy(cpi->switchable_interp_probs, default_switchable_interp_probs);
-
-    const int thr[7] = { 0, 8, 8, 8, 8, 0, 8 };
-    for (int f = 0; f < FRAME_UPDATE_TYPES; f++) {
-      cpi->switchable_interp_thresh[f] = thr[f];
-    }
+    av1_copy(frame_probs->switchable_interp_probs,
+             default_switchable_interp_probs);
   }
 
 #if !CONFIG_REALTIME_ONLY
