a6b44ce4d599d558fbe770fdc7d93cbcef6f6894
Deepa K G <deepa.kg@ittiam.com>
2020-04-11 13:09:11 +0530

    Modularize multi-threading related info in AV1_COMP
    This CL groups multi-thread related structures from
    AV1_COMP into a new structure MultiThreadInfo, adds
    relevant documentation, and cleans up function
    interfaces. Also, the structure CFL_CTX is removed
    from TileDataEnc as it is unused.
    
    BUG=aomedia:2610
    
    Change-Id: Id4ec055daa368a1d2e04ed84109f0e1ede5d814a

--- av1/encoder/av1_multi_thread.h
+++ av1/encoder/av1_multi_thread.h
@@ -14,8 +14,8 @@
 
 #include "av1/encoder/encoder.h"
 
-void av1_row_mt_mem_alloc(AV1_COMP *cpi, int max_sb_rows);
+void av1_row_mt_mem_alloc(AV1_COMP *const cpi, int max_sb_rows);
 
-void av1_row_mt_mem_dealloc(AV1_COMP *cpi);
+void av1_row_mt_mem_dealloc(AV1_COMP *const cpi);
 
 #endif  // AV1_ENCODER_AV1_MULTI_THREAD_H
