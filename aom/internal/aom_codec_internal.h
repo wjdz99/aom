70a44db2709e5c03137001700ad18730dcfd888d
Elliott Karpilovsky <elliottk@google.com>
2020-04-30 19:19:27 -0700

    Remove surprises from aom_codec_control
    The aom_codec_control API has surprising behavior:
    
    ----
    aom_codec_control(&ctx, AOME_SET_CPUUSED, 8);  // Fine
    
    int ctrl_id = AOME_SET_CPUUSED;
    aom_codec_control(&ctx, ctrl_id, 8);  // Compiler error
    ----
    
    aom_codec_control is a macro that expands to
    aom_codec_control_##ctrl_id for each control type.
    When users encounter the above problem, they must either
    call aom_codec_control_ (which is the underlying function
    without type-checking) or define AOM_DISABLE_CTRL_TYPECHECKS.
    Due to this behavior, both versions are used in the codebase.
    
    This patch:
    
    1. Renames aom_codec_control_ to aom_codec_control
    2. Creates the macro AOM_CODEC_CONTROL_TYPECHECKED to perform
       the same type-checking as before, with a simpler implementation
    
    BUG=aomedia:2651
    
    Change-Id: Ic2ec82ccc7a5b68f9a3fe6185f8fc297b4fcafd7

--- aom/internal/aom_codec_internal.h
+++ aom/internal/aom_codec_internal.h
@@ -138,7 +138,7 @@ typedef aom_codec_err_t (*aom_codec_get_si_fn_t)(aom_codec_alg_priv_t *ctx,
  * function, so plugins implementing this interface may trust the input
  * parameters to be properly initialized. However,  this interface does not
  * provide type safety for the exchanged data or assign meanings to the
- * control codes. Those details should be specified in the algorithm's
+ * control IDs. Those details should be specified in the algorithm's
  * header file. In particular, the ctrl_id parameter is guaranteed to exist
  * in the algorithm's control mapping table, and the data parameter may be NULL.
  *
@@ -306,16 +306,6 @@ struct aom_codec_priv {
     aom_codec_cx_pkt_t cx_data_pkt;
   } enc;
 };
-
-#undef AOM_CTRL_USE_TYPE
-#define AOM_CTRL_USE_TYPE(id, typ) \
-  static AOM_INLINE typ id##__value(va_list args) { return va_arg(args, typ); }
-
-#undef AOM_CTRL_USE_TYPE_DEPRECATED
-#define AOM_CTRL_USE_TYPE_DEPRECATED(id, typ) \
-  static AOM_INLINE typ id##__value(va_list args) { return va_arg(args, typ); }
-
-#define CAST(id, arg) id##__value(arg)
 
 /* Internal Utility Functions
  *
