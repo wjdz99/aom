3afd075188a1cbeca0190eb720fb67bd88465470
Elliott Karpilovsky <elliottk@google.com>
2020-05-10 12:24:18 -0700

    Write encoder settings in WebM files
    If the output is webm, the command line (minus the binary name,
    anything matching the input filename, and output flags) will be
    written as an ENCODER_SETTINGS tag in the WebM container.
    
    STATS_CHANGED:
    Output file will be slightly larger for WebM files, but underlying
    bitstream will be the same.
    
    BUG=aomedia:2593
    
    Change-Id: I03e724d1eea9d6a82ef3068100b7ef96ffb3f87b

--- apps/aomenc.c
+++ apps/aomenc.c
@@ -1814,9 +1814,73 @@ static void show_stream_config(struct stream_state *stream,
   SHOW_PARAMS(reduced_tx_type_set);
 }
 
+#if CONFIG_WEBM_IO
+// Simplistic mechanism to detect if an argv parameter refers to
+// an input or output file. Returns the total number of arguments that
+// should be skipped.
+static int skip_input_output_arg(const char *arg, const char *input_fname) {
+  if (strcmp(arg, input_fname) == 0) {
+    return 1;
+  }
+  if (strcmp(arg, "-o") == 0 || strcmp(arg, "--output") == 0) {
+    return 2;
+  }
+  if (strncmp(arg, "--output=", strlen("--output=")) == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+// Simplistic mechanism to extract encoder settings, without having
+// to re-invoke the entire flag-parsing logic. It copies the arguments
+// as-is from argv, but skips the binary name, any arguments that match the
+// input filename, and the output flags "-o" and "--output" (and the following
+// argument for those flags).
+static char *extract_encoder_settings(const char **argv, int argc,
+                                      const char *input_fname) {
+  size_t total_size = 0;
+  int i = 1;
+  while (i < argc) {
+    int num_skip = skip_input_output_arg(argv[i], input_fname);
+    i += num_skip;
+    if (num_skip == 0) {
+      total_size += strlen(argv[i]) + 1;  // 1 is for the space character.
+      ++i;
+    }
+  }
+  if (total_size == 0) {  // No encoder settings.
+    return NULL;
+  }
+  // The "++" is for simplicity -- we always allocate a trailing space, and
+  // we also need space for the final null character.
+  ++total_size;
+  char *result = malloc(total_size);
+  if (result == NULL) {
+    fprintf(stderr,
+            "Warning: out of memory, cannot extract encoder settings\n");
+    return NULL;
+  }
+  char *cur = result;
+  i = 1;
+  while (i < argc) {
+    int num_skip = skip_input_output_arg(argv[i], input_fname);
+    i += num_skip;
+    if (num_skip == 0) {
+      cur += snprintf(cur, total_size, "%s ", argv[i]);
+      ++i;
+    }
+  }
+  // Replace last, extra space with the null character.
+  --cur;
+  *cur = '\0';
+  return result;
+}
+#endif  // CONFIG_WEBM_IO
+
 static void open_output_file(struct stream_state *stream,
                              struct AvxEncoderConfig *global,
-                             const struct AvxRational *pixel_aspect_ratio) {
+                             const struct AvxRational *pixel_aspect_ratio,
+                             const char *encoder_settings) {
   const char *fn = stream->config.out_fn;
   const struct aom_codec_enc_cfg *const cfg = &stream->config.cfg;
 
@@ -1835,12 +1899,13 @@ static void open_output_file(struct stream_state *stream,
     if (write_webm_file_header(&stream->webm_ctx, &stream->encoder, cfg,
                                stream->config.stereo_fmt,
                                get_fourcc_by_aom_encoder(global->codec),
-                               pixel_aspect_ratio) != 0) {
+                               pixel_aspect_ratio, encoder_settings) != 0) {
       fatal("WebM writer initialization failed.");
     }
   }
 #else
   (void)pixel_aspect_ratio;
+  (void)encoder_settings;
 #endif
 
   if (!stream->config.write_webm && stream->config.write_ivf) {
@@ -2534,7 +2599,16 @@ int main(int argc, const char **argv_) {
     FOREACH_STREAM(stream, streams) { setup_pass(stream, &global, pass); }
     FOREACH_STREAM(stream, streams) { initialize_encoder(stream, &global); }
     FOREACH_STREAM(stream, streams) {
-      open_output_file(stream, &global, &input.pixel_aspect_ratio);
+      char *encoder_settings = NULL;
+#if CONFIG_WEBM_IO
+      if (stream->config.write_webm) {
+        encoder_settings =
+            extract_encoder_settings(argv_, argc, input.filename);
+      }
+#endif
+      open_output_file(stream, &global, &input.pixel_aspect_ratio,
+                       encoder_settings);
+      free(encoder_settings);
     }
 
     if (strcmp(get_short_name_by_aom_encoder(global.codec), "av1") == 0) {
