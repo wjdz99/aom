96f49e638974a2a9290cb639b4e2c1b3023efde9
Vitalii Dziumenko <vdziumenko@luxoft.com>
2020-05-14 11:35:10 +0300

    Add NEON version of few subpixl|variance functions
    AVG gain
    
    aom_variance128x128    5.7
    aom_variance128x64     6.0
    aom_variance64x128     6.1
    aom_variance32x16      6.0
    aom_variance16x32      4.3
    aom_variance8x4        4.7
    aom_variance4x8        5.4
    aom_variance4x4        3.9
    
    via C vs NEON/AvxVarianceTest.DISABLED_Speed
    
    aom_sub_pixel_variance128x128    3.9
    aom_sub_pixel_variance128x64     4.0
    aom_sub_pixel_variance64x128     4.2
    aom_sub_pixel_variance64x32      4.1
    aom_sub_pixel_variance32x64      4.0
    aom_sub_pixel_variance32x16      3.9
    aom_sub_pixel_variance16x32      3.7
    aom_sub_pixel_variance16x8       3.5
    aom_sub_pixel_variance8x16       4.0
    aom_sub_pixel_variance8x4        2.2
    aom_sub_pixel_variance4x8        3.8
    aom_sub_pixel_variance4x4        1.9
    
    via C vs NEON/AvxSubpelVarianceTest.DISABLED_Speed
    
    Change-Id: I8f17b795d319417067e4deba1796a83316d4a148

--- aom_dsp/arm/subpel_variance_neon.c
+++ aom_dsp/arm/subpel_variance_neon.c
@@ -20,6 +20,42 @@
 #include "aom_dsp/aom_filter.h"
 #include "aom_dsp/variance.h"
 
+// Load 2 sets of 4 bytes when alignment is not guaranteed.
+static INLINE uint8x8_t load_unaligned_u8(const uint8_t *buf, int stride) {
+  uint32_t a;
+  uint32x2_t a_u32 = vdup_n_u32(0);
+  if (stride == 4) return vld1_u8(buf);
+  memcpy(&a, buf, 4);
+  buf += stride;
+  a_u32 = vld1_lane_u32(&a, a_u32, 0);
+  memcpy(&a, buf, 4);
+  a_u32 = vld1_lane_u32(&a, a_u32, 1);
+  return vreinterpret_u8_u32(a_u32);
+}
+
+// Process a block exactly 4 wide and a multiple of 2 high.
+static void var_filter_block2d_bil_w4(const uint8_t *src_ptr,
+                                      uint8_t *output_ptr,
+                                      unsigned int src_pixels_per_line,
+                                      int pixel_step,
+                                      unsigned int output_height,
+                                      const uint8_t *filter) {
+  const uint8x8_t f0 = vdup_n_u8(filter[0]);
+  const uint8x8_t f1 = vdup_n_u8(filter[1]);
+  unsigned int i;
+  for (i = 0; i < output_height; i += 2) {
+    const uint8x8_t src_0 = load_unaligned_u8(src_ptr, src_pixels_per_line);
+    const uint8x8_t src_1 =
+        load_unaligned_u8(src_ptr + pixel_step, src_pixels_per_line);
+    const uint16x8_t a = vmull_u8(src_0, f0);
+    const uint16x8_t b = vmlal_u8(a, src_1, f1);
+    const uint8x8_t out = vrshrn_n_u16(b, FILTER_BITS);
+    vst1_u8(output_ptr, out);
+    src_ptr += 2 * src_pixels_per_line;
+    output_ptr += 8;
+  }
+}
+
 static void var_filter_block2d_bil_w8(const uint8_t *src_ptr,
                                       uint8_t *output_ptr,
                                       unsigned int src_pixels_per_line,
@@ -27,8 +63,8 @@ static void var_filter_block2d_bil_w8(const uint8_t *src_ptr,
                                       unsigned int output_height,
                                       unsigned int output_width,
                                       const uint8_t *filter) {
-  const uint8x8_t f0 = vmov_n_u8(filter[0]);
-  const uint8x8_t f1 = vmov_n_u8(filter[1]);
+  const uint8x8_t f0 = vdup_n_u8(filter[0]);
+  const uint8x8_t f1 = vdup_n_u8(filter[1]);
   unsigned int i;
   for (i = 0; i < output_height; ++i) {
     const uint8x8_t src_0 = vld1_u8(&src_ptr[0]);
@@ -36,13 +72,14 @@ static void var_filter_block2d_bil_w8(const uint8_t *src_ptr,
     const uint16x8_t a = vmull_u8(src_0, f0);
     const uint16x8_t b = vmlal_u8(a, src_1, f1);
     const uint8x8_t out = vrshrn_n_u16(b, FILTER_BITS);
-    vst1_u8(&output_ptr[0], out);
+    vst1_u8(output_ptr, out);
     // Next row...
     src_ptr += src_pixels_per_line;
     output_ptr += output_width;
   }
 }
 
+// Process a block which is a mutiple of 16 wide and any height.
 static void var_filter_block2d_bil_w16(const uint8_t *src_ptr,
                                        uint8_t *output_ptr,
                                        unsigned int src_pixels_per_line,
@@ -50,8 +87,8 @@ static void var_filter_block2d_bil_w16(const uint8_t *src_ptr,
                                        unsigned int output_height,
                                        unsigned int output_width,
                                        const uint8_t *filter) {
-  const uint8x8_t f0 = vmov_n_u8(filter[0]);
-  const uint8x8_t f1 = vmov_n_u8(filter[1]);
+  const uint8x8_t f0 = vdup_n_u8(filter[0]);
+  const uint8x8_t f1 = vdup_n_u8(filter[1]);
   unsigned int i, j;
   for (i = 0; i < output_height; ++i) {
     for (j = 0; j < output_width; j += 16) {
@@ -63,9 +100,8 @@ static void var_filter_block2d_bil_w16(const uint8_t *src_ptr,
       const uint16x8_t c = vmull_u8(vget_high_u8(src_0), f0);
       const uint16x8_t d = vmlal_u8(c, vget_high_u8(src_1), f1);
       const uint8x8_t out_hi = vrshrn_n_u16(d, FILTER_BITS);
-      vst1q_u8(&output_ptr[j], vcombine_u8(out_lo, out_hi));
+      vst1q_u8(output_ptr + j, vcombine_u8(out_lo, out_hi));
     }
-    // Next row...
     src_ptr += src_pixels_per_line;
     output_ptr += output_width;
   }
@@ -129,3 +165,51 @@ unsigned int aom_sub_pixel_variance64x64_neon(const uint8_t *src,
                              bilinear_filters_2t[yoffset]);
   return aom_variance64x64_neon(temp2, 64, dst, dst_stride, sse);
 }
+
+// 4xM filter writes an extra row to fdata because it processes two rows at a
+// time.
+#define sub_pixel_varianceNxM(n, m)                                 \
+  unsigned int aom_sub_pixel_variance##n##x##m##_neon(              \
+      const uint8_t *a, int a_stride, int xoffset, int yoffset,     \
+      const uint8_t *b, int b_stride, uint32_t *sse) {              \
+    uint8_t temp1[n * m];                                           \
+    if (n == 4) {                                                   \
+      uint8_t temp0[n * (m + 2)];                                   \
+      var_filter_block2d_bil_w4(a, temp0, a_stride, 1, (m + 2),     \
+                                bilinear_filters_2t[xoffset]);      \
+      var_filter_block2d_bil_w4(temp0, temp1, n, n, m,              \
+                                bilinear_filters_2t[yoffset]);      \
+    } else if (n == 8) {                                            \
+      uint8_t temp0[n * (m + 1)];                                   \
+      var_filter_block2d_bil_w8(a, temp0, a_stride, 1, (m + 1), 8,  \
+                                bilinear_filters_2t[xoffset]);      \
+      var_filter_block2d_bil_w8(temp0, temp1, n, n, m, 8,           \
+                                bilinear_filters_2t[yoffset]);      \
+    } else {                                                        \
+      uint8_t temp0[n * (m + 1)];                                   \
+      var_filter_block2d_bil_w16(a, temp0, a_stride, 1, (m + 1), n, \
+                                 bilinear_filters_2t[xoffset]);     \
+      var_filter_block2d_bil_w16(temp0, temp1, n, n, m, n,          \
+                                 bilinear_filters_2t[yoffset]);     \
+    }                                                               \
+    return aom_variance##n##x##m(temp1, n, b, b_stride, sse);       \
+  }
+
+sub_pixel_varianceNxM(4, 4);
+sub_pixel_varianceNxM(4, 8);
+sub_pixel_varianceNxM(8, 4);
+sub_pixel_varianceNxM(8, 16);
+sub_pixel_varianceNxM(16, 8);
+sub_pixel_varianceNxM(16, 32);
+sub_pixel_varianceNxM(32, 16);
+sub_pixel_varianceNxM(32, 64);
+sub_pixel_varianceNxM(64, 32);
+sub_pixel_varianceNxM(64, 128);
+sub_pixel_varianceNxM(128, 64);
+sub_pixel_varianceNxM(128, 128);
+sub_pixel_varianceNxM(8, 32);
+sub_pixel_varianceNxM(32, 8);
+sub_pixel_varianceNxM(64, 16);
+sub_pixel_varianceNxM(16, 64);
+sub_pixel_varianceNxM(4, 16);
+sub_pixel_varianceNxM(16, 4);
