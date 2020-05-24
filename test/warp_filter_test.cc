c7e4968f9ccd5bcfab6874e028e76ca874ef77b8
Remya <remya.prakasan@ittiam.com>
2020-05-24 10:59:14 +0530

    PA2: Add av1_ext_warp_affine_avx2
    Change-Id: I16cf6c5ef04ce048007bece061ce8c03da7e8608

--- test/warp_filter_test.cc
+++ test/warp_filter_test.cc
@@ -11,6 +11,7 @@
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 #include "test/warp_filter_test_util.h"
 using libaom_test::ACMRandom;
+using libaom_test::AV1ExtWarpFilter::AV1ExtWarpFilterTest;
 using libaom_test::AV1HighbdWarpFilter::AV1HighbdWarpFilterTest;
 using libaom_test::AV1WarpFilter::AV1WarpFilterTest;
 using ::testing::make_tuple;
@@ -28,6 +29,17 @@ TEST_P(AV1WarpFilterTest, DISABLED_Speed) {
 INSTANTIATE_TEST_CASE_P(
     C, AV1WarpFilterTest,
     libaom_test::AV1WarpFilter::BuildParams(av1_warp_affine_c));
+
+TEST_P(AV1ExtWarpFilterTest, CheckOutput) {
+  RunCheckOutput(::testing::get<3>(GET_PARAM(0)));
+}
+TEST_P(AV1ExtWarpFilterTest, DISABLED_Speed) {
+  RunSpeedTest(::testing::get<3>(GET_PARAM(0)));
+}
+
+INSTANTIATE_TEST_CASE_P(
+    C, AV1ExtWarpFilterTest,
+    libaom_test::AV1ExtWarpFilter::BuildParams(av1_ext_warp_affine_c));
 
 #if HAVE_SSE4_1
 INSTANTIATE_TEST_CASE_P(
@@ -51,6 +63,10 @@ INSTANTIATE_TEST_CASE_P(SSE4_1, AV1HighbdWarpFilterTest,
 INSTANTIATE_TEST_CASE_P(
     AVX2, AV1WarpFilterTest,
     libaom_test::AV1WarpFilter::BuildParams(av1_warp_affine_avx2));
+
+INSTANTIATE_TEST_CASE_P(
+    AVX2, AV1ExtWarpFilterTest,
+    libaom_test::AV1ExtWarpFilter::BuildParams(av1_ext_warp_affine_avx2));
 #endif  // HAVE_AVX2
 
 #if HAVE_NEON
