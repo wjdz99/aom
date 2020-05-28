7df8965e3891774f5d4abf17b89b986ab5b9e6c0
chiyotsai <chiyotsai@google.com>
2020-05-28 16:55:08 -0700

    Add NN_RECON experiment
    This commit adds NN_RECON experiment.
    
    In this experiment, we add an extra CNN-based reconstruction option at
    the partition level. At each partition block, the encoder will signal
    whether a cnn-based reconstruction option is on for all the transform
    blocks inside. If off, the bitstream with proceed with the usual
    dct-based reconstruction. If on, we will first perform a dct-based
    reconstruction, then we will apply an cnn-based filter to the result of
    dct reconstruction along with some contexts.
    
    This experiment is still work in progress.
    Due to complexity consideration, currently this is only applied to
    BLOCK_16X16. Furthermore, in the rdopt process, the encoder tests the
    cnn filter for all prediction_mode x txfm_type combinations, so the
    encoding is very slow (around 600X the baseline speed). We are working
    to optimize this rdopt process.
    
    In offline validation outside the encoder, this cnn filter provides
    about 0.17 dB gain, but validation on the encoder only shows 0.05%
    bitrate reduction on high resolution images. This is mostly due to two
    factors:
      1. The bitstream signaling is not very efficient. In particular, it is
      currently independent of the quantization index. Intuitively speaking,
      this reconstruction option should be off for small q-index.
      2. Currently the CNNs are only enabled for TX_16X16 due to high
      complexity cost. Once the rdopt process is sped up more, more CNN
      models can be turned on, giving us more gains.
    
    Note: This commit only adds the necessary bitstream/rdopt changes for
    the experiment. The actual model isn't uploaded here.
    
    Change-Id: I650e5d73080f7ac182b390e1d5d2db472f0aeaad

---
+++ av1/tflite_models/intra_txfm_recon_model/tx16x16.cc
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#include "av1/tflite_models/intra_txfm_recon_model/tx16x16.h"
+
+// TODO(chiyotsai@google.com): Upload CNN files here once we are sure that it is
+// properly sanitized.
+const unsigned char tx16x16_tflite[] = {};
