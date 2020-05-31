5f9d253f93f63b56786893f65361b697ad19df7c
Remya <remya.prakasan@ittiam.com>
2020-06-01 00:17:39 +0530

    Add av1_ext_highbd_warp_affine_sse4_1
    STATS_CHANGED
    
    Change-Id: I8c5c5ce2d98e3e9dea22e7967631adcaf8684a2f

--- test/warp_filter_test.cc
+++ test/warp_filter_test.cc
@@ -11,6 +11,7 @@
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 #include "test/warp_filter_test_util.h"
 using libaom_test::ACMRandom;
+using libaom_test::AV1ExtHighbdWarpFilter::AV1ExtHighbdWarpFilterTest;
 using libaom_test::AV1HighbdWarpFilter::AV1HighbdWarpFilterTest;
 using libaom_test::AV1WarpFilter::AV1WarpFilterTest;
 using ::testing::make_tuple;
@@ -44,6 +45,17 @@ TEST_P(AV1HighbdWarpFilterTest, DISABLED_Speed) {
 INSTANTIATE_TEST_CASE_P(SSE4_1, AV1HighbdWarpFilterTest,
                         libaom_test::AV1HighbdWarpFilter::BuildParams(
                             av1_highbd_warp_affine_sse4_1));
+
+TEST_P(AV1ExtHighbdWarpFilterTest, CheckOutput) {
+  RunCheckOutput(::testing::get<4>(GET_PARAM(0)));
+}
+TEST_P(AV1ExtHighbdWarpFilterTest, DISABLED_Speed) {
+  RunSpeedTest(::testing::get<4>(GET_PARAM(0)));
+}
+
+INSTANTIATE_TEST_CASE_P(SSE4_1, AV1ExtHighbdWarpFilterTest,
+                        libaom_test::AV1ExtHighbdWarpFilter::BuildParams(
+                            av1_ext_highbd_warp_affine_sse4_1));
 
 #endif  // HAVE_SSE4_1
 
