60247751841ea892adee94a77dbd39832786f21c
Jingning Han <jingning@google.com>
2020-06-15 13:48:22 -0700

    Redesign AVX2 2d interpolation filter
    Allow flexible support to handle various filter taps in the vertical
    and horizontal directions respectively.
    
    Change-Id: Ic260cf7c29a6f464f4afbcbca78c33bd19a2a789

--- av1/common/x86/convolve_2d_avx2.c
+++ av1/common/x86/convolve_2d_avx2.c
@@ -27,8 +27,7 @@ void av1_convolve_2d_sr_avx2(const uint8_t *src, int src_stride, uint8_t *dst,
                              const int subpel_x_qn, const int subpel_y_qn,
                              ConvolveParams *conv_params) {
   const int bd = 8;
-  int im_stride = 8;
-  int i, is_horiz_4tap = 0, is_vert_4tap = 0;
+  int im_stride = 8, i;
   DECLARE_ALIGNED(32, int16_t, im_block[(MAX_SB_SIZE + MAX_FILTER_TAP) * 8]);
   const int bits =
       FILTER_BITS * 2 - conv_params->round_0 - conv_params->round_1;
@@ -57,23 +56,28 @@ void av1_convolve_2d_sr_avx2(const uint8_t *src, int src_stride, uint8_t *dst,
   prepare_coeffs_lowbd(filter_params_x, subpel_x_qn, coeffs_h);
   prepare_coeffs(filter_params_y, subpel_y_qn, coeffs_v);
 
-  // Condition for checking valid horz_filt taps
-  if (!(_mm256_extract_epi32(_mm256_or_si256(coeffs_h[0], coeffs_h[3]), 0)))
-    is_horiz_4tap = 1;
+  const int16_t *const filter_x = av1_get_interp_filter_subpel_kernel(
+      filter_params_x, subpel_x_qn & SUBPEL_MASK);
+  const int16_t *const filter_y = av1_get_interp_filter_subpel_kernel(
+      filter_params_y, subpel_y_qn & SUBPEL_MASK);
 
-  // Condition for checking valid vert_filt taps
-  if (!(_mm256_extract_epi32(_mm256_or_si256(coeffs_v[0], coeffs_v[3]), 0)))
-    is_vert_4tap = 1;
+  int horiz_tap = SUBPEL_TAPS;
+  int vert_tap = SUBPEL_TAPS;
 
-  // horz_filt as 4 tap and vert_filt as 8 tap
-  if (is_horiz_4tap) {
-    int im_h = h + filter_params_y->taps - 1;
-    const int fo_vert = filter_params_y->taps / 2 - 1;
-    const int fo_horiz = 1;
-    const uint8_t *const src_ptr = src - fo_vert * src_stride - fo_horiz;
+  if (!(filter_x[0] | filter_x[1] | filter_x[6] | filter_x[7])) horiz_tap = 4;
 
-    // horz-filter
-    for (int j = 0; j < w; j += 8) {
+  if (!(filter_y[0] | filter_y[1] | filter_y[6] | filter_y[7])) vert_tap = 4;
+
+  int im_h = h + vert_tap - 1;
+  const int fo_vert = vert_tap / 2 - 1;
+  const int fo_horiz = horiz_tap / 2 - 1;
+  const uint8_t *const src_ptr = src - fo_vert * src_stride - fo_horiz;
+
+  filt[2] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 2));
+  filt[3] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 3));
+
+  for (int j = 0; j < w; j += 8) {
+    if (horiz_tap == 4) {
       for (i = 0; i < (im_h - 2); i += 2) {
         __m256i data = _mm256_castsi128_si256(
             _mm_loadu_si128((__m128i *)&src_ptr[(i * src_stride) + j]));
@@ -98,23 +102,11 @@ void av1_convolve_2d_sr_avx2(const uint8_t *src, int src_stride, uint8_t *dst,
       res =
           _mm256_sra_epi16(_mm256_add_epi16(res, round_const_h), round_shift_h);
       _mm256_store_si256((__m256i *)&im_block[i * im_stride], res);
-
-      // vert filter
-      CONVOLVE_SR_VERTICAL_FILTER_8TAP;
+    } else {
+      CONVOLVE_SR_HORIZONTAL_FILTER_8TAP
     }
-  } else if (is_vert_4tap) {
-    int im_h = h + 3;
-    const int fo_vert = 1;
-    const int fo_horiz = filter_params_x->taps / 2 - 1;
-    const uint8_t *const src_ptr = src - fo_vert * src_stride - fo_horiz;
 
-    filt[2] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 2));
-    filt[3] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 3));
-
-    for (int j = 0; j < w; j += 8) {
-      // horz_filter
-      CONVOLVE_SR_HORIZONTAL_FILTER_8TAP;
-      // vert_filter
+    if (vert_tap == 4) {
       __m256i s[6];
       __m256i src_0 = _mm256_loadu_si256((__m256i *)(im_block + 0 * im_stride));
       __m256i src_1 = _mm256_loadu_si256((__m256i *)(im_block + 1 * im_stride));
@@ -179,21 +171,8 @@ void av1_convolve_2d_sr_avx2(const uint8_t *src, int src_stride, uint8_t *dst,
         s[3] = s[4];
         s[4] = s[5];
       }
-    }
-  } else {
-    int j;
-    int im_h = h + filter_params_y->taps - 1;
-    const int fo_vert = filter_params_y->taps / 2 - 1;
-    const int fo_horiz = filter_params_x->taps / 2 - 1;
-    const uint8_t *const src_ptr = src - fo_vert * src_stride - fo_horiz;
-
-    filt[2] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 2));
-    filt[3] = _mm256_load_si256((__m256i const *)(filt_global_avx2 + 32 * 3));
-
-    for (j = 0; j < w; j += 8) {
-      CONVOLVE_SR_HORIZONTAL_FILTER_8TAP;
-
-      CONVOLVE_SR_VERTICAL_FILTER_8TAP;
+    } else {
+      CONVOLVE_SR_VERTICAL_FILTER_8TAP
     }
   }
 }
