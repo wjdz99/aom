307077cfeab5f53aa98150ea797c2edba28a152d
Vijay Sundaram <vijay.sundaram@intel.com>
2020-06-15 18:38:38 -0700

    Enhanced IBC for Screen Content Compression
    Change-Id: I24caee59fc4dd970eda2f6c20db421f92d788a86

--- av1/encoder/mcomp.h
+++ av1/encoder/mcomp.h
@@ -155,7 +155,12 @@ int av1_full_pixel_search(const struct AV1_COMP *cpi, MACROBLOCK *x,
                           int use_var, int method, int run_mesh_search,
                           int error_per_bit, int *cost_list, const MV *ref_mv,
                           int var_max, int rd, int x_pos, int y_pos, int intra,
-                          const search_site_config *cfg);
+                          const search_site_config *cfg
+#if CONFIG_EXT_IBC_MODES
+                          ,
+                          IBC_MODE ibcMode);
+#endif  // CONFIG_EXT_IBC_MODES
+);
 
 int av1_full_pixel_search_var(const struct AV1_COMP *cpi, MACROBLOCK *x,
                               BLOCK_SIZE bsize, MV *mvp_full, int step_param,
