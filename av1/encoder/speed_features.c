204d543b8c0b4d8f70da4094f18bb0a004ed6971
Yunqing Wang <yunqingwang@google.com>
2020-06-29 15:11:18 -0700

    Modify adaptive_txb_search_level for speed 6
    STATS_CHANGED
    
    Change-Id: I02ba5b31385849fc460fe99948170435d307ea40

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -640,6 +640,7 @@ static void set_good_speed_features_framesize_independent(
     sf->tpl_sf.subpel_force_stop = FULL_PEL;
     sf->tpl_sf.disable_filtered_key_tpl = 1;
 
+    sf->tx_sf.adaptive_txb_search_level = boosted ? 3 : 4;
     sf->tx_sf.use_intra_txb_hash = 1;
     sf->tx_sf.tx_type_search.prune_tx_type_est_rd = 0;
 
