e3da9a1af023f43fe1eb7954050efd47466a0b8f
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2020-07-02 13:43:48 +0530

    Add unit test to check if tile config is respected
    UniformTileConfigTestLarge checks whether number of tile
    rows and tile columns are respected as configured by the user.
    
    This test currently only supports uniform spacing.
    
    Change-Id: I46698b090308c8e88f118e35d677dc0a11865fa4

--- test/test.cmake
+++ test/test.cmake
@@ -132,6 +132,7 @@ if(NOT BUILD_SHARED_LIBS)
                 "${AOM_ROOT}/test/segment_binarization_sync.cc"
                 "${AOM_ROOT}/test/still_picture_test.cc"
                 "${AOM_ROOT}/test/superframe_test.cc"
+                "${AOM_ROOT}/test/tile_config_test.cc"
                 "${AOM_ROOT}/test/tile_independence_test.cc"
                 "${AOM_ROOT}/test/temporal_filter_test.cc")
     if(CONFIG_REALTIME_ONLY)
