1afc23f42f3ef4adf78754ae1b50303c663fbf16
chiyotsai <chiyotsai@google.com>
2020-07-06 14:20:50 -0700

    Remove disable_adaptive_warp_error_thresh
    This CL removes disable_adaptive_warp_error_thresh speed feature and
    enables adaptive_warp_error_thresh on speed 0. Furthermore, the speed
    feature flag gm_erroradv_type is removed as it does not provide benefit
    over the default values.
    
    Performance on Speed 0:
     TESTSET | AVG_PSNR | OVR_PSNR |   SSIM  |  SPD
      LOWRES |  -0.001% |  -0.001% | -0.001% | +0.9%
      MIDRES |  -0.007% |  -0.007% | -0.006% | +1.4%
    
    STATS_CHANGED
    
    BUG=aomedia:2694
    
    Change-Id: I3126c81904956e3620abd19420b1be12ed4e7402

--- av1/encoder/global_motion.c
+++ av1/encoder/global_motion.c
@@ -64,11 +64,10 @@ typedef struct {
   double *level_dy_buffer;
 } ImagePyramid;
 
-int av1_is_enough_erroradvantage(double best_erroradvantage, int params_cost,
-                                 int erroradv_type) {
+int av1_is_enough_erroradvantage(double best_erroradvantage, int params_cost) {
   assert(erroradv_type < GM_ERRORADV_TR_TYPES);
-  return best_erroradvantage < erroradv_tr[erroradv_type] &&
-         best_erroradvantage * params_cost < erroradv_prod_tr[erroradv_type];
+  return best_erroradvantage < erroradv_tr &&
+         best_erroradvantage * params_cost < erroradv_prod_tr;
 }
 
 static void convert_to_params(const double *params, int32_t *model) {
