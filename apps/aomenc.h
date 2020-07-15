714c31b4b6981527751b8179a41a9fd41419111f
Elliott Karpilovsky <elliottk@google.com>
2020-07-14 21:57:33 -0700

    Add command-line for temporal subsampling
    Change-Id: I08448a1e720904ae4b1cf96e99e8e6ec6678ce09

--- apps/aomenc.h
+++ apps/aomenc.h
@@ -43,6 +43,7 @@ struct AvxEncoderConfig {
   int verbose;
   int limit;
   int skip_frames;
+  int temporal_subsample;
   int show_psnr;
   enum TestDecodeFatality test_decode;
   int have_framerate;
