17d64f25007fe505d6d02fa7865e897cf95124df
Debargha Mukherjee <debargha@google.com>
2020-07-15 19:11:05 -0700

    Rename and reorganize config flags for superres tx
    CONFIG_NEW_TX64X64 is now namesd CONFIG_SUPERRES_TX64.
    The option to use a directional Wiener filter is now
    an additional cnfig flag named CONFIG_SUPERRES_TX64_DIRFILTER.
    
    Change-Id: Ifb1f9376781be883e14b972dc25156336f4c6071

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -175,8 +175,10 @@ set_aom_config_var(CONFIG_ENTROPY_CONTEXTS 0 NUMBER
                    "AV2 entropy coding contexts experiment flag")
 set_aom_config_var(CONFIG_WIENER_NONSEP 0 NUMBER
                    "AV2 nonsep Wiener filter experiment flag")
-set_aom_config_var(CONFIG_NEW_TX64X64 0 NUMBER
-                   "AV2 new 64-length transform experiment flag")
+set_aom_config_var(CONFIG_SUPERRES_TX64 0 NUMBER
+                   "AV2 64-length superres tx experiment flag")
+set_aom_config_var(CONFIG_SUPERRES_TX64_DIRFILTER 0 NUMBER
+                   "AV2 64-length superres tx w/ dir filter experiment flag")
 set_aom_config_var(CONFIG_DIFFWTD_42 0 NUMBER
                    "AV2 diffwtd mask experiment flag")
 set_aom_config_var(CONFIG_ILLUM_MCOMP 0 NUMBER
