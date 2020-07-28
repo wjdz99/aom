f48d4c3f9a1dd5085c6bbc70830b6c97a7c8185e
Jinkun Geng <jinkun@google.com>
2020-07-28 06:12:32 +0000

    debig-dbscan-no-merge
    Change-Id: I9bcae3141a7f7eb04d7b41075d6ff1563c5a561c

--- av1/common/mvref_common.c
+++ av1/common/mvref_common.c
@@ -1006,30 +1006,30 @@ static void setup_ref_mv_list(const AV1_COMMON *cm, const MACROBLOCKD *xd,
     // If the spatial mvs can not even fill the MAX_MV_REF_CANDIDATES, we will
     // not cluster them
 
-    // if (nearest_refmv_count > MAX_MV_REF_CANDIDATES) {
-    //   mv_dbscan1(ref_mv_stack, 0, nearest_refmv_count, min_points,
-    //              dist_threshold, (&cluster_num1), cluster_label,
-    //              (rf[1] == NONE_FRAME));
+    if (nearest_refmv_count > MAX_MV_REF_CANDIDATES) {
+      mv_dbscan1(ref_mv_stack, 0, nearest_refmv_count, min_points,
+                 dist_threshold, (&cluster_num1), cluster_label,
+                 (rf[1] == NONE_FRAME));
 
-    //   // Merge MVs
-    //   for (int i = 0; i < nearest_refmv_count; i++) {
-    //     if (cluster_label[i] == -1) {
-    //       // outlier
-    //       continue;
-    //     } else if (cluster_label[i] == i) {
-    //       // centriod update
+      // Merge MVs
+      // for (int i = 0; i < nearest_refmv_count; i++) {
+      //   if (cluster_label[i] == -1) {
+      //     // outlier
+      //     continue;
+      //   } else if (cluster_label[i] == i) {
+      //     // centriod update
 
-    //       merge_mv(ref_mv_stack, ref_mv_weight, cluster_label, 0,
-    //                nearest_refmv_count, i);
-    //     }
-    //   }
-    // }
+      //     merge_mv(ref_mv_stack, ref_mv_weight, cluster_label, 0,
+      //              nearest_refmv_count, i);
+      //   }
+      // }
+    }
 
-    // mv_dbscan1(ref_mv_stack, nearest_refmv_count, (*refmv_count), min_points,
-    //            dist_threshold, (&cluster_num2), cluster_label,
-    //            (rf[1] == NONE_FRAME));
+    mv_dbscan1(ref_mv_stack, nearest_refmv_count, (*refmv_count), min_points,
+               dist_threshold, (&cluster_num2), cluster_label,
+               (rf[1] == NONE_FRAME));
 
-    // // Merge MVs
+    // Merge MVs
     // for (int i = nearest_refmv_count; i < (*refmv_count); i++) {
     //   if (cluster_label[i] == -1) {
     //     // outlier
