7de2e90d17678761f1c771230d034a1b38c88705
Ravi Chaudhary <ravi.chaudhary@ittiam.com>
2020-08-02 01:10:43 +0530

    Signal delta params at SB level for GM
    STATS_CHANGED
    
    Change-Id: If3778f4511adf0147dd5fc3a3a6e4b6157cbfc74

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -213,6 +213,8 @@ set_aom_config_var(CONFIG_DUMP_MFQE_DATA 0 NUMBER
                    "AV2 in-loop MFQE experiment flag")
 set_aom_config_var(CONFIG_INTERINTRA_BORDER 0 NUMBER
                    "Calculate an extended border for interintra prediction")
+set_aom_config_var(CONFIG_SB_WARP 1 NUMBER
+                   "AV2 extension to superblock warp experiment flag")
 
 # To include Tensorflow, make sure to build tensorflow locally using
 # tensorflow/contrib/makefile/build_all_linux.sh and then providing the correct
