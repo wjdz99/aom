0349c6e857ccd68385b8d3821b1e6934af3f8ff3
Prakhar Singh <singhprakhar@google.com>
2020-08-11 18:10:44 -0500

    Modify quantizer when downsampling residuals
    Change-Id: I6981da86cbf426f892f120228ad98e414754697d

--- av1/encoder/tx_search.c
+++ av1/encoder/tx_search.c
@@ -3401,6 +3401,7 @@ void av1_pick_recursive_tx_size_type_yrd(const AV1_COMP *cpi, MACROBLOCK *x,
 
     if (dspl_type > DSPL_NONE && xd->allow_dspl_residual == 0) continue;
 
+    av1_setup_dspl_quantizer(cpi, x, mbmi->segment_id, dspl_type);
     mbmi->dspl_type = dspl_type;
 
     // Pre-compute residue hashes (transform block level) and find existing or
@@ -3426,6 +3427,7 @@ void av1_pick_recursive_tx_size_type_yrd(const AV1_COMP *cpi, MACROBLOCK *x,
 
   int64_t rd = INT64_MAX;
   if (best_rd != INT64_MAX) {
+    av1_setup_dspl_quantizer(cpi, x, mbmi->segment_id, best_dspl_type);
 
     mbmi->dspl_type = best_dspl_type;
     rd = select_tx_size_and_type(
@@ -3433,6 +3435,8 @@ void av1_pick_recursive_tx_size_type_yrd(const AV1_COMP *cpi, MACROBLOCK *x,
         found_rd_info[best_dspl_type] ? matched_rd_info[best_dspl_type] : NULL);
   }
 
+  // Restore original quantizer
+  av1_setup_dspl_quantizer(cpi, x, mbmi->segment_id, DSPL_NONE);
 #else
   // Pre-compute residue hashes (transform block level) and find existing or
   // add new RD records to store and reuse rate and distortion values to speed
