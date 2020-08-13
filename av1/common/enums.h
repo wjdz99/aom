77f94c41509fe2653319586192e69fe2d445510d
chiyotsai <chiyotsai@google.com>
2020-08-12 18:25:21 -0700

    Improve pruning of EXT_RECUR_PARTITIONS experiments
    This commit contains three main changes:
      - The partition pruning at small bsizes is vastly improved by pushing
        better mv candidates to an internal list. Performance wise, the
        speed up remains at ~80%, but the bitrate increase has reduced from
        1.37% to 0.87%. In particular, bad performing videos such as
        TrafficFlow_426x240_30fps_8bit.y4m now performs normally with loss
        going from 5.2% to 2.6%.
      - The partition decision now takes partition rate into consideration.
      - A txfm pipeline is added to simple_motion_search to estimate the
        coefficient rate and reconstruction rate. The coefficient rate is
        estimated using \sum log(1 + coeff^2). Currently, this leads to
        extremely large losses and is disabled by default.
    
    Change-Id: Icca224323520b5315367aae9e3baf4996e9355ab

--- av1/common/enums.h
+++ av1/common/enums.h
@@ -164,7 +164,8 @@ typedef enum {
 
 #if CONFIG_EXT_RECUR_PARTITIONS
 #define KEEP_PARTITION_SPLIT 0
-#define ENABLE_FAST_RECUR_PARTITION 0
+#define ENABLE_FAST_RECUR_PARTITION 2
+#define USE_EST_TXFM 0
 #endif  // CONFIG_EXT_RECUR_PARTITIONS
 
 //  Block partition types.  R: Recursive
