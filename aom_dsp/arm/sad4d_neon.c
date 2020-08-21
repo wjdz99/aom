fdb1a341b39bd9fafe3f96a93d88ccb963519137
Krishna Malladi <kmalladi@google.com>
2020-08-21 10:25:23 -0700

    Add Neon Low Bit-depth for SADSkip
    Performance:
    Speed 	Improvement over C
    0	3.2x
    1	2.5x
    2	2.53x
    3	1.8x
    4	1.78x
    5	1.7x
    
    Patch 2: Update with clang and aom_dsp.cmake
    Patch 3: upadte with clang and add sad_test.cc
    Patch 4: Update aom_dsp.cmake with whitespace
    Patch 5: Restore config/aom_dsp_rtcd.h in sad4d_neon.c
    Patch 6: Add 'const' in line 424 for Ref[4] and define ref variables in sad4d_neon.c
    Change-Id: I97d27c984295624d7856cdfaab9e25221803b92e
    
    Change-Id: I4bebfceaa9b2f3aee9e5430b4f1f233dc64db677

--- aom_dsp/arm/sad4d_neon.c
+++ aom_dsp/arm/sad4d_neon.c
@@ -224,3 +224,223 @@ void aom_sad16x16x4d_neon(const uint8_t *src, int src_stride,
   res[2] = horizontal_long_add_16x8(vec_sum_ref2_lo, vec_sum_ref2_hi);
   res[3] = horizontal_long_add_16x8(vec_sum_ref3_lo, vec_sum_ref3_hi);
 }
+
+static INLINE unsigned int horizontal_add_16x8(const uint16x8_t vec_16x8) {
+  const uint32x4_t a = vpaddlq_u16(vec_16x8);
+  const uint64x2_t b = vpaddlq_u32(a);
+  const uint32x2_t c = vadd_u32(vreinterpret_u32_u64(vget_low_u64(b)),
+                                vreinterpret_u32_u64(vget_high_u64(b)));
+  return vget_lane_u32(c, 0);
+}
+
+static void sad_row16_neon(uint16x8_t *vec_src, const uint8x16_t *q0,
+                           const uint8_t *ref_ptr) {
+  uint8x16_t q1 = vld1q_u8(ref_ptr);
+  uint8x16_t q2 = vabdq_u8(*q0, q1);
+  *vec_src = vpadalq_u8(*vec_src, q2);
+}
+
+void aom_sadMxNx4d_neon(int width, int height, const uint8_t *src,
+                        int src_stride, const uint8_t *const ref[4], int ref_stride,
+                        uint32_t res[4]) {
+  uint8x16_t q4;
+  const uint8_t *ref0, *ref1, *ref2, *ref3;
+
+  ref0 = ref[0];
+  ref1 = ref[1];
+  ref2 = ref[2];
+  ref3 = ref[3];
+
+  res[0] = 0;
+  res[1] = 0;
+  res[2] = 0;
+  res[3] = 0;
+
+
+
+  switch (width) {
+    case 32: {
+      for (int i = 0; i < height; i++) {
+        uint16x8_t q0 = vdupq_n_u16(0);
+        uint16x8_t q1 = vdupq_n_u16(0);
+        uint16x8_t q2 = vdupq_n_u16(0);
+        uint16x8_t q3 = vdupq_n_u16(0);
+
+        q4 = vld1q_u8(src);
+
+        sad_row16_neon(&q0, &q4, ref[0]);
+        sad_row16_neon(&q1, &q4, ref[1]);
+        sad_row16_neon(&q2, &q4, ref[2]);
+        sad_row16_neon(&q3, &q4, ref[3]);
+
+        q4 = vld1q_u8(src + 16);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 16);
+        sad_row16_neon(&q1, &q4, ref[1] + 16);
+        sad_row16_neon(&q2, &q4, ref[2] + 16);
+        sad_row16_neon(&q3, &q4, ref[3] + 16);
+
+        src += src_stride;
+        ref0 += ref_stride;
+        ref1 += ref_stride;
+        ref2 += ref_stride;
+        ref3 += ref_stride;
+
+        res[0] += horizontal_add_16x8(q0);
+        res[1] += horizontal_add_16x8(q1);
+        res[2] += horizontal_add_16x8(q2);
+        res[3] += horizontal_add_16x8(q3);
+      }
+      break;
+    }
+    case 64: {
+      for (int i = 0; i < height; i++) {
+        uint16x8_t q0 = vdupq_n_u16(0);
+        uint16x8_t q1 = vdupq_n_u16(0);
+        uint16x8_t q2 = vdupq_n_u16(0);
+        uint16x8_t q3 = vdupq_n_u16(0);
+
+        q4 = vld1q_u8(src);
+
+        sad_row16_neon(&q0, &q4, ref[0]);
+        sad_row16_neon(&q1, &q4, ref[1]);
+        sad_row16_neon(&q2, &q4, ref[2]);
+        sad_row16_neon(&q3, &q4, ref[3]);
+
+        q4 = vld1q_u8(src + 16);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 16);
+        sad_row16_neon(&q1, &q4, ref[1] + 16);
+        sad_row16_neon(&q2, &q4, ref[2] + 16);
+        sad_row16_neon(&q3, &q4, ref[3] + 16);
+
+        q4 = vld1q_u8(src + 32);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 32);
+        sad_row16_neon(&q1, &q4, ref[1] + 32);
+        sad_row16_neon(&q2, &q4, ref[2] + 32);
+        sad_row16_neon(&q3, &q4, ref[3] + 32);
+
+        q4 = vld1q_u8(src + 48);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 48);
+        sad_row16_neon(&q1, &q4, ref[1] + 48);
+        sad_row16_neon(&q2, &q4, ref[2] + 48);
+        sad_row16_neon(&q3, &q4, ref[3] + 48);
+
+        src += src_stride;
+        ref0 += ref_stride;
+        ref1 += ref_stride;
+        ref2 += ref_stride;
+        ref3 += ref_stride;
+
+        res[0] += horizontal_add_16x8(q0);
+        res[1] += horizontal_add_16x8(q1);
+        res[2] += horizontal_add_16x8(q2);
+        res[3] += horizontal_add_16x8(q3);
+      }
+      break;
+    }
+    case 128: {
+      for (int i = 0; i < height; i++) {
+        uint16x8_t q0 = vdupq_n_u16(0);
+        uint16x8_t q1 = vdupq_n_u16(0);
+        uint16x8_t q2 = vdupq_n_u16(0);
+        uint16x8_t q3 = vdupq_n_u16(0);
+
+        q4 = vld1q_u8(src);
+
+        sad_row16_neon(&q0, &q4, ref[0]);
+        sad_row16_neon(&q1, &q4, ref[1]);
+        sad_row16_neon(&q2, &q4, ref[2]);
+        sad_row16_neon(&q3, &q4, ref[3]);
+
+        q4 = vld1q_u8(src + 16);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 16);
+        sad_row16_neon(&q1, &q4, ref[1] + 16);
+        sad_row16_neon(&q2, &q4, ref[2] + 16);
+        sad_row16_neon(&q3, &q4, ref[3] + 16);
+
+        q4 = vld1q_u8(src + 32);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 32);
+        sad_row16_neon(&q1, &q4, ref[1] + 32);
+        sad_row16_neon(&q2, &q4, ref[2] + 32);
+        sad_row16_neon(&q3, &q4, ref[3] + 32);
+
+        q4 = vld1q_u8(src + 48);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 48);
+        sad_row16_neon(&q1, &q4, ref[1] + 48);
+        sad_row16_neon(&q2, &q4, ref[2] + 48);
+        sad_row16_neon(&q3, &q4, ref[3] + 48);
+
+        q4 = vld1q_u8(src + 64);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 64);
+        sad_row16_neon(&q1, &q4, ref[1] + 64);
+        sad_row16_neon(&q2, &q4, ref[2] + 64);
+        sad_row16_neon(&q3, &q4, ref[3] + 64);
+
+        q4 = vld1q_u8(src + 80);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 80);
+        sad_row16_neon(&q1, &q4, ref[1] + 80);
+        sad_row16_neon(&q2, &q4, ref[2] + 80);
+        sad_row16_neon(&q3, &q4, ref[3] + 80);
+
+        q4 = vld1q_u8(src + 96);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 96);
+        sad_row16_neon(&q1, &q4, ref[1] + 96);
+        sad_row16_neon(&q2, &q4, ref[2] + 96);
+        sad_row16_neon(&q3, &q4, ref[3] + 96);
+
+        q4 = vld1q_u8(src + 112);
+
+        sad_row16_neon(&q0, &q4, ref[0] + 112);
+        sad_row16_neon(&q1, &q4, ref[1] + 112);
+        sad_row16_neon(&q2, &q4, ref[2] + 112);
+        sad_row16_neon(&q3, &q4, ref[3] + 112);
+
+        src += src_stride;
+        ref0 += ref_stride;
+        ref1 += ref_stride;
+        ref2 += ref_stride;
+        ref3 += ref_stride;
+
+        res[0] += horizontal_add_16x8(q0);
+        res[1] += horizontal_add_16x8(q1);
+        res[2] += horizontal_add_16x8(q2);
+        res[3] += horizontal_add_16x8(q3);
+      }
+    }
+  }
+}
+
+#define sad_skip_MxN_neon(m, n)                                                \
+    void aom_sad_skip_##m##x##n##x4d_neon(const uint8_t *src, int src_stride, \
+                                          const uint8_t *const ref[4],        \
+                                          int ref_stride, uint32_t res[4]) {  \
+    aom_sadMxNx4d_neon(m, ((n) >> 1), src, 2 * src_stride, ref,                \
+                       2 * ref_stride, res);                                   \
+    res[0] <<= 1;                                                              \
+    res[1] <<= 1;                                                              \
+    res[2] <<= 1;                                                              \
+    res[3] <<= 1;                                                              \
+  }
+
+sad_skip_MxN_neon(32, 8);
+sad_skip_MxN_neon(32, 16);
+sad_skip_MxN_neon(32, 32);
+sad_skip_MxN_neon(32, 64);
+
+sad_skip_MxN_neon(64, 16);
+sad_skip_MxN_neon(64, 32);
+sad_skip_MxN_neon(64, 64);
+sad_skip_MxN_neon(64, 128);
+
+sad_skip_MxN_neon(128, 64);
+sad_skip_MxN_neon(128, 128);
+#undef sad_skip_MxN_neon
