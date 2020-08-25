9f9b9fb61581244b8f12404a41159b36bddbb51f
Vijay Sundaram <vijay.sundaram@intel.com>
2020-08-24 23:07:10 -0700

    Enhanced IBC Proposal for AV2 :
    Fixed Chroma reconstruction bug for YUV420 content.
    Improvements for faster execution.
    
    Change-Id: I24caee59fc4dd970eda2f6c20db421f92d788a87

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -13014,12 +13014,17 @@ static void rd_intrabc_extract_source_sb(MACROBLOCK *x, uint16_t *InputBlock,
   uint16_t height = block_size_high[bsize];
   uint16_t stride = x->plane[0].src.stride;
 
+  // uint16_t *pixelStartAddr = CONVERT_TO_SHORTPTR(x->plane[0].src.buf);
+
   for (int rows = 0; rows < height; ++rows) {
     for (int cols = 0; cols < width; ++cols) {
       uint16_t *pixelAddr =
           CONVERT_TO_SHORTPTR(x->plane[0].src.buf + rows * stride + cols);
       InputBlock[rows * width + cols] = *(pixelAddr);
     }
+    /*memcpy(InputBlock, pixelStartAddr, width << 1);
+        InputBlock += width;
+        pixelStartAddr += stride;*/
   }
 }
 
@@ -13033,6 +13038,9 @@ static void rd_intrabc_copy_sb(uint16_t *DstBlock, uint16_t *SrcBlock,
     for (int cols = 0; cols < width; ++cols) {
       DstBlock[rows * 128 + cols] = SrcBlock[rows * width + cols];
     }
+    /*memcpy(DstBlock, SrcBlock, width << 1);
+    DstBlock += (uint16_t)(128);
+    SrcBlock += width;*/
   }
 }
 
@@ -13202,53 +13210,46 @@ static int64_t rd_pick_intrabc_mode_sb(const AV1_COMP *cpi, MACROBLOCK *x,
 
 #if CONFIG_EXT_IBC_MODES
   uint16_t *src_block = NULL;
-  uint16_t *ibc_src_block = NULL;
 
   // Iterate through all IBC & IBC+ Modes
   // Allocate & Store Source block
   rd_intrabc_allocate_sb(&src_block, bsize);
   rd_intrabc_extract_source_sb(x, src_block, bsize);
 
-  // Allocate the IBC Source Buffer : Always 128x128, with valid data inset
-  rd_intrabc_allocate_sb(&ibc_src_block, BLOCK_128X128);
-
-  // Assign Macroblock pointer to IBC+ Translated Source
-  x->plane[0].ibc_src = ibc_src_block;
-
   for (IBC_MODE ibcMode = ROTATION_0; ibcMode <= ROTATION_270 /*ROTATION_90*/;
        ++ibcMode) {
     // Translate Source Block & Update Source Pointer for search
     switch (ibcMode) {
       case ROTATION_0:
-        rd_intrabc_copy_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_copy_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case MIRROR_90:
-        rd_intrabc_mirror90_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_mirror90_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case MIRROR_0:
-        rd_intrabc_mirror0_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_mirror0_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case ROTATION_180:
-        rd_intrabc_rotate180_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_rotate180_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case ROTATION_90:
-        rd_intrabc_rotate90_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_rotate90_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case MIRROR_135:
-        rd_intrabc_mirror135_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_mirror135_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case MIRROR_45:
-        rd_intrabc_mirror45_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_mirror45_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       case ROTATION_270:
-        rd_intrabc_rotate270_sb(ibc_src_block, src_block, bsize);
+        rd_intrabc_rotate270_sb(x->plane[0].ibc_src, src_block, bsize);
         break;
 
       default: break;
@@ -13422,12 +13423,20 @@ static int64_t rd_pick_intrabc_mode_sb(const AV1_COMP *cpi, MACROBLOCK *x,
                sizeof(x->blk_skip[0]) * xd->n4_h * xd->n4_w);
       }
     }
+
+    // Disable IBC+ Mode Search for all Shapes bigger than 32x32
+    /*if (ibcMode == ROTATION_0 &&
+        (bsize == BLOCK_128X128 || bsize == BLOCK_128X64 ||
+         bsize == BLOCK_64X128 || bsize == BLOCK_64X64 ||
+         bsize == BLOCK_64X32 || bsize == BLOCK_32X64 || bsize == BLOCK_64X16 ||
+         bsize == BLOCK_16X64)) {
+      break;
+    }*/
 #if CONFIG_EXT_IBC_MODES
   }
 
-  // DeAllocate source & IBC source blocks
+  // DeAllocate source block
   aom_free(src_block);
-  aom_free(ibc_src_block);
 #endif  // CONFIG_EXT_IBC_MODES
 
   *mbmi = best_mbmi;
