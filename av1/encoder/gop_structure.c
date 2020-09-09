f69a30037c30769a7380756cbe89cbb4b784d8fd
Sarah Parker <sarahparker@google.com>
2020-09-08 20:06:00 -0700

    Fix crash when all past frames are level 1
    The refresh rule was not set up to be able to refresh level 1
    frames unless we are also coding a level 1 frame. In some
    subgop configurations, it may be possible to have many level 1
    frames and be forced to refresh one of them. In that case, we refresh
    the oldest one.
    
    Change-Id: Id25744766fd8242afd524259f55932c4dfd57e15

--- av1/encoder/gop_structure.c
+++ av1/encoder/gop_structure.c
@@ -304,8 +304,10 @@ void av1_gop_setup_structure(AV1_COMP *cpi,
   FRAME_INFO *const frame_info = &cpi->frame_info;
   const int key_frame = (frame_params->frame_type == KEY_FRAME);
   const FRAME_UPDATE_TYPE first_frame_update_type =
-      key_frame ? KF_UPDATE
-                : rc->source_alt_ref_active ? OVERLAY_UPDATE : GF_UPDATE;
+      key_frame
+          ? KF_UPDATE
+          : cpi->common.n_altrefs && rc->source_alt_ref_active ? OVERLAY_UPDATE
+                                                               : GF_UPDATE;
   gf_group->is_user_specified = 0;
   gf_group->has_overlay_for_key_frame = 0;
   if (cpi->print_per_frame_stats) {
