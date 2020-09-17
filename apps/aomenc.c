8919a232c8473d01c8b9361053e794262f90d235
Wan-Teh Chang <wtc@google.com>
2020-09-17 12:32:40 -0700

    Clean up some calls to arg_match()
    In parse_global_config(), merge the very first arg_match() call into the
    if-else-if control structure that contains all the other arg_match()
    calls.
    
    In parse_stream_params(), we can break out of the for loop that
    traverses the ctrl_args array as soon as arg_match() returns 1. Also
    remove the null pointer check for ctrl_args_map. If the arg_match() call
    on ctrl_args[i] returns 1, ctrl_args_map cannot possibly be a null
    pointer. In case some static analyzer cannot figure this out, initialize
    ctrl_args_map to a dummy no_arg_ctrl_map array instead of NULL.
    
    Change-Id: I3fc32a43e9d4bb43f2dc2205895196e996c2e38b

--- apps/aomenc.c
+++ apps/aomenc.c
@@ -1044,6 +1044,7 @@ static const int av1_arg_ctrl_map[] = { AOME_SET_CPUUSED,
 #endif  // CONFIG_AV1_ENCODER
 
 static const arg_def_t *no_args[] = { NULL };
+static const int no_arg_ctrl_map[] = { 0 };
 
 static void show_help(FILE *fout, int shorthelp) {
   fprintf(fout, "Usage: %s <options> -o dst_filename src_filename \n",
@@ -1186,12 +1187,11 @@ static void parse_global_config(struct AvxEncoderConfig *global, char ***argv) {
     arg.argv_step = 1;
 
     if (arg_match(&arg, &use_cfg, argi)) {
-      if (cfg_included) continue;
-      parse_cfg(arg.val, &global->encoder_config);
-      cfg_included = 1;
-      continue;
-    }
-    if (arg_match(&arg, &help, argi)) {
+      if (!cfg_included) {
+        parse_cfg(arg.val, &global->encoder_config);
+        cfg_included = 1;
+      }
+    } else if (arg_match(&arg, &help, argi)) {
       show_help(stdout, 0);
       exit(EXIT_SUCCESS);
     } else if (arg_match(&arg, &codecarg, argi)) {
@@ -1439,7 +1439,7 @@ static int parse_stream_params(struct AvxEncoderConfig *global,
   char **argi, **argj;
   struct arg arg;
   static const arg_def_t **ctrl_args = no_args;
-  static const int *ctrl_args_map = NULL;
+  static const int *ctrl_args_map = no_arg_ctrl_map;
   struct stream_config *config = &stream->config;
   int eos_mark_found = 0;
   int webm_forced = 0;
@@ -1638,9 +1638,8 @@ static int parse_stream_params(struct AvxEncoderConfig *global,
       for (i = 0; ctrl_args[i]; i++) {
         if (arg_match(&arg, ctrl_args[i], argi)) {
           match = 1;
-          if (ctrl_args_map) {
-            set_config_arg_ctrls(config, ctrl_args_map[i], &arg);
-          }
+          set_config_arg_ctrls(config, ctrl_args_map[i], &arg);
+          break;
         }
       }
       if (!match) argj++;
