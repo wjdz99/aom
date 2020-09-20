c99c8e02eb599dc64e21c0afe785ad6ab3d10c1a
Wan-Teh Chang <wtc@google.com>
2020-09-19 17:35:12 -0700

    Remove the null pointer check for ctrl_args_map
    Change-Id: I4a8576695461c473cdf7be0526cd870428141198

--- apps/aomenc.c
+++ apps/aomenc.c
@@ -1044,6 +1044,7 @@ static const int av1_arg_ctrl_map[] = { AOME_SET_CPUUSED,
 #endif  // CONFIG_AV1_ENCODER
 
 static const arg_def_t *no_args[] = { NULL };
+static const int no_arg_ctrl_map[] = { 0 };
 
 static void show_help(FILE *fout, int shorthelp) {
   fprintf(fout, "Usage: %s <options> -o dst_filename src_filename \n",
@@ -1437,8 +1438,8 @@ static int parse_stream_params(struct AvxEncoderConfig *global,
                                struct stream_state *stream, char **argv) {
   char **argi, **argj;
   struct arg arg;
-  static const arg_def_t **ctrl_args = no_args;
-  static const int *ctrl_args_map = NULL;
+  const arg_def_t **ctrl_args = no_args;
+  const int *ctrl_args_map = no_arg_ctrl_map;
   struct stream_config *config = &stream->config;
   int eos_mark_found = 0;
   int webm_forced = 0;
@@ -1637,9 +1638,7 @@ static int parse_stream_params(struct AvxEncoderConfig *global,
       for (i = 0; ctrl_args[i]; i++) {
         if (arg_match(&arg, ctrl_args[i], argi)) {
           match = 1;
-          if (ctrl_args_map) {
-            set_config_arg_ctrls(config, ctrl_args_map[i], &arg);
-          }
+          set_config_arg_ctrls(config, ctrl_args_map[i], &arg);
           break;
         }
       }
