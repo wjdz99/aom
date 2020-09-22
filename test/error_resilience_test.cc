20567ab483fa841d83e93e662d6a27bc8abd711c
Jingning Han <jingning@google.com>
2020-09-21 22:28:51 -0700

    Temporarily disable SFrame unit test
    BUG=aomedia:2831
    
    Change-Id: I8b375909b15ec71ebd5a069e8f9814e58bab60d2

--- test/error_resilience_test.cc
+++ test/error_resilience_test.cc
@@ -526,7 +526,7 @@ class SFramePresenceTestLarge
   aom_s_frame_info sframe_info;
 };
 
-TEST_P(SFramePresenceTestLarge, SFramePresenceTest) {
+TEST_P(SFramePresenceTestLarge, DISABLED_SFramePresenceTest) {
   libaom_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                      cfg_.g_timebase.den, cfg_.g_timebase.num,
                                      0, 100);
