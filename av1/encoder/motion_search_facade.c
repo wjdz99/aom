b7fa9a8045b4f824b8c46fa294acf544ae0f874b
Jingning Han <jingning@google.com>
2020-10-10 14:23:20 -0700

    Allow jnt motion search
    Conduct motion search for both joint compound mode and the averge
    compound mode. Retain the effective motion vectors and the selected
    prediction mode for next stage rate-distortion optimizations.
    
    Change-Id: I9555515db818a42cdd30cf4a5e510847c9d55abe

--- av1/encoder/motion_search_facade.c
+++ av1/encoder/motion_search_facade.c
@@ -318,7 +318,7 @@ void av1_single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
                              mv_costs->mv_cost_stack, MV_COST_WEIGHT);
 }
 
-void av1_joint_motion_search(const AV1_COMP *cpi, MACROBLOCK *x,
+int av1_joint_motion_search(const AV1_COMP *cpi, MACROBLOCK *x,
                              BLOCK_SIZE bsize, int_mv *cur_mv,
                              const uint8_t *mask, int mask_stride,
                              int *rate_mv) {
@@ -494,6 +494,8 @@ void av1_joint_motion_search(const AV1_COMP *cpi, MACROBLOCK *x,
                                 mv_costs->nmv_joint_cost,
                                 mv_costs->mv_cost_stack, MV_COST_WEIGHT);
   }
+
+  return AOMMIN(last_besterr[0], last_besterr[1]);
 }
 
 // Search for the best mv for one component of a compound,
