a55df6ce6e0bc5750b32d73a2cc2d6352191b73f
Aniket Wanare <Aniket.wanare@ittiam.com>
2020-11-12 21:22:49 +0530

    Add AVX2 for av1_filter_intra_predictor_c
    This CL adds support for AVX2 intrinsics for the function
    av1_filter_intra_predictor_c().
    
    Change-Id: If901016ca7437875aa14ffa4c62846c0fdeac83d

--- av1/common/x86/filterintra_sse4.c
+++ av1/common/x86/filterintra_sse4.c
@@ -27,9 +27,7 @@ void av1_filter_intra_predictor_sse4_1(uint8_t *dst, ptrdiff_t stride,
 
   assert(bw <= 32 && bh <= 32);
 
-  // The initialization is just for silencing Jenkins static analysis warnings
-  for (r = 0; r < bh + 1; ++r)
-    memset(buffer[r], 0, (bw + 1) * sizeof(buffer[0][0]));
+  // Removed initialization of buffer, not needed
 
   for (r = 0; r < bh; ++r) buffer[r + 1][0] = left[r];
   memcpy(buffer[0], &above[-1], (bw + 1) * sizeof(uint8_t));
