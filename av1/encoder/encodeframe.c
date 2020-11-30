a7082ba92a857802a3aba6a4f555f268240f8003
Marco Paniconi <marpan@google.com>
2020-11-29 22:05:39 -0800

    rtc: Allow for compound modes for nonrd_pickmode
    For nonrd mode (speed >= 7): add LAST_GOLDEN compound
    average mode for 0_0/NEAREST_NEAREST/NEAR_NEAR,
    
    avg. bdrate gain rtc set: ~5.3/5/3.5% for speed 7/8/9.
    avg. bdrate gain rtc_derf: ~0.5% for speed 7.
    
    Speed slowdown of ~10-15% on 360p/480p clips.
    
    Keep the speed feature (use_comp_ref_nonrd) off for now
    (it's already off as default).
    
    Change-Id: I4cb6687427535eb05b0a2e0d2e5192b4ce41ce90

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1744,7 +1744,8 @@ void av1_encode_frame(AV1_COMP *cpi) {
   (void)num_planes;
 #endif
 
-  if (cpi->sf.hl_sf.frame_parameter_update) {
+  if (cpi->sf.hl_sf.frame_parameter_update ||
+      cpi->sf.rt_sf.use_comp_ref_nonrd) {
     RD_COUNTS *const rdc = &cpi->td.rd_counts;
 
     if (frame_is_intra_only(cm))
@@ -1788,6 +1789,10 @@ void av1_encode_frame(AV1_COMP *cpi) {
         features->tx_mode = TX_MODE_LARGEST;
     }
   } else {
+    // This is needed if real-time speed setting is changed on the fly
+    // from one using compound prediction to one using single reference.
+    if (current_frame->reference_mode == REFERENCE_MODE_SELECT)
+      current_frame->reference_mode = SINGLE_REFERENCE;
     encode_frame_internal(cpi);
   }
 }
