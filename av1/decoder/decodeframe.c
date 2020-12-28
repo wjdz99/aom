e5f1aca6ebd31dcaab8794a048fea8ed4c67e638
Yue Chen <yuec@google.com>
2020-12-28 14:54:25 -0800

    Add is_coded_map to record blocks' status
    Since it will be hard to figure out an explicit algorithm for
    checking whether the top right block is available once flexible
    partitions are enabled, we maintain an array is_mi_coded to record
    at the superblock level whether an mi(in 4x4 granularity) has been
    coded. The map is maintained separately at decoder, encoder, and in
    RDO.
    No performance change should be expected.
    
    Change-Id: Ic4c878bd59dd05c6c68762bf66213a142f19448a

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -987,6 +987,7 @@ static AOM_INLINE void decode_token_recon_block(AV1Decoder *const pbi,
   }
 
   av1_visit_palette(pbi, xd, r, set_color_index_map_offset);
+  av1_mark_block_as_coded(xd, bsize, cm->seq_params.sb_size);
 }
 
 static AOM_INLINE void set_inter_tx_size(MB_MODE_INFO *mbmi, int stride_log2,
@@ -1229,6 +1230,7 @@ static AOM_INLINE void parse_decode_block(AV1Decoder *const pbi,
   if (mbmi->skip_txfm) av1_reset_entropy_context(xd, bsize, num_planes);
 
   decode_token_recon_block(pbi, td, r, bsize);
+  av1_mark_block_as_coded(xd, bsize, cm->seq_params.sb_size);
 }
 
 static AOM_INLINE void set_offsets_for_pred_and_recon(AV1Decoder *const pbi,
@@ -2649,6 +2651,7 @@ static AOM_INLINE void decode_tile_sb_row(AV1Decoder *pbi, ThreadData *const td,
 
   for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;
        mi_col += cm->seq_params.mib_size, sb_col_in_tile++) {
+    av1_reset_is_mi_coded_map(&td->dcb.xd, cm->seq_params.mib_size);
     set_cb_buffer(pbi, &td->dcb, pbi->cb_buffer_base, num_planes, mi_row,
                   mi_col);
 
@@ -2731,6 +2734,7 @@ static AOM_INLINE void decode_tile(AV1Decoder *pbi, ThreadData *const td,
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;
          mi_col += cm->seq_params.mib_size) {
+      av1_reset_is_mi_coded_map(xd, cm->seq_params.mib_size);
       set_cb_buffer(pbi, dcb, &td->cb_buffer_base, num_planes, 0, 0);
 
       // Bit-stream parsing and decoding of the superblock
@@ -3168,6 +3172,7 @@ static AOM_INLINE void parse_tile_row_mt(AV1Decoder *pbi, ThreadData *const td,
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;
          mi_col += cm->seq_params.mib_size) {
+      av1_reset_is_mi_coded_map(xd, cm->seq_params.mib_size);
       set_cb_buffer(pbi, dcb, pbi->cb_buffer_base, num_planes, mi_row, mi_col);
 
       // Bit-stream parsing of the superblock
