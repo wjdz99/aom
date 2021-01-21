76a3830568c0b3e663be40786a0c0b713b03b0b5
Sarah Parker <sarahparker@google.com>
2021-01-20 16:42:42 -0800

    Reduce number of thresholds in NEW_REF_SIGNALING
    Assign a threshold per prediction mode rather than
    prediction mode + reference frame combinations. This simplifies
    the framework and moves away from the restrictions currently
    imposed on reference frame assignment. Noise range change in performance
    on lowres2 speeds 0, 1, 2, 3.
    
    BUG=aomedia:2931
    STATS_CHANGED
    
    Change-Id: I4a44acde6b6f19cf31f215fa3880ae4db9809463

--- av1/encoder/context_tree.h
+++ av1/encoder/context_tree.h
@@ -49,7 +49,7 @@ typedef struct PICK_MODE_CONTEXT {
   // For current partition, only if all Y, U, and V transform blocks'
   // coefficients are quantized to 0, skippable is set to 1.
   int skippable;
-#if CONFIG_INTERNAL_STATS
+#if CONFIG_INTERNAL_STATS && !CONFIG_NEW_REF_SIGNALING
   THR_MODES best_mode_index;
 #endif  // CONFIG_INTERNAL_STATS
   int hybrid_pred_diff;
