8f195c2179caba74a6e3531f480ca740a6947ef1
leolzhao <leolzhao@tencent.com>
2021-01-28 11:00:33 -0800

    Encoder bugfix for SDP
    When SDP is applied, only the luma blocks should be used to determine the luma block sizes. However, currently, all YUV blocks are accidentally involved in the luma block partition decision. This pull request is aiming to fix this bug.
    
    Change-Id: I3cc1b9fdf65ae8e074ee29989f1d76412502c4fb
    (cherry picked from commit cbb97825587b3d33724ebd3faf249594c2b418e3)

--- av1/common/cfl.h
+++ av1/common/cfl.h
@@ -27,6 +27,19 @@ static INLINE CFL_ALLOWED_TYPE is_cfl_allowed(const MACROBLOCKD *xd) {
   const BLOCK_SIZE bsize = mbmi->sb_type;
 #endif
   assert(bsize < BLOCK_SIZES_ALL);
+#if CONFIG_SDP
+  const int ssx = xd->plane[AOM_PLANE_U].subsampling_x;
+  const int ssy = xd->plane[AOM_PLANE_U].subsampling_y;
+  const int plane_bsize = get_plane_block_size(bsize, ssx, ssy);
+  if (xd->lossless[mbmi->segment_id]) {
+    // In lossless, CfL is available when the partition size is equal to the
+    // transform size.
+    return (CFL_ALLOWED_TYPE)(plane_bsize == BLOCK_4X4);
+  }
+  // Spec: CfL is available to luma partitions lesser than or equal to 32x32
+  return (CFL_ALLOWED_TYPE)(block_size_wide[bsize] <= 32 &&
+                            block_size_high[bsize] <= 32);
+#else
   if (xd->lossless[mbmi->segment_id]) {
     // In lossless, CfL is available when the partition size is equal to the
     // transform size.
@@ -38,6 +51,7 @@ static INLINE CFL_ALLOWED_TYPE is_cfl_allowed(const MACROBLOCKD *xd) {
   // Spec: CfL is available to luma partitions lesser than or equal to 32x32
   return (CFL_ALLOWED_TYPE)(block_size_wide[bsize] <= 32 &&
                             block_size_high[bsize] <= 32);
+#endif
 }
 
 // Do we need to save the luma pixels from the current block,
