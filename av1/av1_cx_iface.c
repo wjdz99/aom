0e43fae89eabb629ca2a6c55fd093cbf9cea0b0d
venkat sanampudi <sanampudi.venkatarao@ittiam.com>
2021-02-04 12:08:07 +0530

    Optimize bit stream buffer allocation size
    Bit stream buffer size is calculated based on
    maximum number of no-show frames possible.
    
    BUG=aomedia:2938
    
    Change-Id: I5866c42221b7b7f49ebf6840428589b3a4d78cd9

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -2129,8 +2129,28 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
   if (img != NULL) {
     res = validate_img(ctx, img);
     if (res == AOM_CODEC_OK) {
-      size_t data_sz = ALIGN_POWER_OF_TWO(ctx->cfg.g_w, 5) *
-                       ALIGN_POWER_OF_TWO(ctx->cfg.g_h, 5) * get_image_bps(img);
+      // Worst case buffer size needed to hold bit stream corresponding
+      // to one frame.
+      const size_t frame_sz = ALIGN_POWER_OF_TWO(ctx->cfg.g_w, 5) *
+                              ALIGN_POWER_OF_TWO(ctx->cfg.g_h, 5) *
+                              get_image_bps(img) / 8;
+
+      // Due to the presence of no-show frames, bit stream buffer constitutes
+      // data corresponding to multiple frames. Hence bit stream buffer needs
+      // to be allocated based on maximum number of successive no-show frames
+      // possible. The maximum number of successive no-show frames is derived
+      // as below.
+      // 1) When lag_in_frames <= 1, alt-ref frames are not enabled. Hence
+      // maximum number of successive no-show frames is zero.
+      // 2) For lag_in_frames > 1, pyramid height and lag_in_frames can be used
+      // to decide the maximum number of successive no-show frames.
+      const int max_successive_no_show_frames_plus_1 =
+          ctx->cpi->oxcf.gf_cfg.lag_in_frames <= 1
+              ? 1
+              : AOMMIN(ctx->cpi->oxcf.gf_cfg.lag_in_frames,
+                       ctx->cpi->oxcf.gf_cfg.gf_max_pyr_height + 1);
+      size_t data_sz = frame_sz * max_successive_no_show_frames_plus_1;
+
       if (data_sz < kMinCompressedSize) data_sz = kMinCompressedSize;
       if (ctx->cx_data == NULL || ctx->cx_data_sz < data_sz) {
         ctx->cx_data_sz = data_sz;
