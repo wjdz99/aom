1f1c9e932f63cde0fde9bf871a4b146cea5e82b5
chiyotsai <chiyotsai@google.com>
2021-02-05 18:27:15 -0800

    FLEX_MVRES: Perform exhaustive search for sb_mv_precision
    BUG=aomedia:2958
    
    Change-Id: I0f4f08663c171eb268fcacb95cef709cef50d7f5

--- av1/encoder/mv_prec.c
+++ av1/encoder/mv_prec.c
@@ -457,6 +457,6 @@ void av1_pick_and_set_high_precision_mv(AV1_COMP *cpi, int qindex) {
   av1_set_high_precision_mv(cpi, prec);
 
 #if CONFIG_FLEX_MVRES
-  cpi->common.features.use_sb_mv_precision = 0;
+  cpi->common.features.use_sb_mv_precision = 1;
 #endif  // CONFIG_FLEX_MVRES
 }
