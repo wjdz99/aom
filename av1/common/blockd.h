a2ab1427761e01047736b743952e95b6f9cb8886
Hui Su <huisu@google.com>
2021-02-13 11:53:09 -0800

    Introduce the ref_mv_bank experiment
    Store the MVs in buffers (named as ref mv banks) as each superblock is
    encoded, and blocks can refer to these MVs when there are open slots
    in the ref mv stack. There are two types of banks, corresponding to SB
    rows and columns, respectively. MVs in a SB are stored in both the row
    bank and the corresponding column bank.
    
    In the example below, superblock SB_2 can refer to row bank rb_0 and
    column bank cb_0.
    
              cb_0        cb_1
              SB_0        SB_1
    rb_0      SB_2        SB_3
    
    Coding gains for random access:
    a3  0.30%
    a4  0.34%
    a5  0.76%
    
    Change-Id: I8644116c10ffea2a847ec2de302e4f18a47d9477

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -877,6 +877,15 @@ struct scale_factors;
 
 /*!\endcond */
 
+#if CONFIG_REF_MV_BANK
+#define REF_MV_BANK_SIZE 4
+typedef struct {
+  int rmb_queue_count[MODE_CTX_REF_FRAMES];
+  int rmb_queue_start_idx[MODE_CTX_REF_FRAMES];
+  CANDIDATE_MV rmb_queue_buffer[MODE_CTX_REF_FRAMES][REF_MV_BANK_SIZE];
+} REF_MV_BANK;
+#endif  // CONFIG_REF_MV_BANK
+
 /*! \brief Variables related to current coding block.
  *
  * This is a common set of variables used by both encoder and decoder.
@@ -896,6 +905,15 @@ typedef struct macroblockd {
    * Same as cm->mi_params.mi_stride, copied here for convenience.
    */
   int mi_stride;
+
+#if CONFIG_REF_MV_BANK
+  REF_MV_BANK ref_mv_bank_left;
+  // TODO(anyone): 32 is enough to support frame width up to 32 * 128 = 4096.
+  // Ideally this should be allocated dynamically based on frame size.
+  REF_MV_BANK ref_mv_bank_above[32];
+  REF_MV_BANK *ref_mv_bank_left_pt;
+  REF_MV_BANK *ref_mv_bank_above_pt;
+#endif  // CONFIG_REF_MV_BANK
 
   /*!
    * True if current block transmits chroma information.
