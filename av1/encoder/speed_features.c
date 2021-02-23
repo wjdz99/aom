dcd1b989f4400a4eb441c5b25f521271a247664b
Jingning Han <jingning@google.com>
2021-02-23 11:02:05 -0800

    Allow non-square block size at smaller cb level
    Disable non-square block size in partition at large coding block
    level in speed 6. The all intra mode compression loss is 0.66% in
    PSNR and 0.86% in SSIM. The encoding speed up is about 25%.
    
    BUG=aomedia:2959
    
    STATS_CHANGED
    
    Change-Id: I57febe43a1de339d665b9d5a3507f1fd9659cbb3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -274,8 +274,6 @@ static void set_allintra_speed_feature_framesize_dependent(
 
     // TODO(yunqing): use BLOCK_32X32 for >= 4k.
     if (is_4k_or_larger) {
-      sf->part_sf.use_square_partition_only_threshold = BLOCK_64X64;
-    } else if (is_720p_or_larger) {
       sf->part_sf.use_square_partition_only_threshold = BLOCK_32X32;
     } else {
       sf->part_sf.use_square_partition_only_threshold = BLOCK_16X16;
