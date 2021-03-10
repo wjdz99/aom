c72245d7c860e3534be615c3770ea17261d371f4
Vishnu Teja Manyam <vishnu.teja@ittiam.com>
2021-03-10 20:34:55 +0530

    Modify memory allocations in cdef frame to facilitate MT
    This CL modifies the memory allocation of colbuf and srcbuf
    to suit row-level multi-threading.
    
    Change-Id: I3761cf4ea23ffd0ea83fefd4ba68c841b2e02330

--- av1/common/alloccommon.c
+++ av1/common/alloccommon.c
@@ -55,10 +55,16 @@ void av1_free_ref_frame_buffers(BufferPool *pool) {
 void av1_free_cdef_linebuf(AV1_COMMON *const cm) {
   const int num_planes = av1_num_planes(cm);
 
+  if (cm->cdef_info.srcbuf != NULL) aom_free(cm->cdef_info.srcbuf);
+  cm->cdef_info.srcbuf = NULL;
   for (uint8_t plane = 0; plane < num_planes; plane++) {
     if (cm->cdef_info.linebuf[plane] != NULL)
       aom_free(cm->cdef_info.linebuf[plane]);
     cm->cdef_info.linebuf[plane] = NULL;
+
+    if (cm->cdef_info.colbuf[plane] != NULL)
+      aom_free(cm->cdef_info.colbuf[plane]);
+    cm->cdef_info.colbuf[plane] = NULL;
   }
 }
 
@@ -83,6 +89,21 @@ void av1_alloc_cdef_linebuf(AV1_COMMON *const cm) {
                       aom_malloc(sizeof(*cm->cdef_info.linebuf) * num_bufs *
                                  (CDEF_VBORDER << 1) * stride));
     }
+  }
+  if (cm->cdef_info.srcbuf == NULL)
+    CHECK_MEM_ERROR(
+        cm, cm->cdef_info.srcbuf,
+        aom_malloc(sizeof(*cm->cdef_info.srcbuf) * CDEF_INBUF_SIZE));
+
+  for (uint8_t plane = 0; plane < num_planes; plane++) {
+    const int shift = plane == AOM_PLANE_Y ? 0 : cm->seq_params.subsampling_x;
+    const int block_height =
+        (CDEF_BLOCKSIZE << (MI_SIZE_LOG2 - shift)) * 2 * CDEF_VBORDER;
+
+    if (cm->cdef_info.colbuf[plane] == NULL)
+      CHECK_MEM_ERROR(cm, cm->cdef_info.colbuf[plane],
+                      aom_malloc(sizeof(*cm->cdef_info.colbuf[plane]) *
+                                 block_height * CDEF_HBORDER));
   }
   cm->cdef_info.allocated_mi_cols = cm->mi_params.mi_cols;
   cm->cdef_info.allocated_mi_rows = cm->mi_params.mi_rows;
