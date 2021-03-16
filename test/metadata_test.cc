f82b2da9c1fb4df43dca90f2ff347b09b013e712
Jerome Jiang <jianj@google.com>
2021-03-15 17:46:50 -0700

    Exclude non RT tests from realtime only build
    Bug: aomedia:3001
    
    Change-Id: I394d53de70239bc55894d68649f899965fa8b149

--- test/metadata_test.cc
+++ test/metadata_test.cc
@@ -34,7 +34,7 @@ const size_t kMetadataPayloadSizeCll = 4;
 const uint8_t kMetadataPayloadCll[kMetadataPayloadSizeCll] = { 0xB5, 0x01, 0x02,
                                                                0x03 };
 
-#if CONFIG_AV1_ENCODER
+#if CONFIG_AV1_ENCODER && !CONFIG_REALTIME_ONLY
 
 const size_t kMetadataObuSizeT35 = 28;
 const uint8_t kMetadataObuT35[kMetadataObuSizeT35] = {
