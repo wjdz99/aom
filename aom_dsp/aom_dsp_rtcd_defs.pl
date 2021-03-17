43e74e650cf1cd6035e710b3f6f3255b9fbd5ce9
Yue Chen <yuec@google.com>
2021-03-16 17:53:03 -0700

    Disable more simd hbd functions for ERP
    Change-Id: I6a00b6c40cfd9a0762d2c907525f38d80a4508bb

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -378,7 +378,9 @@ specialize qw/aom_convolve8_horiz     sse2 ssse3/, "$avx2_ssse3";
 specialize qw/aom_convolve8_vert      sse2 ssse3/, "$avx2_ssse3";
 
 add_proto qw/void aom_highbd_convolve_copy/, "const uint16_t *src, ptrdiff_t src_stride, uint16_t *dst, ptrdiff_t dst_stride, int w, int h";
-specialize qw/aom_highbd_convolve_copy sse2 avx2/;
+if (aom_config("CONFIG_EXT_RECUR_PARTITIONS") eq "") {
+  specialize qw/aom_highbd_convolve_copy sse2 avx2/;
+} # CONFIG_EXT_RECUR_PARTITIONS
 
 add_proto qw/void aom_highbd_convolve8_horiz/, "const uint8_t *src, ptrdiff_t src_stride, uint8_t *dst, ptrdiff_t dst_stride, const int16_t *filter_x, int x_step_q4, const int16_t *filter_y, int y_step_q4, int w, int h, int bd";
 specialize qw/aom_highbd_convolve8_horiz sse2 avx2/;
