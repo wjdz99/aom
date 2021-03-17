56b1a83b7c7755b973166bf93d7e4a036d93ed85
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2021-03-17 13:33:18 +0530

    Abstract gf_group to AV1_PRIMARY
    Moved gf_group from cpi to ppi.
    
    Change-Id: I0c5b3ae15a01d23543d561910428053f8bc6cb50

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -2087,6 +2087,11 @@ typedef struct AV1_PRIMARY {
    * When set, indicates that internal ARFs are enabled.
    */
   int internal_altref_allowed;
+
+  /*!
+   * Information related to a gf group.
+   */
+  GF_GROUP gf_group;
 } AV1_PRIMARY;
 
 /*!
@@ -2350,11 +2355,6 @@ typedef struct AV1_COMP {
    * Information related to two pass encoding.
    */
   TWO_PASS twopass;
-
-  /*!
-   * Information related to a gf group.
-   */
-  GF_GROUP gf_group;
 
   /*!
    * The frame processing order within a GOP.
@@ -2956,7 +2956,7 @@ ticks_to_timebase_units(const aom_rational64_t *timestamp_ratio, int64_t n) {
 }
 
 static INLINE int frame_is_kf_gf_arf(const AV1_COMP *cpi) {
-  const GF_GROUP *const gf_group = &cpi->gf_group;
+  const GF_GROUP *const gf_group = &cpi->ppi->gf_group;
   const FRAME_UPDATE_TYPE update_type =
       gf_group->update_type[cpi->cur_gf_index];
 
