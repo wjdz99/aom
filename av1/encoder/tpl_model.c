2f058291bbc0c588a82532f650fdc707cade2d4c
Angie Chiang <angiebird@google.com>
2021-03-25 17:58:22 -0700

    Refactor av1_compute_rd_mult_based_on_qindex
    Replace cpi by bit_depth.
    
    Change-Id: I4d0620c7c7e42e2812155a0781117f460393bf58

--- av1/encoder/tpl_model.c
+++ av1/encoder/tpl_model.c
@@ -1160,7 +1160,9 @@ static AOM_INLINE void init_mc_flow_dispenser(AV1_COMP *cpi, int frame_idx,
   av1_frame_init_quantizer(cpi);
 
   tpl_frame->base_rdmult =
-      av1_compute_rd_mult_based_on_qindex(cpi, pframe_qindex) / 6;
+      av1_compute_rd_mult_based_on_qindex(cpi->common.seq_params.bit_depth,
+                                          pframe_qindex) /
+      6;
 
   memset(tpl_txfm_stats, 0, sizeof(*tpl_txfm_stats));
 }
