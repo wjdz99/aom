7879f4a1bf293adac90e120fab87335dfb0d5105
Angie Chiang <angiebird@google.com>
2021-04-05 14:25:17 -0700

    Estimate coeff cost based on transform stats
    av1_est_coeff_entropy(): estimate coeff cost
    
    av1_est_txfm_block_entropy(): estimate transform block cost
    
    av1_laplace_prob(): compute the probability of qcoeff w.r.t. a
    Laplace distribution
    
    Change-Id: I3a6fd87030f7dda19e506e1371a3c365e73c7ed2

---
+++ test/tpl_model_test.cc
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#include <cstdlib>
+
+#include "av1/encoder/tpl_model.h"
+
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+
+TEST(TplModelTest, TransformCoeffEntropyTest1) {
+  // Check the consistency between av1_est_coeff_entropy() and
+  // av1_laplace_prob()
+  double b = 1;
+  double q_step = 1;
+  double zero_bin_ratio = 2;
+  for (int qcoeff = -256; qcoeff < 256; ++qcoeff) {
+    double rate = av1_est_coeff_entropy(q_step, b, zero_bin_ratio, qcoeff);
+    double prob = av1_laplace_prob(q_step, b, zero_bin_ratio, qcoeff);
+    double ref_rate = -log2(prob);
+    EXPECT_DOUBLE_EQ(rate, ref_rate);
+  }
+}
+
+TEST(TplModelTest, TransformCoeffEntropyTest2) {
+  // Check the consistency between av1_est_coeff_entropy(), av1_laplace_prob()
+  // and
+  double b = 1;
+  double q_step = 1;
+  double zero_bin_ratio = 2;
+  double est_expected_rate = 0;
+  for (int qcoeff = -20; qcoeff < 20; ++qcoeff) {
+    double rate = av1_est_coeff_entropy(q_step, b, zero_bin_ratio, qcoeff);
+    double prob = av1_laplace_prob(q_step, b, zero_bin_ratio, qcoeff);
+    est_expected_rate += prob * rate;
+  }
+  double expected_rate = av1_laplace_entropy(q_step, b, zero_bin_ratio);
+  EXPECT_NEAR(expected_rate, est_expected_rate, 0.001);
+}
