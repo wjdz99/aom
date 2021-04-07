fbdf4010aef328f1ce445582fa742f28c082ea7b
Bohan Li <bohanli@google.com>
2021-04-07 13:29:00 -0700

    Perform first pass analysis in av1_init_second_pass.
    Now estimated noise variances and correlation coefficients of the frames are
    available for all twopass stats, right after second pass is initialized.
    
    Change-Id: I9e34784e5e8ce4373ef6834c497a3a99bfb4e968

--- av1/encoder/firstpass.c
+++ av1/encoder/firstpass.c
@@ -108,6 +108,9 @@ void av1_twopass_zero_stats(FIRSTPASS_STATS *section) {
   section->new_mv_count = 0.0;
   section->count = 0.0;
   section->duration = 1.0;
+  section->is_flash = 0;
+  section->noise_var = 0;
+  section->cor_coeff = 1.0;
 }
 
 void av1_accumulate_stats(FIRSTPASS_STATS *section,
