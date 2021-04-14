a9719337b1214e18c51eb4c16ba796a5045840b7
Sarah Parker <sarahparker@google.com>
2021-04-14 11:49:56 -0700

    Add field for new ref frames in mbmi
    This adds a member to mbmi for references using the new indexing
    system and sets it every time mbmi->ref_frame is modified. It
    will enable the conversion of all uses of ref_frame[0]/ref_frame[1]
    to the new indexing system.
    
    Change-Id: Iab0f331a5e3bf63fc2130d0420327d6680d5462f

--- av1/common/pred_common.h
+++ av1/common/pred_common.h
@@ -68,6 +68,21 @@ void av1_get_ref_frames(AV1_COMMON *const cm, int cur_frame_disp,
 void av1_init_new_ref_frame_map(AV1_COMMON *const cm,
                                 RefFrameMapPair *ref_frame_map_pairs,
                                 int cur_frame_disp);
+
+static INLINE int convert_named_ref_to_ranked_ref_index(const NewRefFramesData *const ref_frame_data, int named_idx) {
+  // NONE_FRAME and INVALID_IDX are both equal to -1, simplify by using only
+  // INVALID_IDX for this purpose
+  if (named_idx == NONE_FRAME) return INVALID_IDX;
+  if (named_idx == INVALID_IDX) return INVALID_IDX;
+  if (named_idx == INTRA_FRAME) return INTRA_FRAME_NRS;
+  return ref_frame_data->named_to_ranked_refs[named_idx];
+}
+
+static INLINE int convert_ranked_ref_to_named_ref_index(const NewRefFramesData *const ref_frame_data, int ranked_idx) {
+  if (ranked_idx == INVALID_IDX) return INVALID_IDX;
+  if (ranked_idx == INTRA_FRAME_NRS) return INTRA_FRAME;
+  return ref_frame_data->ranked_to_named_refs[ranked_idx];
+}
 #endif  // NEW_REF_SIGNALING
 
 static INLINE int get_segment_id(const CommonModeInfoParams *const mi_params,
