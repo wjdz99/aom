b272031c352ba56ace1553b0ee803dd152a88052
Yunqing Wang <yunqingwang@google.com>
2021-04-21 15:58:38 -0700

    wip: disable angle delta
    Change-Id: I41f9143aca9ef5ee470626c97639840849320384

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -399,7 +399,7 @@ static struct av1_extracfg default_extra_cfg = {
   1,                       // enable overlay
   1,                       // enable palette
   !CONFIG_SHARP_SETTINGS,  // enable intrabc
-  1,                       // enable angle delta
+  0,                       // enable angle delta
 #if CONFIG_DENOISE
   0,                       // noise_level
   32,                      // noise_block_size
