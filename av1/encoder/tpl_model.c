26e9ed7947b22d1eb892709627d57fe1faf5c208
Kavi Ramamurthy <kavii@google.com>
2021-05-07 17:02:05 +0000

    Add av1_estimate_gop_bitrate to tpl_model.
    Change-Id: Ic0f2ea5809b027c80fad5510c9739cd5926947a0

--- av1/encoder/tpl_model.c
+++ av1/encoder/tpl_model.c
@@ -1753,6 +1753,38 @@ double av1_laplace_estimate_frame_rate(int q_index, int block_count,
   return est_rate;
 }
 
+double av1_estimate_gop_bitrate(const unsigned char *q_index_list,
+                                const int frame_count,
+                                const TplTxfmStats *stats_list) {
+  double gop_bitrate = 0;
+  for (int frame_index = 0; frame_index < frame_count; frame_index++) {
+    int q_index = q_index_list[frame_index];
+    TplTxfmStats *frame_stats = &stats_list[frame_index];
+
+    /* Convert to mean absolute deviation */
+    double mu = 0;
+    for (int i = 0; i < frame_count; i++) {
+      mu += frame_stats->abs_coeff_sum[i];
+    }
+    mu /= frame_count;
+    const int size = 256;
+    double abs_coeff_mean[256];
+    for (int i = 0; i < size; i++) {
+      double sum = frame_stats->abs_coeff_sum[i];
+      if ((sum - mu) == 0) {
+        abs_coeff_mean[i] = 0;
+        continue;
+      }
+      abs_coeff_mean[i] = (sum - mu) / size;
+    }
+
+    double frame_bitrate = av1_laplace_estimate_frame_rate(
+        q_index, frame_stats->txfm_block_count, abs_coeff_mean, 16);
+    gop_bitrate += frame_bitrate;
+  }
+  return gop_bitrate;
+}
+
 double av1_estimate_coeff_entropy(double q_step, double b,
                                   double zero_bin_ratio, int qcoeff) {
   int abs_qcoeff = abs(qcoeff);
