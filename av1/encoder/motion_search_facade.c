51a4c202114848c0c169e8d19ce16501f3dbc6f5
Yunqing Wang <yunqingwang@google.com>
2021-05-11 23:02:48 -0700

    wip: pred_modes mask
    ps4: increase max mv used from 2 to 3 in av1_mv_pred()
    ps5: Only do newmv search for best_mv_idx
    ps6: do newmv search for ref_mv_idx = 0 or best_mv_idx
    
    ps7: ps4 + if ref_mv_stack < 3 MVs, allow 1 more global_mv tested.
    ps8: ps4 + always test global_mv
    ps9: ps8 + use best_mv_idx mv as start_mv in motion search
    
    STATS_CHANGED
    
    Change-Id: Iac79d252b33fe91d1d69112cced4228536704d95

--- av1/encoder/motion_search_facade.c
+++ av1/encoder/motion_search_facade.c
@@ -162,10 +162,19 @@ void av1_single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
 
   const MV ref_mv = av1_get_ref_mv(x, ref_idx).as_mv;
   FULLPEL_MV start_mv;
-  if (mbmi->motion_mode != SIMPLE_TRANSLATION)
+  if (mbmi->motion_mode != SIMPLE_TRANSLATION) {
     start_mv = get_fullmv_from_mv(&mbmi->mv[0].as_mv);
-  else
-    start_mv = get_fullmv_from_mv(&ref_mv);
+  } else {
+//    start_mv = get_fullmv_from_mv(&ref_mv);
+    const int len = AOMMIN(MAX_MV_REF_CANDIDATES + 1, x->mbmi_ext.ref_mv_count[ref]);
+    MV mv_found;
+    if (x->best_mv_idx[ref] >= len)
+      mv_found = x->mbmi_ext.global_mvs[ref].as_mv;
+    else
+      // use ref_mv with index = x->best_mv_idx[ref] instead of index = mbmi->ref_mv_idx.
+      mv_found = av1_get_ref_mv_from_stack(ref_idx, mbmi->ref_frame, x->best_mv_idx[ref], &x->mbmi_ext).as_mv;
+    start_mv = get_fullmv_from_mv(&mv_found);
+  }
 
   // cand stores start_mv and all possible MVs in a SB.
   cand_mv_t cand[MAX_TPL_BLK_IN_SB * MAX_TPL_BLK_IN_SB + 1] = { { { 0, 0 },
@@ -235,6 +244,11 @@ void av1_single_motion_search(const AV1_COMP *const cpi, MACROBLOCK *x,
       break;
     default: assert(0 && "Invalid motion mode!\n");
   }
+
+//  if (mbmi->motion_mode== SIMPLE_TRANSLATION && (x->best_mv_idx[ref] >= x->mbmi_ext.ref_mv_count[ref]))
+//  printf("\n!!!!! mbmi->ref_mv_idx = %d; ref: %d; ref_mv(%d, %d); start_mv(%d, %d)(full), best_mv(%d, %d)(full), step_param: %d;\n ",
+//         mbmi->ref_mv_idx, ref, ref_mv.row, ref_mv.col, start_mv.row, start_mv.col, best_mv->as_fullmv.row, best_mv->as_fullmv.col, step_param);
+
 
   if (scaled_ref_frame) {
     // Swap back the original buffers for subpel motion search.
