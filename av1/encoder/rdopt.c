c59180fae0427915a462912a9c5cd7418c326eb5
Arild Fuldseth (arilfuld) <arilfuld@cisco.com>
2021-06-03 14:07:03 +0200

    More clang style fixes.
    Change-Id: Ie93bc3b6138d10a40ee37f50aee21b5aa9833f6e

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -1782,7 +1782,7 @@ static INLINE int build_cur_mv(int_mv *cur_mv, PREDICTION_MODE this_mode,
 
 #if CONFIG_NO_MV_PARSING_DEPENDENCY
 static INLINE int get_drl_cost(const MB_MODE_INFO *mbmi,
-                               const int(*const drl_mode_cost0)[2]) {
+                               const int (*const drl_mode_cost0)[2]) {
   int cost = 0;
   if (mbmi->mode == NEWMV || mbmi->mode == NEW_NEWMV) {
     for (int idx = 0; idx < 2; ++idx) {
