50e8c58c99f6f713008f3f348ecfb42d8bc07a18
yixindu <yixindu@tencent.com>
2021-06-04 18:30:53 -0700

    Cross-Component Sample Offset (CCSO)
    In this work, a Cross-Component Sample Offset (CCSO) approach is proposed to refine chroma component using luma component. The key component of CCSO is a non-linear offset mapping mechanism implemented as a look-up-table (LUT). The input of the mapping is the co-located reconstructed samples of luma component, and the output is offset values applied on chroma component.
    
    Change-Id: I861dffcdc49c61fda831538274ccc5db3b387100

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -158,7 +158,7 @@ set_aom_config_var(CONFIG_NEW_INTER_MODES 1 NUMBER
                    "AV2 inter mode consolidation experiment flag")
 set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 1 "Enable SMVP improvement")
 set_aom_config_var(CONFIG_TMVP_IMPROVEMENT 1 "Enable TMVP improvement")
-
+set_aom_config_var(CONFIG_CCSO 0 "AV2 cross component sample offset flag.")
 #
 # Variables in this section control optional features of the build system.
 #
