3bff9a3096ef5e0b85ac8962af676fc025d9b178
yixindu <yixindu@tencent.com>
2021-06-04 18:30:53 -0700

    CCSO clean up
    Change-Id: I265cbd1f4c16df0ae8c737a78cda5fd8d2887bf9

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -9,6 +9,8 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
+/** @file */
+
 #ifndef AOM_AV1_COMMON_BLOCKD_H_
 #define AOM_AV1_COMMON_BLOCKD_H_
 
@@ -275,6 +277,10 @@ typedef struct MB_MODE_INFO {
   // Indicate if masked compound is used(1) or not(0).
   uint8_t comp_group_idx : 1;
   int8_t cdef_strength : 4;
+#if CONFIG_CCSO
+  uint8_t ccso_blk_u : 2;
+  uint8_t ccso_blk_v : 2;
+#endif
 } MB_MODE_INFO;
 
 static INLINE int is_intrabc_block(const MB_MODE_INFO *mbmi) {
@@ -868,6 +874,10 @@ typedef struct macroblockd {
    * 'cpi->tile_thr_data[t].td->mb.tmp_pred_bufs'.
    */
   uint8_t *tmp_obmc_bufs[2];
+#if CONFIG_CCSO
+  uint8_t ccso_blk_u;
+  uint8_t ccso_blk_v;
+#endif
 } MACROBLOCKD;
 
 /*!\cond */
