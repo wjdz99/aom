07032fd8782d6f05aadeeda8250563e65aafc56f
yixindu <yixindu@tencent.com>
2021-06-04 18:30:53 -0700

    CCSO clean up
    STATS_CHANGED: add ccso
    
    Change-Id: I265cbd1f4c16df0ae8c737a78cda5fd8d2887bf9

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -275,6 +275,12 @@ typedef struct MB_MODE_INFO {
   // Indicate if masked compound is used(1) or not(0).
   uint8_t comp_group_idx : 1;
   int8_t cdef_strength : 4;
+#if CONFIG_CCSO
+  /** ccso blk u */
+  uint8_t ccso_blk_u : 2;
+  /** ccso blk v */
+  uint8_t ccso_blk_v : 2;
+#endif
 } MB_MODE_INFO;
 
 static INLINE int is_intrabc_block(const MB_MODE_INFO *mbmi) {
@@ -868,6 +874,12 @@ typedef struct macroblockd {
    * 'cpi->tile_thr_data[t].td->mb.tmp_pred_bufs'.
    */
   uint8_t *tmp_obmc_bufs[2];
+#if CONFIG_CCSO
+  /** ccso blk u */
+  uint8_t ccso_blk_u;
+  /** ccso blk v */
+  uint8_t ccso_blk_v;
+#endif
 } MACROBLOCKD;
 
 /*!\cond */
