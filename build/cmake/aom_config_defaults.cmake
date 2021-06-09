3f63637e7a65c395964e95ec2218ad6811568edc
Yunfei Zheng <yzheng@apple.com>
2021-06-08 22:03:45 -0700

    Add SMVP improvement.
    Change-Id: Ib97cbc186500e01f2106dce710ceb15565be6b16

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -139,6 +139,7 @@ set_aom_config_var(CONFIG_REMOVE_DUAL_FILTER 1
                    "AV2 experiment flag to remove dual filter.")
 set_aom_config_var(CONFIG_NEW_INTER_MODES 0 NUMBER
                    "AV2 inter mode consolidation experiment flag")
+set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 0 "Enable SMVP improvement")
 
 #
 # Variables in this section control optional features of the build system.
