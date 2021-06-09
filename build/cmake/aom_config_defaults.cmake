55e57bde699a1d4d4e01e02aa155e34639ee0c14
Yeqing Wu <yeqing_wu@apple.com>
2021-06-08 22:03:45 -0700

    Add SMVP improvement.
    Change-Id: Ib97cbc186500e01f2106dce710ceb15565be6b16

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -153,6 +153,7 @@ set_aom_config_var(
   "AV2 experiment flag to enable offset based refinement of intra prediction.")
 set_aom_config_var(CONFIG_NEW_INTER_MODES 0 NUMBER
                    "AV2 inter mode consolidation experiment flag")
+set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 0 "Enable SMVP improvement")
 
 #
 # Variables in this section control optional features of the build system.
