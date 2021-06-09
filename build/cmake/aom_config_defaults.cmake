8b8dfac3664ca9e85d6860152b094333842a7bd8
Yunfei Zheng <yzheng@apple.com>
2021-06-08 22:03:45 -0700

    Add SMVP improvement.
    Change-Id: Ib97cbc186500e01f2106dce710ceb15565be6b16

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -161,6 +161,7 @@ set_aom_config_var(CONFIG_EXT_RECUR_PARTITIONS 0 NUMBER
                    "AV2 Fully recursive partitions experiment flag")
 set_aom_config_var(CONFIG_OPTFLOW_REFINEMENT 0
                    "AV2 optical flow based MV refinement experimental flag")
+set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 0 "Enable SMVP improvement")
 
 #
 # Variables in this section control optional features of the build system.
