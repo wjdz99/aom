7106e42942455a95fb7f681bbce1119881a0b74e
yaoyaogoogle@google.com <yaoyaogoogle@google.com>
2021-06-11 19:37:56 +0000

    Add spherical coordinate range control
    Change-Id: Ibd9ef4b7e0c47ea4b43d727c5d551de3a8d5e4b1

--- av1/common/spherical_pred.c
+++ av1/common/spherical_pred.c
@@ -17,23 +17,41 @@
 
 void av1_sphere_to_plane_erp(double phi, double theta, int width, int height,
                              double *x, double *y) {
-  // The 0.01 is for issues when comparing floating point numbers
-  // TODO(yaoyaogoogle): Find a better way to solve the issue
-  assert(phi >= -PI * 0.5 - 0.01 && phi <= PI * 0.5 + 0.01 &&
-         theta >= -PI - 0.01 && theta <= PI + 0.01);
+  double phi_mod = fmod(phi, 0.5 * PI);
+  if (phi_mod == 0 && fmod(phi, PI) != 0) {
+    if (fmod(phi, 2 * PI) == -1.5 * PI) {
+      phi_mod = 0.5 * PI;
+    } else if (fmod(phi, 2 * PI) == 1.5 * PI) {
+      phi_mod = -0.5 * PI;
+    } else {
+      phi_mod = phi > 0 ? 0.5 * PI : -0.5 * PI;
+    }
+  }
 
+  double theta_mod = fmod(theta, PI);
+  if (theta_mod == 0 && fmod(theta, 2 * PI) != 0) {
+    if (theta > 0) {
+      theta_mod = PI;
+    } else if (theta < 0) {
+      theta_mod = -PI;
+    }
+  }
   // This should actually be a range related to 1/cos(phi) since x is distorted
   // TODO(yaoyaogoogle): Adjust the width of x according to the interpolation
   // mode
-  *x = theta / PI * width * 0.5 + width * 0.5;
+  *x = theta_mod / PI * (width - 1) * 0.5 + (width - 1) * 0.5;
+
   // No minus sign for y since we only use an imaginary upside-down globe
-  *y = phi / (PI * 0.5) * height * 0.5 + height * 0.5;
+  *y = phi_mod / (PI * 0.5) * (height - 1) * 0.5 + (height - 1) * 0.5;
 }
 
 void av1_plane_to_sphere_erp(double x, double y, int width, int height,
                              double *phi, double *theta) {
-  assert(x < width && x >= 0 && y < height && y >= 0);
+  assert(y >= 0 && y <= height - 1);
 
-  *theta = (x - width * 0.5) / width * 2 * PI;
-  *phi = (y - height * 0.5) / height * PI;
+  double x_mod = fmod(x, width);
+  x_mod = x_mod >= 0 ? x_mod : x_mod + width;
+
+  *theta = (x_mod - (width - 1) * 0.5) / (width - 1) * 2 * PI;
+  *phi = (y - (height - 1) * 0.5) / (height - 1) * PI;
 }
