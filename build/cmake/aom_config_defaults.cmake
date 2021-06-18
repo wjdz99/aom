21f5f1c4065f46aeaea37933f134ea80b02699f5
Yeqing Wu <yeqing_wu@apple.com>
2021-06-18 08:54:36 -0700

    Turn on compiling flag for SMVP improvement
    STATS_CHANGED
    
    Change-Id: I5868b7cd3815184193d7e73ad6518d421d3b56f2

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -153,7 +153,7 @@ set_aom_config_var(
   "AV2 experiment flag to enable offset based refinement of intra prediction.")
 set_aom_config_var(CONFIG_NEW_INTER_MODES 1 NUMBER
                    "AV2 inter mode consolidation experiment flag")
-set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 0 "Enable SMVP improvement")
+set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 1 "Enable SMVP improvement")
 set_aom_config_var(CONFIG_TMVP_IMPROVEMENT 0 "Enable TMVP improvement")
 
 #
