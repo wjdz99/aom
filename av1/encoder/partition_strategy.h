fc2dd81160b8cc2bfd07b8edb089bd099fa5d007
venkat sanampudi <sanampudi.venkatarao@ittiam.com>
2021-06-22 13:49:54 +0530

    Enable intra_cnn_split for screen contents in intra frames
    Added levels to speed feature intra_cnn_split, to introduce less
    aggressive partition pruning for screen contents in intra frames.
    As screen contents tend to choose larger partitions, the sf
    intra_cnn_split is extended to prune split and rectangular partitions
    only for screen contents. In this case, none partitions are not pruned.
    Partition pruning for screen contents is enabled only for speed presets
    5 and 6 in allintra encoding mode.
    Also, the name of the speed feature is changed to
    intra_cnn_based_part_prune_level to indicate the level of
    aggressiveness of pruning.
    
    For allintra video encode (on screen content set),
    
              Instruction Count        BD-Rate Loss(%)
    cpu-used     Reduction(%)   avg.psnr  ovr.psnr    ssim
       5           34.278       3.7686    3.7633      2.8405
       6           17.373       1.5351    1.5328      1.6781
    
    For AVIF still image encode,
    
              Instruction Count    BD-Rate Loss(%)
    cpu-used     Reduction(%)      psnr       ssim
       5           1.246           0.0242     0.0104
       6           0.669          -0.0141    -0.0042
    
    BUG=aomedia:2959
    
    STATS_CHANGED
    
    Change-Id: I3cc72c4ecb159a2922c2f75a4f8a41142662a70d

--- av1/encoder/partition_strategy.h
+++ av1/encoder/partition_strategy.h
@@ -17,13 +17,11 @@
 #include "av1/encoder/encodemb.h"
 #include "av1/encoder/encoder.h"
 
-void av1_intra_mode_cnn_partition(const AV1_COMMON *const cm, MACROBLOCK *x,
-                                  int bsize, int label_idx,
-                                  int *partition_none_allowed,
-                                  int *partition_horz_allowed,
-                                  int *partition_vert_allowed,
-                                  int *do_rectangular_split,
-                                  int *do_square_split);
+void av1_intra_mode_cnn_partition(
+    const AV1_COMMON *const cm, MACROBLOCK *x, int bsize, int label_idx,
+    int intra_cnn_based_part_prune_level, int *partition_none_allowed,
+    int *partition_horz_allowed, int *partition_vert_allowed,
+    int *do_rectangular_split, int *do_square_split);
 
 // Performs a simple_motion_search with a single reference frame and extract
 // the variance of residues. Then use the features to determine whether we want
