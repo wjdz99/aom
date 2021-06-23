BlueSwordM/aom-av1-psy

commit f19edfa95f8dc9fa596282a210757e4d6d0cd490

exports.cmake: use APPLE and WIN32 and use def for mingw-w64
mingw-w64's ld and lld automatically accepts MSVC's def files and CMake
can handle the proper flags for .def files added to a library's sources.

This fixes the export file handling for mingw-w64 lld as that lld does
not support the --version-script argument

Signed-off-by: Christopher Degawa <christopher.degawa@intel.com>
Change-Id: I18a1ce2696e2f9b083ecab3d0d60566d48b68cf0

build/cmake/exports.cmake
@@ -17,9 +17,9 @@ include("${AOM_ROOT}/build/cmake/exports_sources.cmake")
 
 # Creates the custom target which handles generation of the symbol export lists.
 function(setup_exports_target)
-  if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
+  if(APPLE)
     set(symbol_file_ext "syms")
-  elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS" AND MSVC)
+  elseif(WIN32)
     set(symbol_file_ext "def")
   else()
     set(symbol_file_ext "ver")
@@ -43,7 +43,7 @@ function(setup_exports_target)
                             -P
                             "${AOM_ROOT}/build/cmake/generate_exports.cmake"
                     SOURCES ${AOM_EXPORTS_SOURCES}
-                    DEPENDS ${AOM_EXPORTS_SOURCES})
+                    DEPENDS ${AOM_EXPORTS_SOURCES} BYPRODUCTS ${aom_sym_file})
 
   # Make libaom depend on the exports file, and set flags to pick it up when
   # creating the dylib.
@@ -54,14 +54,12 @@ function(setup_exports_target)
                  APPEND_STRING
                  PROPERTY LINK_FLAGS "-exported_symbols_list ${aom_sym_file}")
   elseif(WIN32)
-    if(NOT MSVC)
-      set_property(TARGET aom
-                   APPEND_STRING
-                   PROPERTY LINK_FLAGS "-Wl,--version-script ${aom_sym_file}")
-    else()
+    if(MSVC)
       set_property(TARGET aom
                    APPEND_STRING
                    PROPERTY LINK_FLAGS "/DEF:${aom_sym_file}")
+    else()
+      target_sources(aom PRIVATE "${aom_sym_file}")
     endif()
 
     # TODO(tomfinegan): Sort out the import lib situation and flags for MSVC.

build/cmake/generate_exports.cmake
@@ -23,7 +23,7 @@ include("${AOM_ROOT}/build/cmake/exports_sources.cmake")
 
 if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
   set(symbol_prefix "_")
-elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS" AND AOM_MSVC)
+elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS")
   file(WRITE "${AOM_SYM_FILE}" "LIBRARY aom\n" "EXPORTS\n")
 else()
   set(symbol_suffix ";")
@@ -33,7 +33,7 @@ set(aom_sym_file "${AOM_SYM_FILE}")
 
 if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
   file(REMOVE "${aom_sym_file}")
-elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS" AND AOM_MSVC)
+elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS")
   file(WRITE "${aom_sym_file}" "LIBRARY aom\n" "EXPORTS\n")
 else()
   file(WRITE "${aom_sym_file}" "{\nglobal:\n")
@@ -47,7 +47,7 @@ endforeach()
 
 foreach(exported_symbol ${exported_symbols})
   string(STRIP "${exported_symbol}" exported_symbol)
-  if("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS" AND AOM_MSVC)
+  if("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS")
     string(SUBSTRING ${exported_symbol} 0 4 export_type)
     string(COMPARE EQUAL "${export_type}" "data" is_data)
     if(is_data)

wjdz99/aom

commit ee38a3fafd1cdfba4903366c8d98816692b676c4

exports: use CMAKE_SHARED_LIBRARY_PREFIX to determine lib name
f19edfa95f8dc9fa596282a210757e4d6d0cd490 broke for some people who used
libaom.dll as the linked library as the def file caused the linker to
produce aom.dll instead

Signed-off-by: Christopher Degawa <christopher.degawa@intel.com>
Change-Id: I9040de491d9df5a9434e7874ed8b6e8d1c82a203

build/cmake/exports.cmake
@@ -35,6 +35,7 @@ function(setup_exports_target)
                             -DAOM_SYM_FILE="${aom_sym_file}"
                             -DAOM_MSVC=${MSVC}
                             -DAOM_XCODE=${XCODE}
+                            -DAOM_LIB_PREFIX="${CMAKE_SHARED_LIBRARY_PREFIX}"
                             -DCONFIG_NAME=$<CONFIG>
                             -DCONFIG_AV1_DECODER=${CONFIG_AV1_DECODER}
                             -DCONFIG_AV1_ENCODER=${CONFIG_AV1_ENCODER}

build/cmake/generate_exports.cmake
@@ -10,6 +10,7 @@
 #
 cmake_minimum_required(VERSION 3.5)
 
+# AOM_LIB_PREFIX can be empty
 set(REQUIRED_ARGS "AOM_ROOT" "AOM_CONFIG_DIR" "AOM_TARGET_SYSTEM" "AOM_SYM_FILE"
                   "CONFIG_AV1_DECODER" "CONFIG_AV1_ENCODER")
 
@@ -24,7 +25,7 @@ include("${AOM_ROOT}/build/cmake/exports_sources.cmake")
 if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
   set(symbol_prefix "_")
 elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS")
-  file(WRITE "${AOM_SYM_FILE}" "LIBRARY aom\n" "EXPORTS\n")
+  file(WRITE "${AOM_SYM_FILE}" "LIBRARY ${AOM_LIB_PREFIX}aom\n" "EXPORTS\n")
 else()
   set(symbol_suffix ";")
 endif()
@@ -34,7 +35,7 @@ set(aom_sym_file "${AOM_SYM_FILE}")
 if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
   file(REMOVE "${aom_sym_file}")
 elseif("${AOM_TARGET_SYSTEM}" MATCHES "Windows\|MSYS")
-  file(WRITE "${aom_sym_file}" "LIBRARY aom\n" "EXPORTS\n")
+  file(WRITE "${aom_sym_file}" "LIBRARY ${AOM_LIB_PREFIX}aom\n" "EXPORTS\n")
 else()
   file(WRITE "${aom_sym_file}" "{\nglobal:\n")
 endif()