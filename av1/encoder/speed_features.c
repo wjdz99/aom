2e78c999fa0ef836aa8e7c08cc3b0fe188eff5a2
Yunqing Wang <yunqingwang@google.com>
2021-06-28 14:19:37 -0700

    Extend prune_ext_part_using_split_info to speed 2
    STATS_CHANGED
    
    Change-Id: If3161dca1c3028ba377bb7a1af31c904ba2d59f3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -871,6 +871,7 @@ static void set_good_speed_features_framesize_independent(
     sf->part_sf.allow_partition_search_skip = 1;
     sf->part_sf.reuse_best_prediction_for_part_ab =
         !frame_is_intra_only(&cpi->common);
+    sf->part_sf.prune_ext_part_using_split_info = 1;
 
     sf->mv_sf.auto_mv_step_size = 1;
     sf->mv_sf.simple_motion_subpel_force_stop = QUARTER_PEL;
@@ -927,7 +928,8 @@ static void set_good_speed_features_framesize_independent(
 
     sf->part_sf.less_rectangular_check_level = 2;
     sf->part_sf.simple_motion_search_prune_agg = 1;
-    sf->part_sf.prune_ext_part_using_split_info = 1;
+    sf->part_sf.prune_ext_part_using_split_info = 2;
+
     sf->part_sf.simple_motion_search_rect_split = 1;
 
     sf->mv_sf.full_pixel_search_level = 1;
@@ -997,7 +999,7 @@ static void set_good_speed_features_framesize_independent(
 
     sf->part_sf.simple_motion_search_prune_agg = 2;
     sf->part_sf.simple_motion_search_reduce_search_steps = 4;
-    sf->part_sf.prune_ext_part_using_split_info = 2;
+    sf->part_sf.prune_ext_part_using_split_info = 2;  //
     sf->part_sf.ml_predict_breakout_level = 3;
 
     sf->inter_sf.alt_ref_search_fp = 1;
