2d3031996e274b96b1a37ae4379683b623885660
Kavi Ramamurthy <kavii@google.com>
2021-06-29 21:55:51 +0000

    Integrate the improved bitrate accuracy model into the encoder.
    Active in VBR mode with CONFIG_BITRATE_ACCURACY=1.
    
    BUG=aomedia:3045
    
    Change-Id: I6573a64ebb7834d4528218022f2f7390f3be2cae

--- av1/encoder/encode_strategy.c
+++ av1/encoder/encode_strategy.c
@@ -1024,7 +1024,12 @@ static int denoise_and_encode(AV1_COMP *const cpi, uint8_t *const dest,
         if (gf_group->update_type[0] == KF_UPDATE && cpi->gf_frame_index != 0) {
           gop_bit_budget -= cpi->vbr_rc_info.keyframe_bitrate;
         }
-        // Use the gop_bit_budget to determine gf_group->q_val here.
+        // Use the gop_bit_budget to determine gf_group->q_val.
+        int arf_q =
+            av1_get_arf_q_index_q_mode(cpi, cpi->ppi->tpl_data.tpl_frame);
+        av1_q_mode_estimate_base_q(&cpi->ppi->gf_group,
+                                   cpi->ppi->tpl_data.txfm_stats_list,
+                                   gop_bit_budget, cpi->gf_frame_index, arf_q);
       }
 #endif
     }
