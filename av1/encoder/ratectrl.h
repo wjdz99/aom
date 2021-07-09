290ca77f9e84a8dfe905df8c2f91f6f0f3aff492
Angie Chiang <angiebird@google.com>
2021-07-08 22:02:31 -0700

    Integrate BITRATE_ACCURACY with new ARF q feature
    av1_get_arf_q_index_q_mode() can not be directly used
    in BITRATE_ACCURACY because it's using
    active_worst_quality as leaf frame q index, but in
    BITRATE_ACCURACY, we are tuning the leaf frame q index.
    
    We rename av1_get_arf_q_index_q_mode() as av1_tpl_get_q_index()
    and decompose it into two
    functions, av1_tpl_get_qstep_ratio() and
    av1_get_q_index_from_qstep_ratio().
    
    av1_tpl_get_qstep_ratio() aims at computing the qstep
    ratio between leaf frame q index and arf frame q index.
    
    av1_get_q_index_from_qstep_ratio() finds arf q index based on
    the qstep ratio with respect to the leaf frame q index.
    
    We pass the qstep ratio into av1_q_mode_estimate_base_q()
    so that it can be used to infer arf q index from
    leaf frame q index.
    
    Change-Id: If68a8ffee00656ec22df01c7cc003de5e9540071

--- av1/encoder/ratectrl.h
+++ av1/encoder/ratectrl.h
@@ -713,12 +713,14 @@ int av1_encodedframe_overshoot_cbr(struct AV1_COMP *cpi, int *q);
  *
  * \param[in]       gf_frame_index    Index of the current frame
  * \param[in]       base_q_index      Base q index
- * \param[in]       bit_depth         Bit depth
- * \param[in]       arf_q             ARF q index
+ * \param[in]       arf_qstep_ratio   The quantize step ratio between arf q
+ * index and base q index \param[in]       bit_depth         Bit depth
  * \param[out]      gf_group          Pointer to the GOP
  */
 void av1_q_mode_compute_gop_q_indices(int gf_frame_index, int base_q_index,
-                                      int arf_q, struct GF_GROUP *gf_group);
+                                      double arf_qstep_ratio,
+                                      aom_bit_depth_t bit_depth,
+                                      struct GF_GROUP *gf_group);
 
 /*!\brief Compute the q_indices for a single frame.
  *
