8a6b1b395b93d79f2a8d90a74a037260e0cd9af4
chiyotsai <chiyotsai@google.com>
2021-07-20 18:40:44 -0700

    Add fast version of av1_nn_softmax
    This commit adds a faster but less accurate version of av1_nn_softmax
    where the output size is 16.
    
    This provides SIMD optimization as well as a linear approximation for
    expf. The C code provides 60% encoding time reduction at functional
    level, the SSE3 code 89%.
    
    Performance in RT Mode:
    SPD_SET | TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   |  SPD
    ---------+---------+----------+----------+---------+-------
        5    |rtc_derf | -0.156%  | -0.151%  | -0.210% | +0.8%
        5    |   rtc   | -0.050%  | -0.062%  | -0.049% | +0.6%
    ---------+---------+----------+----------+---------+-------
        6    |rtc_derf | +0.039%  | +0.037%  | +0.037% | +0.7%
        6    |   rtc   | +0.009%  | +0.013%  | -0.015% | +0.8%
    ---------+---------+----------+----------+---------+-------
    
    Performance in GOOD Mode:
     SPD_SET | TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   |  SPD
    ---------+---------+----------+----------+---------+-------
        1    | hdres2  | +0.004%  | +0.011%  | +0.004% | +0.2%
        1    | midres2 | +0.016%  | -0.010%  | +0.003% | +0.1%
        1    | lowres2 | -0.018%  | -0.018%  | +0.005% | +0.2%
    ---------+---------+----------+----------+---------+-------
        2    | hdres2  | -0.080%  | -0.073%  | -0.095% | +0.1%
        2    | midres2 | -0.009%  | -0.010%  | -0.018% | +0.2%
        2    | lowres2 | +0.015%  | +0.013%  | -0.030% | +0.4%
    ---------+---------+----------+----------+---------+-------
        3    | hdres2  | -0.003%  | +0.003%  | +0.024% | +0.2%
        3    | midres2 | -0.018%  | -0.017%  | -0.047% | +0.2%
        3    | lowres2 | -0.053%  | -0.055%  | -0.050% | +0.4%
    ---------+---------+----------+----------+---------+-------
        4    | hdres2  | -0.001%  | -0.001%  | -0.001% | -0.0%
        4    | midres2 | +0.001%  | +0.001%  | +0.001% | +0.0%
        4    | lowres2 | +0.000%  | +0.000%  | +0.000% | +0.0%
    ---------+---------+----------+----------+---------+-------
        5    | hdres2  | +0.000%  | +0.000%  | -0.000% | +0.0%
        5    | midres2 | -0.000%  | -0.000%  | +0.000% | -0.0%
        5    | lowres2 | +0.000%  | +0.000%  | +0.000% | +0.0%
    ---------+---------+----------+----------+---------+-------
        6    | hdres2  | -0.083%  | -0.083%  | +0.009% | +0.1%
        6    | midres2 | -0.008%  | -0.008%  | +0.020% | +0.1%
        6    | lowres2 | +0.003%  | +0.005%  | -0.003% | +0.2%
    ---------+---------+----------+----------+---------+-------
    
    STATS_CHANGED due to less accurate version of softmax
    
    Change-Id: I6243643be4c1e5cd5605f241cdfcf3d4aca80e10

--- av1/encoder/ml.c
+++ av1/encoder/ml.c
@@ -149,8 +149,38 @@ void av1_nn_softmax(const float *input, float *output, int n) {
   for (int i = 0; i < n; i++) {
     // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
     const float normalized_input = AOMMAX(input[i] - max_inp, -10.0f);
-    output[i] = (float)exp(normalized_input);
+    output[i] = expf(normalized_input);
     sum_out += output[i];
   }
   for (int i = 0; i < n; i++) output[i] /= sum_out;
+}
+
+static AOM_INLINE float approx_exp(float y) {
+#define A ((1 << 23) / 0.69314718056f)  // (1 << 23) / ln(2)
+#define B \
+  127  // Offset for the exponent according to IEEE floating point standard.
+#define C 60801  // Magic number controls the accuracy of approximation
+  union float_alias {
+    float as_float;
+    int32_t as_int32;
+  } container;
+  container.as_int32 = ((int32_t)(y * A)) + ((B << 23) - C);
+  return container.as_float;
+#undef A
+#undef B
+#undef C
+}
+
+void av1_nn_fast_softmax_16_c(const float *input, float *output) {
+  const int num_classes = 16;
+  float max_inp = input[0];
+  for (int i = 1; i < num_classes; i++) max_inp = AOMMAX(max_inp, input[i]);
+  float sum_out = 0.0f;
+  for (int i = 0; i < num_classes; i++) {
+    // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
+    const float normalized_input = AOMMAX(input[i] - max_inp, -10.0f);
+    output[i] = approx_exp(normalized_input);
+    sum_out += output[i];
+  }
+  for (int i = 0; i < num_classes; i++) output[i] /= sum_out;
 }
---
+++ test/av1_softmax_test.cc
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2021, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#include <tuple>
+
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+
+#include "aom/aom_integer.h"
+#include "aom_ports/aom_timer.h"
+#include "av1/encoder/ml.h"
+#include "config/aom_config.h"
+#include "config/aom_dsp_rtcd.h"
+#include "config/av1_rtcd.h"
+#include "test/util.h"
+#include "test/register_state_check.h"
+#include "test/acm_random.h"
+
+namespace {
+typedef void (*FastSoftmaxFn)(const float *const input, float *output);
+
+typedef std::tuple<const FastSoftmaxFn, int> FastSoftmaxTestParams;
+
+const float rel_epsilon = 5e-2f;  // Error threshold for functional equivalence
+const float abs_epsilon = 5e-3f;  // Error threshold for functional equivalence
+
+class FastSoftmaxTest : public ::testing::TestWithParam<FastSoftmaxTestParams> {
+ public:
+  FastSoftmaxTest() : _target_fn{ GET_PARAM(0) }, _num_classes(GET_PARAM(1)) {
+    _ref_buf = (float *)calloc(_num_classes, sizeof(*_ref_buf));
+    _dst_buf = (float *)calloc(_num_classes, sizeof(*_dst_buf));
+    _input = (float *)calloc(_num_classes, sizeof(*_input));
+  }
+  ~FastSoftmaxTest() {
+    free(_ref_buf);
+    free(_dst_buf);
+    free(_input);
+  }
+  void RunSoftmaxTest();
+  void RunSoftmaxSpeedTest(const int run_times);
+  void FillInputBuf();
+
+ private:
+  const FastSoftmaxFn _target_fn;
+  const int _num_classes;
+  float *_ref_buf, *_dst_buf, *_input;
+  libaom_test::ACMRandom rng_;
+};
+
+void FastSoftmaxTest::FillInputBuf() {
+  for (int idx = 0; idx < _num_classes; idx++) {
+    _input[idx] = ((float)rng_.Rand31() - (1 << 30)) / (1u << 30);
+  }
+}
+
+void FastSoftmaxTest::RunSoftmaxTest() {
+  av1_nn_softmax(_input, _ref_buf, _num_classes);
+  _target_fn(_input, _dst_buf);
+
+  for (int idx = 0; idx < _num_classes; idx++) {
+    if (_ref_buf[idx] < abs_epsilon) {
+      ASSERT_LE(_dst_buf[idx], abs_epsilon)
+          << "Reference output was near-zero, test output was not" << std::endl;
+    } else {
+      const float error = _dst_buf[idx] - _ref_buf[idx];
+      const float relative_error = fabsf(error / _ref_buf[idx]);
+      ASSERT_LE(relative_error, rel_epsilon)
+          << "Excessive relative error between reference and test output"
+          << std::endl;
+      ASSERT_LE(error, abs_epsilon)
+          << "Excessive absolute error between reference and test output"
+          << std::endl;
+    }
+  }
+}
+
+void FastSoftmaxTest::RunSoftmaxSpeedTest(const int run_times) {
+  aom_usec_timer timer;
+  aom_usec_timer_start(&timer);
+  for (int idx = 0; idx < run_times; idx++) {
+    _target_fn(_input, _dst_buf);
+  }
+  aom_usec_timer_mark(&timer);
+  const int64_t time = aom_usec_timer_elapsed(&timer);
+  std::cout << "Test with " << _num_classes << " classes took " << time
+            << " us." << std::endl;
+}
+
+TEST_P(FastSoftmaxTest, RandomValues) {
+  FillInputBuf();
+  RunSoftmaxTest();
+}
+
+TEST_P(FastSoftmaxTest, DISABLED_Speed) {
+  constexpr int kNumTimes = 1000000;
+  RunSoftmaxSpeedTest(kNumTimes);
+}
+
+void AnchorSoftmax16Fn(const float *input, float *output) {
+  av1_nn_softmax(input, output, 16);
+}
+
+const FastSoftmaxTestParams kArrayParams_c[] = {
+  { AnchorSoftmax16Fn, 16 }, { av1_nn_fast_softmax_16_c, 16 }
+};
+INSTANTIATE_TEST_SUITE_P(C, FastSoftmaxTest,
+                         ::testing::ValuesIn(kArrayParams_c));
+
+// #if HAVE_SSE3 && !CONFIG_EXCLUDE_SIMD_MISMATCH
+// INSTANTIATE_TEST_SUITE_P(
+//     SSE3, FastSoftmaxTest,
+//     ::testing::Values(FastSoftmaxTestParams(av1_nn_fast_softmax_16_sse3,
+//     16)));
+// #endif
+}  // namespace
