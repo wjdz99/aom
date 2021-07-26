149c8543d8246d9f69d2fbe63422d5239c97af71
Peter Kasting <pkasting@chromium.org>
2021-07-26 04:35:52 -0700

    Fix some instances of -Wunused-but-set-variable.
    Bug: chromium:1203071
    Change-Id: Id04d2d6db64664d0f046a20352d6cf2db6bd4332

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -881,7 +881,6 @@ int av1_choose_var_based_partitioning(AV1_COMP *cpi, const TileInfo *const tile,
   VP128x128 *vt;
   VP16x16 *vt2 = NULL;
   unsigned char force_split[85];
-  int avg_32x32;
   int avg_64x64;
   int max_var_32x32[4];
   int min_var_32x32[4];
@@ -989,7 +988,6 @@ int av1_choose_var_based_partitioning(AV1_COMP *cpi, const TileInfo *const tile,
 
   avg_64x64 = 0;
   for (m = 0; m < num_64x64_blocks; ++m) {
-    avg_32x32 = 0;
     max_var_32x32[m] = 0;
     min_var_32x32[m] = INT_MAX;
     const int m2 = m << 2;
@@ -1042,7 +1040,6 @@ int av1_choose_var_based_partitioning(AV1_COMP *cpi, const TileInfo *const tile,
           force_split[m + 1] = 1;
           force_split[0] = 1;
         }
-        avg_32x32 += var_32x32;
       }
     }
     if (!force_split[1 + m]) {
