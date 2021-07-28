5843a93c93e90e3bce800c49174717161b3f5f5a
Yaowu Xu <yaowu@google.com>
2021-07-28 14:01:54 -0700

    WIP: Move AV1 specific code into AV1 folder
    Change-Id: Ia7edca01d78dad55107c9f43285bdf3d6b1da1b3

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -1190,11 +1190,6 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   #
   #
   #
-  add_proto qw/void aom_upsampled_pred/, "MACROBLOCKD *xd, const struct AV1Common *const cm, int mi_row, int mi_col,
-                                          const MV *const mv, uint8_t *comp_pred, int width, int height, int subpel_x_q3,
-                                          int subpel_y_q3, const uint8_t *ref, int ref_stride, int subpel_search";
-  specialize qw/aom_upsampled_pred sse2/;
-
   add_proto qw/void aom_comp_avg_upsampled_pred/, "MACROBLOCKD *xd, const struct AV1Common *const cm, int mi_row, int mi_col,
                                                    const MV *const mv, uint8_t *comp_pred, const uint8_t *pred, int width,
                                                    int height, int subpel_x_q3, int subpel_y_q3, const uint8_t *ref,
