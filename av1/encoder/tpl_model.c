8ee1daedade7988b8aa976ef389bc2c1d6f6556b
Kavi Ramamurthy <kavii@google.com>
2021-07-30 17:58:20 +0000

    Take neighboring motion vectors into account.
    Added a static inline method which looks at the up and left neighbors
    (relative to current) of a motion vector and accounts for similarities
    where the compression will be higher.
    
    Updated tpl_model_test accordingly.
    
    BUG=aomedia:3045
    
    Change-Id: I0b02ba786998e8c967d1ddd0d7f0fc8816904c06

--- av1/encoder/tpl_model.c
+++ av1/encoder/tpl_model.c
@@ -2111,6 +2111,52 @@ double av1_tpl_compute_mv_bits(const TplParams *tpl_data, int gf_group_size,
   return total_mv_bits * mv_scale_factor;
 }
 
+// Take neighbor MVs into account when traversing through blocks.
+static INLINE int_mv optimize_for_neighbor_mvs(const TplDepFrame *tpl_frame,
+                                               int row, int col, int step,
+                                               int tpl_stride,
+                                               int right_shift) {
+  const TplDepStats *tpl_stats =
+      &tpl_frame
+           ->tpl_stats_ptr[av1_tpl_ptr_pos(row, col, tpl_stride, right_shift)];
+  int_mv current_mv = tpl_stats->mv[tpl_stats->ref_frame_index[0]];
+  double current_mv_magnitude =
+      sqrt(pow(current_mv.as_mv.row, 2.0) + pow(current_mv.as_mv.col, 2.0));
+
+  // Retrieve the up and left neighbors.
+  double up_error = DBL_MAX;
+  int_mv up_mv;
+  if (row - step >= 0) {
+    tpl_stats = &tpl_frame->tpl_stats_ptr[av1_tpl_ptr_pos(
+        row - step, col, tpl_stride, right_shift)];
+    up_mv = tpl_stats->mv[tpl_stats->ref_frame_index[0]];
+    up_mv.as_mv.row = current_mv.as_mv.row - up_mv.as_mv.row;
+    up_mv.as_mv.col = current_mv.as_mv.col - up_mv.as_mv.col;
+    up_error =
+        sqrt(pow(abs(up_mv.as_mv.row), 2.0) + pow(abs(up_mv.as_mv.col), 2.0));
+  }
+
+  double left_error = DBL_MAX;
+  int_mv left_mv;
+  if (col - step >= 0) {
+    tpl_stats = &tpl_frame->tpl_stats_ptr[av1_tpl_ptr_pos(
+        row, col - step, tpl_stride, right_shift)];
+    left_mv = tpl_stats->mv[tpl_stats->ref_frame_index[0]];
+    left_mv.as_mv.row = current_mv.as_mv.row - left_mv.as_mv.row;
+    left_mv.as_mv.col = current_mv.as_mv.col - left_mv.as_mv.col;
+    left_error = sqrt(pow(abs(left_mv.as_mv.row), 2.0) +
+                      pow(abs(left_mv.as_mv.col), 2.0));
+  }
+
+  // Return the MV with the minimum distance from current.
+  if (up_error < left_error && up_error < current_mv_magnitude) {
+    return up_mv;
+  } else if (left_error < up_error && left_error < current_mv_magnitude) {
+    return left_mv;
+  }
+  return current_mv;
+}
+
 /* Compute the entropy of motion vectors for a single frame. */
 double av1_tpl_compute_frame_mv_entropy(const TplDepFrame *tpl_frame,
                                         uint8_t right_shift) {
@@ -2127,9 +2173,8 @@ double av1_tpl_compute_frame_mv_entropy(const TplDepFrame *tpl_frame,
 
   for (int row = 0; row < tpl_frame->mi_rows; row += step) {
     for (int col = 0; col < tpl_frame->mi_cols; col += step) {
-      const TplDepStats *tpl_stats = &tpl_frame->tpl_stats_ptr[av1_tpl_ptr_pos(
-          row, col, tpl_stride, right_shift)];
-      int_mv mv = tpl_stats->mv[tpl_stats->ref_frame_index[0]];
+      int_mv mv = optimize_for_neighbor_mvs(tpl_frame, row, col, step,
+                                            tpl_stride, right_shift);
       count_row[clamp(mv.as_mv.row, 0, 499)] += 1;
       count_col[clamp(mv.as_mv.row, 0, 499)] += 1;
       n += 1;
