98738a21988e8815e7ed255ee3598f73a0a5b873
Debargha Mukherjee <debargha@google.com>
2021-08-19 00:21:53 -0700

    Redesign selective ref frame in new ref signaling
    Change-Id: I304dd68bc8ec2ebe6f25524c75fb8a7ffe6193f7

--- av1/encoder/rdopt.h
+++ av1/encoder/rdopt.h
@@ -304,6 +304,64 @@ static INLINE int prune_ref_by_selective_ref_frame(
 #if CONFIG_NEW_REF_SIGNALING
 static INLINE int prune_ref_by_selective_ref_frame_nrs(
     const AV1_COMP *const cpi, const MACROBLOCK *const x,
+    const MV_REFERENCE_FRAME_NRS *const ref_frame) {
+  (void)x;
+  const AV1_COMMON *const cm = &cpi->common;
+  const SPEED_FEATURES *const sf = &cpi->sf;
+
+  if (!sf->inter_sf.selective_ref_frame) return 0;
+
+  const int comp_pred =
+      (ref_frame[1] != INVALID_IDX && ref_frame[1] != INTRA_FRAME_NRS);
+
+  int dir_refrank[2][2];
+  int d0 = get_dir_rank(cm, ref_frame[0], dir_refrank[0]);
+  int d1 = -1;
+  if (comp_pred) d1 = get_dir_rank(cm, ref_frame[1], dir_refrank[1]);
+  const int one_sided_comp = (d0 == d1);
+
+  switch (sf->inter_sf.selective_ref_frame) {
+    case 0: return 0;
+    case 1:
+      if (comp_pred) {
+        if (one_sided_comp) {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 1) return 1;
+        } else {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 2) return 1;
+        }
+      } else {
+        if (dir_refrank[0][d0] > MAX_REF_FRAMES_NRS / 2) return 1;
+      }
+      break;
+    case 2:
+      if (comp_pred) {
+        if (one_sided_comp) {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 0) return 1;
+        } else {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 1) return 1;
+        }
+      } else {
+        if (dir_refrank[0][d0] > MAX_REF_FRAMES_NRS / 2 - 1) return 1;
+      }
+      break;
+    case 3:
+    default:
+      if (comp_pred) {
+        if (one_sided_comp) {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 0) return 1;
+        } else {
+          if (AOMMIN(dir_refrank[0][d0], dir_refrank[1][d1]) > 0) return 1;
+        }
+      } else {
+        if (dir_refrank[0][d0] > MAX_REF_FRAMES_NRS / 2 - 2) return 1;
+      }
+      break;
+  }
+  return 0;
+}
+
+static INLINE int prune_ref_by_selective_ref_frame_nrs_tmp(
+    const AV1_COMP *const cpi, const MACROBLOCK *const x,
     const MV_REFERENCE_FRAME_NRS *const ref_frame,
     const unsigned int *const ref_display_order_hint) {
   const AV1_COMMON *const cm = &cpi->common;
