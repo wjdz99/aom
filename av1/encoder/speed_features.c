2e6624aa451d12f9bb2cb297d40f007f0c15c908
Yunqing Wang <yunqingwang@google.com>
2021-08-24 12:14:14 -0700

    Disable use_fullpel_costlist
    STATS_CHANGED
    
    Change-Id: I58bea1fa97bb4a799b6f4e1a20959dca4c19bc23

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1368,7 +1368,7 @@ static void set_rt_speed_features_framesize_independent(AV1_COMP *cpi,
 
     sf->mv_sf.search_method = FAST_DIAMOND;
     sf->mv_sf.subpel_force_stop = QUARTER_PEL;
-    sf->mv_sf.use_fullpel_costlist = 1;
+    //sf->mv_sf.use_fullpel_costlist = 1;
     sf->mv_sf.subpel_search_method = SUBPEL_TREE_PRUNED_MORE;
 
     sf->inter_sf.inter_mode_rd_model_estimation = 2;
