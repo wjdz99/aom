7f8de1e2272877bfab75095e5cb0f0d47161ccfc
Yunqing Wang <yunqingwang@google.com>
2021-08-24 12:29:48 -0700

    Disable use_real_time_ref_set
    Change-Id: I87a7cf4b18fda32c0f1c660bb0b54e19796ebd42

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1401,7 +1401,7 @@ static void set_rt_speed_features_framesize_independent(AV1_COMP *cpi,
     sf->rt_sf.mode_search_skip_flags |= FLAG_SKIP_INTRA_DIRMISMATCH;
     sf->rt_sf.num_inter_modes_for_tx_search = 5;
     sf->rt_sf.skip_interp_filter_search = 1;
-    sf->rt_sf.use_real_time_ref_set = 1;
+    //sf->rt_sf.use_real_time_ref_set = 1;
     sf->rt_sf.use_simple_rd_model = 1;
 
     sf->rt_sf.check_scene_detection = 1;
