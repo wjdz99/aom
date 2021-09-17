def993e6b819a9e1092cdb81e61e44890bcf6a18
Link.Meng <monthev@gmail.com>
2021-09-17 22:23:20 +0800

    fix cpu detect on apple m1 arm64 BUG=aomedia:3147
    Change-Id: Id9b40c5120ecd2b6f10da6fa51be64d8dab791fe

--- aom_ports/arm_cpudetect.c
+++ aom_ports/arm_cpudetect.c
@@ -38,7 +38,7 @@ static int arm_cpu_env_mask(void) {
   return env && *env ? (int)strtol(env, NULL, 0) : ~0;
 }
 
-#if !CONFIG_RUNTIME_CPU_DETECT
+#if defined(__APPLE__) || !CONFIG_RUNTIME_CPU_DETECT
 
 int aom_arm_cpu_caps(void) {
   /* This function should actually be a no-op. There is no way to adjust any of
@@ -56,7 +56,7 @@ int aom_arm_cpu_caps(void) {
   return flags & mask;
 }
 
-#elif defined(_MSC_VER) /* end !CONFIG_RUNTIME_CPU_DETECT */
+#elif defined(_MSC_VER) /* defined(__APPLE__) || !CONFIG_RUNTIME_CPU_DETECT */
 /*For GetExceptionCode() and EXCEPTION_ILLEGAL_INSTRUCTION.*/
 #define WIN32_LEAN_AND_MEAN
 #define WIN32_EXTRA_LEAN
