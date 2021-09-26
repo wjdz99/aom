f84bd1f7d0a5bb216833572d037b15280bd4b0a8
Ryan Lei <ryanlei@fb.com>
2021-09-26 15:10:32 -0700

    add support for qp adjustment for HDR video
    Change-Id: I99757226aea84d415f3fcf405ef3f4d90bdbf7dd

--- av1/encoder/firstpass.c
+++ av1/encoder/firstpass.c
@@ -1273,7 +1273,8 @@ void av1_first_pass(AV1_COMP *cpi, const int64_t ts_duration) {
 
   av1_set_quantizer(cm, cpi->oxcf.q_cfg.qm_minlevel,
                     cpi->oxcf.q_cfg.qm_maxlevel, qindex,
-                    cpi->oxcf.q_cfg.enable_chroma_deltaq);
+                    cpi->oxcf.q_cfg.enable_chroma_deltaq,
+                    cpi->oxcf.q_cfg.enable_hdr_deltaq);
 
   av1_setup_block_planes(xd, seq_params->subsampling_x,
                          seq_params->subsampling_y, num_planes);
