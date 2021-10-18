6412f48adf6704a016e07df589d118baad3e7dfc
Wan-Teh Chang <wtc@google.com>
2021-10-18 15:57:11 -0700

    Fix GCC 8.3.0 -Wmaybe-uninitialized on left_col
    Fix the GCC 8.3.0 warnings that left_col and left_row may be used
    uninitialized in the av1_nonrd_pick_inter_mode_sb function. Note that
    those two variables are actually in the newmv_diff_bias function, which
    is apparently inlined into the av1_nonrd_pick_inter_mode_sb function by
    the compiler.
    
    BUG=aomedia:3174
    
    Change-Id: I2da7cd6e486e96ea2e34937ac23ed8df15a68d28

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -1055,12 +1055,13 @@ static void newmv_diff_bias(MACROBLOCKD *xd, PREDICTION_MODE this_mode,
   if (this_mode == NEWMV) {
     int al_mv_average_row;
     int al_mv_average_col;
-    int left_row, left_col;
     int row_diff, col_diff;
     int above_mv_valid = 0;
     int left_mv_valid = 0;
-    int above_row = 0;
-    int above_col = 0;
+    // -32768, -32768 indicates an invalid motion vector. See Sec. 7.9.1 of the
+    // AV1 spec.
+    int above_row = -32768, above_col = -32768;
+    int left_row = -32768, left_col = -32768;
     if (bsize >= BLOCK_64X64 && content_state_sb.source_sad != kHighSad &&
         spatial_variance < 300 &&
         (mv_row > 16 || mv_row < -16 || mv_col > 16 || mv_col < -16)) {
