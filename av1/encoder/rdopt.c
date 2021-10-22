cb5c3bd3ef899514a45cb2cbd4f7873261db2ded
Debargha Mukherjee <debargha@google.com>
2021-10-22 00:39:14 -0700

    Enhance compound ref signaling with side context
    For compound modes signal the second reference with different
    contexts for same-sided and opposite-sided compounds.
    
    Change-Id: Iaaf69e291f1ac3ee4520600b66515c0500429a61

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -1122,13 +1122,16 @@ static AOM_INLINE void estimate_ref_frame_costs(
             ref_costs_comp[j][i] = prev_cost;
             if (j < n_refs - 1) {
               aom_cdf_prob ctx = av1_get_ref_pred_context_nrs(xd, j, n_refs);
+              const int bit_type = av1_get_compound_ref_bit_type(
+                  1, &cm->new_ref_frame_data, i, j);
               ref_costs_comp[i][j] +=
-                  mode_costs->compound_ref_cost[ctx][1][j - 1][1];
+                  mode_costs->compound_ref_cost[ctx][bit_type][j - 1][1];
               ref_costs_comp[j][i] +=
-                  mode_costs->compound_ref_cost[ctx][1][j - 1][1];
+                  mode_costs->compound_ref_cost[ctx][bit_type][j - 1][1];
               // Maintain the cost of sending a 0 bit for the 2nd reference to
               // be used in the next iteration.
-              prev_cost += mode_costs->compound_ref_cost[ctx][1][j - 1][0];
+              prev_cost +=
+                  mode_costs->compound_ref_cost[ctx][bit_type][j - 1][0];
             }
           }
         }
