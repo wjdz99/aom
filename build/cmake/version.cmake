0bbd4842bffc395ae2301017bc26466df93ccc18
James Zern <jzern@google.com>
2021-10-25 13:51:28 -0700

    version.cmake: filter git describe matches
    only use those that start with v[0-9] to avoid picking up a newer tag
    which may not match the expected vX.Y.Z format. this is the same
    invocation used by libvpx.
    
    Bug: aomedia:3177
    Fixed: aomedia:3177
    Change-Id: I7dbab56088b32d33442f8a40dae0f1fdd3e2dc88

--- build/cmake/version.cmake
+++ build/cmake/version.cmake
@@ -25,6 +25,7 @@ include("${AOM_ROOT}/build/cmake/util.cmake")
 unset(aom_version)
 if(EXISTS "${GIT_EXECUTABLE}")
   execute_process(COMMAND ${GIT_EXECUTABLE} --git-dir=${AOM_ROOT}/.git describe
+                          --match=v[0-9]*
                   OUTPUT_VARIABLE aom_version
                   ERROR_QUIET
                   RESULT_VARIABLE version_check_result)
