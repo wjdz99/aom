b92d6e15b024ce657978788c7fed7d2f38468e47
Angie Chiang <angiebird@google.com>
2021-10-29 18:30:45 -0700

    Apply filtering before checking shorter gop
    av1_tf_info_filtering() is called before
    is_shorter_gf_interval_better()
    
    av1_tf_info_filtering() is also modified to be called
    multiple times without introduce extra computation
    
    This CL will following impact
    1) Removes the usage of alt_ref_buffer in
       is_shorter_gf_interval_better
    2) Provides 0.1~0.2% ssim gain on midres/hdres/hdres2
    
    q mode --lag-in-frames=48
    (negative is better)
    avg_psnr ovr_psnr ssim
    ugc360p -0.026  -0.022 -0.094
    lowres   0.003  -0.002 -0.076
    midres  -0.013  -0.017 -0.142
    hdres   -0.010  -0.025 -0.208
    hdres2  -0.083  -0.086 -0.212
    
    STATS_CHANGED
    
    Change-Id: I1de9d67c2940fc75bcfb51540591af30ec97b9b6

--- av1/encoder/temporal_filter.h
+++ av1/encoder/temporal_filter.h
@@ -156,6 +156,10 @@ typedef struct TEMPORAL_FILTER_INFO {
    */
   int tf_buf_gf_index[TF_INFO_BUF_COUNT];
   /*!
+   * the display_index offset between next show frame and the frames in the GOP
+   */
+  int tf_buf_display_index_offset[TF_INFO_BUF_COUNT];
+  /*!
    * whether the buf is valid or not.
    */
   int tf_buf_valid[TF_INFO_BUF_COUNT];
@@ -171,6 +175,11 @@ void av1_tf_info_alloc(TEMPORAL_FILTER_INFO *tf_info, struct AV1_COMP *cpi);
  * \param[in,out]   tf_info           Temporal filter info for a gop
  */
 void av1_tf_info_free(TEMPORAL_FILTER_INFO *tf_info);
+
+/*!\brief Reset validity of tf_buf in TEMPORAL_FILTER_INFO
+ * \param[in,out]   tf_info           Temporal filter info for a gop
+ */
+void av1_tf_info_reset(TEMPORAL_FILTER_INFO *tf_info);
 
 /*!\brief Apply temporal filter for key frame and ARF in a gop
  * \param[in,out]   tf_info           Temporal filter info for a gop
