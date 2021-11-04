69341aea525b8b279cb2c7f805c5130a7176ada6
Marco Paniconi <marpan@google.com>
2021-11-04 00:09:01 -0700

    rtc: Speed feature to switch to half_pel at block level
    Add speed feature, set at speed >= 10, to allow for
    switching to half_pel for newmv at coding block level.
    Switch is based on the size of the integer motion vector,
    along with bsize and avg_frame_low_motion.
    
    avg. bdrate loss ~0.8 on rtc_set, max is 1.6 for dark720p
    speedup ~2% on vga.
    
    Change-Id: I38201e2a9d5ac8336045b2b58168686b541d2933

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -143,6 +143,26 @@ static INLINE void init_best_pickmode(BEST_PICKMODE *bp) {
   memset(&bp->wm_params, 0, sizeof(bp->wm_params));
 }
 
+static INLINE void subpel_select(AV1_COMP *cpi, BLOCK_SIZE bsize,
+                                 int_mv *mv, int subpel_orig) {
+  if (!cpi->sf.rt_sf.force_half_pel_block ||
+      subpel_orig >= HALF_PEL) {
+    cpi->sf.mv_sf.subpel_force_stop = subpel_orig;
+  } else {
+    int mv_thresh = 4;
+    mv_thresh = (bsize > BLOCK_32X32) ? 2 : (bsize > BLOCK_16X16) ? 4 : 6;
+    if (cpi->rc.avg_frame_low_motion > 0 &&
+        cpi->rc.avg_frame_low_motion < 40)
+      mv_thresh = 12;
+    if (abs(mv->as_fullmv.row) >= mv_thresh ||
+        abs(mv->as_fullmv.col) >= mv_thresh)
+      cpi->sf.mv_sf.subpel_force_stop = HALF_PEL;
+    else
+      cpi->sf.mv_sf.subpel_force_stop = subpel_orig;
+  }
+}
+
+
 /*!\brief Runs Motion Estimation for a specific block and specific ref frame.
  *
  * \ingroup nonrd_mode_search
@@ -228,12 +248,15 @@ static int combined_motion_search(AV1_COMP *cpi, MACROBLOCK *x,
 
   if (rv && search_subpel) {
     SUBPEL_MOTION_SEARCH_PARAMS ms_params;
+    int subpel_orig = cpi->sf.mv_sf.subpel_force_stop;
+    subpel_select(cpi, bsize, tmp_mv, subpel_orig);
     av1_make_default_subpel_ms_params(&ms_params, cpi, x, bsize, &ref_mv,
                                       cost_list);
     MV subpel_start_mv = get_mv_from_fullmv(&tmp_mv->as_fullmv);
     cpi->mv_search_params.find_fractional_mv_step(
         xd, cm, &ms_params, subpel_start_mv, &tmp_mv->as_mv, &dis,
         &x->pred_sse[ref], NULL);
+    cpi->sf.mv_sf.subpel_force_stop = subpel_orig;
 
     *rate_mv =
         av1_mv_bit_cost(&tmp_mv->as_mv, &ref_mv, x->mv_costs->nmv_joint_cost,
@@ -313,12 +336,15 @@ static int search_new_mv(AV1_COMP *cpi, MACROBLOCK *x,
     frame_mv[NEWMV][ref_frame].as_mv.col >>= 3;
 
     SUBPEL_MOTION_SEARCH_PARAMS ms_params;
+    int subpel_orig = cpi->sf.mv_sf.subpel_force_stop;
+    subpel_select(cpi, bsize, &best_mv, subpel_orig);
     av1_make_default_subpel_ms_params(&ms_params, cpi, x, bsize, &ref_mv,
                                       cost_list);
     MV start_mv = get_mv_from_fullmv(&best_mv.as_fullmv);
     cpi->mv_search_params.find_fractional_mv_step(
         xd, cm, &ms_params, start_mv, &best_mv.as_mv, &dis,
         &x->pred_sse[ref_frame], NULL);
+    cpi->sf.mv_sf.subpel_force_stop = subpel_orig;
     frame_mv[NEWMV][ref_frame].as_int = best_mv.as_int;
   } else if (!combined_motion_search(cpi, x, bsize, mi_row, mi_col,
                                      &frame_mv[NEWMV][ref_frame], rate_mv,
