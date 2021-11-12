70aef4681355e8f24a8b80bae6ae87df4816e01f
James Zern <jzern@google.com>
2021-11-12 10:20:44 -0800

    sadx4d_sse2.asm: remove remaining x4d_avg support
    Change-Id: I53191ef53e1e774924db13a6e0740be6d8fef607

--- aom_dsp/x86/sad4d_sse2.asm
+++ aom_dsp/x86/sad4d_sse2.asm
@@ -15,31 +15,9 @@
 
 SECTION .text
 
-%macro AVG_4x2x4 2
-  movh                  m2, [second_predq]
-  movlhps               m2, m2
-  pavgb                 %1, m2
-  pavgb                 %2, m2
-  lea                   second_predq, [second_predq+8]
-%endmacro
-; 'mflag' affect a lot how the code works.
-;
-; When 'mflag' is false, the 'src_strideq' resides in register,
-; [srcq + src_strideq + offset] is allowed, so we can simply
-; use such form to access src memory and don't bother to update
-; 'srcq' at each line. We only update 'srcq' each two-lines using
-; a compact LEA instruction like [srcq+src_strideq*2].
-;
-; When 'mflag' is true, the 'src_strideq' resides in memory.
-; we cannot use above form to access memory, we have to update
-; 'srcq' at each line break. As we process two parts (first,second)
-; together in each macro function, the second part may also sit
-; in the next line, which means we also need to possibly add
-; one 'src_strideq' to 'srcq' before processing second part.
-
 %macro HANDLE_FIRST_OFFSET 2
   %define first_offset %2
-  %if mflag == 0 && %1 == 1
+  %if %1 == 1
     %define first_offset (src_strideq + %2)
   %endif
 %endmacro
@@ -47,10 +25,7 @@ SECTION .text
 ; first_extraline, second_extraline, in_line_offset
 %macro HANDLE_SECOND_OFFSET 3
   %define second_offset %3
-  %if mflag && %1 == 0 && %2 == 1
-    add srcq, src_strideq
-  %endif
-  %if mflag == 0 && %2 == 1
+  %if %2 == 1
     %define second_offset (src_strideq + %3)
   %endif
 %endmacro
@@ -64,10 +39,7 @@ SECTION .text
 ; complex address calculation using LEA, and fall back to
 ; using simple ADD instruction at each line ending.
 %macro ADVANCE_END_OF_LINE 1
-  %if mflag
-    add srcq, src_strideq
-  %endif
-  %if mflag == 0 && %1 == 2
+  %if %1 == 2
     lea                 srcq, [srcq +src_strideq*2]
   %endif
 
@@ -81,12 +53,12 @@ SECTION .text
 
 ; Please note that the second_offset of src is for in_line_offset,
 ; so it is less than src_stride.
-; PROCESS_4x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_4x2x4 first, off_{first,second}_{src,ref},
 ;               {first, second}_extraline, line_ending
-%macro PROCESS_4x2x4 9
-  HANDLE_FIRST_OFFSET   %7, %2
+%macro PROCESS_4x2x4 8
+  HANDLE_FIRST_OFFSET   %6, %2
   movd                  m0, [srcq + first_offset]
-  HANDLE_SECOND_OFFSET  %7, %8, %4
+  HANDLE_SECOND_OFFSET  %6, %7, %4
 %if %1 == 1
   movd                  m6, [ref1q+%3]
   movd                  m4, [ref2q+%3]
@@ -106,9 +78,6 @@ SECTION .text
   movlhps               m0, m0
   movlhps               m6, m4
   movlhps               m7, m5
-%if %6 == 1
-  AVG_4x2x4             m6, m7
-%endif
   psadbw                m6, m0
   psadbw                m7, m0
 %else
@@ -129,25 +98,22 @@ SECTION .text
   movlhps               m0, m0
   movlhps               m1, m2
   movlhps               m3, m4
-%if %6 == 1
-  AVG_4x2x4             m1, m3
-%endif
   psadbw                m1, m0
   psadbw                m3, m0
   paddd                 m6, m1
   paddd                 m7, m3
 %endif
-%if %9 > 0
-  ADVANCE_END_OF_LINE %9
+%if %8 > 0
+  ADVANCE_END_OF_LINE %8
 %endif
 %endmacro
 
-; PROCESS_8x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_8x2x4 first, off_{first,second}_{src,ref},
 ;               {first,second}_extraline, line_ending
-%macro PROCESS_8x2x4 9
-  HANDLE_FIRST_OFFSET   %7, %2
+%macro PROCESS_8x2x4 8
+  HANDLE_FIRST_OFFSET   %6, %2
   movh                  m0, [srcq + first_offset]
-  HANDLE_SECOND_OFFSET  %7, %8, %4
+  HANDLE_SECOND_OFFSET  %6, %7, %4
 %if %1 == 1
   movh                  m4, [ref1q+%3]
   movh                  m5, [ref2q+%3]
@@ -158,14 +124,6 @@ SECTION .text
   movhps                m5, [ref2q+%5]
   movhps                m6, [ref3q+%5]
   movhps                m7, [ref4q+%5]
-%if %6 == 1
-  movu                  m3, [second_predq]
-  pavgb                 m4, m3
-  pavgb                 m5, m3
-  pavgb                 m6, m3
-  pavgb                 m7, m3
-  lea                   second_predq, [second_predq+mmsize]
-%endif
   psadbw                m4, m0
   psadbw                m5, m0
   psadbw                m6, m0
@@ -176,11 +134,6 @@ SECTION .text
   movhps                m0, [srcq + second_offset]
   movhps                m1, [ref1q+%5]
   movhps                m2, [ref2q+%5]
-%if %6 == 1
-  movu                  m3, [second_predq]
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m4, m1
@@ -190,41 +143,28 @@ SECTION .text
   movhps                m1, [ref3q+%5]
   movh                  m2, [ref4q+%3]
   movhps                m2, [ref4q+%5]
-%if %6 == 1
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-  lea                   second_predq, [second_predq+mmsize]
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m6, m1
   paddd                 m7, m2
 %endif
-%if %9 > 0
-  ADVANCE_END_OF_LINE %9
+%if %8 > 0
+  ADVANCE_END_OF_LINE %8
 %endif
 %endmacro
 
-; PROCESS_16x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_16x2x4 first, off_{first,second}_{src,ref},
 ;                {first,second}_extraline, line_ending
-%macro PROCESS_16x2x4 9
+%macro PROCESS_16x2x4 8
   ; 1st 16 px
-  HANDLE_FIRST_OFFSET   %7, %2
+  HANDLE_FIRST_OFFSET   %6, %2
   mova                  m0, [srcq + first_offset]
-  HANDLE_SECOND_OFFSET  %7, %8, %4
+  HANDLE_SECOND_OFFSET  %6, %7, %4
 %if %1 == 1
   movu                  m4, [ref1q+%3]
   movu                  m5, [ref2q+%3]
   movu                  m6, [ref3q+%3]
   movu                  m7, [ref4q+%3]
-%if %6 == 1
-  movu                  m3, [second_predq]
-  pavgb                 m4, m3
-  pavgb                 m5, m3
-  pavgb                 m6, m3
-  pavgb                 m7, m3
-  lea                   second_predq, [second_predq+mmsize]
-%endif
   psadbw                m4, m0
   psadbw                m5, m0
   psadbw                m6, m0
@@ -232,11 +172,6 @@ SECTION .text
 %else ; %1 == 1
   movu                  m1, [ref1q+%3]
   movu                  m2, [ref2q+%3]
-%if %6 == 1
-  movu                  m3, [second_predq]
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m4, m1
@@ -244,11 +179,6 @@ SECTION .text
 
   movu                  m1, [ref3q+%3]
   movu                  m2, [ref4q+%3]
-%if %6 == 1
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-  lea                   second_predq, [second_predq+mmsize]
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m6, m1
@@ -260,11 +190,6 @@ SECTION .text
   movu                  m1, [ref1q+%5]
   movu                  m2, [ref2q+%5]
 
-%if %6 == 1
-  movu                  m3, [second_predq]
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m4, m1
@@ -273,40 +198,35 @@ SECTION .text
   movu                  m1, [ref3q+%5]
   movu                  m2, [ref4q+%5]
 
-%if %9 > 0
-  ADVANCE_END_OF_LINE %9
+%if %8 > 0
+  ADVANCE_END_OF_LINE %8
 %endif
 
-%if %6 == 1
-  pavgb                 m1, m3
-  pavgb                 m2, m3
-  lea                   second_predq, [second_predq+mmsize]
-%endif
   psadbw                m1, m0
   psadbw                m2, m0
   paddd                 m6, m1
   paddd                 m7, m2
 %endmacro
 
-; PROCESS_32x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_32x2x4 first, off_{first,second}_{src,ref},
 ;                {first,second}_extraline, line_ending
-%macro PROCESS_32x2x4 9
-  PROCESS_16x2x4 %1, %2, %3, %2 + 16, %3 + 16, %6, %7, %7, %8 - %7
-  PROCESS_16x2x4  0, %4, %5, %4 + 16, %5 + 16, %6, %8, %8, %9
+%macro PROCESS_32x2x4 8
+  PROCESS_16x2x4 %1, %2, %3, %2 + 16, %3 + 16, %6, %6, %7 - %6
+  PROCESS_16x2x4  0, %4, %5, %4 + 16, %5 + 16, %7, %7, %8
 %endmacro
 
-; PROCESS_64x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_64x2x4 first, off_{first,second}_{src,ref},
 ;                {first,second}_extraline, line_ending
-%macro PROCESS_64x2x4 9
-  PROCESS_32x2x4 %1, %2, %3, %2 + 32, %3 + 32, %6, %7, %7, %8 - %7
-  PROCESS_32x2x4  0, %4, %5, %4 + 32, %5 + 32, %6, %8, %8, %9
+%macro PROCESS_64x2x4 8
+  PROCESS_32x2x4 %1, %2, %3, %2 + 32, %3 + 32, %6, %6, %7 - %6
+  PROCESS_32x2x4  0, %4, %5, %4 + 32, %5 + 32, %7, %7, %8
 %endmacro
 
-; PROCESS_128x2x4 first, off_{first,second}_{src,ref}, do_avg,
+; PROCESS_128x2x4 first, off_{first,second}_{src,ref},
 ;                 {first,second}_extraline, line_ending
-%macro PROCESS_128x2x4 9
-  PROCESS_64x2x4 %1, %2, %3, %2 + 64, %3 + 64, %6, %7, %7, %8 - %7
-  PROCESS_64x2x4  0, %4, %5, %4 + 64, %5 + 64, %6, %8, %8, %9
+%macro PROCESS_128x2x4 8
+  PROCESS_64x2x4 %1, %2, %3, %2 + 64, %3 + 64, %6, %6, %7 - %6
+  PROCESS_64x2x4  0, %4, %5, %4 + 64, %5 + 64, %7, %7, %8
 %endmacro
 
 ; void aom_sadNxNx4d_sse2(uint8_t *src,    int src_stride,
@@ -315,11 +235,9 @@ SECTION .text
 ; Macro Arguments:
 ;   1: Width
 ;   2: Height
-;   3: If 0, then normal sad, else avg
-;   4: If 0, then normal sad, else skip rows
-;   TODO(jzern): remove the remnants of avg and merge %3 & %4
-%macro SADNXN4D 2-4 0,0
-%if %4 == 1  ; skip rows
+;   3: If 0, then normal sad, else skip rows
+%macro SADNXN4D 2-3 0
+%if %3 == 1  ; skip rows
 %if ARCH_X86_64
 cglobal sad_skip_%1x%2x4d, 5, 8, 8, src, src_stride, ref1, ref_stride, \
                               res, ref2, ref3, ref4
@@ -338,8 +256,7 @@ cglobal sad%1x%2x4d, 4, 7, 8, src, src_stride, ref1, ref_stride, \
 %endif
 %endif
 
-  %define mflag ((1 - ARCH_X86_64) & %3)
-%if %4 == 1
+%if %3 == 1
   lea          src_strided, [2*src_strided]
   lea          ref_strided, [2*ref_strided]
 %endif
@@ -351,17 +268,17 @@ cglobal sad%1x%2x4d, 4, 7, 8, src, src_stride, ref1, ref_stride, \
   mov                ref4q, [ref1q+gprsize*3]
   mov                ref1q, [ref1q+gprsize*0]
 
-  PROCESS_%1x2x4 1, 0, 0, 0, ref_strideq, %3, 0, 1, 2
-%if %4 == 1  ; downsample number of rows by 2
+  PROCESS_%1x2x4 1, 0, 0, 0, ref_strideq, 0, 1, 2
+%if %3 == 1  ; downsample number of rows by 2
 %define num_rep (%2-8)/4
 %else
 %define num_rep (%2-4)/2
 %endif
 %rep num_rep
-  PROCESS_%1x2x4 0, 0, 0, 0, ref_strideq, %3, 0, 1, 2
+  PROCESS_%1x2x4 0, 0, 0, 0, ref_strideq, 0, 1, 2
 %endrep
 %undef num_rep
-  PROCESS_%1x2x4 0, 0, 0, 0, ref_strideq, %3, 0, 1, 2
+  PROCESS_%1x2x4 0, 0, 0, 0, ref_strideq, 0, 1, 2
 
 %if %3 == 0
   %define resultq r4
@@ -378,7 +295,7 @@ cglobal sad%1x%2x4d, 4, 7, 8, src, src_stride, ref1, ref_stride, \
   punpcklqdq            m4, m6
   punpckhqdq            m5, m7
   paddd                 m4, m5
-%if %4 == 1
+%if %3 == 1
   pslld                 m4, 1
 %endif
   movifnidn             resultq, resultmp
@@ -387,7 +304,7 @@ cglobal sad%1x%2x4d, 4, 7, 8, src, src_stride, ref1, ref_stride, \
 %else
   pshufd            m6, m6, 0x08
   pshufd            m7, m7, 0x08
-%if %4 == 1
+%if %3 == 1
   pslld                 m6, 1
   pslld                 m7, 1
 %endif
@@ -423,26 +340,26 @@ SADNXN4D  32,   8
 SADNXN4D  16,  64
 SADNXN4D  64,  16
 %endif
-SADNXN4D 128, 128, 0, 1
-SADNXN4D 128,  64, 0, 1
-SADNXN4D  64, 128, 0, 1
-SADNXN4D  64,  64, 0, 1
-SADNXN4D  64,  32, 0, 1
-SADNXN4D  32,  64, 0, 1
-SADNXN4D  32,  32, 0, 1
-SADNXN4D  32,  16, 0, 1
-SADNXN4D  16,  32, 0, 1
-SADNXN4D  16,  16, 0, 1
-SADNXN4D  16,   8, 0, 1
-SADNXN4D   8,  16, 0, 1
-SADNXN4D   8,   8, 0, 1
-SADNXN4D   4,   8, 0, 1
+SADNXN4D 128, 128, 1
+SADNXN4D 128,  64, 1
+SADNXN4D  64, 128, 1
+SADNXN4D  64,  64, 1
+SADNXN4D  64,  32, 1
+SADNXN4D  32,  64, 1
+SADNXN4D  32,  32, 1
+SADNXN4D  32,  16, 1
+SADNXN4D  16,  32, 1
+SADNXN4D  16,  16, 1
+SADNXN4D  16,   8, 1
+SADNXN4D   8,  16, 1
+SADNXN4D   8,   8, 1
+SADNXN4D   4,   8, 1
 %if CONFIG_REALTIME_ONLY==0
-SADNXN4D   4,  16, 0, 1
-SADNXN4D   8,  32, 0, 1
-SADNXN4D  32,   8, 0, 1
-SADNXN4D  16,  64, 0, 1
-SADNXN4D  64,  16, 0, 1
+SADNXN4D   4,  16, 1
+SADNXN4D   8,  32, 1
+SADNXN4D  32,   8, 1
+SADNXN4D  16,  64, 1
+SADNXN4D  64,  16, 1
 %endif
 
 ; Different assembly is needed when the height gets subsampled to 2
