540a65d2542ac0d936efb1f9c590a21d70e00af4
Marco Paniconi <marpan@google.com>
2021-12-02 00:12:58 -0800

    rtc-svc: Fix to avoid removing references in svc
    For svc the references are set externally and
    should not be removed. And only allow for pruning golden
    and altref in nonrd pickmode if last is a reference.
    
    Change-Id: I8f77ad87b280e9f80693c9a1686b6fef1164bc3e

--- av1/encoder/tpl_model.c
+++ av1/encoder/tpl_model.c
@@ -1100,7 +1100,6 @@ static AOM_INLINE void init_mc_flow_dispenser(AV1_COMP *cpi, int frame_idx,
   ref_frame_flags =
       get_ref_frame_flags(&cpi->sf, is_one_pass_rt_params(cpi),
                           ref_frames_ordered, cpi->ext_flags.ref_frame_flags);
-
   enforce_max_ref_frames(cpi, &ref_frame_flags, ref_frame_display_indices,
                          tpl_frame->frame_display_index);
 
