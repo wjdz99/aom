6b961506836abd3d4324400699da3a751a0c7bbc
BlueSwordM <zakdjeb@gmail.com>
2021-12-05 02:21:06 -0500

    Add deltaq-mode=5 HDR description to aomenc
    Change-Id: Id513c80007412eef02eb682f341c6df66f320e72

--- av1/arg_defs.c
+++ av1/arg_defs.c
@@ -518,7 +518,8 @@ const av1_codec_arg_definitions_t g_av1_codec_arg_defs = {
       ARG_DEF(NULL, "deltaq-mode", 1,
               "Delta qindex mode (0: off, 1: deltaq objective (default), "
               "2: deltaq placeholder, 3: key frame visual quality, 4: user "
-              "rating based visual quality optimization). "
+              "rating based visual quality optimization,
+              5: HDR deltaq optimization). "
               "Currently this requires enable-tpl-model as a prerequisite."),
   .deltaq_strength = ARG_DEF(NULL, "deltaq-strength", 1,
                              "Deltaq strength for"
