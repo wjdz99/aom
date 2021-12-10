2b0632d9169b59484688c48d0c4e216c3400bd1f
James Zern <jzern@google.com>
2021-12-09 19:24:26 -0800

    av1_initalize_enc(): protect against multiple invocations
    this removes the burden from callers; the rtcd functions are left with a
    mostly redundant (outside of tests) once() as top-level functions should
    ensure their constraints are met
    
    Change-Id: I339e49b34527ea09ca407d48847c58c0f6fcfbcc

--- av1/common/av1_rtcd.c
+++ av1/common/av1_rtcd.c
@@ -16,7 +16,5 @@
 #include "aom_ports/aom_once.h"
 
 void av1_rtcd() {
-  // TODO(JBB): Remove this aom_once, by insuring that both the encoder and
-  // decoder setup functions are protected by aom_once();
   aom_once(setup_rtcd_internal);
 }
