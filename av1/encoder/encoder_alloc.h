45760a647cbdc9fcc4f1ae8b5d77d471ccbbd38e
Yunqing Wang <yunqingwang@google.com>
2022-01-28 16:09:41 -0800

    wip: test
    Change-Id: I1807bd0cafe49f86d228693d93689e67b38d2cd4

--- av1/encoder/encoder_alloc.h
+++ av1/encoder/encoder_alloc.h
@@ -273,6 +273,7 @@ static AOM_INLINE void dealloc_compressor_data(AV1_COMP *cpi) {
   aom_free_frame_buffer(&cpi->trial_frame_rst);
   aom_free_frame_buffer(&cpi->scaled_source);
   aom_free_frame_buffer(&cpi->scaled_last_source);
+  aom_free_frame_buffer(&cpi->orig_source);   // rtc
 
   free_token_info(token_info);
 
