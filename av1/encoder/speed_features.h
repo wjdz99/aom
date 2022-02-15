eec738b5a0e18e2ee791a0ecb52f5fcb26f6ed83
Jayasanker J <jayasanker.j@ittiam.com>
2022-02-15 13:13:21 +0530

    Allintra: Prune SPLIT in var based partitioning
    The sf vbp_prune_16x16_split_using_min_max_sub_blk_var is
    introduced in var based partitioning to choose between SPLIT
    or NONE partitioning based on the minimum and maximum sub-block
    variances. This sf is currently enabled only for bsize 16X16.
    
    For AVIF still-image encode,
    
                 Encode Time      BD-Rate Loss(%)
    cpu-used     Reduction(%)     psnr       ssim
        9           8.436        -0.7805    -0.4677
    
    STATS_CHANGED
    
    Change-Id: I8dc8726d25a15fd4579273851499ffaa4edf2d59

--- av1/encoder/speed_features.h
+++ av1/encoder/speed_features.h
@@ -1502,6 +1502,14 @@ typedef struct REAL_TIME_SPEED_FEATURES {
 
   // Reduce the mv resolution for zero mv if the variance is low.
   bool reduce_zeromv_mvres;
+
+  // Avoid the partitioning of a 16x16 block in variance based partitioning by
+  // making use of minimum and maximum sub-block variances.
+  // For allintra encode, this sf reduces instruction count by 5.39% for speed 9
+  // on a typical video dataset with coding performance gain of 1.44%.
+  // For AVIF image encode, this sf reduces encode time by 8.44% for speed 9 on
+  // a typical image dataset with coding performance gain of 0.78%.
+  int vbp_prune_16x16_split_using_min_max_sub_blk_var;
 } REAL_TIME_SPEED_FEATURES;
 
 /*!\endcond */
