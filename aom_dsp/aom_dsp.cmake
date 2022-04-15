3ddc7bfe675bffe1a9d53674995b0e4348c666c6
Jerome Jiang <jianj@google.com>
2022-04-15 17:52:28 -0400

    Remove REALTIME_ONLY from decoder
    Shared lib binary size increases from 2627128 bytes to 2730936 bytes
    with arm64 build, which is about 4% increase.
    
    Bug: aomedia:3248
    Change-Id: I80c1e690d910c984f786c93093f10de890e1c19b

--- aom_dsp/aom_dsp.cmake
+++ aom_dsp/aom_dsp.cmake
@@ -304,16 +304,16 @@ if(CONFIG_AV1_ENCODER)
     list(APPEND AOM_DSP_ENCODER_SOURCES "${AOM_ROOT}/aom_dsp/butteraugli.c"
                 "${AOM_ROOT}/aom_dsp/butteraugli.h")
   endif()
+endif()
 
-  if(CONFIG_REALTIME_ONLY)
-    list(REMOVE_ITEM AOM_DSP_ENCODER_INTRIN_AVX2
-                     "${AOM_ROOT}/aom_dsp/x86/obmc_sad_avx2.c"
-                     "${AOM_ROOT}/aom_dsp/x86/obmc_variance_avx2.c")
+if(CONFIG_REALTIME_ONLY)
+  list(REMOVE_ITEM AOM_DSP_ENCODER_INTRIN_AVX2
+                  "${AOM_ROOT}/aom_dsp/x86/obmc_sad_avx2.c"
+                  "${AOM_ROOT}/aom_dsp/x86/obmc_variance_avx2.c")
 
-    list(REMOVE_ITEM AOM_DSP_ENCODER_INTRIN_SSE4_1
-                     "${AOM_ROOT}/aom_dsp/x86/obmc_sad_sse4.c"
-                     "${AOM_ROOT}/aom_dsp/x86/obmc_variance_sse4.c")
-  endif()
+  list(REMOVE_ITEM AOM_DSP_ENCODER_INTRIN_SSE4_1
+                  "${AOM_ROOT}/aom_dsp/x86/obmc_sad_sse4.c"
+                  "${AOM_ROOT}/aom_dsp/x86/obmc_variance_sse4.c")
 endif()
 
 # Creates aom_dsp build targets. Must not be called until after libaom target
