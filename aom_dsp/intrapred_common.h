8b26862d8219e68f7afa34089ed42d5e5ea7448b
James Zern <jzern@google.com>
2022-04-28 11:24:24 -0700

    intrapred_common.h: align smooth_weights*[]
    this ensures NEON loads using a cast to uint32_t* have sufficient
    alignment; using 8 preserves the potential for an alignment hint in
    load_weight_w8
    
    Change-Id: I7f7c00d277cbfb1b80bdd4037e81ca216fe37a87

--- aom_dsp/intrapred_common.h
+++ aom_dsp/intrapred_common.h
@@ -18,7 +18,7 @@
 // 2^SMOOTH_WEIGHT_LOG2_SCALE.
 #define SMOOTH_WEIGHT_LOG2_SCALE 8
 
-static const uint8_t smooth_weights[] = {
+DECLARE_ALIGNED(8, static const uint8_t, smooth_weights[]) = {
   // bs = 4
   255, 149, 85, 64,
   // bs = 8
@@ -35,7 +35,7 @@ static const uint8_t smooth_weights[] = {
   13, 12, 10, 9, 8, 7, 6, 6, 5, 5, 4, 4, 4
 };
 
-static const uint16_t smooth_weights_u16[] = {
+DECLARE_ALIGNED(8, static const uint16_t, smooth_weights_u16[]) = {
   // block dimension = 4
   255, 149, 85, 64,
   // block dimension = 8
