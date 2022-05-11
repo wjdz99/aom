9e92264e5d40f5e351ad7a463b7bcd487c7da418
Angie Chiang <angiebird@google.com>
2022-05-10 21:23:01 -0700

    Add DuckyEncode APIs
    Define the APIs and implemented InitEncoder, FreeEncoder and
    ComputeFirstPassStats.
    
    EncodeFrame will be added in a follow-up CL.
    
    Bug: b/221916304
    
    Change-Id: I17cb48578a6544d5c55a6f785ddc93eac49866d4

---
+++ av1/ducky_encode.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
+#ifndef AOM_AV1_DUCKY_ENCODE_H_
+#define AOM_AV1_DUCKY_ENCODE_H_
+
+#include <string>
+#include <vector>
+
+#include "av1/encoder/firstpass.h"
+#include "aom/aom_encoder.h"
+
+namespace aom {
+struct VideoInfo {
+  int frame_width;
+  int frame_height;
+  aom_rational_t frame_rate;
+  aom_img_fmt_t img_fmt;
+  int frame_count;
+  std::string file_path;
+};
+
+class DuckyEncode {
+ public:
+  DuckyEncode(const VideoInfo &video_info);
+  ~DuckyEncode();
+  std::vector<FIRSTPASS_STATS> ComputeFirstPassStats();
+  void StartEncode(const std::vector<FIRSTPASS_STATS> &stats_list);
+  void EncodeFrame(std::vector<uint8_t> *bitstream_buf);
+  void EndEncode();
+
+ private:
+  class EncodeImpl;
+  std::unique_ptr<EncodeImpl> impl_ptr_;
+};
+}  // namespace aom
+
+#endif  // AOM_AV1_DUCKY_ENCODE_H_
