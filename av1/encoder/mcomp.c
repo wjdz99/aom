7090941d19e470cdad4a04c9b8ba659cf2ae115b
chiyotsai <chiyotsai@google.com>
2022-05-23 18:02:27 -0700

    Add assertion for resize mode
    Change-Id: I4785f424ae6f405be004d473a34fc98600c54043

--- av1/encoder/mcomp.c
+++ av1/encoder/mcomp.c
@@ -1671,6 +1671,10 @@ int av1_full_pixel_search(const FULLPEL_MV start_mv,
   const int is_intra_mode = ms_params->is_intra_mode;
   int run_mesh_search = ms_params->run_mesh_search;
 
+  assert(ms_params->ms_buffers.src->width == ms_params->ms_buffers.ref->width &&
+         ms_params->ms_buffers.src->height ==
+             ms_params->ms_buffers.ref->height);
+
   int var = 0;
   MARK_MV_INVALID(best_mv);
   if (second_best_mv) {
