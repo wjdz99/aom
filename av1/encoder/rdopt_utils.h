ace3fb271f56279e17fa10d9a0932c43839beec5
Mudassir Galagnath <mudassir.galaganath@ittiam.com>
2022-07-03 22:09:53 +0530

    Allintra: Introduce sf prune_smooth_intra_mode_for_chroma
    Smooth prediction mode of chroma is pruned using chroma source
    variance in intra frames. To facilitate this change, a new
    speed feature prune_smooth_intra_mode_for_chroma is introduced.
    This change is applicable for allintra speed>=6.
    
    For AVIF still-image encode,
    
                 Encode Time             BD-Rate Loss(%)
    cpu-used     Reduction(%)   psnr     psnrCb   psnrCr   ssim
       6          1.556         0.0492  -0.0391   0.0692   0.0615
       7          2.139        -0.0136  -0.4015  -0.2347   0.0172
       8          0.903         0.0029  -0.0066  -0.0271   0.0067
    
    STATS_CHANGED
    
    Change-Id: I7e998c732d597e63b436bb463c782f7061e1c899

--- av1/encoder/rdopt_utils.h
+++ av1/encoder/rdopt_utils.h
@@ -748,6 +748,10 @@ unsigned int av1_high_get_sby_perpixel_variance(const struct AV1_COMP *cpi,
                                                 const struct buf_2d *ref,
                                                 BLOCK_SIZE bs, int bd);
 
+unsigned int av1_get_sbuv_perpixel_variance(const struct AV1_COMP *cpi,
+                                            const MACROBLOCK *x,
+                                            BLOCK_SIZE bsize, int plane);
+
 static INLINE int is_mode_intra(PREDICTION_MODE mode) {
   return mode < INTRA_MODE_END;
 }
