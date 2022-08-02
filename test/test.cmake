9136db82eb3ef969ffac09282844fe5acc54ac1f
Kyle Siefring <siekyleb@amazon.com>
2022-08-02 15:16:34 -0400

    Speed up mv cost table setup
    Compute in O(n) instead of O(n log(n)) (where n is the number of motion
    vector values). Leads to a x15 speedup with clang (~x10 with gcc).
    
    Change-Id: Ibe650a9e247d864ca3b7b237d64208580a5946bb

--- test/test.cmake
+++ test/test.cmake
@@ -90,6 +90,7 @@ list(APPEND AOM_UNIT_TEST_ENCODER_SOURCES
             "${AOM_ROOT}/test/i420_video_source.h"
             "${AOM_ROOT}/test/level_test.cc"
             "${AOM_ROOT}/test/monochrome_test.cc"
+            "${AOM_ROOT}/test/mv_cost_test.cc"
             "${AOM_ROOT}/test/resize_test.cc"
             "${AOM_ROOT}/test/scalability_test.cc"
             "${AOM_ROOT}/test/sharpness_test.cc"
