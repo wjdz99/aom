0a4ece19eec1ff2b6eb3fa66988dde489009089d
Jingning Han <jingning@google.com>
2022-08-09 22:38:06 -0700

    Update the forward_stack_ priority list
    Follow the naming convention related to the ARF and intermediate
    ARFs.
    
    BUG=b/241257063
    
    Change-Id: Ic3d7524feb8284fa946b5141474659efc9b15286

--- test/ratectrl_qmode_test.cc
+++ test/ratectrl_qmode_test.cc
@@ -695,11 +695,12 @@ void TestRefFrameManagerPriority(const RefFrameManager &ref_manager,
   int ref_count = ref_manager.GetRefFrameCountByType(type);
   int prev_global_order_idx = ref_manager.CurGlobalOrderIdx();
   // The lower the priority is, the closer the gop_frame.global_order_idx should
-  // be with cur_global_order_idx_
+  // be with cur_global_order_idx_, with exception of a base layer ARF.
   for (int priority = 0; priority < ref_count; ++priority) {
     GopFrame gop_frame = ref_manager.GetRefFrameByPriority(type, priority);
     EXPECT_EQ(gop_frame.is_valid, true);
     if (type == RefUpdateType::kForward) {
+      if (priority == 0) continue;
       EXPECT_GE(gop_frame.global_order_idx, prev_global_order_idx);
     } else {
       EXPECT_LE(gop_frame.global_order_idx, prev_global_order_idx);
