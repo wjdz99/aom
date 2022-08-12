495c3942e53da4f0fa2b93b482713d46ecbd8c97
Yunqing Wang <yunqingwang@google.com>
2022-08-12 11:54:32 -0700

    Use NEARESTMV mode in SC static area encoding
    In RTC screen content encoding, we use NEARESTMV mode instead of
    GLOBALMV to improve BD-rate saving.
    
    STATS_CHANGED for SC
    
    Change-Id: I483499e164287a7a6bcbf49d26a129909c45958a

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -2955,7 +2955,7 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
     if (!use_ref_frame_mask[ref_frame]) continue;
 
     if (x->force_zeromv_skip &&
-        (this_mode != GLOBALMV || ref_frame != LAST_FRAME))
+        (this_mode != NEARESTMV || ref_frame != LAST_FRAME))
       continue;
 
     force_mv_inter_layer = 0;
