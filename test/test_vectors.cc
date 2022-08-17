494583b6c41ded5055ebc68a6dba4a75cb02d253
Mudassir Galagnath <mudassir.galaganath@ittiam.com>
2022-08-17 19:54:41 +0530

    test_vectors: add av1-1-b8-intraBC-dv.ivf
    This CL adds av1-1-b8-intraBC-dv.ivf file to kAV1TestVectors.
    created with: ./aomenc -o $ouput-file --allintra --cpu-used=6
    --sb-size=64 --cq-level=29 $input-file --limit=2
    
    Change-Id: I41a54136723bfc75ed18a9b646aa58b1e0d734ec

--- test/test_vectors.cc
+++ test/test_vectors.cc
@@ -258,7 +258,8 @@ const char *const kAV1TestVectors[] = { "av1-1-b8-00-quantizer-00.ivf",
                                         "av1-1-b8-22-svc-L2T1.ivf",
                                         "av1-1-b8-22-svc-L2T2.ivf",
                                         "av1-1-b8-23-film_grain-50.ivf",
-                                        "av1-1-b8-24-monochrome.ivf" };
+                                        "av1-1-b8-24-monochrome.ivf",
+                                        "av1-1-b8-intraBC-dv.ivf" };
 const int kNumAV1TestVectors = NELEMENTS(kAV1TestVectors);
 #endif  // CONFIG_AV1_DECODER
 
