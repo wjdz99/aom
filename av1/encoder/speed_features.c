b9d9844eb66fe07ecb73418c6d6f3812fcf576b9
linzhen <linzhen@google.com>
2022-09-19 23:09:28 +0000

    Change enable_ref_short_signaling to bool type
    Based on the usage of enable_ref_short_signaling, it's better to define
    it as boolean instead of int.
    
    Change-Id: I8dcf4c4ab522050682be1d81c32d89a789fbf5ef

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1282,13 +1282,12 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
       sf->rt_sf.use_rtc_tf = 2;
       // Only turn on enable_ref_short_signaling for low resolution when only
       // LAST and GOLDEN ref frames are used.
-      sf->rt_sf.enable_ref_short_signaling =
-          (sf->rt_sf.use_nonrd_altref_frame ||
-           (sf->rt_sf.use_comp_ref_nonrd &&
-            (sf->rt_sf.ref_frame_comp_nonrd[1] ||
-             sf->rt_sf.ref_frame_comp_nonrd[2])))
-              ? 0
-              : 1;
+      if (!sf->rt_sf.use_nonrd_altref_frame &&
+          (!sf->rt_sf.use_comp_ref_nonrd ||
+           (!sf->rt_sf.ref_frame_comp_nonrd[1] &&
+            !sf->rt_sf.ref_frame_comp_nonrd[2]))) {
+        sf->rt_sf.enable_ref_short_signaling = true;
+      };
 // TODO(kyslov) Re-enable when AV1 models are trained
 #if 0
 #if CONFIG_RT_ML_PARTITIONING
@@ -2083,7 +2082,7 @@ static AOM_INLINE void init_rt_sf(REAL_TIME_SPEED_FEATURES *rt_sf) {
   rt_sf->set_zeromv_skip_based_on_source_sad = 1;
   rt_sf->use_adaptive_subpel_search = false;
   rt_sf->screen_content_cdef_filter_qindex_thresh = 0;
-  rt_sf->enable_ref_short_signaling = 0;
+  rt_sf->enable_ref_short_signaling = false;
 }
 
 // Populate appropriate sub-pel search method based on speed feature and user
