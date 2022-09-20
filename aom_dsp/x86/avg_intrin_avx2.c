e52bcd6d255b97009a6c475c694d3d5f765d5318
Ranjit Kumar Tulabandu <ranjit.tulabandu@ittiam.com>
2022-09-21 01:11:26 +0530

    WIP : Test Hadamard SIMD functions
    Change-Id: I5b36e1f6751ba8c90f3a3cc2bbc90e31abc45970

--- aom_dsp/x86/avg_intrin_avx2.c
+++ aom_dsp/x86/avg_intrin_avx2.c
@@ -16,7 +16,7 @@
 #include "aom_dsp/x86/bitdepth_conversion_avx2.h"
 #include "aom_ports/mem.h"
 
-static void hadamard_col8x2_avx2(__m256i *in, int iter) {
+static void hadamard_col8x2_avx2(__m256i *in) {
   __m256i a0 = in[0];
   __m256i a1 = in[1];
   __m256i a2 = in[2];
@@ -44,52 +44,41 @@ static void hadamard_col8x2_avx2(__m256i *in, int iter) {
   a6 = _mm256_sub_epi16(b4, b6);
   a7 = _mm256_sub_epi16(b5, b7);
 
-  if (iter == 0) {
-    b0 = _mm256_add_epi16(a0, a4);
-    b7 = _mm256_add_epi16(a1, a5);
-    b3 = _mm256_add_epi16(a2, a6);
-    b4 = _mm256_add_epi16(a3, a7);
-    b2 = _mm256_sub_epi16(a0, a4);
-    b6 = _mm256_sub_epi16(a1, a5);
-    b1 = _mm256_sub_epi16(a2, a6);
-    b5 = _mm256_sub_epi16(a3, a7);
+  b0 = _mm256_add_epi16(a0, a4);
+  b7 = _mm256_add_epi16(a1, a5);
+  b3 = _mm256_add_epi16(a2, a6);
+  b4 = _mm256_add_epi16(a3, a7);
+  b2 = _mm256_sub_epi16(a0, a4);
+  b6 = _mm256_sub_epi16(a1, a5);
+  b1 = _mm256_sub_epi16(a2, a6);
+  b5 = _mm256_sub_epi16(a3, a7);
 
-    a0 = _mm256_unpacklo_epi16(b0, b1);
-    a1 = _mm256_unpacklo_epi16(b2, b3);
-    a2 = _mm256_unpackhi_epi16(b0, b1);
-    a3 = _mm256_unpackhi_epi16(b2, b3);
-    a4 = _mm256_unpacklo_epi16(b4, b5);
-    a5 = _mm256_unpacklo_epi16(b6, b7);
-    a6 = _mm256_unpackhi_epi16(b4, b5);
-    a7 = _mm256_unpackhi_epi16(b6, b7);
+  a0 = _mm256_unpacklo_epi16(b0, b1);
+  a1 = _mm256_unpacklo_epi16(b2, b3);
+  a2 = _mm256_unpackhi_epi16(b0, b1);
+  a3 = _mm256_unpackhi_epi16(b2, b3);
+  a4 = _mm256_unpacklo_epi16(b4, b5);
+  a5 = _mm256_unpacklo_epi16(b6, b7);
+  a6 = _mm256_unpackhi_epi16(b4, b5);
+  a7 = _mm256_unpackhi_epi16(b6, b7);
 
-    b0 = _mm256_unpacklo_epi32(a0, a1);
-    b1 = _mm256_unpacklo_epi32(a4, a5);
-    b2 = _mm256_unpackhi_epi32(a0, a1);
-    b3 = _mm256_unpackhi_epi32(a4, a5);
-    b4 = _mm256_unpacklo_epi32(a2, a3);
-    b5 = _mm256_unpacklo_epi32(a6, a7);
-    b6 = _mm256_unpackhi_epi32(a2, a3);
-    b7 = _mm256_unpackhi_epi32(a6, a7);
+  b0 = _mm256_unpacklo_epi32(a0, a1);
+  b1 = _mm256_unpacklo_epi32(a4, a5);
+  b2 = _mm256_unpackhi_epi32(a0, a1);
+  b3 = _mm256_unpackhi_epi32(a4, a5);
+  b4 = _mm256_unpacklo_epi32(a2, a3);
+  b5 = _mm256_unpacklo_epi32(a6, a7);
+  b6 = _mm256_unpackhi_epi32(a2, a3);
+  b7 = _mm256_unpackhi_epi32(a6, a7);
 
-    in[0] = _mm256_unpacklo_epi64(b0, b1);
-    in[1] = _mm256_unpackhi_epi64(b0, b1);
-    in[2] = _mm256_unpacklo_epi64(b2, b3);
-    in[3] = _mm256_unpackhi_epi64(b2, b3);
-    in[4] = _mm256_unpacklo_epi64(b4, b5);
-    in[5] = _mm256_unpackhi_epi64(b4, b5);
-    in[6] = _mm256_unpacklo_epi64(b6, b7);
-    in[7] = _mm256_unpackhi_epi64(b6, b7);
-  } else {
-    in[0] = _mm256_add_epi16(a0, a4);
-    in[7] = _mm256_add_epi16(a1, a5);
-    in[3] = _mm256_add_epi16(a2, a6);
-    in[4] = _mm256_add_epi16(a3, a7);
-    in[2] = _mm256_sub_epi16(a0, a4);
-    in[6] = _mm256_sub_epi16(a1, a5);
-    in[1] = _mm256_sub_epi16(a2, a6);
-    in[5] = _mm256_sub_epi16(a3, a7);
-  }
+  in[0] = _mm256_unpacklo_epi64(b0, b1);
+  in[1] = _mm256_unpackhi_epi64(b0, b1);
+  in[2] = _mm256_unpacklo_epi64(b2, b3);
+  in[3] = _mm256_unpackhi_epi64(b2, b3);
+  in[4] = _mm256_unpacklo_epi64(b4, b5);
+  in[5] = _mm256_unpackhi_epi64(b4, b5);
+  in[6] = _mm256_unpacklo_epi64(b6, b7);
+  in[7] = _mm256_unpackhi_epi64(b6, b7);
 }
 
 void aom_hadamard_lp_8x8_dual_avx2(const int16_t *src_diff,
@@ -104,8 +93,8 @@ void aom_hadamard_lp_8x8_dual_avx2(const int16_t *src_diff,
   src[6] = _mm256_loadu_si256((const __m256i *)(src_diff += src_stride));
   src[7] = _mm256_loadu_si256((const __m256i *)(src_diff + src_stride));
 
-  hadamard_col8x2_avx2(src, 0);
-  hadamard_col8x2_avx2(src, 1);
+  hadamard_col8x2_avx2(src);
+  hadamard_col8x2_avx2(src);
 
   _mm256_storeu_si256((__m256i *)coeff,
                       _mm256_permute2x128_si256(src[0], src[1], 0x20));
