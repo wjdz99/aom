ea272be55265602a09392080a62bd80f5ee34eac
Marco Paniconi <marpan@google.com>
2022-10-11 15:42:17 -0700

    rtc: Allow golden-ref based on block sad, sp10
    For the aggressive ref frame pruning mode,
    nonrd_prune_ref_frame_search >= 3 (speed >= 10):
    the golden ref is often completely skipped, but in
    some cases kept based on superblock sad.
    Here we add condition based on the (already computed)
    sad of coding block, before mode testing, from the
    find_predictors() (sad of nearest/nearmv for last-ref),
    to make a finer/better decision to search golden ref.
    
    The current logic/threshold is choosen to be conservative
    to avoid slowdown.
    
    This is generally useful for uncovered areas, and shows
    visual improvement on face after hand moving
    (desktop2_180p).
    
    Stats changed for speed 10 (overall neutral)
                 avg_psnr/ovr_psnr/ssim, IC%
    rtc_derf:     -0.35/-0.40/-0.50, -0.50
    
    bdrate gains of ~1-2% on desktop2, desktopqvga and jimredvga.
    
    Neutral change for rtc set and rtc_screen.
    Change-Id: I57056d3207fba333783ec9686aa25fa34f1f8847

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -2326,6 +2326,21 @@ static AOM_INLINE void get_ref_frame_use_mask(AV1_COMP *cpi, MACROBLOCK *x,
       (x->color_sensitivity_sb_g[0] == 1 || x->color_sensitivity_sb_g[1] == 1))
     use_golden_ref_frame = 0;
 
+  // If GOLDEN and ALTREF are not being selected as a references
+  // (use_golden_ref_frame/use_alt_ref_frame = 0), check to allow GOLDEN back
+  // based on the sad of nearest/nearmv of LAST reference. If this block sad is
+  // large, keep golden as reference. Only do this for the agrressive pruning
+  // mode and avoid it when color is set for golden reference.
+  if ((cpi->ref_frame_flags & AOM_LAST_FLAG) && !use_golden_ref_frame &&
+      !use_alt_ref_frame && x->pred_mv_sad[LAST_FRAME] != INT_MAX &&
+      x->nonrd_prune_ref_frame_search >= 3 &&
+      x->color_sensitivity_sb_g[0] == 0 && x->color_sensitivity_sb_g[1] == 0) {
+    int thr = (cm->width * cm->height >= 1280 * 720) ? 300 : 200;
+    int pred = x->pred_mv_sad[LAST_FRAME] >>
+               (b_width_log2_lookup[bsize] + b_height_log2_lookup[bsize]);
+    if (pred > thr) use_golden_ref_frame = 1;
+  }
+
   use_alt_ref_frame =
       cpi->ref_frame_flags & AOM_ALT_FLAG ? use_alt_ref_frame : 0;
   use_golden_ref_frame =
@@ -3086,6 +3101,10 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
     svc_mv_row = -4;
   }
 
+  if (cpi->ref_frame_flags & AOM_LAST_FLAG)
+    find_predictors(cpi, x, LAST_FRAME, frame_mv, tile_data, yv12_mb, bsize,
+                    force_skip_low_temp_var, x->force_zeromv_skip_for_blk);
+
   get_ref_frame_use_mask(cpi, x, mi, mi_row, mi_col, bsize, gf_temporal_ref,
                          use_ref_frame_mask, &force_skip_low_temp_var);
 
@@ -3105,7 +3124,8 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
     tot_num_comp_modes = 0;
   }
 
-  for (MV_REFERENCE_FRAME ref_frame_iter = LAST_FRAME;
+  // Start at LAST_FRAME + 1.
+  for (MV_REFERENCE_FRAME ref_frame_iter = LAST_FRAME + 1;
        ref_frame_iter <= ALTREF_FRAME; ++ref_frame_iter) {
     if (use_ref_frame_mask[ref_frame_iter]) {
       find_predictors(cpi, x, ref_frame_iter, frame_mv, tile_data, yv12_mb,
@@ -3113,10 +3133,12 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
     }
   }
 
-  thresh_sad_pred = ((int64_t)x->pred_mv_sad[LAST_FRAME]) << 1;
-  // Increase threshold for less aggressive pruning.
-  if (cpi->sf.rt_sf.nonrd_prune_ref_frame_search == 1)
-    thresh_sad_pred += (x->pred_mv_sad[LAST_FRAME] >> 2);
+  if (x->pred_mv_sad[LAST_FRAME] != INT_MAX) {
+    thresh_sad_pred = ((int64_t)x->pred_mv_sad[LAST_FRAME]) << 1;
+    // Increase threshold for less aggressive pruning.
+    if (cpi->sf.rt_sf.nonrd_prune_ref_frame_search == 1)
+      thresh_sad_pred += (x->pred_mv_sad[LAST_FRAME] >> 2);
+  }
 
   const int large_block = bsize >= BLOCK_32X32;
   const int use_model_yrd_large =
