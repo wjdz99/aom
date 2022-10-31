6079ad3241224d8afd01bbb53eb5b4f97b703655
Marco Paniconi <marpan@google.com>
2022-10-31 00:41:42 -0700

    rtc-svc: Fixes for screen with spatial layers
    Several fixes to make screen work with spatial layers:
    
    -last_source is set based on superframe count to ensure
     last_source is the previous source for all spatial layers
    -source_last_TL0 is used to ensure last_source corresponds
     to the LAST reference.
    -scene detection is only done on SLO with full resolution,
     and the state of the detection is passed on to the other
     spatial layers.
    -source_sad is allowed for every spatial layer.
    -cyclic refresh counter on distance to scene change/max_q
     is added to the layer context and updated per spatial layer.
    -allow for prediction from lower spatial layer in nonrd_pickmode.
    
    Unittest added for screen with spatial layers.
    
    No change in stats for default 1 layer encoding.
    
    Bug: aomedia:3346
    
    Change-Id: I87961ead6154fc3642071ab67e99a4595565d00d

--- av1/encoder/encodeframe_utils.c
+++ av1/encoder/encodeframe_utils.c
@@ -1389,6 +1389,7 @@ void av1_source_content_sb(AV1_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
   uint8_t *src_y = cpi->source->y_buffer;
   const int src_ystride = cpi->source->y_stride;
   const int src_offset = src_ystride * (mi_row << 2) + (mi_col << 2);
+  if (cpi->last_source == NULL) return;
   uint8_t *last_src_y = cpi->last_source->y_buffer;
   const int last_src_ystride = cpi->last_source->y_stride;
   const int last_src_offset = last_src_ystride * (mi_row << 2) + (mi_col << 2);
@@ -1426,7 +1427,7 @@ void av1_source_content_sb(AV1_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
     x->content_state_sb.low_sumdiff = 1;
 
   if (!cpi->sf.rt_sf.use_rtc_tf || cpi->rc.high_source_sad ||
-      cpi->rc.frame_source_sad > 20000)
+      cpi->rc.frame_source_sad > 20000 || cpi->svc.number_spatial_layers > 1)
     return;
 
   // In-place temporal filter. If psnr calculation is enabled, we store the
