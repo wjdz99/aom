2f4b76b4feb3780bd45cbc6081c8171b0f52330d
Yunqing Wang <yunqingwang@google.com>
2022-11-07 17:08:10 -0800

    Improve rate control bit estimation
    Improved bit estimation in rate control by incorporating
    frame complexity. As an ongoing experiment, this was first
    enabled for speed 7. This gave good BDRate gain with almost
    no speed loss.
    
    Borg test result for rtc set:
      avg_psnr:  ovr_psnr:   ssim:  encoding_spdup:
       -0.561     -0.724    -0.985    -0.239
    
    STATS_CHANGED
    
    Change-Id: Iee29ec0ae2682fd307292b34240236b7f5c93d90

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1315,7 +1315,15 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     }
     if (speed == 6) sf->part_sf.disable_8x8_part_based_on_qidx = 1;
     if (speed >= 6) sf->rt_sf.skip_newmv_mode_based_on_sse = 2;
-    if (speed == 7) sf->rt_sf.prefer_large_partition_blocks = 1;
+    if (speed == 7) {
+      sf->rt_sf.prefer_large_partition_blocks = 1;
+#if CONFIG_FRAME_SIZE_OUTPUT
+      // Enable this feature for [360p, 720p] res range initially.
+      if (!cpi->rc.rtc_external_ratectrl &&
+          AOMMIN(cm->width, cm->height) <= 720)
+        sf->hl_sf.accurate_bit_estimate = 1;
+#endif
+    }
     if (speed >= 7) {
       sf->rt_sf.use_rtc_tf = 1;
     }
@@ -1430,6 +1438,10 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     } else {
       sf->rt_sf.use_comp_ref_nonrd = 0;
     }
+
+    if (cpi->svc.number_spatial_layers > 1 ||
+        cpi->svc.number_temporal_layers > 1)
+      sf->hl_sf.accurate_bit_estimate = 0;
   }
   // Screen settings.
   if (cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN) {
@@ -1483,6 +1495,7 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
       sf->rt_sf.fullpel_search_step_param = 2;
     }
     sf->rt_sf.partition_direct_merging = 0;
+    sf->hl_sf.accurate_bit_estimate = 0;
   }
 }
 
@@ -1783,6 +1796,7 @@ static AOM_INLINE void init_hl_sf(HIGH_LEVEL_SPEED_FEATURES *hl_sf) {
   hl_sf->disable_extra_sc_testing = 0;
   hl_sf->second_alt_ref_filtering = 1;
   hl_sf->num_frames_used_in_tf = INT_MAX;
+  hl_sf->accurate_bit_estimate = 0;
 }
 
 static AOM_INLINE void init_fp_sf(FIRST_PASS_SPEED_FEATURES *fp_sf) {
