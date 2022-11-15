37af26054a58d131f9a98474ca07ce4480b67c74
Yuan Tong <tongyuan200097@gmail.com>
2022-11-15 19:47:27 +0800

    Correctly select between normative and non-normative scaler
    - `av1_resize_and_extend_frame` handles any scaling ratio >= 1/4, not
      only the listed cases.
    - Verify scaling ratios of both Y and UV planes when selecting scaler.
    - fix `av1_resize_and_extend_frame_c` always assume input is YUV420.
    
    BUG: aomedia:3203
    Change-Id: I6edfdea645a3d909dd4937197d500e7571db83cf

--- av1/common/resize.h
+++ av1/common/resize.h
@@ -105,21 +105,16 @@ static INLINE int av1_superres_scaled(const AV1_COMMON *cm) {
   return (cm->width != cm->superres_upscaled_width);
 }
 
-// There's SIMD optimizations for 1/4, 1/2 and 3/4 downscaling.
-// SSSE3 also has optimizations for 2x upscaling.
-// Use non normative scalers for other scaling ratios.
+// The normative scaler av1_resize_and_extend_frame() can only handle
+// scaling ratios >= 1/4. See comment in aom_convolve8_c() for detail.
+// Use the non-normative scaler av1_resize_and_extend_frame_nonnormative()
+// for other scaling ratios.
 static INLINE bool av1_has_optimized_scaler(const int src_width,
                                             const int src_height,
                                             const int dst_width,
                                             const int dst_height) {
   const bool has_optimized_scaler =
-      (dst_width * 4 == src_width && dst_height * 4 == src_height) ||
-      (dst_width * 2 == src_width && dst_height * 2 == src_height) ||
-      (dst_width * 4 == src_width * 3 && dst_height * 4 == src_height * 3);
-#if HAVE_SSSE3
-  return has_optimized_scaler ||
-         (dst_width == src_width * 2 && dst_height == src_height * 2);
-#endif
+      (dst_width * 4 >= src_width && dst_height * 4 >= src_height);
   return has_optimized_scaler;
 }
 
