46b1f96bf961f9a1dad65684a7fd86f42d6884c2
Rachel Barker <rachelbarker@google.com>
2022-11-30 15:45:11 +0000

    Move global motion estimation code to aom_dsp/
    Relocate the core flow estimation code (ie, all of the code which
    handles models in double precision, before we integerize) to a
    new directory aom_dsp/flow_estimation/ .
    
    This is a major step toward the ultimate goal of making the
    flow estimation code codec-independent, but this step is not quite
    completed here.
    
    No behavioural changes are made here, only structural changes. So
    this patch has no impact on BDRATE and overall speed.
    
    Change-Id: Icbdabbfed451d9e5b0a9e5d51d341b31b03cc547

---
+++ aom_dsp/flow_estimation/corner_detect.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at aomedia.org/license/software-license/bsd-3-c-c/.  If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * aomedia.org/license/patent-license/.
+ */
+
+#ifndef AOM_AOM_DSP_FLOW_ESTIMATION_CORNER_DETECT_H_
+#define AOM_AOM_DSP_FLOW_ESTIMATION_CORNER_DETECT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int aom_fast_corner_detect(unsigned char *buf, int width, int height,
+                           int stride, int *points, int max_points);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // AOM_AOM_DSP_FLOW_ESTIMATION_CORNER_DETECT_H_
---
+++ aom_dsp/flow_estimation/ransac.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at aomedia.org/license/software-license/bsd-3-c-c/.  If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * aomedia.org/license/patent-license/.
+ */
+
+#ifndef AOM_AOM_DSP_FLOW_ESTIMATION_RANSAC_H_
+#define AOM_AOM_DSP_FLOW_ESTIMATION_RANSAC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <memory.h>
+
+#include "aom_dsp/flow_estimation/flow_estimation.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int (*RansacFunc)(int *matched_points, int npoints,
+                          int *num_inliers_by_motion,
+                          MotionModel *params_by_motion, int num_motions);
+typedef int (*RansacFuncDouble)(double *matched_points, int npoints,
+                                int *num_inliers_by_motion,
+                                MotionModel *params_by_motion, int num_motions);
+RansacFunc aom_get_ransac_type(TransformationType type);
+RansacFuncDouble aom_get_ransac_double_prec_type(TransformationType type);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // AOM_AOM_DSP_FLOW_ESTIMATION_RANSAC_H_
