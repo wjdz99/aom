cafd0c6d1d150870ea67ff4b29465c168bac20c8
Rachel Barker <rachelbarker@google.com>
2022-11-30 15:45:11 +0000

    Move global motion estimation code to aom_dsp/
    Relocate the core flow estimation code (ie, all of the code which
    handles models in double precision, before we integerize) to a
    new directory aom_dsp/flow_estimation/ .
    
    This is a major step toward the ultimate goal of making the
    flow estimation code codec-independent, but this step is not quite
    completed here.
    
    No behavioural changes are made here, only structural changes. So
    this patch has no impact on BDRATE and overall speed.
    
    Change-Id: Icbdabbfed451d9e5b0a9e5d51d341b31b03cc547

---
+++ aom_dsp/flow_estimation/disflow.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at aomedia.org/license/software-license/bsd-3-c-c/.  If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * aomedia.org/license/patent-license/.
+ */
+
+#ifndef AOM_FLOW_ESTIMATION_DISFLOW_H_
+#define AOM_FLOW_ESTIMATION_DISFLOW_H_
+
+#include "aom_dsp/flow_estimation/flow_estimation.h"
+#include "aom_scale/yv12config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int aom_compute_global_motion_disflow_based(
+    TransformationType type, unsigned char *frm_buffer, int frm_width,
+    int frm_height, int frm_stride, int *frm_corners, int num_frm_corners,
+    YV12_BUFFER_CONFIG *ref, int bit_depth, int *num_inliers_by_motion,
+    MotionModel *params_by_motion, int num_motions);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // AOM_FLOW_ESTIMATION_DISFLOW_H_
