84007a87e44f63894489d0a3f207d5813e6bc3fa
Rachel Barker <rachelbarker@google.com>
2022-12-07 15:59:53 +0000

    Replace y_buffer_8bit with a downsampling pyramid
    Change-Id: Ic1b24b032ad23d1d4dd878262f7562be04e24e69

--- av1/encoder/encoder.h
+++ av1/encoder/encoder.h
@@ -3462,6 +3462,12 @@ typedef struct AV1_COMP {
    * Block level thresholds to force zeromv-skip at partition level.
    */
   unsigned int zeromv_skip_thresh_exit_part[BLOCK_SIZES_ALL];
+
+  /*!
+   *  Number of downsampling pyramid levels to allocate for each frame
+   *  This is currently only used for global motion
+   */
+  int image_pyramid_levels;
 } AV1_COMP;
 
 /*!
