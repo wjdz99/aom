60d96a4c61fda7e57db3c0742c8b585ad1906966
Neeraj Gadgil <neeraj.gadgil@ittiam.com>
2022-12-09 13:24:54 +0530

    Modify encoder's entropy coding byte-push mechanism
    Changed the byte-push mechanism in libaom entropy
    encoder, as suggested in a To Do comment. In the
    parent version, the register is flushed when at
    least 1 byte is available. In the CL, we make the
    register 64-bit and flush it when at least 4 bytes
    are accumulated in it.
    
    An intermediate 16-bit buffer 'precarry_buf' is
    removed and carry-propagation mechanism is changed.
    
    No changes to the decoder logic at this time, it
    already has an OPT comment about using uint64_t
    for decode-time optimization.
    
    AV1 RTC Encoder:
    
    cpu   Enc time
           red (%)
     5      0.407
     6      0.406
     7      0.612
     8      0.964
     9      1.278
    10      1.654
    
    AVIF Encoder:
    
    cpu   Enc time
           red (%)
     6      0.238
     7      0.302
     8      0.664
     9      1.929
    
    This is a bit-exact change.
    
    Change-Id: If23e711145aef1fa193523c98b29c7c82eff76b7

--- aom_dsp/entenc.h
+++ aom_dsp/entenc.h
@@ -13,10 +13,21 @@
 #define AOM_AOM_DSP_ENTENC_H_
 #include <stddef.h>
 #include "aom_dsp/entcode.h"
+#include "aom_ports/msvc.h"
+
+/* Needed for MSVC */
+#ifdef _MSC_VER
+#if defined(_M_X64) || defined(_M_IX86)
+#include <intrin.h>
+#define USE_MSC_INTRINSICS
+#endif
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef uint64_t enc_od_ec_window;
 
 typedef struct od_ec_enc od_ec_enc;
 
@@ -30,14 +41,10 @@ struct od_ec_enc {
   unsigned char *buf;
   /*The size of the buffer.*/
   uint32_t storage;
-  /*A buffer for output bytes with their associated carry flags.*/
-  uint16_t *precarry_buf;
-  /*The size of the pre-carry buffer.*/
-  uint32_t precarry_storage;
   /*The offset at which the next entropy-coded byte will be written.*/
   uint32_t offs;
   /*The low end of the current range.*/
-  od_ec_window low;
+  enc_od_ec_window low;
   /*The number of values in the current range.*/
   uint16_t rng;
   /*The number of bits of data in the current value.*/
@@ -77,6 +84,61 @@ OD_WARN_UNUSED_RESULT uint32_t od_ec_enc_tell_frac(const od_ec_enc *enc)
 
 void od_ec_enc_checkpoint(od_ec_enc *dst, const od_ec_enc *src);
 void od_ec_enc_rollback(od_ec_enc *dst, const od_ec_enc *src);
+
+// buf is the frame bitbuffer, offs is where carry to be added
+static AOM_INLINE void propagate_carry_bwd(unsigned char* buf, uint32_t offs) {
+    uint16_t sum, carry = 1;
+    while (carry && (offs > 0)) {
+        sum = (uint16_t)buf[offs] + 1;
+        buf[offs--] = (unsigned char)sum;
+        carry = sum >> 8;
+    }
+    if (carry) {
+        sum = (uint16_t)buf[offs] + 1;
+        buf[offs] = (unsigned char)sum;
+        assert(!(sum >> 8));
+    }
+}
+
+static AOM_INLINE void write_enc_data_to_out_buf(unsigned char* out, uint32_t offs, 
+    uint64_t output, uint64_t carry, uint32_t* enc_offs, uint8_t num_bytes_ready) {
+#if defined(__GNUC__)
+    // GNU builtin
+    const uint64_t reg =
+        __builtin_bswap64(output) >> ((8 - num_bytes_ready) << 3);
+    memcpy(&out[offs], &reg, 8);
+    // Propagate carry backwards if exists
+    if (carry) {
+        assert(offs > 0);
+        propagate_carry_bwd(out, offs - 1);
+    }
+    *enc_offs = offs + num_bytes_ready;
+#elif defined(USE_MSC_INTRINSICS)
+    // MSVC
+    const uint64_t reg =
+        _byteswap_uint64(output) >> ((8 - num_bytes_ready) << 3);
+    memcpy(&out[offs], &reg, 8);
+    // Propagate carry backwards if exists
+    if (carry) {
+        assert(offs > 0);
+        propagate_carry_bwd(out, offs - 1);
+    }
+    *enc_offs = offs + num_bytes_ready;
+#else
+    // C function
+    int num_bytes_ready_minus_1 = num_bytes_ready - 1;
+    assert(num_bytes_ready_minus_1 >= 0);
+    uint16_t val = (uint16_t)(output >> (8 * num_bytes_ready_minus_1--));
+    out[offs] = (unsigned char)(val & 0x00FF);
+    if (carry) propagate_carry_bwd(out, offs - 1);
+    offs++;
+    while (num_bytes_ready_minus_1 >= 0) {
+        val = (uint16_t)(output >> (8 * (num_bytes_ready_minus_1--)));
+        out[offs++] = (unsigned char)(val & 0x00FF);
+    }
+    *enc_offs = offs;
+#endif
+}
 
 #ifdef __cplusplus
 }  // extern "C"
