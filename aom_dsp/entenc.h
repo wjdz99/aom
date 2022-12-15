c407c4e3dc0f6a60904ce6d63431093010a6cc46
Neeraj Gadgil <neeraj.gadgil@ittiam.com>
2022-12-15 16:36:21 +0530

    Modify encoder's entropy coding byte-push mechanism
    Changed the byte-push mechanism in libaom entropy
    encoder, as suggested in a To Do comment. In the
    parent version, the register is flushed when at
    least 1 byte is available. In the CL, we make the
    register 64-bit and flush it when at least 4 bytes
    are accumulated in it.
    
    An intermediate 16-bit buffer 'precarry_buf' is
    removed and carry-propagation mechanism is changed.
    
    No changes to the decoder logic at this time, it
    already has an OPT comment about using uint64_t
    for decode-time optimization.
    
    AV1 RTC Encoder:
    
    cpu   Enc time
           red (%)
     5      0.407
     6      0.406
     7      0.612
     8      0.964
     9      1.278
    10      1.654
    
    AVIF Encoder:
    
    cpu   Enc time
           red (%)
     6      0.238
     7      0.302
     8      0.664
     9      1.929
    
    This is a bit-exact change.
    
    Change-Id: If23e711145aef1fa193523c98b29c7c82eff76b7

--- aom_dsp/entenc.h
+++ aom_dsp/entenc.h
@@ -13,10 +13,20 @@
 #define AOM_AOM_DSP_ENTENC_H_
 #include <stddef.h>
 #include "aom_dsp/entcode.h"
+#include "aom_ports/msvc.h"
+
+#ifdef _MSC_VER
+#if defined(_M_X64) || defined(_M_IX86)
+#include <intrin.h>
+#define USE_MSC_INTRINSICS
+#endif
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef uint64_t od_ec_enc_window;
 
 typedef struct od_ec_enc od_ec_enc;
 
@@ -30,14 +40,10 @@ struct od_ec_enc {
   unsigned char *buf;
   /*The size of the buffer.*/
   uint32_t storage;
-  /*A buffer for output bytes with their associated carry flags.*/
-  uint16_t *precarry_buf;
-  /*The size of the pre-carry buffer.*/
-  uint32_t precarry_storage;
   /*The offset at which the next entropy-coded byte will be written.*/
   uint32_t offs;
   /*The low end of the current range.*/
-  od_ec_window low;
+  od_ec_enc_window low;
   /*The number of values in the current range.*/
   uint16_t rng;
   /*The number of bits of data in the current value.*/
@@ -77,6 +83,71 @@ OD_WARN_UNUSED_RESULT uint32_t od_ec_enc_tell_frac(const od_ec_enc *enc)
 
 void od_ec_enc_checkpoint(od_ec_enc *dst, const od_ec_enc *src);
 void od_ec_enc_rollback(od_ec_enc *dst, const od_ec_enc *src);
+
+// get_byteswap64:
+// Returns the number (uint64_t) with byte-positions reversed
+// e.g. input 0x123456789ABCDEF0 returns 0xF0DEBC9A78563412
+
+// GCC compiler: https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+// MSVC: https://learn.microsoft.com/en-us/cpp/c-runtime-library/
+
+// use GNU builtins where available.
+#if defined(__GNUC__) && \
+    ((__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || __GNUC__ >= 4)
+static uint64_t get_byteswap64(uint64_t num) { return __builtin_bswap64(num); }
+#elif defined(USE_MSC_INTRINSICS)
+static uint64_t get_byteswap64(uint64_t num) { return _byteswap_uint64(num); }
+#undef USE_MSC_INTRINSICS
+#else
+static uint64_t get_byteswap64(uint64_t num) {
+  uint64_t out = 0x00;
+  uint64_t mask = 0xFF00000000000000;
+  int bit_shift = 56;  // 7 bytes
+  do {
+    out |= (num & mask) >> bit_shift;
+    mask >>= 8;
+    bit_shift -= 16;
+  } while (bit_shift >= 0);
+  bit_shift = 8;  // 1 byte
+  do {
+    out |= (num & mask) << bit_shift;
+    mask >>= 8;
+    bit_shift += 16;
+  } while (bit_shift <= 56);
+  return out;
+}
+#endif
+
+// buf is the frame bitbuffer, offs is where carry to be added
+static AOM_INLINE void propagate_carry_bwd(unsigned char *buf, uint32_t offs) {
+  uint16_t sum, carry = 1;
+  do {
+    sum = (uint16_t)buf[offs] + 1;
+    buf[offs--] = (unsigned char)sum;
+    carry = sum >> 8;
+  } while (carry && (offs > 0));
+  if (carry) {
+    sum = (uint16_t)buf[offs] + 1;
+    buf[offs] = (unsigned char)sum;
+    assert(!(sum >> 8));
+  }
+}
+
+// Reverse byte order and write data to buffer adding the carry-bit
+static AOM_INLINE void write_enc_data_to_out_buf(unsigned char *out,
+                                                 uint32_t offs, uint64_t output,
+                                                 uint64_t carry,
+                                                 uint32_t *enc_offs,
+                                                 uint8_t num_bytes_ready) {
+  const uint64_t reg = get_byteswap64(output) >> ((8 - num_bytes_ready) << 3);
+  memcpy(&out[offs], &reg, 8);
+  // Propagate carry backwards if exists
+  if (carry) {
+    assert(offs > 0);
+    propagate_carry_bwd(out, offs - 1);
+  }
+  *enc_offs = offs + num_bytes_ready;
+}
 
 #ifdef __cplusplus
 }  // extern "C"
