0e1a2ef04631a176d99ad513d6d99cfeca3da194
Yunqing Wang <yunqingwang@google.com>
2023-01-06 15:40:24 -0800

    Improve VBP motion estimation for RTC speed >= 9
    Enabled neighbours' MV evaluation for speed >= 9.
    
    STATS_CHANGED
    
    Change-Id: I837773254d87b82bc064128ac4afa616492a0454

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1438,6 +1438,7 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     // estimate_motion_for_var_based_partition == 2 helps here.
     if (sf->rt_sf.estimate_motion_for_var_based_partition == 2)
       sf->rt_sf.estimate_motion_for_var_based_partition = 1;
+    if (speed >= 9) sf->rt_sf.estimate_motion_for_var_based_partition = 0;
   }
   // Screen settings.
   if (cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN) {
@@ -1501,6 +1502,7 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     // for screen contents.
     if (sf->rt_sf.estimate_motion_for_var_based_partition == 2)
       sf->rt_sf.estimate_motion_for_var_based_partition = 1;
+    if (speed >= 9) sf->rt_sf.estimate_motion_for_var_based_partition = 0;
   }
 }
 
@@ -1772,7 +1774,7 @@ static void set_rt_speed_features_framesize_independent(AV1_COMP *cpi,
   }
   if (speed >= 9) {
     sf->rt_sf.sse_early_term_inter_search = EARLY_TERM_IDX_3;
-    sf->rt_sf.estimate_motion_for_var_based_partition = 0;
+    sf->rt_sf.estimate_motion_for_var_based_partition = 3;
     sf->rt_sf.prefer_large_partition_blocks = 3;
     sf->rt_sf.skip_intra_pred = 2;
     sf->rt_sf.var_part_split_threshold_shift = 9;
