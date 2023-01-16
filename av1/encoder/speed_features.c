8269027479f5cf42f6ca22dcfcb3c44dfbb5d28d
Rachel Barker <rachelbarker@google.com>
2023-01-16 20:09:45 +0000

    Retune global motion related speed features
    * Enable global motion for "good" mode speeds 3 and 4
    
    * Use disflow-based global motion by default, as it is much faster
      and gives better results than feature matching
    
    * Add a speed feature, num_refinement_steps, which determines
      how many refinement iterations we do in av1_refine_integerized_param
    
    * Fix interaction of prune_zero_mv_with_sse and skip_repeated_ref_mv
      speed features with global motion
    
    * Fix assertions in prune_zero_mv_with_sse code, to check the correct
      conditions for compound blocks
    
    * Retune the following speed features:
      * Global motion usage
      * Number of global motion refinement steps
      * prune_zero_mv_with_sse
      * skip_repeated_ref_mv
    
    Results:
    
          |      lowres2       |      midres2       |       hdres2
    Speed |  BDRATE | Enc time |  BDRATE | Enc Time |  BDRATE | Enc Time
    ------+---------+----------+---------+----------+---------+---------
      1   | -0.029% |  -2.289% | -0.057% |  -2.201% | -0.058% |  -0.192%
      2   | +0.010% |  -5.912% | -0.036% |  -4.855% | -0.023% |  -1.333%
      3   | -1.302% |  +3.598% | -0.857% |  +3.607% | -0.425% |  +3.703%
      4   | -1.299% |  +4.483% | -0.909% |  +5.038% | -0.511% |  +5.110%
      5   | -0.051% |  -0.324% | -0.136% |  +0.213% | -0.049% |  +0.099%
      6   | -0.013% |  +0.225% | -0.050% |  +0.080% | -0.021% |  +0.113%
    
    STATS_CHANGED
    
    Change-Id: Ie054a79890ecf6ef8b57f0dbffd2ebca5aecbda1

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -999,6 +999,7 @@ static void set_good_speed_features_framesize_independent(
     sf->fp_sf.skip_motion_search_threshold = 25;
 
     sf->gm_sf.disable_gm_search_based_on_stats = 1;
+    sf->gm_sf.num_refinement_steps = 2;
 
     sf->part_sf.reuse_best_prediction_for_part_ab =
         !frame_is_intra_only(&cpi->common);
@@ -1055,8 +1056,8 @@ static void set_good_speed_features_framesize_independent(
   if (speed >= 3) {
     sf->hl_sf.high_precision_mv_usage = CURRENT_Q;
 
-    sf->gm_sf.gm_search_type = GM_DISABLE_SEARCH;
     sf->gm_sf.prune_zero_mv_with_sse = 1;
+    sf->gm_sf.num_refinement_steps = 0;
 
     sf->part_sf.less_rectangular_check_level = 2;
     sf->part_sf.simple_motion_search_prune_agg =
@@ -1082,7 +1083,6 @@ static void set_good_speed_features_framesize_independent(
     sf->inter_sf.prune_inter_modes_based_on_tpl = boosted ? 0 : 1;
     sf->inter_sf.prune_comp_search_by_single_result = boosted ? 4 : 2;
     sf->inter_sf.selective_ref_frame = 5;
-    sf->inter_sf.skip_repeated_ref_mv = 1;
     sf->inter_sf.reuse_compound_type_decision = 1;
     sf->inter_sf.txfm_rd_gate_level =
         boosted ? 0 : (is_boosted_arf2_bwd_type ? 1 : 2);
@@ -1129,9 +1129,9 @@ static void set_good_speed_features_framesize_independent(
   }
 
   if (speed >= 4) {
-    sf->gm_sf.prune_zero_mv_with_sse = 2;
-
     sf->mv_sf.subpel_search_method = SUBPEL_TREE_PRUNED_MORE;
+
+    sf->gm_sf.prune_zero_mv_with_sse = 2;
 
     sf->part_sf.simple_motion_search_prune_agg =
         allow_screen_content_tools ? SIMPLE_AGG_LVL0 : SIMPLE_AGG_LVL2;
@@ -1187,6 +1187,8 @@ static void set_good_speed_features_framesize_independent(
     sf->hl_sf.weight_calc_level_in_tf = use_hbd ? 0 : 1;
 
     sf->fp_sf.reduce_mv_step_param = 4;
+
+    sf->gm_sf.gm_search_type = GM_DISABLE_SEARCH;
 
     sf->part_sf.simple_motion_search_prune_agg =
         allow_screen_content_tools ? SIMPLE_AGG_LVL0 : SIMPLE_AGG_LVL3;
@@ -1849,6 +1851,7 @@ static AOM_INLINE void init_gm_sf(GLOBAL_MOTION_SPEED_FEATURES *gm_sf) {
   gm_sf->prune_ref_frame_for_gm_search = 0;
   gm_sf->prune_zero_mv_with_sse = 0;
   gm_sf->disable_gm_search_based_on_stats = 0;
+  gm_sf->num_refinement_steps = GM_MAX_REFINEMENT_STEPS;
 }
 
 static AOM_INLINE void init_part_sf(PARTITION_SPEED_FEATURES *part_sf) {
