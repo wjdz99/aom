d83e8b6ca055ad32cbf2e6647b93014cf2a1b886
Yunqing Wang <yunqingwang@google.com>
2023-01-18 15:41:16 -0800

    Improve frame interpolation filter resetting
    In current code base, the frame interpolation filter is allowed to be
    reset only for EIGHTTAP_REGULAR filter type. This condition is overly
    strict and would miss opportunities for bit saving. This CL modifies
    the way that the filter type counts are recorded to ensure the correct
    frame filter type resetting decision, and also loosens the condition
    for frame filter resetting. This change will help some RTC encoding,
    which will be implemented in the following CL.
    
    STATS_CHANGED
    
    Change-Id: I34e7e0faffcb80344468294c0b59cc03e1e6d58c

--- av1/encoder/encoder_utils.c
+++ av1/encoder/encoder_utils.c
@@ -1106,11 +1106,12 @@ static void fix_interp_filter(InterpFilter *const interp_filter,
         count[i] += counts->switchable_interp[j][i];
       num_filters_used += (count[i] > 0);
     }
+
     if (num_filters_used == 1) {
       // Only one filter is used. So set the filter at frame level
       for (int i = 0; i < SWITCHABLE_FILTERS; ++i) {
         if (count[i]) {
-          if (i == EIGHTTAP_REGULAR) *interp_filter = i;
+          *interp_filter = i;
           break;
         }
       }
@@ -1160,7 +1161,8 @@ void av1_finalize_encoded_frame(AV1_COMP *const cpi) {
     }
   }
 
-  fix_interp_filter(&cm->features.interp_filter, cpi->td.counts);
+  if (!frame_is_intra_only(cm))
+    fix_interp_filter(&cm->features.interp_filter, cpi->td.counts);
 }
 
 int av1_is_integer_mv(const YV12_BUFFER_CONFIG *cur_picture,
