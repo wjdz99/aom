c1a7dbe1bebfd1ed7538c1d7cb1c1930c3418ef6
Marco Paniconi <marpan@google.com>
2023-03-19 23:42:40 -0700

    rtc: Use 4x4 for blk_skip on inter modes
    This is needed if tx_size for inter modes
    gets set to 4x4. Currently nonrd_pickmode
    never uses tx_size below 8X8 (for inter-modes,
    intra may use 4x4), But for lossless modes 4X4
    tx_size is forced, and this causes incorrect behavior.
    Always using 4x4 for blk_skip fixes this.
    
    This will cause some small slowdown since memset/memcpy
    is done for 4x4 blocks instead of 8x8, but code is.
    cleaner and safer this way (always keeping 4x4).
    
    Change-Id: Ice75353dffb3bdb0d736ce9649fa3bb9cfba7ce8

--- av1/encoder/nonrd_opt.h
+++ av1/encoder/nonrd_opt.h
@@ -542,7 +542,7 @@ static INLINE void store_coding_context_nonrd(MACROBLOCK *x,
 }
 
 void av1_block_yrd(MACROBLOCK *x, RD_STATS *this_rdc, int *skippable,
-                   BLOCK_SIZE bsize, TX_SIZE tx_size, int is_inter_mode);
+                   BLOCK_SIZE bsize, TX_SIZE tx_size);
 
 void av1_block_yrd_idtx(MACROBLOCK *x, RD_STATS *this_rdc, int *skippable,
                         BLOCK_SIZE bsize, TX_SIZE tx_size);
