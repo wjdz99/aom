49278b164095955ee2fb02d97c8765183cdef995
Marco Paniconi <marpan@google.com>
2023-03-21 13:42:27 -0700

    rtc: Remove blk_skip from best_mode
    Copy directly into ctx, save some memset/memcp,
    and removes unneeded code. Also for intra modes:
    only do the memset once, if intra modes are selected
    as best mode.
    
    bit-exact change
    IC change:
    
    Change-Id: Id77746e0c146a0f38cc03d9b7ced5fbda25e5869

--- av1/encoder/nonrd_opt.c
+++ av1/encoder/nonrd_opt.c
@@ -921,10 +921,10 @@ void av1_estimate_intra_mode(AV1_COMP *cpi, MACROBLOCK *x, BLOCK_SIZE bsize,
       mi->uv_mode = this_mode;
       mi->mv[0].as_int = INVALID_MV;
       mi->mv[1].as_int = INVALID_MV;
-      if (!this_rdc.skip_txfm)
-        memset(ctx->blk_skip, 0,
-               sizeof(x->txfm_search_info.blk_skip[0]) * ctx->num_4x4_blk);
     }
   }
+  if (best_pickmode->best_ref_frame == INTRA_FRAME)
+    memset(ctx->blk_skip, 0,
+           sizeof(x->txfm_search_info.blk_skip[0]) * ctx->num_4x4_blk);
   mi->tx_size = best_pickmode->best_tx_size;
 }
