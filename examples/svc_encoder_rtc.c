e315548be949b7d7a7609305e1658d2328d6cb71
Jerome Jiang <jianj@google.com>
2023-03-27 09:18:08 -0400

    Fix overflow and memory leak of svc_encoder_rtc
    Bug: aomedia:3404
    Change-Id: Ie9e3ec9fe627029228c215ee44ea851a20426f57

--- examples/svc_encoder_rtc.c
+++ examples/svc_encoder_rtc.c
@@ -252,9 +252,11 @@ static aom_codec_err_t parse_layer_options_from_string(
       (option1 == NULL && type == SCALE_FACTOR))
     return AOM_CODEC_INVALID_PARAM;
 
-  input_string = malloc(strlen(input));
+  const size_t input_length = strlen(input);
+  input_string = malloc(input_length + 1);
   if (!input_string) die("Failed to allocate input string.");
-  memcpy(input_string, input, strlen(input));
+  memcpy(input_string, input, input_length);
+  input_string[input_length] = '\0';
   if (input_string == NULL) return AOM_CODEC_MEM_ERROR;
   token = strtok(input_string, delim);  // NOLINT
   for (i = 0; i < num_layers; ++i) {
@@ -1694,6 +1696,11 @@ int main(int argc, const char **argv) {
          1000000 * (double)frame_cnt / (double)cx_time);
 
   if (aom_codec_destroy(&codec)) die_codec(&codec, "Failed to destroy codec");
+
+#if CONFIG_AV1_DECODER
+  if (aom_codec_destroy(&decoder))
+    die_codec(&decoder, "Failed to destroy decoder");
+#endif
 
 #if CONFIG_INTERNAL_STATS
   fprintf(stats_file, "No mismatch detected in recon buffers\n");
