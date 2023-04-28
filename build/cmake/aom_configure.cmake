47c9c93f7850744a55ecb34f8ce4388cf110352d
Wan-Teh Chang <wtc@google.com>
2023-04-28 16:25:49 -0700

    Explicitly specify /std:c++14 for MSVC
    The /std:c++14 option is the default. Explicitly specify it for clarity.
    
    Change-Id: I70218dbeb9f3c1a8b0c2abdf451de537f2ce14ef

--- build/cmake/aom_configure.cmake
+++ build/cmake/aom_configure.cmake
@@ -302,6 +302,9 @@ endif()
 
 # Test compiler flags.
 if(MSVC)
+  # The C++ compiler in Visual Studio doesn't support C++ standards modes
+  # earlier than C++14.
+  add_cxx_flag_if_supported("/std:c++14")
   add_compiler_flag_if_supported("/W3")
 
   # Disable MSVC warnings that suggest making code non-portable.
