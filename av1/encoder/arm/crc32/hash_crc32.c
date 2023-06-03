wjdz99/aom

commit fe7676b2c113501b3f1ef399b16f8aebd0572c44

s/__aarch64__/AOM_ARCH_AARCH64/
This allows AArch64 to be correctly detected when building with Visual
Studio (cl.exe). There are still test failures, however.

Microsoft's compiler doesn't define __ARM_FEATURE_*. To use those paths
we may need to rely on _M_ARM64_EXTENSION.

This change is similar to the one in libvpx:
57b9afa58 s/__aarch64__/VPX_ARCH_AARCH64/

Bug: b/277255390
Change-Id: Ie3174d59c1bcdab5677b81b0176cc363cf18018f

av1/encoder/arm/crc32/hash_crc32.c
@@ -37,7 +37,7 @@ uint32_t av1_get_crc32c_value_arm_crc32(void *crc_calculator, uint8_t *p,
   const uint8_t *buf = p;
   uint32_t crc = 0xFFFFFFFF;
 
-#if !defined(__aarch64__)
+#if !AOM_ARCH_AARCH64
   // Align input to 8-byte boundary (only necessary for 32-bit builds.)
   while (len && ((uintptr_t)buf & 7)) {
     crc = __crc32cb(crc, *buf++);

aom_dsp/arm/avg_neon.c
@@ -18,7 +18,7 @@
 #include "aom_dsp/arm/transpose_neon.h"
 #include "aom_ports/mem.h"
 
-#if !defined(__aarch64__)
+#if !AOM_ARCH_AARCH64
 static INLINE uint32x2_t horizontal_add_u16x8_v(const uint16x8_t a) {
   const uint32x4_t b = vpaddlq_u16(a);
   const uint64x2_t c = vpaddlq_u32(b);
@@ -30,7 +30,7 @@ static INLINE uint32x2_t horizontal_add_u16x8_v(const uint16x8_t a) {
 unsigned int aom_avg_4x4_neon(const uint8_t *a, int a_stride) {
   const uint8x16_t b = load_unaligned_u8q(a, a_stride);
   const uint16x8_t c = vaddl_u8(vget_low_u8(b), vget_high_u8(b));
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   const uint32_t d = vaddlvq_u16(c);
   return (d + 8) >> 4;
 #else
@@ -53,7 +53,7 @@ unsigned int aom_avg_8x8_neon(const uint8_t *a, int a_stride) {
     sum = vaddw_u8(sum, e);
   }
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   const uint32_t d = vaddlvq_u16(sum);
   return (d + 32) >> 6;
 #else
@@ -216,7 +216,7 @@ int aom_vector_var_neon(const int16_t *ref, const int16_t *src, int bwl) {
     v_mean = vpadalq_s16(v_mean, diff);
     v_low = vget_low_s16(diff);
     v_sse = vmlal_s16(v_sse, v_low, v_low);
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
     v_sse = vmlal_high_s16(v_sse, diff, diff);
 #else
     const int16x4_t v_high = vget_high_s16(diff);
@@ -259,7 +259,7 @@ void aom_minmax_8x8_neon(const uint8_t *a, int a_stride, const uint8_t *b,
   const uint8x16_t ab07_max = vmaxq_u8(ab0123_max, ab4567_max);
   const uint8x16_t ab07_min = vminq_u8(ab0123_min, ab4567_min);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   *min = *max = 0;  // Clear high bits
   *((uint8_t *)max) = vmaxvq_u8(ab07_max);
   *((uint8_t *)min) = vminvq_u8(ab07_min);

aom_dsp/arm/highbd_avg_neon.c
@@ -98,7 +98,7 @@ void aom_highbd_minmax_8x8_neon(const uint8_t *s8, int p, const uint8_t *d8,
   const uint16x8_t min4567 = vminq_u16(min45, min67);
   const uint16x8_t min07 = vminq_u16(min0123, min4567);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   *max = (int)vmaxvq_u16(max07);
   *min = (int)vminvq_u16(min07);
 #else

aom_dsp/arm/highbd_loopfilter_neon.c
@@ -247,12 +247,12 @@ void aom_highbd_lpf_horizontal_4_neon(uint16_t *s, int pitch,
   filter4_masks(p0q0, p1q1, hev_thresh, outer_mask, inner_thresh, &hev_mask,
                 &needs_filter4_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter4_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -313,12 +313,12 @@ void aom_highbd_lpf_vertical_4_neon(uint16_t *s, int pitch,
   filter4_masks(p0q0, p1q1, hev_thresh, outer_mask, inner_thresh, &hev_mask,
                 &needs_filter4_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter4_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -437,12 +437,12 @@ void aom_highbd_lpf_horizontal_6_neon(uint16_t *s, int pitch,
   filter6_masks(p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh, bd,
                 &needs_filter_mask, &is_flat3_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -528,12 +528,12 @@ void aom_highbd_lpf_vertical_6_neon(uint16_t *s, int pitch,
   filter6_masks(p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh, bd,
                 &needs_filter_mask, &is_flat3_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -684,12 +684,12 @@ void aom_highbd_lpf_horizontal_8_neon(uint16_t *s, int pitch,
   filter8_masks(p3q3, p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh,
                 bd, &needs_filter_mask, &is_flat4_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -783,12 +783,12 @@ void aom_highbd_lpf_vertical_8_neon(uint16_t *s, int pitch,
   filter8_masks(p3q3, p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh,
                 bd, &needs_filter_mask, &is_flat4_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   // Copy the masks to the high bits for packed comparisons later.
   const uint16x8_t hev_mask_8 = vcombine_u16(hev_mask, hev_mask);
@@ -976,12 +976,12 @@ void aom_highbd_lpf_horizontal_14_neon(uint16_t *s, int pitch,
   filter8_masks(p3q3, p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh,
                 bd, &needs_filter_mask, &is_flat4_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
   const uint16x8_t p4q4 = vcombine_u16(src[2], src[11]);
   const uint16x8_t p5q5 = vcombine_u16(src[1], src[12]);
   const uint16x8_t p6q6 = vcombine_u16(src[0], src[13]);
@@ -1083,7 +1083,7 @@ void aom_highbd_lpf_horizontal_14_dual_neon(
 static INLINE uint16x8x2_t permute_acdb64(const uint16x8_t ab,
                                           const uint16x8_t cd) {
   uint16x8x2_t acdb;
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   // a[b] <- [c]d
   acdb.val[0] = vreinterpretq_u16_u64(
       vtrn1q_u64(vreinterpretq_u64_u16(ab), vreinterpretq_u64_u16(cd)));
@@ -1099,7 +1099,7 @@ static INLINE uint16x8x2_t permute_acdb64(const uint16x8_t ab,
   acdb.val[1] = vreinterpretq_u16_u64(
       vsetq_lane_u64(vgetq_lane_u64(vreinterpretq_u64_u16(cd), 1),
                      vreinterpretq_u64_u16(ab), 0));
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
   return acdb;
 }
 
@@ -1144,12 +1144,12 @@ void aom_highbd_lpf_vertical_14_neon(uint16_t *s, int pitch,
   filter8_masks(p3q3, p2q2, p1q1, p0q0, hev_thresh, outer_mask, inner_thresh,
                 bd, &needs_filter_mask, &is_flat4_mask, &hev_mask);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   if (vaddv_u16(needs_filter_mask) == 0) {
     // None of the values will be filtered.
     return;
   }
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
   const uint16x8_t p4q4 =
       vcombine_u16(vget_low_u16(src_p[3]), vget_high_u16(src_q[0]));
   const uint16x8_t p5q5 =

aom_dsp/arm/highbd_quantize_neon.c
@@ -19,7 +19,7 @@
 #include "av1/encoder/av1_quantize.h"
 
 static INLINE uint32_t sum_abs_coeff(const uint32x4_t a) {
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   return vaddvq_u32(a);
 #else
   const uint64x2_t b = vpaddlq_u32(a);
@@ -98,7 +98,7 @@ static INLINE void get_min_max_lane_eob(const int16_t *iscan,
 }
 
 static INLINE uint16_t get_max_eob(int16x8_t v_eobmax) {
-#ifdef __aarch64__
+#if AOM_ARCH_AARCH64
   return (uint16_t)vmaxvq_s16(v_eobmax);
 #else
   const int16x4_t v_eobmax_3210 =
@@ -116,7 +116,7 @@ static INLINE uint16_t get_max_eob(int16x8_t v_eobmax) {
 }
 
 static INLINE uint16_t get_min_eob(int16x8_t v_eobmin) {
-#ifdef __aarch64__
+#if AOM_ARCH_AARCH64
   return (uint16_t)vminvq_s16(v_eobmin);
 #else
   const int16x4_t v_eobmin_3210 =

aom_dsp/arm/intrapred_neon.c
@@ -84,7 +84,7 @@ static INLINE uint16x8_t dc_load_sum_8(const uint8_t *in) {
 }
 
 static INLINE uint16x8_t horizontal_add_and_broadcast_u16x8(uint16x8_t a) {
-#ifdef __aarch64__
+#if AOM_ARCH_AARCH64
   // On AArch64 we could also use vdupq_n_u16(vaddvq_u16(a)) here to save an
   // instruction, however the addv instruction is usually slightly more
   // expensive than a pairwise addition, so the need for immediately
@@ -1528,13 +1528,13 @@ static void dr_prediction_z2_Nx4_neon(int N, uint8_t *dst, ptrdiff_t stride,
   int16x4_t v_frac_bits_y = vdup_n_s16(-frac_bits_y);
   int16x4_t min_base_y64 = vdup_n_s16(min_base_y);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   // Use ext rather than loading left + 14 directly to avoid over-read.
   const uint8x16_t left_m2 = vld1q_u8(left - 2);
   const uint8x16_t left_0 = vld1q_u8(left);
   const uint8x16_t left_14 = vextq_u8(left_0, left_0, 14);
   const uint8x16x2_t left_vals = { { left_m2, left_14 } };
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   for (int r = 0; r < N; r++) {
     uint16x8_t res, shift;
@@ -1593,13 +1593,13 @@ static void dr_prediction_z2_Nx4_neon(int N, uint8_t *dst, ptrdiff_t stride,
       // Values in base_y_c64 range from -2 through 14 inclusive.
       base_y_c64 = vbic_s16(base_y_c64, vreinterpret_s16_u16(mask64));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
       uint8x8_t left_idx0 = vreinterpret_u8_s16(base_y_c64 + 2);  // [0, 16]
       uint8x8_t left_idx1 = vreinterpret_u8_s16(base_y_c64 + 3);  // [1, 17]
 
       uint8x8_t a0_y = vtrn1_u8(vqtbl2_u8(left_vals, left_idx0), v_zero_u8);
       uint8x8_t a1_y = vtrn1_u8(vqtbl2_u8(left_vals, left_idx1), v_zero_u8);
-#else   // !defined(__aarch64__)
+#else   // !AOM_ARCH_AARCH64
       DECLARE_ALIGNED(32, int16_t, base_y_c[4]);
 
       vst1_s16(base_y_c, base_y_c64);
@@ -1616,7 +1616,7 @@ static void dr_prediction_z2_Nx4_neon(int N, uint8_t *dst, ptrdiff_t stride,
       a1_y = vld1_lane_u8(left + base_y_c[1], a1_y, 2);
       a1_y = vld1_lane_u8(left + base_y_c[2], a1_y, 4);
       a1_y = vld1_lane_u8(left + base_y_c[3], a1_y, 6);
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
       if (upsample_left) {
         v_shift.val[1] = vshr_n_u16(
@@ -1696,7 +1696,7 @@ static void dr_prediction_z2_Nx8_neon(int N, uint8_t *dst, ptrdiff_t stride,
   uint16x8_t c1234 = vcombine_u16(vcreate_u16(0x0004000300020001),
                                   vcreate_u16(0x0008000700060005));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   // Use ext rather than loading left + 30 directly to avoid over-read.
   const uint8x16_t left_m2 = vld1q_u8(left - 2);
   const uint8x16_t left_0 = vld1q_u8(left + 0);
@@ -1704,7 +1704,7 @@ static void dr_prediction_z2_Nx8_neon(int N, uint8_t *dst, ptrdiff_t stride,
   const uint8x16_t left_14 = vextq_u8(left_0, left_16, 14);
   const uint8x16_t left_30 = vextq_u8(left_16, left_16, 14);
   const uint8x16x3_t left_vals = { { left_m2, left_14, left_30 } };
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   for (int r = 0; r < N; r++) {
     uint8x8_t resx, resy, resxy;
@@ -1776,7 +1776,7 @@ static void dr_prediction_z2_Nx8_neon(int N, uint8_t *dst, ptrdiff_t stride,
       // Values in base_y_c128 range from -2 through 31 inclusive.
       base_y_c128 = vbicq_s16(base_y_c128, vreinterpretq_s16_u16(mask128));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
       uint8x16_t left_idx0 = vreinterpretq_u8_s16(base_y_c128 + 2);  // [0, 33]
       uint8x16_t left_idx1 = vreinterpretq_u8_s16(base_y_c128 + 3);  // [1, 34]
       uint8x16_t left_idx01 = vuzp1q_u8(left_idx0, left_idx1);
@@ -1784,7 +1784,7 @@ static void dr_prediction_z2_Nx8_neon(int N, uint8_t *dst, ptrdiff_t stride,
       uint8x16_t a01_x = vqtbl3q_u8(left_vals, left_idx01);
       uint8x8_t a0_x1 = vget_low_u8(a01_x);
       uint8x8_t a1_x1 = vget_high_u8(a01_x);
-#else   // !defined(__aarch64__)
+#else   // !AOM_ARCH_AARCH64
       DECLARE_ALIGNED(32, int16_t, base_y_c[16]);
 
       vst1q_s16(base_y_c, base_y_c128);
@@ -1809,7 +1809,7 @@ static void dr_prediction_z2_Nx8_neon(int N, uint8_t *dst, ptrdiff_t stride,
       a1_x1 = vld1_lane_u8(left + base_y_c[5], a1_x1, 5);
       a1_x1 = vld1_lane_u8(left + base_y_c[6], a1_x1, 6);
       a1_x1 = vld1_lane_u8(left + base_y_c[7], a1_x1, 7);
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
       if (upsample_left) {
         shift.val[1] = vshrq_n_u16(
@@ -1867,7 +1867,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
   c1234.val[0] = vaddq_u16(c0123.val[0], c1);
   c1234.val[1] = vaddq_u16(c0123.val[1], c1);
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   const uint8x16_t left_m1 = vld1q_u8(left - 1);
   const uint8x16_t left_0 = vld1q_u8(left + 0);
   const uint8x16_t left_16 = vld1q_u8(left + 16);
@@ -1878,7 +1878,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
   const uint8x16_t left_47 = vextq_u8(left_32, left_48, 15);
   const uint8x16x4_t left_vals0 = { { left_m1, left_15, left_31, left_47 } };
   const uint8x16x4_t left_vals1 = { { left_0, left_16, left_32, left_48 } };
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
   for (int r = 0; r < H; r++) {
     uint16x8x2_t res, r6, shift;
@@ -1991,7 +1991,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
           a0_y128 = vandq_u8(a0_y128, v_loadmaskz2);
           a1_y128 = vld1q_u8(left + min_y + 1);
           a1_y128 = vandq_u8(a1_y128, v_loadmaskz2);
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
           a0_y128 = vqtbl1q_u8(a0_y128, vreinterpretq_u8_s8(base_y_offset128));
           a1_y128 = vqtbl1q_u8(a1_y128, vreinterpretq_u8_s8(base_y_offset128));
 #else
@@ -2023,7 +2023,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
           base_y_c256.val[1] = vbicq_s16(base_y_c256.val[1],
                                          vreinterpretq_s16_u16(mask256.val[1]));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
           // Values in left_idx{0,1} range from 0 through 63 inclusive.
           uint8x16_t left_idx0 = vreinterpretq_u8_s16(base_y_c256.val[0] + 1);
           uint8x16_t left_idx1 = vreinterpretq_u8_s16(base_y_c256.val[1] + 1);
@@ -2037,7 +2037,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
           a0_y1 = vget_high_u8(a0_y01);
           a1_y0 = vget_low_u8(a1_y01);
           a1_y1 = vget_high_u8(a1_y01);
-#else   // !defined(__aarch64__)
+#else   // !AOM_ARCH_AARCH64
           DECLARE_ALIGNED(32, int16_t, base_y_c[16]);
 
           vst1q_s16(base_y_c, base_y_c256.val[0]);
@@ -2086,7 +2086,7 @@ static void dr_prediction_z2_HxW_neon(int H, int W, uint8_t *dst,
           a1_y1 = vld1_lane_u8(left + base_y_c[13], a1_y1, 5);
           a1_y1 = vld1_lane_u8(left + base_y_c[14], a1_y1, 6);
           a1_y1 = vld1_lane_u8(left + base_y_c[15], a1_y1, 7);
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
         }
 
         shifty.val[0] = vshrq_n_u16(
@@ -2318,7 +2318,7 @@ static AOM_FORCE_INLINE void transpose8x16_16x8_neon(uint8x16_t *x,
   w11 = vzipq_u32(vreinterpretq_u32_u16(w6.val[1]),
                   vreinterpretq_u32_u16(w7.val[1]));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[0] = vzip1q_u64(vreinterpretq_u64_u32(w8.val[0]),
                     vreinterpretq_u64_u32(w9.val[0]));
   d[1] = vzip2q_u64(vreinterpretq_u64_u32(w8.val[0]),
@@ -2388,7 +2388,7 @@ static AOM_FORCE_INLINE void transpose16x16_neon(uint8x16_t *x, uint64x2_t *d) {
   w15 = vzipq_u32(vreinterpretq_u32_u16(w10.val[1]),
                   vreinterpretq_u32_u16(w11.val[1]));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[0] = vzip1q_u64(vreinterpretq_u64_u32(w12.val[0]),
                     vreinterpretq_u64_u32(w13.val[0]));
   d[1] = vzip2q_u64(vreinterpretq_u64_u32(w12.val[0]),
@@ -2443,7 +2443,7 @@ static AOM_FORCE_INLINE void transpose16x16_neon(uint8x16_t *x, uint64x2_t *d) {
   w15 = vzipq_u32(vreinterpretq_u32_u16(w10.val[1]),
                   vreinterpretq_u32_u16(w11.val[1]));
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[8] = vzip1q_u64(vreinterpretq_u64_u32(w12.val[0]),
                     vreinterpretq_u64_u32(w13.val[0]));
   d[9] = vzip2q_u64(vreinterpretq_u64_u32(w12.val[0]),
@@ -2516,7 +2516,7 @@ static AOM_FORCE_INLINE void transpose16x32_neon(uint8x16x2_t *x,
 
   // Store first 4-line result
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[0].val[0] = vzip1q_u64(vreinterpretq_u64_u32(w6.val[0]),
                            vreinterpretq_u64_u32(w14.val[0]));
   d[0].val[1] = vzip2q_u64(vreinterpretq_u64_u32(w6.val[0]),
@@ -2572,7 +2572,7 @@ static AOM_FORCE_INLINE void transpose16x32_neon(uint8x16x2_t *x,
 
   // Store second 4-line result
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[4].val[0] = vzip1q_u64(vreinterpretq_u64_u32(w6.val[0]),
                            vreinterpretq_u64_u32(w14.val[0]));
   d[4].val[1] = vzip2q_u64(vreinterpretq_u64_u32(w6.val[0]),
@@ -2639,7 +2639,7 @@ static AOM_FORCE_INLINE void transpose16x32_neon(uint8x16x2_t *x,
 
   // Store first 4-line result
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[8].val[0] = vzip1q_u64(vreinterpretq_u64_u32(w6.val[0]),
                            vreinterpretq_u64_u32(w14.val[0]));
   d[8].val[1] = vzip2q_u64(vreinterpretq_u64_u32(w6.val[0]),
@@ -2695,7 +2695,7 @@ static AOM_FORCE_INLINE void transpose16x32_neon(uint8x16x2_t *x,
 
   // Store second 4-line result
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
   d[12].val[0] = vzip1q_u64(vreinterpretq_u64_u32(w6.val[0]),
                             vreinterpretq_u64_u32(w14.val[0]));
   d[12].val[1] = vzip2q_u64(vreinterpretq_u64_u32(w6.val[0]),

aom_dsp/arm/obmc_sad_neon.c
@@ -37,7 +37,7 @@ static INLINE void obmc_sad_8x1_s16_neon(int16x8_t ref_s16, const int32_t *mask,
   *sum = vrsraq_n_u32(*sum, abs_hi, 12);
 }
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
 
 // Use tbl for doing a double-width zero extension from 8->32 bits since we can
 // do this in one instruction rather than two (indices out of range (255 here)
@@ -110,7 +110,7 @@ static INLINE unsigned int obmc_sad_large_neon(const uint8_t *ref,
   return horizontal_add_u32x4(vaddq_u32(sum[0], sum[1]));
 }
 
-#else  // !defined(__aarch64__)
+#else  // !AOM_ARCH_AARCH64
 
 static INLINE unsigned int obmc_sad_large_neon(const uint8_t *ref,
                                                int ref_stride,
@@ -144,7 +144,7 @@ static INLINE unsigned int obmc_sad_large_neon(const uint8_t *ref,
   return horizontal_add_u32x4(sum);
 }
 
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
 static INLINE unsigned int obmc_sad_128xh_neon(const uint8_t *ref,
                                                int ref_stride,

aom_dsp/arm/obmc_variance_neon.c
@@ -55,7 +55,7 @@ static INLINE void obmc_variance_8x1_s16_neon(int16x8_t pre_s16,
   *ssev = vmlaq_s32(*ssev, round_s32_hi, round_s32_hi);
 }
 
-#if defined(__aarch64__)
+#if AOM_ARCH_AARCH64
 
 // Use tbl for doing a double-width zero extension from 8->32 bits since we can
 // do this in one instruction rather than two (indices out of range (255 here)
@@ -140,7 +140,7 @@ static INLINE void obmc_variance_large_neon(const uint8_t *pre, int pre_stride,
   *sum = horizontal_add_s32x4(sumv);
 }
 
-#else  // !defined(__aarch64__)
+#else  // !AOM_ARCH_AARCH64
 
 static INLINE void obmc_variance_large_neon(const uint8_t *pre, int pre_stride,
                                             const int32_t *wsrc,
@@ -180,7 +180,7 @@ static INLINE void obmc_variance_large_neon(const uint8_t *pre, int pre_stride,
   *sum = horizontal_add_s32x4(sumv);
 }
 
-#endif  // defined(__aarch64__)
+#endif  // AOM_ARCH_AARCH64
 
 static INLINE void obmc_variance_neon_128xh(const uint8_t *pre, int pre_stride,
                                             const int32_t *wsrc,

commit 1cb9cff82ac55e28bfffe6772cb632c5d3bcef13

Include config/aom_config.h for AOM_ARCH_AARCH64
We test the value of AOM_ARCH_AARCH64 (zero or nonzero) rather than
whether it is defined or undefined. So we need to include
config/aom_config.h for the macro's definition.

This bug was introduced in commit fe7676b.

Bug: b/277255390
Change-Id: Ib1cc7527961d6cb2b4ebbc6ee4dcee9aad43c267

av1/encoder/arm/crc32/hash_crc32.c
@@ -13,6 +13,8 @@
 #include <stddef.h>
 #include <arm_acle.h>
 
+#include "config/aom_config.h"
+
 #define CRC_LOOP(op, crc, type, buf, len) \
   while ((len) >= sizeof(type)) {         \
     (crc) = op((crc), *(type *)(buf));    \