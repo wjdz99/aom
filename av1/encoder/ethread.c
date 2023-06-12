b88b555f72ed64adad239db2ae950120d22ce01c
Mudassir Galaganath <mudassir.galaganath@ittiam.com>
2023-06-12 17:20:43 +0530

    Propagate error from worker to main thread during row-MT
    This change sets a longjmp target in enc_row_mt_worker_hook()
    to propagate error from worker threads to the main thread
    during row multi-threading. It also handles the error
    propagation in case of pipelining of loop filter MT after
    encoding.
    
    Bug: aomedia:3276
    
    Change-Id: I6117dbe2de0f4a2dd73e6f58a7624a1d79561da0

--- av1/encoder/ethread.c
+++ av1/encoder/ethread.c
@@ -264,6 +264,7 @@ static void row_mt_mem_alloc(AV1_COMP *cpi, int max_rows, int max_cols,
   enc_row_mt->allocated_rows = max_rows;
   enc_row_mt->allocated_cols = max_cols - 1;
   enc_row_mt->allocated_sb_rows = sb_rows;
+  enc_row_mt->row_mt_exit = 0;
 }
 
 void av1_row_mt_mem_dealloc(AV1_COMP *cpi) {
@@ -455,6 +456,7 @@ static void launch_loop_filter_rows(AV1_COMMON *cm, EncWorkerData *thread_data,
   AV1LfSync *const lf_sync = (AV1LfSync *)thread_data->lf_sync;
   const int sb_rows = get_sb_rows_in_frame(cm);
   AV1LfMTInfo *cur_job_info;
+  int row_mt_exit = 0;
   (void)enc_row_mt;
 #if CONFIG_MULTITHREAD
   pthread_mutex_t *enc_row_mt_mutex_ = enc_row_mt->mutex_;
@@ -469,12 +471,15 @@ static void launch_loop_filter_rows(AV1_COMMON *cm, EncWorkerData *thread_data,
     const int next_sb_row = AOMMIN(sb_rows - 1, cur_sb_row + 1);
     // Wait for current and next superblock row to finish encoding.
     pthread_mutex_lock(enc_row_mt_mutex_);
+    row_mt_exit = enc_row_mt->row_mt_exit;
     while (enc_row_mt->num_tile_cols_done[cur_sb_row] < cm->tiles.cols ||
            enc_row_mt->num_tile_cols_done[next_sb_row] < cm->tiles.cols) {
       pthread_cond_wait(enc_row_mt->cond_, enc_row_mt_mutex_);
     }
     pthread_mutex_unlock(enc_row_mt_mutex_);
 #endif
+    if (row_mt_exit) return;
+
     av1_thread_loop_filter_rows(
         lf_data->frame_buffer, lf_data->cm, lf_data->planes, lf_data->xd,
         cur_job_info->mi_row, cur_job_info->plane, cur_job_info->dir,
@@ -483,18 +488,77 @@ static void launch_loop_filter_rows(AV1_COMMON *cm, EncWorkerData *thread_data,
   }
 }
 
+void av1_set_encoding_done_for_row(AV1_COMP *cpi, MACROBLOCKD *xd) {
+  AV1_COMMON *const cm = &cpi->common;
+  TileDataEnc *const this_tile =
+      &cpi->tile_data[xd->tile.tile_row * cm->tiles.cols + xd->tile.tile_col];
+  const TileInfo *const tile_info = &this_tile->tile_info;
+  MultiThreadInfo *const mt_info = &cpi->mt_info;
+  AV1EncRowMultiThreadInfo *const enc_row_mt = &mt_info->enc_row_mt;
+  AV1EncRowMultiThreadSync *const row_mt_sync = &this_tile->row_mt_sync;
+  const int sb_row =
+      (xd->mi_row - tile_info->mi_row_start) >> cm->seq_params->mib_size_log2;
+  const int sb_cols_in_tile = av1_get_sb_cols_in_tile(cm, tile_info);
+
+  // In case of row-multithreading, due to top-right dependency, the worker on
+  // an SB row waits for the completion of the encode of the top and top-right
+  // SBs. Hence, in case a thread (main/worker) encounters an error, update that
+  // its encoding is complete before early exit in order to avoid the dependent
+  // workers from waiting indefinitely.
+  enc_row_mt->sync_write_ptr(row_mt_sync, sb_row, sb_cols_in_tile - 1,
+                             sb_cols_in_tile);
+}
+
+static AOM_INLINE void set_loop_filter_dependency_as_resolved(AV1_COMP *cpi) {
+  AV1_COMMON *const cm = &cpi->common;
+  AV1EncRowMultiThreadInfo *const enc_row_mt = &cpi->mt_info.enc_row_mt;
+  const int sb_rows_in_frame = get_sb_rows_in_frame(cm);
+
+  // In case of error, set enc_row_mt->num_tile_cols_done to maximum tile
+  // columns (cm->tiles.cols) to ensure that other workers do not wait
+  // indefinitely in launch_loop_filter_rows().
+  memset(enc_row_mt->num_tile_cols_done, cm->tiles.cols,
+         sizeof(*enc_row_mt->num_tile_cols_done) * sb_rows_in_frame);
+
+#if CONFIG_MULTITHREAD
+  pthread_cond_broadcast(enc_row_mt->cond_);
+#endif
+}
+
 static int enc_row_mt_worker_hook(void *arg1, void *unused) {
   EncWorkerData *const thread_data = (EncWorkerData *)arg1;
   AV1_COMP *const cpi = thread_data->cpi;
   AV1_COMMON *const cm = &cpi->common;
   int thread_id = thread_data->thread_id;
   AV1EncRowMultiThreadInfo *const enc_row_mt = &cpi->mt_info.enc_row_mt;
-  int cur_tile_id = enc_row_mt->thread_id_to_tile_id[thread_id];
   const int mib_size_log2 = cm->seq_params->mib_size_log2;
 #if CONFIG_MULTITHREAD
   pthread_mutex_t *enc_row_mt_mutex_ = enc_row_mt->mutex_;
 #endif
   (void)unused;
+
+  struct aom_internal_error_info *const error_info = &thread_data->error_info;
+  MACROBLOCKD *const xd = &thread_data->td->mb.e_mbd;
+  xd->error_info = error_info;
+
+  // The jmp_buf is valid only for the duration of the function that calls
+  // setjmp(). Therefore, this function must reset the 'setjmp' field to 0
+  // before it returns.
+  if (setjmp(error_info->jmp)) {
+    error_info->setjmp = 0;
+#if CONFIG_MULTITHREAD
+    pthread_mutex_lock(enc_row_mt_mutex_);
+    enc_row_mt->row_mt_exit = 1;
+    set_loop_filter_dependency_as_resolved(cpi);
+    pthread_mutex_unlock(enc_row_mt_mutex_);
+#endif
+    av1_set_encoding_done_for_row(cpi, xd);
+    return 0;
+  }
+  error_info->setjmp = 1;
+
+  int cur_tile_id = enc_row_mt->thread_id_to_tile_id[thread_id];
+
   // Preallocate the pc_tree for realtime coding to reduce the cost of memory
   // allocation.
   thread_data->td->rt_pc_root =
@@ -505,7 +569,7 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
   assert(cur_tile_id != -1);
 
   const BLOCK_SIZE fp_block_size = cpi->fp_block_size;
-  int end_of_frame = 0;
+  int end_of_frame = 0, row_mt_exit = 0;
 
   // When master thread does not have a valid job to process, xd->tile_ctx
   // is not set and it contains NULL pointer. This can result in NULL pointer
@@ -518,7 +582,9 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
 #if CONFIG_MULTITHREAD
     pthread_mutex_lock(enc_row_mt_mutex_);
 #endif
-    if (!get_next_job(&cpi->tile_data[cur_tile_id], &current_mi_row,
+    row_mt_exit = enc_row_mt->row_mt_exit;
+    if (!row_mt_exit &&
+        !get_next_job(&cpi->tile_data[cur_tile_id], &current_mi_row,
                       cm->seq_params->mib_size)) {
       // No jobs are available for the current tile. Query for the status of
       // other tiles and get the next job if available
@@ -529,7 +595,11 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
 #if CONFIG_MULTITHREAD
     pthread_mutex_unlock(enc_row_mt_mutex_);
 #endif
-    if (end_of_frame == 1) break;
+    // When row_mt_exit is set to 1, other workers need not pursue any further
+    // jobs.
+    if (row_mt_exit) return 1;
+
+    if (end_of_frame) break;
 
     TileDataEnc *const this_tile = &cpi->tile_data[cur_tile_id];
     AV1EncRowMultiThreadSync *const row_mt_sync = &this_tile->row_mt_sync;
@@ -538,6 +608,11 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
     const int tile_col = tile_info->tile_col;
     ThreadData *td = thread_data->td;
     const int sb_row = current_mi_row >> mib_size_log2;
+
+    // Required in av1_set_encoding_done_for_row() to derive 'tile_data' and
+    // 'sb_row'.
+    xd->mi_row = current_mi_row;
+    xd->tile = *tile_info;
 
     assert(current_mi_row != -1 && current_mi_row <= tile_info->mi_row_end);
 
@@ -585,6 +660,7 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
   }
   av1_free_pc_tree_recursive(thread_data->td->rt_pc_root, av1_num_planes(cm), 0,
                              0, cpi->sf.part_sf.partition_search_type);
+  error_info->setjmp = 0;
   return 1;
 }
 
