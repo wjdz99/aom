313ea4dd1ab01f1415352181b9cbf73dee11c1ad
Konstantinos Margaritis <konma@vectorcamp.gr>
2023-06-19 15:34:27 +0000

    [NEON] Optimize aom_highbd_dist_wtd_comp_avg_upsampled_pred_neon
    Passes the unit and argon tests, profiling results pending, need
    to figure out the cmd line options to enable it.
    
    Change-Id: Ibc87bec7d4b0cfd8f712f628bb69609a7e1c7983

--- test/comp_avg_pred_test.cc
+++ test/comp_avg_pred_test.cc
@@ -67,6 +67,12 @@ INSTANTIATE_TEST_SUITE_P(SSE2, AV1HighBDDISTWTDCOMPAVGTest,
                              aom_highbd_dist_wtd_comp_avg_pred_sse2, 1));
 #endif
 
+#if HAVE_NEON
+INSTANTIATE_TEST_SUITE_P(NEON, AV1HighBDDISTWTDCOMPAVGTest,
+                         libaom_test::AV1DISTWTDCOMPAVG::BuildParams(
+                             aom_highbd_dist_wtd_comp_avg_pred_neon, 1));
+#endif
+
 TEST_P(AV1HighBDDISTWTDCOMPAVGUPSAMPLEDTest, DISABLED_Speed) {
   RunSpeedTest(GET_PARAM(1));
 }
@@ -80,6 +86,13 @@ INSTANTIATE_TEST_SUITE_P(SSE2, AV1HighBDDISTWTDCOMPAVGUPSAMPLEDTest,
                          libaom_test::AV1DISTWTDCOMPAVG::BuildParams(
                              aom_highbd_dist_wtd_comp_avg_upsampled_pred_sse2));
 #endif
+
+#if HAVE_NEON
+INSTANTIATE_TEST_SUITE_P(NEON, AV1HighBDDISTWTDCOMPAVGUPSAMPLEDTest,
+                         libaom_test::AV1DISTWTDCOMPAVG::BuildParams(
+                             aom_highbd_dist_wtd_comp_avg_upsampled_pred_neon));
+#endif
+
 #endif  // CONFIG_AV1_HIGHBITDEPTH
 
 }  // namespace
