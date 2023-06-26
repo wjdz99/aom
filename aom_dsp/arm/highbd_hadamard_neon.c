66e9b8d016ccb0198f0f6cfe6c3742c88ebc36a2
Wan-Teh Chang <wtc@google.com>
2023-06-26 16:08:12 -0700

    Alt fix for Hadamard 32x32 test failures for Neon
    When I wrote the previous fix, I didn't know vhaddq_s16() and
    vhsubq_s16() have 17-bit intermediate precision. The alternative fix for
    aom_hadamard_32x32_neon() avoids going to int32x4_t.
    
    Bug: aomedia:3455
    Change-Id: I676887db8df1bf0b769747b07e7501f0e964937e

--- aom_dsp/arm/highbd_hadamard_neon.c
+++ aom_dsp/arm/highbd_hadamard_neon.c
@@ -195,10 +195,10 @@ void aom_highbd_hadamard_32x32_neon(const int16_t *src_diff,
     int32x4_t a2 = vld1q_s32(coeff + 4 * i + 512);
     int32x4_t a3 = vld1q_s32(coeff + 4 * i + 768);
 
-    int32x4_t b0 = vshrq_n_s32(vaddq_s32(a0, a1), 2);
-    int32x4_t b1 = vshrq_n_s32(vsubq_s32(a0, a1), 2);
-    int32x4_t b2 = vshrq_n_s32(vaddq_s32(a2, a3), 2);
-    int32x4_t b3 = vshrq_n_s32(vsubq_s32(a2, a3), 2);
+    int32x4_t b0 = vshrq_n_s32(vhaddq_s32(a0, a1), 1);
+    int32x4_t b1 = vshrq_n_s32(vhsubq_s32(a0, a1), 1);
+    int32x4_t b2 = vshrq_n_s32(vhaddq_s32(a2, a3), 1);
+    int32x4_t b3 = vshrq_n_s32(vhsubq_s32(a2, a3), 1);
 
     int32x4_t c0 = vaddq_s32(b0, b2);
     int32x4_t c1 = vaddq_s32(b1, b3);
