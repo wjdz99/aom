b8a09727f1dfeb81557d9723d98ec100f44e662d
Marco Paniconi <marpan@google.com>
2023-08-03 16:21:09 -0700

    rtc: Fix to color artifacts for screen
    Move up source_variance computation to
    superblock level, and use it to condition
    thresholds for setting color_sensitivity,
    on both superblock and coding block level.
    
    This significantly reduces color artifacts
    for screen content mode.
    
    Change-Id: Iee0d8d8c7f41710d9d02b0ef8640fe907c9de701

--- av1/encoder/partition_search.c
+++ av1/encoder/partition_search.c
@@ -2265,8 +2265,9 @@ static void pick_sb_modes_nonrd(AV1_COMP *const cpi, TileDataEnc *tile_data,
       get_force_zeromv_skip_flag_for_blk(cpi, x, bsize);
 
   if (!x->force_zeromv_skip_for_blk) {
-    x->source_variance = av1_get_perpixel_variance_facade(
-        cpi, xd, &x->plane[0].src, bsize, AOM_PLANE_Y);
+    if (x->source_variance == UINT_MAX || bsize < cm->seq_params->sb_size)
+      x->source_variance = av1_get_perpixel_variance_facade(
+         cpi, xd, &x->plane[0].src, bsize, AOM_PLANE_Y);
   }
   // Save rdmult before it might be changed, so it can be restored later.
   const int orig_rdmult = x->rdmult;
