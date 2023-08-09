4bd741e59521bd3baf848817f16356b126cbb0ff
Marco Paniconi <marpan@google.com>
2023-08-09 14:36:15 -0700

    rtc: Enable superblock ME in partitioning for screen
    For real-time screen mode, speeds >= 7:
    Enable superblock ME (estimate_motion_for_var_based_partition)
    for screen, and only allow it for superblock with motion and
    spatial variance above some threshold.
    
    Use it to detect motion, whose SAD is small (much less than zeromv).
    This could capture coherent more like scrolling. The motion
    vector extracted here is then used in the nonrd_pickmode.
    
    For superblocks where motion estimation is much better
    than zero-motion, coding block size will be larger and pickmode
    will always test the motion vector for superblock ME,
    this can lead to better quality for scroll with less bits.
    
    Also allow for larger search window in the int_pro_motion(),
    for superblocks with high motion.
    
    The feature sf->estimate_motion_for_var_based_partition
    was disabled for screen here:
    https://aomedia-review.googlesource.com/c/aom/+/178981
    This CL is an attempt to make it useful/work for screen.
    
    WIP
    
    Change-Id: I210f86712f8c6f958d5498b19afb97ffa9be8654

--- av1/encoder/mcomp.h
+++ av1/encoder/mcomp.h
@@ -270,7 +270,8 @@ int av1_init_search_range(int size);
 unsigned int av1_int_pro_motion_estimation(const struct AV1_COMP *cpi,
                                            MACROBLOCK *x, BLOCK_SIZE bsize,
                                            int mi_row, int mi_col,
-                                           const MV *ref_mv);
+                                           const MV *ref_mv,
+                                           unsigned int *y_sad_zero, int scale);
 
 int av1_refining_search_8p_c(const FULLPEL_MOTION_SEARCH_PARAMS *ms_params,
                              const FULLPEL_MV start_mv, FULLPEL_MV *best_mv);
