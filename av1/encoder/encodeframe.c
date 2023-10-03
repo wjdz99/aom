938caa66fbb363dcd430726ecb3c88688c1cdf29
Marco Paniconi <marpan@google.com>
2023-10-03 14:51:56 -0700

    rtc: Speed 11 for video mode, for resoln < 480p.
    -force single reference
    -aggressive cdef skip
    -selective cdf_update
    -turn off motion interpolation filter search
    -use only DC for intra-modes
    -faster/fixed partitioning
    -adjust thresholds for source_sad
    
    WIP
    
    bdrate and IC change from speed 10 rtc_derf:
    
    Change-Id: Ia14d35d6d6ba81532e904c136a7ca388ed4674a4

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -538,8 +538,12 @@ static AOM_INLINE void encode_nonrd_sb(AV1_COMP *cpi, ThreadData *td,
   if (sf->part_sf.partition_search_type == FIXED_PARTITION || seg_skip) {
     // set a fixed-size partition
     av1_set_offsets(cpi, tile_info, x, mi_row, mi_col, sb_size);
-    const BLOCK_SIZE bsize =
-        seg_skip ? sb_size : sf->part_sf.fixed_partition_size;
+    BLOCK_SIZE bsize_select = sf->part_sf.fixed_partition_size;
+    if (sf->rt_sf.use_fast_fixed_part) {
+      if (x->content_state_sb.source_sad_nonrd > kMedSad)
+        bsize_select = BLOCK_16X16;
+    }
+    const BLOCK_SIZE bsize = seg_skip ? sb_size : bsize_select;
     av1_set_fixed_partitioning(cpi, tile_info, mi, mi_row, mi_col, bsize);
   } else if (sf->part_sf.partition_search_type == VAR_BASED_PARTITION) {
     // set a variance-based partition
