a0bdd0a2dd615c968a7e4edcbc486aae8ed6a762
Satheesh Kumar <satheesh.kumar@ittiam.com>
2023-11-03 16:15:56 +0530

    Handle allocation failure in sgr SIMD functions
    In this CL, thread-specific 'error_info' is passed to the low level
    functions of selfguided restoration module to service the  allocation
    failure of local buffer in av1_selfguided_restoration_avx2()
    and av1_selfguided_restoration_sse4_1().
    
    Bug: aomedia:3276
    Change-Id: I37dabdc2f0181ab5de0c5075c0f9791b15bb1c8e

--- av1/common/restoration.h
+++ av1/common/restoration.h
@@ -318,7 +318,8 @@ typedef struct {
 typedef void (*rest_unit_visitor_t)(const RestorationTileLimits *limits,
                                     int rest_unit_idx, void *priv,
                                     int32_t *tmpbuf,
-                                    RestorationLineBuffers *rlbs);
+                                    RestorationLineBuffers *rlbs,
+                                    struct aom_internal_error_info *error_info);
 
 typedef struct FilterFrameCtxt {
   const RestorationInfo *rsi;
@@ -378,18 +379,17 @@ void av1_decode_xq(const int *xqd, int *xq, const sgr_params_type *params);
  * \param[in]  tmpbuf        Scratch buffer used by the sgrproj filter which
  *                           should be at least SGRPROJ_TMPBUF_SIZE big.
  * \param[in]  optimized_lr  Whether to use fast optimized Loop Restoration
+ * \param[in]  error_info    Error info for tracking errors
  *
  * \remark Nothing is returned. Instead, the filtered unit is output in
  * \c dst8 at the proper restoration unit offset.
  */
-void av1_loop_restoration_filter_unit(const RestorationTileLimits *limits,
-                                      const RestorationUnitInfo *rui,
-                                      const RestorationStripeBoundaries *rsb,
-                                      RestorationLineBuffers *rlbs, int plane_w,
-                                      int plane_h, int ss_x, int ss_y,
-                                      int highbd, int bit_depth, uint8_t *data8,
-                                      int stride, uint8_t *dst8, int dst_stride,
-                                      int32_t *tmpbuf, int optimized_lr);
+void av1_loop_restoration_filter_unit(
+    const RestorationTileLimits *limits, const RestorationUnitInfo *rui,
+    const RestorationStripeBoundaries *rsb, RestorationLineBuffers *rlbs,
+    int plane_w, int plane_h, int ss_x, int ss_y, int highbd, int bit_depth,
+    uint8_t *data8, int stride, uint8_t *dst8, int dst_stride, int32_t *tmpbuf,
+    int optimized_lr, struct aom_internal_error_info *error_info);
 
 /*!\brief Function for applying loop restoration filter to a frame
  *
