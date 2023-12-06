a7abca0c6a5a2000f6214f2bccd9b1b34b2b918d
Marco Paniconi <marpan@google.com>
2023-12-06 14:55:30 -0800

    rtc: Enable src_sad_blk_64x64 for superres mode
    Change-Id: I07d0bc66b31f5f012163833f4f65c889e03d5f2e

--- av1/encoder/ratectrl.c
+++ av1/encoder/ratectrl.c
@@ -3038,7 +3038,8 @@ static void rc_scene_detection_onepass_rt(AV1_COMP *cpi,
   const int border =
       rc->prev_frame_is_dropped || cpi->svc.number_temporal_layers > 1;
   // Store blkwise SAD for later use
-  if (width == cm->render_width && height == cm->render_height) {
+  if (cpi->oxcf.superres_cfg.enable_superres ||
+      (width == cm->render_width && height == cm->render_height)) {
     if (cpi->src_sad_blk_64x64 == NULL) {
       CHECK_MEM_ERROR(cm, cpi->src_sad_blk_64x64,
                       (uint64_t *)aom_calloc(sb_cols * sb_rows,
