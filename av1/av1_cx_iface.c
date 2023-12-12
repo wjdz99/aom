6931ed84a28178835d454c714d37fdb7ec41e958
Cheng Chen <chengchen@google.com>
2023-12-12 11:45:43 -0800

    Set initial_dimensions in encoder init
    Set initial_dimensions in encoder initialization, so that
    initial_dimensions are not 0, and some conditions related to
    frame size and initial_dimensions comparisons in encoder config
    are not triggered.
    
    This avoids unnecessary memory allocations during encode config
    calls.
    
    A new variable cpi->frame_size_related_setup_done is added to
    track whether frame size related setup and allocations have been
    done, which is necessary since initial_dimensions are no longer able
    to achieve this functionality after change.
    
    Bug=aomedia:3526
    
    Change-Id: I7b2d7aa6da4e616bd5999c8089e86a445b4ac73c

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -1521,11 +1521,12 @@ static aom_codec_err_t encoder_set_config(aom_codec_alg_priv_t *ctx,
          !valid_ref_frame_size(ctx->ppi->cpi->last_coded_width,
                                ctx->ppi->cpi->last_coded_height, cfg->g_w,
                                cfg->g_h)) ||
-        (initial_dimensions->width &&
-         (int)cfg->g_w > initial_dimensions->width) ||
-        (initial_dimensions->height &&
-         (int)cfg->g_h > initial_dimensions->height))
+        ((int)cfg->g_w > initial_dimensions->width) ||
+        ((int)cfg->g_h > initial_dimensions->height)) {
+      // TODO(chengchen): analyze and see if initial_dimensions could be
+      // replaced by last_coded_width/height or removed.
       force_key = 1;
+    }
   }
 
   if (ctx->monochrome_on_init && cfg->monochrome == 0) {
