a8f7919365164528af375f2b51e5d42a89b30586
Marco Paniconi <marpan@google.com>
2024-02-02 17:37:07 +0000

    rtc: Fixes for active_maps for rtc encoder
    Add example to test active maps in the sample
    encoder: svc_encoder_rtc.c.
    
    Make some changes inside encoder to make active_maps
    work for rtc mode:
    1) allowing for cyclic refresh to stay enabled on the
       active segment.
    2) for blocks with segment_skip set: set the flag
       x->force_zeromv_skip_blk = 1 and enter av1_nonrd_pick_inter_mode().
       Avoid the call to av1_rd_pick_inter_mode_sb_seg_skip(), which
       is defined for rd pickmode and needs to be modified for nonrd.
       This reduces bad artifacts when using active_contours for
       speeds >= 7.
    3) Fix to reset segment on skip for active_maps.
    
    Extend ActiveMapTest to speed up to 11, and for AQ_MODE=3 (cyclic_refresh)
    .
    Change-Id: I6bfe26e14526b6799ab1f138fb347ca63a0d2ae1

--- av1/encoder/aq_cyclicrefresh.c
+++ av1/encoder/aq_cyclicrefresh.c
@@ -425,7 +425,7 @@ void av1_cyclic_refresh_update_parameters(AV1_COMP *const cpi) {
   // we use it only for speed > 9 for now.
   // Also if loop-filter deltas is applied via segment, then
   // we need to set cr->skip_over4x4 = 1.
-  cr->skip_over4x4 = (cpi->oxcf.speed > 9) ? 1 : 0;
+  cr->skip_over4x4 = (cpi->oxcf.speed > 9 && !cpi->active_map.enabled) ? 1 : 0;
 
   // should we enable cyclic refresh on this frame.
   cr->apply_cyclic_refresh = 1;
