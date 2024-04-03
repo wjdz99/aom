a87d38521d6e29c7251bfe27f97f8f992fa6410c
Marco Paniconi <marpan@google.com>
2024-04-03 14:41:12 -0700

    rtc: Increase loopfilter strength for speed 11
    This is for speed 11 video mode, vga, where
    the speed feature rt_sf->use_fast_fixed_part is used.
    Increase the loopfilter strength to reduce artifacts.
    
    This CL also fixes an issue with spatial layers
    on key frames for the rtc_sf->use_fast_fixed_part feature:
    avoid entering the fixed partition for layers whose base
    is key frame.
    
    Stats change for speed 11 rtc (480/360p only):
    avg/ovr/ssim, IC speedup
    -2.98/-2.98/-2.57, -0.325
    
    Change-Id: I59bb2594168e93687e80a2b60b4848c105fa066e

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -537,7 +537,9 @@ static AOM_INLINE void encode_nonrd_sb(AV1_COMP *cpi, ThreadData *td,
   // Set the partition
   if (sf->part_sf.partition_search_type == FIXED_PARTITION || seg_skip ||
       (sf->rt_sf.use_fast_fixed_part && x->sb_force_fixed_part == 1 &&
-       !frame_is_intra_only(cm))) {
+       (!frame_is_intra_only(cm) &&
+        (!cpi->ppi->use_svc ||
+         !cpi->svc.layer_context[cpi->svc.temporal_layer_id].is_key_frame)))) {
     // set a fixed-size partition
     av1_set_offsets(cpi, tile_info, x, mi_row, mi_col, sb_size);
     BLOCK_SIZE bsize_select = sf->part_sf.fixed_partition_size;
@@ -1136,6 +1138,7 @@ static AOM_INLINE void encode_sb_row(AV1_COMP *cpi, ThreadData *td,
   const int mib_size_log2 = cm->seq_params->mib_size_log2;
   const int sb_row = (mi_row - tile_info->mi_row_start) >> mib_size_log2;
   const int use_nonrd_mode = cpi->sf.rt_sf.use_nonrd_pick_mode;
+  uint8_t segment_id = 0;
 
 #if CONFIG_COLLECT_COMPONENT_TIMING
   start_timing(cpi, encode_sb_row_time);
@@ -1224,7 +1227,7 @@ static AOM_INLINE void encode_sb_row(AV1_COMP *cpi, ThreadData *td,
     if (seg->enabled) {
       const uint8_t *const map =
           seg->update_map ? cpi->enc_seg.map : cm->last_frame_seg_map;
-      const uint8_t segment_id =
+      segment_id =
           map ? get_segment_id(&cm->mi_params, map, sb_size, mi_row, mi_col)
               : 0;
       seg_skip = segfeature_active(seg, segment_id, SEG_LVL_SKIP);
