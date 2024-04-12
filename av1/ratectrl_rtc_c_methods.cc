48847ca0766d1b7d0bf85097a49b5e64d80f95fc
Pradeep Kumar <pradeep.kumar@intel.corp-partner.google.com>
2024-04-12 14:42:37 +0530

    Adding extern 'c' methods for AV1 ratecontrol
    This modification helps to use rate control APIs for AV1 with dlopen()
    related APIs.
    
    Change-Id: Ife9caf445541b2a1bae108e294392b45f32f0b08

---
+++ av1/ratectrl_rtc_c_methods.cc
@@ -0,0 +1,54 @@
+#include "av1/ratectrl_rtc.h"
+namespace aom {
+extern "C"
+{
+
+    void * createAV1Controller(const AV1RateControlRtcConfig &rc_cfg)
+    {
+        std::unique_ptr<AV1RateControlRTC> controller;
+        controller = AV1RateControlRTC::Create(rc_cfg);
+        return controller.release();
+    }
+
+    bool UpdateRateControl_AV1(void *controller, const AV1RateControlRtcConfig &rc_cfg)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->UpdateRateControl(rc_cfg);
+    }
+
+    int GetQP_AV1(void *controller)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->GetQP();
+    }
+
+    AV1LoopfilterLevel GetLoopfilterLevel_AV1(void *controller)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->GetLoopfilterLevel();
+    }
+
+    FrameDropDecision ComputeQP_AV1(void *controller, const AV1FrameParamsRTC &frame_params)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->ComputeQP(frame_params);
+    }
+
+    void PostEncodeUpdate_AV1(void *controller, uint64_t encoded_frame_size)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->PostEncodeUpdate(encoded_frame_size);
+    }
+
+    bool GetSegmentationData_AV1(void *controller, AV1SegmentationData *segmentation_data)
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->GetSegmentationData(segmentation_data);
+    }
+
+    AV1CdefInfo GetCdefInfo_AV1(void *controller) 
+    {
+        return static_cast<AV1RateControlRTC *>(controller )->GetCdefInfo();
+    }
+
+    AV1RateControlRtcConfig* create_av1_rate_control_config() 
+    {
+        return new AV1RateControlRtcConfig();
+    }
+
+}//extern c 
+}  // namespace aom
