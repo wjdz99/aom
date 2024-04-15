d31777d4d1db5feb9ec350940c8b22586230e30b
Pradeep Kumar <pradeep.kumar@intel.corp-partner.google.com>
2024-04-15 16:12:51 +0530

    Adding extern 'c' methods for AV1 ratecontrol
    This changes allow the rate control methods present in
    class -AV1RateControlRTC to get exposed using extern C
    methods. This helps to dynamic loading of lib aom_av1_rc
    using dlopen()
    
    Change-Id: Ife9caf445541b2a1bae108e294392b45f32f0b08
    Signed-off-by: Pradeep Kumar <pradeep.kumar@intel.corp-partner.google.com>

--- av1/ratectrl_rtc_c_methods.cc
+++ av1/ratectrl_rtc_c_methods.cc
@@ -1,54 +1,50 @@
 #include "av1/ratectrl_rtc.h"
 namespace aom {
-extern "C"
-{
+extern "C" {
 
-    void * createAV1Controller(const AV1RateControlRtcConfig &rc_cfg)
-    {
-        std::unique_ptr<AV1RateControlRTC> controller;
-        controller = AV1RateControlRTC::Create(rc_cfg);
-        return controller.release();
-    }
+void *createAV1Controller(const AV1RateControlRtcConfig &rc_cfg) {
+  std::unique_ptr<AV1RateControlRTC> controller;
+  controller = AV1RateControlRTC::Create(rc_cfg);
+  return controller.release();
+}
 
-    bool UpdateRateControl_AV1(void *controller, const AV1RateControlRtcConfig &rc_cfg)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->UpdateRateControl(rc_cfg);
-    }
+bool UpdateRateControl_AV1(void *controller,
+                           const AV1RateControlRtcConfig &rc_cfg) {
+  return static_cast<AV1RateControlRTC *>(controller)
+      ->UpdateRateControl(rc_cfg);
+}
 
-    int GetQP_AV1(void *controller)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->GetQP();
-    }
+int GetQP_AV1(void *controller) {
+  return static_cast<AV1RateControlRTC *>(controller)->GetQP();
+}
 
-    AV1LoopfilterLevel GetLoopfilterLevel_AV1(void *controller)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->GetLoopfilterLevel();
-    }
+AV1LoopfilterLevel GetLoopfilterLevel_AV1(void *controller) {
+  return static_cast<AV1RateControlRTC *>(controller)->GetLoopfilterLevel();
+}
 
-    FrameDropDecision ComputeQP_AV1(void *controller, const AV1FrameParamsRTC &frame_params)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->ComputeQP(frame_params);
-    }
+FrameDropDecision ComputeQP_AV1(void *controller,
+                                const AV1FrameParamsRTC &frame_params) {
+  return static_cast<AV1RateControlRTC *>(controller)->ComputeQP(frame_params);
+}
 
-    void PostEncodeUpdate_AV1(void *controller, uint64_t encoded_frame_size)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->PostEncodeUpdate(encoded_frame_size);
-    }
+void PostEncodeUpdate_AV1(void *controller, uint64_t encoded_frame_size) {
+  return static_cast<AV1RateControlRTC *>(controller)
+      ->PostEncodeUpdate(encoded_frame_size);
+}
 
-    bool GetSegmentationData_AV1(void *controller, AV1SegmentationData *segmentation_data)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->GetSegmentationData(segmentation_data);
-    }
+bool GetSegmentationData_AV1(void *controller,
+                             AV1SegmentationData *segmentation_data) {
+  return static_cast<AV1RateControlRTC *>(controller)
+      ->GetSegmentationData(segmentation_data);
+}
 
-    AV1CdefInfo GetCdefInfo_AV1(void *controller)
-    {
-        return static_cast<AV1RateControlRTC *>(controller )->GetCdefInfo();
-    }
+AV1CdefInfo GetCdefInfo_AV1(void *controller) {
+  return static_cast<AV1RateControlRTC *>(controller)->GetCdefInfo();
+}
 
-    AV1RateControlRtcConfig* create_av1_rate_control_config()
-    {
-        return new AV1RateControlRtcConfig();
-    }
+AV1RateControlRtcConfig *create_av1_rate_control_config() {
+  return new AV1RateControlRtcConfig();
+}
 
-}//extern c
+}  // extern c
 }  // namespace aom
