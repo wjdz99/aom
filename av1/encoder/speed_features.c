24ae605894a9eb22abf88796c27625765f227dd4
Marco Paniconi <marpan@google.com>
2024-06-04 14:45:27 -0700

    rtc: Speedup for dynamic screen content
    For rtc screen mode, speed >= 10:
    When the content has high motion (detected from
    soure_sad from scene detection): adjust some speed
    features and increase the base partition threshold to
    make the encoder faster.
    
    This has negligible effect on rtc_screen set, but
    for very high motion screen content it reduces
    instruction count by ~40%.
    
    Change-Id: I68071da2b40731cc81ac15a8010976e6ef234776

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1610,10 +1610,14 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
           sf->rt_sf.intra_y_mode_bsize_mask_nrd[i] = INTRA_DC_H_V;
       }
     }
-    if (cpi->rc.max_block_source_sad > 20000 &&
-        cpi->rc.frame_source_sad > 100 && speed >= 6 &&
-        (cpi->rc.percent_blocks_with_motion > 1 ||
-         cpi->svc.last_layer_dropped[0])) {
+    if (speed >= 10 && cpi->rc.high_motion_content_screen) {
+      sf->rt_sf.higher_thresh_scene_detection = 1;
+      sf->rt_sf.force_only_last_ref = 1;
+      sf->rt_sf.use_nonrd_filter_search = 0;
+    } else if (cpi->rc.max_block_source_sad > 20000 &&
+               cpi->rc.frame_source_sad > 100 && speed >= 6 &&
+               (cpi->rc.percent_blocks_with_motion > 1 ||
+                cpi->svc.last_layer_dropped[0])) {
       sf->mv_sf.search_method = NSTEP;
       sf->rt_sf.fullpel_search_step_param = 2;
     }
