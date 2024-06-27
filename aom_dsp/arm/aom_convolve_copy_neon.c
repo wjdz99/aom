475a6283f827fcab1db49ebd1f092e66e1d18e00
Wan-Teh Chang <wtc@google.com>
2024-06-27 10:28:24 -0700

    Handle w=2 case in aom_highbd_convolve_copy_neon()
    Fix the incorrect assumption that if w < 8, then w == 4. w may be equal
    to 2.
    
    Tested:
    cmake ../aom -G Ninja -DFORCE_HIGHBITDEPTH_DECODING=1
    ninja
    ./test_libaom --gtest_filter=*TestVectorTest*
    
    cmake ../aom -G Ninja -DSANITIZE=address
    ninja
    ./test_libaom --gtest_filter=*ConvolveCopy*
    
    Bug: 349832592
    Change-Id: I2600d00b097a94a079c4827cdc894d02cf03c42e

--- aom_dsp/arm/aom_convolve_copy_neon.c
+++ aom_dsp/arm/aom_convolve_copy_neon.c
@@ -57,7 +57,20 @@ void aom_convolve_copy_neon(const uint8_t *src, ptrdiff_t src_stride,
 void aom_highbd_convolve_copy_neon(const uint16_t *src, ptrdiff_t src_stride,
                                    uint16_t *dst, ptrdiff_t dst_stride, int w,
                                    int h) {
-  if (w < 8) {  // copy4
+  if (w < 4) {  // copy2
+    do {
+      dst[0] = src[0];
+      dst[1] = src[1];
+      src += src_stride;
+      dst += dst_stride;
+
+      dst[0] = src[0];
+      dst[1] = src[1];
+      src += src_stride;
+      dst += dst_stride;
+      h -= 2;
+    } while (h != 0);
+  } else if (w == 4) {  // copy4
     uint16x4_t s0, s1;
     do {
       s0 = vld1_u16(src);
