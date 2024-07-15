87caf7e40c1efe4d5cc57f4bd0b4d5792d0f70b2
Marco Paniconi <marpan@google.com>
2024-07-15 15:48:03 -0700

    rtc: Adjust QP for keyframes based on source content
    For keyframes in rtc: compute some spatial metrics
    and use it, along with the last keyframe
    encoded/target_size, to adjust the QP.
    
    Change-Id: I0316d9303cb0ada4ed75948bbfc8196218870514

--- av1/encoder/speed_features.h
+++ av1/encoder/speed_features.h
@@ -1648,6 +1648,9 @@ typedef struct REAL_TIME_SPEED_FEATURES {
   // Check for scene/content change detection on every frame before encoding.
   int check_scene_detection;
 
+  // For keyframes in rtc: adjust the RC based on content.
+  int rc_adjust_keyframe;
+
   // For nonrd mode: Prefer larger partition blks in variance based partitioning
   // 0: disabled, 1-3: increasing aggressiveness
   int prefer_large_partition_blocks;
