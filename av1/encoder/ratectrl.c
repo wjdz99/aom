ae91c840d73c2acd80807a182e927cfa429e7009
Marco Paniconi <marpan@google.com>
2024-08-06 11:09:49 -0700

    rtc: Convert the input max_consec_drop to time units
    This is more natural unit to use for rtc applications.
    At the start of av1_get_one_pass_rt_params() the time units is
    converted to frame number (via framerate), which is the unit used
    internally in the frame dropper.
    
    Change-Id: I27f5573e6bcb3831f9159798919eab3cc9ab6991

--- av1/encoder/ratectrl.c
+++ av1/encoder/ratectrl.c
@@ -3680,6 +3680,8 @@ void av1_get_one_pass_rt_params(AV1_COMP *cpi, FRAME_TYPE *const frame_type,
   const int layer =
       LAYER_IDS_TO_IDX(svc->spatial_layer_id, svc->temporal_layer_id,
                        svc->number_temporal_layers);
+  rc->max_consec_drop =
+      (int)round(rc->max_consec_drop_time * cpi->framerate / 1000);
   if (cpi->ppi->use_svc) {
     av1_update_temporal_layer_framerate(cpi);
     av1_restore_layer_context(cpi);
