63e75e2e1a1c46d07f680cbe934e010018055750
Salome Thirot <salome.thirot@arm.com>
2024-08-15 18:17:37 +0100

    Optimize Neon implementation of av1_compute_stats_highbd
    This new version is a port of the SVT-AV1 implementation and is
    considerably (~10x) faster, as it reuses most of the computation instead
    of manually calculating every individual element of matrix H.
    
    Delete the SVE version as well, as the new Neon implementation is now 3x
    to 6x faster.
    
    Change-Id: I5b866cc8ed869aefa5c0331352316b86f0c65c34

--- aom_dsp/arm/transpose_neon.h
+++ aom_dsp/arm/transpose_neon.h
@@ -1330,4 +1330,15 @@ static inline void transpose_arrays_s16_8x4(const int16x8_t *const in,
   out[7] = vget_high_s16(vreinterpretq_s16_u32(c1.val[1]));
 }
 
+static inline void transpose_s64_4x4_neon(const int64x2_t *in, int64x2_t *out) {
+  out[0] = vtrn1q_s64(in[0], in[2]);
+  out[4] = vtrn1q_s64(in[1], in[3]);
+  out[1] = vtrn1q_s64(in[4], in[6]);
+  out[5] = vtrn1q_s64(in[5], in[7]);
+  out[2] = vtrn2q_s64(in[0], in[2]);
+  out[6] = vtrn2q_s64(in[1], in[3]);
+  out[3] = vtrn2q_s64(in[4], in[6]);
+  out[7] = vtrn2q_s64(in[5], in[7]);
+}
+
 #endif  // AOM_AOM_DSP_ARM_TRANSPOSE_NEON_H_
