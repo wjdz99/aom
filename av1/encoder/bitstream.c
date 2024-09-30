712f09bb38af9f57bd80ba1e12b0fd28de4be5f6
Marco Paniconi <marpan@google.com>
2024-09-30 15:28:02 -0700

    rtc-svc: Fix to reset ref_idx for svc
    The rtc_ref->ref_idx[] is mapped to the
    remapped_idx[] at the start of encoding, so on
    reset of ref_idx in we should also reset the
    remapped_ref_idx, as this is used further below
    in the function write_frame_size_with_refs(line ~3151).
    
    The previous fix of constraining the reset
    to single reference is not quite correct and
    can still fail.
    
    Added two test cases that trigger the failure.
    Bug: 369633254
    Change-Id: I86490e3f96ffcea7fb6e972a7d2085675c65fcc4
    
    Change-Id: Ifcc48edff86a2eca6253ea00c81f8901c8ff0631

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -3102,7 +3102,6 @@ static inline void write_uncompressed_header_obu(
         aom_wb_write_literal(wb, gld_ref, REF_FRAMES_LOG2);
       }
       int first_ref_map_idx = INVALID_IDX;
-      int num_references = 0;
       if (cpi->ppi->rtc_ref.set_ref_frame_config) {
         for (ref_frame = LAST_FRAME; ref_frame <= ALTREF_FRAME; ++ref_frame) {
           if (cpi->ppi->rtc_ref.reference[ref_frame - 1] == 1) {
@@ -3110,15 +3109,12 @@ static inline void write_uncompressed_header_obu(
             break;
           }
         }
-        for (ref_frame = LAST_FRAME; ref_frame <= ALTREF_FRAME; ++ref_frame) {
-          if (cpi->ppi->rtc_ref.reference[ref_frame - 1] == 1) num_references++;
-        }
       }
       for (ref_frame = LAST_FRAME; ref_frame <= ALTREF_FRAME; ++ref_frame) {
         assert(get_ref_frame_map_idx(cm, ref_frame) != INVALID_IDX);
         if (!current_frame->frame_refs_short_signaling) {
           if (cpi->ppi->rtc_ref.set_ref_frame_config &&
-              first_ref_map_idx != INVALID_IDX && num_references == 1 &&
+              first_ref_map_idx != INVALID_IDX &&
               !seq_params->order_hint_info.enable_order_hint) {
             // For the usage of set_ref_frame_config:
             // for any reference not used set their ref_map_idx
@@ -3126,6 +3122,7 @@ static inline void write_uncompressed_header_obu(
             const int map_idx = cpi->ppi->rtc_ref.reference[ref_frame - 1]
                                     ? get_ref_frame_map_idx(cm, ref_frame)
                                     : first_ref_map_idx;
+            cm->remapped_ref_idx[ref_frame - 1] = map_idx;
             aom_wb_write_literal(wb, map_idx, REF_FRAMES_LOG2);
           } else {
             aom_wb_write_literal(wb, get_ref_frame_map_idx(cm, ref_frame),
