a0b6b00031490705682f9548cf7a7558306699a2
Marco Paniconi <marpan@google.com>
2024-11-24 18:14:58 -0800

    rtc-screen: Prune palette testing for speed 11
    On scene changes for speed 11 screen mode:
    prune the testing of the palette mode to reduce
    spikes in encode_time, as observed in the issue below.
    
    The pruning is done when the palette mode is not expected
    to be so effective, so there is a reduction in the big
    encode_time spikes, with minimal/acceptable quality loss.
    
    Bug: 380247338
    Change-Id: If32179ceb438922304315fc5260a11c58103660f

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -3121,7 +3121,8 @@ static inline bool enable_palette(AV1_COMP *cpi, bool is_mode_intra,
                                   BLOCK_SIZE bsize,
                                   unsigned int source_variance,
                                   int force_zeromv_skip, int skip_idtx_palette,
-                                  int force_palette_test) {
+                                  int force_palette_test,
+                                  unsigned int best_intra_sad_norm) {
   if (!cpi->oxcf.tool_cfg.enable_palette) return false;
   if (!av1_allow_palette(cpi->common.features.allow_screen_content_tools,
                          bsize)) {
@@ -3134,6 +3135,10 @@ static inline bool enable_palette(AV1_COMP *cpi, bool is_mode_intra,
        bsize > BLOCK_16X16)) {
     return false;
   }
+
+  if (prune_palette_testing_sad(cpi, source_variance) &&
+      best_intra_sad_norm < 30)
+    return false;
 
   if ((is_mode_intra || force_palette_test) && source_variance > 0 &&
       !force_zeromv_skip &&
@@ -3487,12 +3492,13 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
   }
 
   // Evaluate Intra modes in inter frame
+  unsigned int best_intra_sad_norm = UINT_MAX;
   if (!x->force_zeromv_skip_for_blk)
     av1_estimate_intra_mode(cpi, x, bsize, best_early_term,
                             search_state.ref_costs_single[INTRA_FRAME],
                             reuse_inter_pred, &orig_dst, tmp_buffer,
                             &this_mode_pred, &search_state.best_rdc,
-                            best_pickmode, ctx);
+                            best_pickmode, ctx, &best_intra_sad_norm);
 
   int skip_idtx_palette = (x->color_sensitivity[COLOR_SENS_IDX(AOM_PLANE_U)] ||
                            x->color_sensitivity[COLOR_SENS_IDX(AOM_PLANE_V)]) &&
@@ -3501,7 +3507,11 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
 
   bool try_palette = enable_palette(
       cpi, is_mode_intra(best_pickmode->best_mode), bsize, x->source_variance,
-      x->force_zeromv_skip_for_blk, skip_idtx_palette, force_palette_test);
+      x->force_zeromv_skip_for_blk, skip_idtx_palette, force_palette_test,
+      best_intra_sad_norm);
+
+  if (try_palette && prune_palette_testing_sad(cpi, x->source_variance))
+    x->color_palette_thresh = 32;
 
   // Perform screen content mode evaluation for non-rd
   handle_screen_content_mode_nonrd(
