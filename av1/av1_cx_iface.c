620502d935e54d1c5fa8510e38c2584a7ead35cc
Wan-Teh Chang <wtc@google.com>
2024-12-02 10:42:09 -0800

    Cosmetic: Spell "allintra" as "all-intra"
    Change-Id: Ief37d8908572cc746d5737f05507f3131e8dd1ff

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -1239,7 +1239,7 @@ static void set_encoder_config(AV1EncoderConfig *oxcf,
   // Set cost update frequency configuration.
   oxcf->cost_upd_freq.coeff = (COST_UPDATE_TYPE)extra_cfg->coeff_cost_upd_freq;
   oxcf->cost_upd_freq.mode = (COST_UPDATE_TYPE)extra_cfg->mode_cost_upd_freq;
-  // Avoid MV cost update for allintra encoding mode.
+  // Avoid MV cost update for all-intra encoding mode.
   oxcf->cost_upd_freq.mv = (cfg->kf_max_dist != 0)
                                ? (COST_UPDATE_TYPE)extra_cfg->mv_cost_upd_freq
                                : COST_UPD_OFF;
